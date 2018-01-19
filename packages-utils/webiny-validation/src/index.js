57b6d99cda31b36c67c7810678c8d3dd00f560ca
Adrian Smijulj <adrian1358@gmail.com>
2018-01-19 10:23:01 +0100

    feat(webiny-validation): Implement option that enables users to execute data validation synchronously.

--- packages-utils/webiny-validation/src/index.js
+++ packages-utils/webiny-validation/src/index.js
@@ -2,66 +2,93 @@ const _ = require('lodash');
 const ValidationError = require('./validationError');
 
 class Validation {
-    constructor() {
-        this.validators = require('./validators');
-    }
+	constructor() {
+		this.validators = require('./validators');
+	}
 
-    setValidator(name, callable) {
-        this.validators[name] = callable;
-        return this;
-    }
+	setValidator(name, callable) {
+		this.validators[name] = callable;
+		return this;
+	}
 
-    getValidator(name) {
-        if (!this.validators[name]) {
-            throw new ValidationError('Validator `' + name + '` does not exist!', name);
-        }
-        return this.validators[name];
-    }
-
-    parseValidateProperty(validators) {
-        if (!validators) {
-            return false;
-        }
-
-        let validate = validators;
-        if (_.isString(validators)) {
-            validate = validators.split(',');
-        }
-
-        validators = {};
-        validate.forEach(v => {
-            let validator = null;
-            let vName = null;
-            if (_.isString(v)) {
-                validator = _.trim(v).split(':');
-                vName = validator.shift();
-            } else {
-                validator = v;
-                vName = _.uniqueId('validator');
-            }
-            validators[vName] = validator;
-        });
-        return validators;
-    }
-
-    async validate(value, validators) {
-        if (_.isString(validators)) {
-            validators = this.parseValidateProperty(validators);
-        }
-
-        if (!validators) {
-        	return;
-        }
-
-		for (const [name, parameters] of Object.entries(validators)) {
-            const validator = this.getValidator(name);
-            try {
-                await validator(value, parameters);
-            } catch (e) {
-                throw new ValidationError(e.message, name, value);
-            }
+	getValidator(name) {
+		if (!this.validators[name]) {
+			throw new ValidationError('Validator `' + name + '` does not exist!', name);
 		}
-    }
+		return this.validators[name];
+	}
+
+	parseValidateProperty(validators) {
+		if (!validators) {
+			return false;
+		}
+
+		let validate = validators;
+		if (_.isString(validators)) {
+			validate = validators.split(',');
+		}
+
+		validators = {};
+		validate.forEach(v => {
+			let validator = null;
+			let vName = null;
+			if (_.isString(v)) {
+				validator = _.trim(v).split(':');
+				vName = validator.shift();
+			} else {
+				validator = v;
+				vName = _.uniqueId('validator');
+			}
+			validators[vName] = validator;
+		});
+		return validators;
+	}
+
+	validate(value, validators, options = {}) {
+		if (_.isString(validators)) {
+			validators = this.parseValidateProperty(validators);
+		}
+
+		if (!validators) {
+			return;
+		}
+
+		if (options.async === false) {
+			return this.__validateSync(value, validators, options);
+		}
+
+		return this.__validateAsync(value, validators, options);
+	}
+
+	async __validateAsync(value, validators, options = {}) {
+		for (const [name, parameters] of Object.entries(validators)) {
+			const validator = this.getValidator(name);
+			try {
+				await validator(value, parameters);
+			} catch (e) {
+				if (options.throw === false) {
+					return {message: e.message, name, value};
+				}
+				throw new ValidationError(e.message, name, value);
+			}
+		}
+		return true;
+	}
+
+	__validateSync(value, validators, options = {}) {
+		for (const [name, parameters] of Object.entries(validators)) {
+			const validator = this.getValidator(name);
+			try {
+				validator(value, parameters);
+			} catch (e) {
+				if (options.throw === false) {
+					return {message: e.message, name, value};
+				}
+				throw new ValidationError(e.message, name, value);
+			}
+			return true;
+		}
+	}
 }
 
 const validation = new Validation();
---
+++ packages-utils/webiny-validation/tests/syncAsyncValidation.test.js
@@ -0,0 +1,37 @@
+const {validation, ValidationError} = require('./../src');
+import {assert} from 'chai';
+
+describe('multiple validators test', () => {
+	it('by default it must validate asynchronously', async () => {
+		const valid = validation.validate('test', 'required');
+		assert.instanceOf(valid, Promise);
+		assert.isTrue(await valid);
+
+		let invalid = validation.validate('', 'required');
+		assert.instanceOf(invalid, Promise);
+
+		let error = null;
+		try {
+			await invalid;
+		} catch (e) {
+			error = e;
+		}
+
+		assert.instanceOf(error, ValidationError);
+
+	});
+
+	it('should validate synchronously when options\' async flag is set to false', async () => {
+		assert.isTrue(validation.validate('test', 'required', {async: false}));
+
+		let error = null;
+		try {
+			validation.validate('', 'required', {async: false});
+		} catch (e) {
+			error = e;
+		}
+
+		assert.instanceOf(error, ValidationError);
+
+	});
+});
