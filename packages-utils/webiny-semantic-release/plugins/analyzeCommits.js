f91146ced340c1a65cc0c9cae082f906876dc2c0
Pavel910 <pavel@webiny.com>
2018-02-16 18:17:43 +0100

    feat(release): initial release.
    affects: webiny-api-security, webiny-api, webiny-backend-app, webiny-client, webiny-skeleton-app,
    webiny-ui, webiny-compose, webiny-data-extractor, webiny-entity-memory, webiny-entity-mysql,
    webiny-entity, webiny-file-storage-local, webiny-file-storage-s3, webiny-file-storage, webiny-model,
    webiny-scripts, webiny-service-manager, webiny-sql-generator, webiny-validation

--- packages-utils/webiny-semantic-release/plugins/analyzeCommits.js
+++ packages-utils/webiny-semantic-release/plugins/analyzeCommits.js
@@ -2,36 +2,20 @@ const commitAnalyzer = require("@semantic-release/commit-analyzer");
 const readPkg = require("read-pkg");
 const getRelevantCommits = require("./utils/relevantCommits");
 
-const winston = require("winston");
-winston.cli();
-const logger = new winston.Logger({
-    transports: [new winston.transports.Console()]
-});
-const log = logger.cli();
-
 module.exports = function(pluginConfig, _ref) {
     return new Promise(resolve => {
         readPkg(process.cwd()).then(pkg => {
             const relevantCommits = getRelevantCommits(_ref.commits, pkg);
 
-            commitAnalyzer({}, Object.assign(_ref, { commits: relevantCommits }), function(
-                err,
-                type
-            ) {
-                log.info(
-                    "Analyzed",
-                    relevantCommits.length,
-                    "/",
-                    _ref.commits.length,
-                    "commits to determine type",
-                    type,
-                    "for",
-                    pkg.name
+            commitAnalyzer({}, Object.assign(_ref, { commits: relevantCommits })).then(type => {
+                console.log(
+                    `Analyzed ${relevantCommits.length}/${
+                        _ref.commits.length
+                    } commits to determine type ${type} for ${pkg.name}`
                 );
                 relevantCommits.length &&
-                    log.info(
-                        "Relevant commits:\n*",
-                        relevantCommits.map(c => c.subject).join("\n* ")
+                    console.log(
+                        `Relevant commits:\n* ${relevantCommits.map(c => c.subject).join("\n* ")}`
                     );
                 resolve(type);
             });
