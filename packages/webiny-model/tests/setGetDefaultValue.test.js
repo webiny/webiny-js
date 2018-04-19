d733dc49a58191ff06e7e4ad2c55a71fdf2c7254
Adrian Smijulj <adrian1358@gmail.com>
2018-04-19 09:44:44 +0200

    test(setDefaultValue): test - ensure setValue is called when setting default one.
    affects: webiny-model

--- packages/webiny-model/tests/setGetDefaultValue.test.js
+++ packages/webiny-model/tests/setGetDefaultValue.test.js
@@ -1,5 +1,7 @@
 import { assert } from "chai";
 import Model from "./../src/model";
+import sinon from "sinon";
+const sandbox = sinon.sandbox.create();
 
 describe("setDefaultValue test", function() {
     it("should set/get default values", async () => {
@@ -31,5 +33,24 @@ describe("setDefaultValue test", function() {
         assert.equal(model.something2, 555);
         assert.equal(model.something3, 666);
         assert.instanceOf(model.createdOn, Date);
+    });
+
+    it("should call setValue for setting default value and mark attribute as dirty", async () => {
+        const model = new Model(function() {
+            this.attr("email")
+                .char()
+                .setValidators("required,email")
+                .setDefaultValue("test@gmail.com");
+        });
+
+        const setValueSpy = sandbox.spy(model.getAttribute("email"), "setValue");
+        await model.populate({});
+
+        assert.equal(model.email, "test@gmail.com");
+        assert.isTrue(model.getAttribute("email").value.isDirty());
+        assert.equal(setValueSpy.callCount, 1);
+        assert.equal(setValueSpy.getCall(0).args[0], "test@gmail.com");
+
+        setValueSpy.restore();
     });
 });
