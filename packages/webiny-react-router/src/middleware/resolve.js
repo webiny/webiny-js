fd9f413f2b9f027930150376bb4e6531c43c7d7a
Pavel910 <pavel@webiny.com>
2018-04-20 15:28:34 +0200

    fix: fix URL generation with basename and add missing resolve params.
    affects: webiny-react-router

--- packages/webiny-react-router/src/middleware/resolve.js
+++ packages/webiny-react-router/src/middleware/resolve.js
@@ -1,10 +1,10 @@
 export default () => {
     return async (params, next) => {
         // Resolve middleware
-        const { route } = params;
+        const { route, match } = params;
 
         if (route.resolve) {
-            params.resolve = await route.resolve({ route });
+            params.resolve = await route.resolve({ route, match });
         }
 
         next();
