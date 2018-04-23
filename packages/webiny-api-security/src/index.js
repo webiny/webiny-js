82f6cd0b22168e3ed2064decc05325ffe5cf7614
Adrian Smijulj <adrian1358@gmail.com>
2018-04-23 20:27:37 +0200

    WIP(authorization): implement authorization middleware.
    affects: webiny-api-security

--- packages/webiny-api-security/src/index.js
+++ packages/webiny-api-security/src/index.js
@@ -7,4 +7,5 @@ export { default as Permission } from "./entities/permission.entity";
 export { default as JwtToken } from "./tokens/jwtToken";
 export { default as credentialsStrategy } from "./strategies/credentialsStrategy";
 export { default as authentication } from "./middleware/authentication";
+export { default as authorization } from "./middleware/authorization";
 export { default as app } from "./app";
--- packages/webiny-api-security/src/middleware/authorization.js
+++ packages/webiny-api-security/src/middleware/authorization.js
@@ -1,6 +1,4 @@
 // @flow
-import { app } from "webiny-api";
-import type { MatchedApiMethod } from "webiny-api";
 
 export default () => {
     /**
@@ -9,19 +7,22 @@ export default () => {
      *
      * @param {Object} params
      * @param {Request} params.req
-     * @param {ApiMethod} params.apiMethod
      * @param {Api} params.app
      * @param next
      * @return {Promise<void>}
      */
-    return async (
-        params: { req: express$Request, matchedApiMethod: MatchedApiMethod },
-        next: Function
-    ) => {
-        const { req, matchedApiMethod } = params;
-        await app.services
-            .get("authorization")
-            .canExecute(matchedApiMethod.getApiMethod(), req.identity);
+    return async (params, next: Function) => {
+        const identity = params.req.identity;
+        if (!identity) {
+            return;
+        }
+
+        // TODO: we must remove fields which cannot be fetched.
+        /*    params.graphql.query.definitions.forEach(operation => {
+            // const name = operation.selectionSet.selections[0].name.value;
+            // const roles = identity.roles;
+        });*/
+
         next();
     };
 };
