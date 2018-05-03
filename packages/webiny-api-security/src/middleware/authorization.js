e9c56f35cf1415ef271a731b49cadbbcd6969dd6
Adrian Smijulj <adrian1358@gmail.com>
2018-05-03 18:45:22 +0200

    WIP(authorization): implement field scope checks.
    affects: webiny-api-security

--- packages/webiny-api-security/src/middleware/authorization.js
+++ packages/webiny-api-security/src/middleware/authorization.js
@@ -1,5 +1,6 @@
 // @flow
 import Permission from "./../entities/permission.entity";
+import _ from "lodash";
 
 export default () => {
     /**
@@ -12,41 +13,72 @@ export default () => {
     return async (params, next: Function) => {
         const identity = params.req.identity;
 
-        let scope = {};
+        let identityScope = {};
         if (identity) {
             // Scope received from identity already possesses 'anonymous' permission's scope.
-            scope = await identity.scope;
+            identityScope = await identity.scope;
         } else {
             const anonymousPermission = await Permission.findOne({ query: { slug: "anonymous" } });
             for (let operationName in anonymousPermission.scope) {
-                if (!scope[operationName]) {
-                    scope[operationName] = [];
+                if (!identityScope[operationName]) {
+                    identityScope[operationName] = [];
                 }
-                scope[operationName].push(anonymousPermission.scope[operationName]);
+                identityScope[operationName].push(anonymousPermission.scope[operationName]);
             }
         }
 
         for (let i = 0; i < params.graphql.documentAST.definitions.length; i++) {
-            let operation = params.graphql.documentAST.definitions[i];
-            const name = operation.selectionSet.selections[0].name.value;
+            let operationDefinition = params.graphql.documentAST.definitions[i];
 
-            if (!scope[name]) {
-                throw Error(`Not authorized to access "${name}" operation.`);
-            }
+            for (let j = 0; j < operationDefinition.selectionSet.selections.length; j++) {
+                let selection = operationDefinition.selectionSet.selections[j];
+                if (selection.kind === "Field") {
+                    const fieldName = selection.name.value;
+                    const fieldScopes = identityScope[fieldName] || [];
 
-            let hasAccess = false;
-            scope[name].forEach(item => {
-                if (item === true || item instanceof Object) {
-                    hasAccess = true;
-                    return false;
+                    let hasAccess = false;
+                    fieldScopes.forEach(fieldScope => {
+                        if (fieldScope === true || fieldScope instanceof Object) {
+                            hasAccess = true;
+                            return false;
+                        }
+                    });
+
+                    if (!hasAccess) {
+                        throw Error(`Not authorized to access "${fieldName}" operation.`);
+                    }
+
+                    if (fieldName === "listSecurityPermissions") {
+                        if (selection.selectionSet) {
+                            applyScope(selection.selectionSet, fieldScopes);
+                        }
+                    }
                 }
-            });
-
-            if (!hasAccess) {
-                throw Error(`Not authorized to access "${name}" operation.`);
             }
         }
 
         next();
     };
+};
+
+const applyScope = (selectionSet, scopesList, parentFields = "") => {
+    selectionSet.selections.forEach(selection => {
+        if (selection.kind === "Field") {
+            const fieldName = selection.name.value;
+
+            // let hasAccess = false;
+            scopesList.forEach(scope => {
+                const fullPath = parentFields ? `${parentFields}.${fieldName}` : fieldName;
+                if (_.get(scope, fullPath)) {
+                    // hasAccess = true;
+                    if (selection.selectionSet) {
+                        applyScope(selection.selectionSet, scopesList, fullPath);
+                    }
+                    return false;
+                }
+
+                // TODO: remove keys.
+            });
+        }
+    });
 };
