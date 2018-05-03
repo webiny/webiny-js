cc7bb298647e06dbcbf861b5e475def62a201587
Adrian Smijulj <adrian1358@gmail.com>
2018-05-03 10:11:47 +0200

    WIP(authorization): implement access-to-operations check.
    affects: webiny-api-security

--- packages/webiny-api-security/src/middleware/authorization.js
+++ packages/webiny-api-security/src/middleware/authorization.js
@@ -1,4 +1,6 @@
 // @flow
+import Permission from "./../entities/permission.entity";
+
 export default () => {
     /**
      * Authorization middleware.
@@ -9,17 +11,42 @@ export default () => {
      */
     return async (params, next: Function) => {
         const identity = params.req.identity;
-        if (!identity) {
-            next();
-        } else {
-            // const scope = await identity.scope;
-            // TODO: we must remove fields which cannot be fetched.
-            /*    params.graphql.query.definitions.forEach(operation => {
-                // const name = operation.selectionSet.selections[0].name.value;
-                // const roles = identity.roles;
-            });*/
 
-            next();
+        let scope = {};
+        if (identity) {
+            // Scope received from identity already possesses 'anonymous' permission's scope.
+            scope = await identity.scope;
+        } else {
+            const anonymousPermission = await Permission.findOne({ query: { slug: "anonymous" } });
+            for (let operationName in anonymousPermission.scope) {
+                if (!scope[operationName]) {
+                    scope[operationName] = [];
+                }
+                scope[operationName].push(anonymousPermission.scope[operationName]);
+            }
         }
+
+        for (let i = 0; i < params.graphql.documentAST.definitions.length; i++) {
+            let operation = params.graphql.documentAST.definitions[i];
+            const name = operation.selectionSet.selections[0].name.value;
+
+            if (!scope[name]) {
+                throw Error(`Not authorized to access "${name}" operation.`);
+            }
+
+            let hasAccess = false;
+            scope[name].forEach(item => {
+                if (item === true || item instanceof Object) {
+                    hasAccess = true;
+                    return false;
+                }
+            });
+
+            if (!hasAccess) {
+                throw Error(`Not authorized to access "${name}" operation.`);
+            }
+        }
+
+        next();
     };
 };
