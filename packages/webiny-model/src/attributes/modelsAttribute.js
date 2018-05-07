af2e3efb7819e63804d7fac690be67d0e805aa12
Adrian Smijulj <adrian1358@gmail.com>
2018-05-07 22:59:32 +0200

    feat(models attribute): enable key/value support on models attribute.
    affects: webiny-model
    
    Sometimes, an array of models is not suitable, but key/value format is. Added a flag which enables

--- packages/webiny-model/src/attributes/modelsAttribute.js
+++ packages/webiny-model/src/attributes/modelsAttribute.js
@@ -7,8 +7,14 @@ import { AttributesContainer } from "../index";
 
 class ModelsAttribute extends Attribute {
     modelClass: Class<Model>;
+    keyValue: boolean;
 
-    constructor(name: string, attributesContainer: AttributesContainer, model: Class<Model>) {
+    constructor(
+        name: string,
+        attributesContainer: AttributesContainer,
+        model: Class<Model>,
+        keyValue: boolean = false
+    ) {
         super(name, attributesContainer);
 
         // If provided class is not a subclass of Model, we must throw an error.
@@ -19,8 +25,10 @@ class ModelsAttribute extends Attribute {
             );
         }
 
-        this.value.current = [];
         this.modelClass = model;
+
+        this.keyValue = keyValue;
+        this.value.current = this.keyValue ? {} : [];
     }
 
     getModelClass(): Class<Model> {
@@ -39,69 +47,131 @@ class ModelsAttribute extends Attribute {
 
         const finalValues = this.onSetCallback(values);
 
-        // Even if the value is invalid (eg. a string), we allow it here, but calling validate() will fail.
-        if (!(finalValues instanceof Array)) {
-            this.value.setCurrent(finalValues);
-            return;
-        }
-
-        let newValues = [];
-        for (let i = 0; i < finalValues.length; i++) {
-            if (_.isPlainObject(finalValues[i])) {
-                const newValue = this.getModelInstance();
-                newValue.populate(_.clone(finalValues[i]));
-                newValues.push(newValue);
-            } else {
-                newValues.push(finalValues[i]);
+        if (this.keyValue) {
+            // Even if the value is invalid (eg. a string), we allow it here, but calling validate() will fail.
+            if (!(finalValues instanceof Object)) {
+                this.value.setCurrent(finalValues);
+                return;
             }
+
+            let newValues = {};
+            for (let key in finalValues) {
+                if (_.isPlainObject(finalValues[key])) {
+                    const newValue = this.getModelInstance();
+                    newValue.populate(_.clone(finalValues[key]));
+                    newValues[key] = newValue;
+                } else {
+                    newValues[key] = finalValues[key];
+                }
+            }
+            this.value.setCurrent(newValues);
+        } else {
+            // Even if the value is invalid (eg. a string), we allow it here, but calling validate() will fail.
+            if (!(finalValues instanceof Array)) {
+                this.value.setCurrent(finalValues);
+                return;
+            }
+
+            let newValues = [];
+            for (let i = 0; i < finalValues.length; i++) {
+                if (_.isPlainObject(finalValues[i])) {
+                    const newValue = this.getModelInstance();
+                    newValue.populate(_.clone(finalValues[i]));
+                    newValues.push(newValue);
+                } else {
+                    newValues.push(finalValues[i]);
+                }
+            }
+            this.value.setCurrent(newValues);
         }
-        this.value.setCurrent(newValues);
     }
 
     async getJSONValue(): Promise<mixed> {
         const value = this.value.getCurrent();
-        if (value instanceof Array) {
-            const json = [];
-            for (let i = 0; i < value.length; i++) {
-                json.push({});
+        if (this.keyValue) {
+            if (value instanceof Object) {
+                return {};
             }
-            return json;
-        }
 
-        return value;
+            return value;
+        } else {
+            if (value instanceof Array) {
+                const json = [];
+                for (let i = 0; i < value.length; i++) {
+                    json.push({});
+                }
+                return json;
+            }
+
+            return value;
+        }
     }
 
     async getStorageValue(): Promise<Array<Object>> {
         const value = this.getValue();
-        if (value instanceof Array) {
-            const data = [];
-            for (let i = 0; i < value.length; i++) {
-                const model = value[i];
-                const json = {};
-                for (let name in model.getAttributes()) {
-                    const attribute = model.getAttribute(name);
-                    // $FlowIgnore - we can be sure we have attribute because it's pulled from list of attributes, using getAttributes() method.
-                    if (attribute.getToStorage()) {
+        if (this.keyValue) {
+            if (value instanceof Object) {
+                const data = {};
+                for (let key in value) {
+                    const model = value[key];
+                    const json = {};
+                    for (let name in model.getAttributes()) {
+                        const attribute = model.getAttribute(name);
                         // $FlowIgnore - we can be sure we have attribute because it's pulled from list of attributes, using getAttributes() method.
-                        json[name] = await attribute.getStorageValue();
+                        if (attribute.getToStorage()) {
+                            // $FlowIgnore - we can be sure we have attribute because it's pulled from list of attributes, using getAttributes() method.
+                            json[name] = await attribute.getStorageValue();
+                        }
                     }
+                    data[key] = json;
                 }
-                data.push(json);
+                return data;
             }
-            return data;
+            return {};
+        } else {
+            if (value instanceof Array) {
+                const data = [];
+                for (let i = 0; i < value.length; i++) {
+                    const model = value[i];
+                    const json = {};
+                    for (let name in model.getAttributes()) {
+                        const attribute = model.getAttribute(name);
+                        // $FlowIgnore - we can be sure we have attribute because it's pulled from list of attributes, using getAttributes() method.
+                        if (attribute.getToStorage()) {
+                            // $FlowIgnore - we can be sure we have attribute because it's pulled from list of attributes, using getAttributes() method.
+                            json[name] = await attribute.getStorageValue();
+                        }
+                    }
+                    data.push(json);
+                }
+                return data;
+            }
+            return [];
         }
-        return [];
     }
 
     setStorageValue(value: mixed): this {
-        const newValue = [];
-        if (Array.isArray(value)) {
-            value.forEach(item => {
-                newValue.push(this.getModelInstance().populateFromStorage(item));
-            });
+        if (this.keyValue) {
+            const newValue = {};
+            if (value instanceof Object) {
+                for (let key in value) {
+                    const item = value[key];
+                    newValue[key] = this.getModelInstance().populateFromStorage(item);
+                }
 
-            this.value.setCurrent(newValue, { skipDifferenceCheck: true });
+                this.value.setCurrent(newValue, { skipDifferenceCheck: true });
+            }
+        } else {
+            const newValue = [];
+            if (Array.isArray(value)) {
+                value.forEach(item => {
+                    newValue.push(this.getModelInstance().populateFromStorage(item));
+                });
+
+                this.value.setCurrent(newValue, { skipDifferenceCheck: true });
+            }
         }
+
         return this;
     }
 
@@ -109,42 +179,82 @@ class ModelsAttribute extends Attribute {
      * If value is assigned (checked in the parent validate call), it must by an instance of Model.
      */
     async validateType(value: mixed) {
-        if (value instanceof Array) {
-            return;
+        if (this.keyValue) {
+            if (value instanceof Object) {
+                return;
+            }
+            this.expected("object", typeof value);
+        } else {
+            if (value instanceof Array) {
+                return;
+            }
+            this.expected("array", typeof value);
         }
-        this.expected("array", typeof value);
     }
 
     async validateValue(value: mixed) {
-        const arrayValue = ((value: any): Array<Model>);
-        // This validates on all of the model's levels.
-        const errors = [];
+        if (this.keyValue) {
+            const objectValue = ((value: any): Object);
+            // This validates on all of the model's levels.
+            const errors = [];
 
-        for (let i = 0; i < arrayValue.length; i++) {
-            const current = arrayValue[i];
-            if (!(current instanceof this.getModelClass())) {
-                errors.push({
-                    code: ModelError.INVALID_ATTRIBUTE,
-                    data: {
-                        index: i
-                    },
-                    message: `Validation failed, item at index ${i} not an instance of Model class.`
-                });
-                continue;
+            for (let key in objectValue) {
+                const current = objectValue[key];
+                if (!(current instanceof this.getModelClass())) {
+                    errors.push({
+                        code: ModelError.INVALID_ATTRIBUTE,
+                        data: {
+                            index: key
+                        },
+                        message: `Validation failed, item at index ${key} not an instance of Model class.`
+                    });
+                    continue;
+                }
+                try {
+                    await current.validate();
+                } catch (e) {
+                    errors.push({
+                        code: e.code,
+                        data: { index: key, ...e.data },
+                        message: e.message
+                    });
+                }
             }
-            try {
-                await current.validate();
-            } catch (e) {
-                errors.push({
-                    code: e.code,
-                    data: { index: i, ...e.data },
-                    message: e.message
-                });
-            }
-        }
 
-        if (!_.isEmpty(errors)) {
-            throw new ModelError("Validation failed.", ModelError.INVALID_ATTRIBUTE, errors);
+            if (!_.isEmpty(errors)) {
+                throw new ModelError("Validation failed.", ModelError.INVALID_ATTRIBUTE, errors);
+            }
+        } else {
+            const arrayValue = ((value: any): Array<Model>);
+            // This validates on all of the model's levels.
+            const errors = [];
+
+            for (let i = 0; i < arrayValue.length; i++) {
+                const current = arrayValue[i];
+                if (!(current instanceof this.getModelClass())) {
+                    errors.push({
+                        code: ModelError.INVALID_ATTRIBUTE,
+                        data: {
+                            index: i
+                        },
+                        message: `Validation failed, item at index ${i} not an instance of Model class.`
+                    });
+                    continue;
+                }
+                try {
+                    await current.validate();
+                } catch (e) {
+                    errors.push({
+                        code: e.code,
+                        data: { index: i, ...e.data },
+                        message: e.message
+                    });
+                }
+            }
+
+            if (!_.isEmpty(errors)) {
+                throw new ModelError("Validation failed.", ModelError.INVALID_ATTRIBUTE, errors);
+            }
         }
     }
 }
---
+++ packages/webiny-model/tests/attributes/modelsObject.test.js
@@ -0,0 +1,438 @@
+import { assert } from "chai";
+import ModelError from "./../../src/modelError";
+import { Model } from "../../../webiny-model/src";
+
+describe("attribute models test", function() {
+    class Model1 extends Model {
+        constructor() {
+            super();
+            this.attr("name")
+                .char()
+                .setValidators("required");
+            this.attr("number").integer();
+            this.attr("type")
+                .char()
+                .setValidators("in:cat:dog:mouse:parrot");
+        }
+    }
+
+    class Model2 extends Model {
+        constructor() {
+            super();
+            this.attr("firstName")
+                .char()
+                .setValidators("required");
+            this.attr("lastName")
+                .char()
+                .setValidators("required");
+            this.attr("enabled").boolean();
+        }
+    }
+
+    const model = new Model(function() {
+        this.attr("attribute1").models(Model1, true);
+        this.attr("attribute2").models(Model2, true);
+    });
+
+    it("should not accept inline functions, must always receive a Model class", async () => {
+        class ModelsAttributeWithoutModelsClassModel extends Model {
+            constructor() {
+                super();
+                this.attr("modelsAttribute1").models(() => {});
+            }
+        }
+
+        try {
+            new ModelsAttributeWithoutModelsClassModel();
+        } catch (e) {
+            assert.equal(
+                e.message,
+                `"models" attribute "modelsAttribute1" received an invalid class (subclass of Model is required).`
+            );
+            return;
+        }
+
+        throw Error(`Error should've been thrown.`);
+    });
+
+    it("should fail - attributes should accept object with models as values", async () => {
+        model.attribute1 = new Model1();
+        assert.instanceOf(model.attribute1, Model1);
+
+        model.attribute2 = new Model1();
+        assert.instanceOf(model.attribute2, Model1);
+
+        try {
+            await model.validate();
+        } catch (e) {
+            assert.equal(e.data.invalidAttributes.attribute1.code, ModelError.INVALID_ATTRIBUTE);
+            assert.equal(e.data.invalidAttributes.attribute2.code, ModelError.INVALID_ATTRIBUTE);
+            return;
+        }
+
+        throw Error("Error should've been thrown.");
+    });
+
+    it("should pass - empty objects set", async () => {
+        model.attribute1 = {};
+        model.attribute2 = {};
+        await model.validate();
+    });
+
+    it("should fail - object with empty plain objects as values set - nested validation must be triggered", async () => {
+        model.attribute1 = { first: {}, second: {} };
+        model.attribute2 = { first: {}, second: {}, third: {} };
+        try {
+            await model.validate();
+        } catch (e) {
+            const attr1 = e.data.invalidAttributes.attribute1;
+            assert.lengthOf(attr1.data, 2);
+            assert.equal(attr1.data[0].data.index, 0);
+            assert.equal(
+                attr1.data[0].data.invalidAttributes.name.code,
+                ModelError.INVALID_ATTRIBUTE
+            );
+            assert.equal(attr1.data[0].data.invalidAttributes.name.data.validator, "required");
+            assert.notExists(attr1.data[0].data.invalidAttributes.type);
+
+            const attr2 = e.data.invalidAttributes.attribute2;
+            assert.lengthOf(attr2.data, 3);
+            assert.equal(attr2.data[0].data.index, 0);
+            assert.equal(attr2.data[1].data.index, 1);
+            assert.equal(attr2.data[2].data.index, 2);
+
+            assert.equal(
+                attr2.data[0].data.invalidAttributes.firstName.code,
+                ModelError.INVALID_ATTRIBUTE
+            );
+            assert.equal(
+                attr2.data[0].data.invalidAttributes.lastName.code,
+                ModelError.INVALID_ATTRIBUTE
+            );
+            assert.notExists(attr2.data[0].data.invalidAttributes.enabled);
+
+            return;
+        }
+        throw Error("Error should've been thrown.");
+    });
+
+    it("should pass - valid data sent", async () => {
+        model.attribute1 = {
+            first: { name: "Enlai", type: "dog" },
+            second: { name: "Rocky", type: "dog" },
+            third: { name: "Lina", type: "parrot" }
+        };
+        model.attribute2 = {
+            first: { firstName: "John", lastName: "Doe" },
+            second: { firstName: "Jane", lastName: "Doe" }
+        };
+        await model.validate();
+    });
+
+    it("should fail - all good except last item of attribute1", async () => {
+        model.attribute1 = [
+            { name: "Enlai", type: "dog" },
+            { name: "Rocky", type: "dog" },
+            { name: "Lina", type: "bird" }
+        ];
+        model.attribute2 = [
+            { firstName: "John", lastName: "Doe" },
+            { firstName: "Jane", lastName: "Doe" }
+        ];
+
+        try {
+            await model.validate();
+        } catch (e) {
+            const attr1 = e.data.invalidAttributes.attribute1;
+            assert.lengthOf(attr1.data, 1);
+            assert.equal(attr1.data[0].data.index, 2);
+            assert.equal(
+                attr1.data[0].data.invalidAttributes.type.code,
+                ModelError.INVALID_ATTRIBUTE
+            );
+            assert.equal(attr1.data[0].data.invalidAttributes.type.data.validator, "in");
+        }
+    });
+
+    it("should fail on validation since an item is not an object of any type", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = [{ name: "Enlai", type: "dog" }, { name: "Rocky", type: "dog" }, 123];
+
+        try {
+            await newModel.validate();
+        } catch (e) {
+            assert.equal(e.code, "INVALID_ATTRIBUTES");
+            assert.deepEqual(e.data, {
+                invalidAttributes: {
+                    attribute1: {
+                        code: "INVALID_ATTRIBUTE",
+                        data: [
+                            {
+                                code: "INVALID_ATTRIBUTE",
+                                data: {
+                                    index: 2
+                                },
+                                message:
+                                    "Validation failed, item at index 2 not an instance of Model class."
+                            }
+                        ],
+                        message: "Validation failed."
+                    }
+                }
+            });
+            return;
+        }
+
+        throw Error("Error should've been thrown.");
+    });
+
+    it("validation must be executed on both attribute and model level", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1")
+                .models(Model1)
+                .setValidators("required,minLength:2");
+        });
+
+        let error = null;
+        try {
+            await newModel.validate();
+        } catch (e) {
+            error = e;
+        }
+
+        assert.instanceOf(error, ModelError);
+        assert.deepEqual(error.data, {
+            invalidAttributes: {
+                attribute1: {
+                    code: "INVALID_ATTRIBUTE",
+                    data: {
+                        message: "Value is required.",
+                        value: null,
+                        validator: "required"
+                    },
+                    message: "Invalid attribute."
+                }
+            }
+        });
+
+        error = null;
+        try {
+            newModel.attribute1 = [{ name: "Enlai", type: "dog" }];
+            await newModel.validate();
+        } catch (e) {
+            error = e;
+        }
+
+        assert.instanceOf(error, ModelError);
+        assert.equal(error.data.invalidAttributes.attribute1.code, "INVALID_ATTRIBUTE");
+        assert.equal(error.data.invalidAttributes.attribute1.data.validator, "minLength");
+        assert.lengthOf(error.data.invalidAttributes.attribute1.data.value, 1);
+        assert.instanceOf(error.data.invalidAttributes.attribute1.data.value[0], Model);
+
+        newModel.attribute1 = [{ name: "Enlai", type: "dog" }, { name: "Enlai", type: "dog" }];
+        await newModel.validate();
+    });
+
+    it("should not set value if setOnce is enabled", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = [
+            { name: "Enlai", type: "dog" },
+            { name: "Rocky", type: "dog" },
+            { name: "Lina", type: "bird" }
+        ];
+
+        newModel.getAttribute("attribute1").setOnce();
+
+        await newModel.set("attribute1", null);
+
+        assert.equal(await newModel.get("attribute1.0.name"), "Enlai");
+        assert.equal(await newModel.get("attribute1.1.name"), "Rocky");
+        assert.equal(await newModel.get("attribute1.2.name"), "Lina");
+    });
+
+    it("getJSONValue must return an empty array if nothing was set", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        assert.deepEqual(await newModel.getAttribute("attribute1").getJSONValue(), []);
+    });
+
+    it("getStorageValue method must return empty array if nothing is set", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        assert.deepEqual(await newModel.getAttribute("attribute1").getStorageValue(), []);
+    });
+
+    it("getStorageValue method must return initially set value if it's not an array", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = null;
+        assert.deepEqual(await newModel.getAttribute("attribute1").getStorageValue(), []);
+    });
+
+    it("getStorageValue must iterate through all models and all of its attributes and return its storage values", async () => {
+        const storageModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+            this.attr("attribute2").models(Model2);
+        });
+
+        storageModel.attribute1 = [
+            { name: "Enlai", type: "dog" },
+            { name: "Rocky", type: "dog" },
+            { name: "Lina", type: "parrot" }
+        ];
+
+        storageModel.attribute2 = [
+            { firstName: "John", lastName: "Doe" },
+            { firstName: "Jane", lastName: "Doe" }
+        ];
+
+        const data = await storageModel.toStorage();
+
+        assert.deepEqual(data, {
+            attribute1: [
+                {
+                    number: null,
+                    name: "Enlai",
+                    type: "dog"
+                },
+                {
+                    number: null,
+                    name: "Rocky",
+                    type: "dog"
+                },
+                {
+                    number: null,
+                    name: "Lina",
+                    type: "parrot"
+                }
+            ],
+            attribute2: [
+                {
+                    enabled: null,
+                    firstName: "John",
+                    lastName: "Doe"
+                },
+                {
+                    enabled: null,
+                    firstName: "Jane",
+                    lastName: "Doe"
+                }
+            ]
+        });
+    });
+
+    it("setStorageValue must omit value if it is not an array", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.getAttribute("attribute1").setStorageValue([{ name: "one" }, { name: "two" }]);
+        assert.deepEqual(await newModel.toJSON("attribute1[name]"), {
+            attribute1: [
+                {
+                    name: "one"
+                },
+                {
+                    name: "two"
+                }
+            ]
+        });
+
+        newModel.getAttribute("attribute1").setStorageValue({});
+        newModel.getAttribute("attribute1").setStorageValue(null);
+        newModel.getAttribute("attribute1").setStorageValue(123);
+        assert.deepEqual(await newModel.toJSON("attribute1[name]"), {
+            attribute1: [{ name: "one" }, { name: "two" }]
+        });
+    });
+
+    it("when toJSON is called, it must return values correctly", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = [];
+        assert.deepEqual(await newModel.toJSON("attribute1.name"), {
+            attribute1: []
+        });
+
+        newModel.getAttribute("attribute1").setStorageValue([{ name: "one" }, { name: "two" }]);
+        assert.deepEqual(await newModel.toJSON("attribute1[name]"), {
+            attribute1: [{ name: "one" }, { name: "two" }]
+        });
+
+        newModel.attribute1 = null;
+        assert.deepEqual(await newModel.toJSON("attribute1.name"), {
+            attribute1: null
+        });
+    });
+
+    it("getJSONValue must return values correctly", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = [];
+        assert.deepEqual(await newModel.getAttribute("attribute1").getJSONValue(), []);
+
+        newModel.attribute1 = 123;
+        assert.deepEqual(await newModel.getAttribute("attribute1").getJSONValue(), 123);
+    });
+
+    it("getJSONValue must return empty objects as items", async () => {
+        const newModel = new Model(function() {
+            this.attr("attribute1").models(Model1);
+        });
+
+        newModel.attribute1 = [{ name: 123, age: 123 }, { name: 234, age: 456 }];
+        assert.deepEqual(await newModel.getAttribute("attribute1").getJSONValue(), [{}, {}]);
+    });
+
+    it("onSet/onGet must be triggered correctly", async () => {
+        const newModel = new Model(function() {
+            this.attr("someModels").models(Model1);
+        });
+
+        newModel.getAttribute("someModels").onSet(value => {
+            const final = [];
+            value.forEach((value, index) => {
+                final.push({ name: "index-" + index });
+            });
+            return final;
+        });
+
+        newModel.populate({
+            someModels: [
+                {
+                    name: "Webiny LTD",
+                    city: "London"
+                },
+                {
+                    name: "Webiny LTD 2",
+                    city: "London 2"
+                }
+            ]
+        });
+
+        assert.equal(newModel.someModels[0].name, "index-0");
+        assert.equal(newModel.someModels[1].name, "index-1");
+
+        newModel.getAttribute("someModels").onGet(() => {
+            return "random";
+        });
+
+        assert.equal(newModel.someModels, "random");
+    });
+});
