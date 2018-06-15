62e618b1936ead4bfe30c7367be7894a16435d02
Adrian Smijulj <adrian1358@gmail.com>
2018-06-15 10:41:15 +0200

    fix(import): fix invalid import.
    affects: webiny-api

--- packages/webiny-api/src/security/graphql/overrideCreateApiTokenMutation.js
+++ packages/webiny-api/src/security/graphql/overrideCreateApiTokenMutation.js
@@ -2,7 +2,7 @@ import { GraphQLNonNull } from "graphql";
 import GraphQLJSON from "graphql-type-json";
 import ApiToken from "./../../entities/ApiToken.entity";
 import { ModelError } from "webiny-model";
-import InvalidAttributesError from "./../../../src/graphql/utils/crud/InvalidAttributesError";
+import InvalidAttributesError from "./../../graphql/utils/crud/InvalidAttributesError";
 
 /**
  * We needed to override basic create operation because an additional token activation is needed after initial save.
