2b93e341d40c8e35c1c9fb32f39ccedc87ddecec
Pavel910 <pavel@webiny.com>
2018-06-16 20:49:39 +0200

    chore(examples): add cra example

---
+++ examples/cra/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
+
+if (module.hot) {
+    module.hot.accept();
+}
