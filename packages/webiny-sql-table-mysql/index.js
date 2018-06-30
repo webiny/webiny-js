fb45ea5b7083921859645cfb1b6fbcb059cef564
Adrian Smijulj <adrian1358@gmail.com>
2018-06-30 22:04:02 +0200

    chore(tests): upgrade tests - webiny-sql-table-mysql.
    affects: webiny-sql-table-mysql

---
+++ packages/webiny-sql-table-mysql/index.js
@@ -0,0 +1,4 @@
+// @flow
+export { default as ColumnsContainer } from "./src/columnsContainer";
+export { default as IndexesContainer } from "./src/indexesContainer";
+export { default as MySQLDriver } from "./src/mysqlDriver";
