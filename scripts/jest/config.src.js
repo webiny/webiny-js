d90bb8560b550442fc8b47182e67ca76a6df4a68
Adrian Smijulj <adrian1358@gmail.com>
2018-07-01 11:35:49 +0200

    test(configs): folders must be properly loaded while running tests.

--- scripts/jest/config.src.js
+++ scripts/jest/config.src.js
@@ -1,4 +1,17 @@
-"use strict";
-
+// @flow
 const baseConfig = require("./config.base");
-module.exports = Object.assign({}, baseConfig);
+const listPackages = require("./../utils/listPackages");
+
+// Create a module map to point React packages to the build output
+const moduleNameMapper = {};
+listPackages().forEach(name => {
+    // Root entry point - it's good, no need to change anything here.
+    // moduleNameMapper[`^${name}$`] = `...`;
+
+    // Named entry points
+    moduleNameMapper[`^${name}/(.*)$`] = `<rootDir>packages/${name}/src/$1`;
+});
+
+module.exports = Object.assign({}, baseConfig, {
+    moduleNameMapper
+});
