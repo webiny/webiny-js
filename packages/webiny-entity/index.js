197c86320ec78135759d9cd6b3fc80c940849856
Adrian Smijulj <adrian1358@gmail.com>
2018-07-01 14:58:15 +0200

    chore(package.json): clean unneeded deps.
    affects: webiny-entity, webiny-file-storage-local, webiny-jimp, webiny-sql-table-mysql,
    webiny-sql-table

---
+++ packages/webiny-entity/index.js
@@ -0,0 +1,17 @@
+// @flow
+export { default as Entity } from "./src/entity";
+export { default as EntityError } from "./src/entityError";
+export { default as EntityModel } from "./src/entityModel";
+export { default as EntityPool } from "./src/entityPool";
+export { default as EntityPoolEntry } from "./src/entityPoolEntry";
+export { default as EntityCollection } from "./src/entityCollection";
+export { default as EntityAttributesContainer } from "./src/entityAttributesContainer";
+export { default as QueryResult } from "./src/queryResult";
+export { default as Driver } from "./src/driver";
+export { default as EventHandler } from "./src/eventHandler";
+
+// Attributes.
+export { default as EntityAttribute } from "./src/entityAttributes/entityAttribute";
+export { default as EntitiesAttribute } from "./src/entityAttributes/entitiesAttribute";
+export { default as ModelAttribute } from "./src/entityAttributes/modelAttribute";
+export { default as ModelsAttribute } from "./src/entityAttributes/modelsAttribute";
