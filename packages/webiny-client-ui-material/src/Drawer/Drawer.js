32864000850d6895ba381ed0a1359c378aa013ae
Adrian Smijulj <adrian1358@gmail.com>
2018-07-05 13:44:09 +0200

    feat(material): implement Drawer component.
    affects: webiny-client-ui-material

---
+++ packages/webiny-client-ui-material/src/Drawer/Drawer.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from "react";
+import { Drawer as RmwcDrawer, DrawerContent, DrawerHeader } from "rmwc/Drawer";
+
+type Props = {
+    // One or more Drawer.Item components.
+    // One or more Drawer.Item components.
+    children: React.ChildrenArray<
+        React.Element<typeof Drawer.Header> | React.Element<typeof Drawer.Content>
+    >,
+
+    // If true, drawer will be permanently fixed inside of a view (works for temporary and persistent modes).
+    open?: boolean,
+
+    // Choose display mode:
+    // "permanent" (default) - drawer will be permanently fixed inside of a view.
+    // "temporary" - drawer will be displayed fixed on the side of the entire viewport.
+    // "persistent" - similar to permanent, except its visibility can be toggled (while closed, it still takes up viewable space).
+    mode?: "permanent" | "persistent" | "temporary"
+};
+
+const Drawer = (props: Props) => {
+    // Let's pass "permanent" / "persistent" / "temporary" flags as "mode" prop instead.
+    const mode = props.mode || "permanent";
+    return <RmwcDrawer {...{ [mode]: true }} {...props} />;
+};
+
+const Header = props => <DrawerHeader {...props} />;
+const Content = props => <DrawerContent {...props} />;
+
+Drawer.Header = Header;
+Drawer.Content = Content;
+
+export default Drawer;
---
+++ packages/webiny-client-ui-material/src/Drawer/README.md
@@ -0,0 +1,8 @@
+# Drawer
+
+Use Drawer component to display a list of choices, once the handler is triggered.
+
+## Import
+```js
+import Drawer from "webiny-client-ui-material/Drawer";
+```
