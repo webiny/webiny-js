0c169d76b2cb324553dfbf513fe9f2f1d73d684b
Pavel910 <pavel@webiny.com>
2018-09-18 12:26:41 +0200

    feat: ✨  implement element groups via plugins.
    affects: webiny-app-cms
    
    ISSUES CLOSED: #110

---
+++ packages/webiny-app-cms/src/plugins/editor/elementGroups/image/index.js
@@ -0,0 +1,16 @@
+// @flow
+import React from "react";
+import { ReactComponent as ImageGroupIcon } from "webiny-app-cms/editor/assets/icons/round-collections-24px.svg";
+import type { ElementGroupPluginType } from "webiny-app-cms/types";
+
+const imageGroup: ElementGroupPluginType = {
+    name: "cms-element-group-image",
+    type: "cms-element-group",
+    group: {
+        name: "image",
+        title: "Image",
+        icon: <ImageGroupIcon/>
+    }
+};
+
+export default imageGroup;
