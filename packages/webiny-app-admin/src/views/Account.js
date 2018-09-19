7fda4689fb629955100833e9988031d000b70b81
Adrian Smijulj <adrian1358@gmail.com>
2018-09-19 22:59:38 +0200

    fix: 🐛  Implement basic load / save in account form.
    affects: webiny-app-admin
    
    ISSUES CLOSED: fixes #144

--- packages/webiny-app-admin/src/views/Account.js
+++ packages/webiny-app-admin/src/views/Account.js
@@ -1,14 +1,17 @@
 // @flow
 import * as React from "react";
+import { app } from "webiny-app";
+import { graphqlQuery } from "webiny-app/actions";
 import { i18n } from "webiny-app/i18n";
 import { Form } from "webiny-form";
 import { Grid, Cell } from "webiny-ui/Grid";
 import { Input } from "webiny-ui/Input";
 import { ButtonPrimary } from "webiny-ui/Button";
-import { withForm } from "webiny-app/components";
 import { withSnackbar } from "webiny-app-admin/components";
 import compose from "recompose/compose";
 import AvatarImage from "./Users/AvatarImage";
+import gql from "graphql-tag";
+import { connect } from "react-redux";
 
 import {
     SimpleForm,
@@ -19,89 +22,91 @@ import {
 
 const t = i18n.namespace("Security.UsersForm");
 
-const UsersForm = props => {
-    const { SecurityUserForm } = props;
-
-    return (
-        <Form
-            {...SecurityUserForm}
-            onSubmit={data => {
-                SecurityUserForm.submit({
-                    data,
-                    onSuccess: () => props.showSnackbar(t`Account information update successfully.`)
-                });
-            }}
-        >
-            {({ data, form, Bind }) => (
-                <SimpleForm>
-                    <SimpleFormHeader title={"Account"} />
-                    <SimpleFormContent>
-                        <Grid>
-                            <Cell span={3}>
-                                <Grid>
-                                    <Cell span={12}>
-                                        <Bind name="image" validators={["required"]}>
-                                            <AvatarImage />
-                                        </Bind>
-                                    </Cell>
-                                </Grid>
-                            </Cell>
-                            <Cell span={9}>
-                                <Grid>
-                                    <Cell span={12}>
-                                        <Bind name="email" validators={["required"]}>
-                                            <Input label={t`E-mail`} />
-                                        </Bind>
-                                    </Cell>
-                                    <Cell span={12}>
-                                        <Bind
-                                            name="password"
-                                            validators={
-                                                data.id ? ["password"] : ["required", "password"]
+const UsersForm = props => (
+    <Form
+        data={app.security.identity}
+        onSubmit={data => {
+            props.graphqlQuery({
+                query: gql`
+                    query updateMe($data: JSON!) {
+                        Me {
+                            update(data: $data) {
+                                ... on SecurityUser {
+                                    id
+                                }
+                            }
+                        }
+                    }
+                `,
+                variables: { data },
+                onSuccess: () => {
+                    props.showSnackbar(t`Account information update successfully.`);
+                }
+            });
+        }}
+    >
+        {({ data, form, Bind }) => (
+            <SimpleForm>
+                <SimpleFormHeader title={"Account"} />
+                <SimpleFormContent>
+                    <Grid>
+                        <Cell span={3}>
+                            <Grid>
+                                <Cell span={12}>
+                                    <Bind name="avatar">
+                                        <AvatarImage />
+                                    </Bind>
+                                </Cell>
+                            </Grid>
+                        </Cell>
+                        <Cell span={9}>
+                            <Grid>
+                                <Cell span={12}>
+                                    <Bind name="email" validators={["required"]}>
+                                        <Input label={t`E-mail`} />
+                                    </Bind>
+                                </Cell>
+                                <Cell span={12}>
+                                    <Bind name="password" validators={["password"]}>
+                                        <Input
+                                            autoComplete="off"
+                                            description={
+                                                data.id && t`Type a new password to reset it.`
                                             }
-                                        >
-                                            <Input
-                                                autoComplete="off"
-                                                description={
-                                                    data.id && t`Type a new password to reset it.`
-                                                }
-                                                type="password"
-                                                label={t`Password`}
-                                            />
-                                        </Bind>
-                                    </Cell>
+                                            type="password"
+                                            label={t`Password`}
+                                        />
+                                    </Bind>
+                                </Cell>
 
-                                    <Cell span={12}>
-                                        <Bind name="firstName" validators={["required"]}>
-                                            <Input label={t`First Name`} />
-                                        </Bind>
-                                    </Cell>
-                                    <Cell span={12}>
-                                        <Bind name="lastName" validators={["required"]}>
-                                            <Input label={t`Last name`} />
-                                        </Bind>
-                                    </Cell>
-                                </Grid>
-                            </Cell>
-                        </Grid>
-                    </SimpleFormContent>
-                    <SimpleFormFooter>
-                        <ButtonPrimary type="primary" onClick={form.submit} align="right">
-                            {t`Update account`}
-                        </ButtonPrimary>
-                    </SimpleFormFooter>
-                </SimpleForm>
-            )}
-        </Form>
-    );
-};
+                                <Cell span={12}>
+                                    <Bind name="firstName">
+                                        <Input label={t`First Name`} />
+                                    </Bind>
+                                </Cell>
+                                <Cell span={12}>
+                                    <Bind name="lastName">
+                                        <Input label={t`Last name`} />
+                                    </Bind>
+                                </Cell>
+                            </Grid>
+                        </Cell>
+                    </Grid>
+                </SimpleFormContent>
+                <SimpleFormFooter>
+                    <ButtonPrimary type="primary" onClick={form.submit} align="right">
+                        {t`Update account`}
+                    </ButtonPrimary>
+                </SimpleFormFooter>
+            </SimpleForm>
+        )}
+    </Form>
+);
 
 export default compose(
     withSnackbar(),
-    withForm({
-        name: "SecurityUserForm",
-        type: "Security.Users",
-        fields:
-            "id email firstName lastName fullName avatar { src } enabled groups { id name } policies { id name }"
-    })
+    connect(
+        null,
+        { graphqlQuery }
+    )
 )(UsersForm);
