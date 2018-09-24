e61287f4d41aa01354eedefdd9349d42196653b6
Pavel910 <pavel@webiny.com>
2018-09-24 23:01:46 +0200

    Switch to fixed versioning

---
+++ scripts/release/index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const hookStd = require("hook-std");
+const { argv } = require("yargs");
+const buildParams = require("./utils/buildParams");
+const stdOut = require("./utils/stdOut");
+const compose = require("./utils/compose");
+const getPackages = require("./utils/getPackages");
+
+const analyzeCommits = require("./plugins/analyzeCommits");
+const githubVerify = require("./plugins/github/verify");
+const githubPublish = require("./plugins/github/publish");
+const npmVerify = require("./plugins/npm/verify");
+const npmPublish = require("./plugins/npm/publish");
+const updatePackages = require("./plugins/updatePackages");
+
+
+const release = async config => {
+    const { params, plugins } = await buildParams(config);
+
+    // Connect to the stdout and process each line of the output using `stdOut` function
+    const { unhook } = hookStd(
+        {
+            silent: false,
+            streams: [process.stdout, process.stderr].filter(Boolean)
+        },
+        stdOut
+    );
+    try {
+        await compose(plugins)(params);
+        unhook();
+        return params;
+    } catch (err) {
+        console.log(err);
+    }
+    unhook();
+};
+
+release({
+    preview: argv.preview || false,
+    registryUrl: "http://localhost:32771",
+    branch: argv.branch || "master",
+    packages: getPackages("build/node_modules/*"),
+    plugins: [
+        githubVerify(),
+        npmVerify(),
+        analyzeCommits(),
+        updatePackages(),
+        // Create git tag
+        // First we publish to npm
+        npmPublish(),
+        // Publish a new release to github
+        githubPublish()
+    ]
+});
---
+++ scripts/release/plugins/npm/publish.js
@@ -0,0 +1,50 @@
+const execa = require("execa");
+const path = require("path");
+const fs = require("fs-extra");
+
+module.exports = () => {
+    return async ({ packages, nextRelease, logger, config }, next) => {
+        const { registryUrl = "https://registry.npmjs.org", tag } = config;
+        for (let i = 0; i < packages.length; i++) {
+            const pkg = packages[i];
+            logger.log(
+                "Publishing %s version %s to npm registry" + (registryUrl ? " %s" : ""),
+                ...[pkg.name, nextRelease.version, registryUrl].filter(v => v)
+            );
+
+            const command = [
+                `npm publish`,
+                tag ? `--tag ${tag}` : null,
+                `--registry ${registryUrl}`,
+                pkg.location
+            ]
+                .filter(v => v)
+                .join(" ");
+
+            if (config.preview) {
+                logger.log(`DRY: %s`, command);
+            } else {
+                try {
+                    // write the updated package.json to disk before publishing
+                    fs.writeJsonSync(path.join(pkg.location, "package.json"), pkg.package, {
+                        spaces: 2
+                    });
+                    // We need to unset the `npm_` env variables to make sure local `.npmrc` is being read.
+                    // This is required when running scripts with yarn: https://github.com/yarnpkg/yarn/issues/4475
+                    const shell = await execa.shell(
+                        `unset $(env | awk -F= '$1 ~ /^npm_/ {print $1}') & ${command}`
+                    );
+                    logger.log(shell.stdout);
+                    pkg.npmPublish = {
+                        ...shell
+                    };
+                } catch (err) {
+                    logger.log(err.toString());
+                    pkg.npmPublish = { error: err };
+                }
+            }
+        }
+
+        next();
+    };
+};
