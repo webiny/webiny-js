bafaf78585e1c65099aa0e084aacf7eb8a609668
Pavel910 <pavel@webiny.com>
2018-09-27 13:49:47 +0200

    chore: add idea.js file to help webstorm resolve our packages [skip ci].

---
+++ idea.js
@@ -0,0 +1,11 @@
+// This file helps Jetbrains resolve aliases
+System.config({
+    paths: {
+        "webiny-app/*": "./packages/webiny-app/src/*",
+        "webiny-app-admin/*": "./packages/webiny-app-admin/src/*",
+        "webiny-app-cms/*": "./packages/webiny-app-cms/src/*",
+        "webiny-api/*": "./packages/webiny-api/src/*",
+        "webiny-api-cms/*": "./packages/webiny-api-cms/src/*",
+        "webiny-ui/*": "./packages/webiny-ui/src/*"
+    }
+});
