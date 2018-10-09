92ab700e744f56a49a7a91cb0c05dc795ec4c0c0
Adrian Smijulj <adrian1358@gmail.com>
2018-10-09 11:20:13 +0200

    Use AppConfig provider.

--- packages/demo-admin/src/App.js
+++ packages/demo-admin/src/App.js
@@ -1,7 +1,9 @@
+// @flow
 import { hot } from "react-hot-loader";
 import React from "react";
 import { Provider as StateProvider } from "react-redux";
 import { app } from "webiny-app";
+import { AppConfig } from "webiny-app/config";
 import { router, Router } from "webiny-app/router";
 import { app as adminApp, Theme as AdminTheme } from "webiny-app-admin";
 import { app as cmsApp } from "webiny-app-cms/admin";
@@ -32,13 +34,15 @@ if (!app.initialized) {
 
 const App = ({ store }) => {
     return (
-        <StateProvider store={store}>
-            <CmsTheme theme={myTheme}>
-                <AdminTheme>
-                    <Router router={router} />
-                </AdminTheme>
-            </CmsTheme>
-        </StateProvider>
+        <AppConfig config={config()}>
+            <StateProvider store={store}>
+                <CmsTheme theme={myTheme}>
+                    <AdminTheme>
+                        <Router router={router} />
+                    </AdminTheme>
+                </CmsTheme>
+            </StateProvider>
+        </AppConfig>
     );
 };
 
