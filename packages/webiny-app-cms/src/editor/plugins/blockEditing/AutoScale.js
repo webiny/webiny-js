6c5710746cdd93ece570610f6efc8a66e8e9eba7
Sven <sven@webiny.com>
2018-10-14 17:28:40 +0100

    fix: 🐛  Fixed scaling of block preview. (#200)

--- packages/webiny-app-cms/src/editor/plugins/blockEditing/AutoScale.js
+++ packages/webiny-app-cms/src/editor/plugins/blockEditing/AutoScale.js
@@ -4,38 +4,64 @@ export default class AutoScale extends React.Component {
     content = React.createRef();
 
     state = {
-        wrapperSize: { width: 0, height: 0 },
+        width: null,
+        height: null,
         contentSize: { width: 0, height: 0 },
         scale: 1
     };
 
     componentDidMount() {
-        const actualContent = this.content.current.children[0];
-
-        this.updateState({
-            ...this.state,
-            contentSize: { width: actualContent.offsetWidth, height: actualContent.offsetHeight }
-        });
+        /**
+         * Because there is a transition animation showing the block content,
+         * getting dimensions of the block before the transition ends can lead to wrong dimensions.
+         * The setTimeout offsets that problem.
+         */
+        setTimeout(()=> {
+            this.updateState();
+        }, 200);
     }
 
-    updateState(newState) {
-        const { maxWidth } = this.props;
-        const { contentSize } = newState;
+    updateState() {
+        const actualContent = this.content.current.children[0];
+        const contentSize = { width: actualContent.offsetWidth, height: actualContent.offsetHeight }
 
-        let scale = maxWidth / contentSize.width;
+        const { maxWidth, maxHeight } = this.props;
+
+        let scaleWidth = maxWidth / contentSize.width;
+        let scaleHeight = maxHeight / contentSize.height;
+
+        console.log(contentSize.width+" "+contentSize.height);
+
+        let scale = scaleWidth;
+        let height = maxHeight/scaleWidth;
+        let width = maxWidth/scaleWidth;
+        if(scaleHeight<scaleWidth){
+            scale = scaleHeight;
+            height = maxHeight/scaleHeight;
+            width = maxHeight/scaleHeight;
+        }
+
+        if((width/scale)>(maxWidth/scale)){
+            width = (maxWidth/scale);
+        }
+
+        if((height/scale)>(maxHeight/scale)){
+            height = (maxHeight/scale);
+        }
 
         if (scale > 1) {
             scale = 1;
         }
 
         this.setState({
-            ...newState,
+            width: width,
+            height: height,
             scale
         });
     }
 
     render() {
-        const { scale } = this.state;
+        const { scale, width, height } = this.state;
         const { children } = this.props;
 
         return (
@@ -45,7 +71,9 @@ export default class AutoScale extends React.Component {
                     transform: "scale(" + scale + ")",
                     transformOrigin: "top",
                     position: "absolute",
-                    top: 0
+                    top: 0,
+                    width: (width ? width+"px" : 'auto'),
+                    height: (height ? height+"px" : 'auto'),
                 }}
             >
                 {React.Children.only(children)}
