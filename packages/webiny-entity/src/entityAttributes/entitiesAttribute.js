1039fa5dfbde6b7ee6ab45777d63d242ef728f17
Adrian Smijulj <adrian1358@gmail.com>
2018-10-17 22:04:52 +0200

    __afterXYZ and __beforeXYZ

--- packages/webiny-entity/src/entityAttributes/entitiesAttribute.js
+++ packages/webiny-entity/src/entityAttributes/entitiesAttribute.js
@@ -78,7 +78,7 @@ class EntitiesAttribute extends Attribute {
          * Same as in EntityAttribute, entities present here were already validated when parent entity called the validate method.
          * At this point, entities are ready to be saved (only loaded entities).
          */
-        this.parentEntity.on("afterSave", async () => {
+        this.parentEntity.on("__afterSave", async () => {
             // We don't have to do the following check here:
             // this.value.isLoading() && (await this.value.load());
 
