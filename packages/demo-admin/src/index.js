1cf3d105c54f8bc42102fb6d956452e7114c9ce1
Pavel910 <pavel@webiny.com>
2018-10-19 06:36:31 +0200

    Move app config to the App component.

--- packages/demo-admin/src/index.js
+++ packages/demo-admin/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import config from "./config/index";
 import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App config={config} />, document.getElementById("root"));
