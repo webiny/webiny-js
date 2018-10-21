b73be0a6bbe83e13f76855463da96ff2d36e22e9
SvenAlHamad <sven@webiny.com>
2018-10-21 11:26:24 +0100

    fix: 🐛  Fixed z-index on page preview.

--- packages/webiny-app-cms/src/admin/plugins/pageDetails/previewContent/index.js
+++ packages/webiny-app-cms/src/admin/plugins/pageDetails/previewContent/index.js
@@ -3,6 +3,12 @@ import * as React from "react";
 import type { CmsPageDetailsPluginType, WithPageDetailsProps } from "webiny-app-cms/types";
 import { Tab } from "webiny-ui/Tabs";
 import RenderElement from "webiny-app-cms/render/components/Element";
+import styled from "react-emotion";
+
+const RenderBlock = styled("div")({
+    position: "relative",
+    zIndex: 0
+});
 
 export default ({
     name: "cms-page-details-revision-content-preview",
@@ -10,7 +16,9 @@ export default ({
     render({ pageDetails }: WithPageDetailsProps) {
         return (
             <Tab label={"Page preview"}>
-                <RenderElement element={pageDetails.revision.data.content} />
+                <RenderBlock>
+                    <RenderElement element={pageDetails.revision.data.content} />
+                </RenderBlock>
             </Tab>
         );
     }
