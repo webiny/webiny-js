d44bbabd65633207adcbe77ca215cd6df2c24131
SvenAlHamad <sven@webiny.com>
2018-10-21 14:12:32 +0100

    feat: ✨  Improved "no page selected" message design.

--- packages/webiny-app-cms/src/admin/views/Pages/PageDetails.js
+++ packages/webiny-app-cms/src/admin/views/Pages/PageDetails.js
@@ -8,6 +8,8 @@ import { PageDetailsProvider, PageDetailsConsumer } from "../../components/PageD
 import type { WithRouterProps } from "webiny-app/components";
 import { loadRevision, loadPageRevisions } from "webiny-app-cms/admin/graphql/pages";
 import Loader from "./Loader";
+import styled from "react-emotion";
+import {Elevation} from "webiny-ui/Elevation";
 
 type Props = WithRouterProps & {
     pageId: string,
@@ -24,9 +26,31 @@ type Props = WithRouterProps & {
     }
 };
 
+const EmptySelect = styled("div")({
+   width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    ".select-page":{
+        maxWidth: 400,
+        padding: '50px 100px',
+        textAlign: "center",
+        display: "block",
+        borderRadius: 2,
+        backgroundColor: 'var(--mdc-theme-surface)'
+    }
+});
+
+
+
 const PageDetails = ({ router, pageId, revision, revisions, refreshPages }: Props) => {
     if (!router.getQuery("revision")) {
-        return <div>Select a page on the left!</div>;
+        return <EmptySelect>
+            <Elevation z={2} className={"select-page"}>
+                Select a page on the left side, or click the green button to create a new one.
+            </Elevation>
+        </EmptySelect>;
     }
 
     if (revision.loading) {
