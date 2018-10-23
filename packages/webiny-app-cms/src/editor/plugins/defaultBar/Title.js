9e1735420ae22e77463d9d8558280ab6f3ab1d3b
Pavel Denisjuk <pavel@webiny.com>
2018-10-23 06:57:39 +0200

    feat: handle page title using Esc, Enter and blur events. (#251)

--- packages/webiny-app-cms/src/editor/plugins/defaultBar/Title.js
+++ packages/webiny-app-cms/src/editor/plugins/defaultBar/Title.js
@@ -1,6 +1,6 @@
 //@flow
 import React from "react";
-import { compose, withState } from "recompose";
+import { compose, withState, withProps, withHandlers } from "recompose";
 import { get } from "dot-prop-immutable";
 import { connect } from "react-redux";
 import { Input } from "webiny-ui/Input";
@@ -8,40 +8,48 @@ import { updateRevision } from "./actions";
 import { getPage, getRevision } from "webiny-app-cms/editor/selectors";
 import { Tooltip } from "webiny-ui/Tooltip";
 import { Typography } from "webiny-ui/Typography";
-import { ReactComponent as CloseIcon } from "webiny-app-cms/editor/assets/icons/close.svg";
-import { IconButton } from "webiny-ui/Button";
 import {
     PageMeta,
     PageTitle,
     pageTitleWrapper,
     PageVersion,
-    resetBtn,
     TitleInputWrapper,
     TitleWrapper
 } from "./Styled";
 
 type Props = {
     page: Object,
+    title: string,
     revision: Object,
     updateRevision: ({ title: string, history?: boolean }) => void,
     editTitle: boolean,
-    setEdit: (flag: boolean) => void
+    enableEdit: Function,
+    setTitle: (title: string) => void,
+    onKeyDown: Function,
+    onBlur: Function
 };
 
-const Title = ({ revision, page, updateRevision, editTitle, setEdit }: Props) => {
+const Title = ({
+    revision,
+    page,
+    updateRevision,
+    editTitle,
+    enableEdit,
+    setTitle,
+    title,
+    onKeyDown,
+    onBlur
+}: Props) => {
     return editTitle ? (
         <TitleInputWrapper>
             <Input
                 autoFocus
                 fullwidth
-                value={revision.title}
-                onChange={title => updateRevision({ title, history: false })}
-                onBlur={() => {
-                    setEdit(false);
-                    updateRevision({ title: revision.title });
-                }}
+                value={title}
+                onChange={setTitle}
+                onKeyDown={onKeyDown}
+                onBlur={onBlur}
             />
-            <IconButton className={resetBtn} onClick={() => setEdit(false)} icon={<CloseIcon />} />
         </TitleInputWrapper>
     ) : (
         <TitleWrapper>
@@ -57,9 +65,7 @@ const Title = ({ revision, page, updateRevision, editTitle, setEdit }: Props) =>
                     placement={"bottom"}
                     content={<span>Rename</span>}
                 >
-                    <PageTitle onClick={() => setEdit(true)}>
-                        {revision.title || "Untitled"}
-                    </PageTitle>
+                    <PageTitle onClick={enableEdit}>{title}</PageTitle>
                 </Tooltip>
                 <PageVersion>(v{revision.version})</PageVersion>
             </div>
@@ -72,5 +78,43 @@ export default compose(
         state => ({ page: getPage(state), revision: getRevision(state) }),
         { updateRevision }
     ),
-    withState("editTitle", "setEdit", false)
+    withState("editTitle", "setEdit", false),
+    withState("title", "setTitle", null),
+    withProps(({ title, revision }) => ({
+        title: title === null ? revision.title : title
+    })),
+    withHandlers({
+        enableEdit: ({ setEdit }) => () => setEdit(true),
+        onBlur: ({ title, setEdit, setTitle, updateRevision }) => () => {
+            if (title === "") {
+                title = "Untitled";
+                setTitle(title);
+            }
+            setEdit(false);
+            updateRevision({ title });
+        },
+        onKeyDown: ({ title, setTitle, setEdit, revision, updateRevision }) => (
+            e: SyntheticKeyboardEvent<HTMLInputElement>
+        ) => {
+            switch (e.key) {
+                case "Escape":
+                    e.preventDefault();
+                    setEdit(false);
+                    setTitle(revision.title);
+                    break;
+                case "Enter":
+                    if (title === "") {
+                        title = "Untitled";
+                        setTitle(title);
+                    }
+
+                    e.preventDefault();
+                    setEdit(false);
+                    updateRevision({ title });
+                    break;
+                default:
+                    return;
+            }
+        }
+    })
 )(Title);
