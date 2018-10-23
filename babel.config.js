a55a48940cde8fccee21caf15ec370483dbfdbc4
Pavel Denisjuk <pavel@webiny.com>
2018-10-23 07:08:06 +0200

    Update babel configs to work with CRA v2. (#253)
    * Update to react-scripts@2.0.5
    
    * chore: ⚡️  Update babel configs to work with CRA v2

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,17 @@
+// @flowIgnore
 const path = require("path");
 const getPackages = require("get-yarn-workspaces");
 const packages = getPackages(path.join(process.cwd(), "packages"));
 
+const aliases = packages.reduce((aliases, dir) => {
+    const name = path.basename(dir);
+    aliases[`^${name}/types`] = `${name}/types`;
+    aliases[`^${name}/(?!src)(.+)$`] = `${name}/src/\\1`;
+    return aliases;
+}, {});
+
 module.exports = {
+    babelrc: true,
     babelrcRoots: packages,
-    plugins: [
-        [
-            "babel-plugin-module-resolver",
-            {
-                alias: packages.reduce((aliases, dir) => {
-                    const name = path.basename(dir);
-                    aliases[`^${name}/types`] = `${name}/types`;
-                    aliases[`^${name}/(?!src)(.+)$`] = `${name}/src/\\1`;
-                    return aliases;
-                }, {})
-            }
-        ]
-    ]
+    plugins: [["babel-plugin-module-resolver", { alias: aliases }]]
 };
