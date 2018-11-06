62330ddb0029d65d8b3a3f401beefe9ee73ea989
sven <sven@webiny.com>
2018-11-06 20:57:48 +0000

    Youtube embed resize fix.

--- packages/webiny-app-cms/src/editor/plugins/elements/media/youtube/YoutubeEmbed.js
+++ packages/webiny-app-cms/src/editor/plugins/elements/media/youtube/YoutubeEmbed.js
@@ -2,9 +2,39 @@ import * as React from "react";
 import { css } from "emotion";
 import { get } from "dot-prop-immutable";
 
-const centerAlign = css({ width: "100%", textAlign: "center" });
+const outerWrapper = css({
+    boxSizing: "border-box"
+});
 
-const YoutubeEmbed = (props) => {
+const innerWrapper = css({
+    left: 0,
+    width: "100%",
+    height: "auto",
+    position: "relative",
+    paddingBottom: 0
+});
+
+const scaleableVideo = css({
+    position: "relative",
+    height: 0,
+    paddingTop: "56.25%",
+    width: "100%",
+    backgroundColor: "#000",
+    marginBottom: "1.2rem",
+    ["iframe"]: {
+        maxWidth: "100%",
+        border: "none",
+        display: "block",
+        height: "100%",
+        margin: 0,
+        padding: 0,
+        position: "absolute",
+        top: 0,
+        width: "100%"
+    }
+});
+
+const YoutubeEmbed = props => {
     const { element, data } = props;
 
     if (data && data.loading) {
@@ -12,11 +42,15 @@ const YoutubeEmbed = (props) => {
     }
 
     return (
-        <div
-            id={"cms-embed-" + element.id}
-            className={centerAlign}
-            dangerouslySetInnerHTML={{ __html: get(element, "data.oembed.html") || "" }}
-        />
+        <div className={outerWrapper}>
+            <div className={innerWrapper}>
+                <div
+                    id={"cms-embed-" + element.id}
+                    className={scaleableVideo}
+                    dangerouslySetInnerHTML={{ __html: get(element, "data.oembed.html") || "" }}
+                />
+            </div>
+        </div>
     );
 };
 
