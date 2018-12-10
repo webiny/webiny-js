143fdf68c71ebb21e8e5bece36d98b7cdfeaa1a8
Adrian Smijulj <adrian1358@gmail.com>
2018-12-10 13:20:03 +0100

    fix: scaling select in background image settings

--- packages/webiny-app-cms/src/editor/plugins/elementSettings/components/Select.js
+++ packages/webiny-app-cms/src/editor/plugins/elementSettings/components/Select.js
@@ -9,21 +9,21 @@ type Props = {
     label: string,
     value: string,
     updateValue: Function,
-    options: Array<string>
+    options?: Array<string>,
+    // One or more <option> or <optgroup> elements.
+    children?: React.ChildrenArray<React.Element<"option"> | React.Element<"optgroup">>
 };
 
-export default pure(({ label, value, updateValue, options }: Props) => {
+export default pure(({ label, value, updateValue, options, children }: Props) => {
     return (
         <React.Fragment>
             <Cell span={4}>
                 <Typography use={"overline"}>{label}</Typography>
             </Cell>
             <Cell span={8}>
-                <Select
-                    value={value}
-                    onChange={updateValue}
-                    options={options}
-                />
+                <Select value={value} onChange={updateValue} options={options}>
+                    {children}
+                </Select>
             </Cell>
         </React.Fragment>
     );
