55a98fea18c1e8a2975698ef344646b159badd5e
Adrian Smijulj <adrian1358@gmail.com>
2018-12-14 11:55:05 +0100

    put seo to head of page

--- packages/webiny-app-cms/src/site/components/Page/Content.js
+++ packages/webiny-app-cms/src/site/components/Page/Content.js
@@ -4,13 +4,25 @@ import React from "react";
 import Element from "webiny-app-cms/render/components/Element";
 import Layout from "./../Layout";
 import { Helmet } from "react-helmet";
+import { get } from "lodash";
 
 const Content = ({ page }: { page: Object }) => {
+    const seo = {
+        title: "",
+        description: "",
+        meta: [],
+        ...get(page, "settings.seo")
+    };
+
     return (
         <div className="webiny-cms-page">
             <Helmet>
                 <meta charSet="utf-8" />
-                <title>{page.title}</title>
+                {seo.title && <title>{page.title}</title>}
+                {seo.description && <meta name="description" content={seo.description} />}
+                {seo.meta.map(({ name, content }, index) => (
+                    <meta key={index} name={name} content={content} />
+                ))}
             </Helmet>
             <Layout layout={page.settings.general.layout}>
                 <Element element={page.content} />
