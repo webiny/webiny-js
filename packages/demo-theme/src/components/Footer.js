37d3f1f3efdfddb7acecc0eda99856a864733290
Adrian Smijulj <adrian1358@gmail.com>
2018-12-21 21:06:26 +0100

    Added animations to blocks.

--- packages/demo-theme/src/components/Footer.js
+++ packages/demo-theme/src/components/Footer.js
@@ -1,17 +1,35 @@
 //@flow
 import * as React from "react";
+import { getFooterData } from "./graphql";
+import { Query } from "react-apollo";
+import { get } from "lodash";
 
-const Footer = () => (
-    <div className={"webiny-cms-section-footer"}>
-        <div className="webiny-cms-section-footer__wrapper">
-            <div className={"webiny-cms-section-footer__logo"}>
-                <a href="/">Logo</a> $siteName (c) 2018
-            </div>
-            <div className={"webiny-cms-section-footer__social"}>
-                <a href="/">social</a>
-            </div>
-        </div>
-    </div>
-);
+const Footer = () => {
+    return (
+        <Query query={getFooterData}>
+            {({ data: response }) => {
+                const { name, logo, social } = get(response, "settings.general") || {};
+
+                return (
+                    <div className={"webiny-cms-section-footer"}>
+                        <div className="webiny-cms-section-footer__wrapper">
+                            <div className={"webiny-cms-section-footer__logo"}>
+                                <a href="/">{logo && <img src={logo.src} alt={name} />}</a> {name}{" "}
+                                (c) 2018
+                            </div>
+                            {social && (
+                                <div className={"webiny-cms-section-footer__social"}>
+                                    <a href={social.facebook}>Facebook</a>
+                                    <a href={social.instagram}>Instagram</a>
+                                    <a href={social.twitter}>Twitter</a>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                );
+            }}
+        </Query>
+    );
+};
 
 export { Footer };
--- packages/demo-theme/src/components/Header.js
+++ packages/demo-theme/src/components/Header.js
@@ -3,7 +3,9 @@ import * as React from "react";
 import { Menu } from "webiny-app-cms/src/render/components";
 import HamburgerMenu from "react-hamburger-menu";
 import classNames from "classnames";
-
+import { get } from "lodash";
+import { Query } from "react-apollo";
+import { getHeaderData } from "./graphql";
 type Props = null;
 type State = {
     mobileMenuOpen: boolean
@@ -20,45 +22,53 @@ class Header extends React.Component<Props, State> {
 
     render() {
         return (
-            <div className={"webiny-cms-section-header"}>
-                <div className={"webiny-cms-section-header__logo"}>
-                    <a href="/">Logo</a>
-                </div>
-                <nav
-                    className={classNames("webiny-cms-section-header__navigation", {
-                        "webiny-cms-section-header__navigation--mobile-active": this.state
-                            .mobileMenuOpen
-                    })}
-                >
-                    <Menu slug={"demo-menu"} component={"default"} />
-                    <div className={"webiny-cms-section-header__mobile-site-name"}>
-                        <a href="/">Site Name</a>
-                    </div>
-                </nav>
-                <div
-                    onClick={this.toggleMobileMenu}
-                    className="webiny-cms-section-header__mobile-icon"
-                >
-                    <HamburgerMenu
-                        isOpen={this.state.mobileMenuOpen}
-                        menuClicked={this.toggleMobileMenu}
-                        width={18}
-                        height={15}
-                        strokeWidth={1}
-                        rotate={0}
-                        color="black"
-                        borderRadius={0}
-                        animationDuration={0.5}
-                    />
-                </div>
-                <div
-                    onClick={this.toggleMobileMenu}
-                    className={classNames("webiny-cms-section-header__mobile-overlay", {
-                        "webiny-cms-section-header__mobile-overlay--active": this.state
-                            .mobileMenuOpen
-                    })}
-                />
-            </div>
+            <Query query={getHeaderData}>
+                {({ data: response }) => {
+                    const { name, logo } = get(response, "settings.general") || {};
+
+                    return (
+                        <div className={"webiny-cms-section-header"}>
+                            <div className={"webiny-cms-section-header__logo"}>
+                                <a href="/">{logo && <img src={logo.src} alt={name} />}</a>
+                            </div>
+                            <nav
+                                className={classNames("webiny-cms-section-header__navigation", {
+                                    "webiny-cms-section-header__navigation--mobile-active": this
+                                        .state.mobileMenuOpen
+                                })}
+                            >
+                                <Menu slug={"demo-menu"} component={"default"} />
+                                <div className={"webiny-cms-section-header__mobile-site-name"}>
+                                    <a href="/">{name}</a>
+                                </div>
+                            </nav>
+                            <div
+                                onClick={this.toggleMobileMenu}
+                                className="webiny-cms-section-header__mobile-icon"
+                            >
+                                <HamburgerMenu
+                                    isOpen={this.state.mobileMenuOpen}
+                                    menuClicked={this.toggleMobileMenu}
+                                    width={18}
+                                    height={15}
+                                    strokeWidth={1}
+                                    rotate={0}
+                                    color="black"
+                                    borderRadius={0}
+                                    animationDuration={0.5}
+                                />
+                            </div>
+                            <div
+                                onClick={this.toggleMobileMenu}
+                                className={classNames("webiny-cms-section-header__mobile-overlay", {
+                                    "webiny-cms-section-header__mobile-overlay--active": this.state
+                                        .mobileMenuOpen
+                                })}
+                            />
+                        </div>
+                    );
+                }}
+            </Query>
         );
     }
 }
