8cacb4602563044034d337a57e87fb339aaba096
Pavel Denisjuk <pavel@webiny.com>
2018-12-25 15:21:33 +0100

    feat: add editor breadcrumbs. (#376)

---
+++ packages/webiny-app-cms/src/editor/plugins/breadcrumbs/index.js
@@ -0,0 +1,11 @@
+// @flow
+import * as React from "react";
+import Breadcrumbs from "./Breadcrumbs";
+
+export default {
+    name: "cms-editor-breadcrumbs",
+    type: "cms-editor-content",
+    render() {
+        return <Breadcrumbs/>;
+    }
+};
