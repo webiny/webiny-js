c98a5fc29f4ff819c4fb7946721d72b6626504c7
SvenAlHamad <sven@webiny.com>
2018-12-26 14:20:39 +0000

    Dynamic fieldset dark theme styling.

--- packages/webiny-app-cms/src/editor/plugins/pageSettings/components/OpenGraphTags.js
+++ packages/webiny-app-cms/src/editor/plugins/pageSettings/components/OpenGraphTags.js
@@ -24,6 +24,10 @@ const controlButtons = css({
     }
 });
 
+const textStyling = css({
+    color: "var(--mdc-theme-text-secondary-on-background)"
+});
+
 const MetaTags = ({ prefix, value, onChange, Bind, ...other }: Props) => {
     return (
         <DynamicFieldset value={value} onChange={onChange} {...other}>
@@ -31,7 +35,7 @@ const MetaTags = ({ prefix, value, onChange, Bind, ...other }: Props) => {
                 <React.Fragment>
                     {header(() => (
                         <Grid style={{ paddingTop: 0, paddingBottom: 0 }}>
-                            <Cell span={12}>
+                            <Cell span={12} className={textStyling}>
                                 <Typography use={"button"}>Open Graph tags</Typography>
                             </Cell>
                         </Grid>
@@ -64,7 +68,7 @@ const MetaTags = ({ prefix, value, onChange, Bind, ...other }: Props) => {
                     ))}
                     {empty(() => (
                         <Grid>
-                            <Cell span={12}>
+                            <Cell span={12} className={textStyling}>
                                 <Typography use={"button"}>
                                     To add other Open Graph tags, click{" "}
                                     <ButtonPrimary onClick={actions.add()}>
