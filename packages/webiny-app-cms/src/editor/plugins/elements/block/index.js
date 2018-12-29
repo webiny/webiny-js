58eb5aff55d84fa7dafde8956f2af44f29698fea
SvenAlHamad <sven@webiny.com>
2018-12-29 20:13:58 +0000

    Block default margins adjusted.

--- packages/webiny-app-cms/src/editor/plugins/elements/block/index.js
+++ packages/webiny-app-cms/src/editor/plugins/elements/block/index.js
@@ -46,7 +46,10 @@ export default (): ElementPluginType => {
                 settings: {
                     style: {
                         width: { value: "1000px" },
-                        margin: { all: 0 },
+                        margin: {
+                            mobile: { top: 15, left: 15, right: 15, bottom: 15 },
+                            desktop: { top: 25, left: 0, right: 0, bottom: 25 }
+                        },
                         padding: { all: 0 }
                     }
                 },
