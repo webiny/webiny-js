a1fb6979ac89e7126c84c6ba103a3356dd3dde6b
Pavel910 <pavel@webiny.com>
2018-12-30 13:28:55 +0100

    Update to use rescripts instead of react-app-rewired.

--- packages/webiny-rewire/configs/overrides/babel.js
+++ packages/webiny-rewire/configs/overrides/babel.js
@@ -1,9 +1,13 @@
 // @flowIgnore
+const get = require("lodash.get");
 const paths = require("react-scripts/config/paths");
-const { getBabelLoader } = require("react-app-rewired");
+const { getPaths } = require("@rescripts/utilities");
+
+const isBabelLoader = x => x && x.loader && x.loader.includes("babel-loader");
 
 module.exports = (rules, packages, aliases) => {
-    const babelLoader = getBabelLoader(rules);
+    const babelLoaderPaths = getPaths(isBabelLoader, rules);
+    const babelLoader = get(rules, babelLoaderPaths[0].join("."));
     babelLoader.include = [paths.appSrc, ...packages];
     babelLoader.options = {
         ...babelLoader.options,
