b4fd5b6801918f8183c8f42af7878dffb7096331
Adrian Smijulj <adrian1358@gmail.com>
2019-01-02 10:40:15 +0100

    fix: optimize uploaded images (local development) (#412)

--- packages/demo-api/src/files/create.js
+++ packages/demo-api/src/files/create.js
@@ -4,6 +4,7 @@ import uniqueId from "uniqid";
 import decodeBase64Src from "./utils/decodeBase64Src";
 import mime from "mime-types";
 import sanitizeFilename from "sanitize-filename";
+import sharp from "sharp";
 
 const create = async (src: string, options: Object = {}) => {
     if (!src) {
@@ -18,11 +19,21 @@ const create = async (src: string, options: Object = {}) => {
 
     fs.ensureDir(paths.folder);
 
-    const { buffer, type } = decodeBase64Src(src);
-    const extension: string = mime.extension(type);
+    let { buffer, type } = decodeBase64Src(src);
+
+    // If we are dealing with an image, compress it.
+    const supportedImageTypes = ["image/jpg", "image/jpeg", "image/png"];
+    if (supportedImageTypes.includes(type)) {
+        buffer = await sharp(buffer)
+            .resize({ width: 1920, height: 1440, withoutEnlargement: true, fit: "inside" })
+            .toFormat("jpeg", { quality: 90 })
+            .toBuffer();
+        type = "image/jpeg";
+    }
 
     // Generate unique filename.
     let name = options.name || "";
+    const extension: string = mime.extension(type);
     if (name) {
         // Remove extension.
         name =
