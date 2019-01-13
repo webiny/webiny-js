d8166d2c8a94a8fc3be1b20a49a5fe3a7775efdf
Adrian Smijulj <adrian1358@gmail.com>
2019-01-13 11:14:53 +0100

    searchTags resolver

---
+++ packages/webiny-api-cms/src/plugins/graphql/pageResolvers/searchTags.js
@@ -0,0 +1,22 @@
+// @flow
+export default async (root: any, args: Object, context: Object) => {
+    const { Page } = context.cms.entities;
+    const { query } = args;
+    const pipeline = [
+        { $match: { deleted: false } },
+        { $replaceRoot: { newRoot: "$settings.general" } },
+        { $project: { tags: 1 } },
+        { $unwind: "$tags" },
+        { $match: { tags: { $regex: `.*${query}.*`, $options: "i" } } }
+    ];
+
+    const results = await Page.getDriver()
+        .getDatabase()
+        .collection(Page.getDriver().getCollectionName(Page))
+        .aggregate(pipeline)
+        .toArray();
+
+    return {
+        data: results.map(item => item.tags)
+    };
+};
