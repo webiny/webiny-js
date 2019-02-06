ac2f957f2cd79d5edc58db53b4049ce9dae2d0cf
Adrian Smijulj <adrian1358@gmail.com>
2019-02-06 13:07:17 +0100

    pages search fix

--- packages/webiny-api-cms/src/plugins/graphql/pageResolvers/listPages.js
+++ packages/webiny-api-cms/src/plugins/graphql/pageResolvers/listPages.js
@@ -65,8 +65,10 @@ export default (entityFetcher: EntityFetcher) => async (
         }
     ]);
 
+
+    console.log('dobeo IDjeve', ids)
     return new ListResponse(
-        await entityClass.find({ id: { $in: ids.map(item => item._id) } }),
+        await entityClass.find({query: { id: { $in: ids.map(item => item._id) } }}),
         createPaginationMeta({
             page,
             perPage,
