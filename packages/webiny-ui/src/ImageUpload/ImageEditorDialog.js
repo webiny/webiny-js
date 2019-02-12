d460bb92eecd6c2ab8a27f099fc2a56913fde29a
Adrian Smijulj <adrian1358@gmail.com>
2019-02-12 13:00:22 +0100

    Prevent storing previous edit session on opening the same image.

--- packages/webiny-ui/src/ImageUpload/ImageEditorDialog.js
+++ packages/webiny-ui/src/ImageUpload/ImageEditorDialog.js
@@ -6,11 +6,22 @@ import { Dialog, DialogAccept, DialogCancel, DialogFooter, DialogBody } from "we
 type Props = Object & { src: ?string };
 
 class ImageEditorDialog extends React.Component<Props> {
+    imageEditor = React.createRef();
+
+    componentDidUpdate(previousProps) {
+        // Reset form when closing the dialog.
+        const open = { previous: previousProps.open, current: this.props.open };
+        if (open.previous && !open.current) {
+            // Added timeout to avoid minor text flicker when closing the dialog.
+            this.imageEditor.current.resetCanvas();
+        }
+    }
+
     render() {
         const { src, onAccept, ...dialogProps } = this.props;
         return (
             <Dialog {...dialogProps}>
-                <ImageEditor src={src}>
+                <ImageEditor src={src} ref={this.imageEditor}>
                     {({ render, getCanvasDataUrl, activeTool, applyActiveTool }) => (
                         <>
                             <DialogBody>{render()}</DialogBody>
