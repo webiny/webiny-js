f1ade0e7fb351ab54b2af038f518b29105e0cf90
Pavel910 <pavel@webiny.com>
2019-03-01 12:09:35 +0100

    Handle rendering of default menu when no menus are loaded.

--- packages/demo-theme/src/components/DefaultMenu.js
+++ packages/demo-theme/src/components/DefaultMenu.js
@@ -2,6 +2,10 @@
 import React from "react";
 
 const DefaultMenu = ({ data }: Object) => {
+    if (!data) {
+        return null;
+    }
+
     return (
         <ul>
             {data.items.map(item => {
