1d7e8c2935baa2bcf704fd0219bd28c29202f11d
Pavel910 <pavel@webiny.com>
2019-03-13 20:11:33 +0100

    Re-export blocks and pages. Cleanup and fix CMS data.

--- packages/webiny-api-cms/src/install/plugins/importData.js
+++ packages/webiny-api-cms/src/install/plugins/importData.js
@@ -6,7 +6,7 @@ import * as data from "./data";
 import { get } from "lodash";
 export default async (context: Object) => {
     setupEntities(context);
-    const { Menu, CmsSettings } = context.cms.entities;
+    const { CmsSettings } = context.cms.entities;
 
     const { Group } = context.security.entities;
 
@@ -14,35 +14,13 @@ export default async (context: Object) => {
     const group = new Group();
     group.populate({ ...data.group, roles: data.roles }).save();
 
-    const menu = new Menu();
-    menu.populate({
-        title: "Demo Menu",
-        slug: "demo-menu",
-        description: "This is a demo menu.",
-        items: [
-            {
-                type: "cms-menu-item-link",
-                title: "Link 1",
-                url: "https://www.google.com",
-                id: "jopxai1f"
-            },
-            {
-                type: "cms-menu-item-link",
-                title: "Link 2",
-                url: "https://www.duckduckgo.com",
-                id: "jopxaxqh"
-            }
-        ]
-    });
-
-    await menu.save();
-
     await createDefaultBlocks(context);
 
     // Settings init.
     const cmsSettings = new CmsSettings();
     await createDefaultPages(context, { cmsSettings });
     cmsSettings.data.domain = get(context, "cms.siteUrl");
+
 
     await cmsSettings.save();
 };
