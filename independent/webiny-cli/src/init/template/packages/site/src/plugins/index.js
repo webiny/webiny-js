f58832f762065129f0f0085d1c5ebcac01ac1642
Pavel910 <pavel@webiny.com>
2019-03-20 08:00:29 +0100

    Fix cookie policy plugins import in site app.

--- independent/webiny-cli/src/init/template/packages/site/src/plugins/index.js
+++ independent/webiny-cli/src/init/template/packages/site/src/plugins/index.js
@@ -1,7 +1,7 @@
 // @flow
 import cmsPlugins from "webiny-app-cms/site/plugins";
 import { fileUploadPlugin, imagePlugin } from "webiny-app/plugins";
-import cookiePolicyPlugins from "webiny-integration-cookie-policy/plugins/admin";
+import cookiePolicyPlugins from "webiny-integration-cookie-policy/plugins/render";
 import typeformPlugins from "webiny-integration-typeform/plugins/render";
 import mailchimpPlugins from "webiny-integration-mailchimp/plugins/render";
 import gtmPlugins from "webiny-integration-google-tag-manager/plugins/render";
