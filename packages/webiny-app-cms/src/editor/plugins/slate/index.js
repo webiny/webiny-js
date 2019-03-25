02140214acce6bdbb81b3817ff5fd472e8dbb0ad
Pavel Denisjuk <pavel@webiny.com>
2019-03-25 20:40:36 +0100

    fix: auto-scroll document when Slate cursor is outside the viewport. (#495)

--- packages/webiny-app-cms/src/editor/plugins/slate/index.js
+++ packages/webiny-app-cms/src/editor/plugins/slate/index.js
@@ -7,6 +7,7 @@ import codeFactory from "./code";
 import breakFactory from "./break";
 import blockFactory from "./block";
 import linkFactory from "./link";
+import scrollFactory from "./scroll";
 
 const blockPlugins = blockFactory();
 const boldPlugins = boldFactory();
@@ -16,23 +17,25 @@ const codePlugins = codeFactory();
 const linkPlugins = linkFactory();
 const listsPlugins = listsFactory();
 const breakPlugins = breakFactory();
+const scrollPlugins = scrollFactory();
 
 export default [
     // Menu plugins
-    ...blockPlugins.menu,
-    ...boldPlugins.menu,
-    ...italicPlugins.menu,
-    ...underlinePlugins.menu,
-    ...codePlugins.menu,
-    ...linkPlugins.menu,
-    ...listsPlugins.menu,
+    blockPlugins.menu,
+    boldPlugins.menu,
+    italicPlugins.menu,
+    underlinePlugins.menu,
+    codePlugins.menu,
+    linkPlugins.menu,
+    listsPlugins.menu,
     // Editor plugins
-    ...boldPlugins.editor,
-    ...italicPlugins.editor,
-    ...underlinePlugins.editor,
-    ...codePlugins.editor,
-    ...listsPlugins.editor,
-    ...linkPlugins.editor,
-    ...breakPlugins.editor,
-    ...blockPlugins.editor
+    boldPlugins.editor,
+    italicPlugins.editor,
+    underlinePlugins.editor,
+    codePlugins.editor,
+    listsPlugins.editor,
+    linkPlugins.editor,
+    breakPlugins.editor,
+    blockPlugins.editor,
+    scrollPlugins.editor
 ];
