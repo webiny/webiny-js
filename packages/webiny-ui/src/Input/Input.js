7d5d433a84503a31cdc492217fc780b4dbaec0a5
Pavel910 <pavel@webiny.com>
2019-03-27 15:39:59 +0100

    chore: update Input description Flow type.

--- packages/webiny-ui/src/Input/Input.js
+++ packages/webiny-ui/src/Input/Input.js
@@ -22,7 +22,7 @@ type Props = FormComponentProps & {
     disabled?: boolean,
 
     // Description beneath the input.
-    description?: React.Element<any>,
+    description?: React.Node,
 
     // Placeholder is used with `fullwidth` prop instead of a `label`. `label` and `placeholder` are always mutually exclusive.
     placeholder?: string,
