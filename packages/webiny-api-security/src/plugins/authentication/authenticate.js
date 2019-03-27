bad8ddae3d37921e640bcafaf9fa09609ca73e8f
Pavel910 <pavel@webiny.com>
2019-03-27 18:38:38 +0100

    fix: handle both sentence and lower case for authorization header.

--- packages/webiny-api-security/src/plugins/authentication/authenticate.js
+++ packages/webiny-api-security/src/plugins/authentication/authenticate.js
@@ -3,7 +3,9 @@ import { JwtToken } from "./jwtToken";
 
 export default async (context: Object) => {
     const { config, event } = context;
-    let token = (event.headers.Authorization || "").replace(/[b|B]earer\s/, "");
+    const { headers } = event;
+    const authorization = headers["Authorization"] || headers["authorization"] || "";
+    let token = authorization.replace(/[b|B]earer\s/, "");
     let user = null;
     if (token !== "" && event.httpMethod === "POST") {
         const jwt = new JwtToken({ secret: config.security.token.secret });
