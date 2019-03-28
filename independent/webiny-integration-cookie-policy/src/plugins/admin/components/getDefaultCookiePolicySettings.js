7355a016a992cad6cf475186ec0926b5578a3b44
Adrian Smijulj <adrian1358@gmail.com>
2019-03-28 15:05:05 +0100

    fix: add default theme for cookie-policy-integration (#504)

---
+++ independent/webiny-integration-cookie-policy/src/plugins/admin/components/getDefaultCookiePolicySettings.js
@@ -0,0 +1,21 @@
+export default () => {
+    return {
+        position: "bottom",
+        palette: {
+            popup: {
+                background: "#ffffff",
+                text: "#0c0c0c"
+            },
+            button: {
+                background: "#fa5723",
+                text: "#ffffff"
+            }
+        },
+        content: {
+            href: "",
+            message: "",
+            dismiss: "",
+            link: ""
+        }
+    };
+};
