9246c0cdc390a6e92a946cb901d2c49463c2275f
SvenAlHamad <sven@webiny.com>
2019-04-13 20:06:50 +0100

    File details preview and edit, download icons

--- packages/webiny-admin/src/components/FileManager/FileDetails.js
+++ packages/webiny-admin/src/components/FileManager/FileDetails.js
@@ -4,13 +4,21 @@ import React from "react";
 import bytes from "bytes";
 import { css } from "emotion";
 import { Drawer, DrawerContent } from "webiny-ui/Drawer";
-import { ButtonPrimary } from "webiny-ui/Button";
+import { IconButton } from "webiny-ui/Button";
 import { Link } from "webiny-app/router";
+import { Image } from "webiny-app/components";
 import Tags from "./FileDetails/Tags";
+import { Tooltip } from "webiny-ui/Tooltip";
+
+import { ReactComponent as DownloadIcon } from "./icons/round-cloud_download-24px.svg";
+
+import { ReactComponent as EditIcon } from "./icons/round-edit-24px.svg";
 
 const style = {
     wrapper: css({
-        padding: 10
+        padding: 10,
+        height: "100%",
+        overflow: "auto"
     }),
     header: css({
         textAlign: "center",
@@ -18,6 +26,29 @@ const style = {
         padding: 10,
         fontWeight: "600",
         color: "var(--mdc-theme-on-surface)"
+    }),
+    preview: css({
+        backgroundColor: "var(--mdc-theme-background)",
+        padding: 10,
+        position: "relative",
+        width: 200,
+        height: 200,
+        margin: "0 auto",
+        img: {
+            maxHeight: 200,
+            maxWidth: 200,
+            width: "auto",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translateX(-50%) translateY(-50%)",
+            backgroundColor: "#fff"
+        }
+    }),
+    download: css({
+        textAlign: "center",
+        margin: "0 auto",
+        width: "100%"
     }),
     list: css({
         textAlign: "left",
@@ -44,8 +75,26 @@ export default function FileDetails(props: *) {
     const { file, onClose } = props;
     return (
         <Drawer dir="rtl" modal open={file} onClose={onClose}>
-            <div className={style.wrapper}>
+            <div className={style.wrapper} dir="ltr">
                 <div className={style.header}>File details</div>
+                <div className={style.preview}>
+                    {file && <Image src={file.src} alt={file.name} transform={{ width: 300 }} />}
+                </div>
+                <div className={style.download}>
+                    {file && (
+                        <>
+                            <Tooltip content={<span>Download file</span>} placement={"bottom"}>
+                                <IconButton
+                                    onClick={() => window.open(file.src, "_blank")}
+                                    icon={<DownloadIcon style={{ margin: "0 8px 0 0" }} />}
+                                />
+                            </Tooltip>
+                            <Tooltip content={<span>Edit image</span>} placement={"bottom"}>
+                                <IconButton icon={<EditIcon style={{ margin: "0 8px 0 0" }} />} />
+                            </Tooltip>
+                        </>
+                    )}
+                </div>
                 {file && (
                     <DrawerContent dir="ltr">
                         <ul className={style.list}>
@@ -64,13 +113,6 @@ export default function FileDetails(props: *) {
                             <li>
                                 <li-title>Tags:</li-title>
                                 <Tags {...props} />
-                            </li>
-                            <li>
-                                <li-content>
-                                    <Link target="_blank" url={file.src}>
-                                        <ButtonPrimary>Download</ButtonPrimary>
-                                    </Link>
-                                </li-content>
                             </li>
                         </ul>
                     </DrawerContent>
