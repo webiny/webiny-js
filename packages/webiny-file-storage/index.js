023911c48d002a55d792f5cb5e9a985d8022d9e8
Pavel910 <pavel@webiny.com>
2019-04-14 18:18:36 +0200

    chore: remove unused packages.

--- packages/webiny-file-storage/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { default as Storage } from "./src/storage";
-export { default as StorageError } from "./src/storageError";
-export { default as File } from "./src/file";
--- packages/webiny-file-storage/src/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { default as Storage } from "./storage";
-export { default as StorageError } from "./storageError";
-export { default as File } from "./file";
