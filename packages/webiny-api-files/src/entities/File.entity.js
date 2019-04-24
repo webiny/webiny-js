a4cea79c2272145e5222f93a630e858c7dc2be26
Adrian Smijulj <adrian1358@gmail.com>
2019-04-24 19:22:03 +0200

    wip

--- packages/webiny-api-files/src/entities/File.entity.js
+++ packages/webiny-api-files/src/entities/File.entity.js
@@ -1,84 +1,65 @@
 // @flow
-import { Entity } from "webiny-entity";
+import { compose } from "lodash";
+import { validation } from "webiny-validation";
+import { withName } from "@commodo/name";
+import { withHooks } from "@commodo/hooks";
+import { withFields, number, string } from "@commodo/fields";
 
-export interface IFile extends Entity {
-    size: number;
-    type: string;
-    src: string;
-    name: string;
-}
+const FileType = {
+    size: number,
+    type: string,
+    src: string,
+    name: string
+};
 
-export const fileFactory = ({ user = {} }: IFile) => {
-    class File extends Entity {
-        createdBy: Entity;
-        size: number;
-        type: string;
-        src: string;
-        name: string;
+export const File: FileType = compose(
+    withHooks({
+        beforeCreate: async () => {
+            if (!this.src.startsWith("/") || this.src.startsWith("http")) {
+                throw Error(
+                    `File "src" must be a relative path, starting with forward slash ("/").`
+                );
+            }
 
-        constructor() {
-            super();
-            this.attr("createdBy")
-                .char()
-                .setDefaultValue(user.id)
-                .setOnce();
+            if (await File.findOne({ query: { src: this.src } })) {
+                throw Error(`File "src" must be unique. `);
+            }
+        }
+    }),
+    withFields({
+        size: number({
+            validation: validation.create("required")
+        }),
+        type: string({
+            validation: validation.create("required,maxLength:50")
+        }),
+        src: string({
+            validation: validation.create("required,maxLength:200")
+        }),
+        name: string({
+            validation: validation.create("required,maxLength:100")
+        }),
+        tags: string({
+            list: true,
+            validation: tags => {
+                if (Array.isArray(tags)) {
+                    if (tags.length > 15) {
+                        throw Error("You cannot set more than 15 tags.");
+                    }
 
-            this.attr("size")
-                .integer()
-                .setValidators("required")
-                .setOnce();
-
-            this.attr("type")
-                .char()
-                .setValidators("required,maxLength:50")
-                .setOnce();
-
-            this.attr("src")
-                .char()
-                .setValidators("required,maxLength:200")
-                .setOnce();
-
-            this.attr("name")
-                .char()
-                .setValidators("required,maxLength:100");
-
-            this.attr("tags")
-                .array()
-                .setValidators(function(tags) {
-                    if (Array.isArray(tags)) {
-                        if (tags.length > 15) {
-                            throw Error("You cannot set more than 15 tags.");
+                    for (let i = 0; i < tags.length; i++) {
+                        let tag = tags[i];
+                        if (typeof tag !== "string") {
+                            throw Error("Tag must be typeof string.");
                         }
 
-                        for (let i = 0; i < tags.length; i++) {
-                            let tag = tags[i];
-                            if (typeof tag !== "string") {
-                                throw Error("Tag must be typeof string.");
-                            }
-
-                            if (tag.length > 50) {
-                                throw Error(`Tag ${tag} is more than 50 characters long.`);
-                            }
+                        if (tag.length > 50) {
+                            throw Error(`Tag ${tag} is more than 50 characters long.`);
                         }
                     }
-                });
-
-            this.on("beforeCreate", async () => {
-                // TODO: improve this if needed in the future.
-                if (!this.src.startsWith("/") || this.src.startsWith("http")) {
-                    throw Error(
-                        `File "src" must be a relative path, starting with forward slash ("/").`
-                    );
                 }
-
-                if (await File.findOne({ query: { src: this.src } })) {
-                    throw Error(`File "src" must be unique. `);
-                }
-            });
-        }
-    }
-
-    File.classId = "File";
-
-    return File;
-};
+            }
+        })
+    }),
+    withName("File")
+)(function() {});
