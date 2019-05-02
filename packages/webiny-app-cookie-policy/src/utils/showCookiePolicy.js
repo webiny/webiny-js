ec2296206e1f2e3cb258719932edf3808e6c553f
Adrian Smijulj <adrian1358@gmail.com>
2019-05-02 13:58:12 +0200

    fix: cookie policy banner must not reinitialize itself (#526)

--- packages/webiny-app-cookie-policy/src/utils/showCookiePolicy.js
+++ packages/webiny-app-cookie-policy/src/utils/showCookiePolicy.js
@@ -19,7 +19,16 @@ const prepareParams = (params: Object) => {
     return prepared;
 };
 
-const showCookiePolicy = async (params: Object) => {
+let initialized = false;
+const showCookiePolicy = async (params: Object, previewMode: boolean = false) => {
+    if (!previewMode) {
+        if (initialized) {
+            return;
+        }
+
+        initialized = true;
+    }
+
     await load(
         "//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css",
         "//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"
