4ddf30511a131b6c3f8644eab32ce8848cf748ca
Pavel910 <pavel@webiny.com>
2019-05-20 15:24:20 +0200

    fix: remove webfontloader.

--- packages/webiny-app-cms/src/context/CmsProvider.js
+++ packages/webiny-app-cms/src/context/CmsProvider.js
@@ -1,12 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { CmsContextProvider } from "./CmsContext";
-import WebFont from "webfontloader";
 
 export default function CmsProvider({ children, ...props }) {
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            WebFont.load(props.theme.fonts);
-        }
-    }, []);
     return <CmsContextProvider {...props}>{children}</CmsContextProvider>;
 }
