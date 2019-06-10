b15b05aed346bdc19070eb6c7af9774d4a5e53a4
SvenAlHamad <sven@webiny.com>
2019-06-10 14:34:39 +0100

    Form sidebar now has a title

--- packages/webiny-app-forms/src/admin/components/FormEditor/Content.js
+++ packages/webiny-app-forms/src/admin/components/FormEditor/Content.js
@@ -2,7 +2,9 @@ import React from "react";
 import { css } from "emotion";
 import styled from "react-emotion";
 import { SplitView, LeftPanel, RightPanel } from "webiny-admin/components/SplitView";
+import { Typography } from "webiny-ui/Typography";
 import { Tabs, Tab } from "webiny-ui/Tabs";
+import { Icon } from "webiny-ui/Icon";
 import { EditTab } from "./Tabs/EditTab";
 import { TriggersTab } from "./Tabs/TriggersTab";
 import { PreviewTab } from "./Tabs/PreviewTab";
@@ -10,11 +12,29 @@ import { Fields } from "./Fields";
 import { useHotkeys } from "react-hotkeyz";
 import { useFormEditor } from "webiny-app-forms/admin/components/FormEditor/Context";
 
+import { ReactComponent as FormIcon } from "./icons/round-assignment-24px.svg";
+
 const ContentContainer = styled("div")({
     paddingTop: 65
 });
 
-const leftPanel = css({ padding: 40 });
+const LeftBarTitle = styled("div")({
+    borderBottom: "1px solid var(--mdc-theme-on-background)",
+    display: "flex",
+    alignItems: "center",
+    padding: 25,
+    color: "var(--mdc-theme-on-surface)"
+});
+
+const titleIcon = css({
+    height: 24,
+    marginRight: 15,
+    color: "var(--mdc-theme-primary)"
+});
+
+const LeftBarFieldList = styled("div")({
+    padding: 40
+});
 
 export default function Content() {
     const { saveForm } = useFormEditor();
@@ -31,8 +51,14 @@ export default function Content() {
     return (
         <ContentContainer>
             <SplitView>
-                <LeftPanel span={4} className={leftPanel}>
-                    <Fields />
+                <LeftPanel span={4}>
+                    <LeftBarTitle>
+                        <Icon className={titleIcon} icon={<FormIcon />} />
+                        <Typography use={"headline6"}>Form Elements</Typography>
+                    </LeftBarTitle>
+                    <LeftBarFieldList>
+                        <Fields />
+                    </LeftBarFieldList>
                 </LeftPanel>
                 <RightPanel span={8}>
                     <Tabs>
