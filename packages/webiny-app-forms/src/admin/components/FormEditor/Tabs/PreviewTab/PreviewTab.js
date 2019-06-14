46cdf89dff12b470b1882971809d52d00a7db902
SvenAlHamad <sven@webiny.com>
2019-06-14 14:50:35 +0100

    Form preview tab styling

--- packages/webiny-app-forms/src/admin/components/FormEditor/Tabs/PreviewTab/PreviewTab.js
+++ packages/webiny-app-forms/src/admin/components/FormEditor/Tabs/PreviewTab/PreviewTab.js
@@ -1,23 +1,23 @@
 // @flow
 import React from "react";
-import styled from "react-emotion";
+import { css } from "emotion";
 import { useFormEditor } from "webiny-app-forms/admin/components/FormEditor/Context";
 import { Elevation } from "webiny-ui/Elevation";
 import { Form } from "webiny-app-forms/components/Form";
 
-const Container = styled("div")({
-    padding: "40px 60px",
-    background: "white"
+const formPreviewWrapper = css({
+    padding: 40,
+    backgroundColor: "var(--webiny-cms-theme-surface) !important",
+    margin: 40,
+    boxSizing: "border-box"
 });
 
 export const PreviewTab = () => {
     const { data } = useFormEditor();
 
     return (
-        <Elevation z={1}>
-            <Container>
-                <Form preview data={data} />
-            </Container>
+        <Elevation z={1} className={formPreviewWrapper}>
+            <Form preview data={data} />
         </Elevation>
     );
 };
