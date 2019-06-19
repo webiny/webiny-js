0d940ca6f34dffe14199cc2b920996f49318228b
SvenAlHamad <sven@webiny.com>
2019-06-19 19:33:07 +0100

    Submission dialog styling

--- packages/webiny-app-forms/src/admin/plugins/formDetails/formSubmissions/FormSubmissionsList/FormSubmissionDialog.js
+++ packages/webiny-app-forms/src/admin/plugins/formDetails/formSubmissions/FormSubmissionsList/FormSubmissionDialog.js
@@ -9,6 +9,7 @@ import {
     DialogCancel,
     DialogFooter
 } from "webiny-ui/Dialog";
+import { Typography } from "webiny-ui/Typography";
 
 import { i18n } from "webiny-app/i18n";
 const t = i18n.namespace("FormEditor.FormSubmissionDialog");
@@ -16,7 +17,13 @@ const t = i18n.namespace("FormEditor.FormSubmissionDialog");
 const dialogBody = css({
     "&.mdc-dialog__body": {
         marginTop: 0,
-        padding: "0"
+        padding: "0",
+        dl: {
+            padding: 25,
+            dd: {
+                marginBottom: 10
+            }
+        }
     }
 });
 
@@ -35,16 +42,23 @@ const FormSubmissionDialog = ({ formSubmission, onClose, form }: Props) => {
                     </DialogHeader>
 
                     <DialogBody className={dialogBody}>
-                        <ul>
+                        <dl>
                             {Object.keys(formSubmission.data).map(fieldId => {
                                 const field = form.fields.find(field => field.fieldId === fieldId);
                                 return (
-                                    <li key={fieldId}>
-                                        <strong>{field.label}</strong>: {formSubmission.data[fieldId]}
-                                    </li>
+                                    <>
+                                        <dt key={fieldId}>
+                                            <Typography use="overline">{field.label}</Typography>
+                                        </dt>
+                                        <dd>
+                                            <Typography use="body1">
+                                                {formSubmission.data[fieldId]}
+                                            </Typography>
+                                        </dd>
+                                    </>
                                 );
                             })}
-                        </ul>
+                        </dl>
                     </DialogBody>
                     <DialogFooter>
                         <DialogCancel onClick={onClose}>{t`Cancel`}</DialogCancel>
