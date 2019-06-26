1a4c4f71093a0259104abd39b718a3c3062d84d6
SvenAlHamad <sven@webiny.com>
2019-06-26 15:14:24 +0100

    Webhooks styling

--- packages/webiny-app-forms/src/editor/plugins/triggers/webhook.js
+++ packages/webiny-app-forms/src/editor/plugins/triggers/webhook.js
@@ -1,24 +1,8 @@
 import React from "react";
 import { Grid, Cell } from "webiny-ui/Grid";
 import { ReactComponent as LinkIcon } from "./icons/round-link-24px.svg";
-import { DynamicFieldset } from "webiny-ui/DynamicFieldset";
-import { Input } from "webiny-ui/Input";
-import { Typography } from "webiny-ui/Typography";
-import { css } from "emotion";
-import { ButtonPrimary, ButtonSecondary } from "webiny-ui/Button";
-
-const controlButtons = css({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    ">button": {
-        marginRight: 15
-    }
-});
-
-const textStyling = css({
-    color: "var(--mdc-theme-text-secondary-on-background)"
-});
+import { SimpleInputFieldset } from "webiny-ui/DynamicFieldset";
+import { ButtonPrimary } from "webiny-ui/Button";
 
 export default {
     type: "form-editor-trigger",
@@ -34,60 +18,16 @@ export default {
                     <Cell span={12}>
                         <Bind name={"urls"}>
                             {({ value, onChange, ...other }) => (
-                                <DynamicFieldset value={value} onChange={onChange} {...other}>
-                                    {({ actions, header, row, empty }) => (
-                                        <>
-                                            {header(() => (
-                                                <Grid style={{ paddingTop: 0, paddingBottom: 0 }}>
-                                                    <Cell span={12} className={textStyling}>
-                                                        <Typography use={"button"}>
-                                                            Webhook URLs
-                                                        </Typography>
-                                                    </Cell>
-                                                </Grid>
-                                            ))}
-                                            {row(({ index }) => (
-                                                <Grid>
-                                                    <Cell span={9}>
-                                                        <Bind
-                                                            name={`urls.${index}`}
-                                                            validators={["required", "url"]}
-                                                        >
-                                                            <Input label={"URL"} />
-                                                        </Bind>
-                                                    </Cell>
-                                                    <Cell span={3} className={controlButtons}>
-                                                        <ButtonPrimary
-                                                            small
-                                                            onClick={actions.add(index)}
-                                                        >
-                                                            +
-                                                        </ButtonPrimary>
-                                                        <ButtonSecondary
-                                                            small
-                                                            onClick={actions.remove(index)}
-                                                        >
-                                                            -
-                                                        </ButtonSecondary>
-                                                    </Cell>
-                                                </Grid>
-                                            ))}
-                                            {empty(() => (
-                                                <Grid>
-                                                    <Cell span={12} className={textStyling}>
-                                                        <Typography use={"button"}>
-                                                            Webhook URLs
-                                                        </Typography>
-                                                        <br />
-                                                        <ButtonPrimary onClick={actions.add()}>
-                                                            Add URL
-                                                        </ButtonPrimary>
-                                                    </Cell>
-                                                </Grid>
-                                            ))}
-                                        </>
-                                    )}
-                                </DynamicFieldset>
+                                <>
+                                    <SimpleInputFieldset
+                                        value={value}
+                                        onChange={onChange}
+                                        title={"Webhook URLs"}
+                                        inputLabel={"URL"}
+                                        addButtonLabel={"+ Add URL"}
+                                        {...other}
+                                    />
+                                </>
                             )}
                         </Bind>
                     </Cell>
