e10d1bf39166533eeccd06a8ff8fca8b904a3ada
Adrian Smijulj <adrian1358@gmail.com>
2019-07-08 20:50:32 +0200

    wip: page content - save images as references, not data

--- packages/webiny-api-cms/src/plugins/index.js
+++ packages/webiny-api-cms/src/plugins/index.js
@@ -5,5 +5,6 @@ import social from "./pageSettings/social";
 import entities from "./entities";
 import settings from "./settings";
 import graphql from "./graphql";
+import pageContentModifiers from "./entities/pageContentModifiers";
 
-export default [general, seo, social, entities, graphql, settings];
+export default [general, seo, social, entities, graphql, settings, pageContentModifiers];
