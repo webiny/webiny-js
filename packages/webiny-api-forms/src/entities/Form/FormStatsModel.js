15c94ea7807709f91a84d9e1190b26c479eac4de
Adrian Smijulj <adrian1358@gmail.com>
2019-08-01 14:17:37 +0200

    fix: bad find/replace

--- packages/webiny-api-forms/src/entities/Form/FormStatsModel.js
+++ packages/webiny-api-forms/src/entities/Form/FormStatsModel.js
@@ -1,12 +1,35 @@
 import { Model } from "webiny-model";
 
-class LayoutSettingsModel extends Model {
+class FormStatsModel extends Model {
+    views: number;
+    submissions: number;
+    conversionRate: number;
+
     constructor() {
         super();
-        this.attr("renderer")
-            .char()
-            .setValue("default");
+        this.attr("views")
+            .integer()
+            .setDefaultValue(0);
+        this.attr("submissions")
+            .integer()
+            .setDefaultValue(0);
+        this.attr("conversionRate")
+            .float()
+            .setDynamic(() => {
+                if (this.views > 0) {
+                    return ((this.submissions / this.views) * 100).toFixed(2);
+                }
+                return 0;
+            });
+    }
+
+    incrementViews() {
+        this.views++;
+    }
+
+    incrementSubmissions() {
+        this.submissions = this.submissions + 1;
     }
 }
 
-export default LayoutSettingsModel;
+export default FormStatsModel;
