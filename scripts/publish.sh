4f1422413802c77ccccad980982e7c98b0ebafac
Pavel Denisjuk <pavel@webiny.com>
2019-08-25 00:08:49 +0200

    chore: update lerna config.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-lerna publish 1.0.0-next.0 --allow-branch=refactor/scoped-packages --no-push --no-git-tag-version --yes
+lerna version prerelease --yes --conventional-prerelease
+lerna publish
 # Optionally, run commit/push of CHANGELOG.md
