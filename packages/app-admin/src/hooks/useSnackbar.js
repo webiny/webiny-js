4944852a6a9801c7a307bf79c63b5ba8844c2b29
Adrian Smijulj <adrian1358@gmail.com>
2019-09-02 18:49:42 +0200

    wip: useCrud

---
+++ packages/app-admin/src/hooks/useSnackbar.js
@@ -0,0 +1,20 @@
+// @flow
+import * as React from "react";
+import { useUi } from "@webiny/app/components";
+
+export const useSnackbar = () => {
+    const ui = useUi();
+
+    return {
+        showSnackbar: (message, options) => {
+            ui.setState(ui => {
+                return { ...ui, snackbar: { message, options } };
+            });
+        },
+        hideSnackbar: () => {
+            ui.setState(ui => {
+                return { ...ui, snackbar: null };
+            });
+        }
+    };
+};
--- packages/app-security/src/admin/views/Users.js
+++ packages/app-security/src/admin/views/Users.js
@@ -1,41 +1,27 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router-dom";
-import { compose } from "recompose";
 import { get } from "dot-prop-immutable";
 import { pick } from "lodash";
 import { i18n } from "@webiny/app/i18n";
 import { SplitView, LeftPanel, RightPanel } from "@webiny/app-admin/components/SplitView";
 import { FloatingActionButton } from "@webiny/app-admin/components/FloatingActionButton";
-import { withCrud } from "@webiny/app-admin/components";
+import { useCrud } from "@webiny/app-admin/hooks/useCrud";
 import UsersDataList from "./Users/UsersDataList";
 import UsersForm from "./Users/UsersForm";
 import { createUser, deleteUser, loadUser, loadUsers, updateUser } from "./Users/graphql";
 
 const t = i18n.namespace("Security.Users");
 
-const Users = ({ formProps, listProps, location, history }) => {
+const Users = ({ location, history }) => {
     const createNew = useCallback(() => {
         const query = new URLSearchParams(location.search);
         query.delete("id");
         history.push({ search: query.toString() });
     });
 
-    return (
-        <SplitView>
-            <LeftPanel>
-                <UsersDataList {...listProps} />
-            </LeftPanel>
-            <RightPanel>
-                <UsersForm {...formProps} />
-            </RightPanel>
-
-            <FloatingActionButton onClick={createNew} />
-        </SplitView>
-    );
-};
-
-export default compose(
-    withCrud({
+    const { formProps, listProps } = useCrud({
+        history,
+        location,
         list: {
             get: {
                 query: loadUsers,
@@ -76,6 +62,20 @@ export default compose(
                 snackbar: data => t`User {name} saved successfully.`({ name: data.fullName })
             }
         }
-    }),
-    withRouter
-)(Users);
+    });
+
+    return (
+        <SplitView>
+            <LeftPanel>
+                <UsersDataList {...listProps} />
+            </LeftPanel>
+            <RightPanel>
+                <UsersForm {...formProps} />
+            </RightPanel>
+
+            <FloatingActionButton onClick={createNew} />
+        </SplitView>
+    );
+};
+
+export default withRouter(Users);
