ab7a73a1147817ea4a1094587d6a2e3949299269
Pavel Denisjuk <pavel@webiny.com>
2019-09-02 15:50:17 +0200

    wip: refactor SearchBlocks.

--- packages/app-page-builder/src/editor/plugins/blockEditing/SearchBlocks.js
+++ packages/app-page-builder/src/editor/plugins/blockEditing/SearchBlocks.js
@@ -1,11 +1,12 @@
 //@flow
-import * as React from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { compose, withState } from "recompose";
 import { Mutation } from "react-apollo";
 import { connect } from "@webiny/app-page-builder/editor/redux";
 import { deactivatePlugin, updateElement } from "@webiny/app-page-builder/editor/actions";
 import { getContent } from "@webiny/app-page-builder/editor/selectors";
-import { withKeyHandler } from "@webiny/app-page-builder/editor/components";
+import { useKeyHandler } from "@webiny/app-page-builder/editor/hooks/useKeyHandler";
+import { useSnackbar } from "@webiny/app-admin/components/withSnackbar";
 import { getPlugins, unregisterPlugin } from "@webiny/plugins";
 import { createBlockElements } from "@webiny/app-page-builder/editor/utils";
 import { OverlayLayout } from "@webiny/app-admin/components/OverlayLayout";
@@ -54,36 +55,39 @@ const allBlockCategory = {
     icon: <AllIcon />
 };
 
-class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
-    state = {
-        search: "",
-        activeTab: 0,
-        editingBlock: null
-    };
+const SearchBar = ({ updateElement, content }) => {
+    const [search, setSearch] = useState("");
+    const [activeTab, setActiveTab] = useState(0);
+    const [editingBlock, setEditingBlock] = useState(null);
 
-    componentDidMount() {
-        this.props.addKeyHandler("escape", e => {
+    const { addKeyHandler, removeKeyHandler } = useKeyHandler();
+
+    useEffect(() => {
+        addKeyHandler("escape", e => {
             e.preventDefault();
             this.props.deactivatePlugin({ name: "pb-editor-search-blocks-bar" });
         });
-    }
 
-    componentWillUnmount() {
-        this.props.removeKeyHandler("escape");
-    }
+        return () => removeKeyHandler("escape");
+    }, []);
 
-    addBlockToContent = plugin => {
-        const { content } = this.props;
-        const element = { ...content, elements: [...content.elements, createBlockElements(plugin.name)] };
-        this.props.updateElement({ element });
-    };
+    const addBlockToContent = useCallback(
+        plugin => {
+            const element = {
+                ...content,
+                elements: [...content.elements, createBlockElements(plugin.name)]
+            };
+            updateElement({ element });
+        },
+        [content]
+    );
 
-    getCategoryBlocksCount({ plugins, category }) {
+    const getCategoryBlocksCount = useCallback(({ plugins, category }) => {
         return this.getBlocksList({
             blocks: plugins.blocks,
             categories: { active: category }
         }).length;
-    }
+    });
 
     /**
      * Returns a list of blocks - by selected category and by searched term (if present).
@@ -91,7 +95,7 @@ class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
      * @param categories
      * @returns {*}
      */
-    getBlocksList({ blocks, categories }) {
+    const getBlocksList = useCallback(({ blocks, categories }) => {
         const activeCategory = categories.active;
         if (!activeCategory) {
             return [];
@@ -120,10 +124,11 @@ class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
         }
 
         return output;
-    }
+    });
 
-    deleteBlock = async ({ plugin, deleteElement }) => {
-        const { showSnackbar } = this.props;
+    const { showSnackbar } = useSnackbar();
+
+    const deleteBlock = useCallback(async ({ plugin, deleteElement }) => {
         const response = await deleteElement({
             variables: {
                 id: plugin.id
@@ -138,72 +143,93 @@ class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
 
         unregisterPlugin(plugin.name);
         showSnackbar("Block " + plugin.title + " successfully deleted.");
-    };
+    });
 
-    updateBlock = async ({ updateElement, data: { title: name, category } }) => {
-        const plugin = this.state.editingBlock;
-        if (!plugin) {
-            return;
-        }
-
-        const { showSnackbar } = this.props;
-        const response = await updateElement({
-            variables: {
-                id: plugin.id,
-                data: { name, category }
+    const updateBlock = useCallback(
+        async ({ updateElement, data: { title: name, category } }) => {
+            if (!editingBlock) {
+                return;
             }
-        });
 
-        const { error, data } = response.data.pageBuilder.updateElement;
-        if (error) {
-            showSnackbar(error.message);
-            return;
-        }
+            const response = await updateElement({
+                variables: {
+                    id: editingBlock.id,
+                    data: { name, category }
+                }
+            });
 
-        // This will replace previously registered block plugin.
-        createBlockPlugin(data);
+            const { error, data } = response.data.pageBuilder.updateElement;
+            if (error) {
+                showSnackbar(error.message);
+                return;
+            }
 
-        this.setState({ editingBlock: null });
-        setTimeout(() => {
-            // For better UX, success message is shown after 300ms has passed.
-            showSnackbar("Block " + plugin.title + " successfully saved.");
-        }, 300);
-    };
+            // This will replace previously registered block plugin.
+            createBlockPlugin(data);
 
-    renderBlocksList({ plugins, category, deleteElement, updateElement, blocksList }) {
-        const categoryPlugin = plugins.categories.list.find(pl => pl.categoryName === category);
+            setEditingBlock(null);
+            setTimeout(() => {
+                // For better UX, success message is shown after 300ms has passed.
+                showSnackbar("Block " + editingBlock.title + " successfully saved.");
+            }, 300);
+        },
+        [editingBlock]
+    );
 
-        return (
-            <SimpleForm>
-                <SimpleFormHeader title={categoryPlugin.title} icon={categoryPlugin.icon} />
-                <SimpleFormContent>
-                    <Styled.BlockList>
-                        <BlocksList
-                            category={category}
-                            addBlock={this.addBlockToContent}
-                            deactivatePlugin={this.props.deactivatePlugin}
-                            blocks={blocksList}
-                            onEdit={plugin => this.setState({ editingBlock: plugin })}
-                            onDelete={plugin =>
-                                this.deleteBlock({
-                                    plugin,
-                                    deleteElement
-                                })
-                            }
+    const renderBlocksList = useCallback(
+        ({ plugins, category, deleteElement, updateElement, blocksList }) => {
+            const categoryPlugin = plugins.categories.list.find(pl => pl.categoryName === category);
+
+            return (
+                <SimpleForm>
+                    <SimpleFormHeader title={categoryPlugin.title} icon={categoryPlugin.icon} />
+                    <SimpleFormContent>
+                        <Styled.BlockList>
+                            <BlocksList
+                                category={category}
+                                addBlock={addBlockToContent}
+                                deactivatePlugin={deactivatePlugin}
+                                blocks={blocksList}
+                                onEdit={plugin => setEditingBlock(plugin)}
+                                onDelete={plugin =>
+                                    deleteBlock({
+                                        plugin,
+                                        deleteElement
+                                    })
+                                }
+                            />
+                        </Styled.BlockList>
+
+                        <EditBlockDialog
+                            onClose={() => setEditingBlock(null)}
+                            onSubmit={data => updateBlock({ data, updateElement })}
+                            open={!!editingBlock}
+                            plugin={editingBlock}
                         />
-                    </Styled.BlockList>
+                    </SimpleFormContent>
+                </SimpleForm>
+            );
+        },
+        [editingBlock, addBlockToContent, deleteBlock, updateBlock]
+    );
 
-                    <EditBlockDialog
-                        onClose={() => this.setState({ editingBlock: null })}
-                        onSubmit={data => this.updateBlock({ data, updateElement })}
-                        open={!!this.state.editingBlock}
-                        plugin={this.state.editingBlock}
-                    />
-                </SimpleFormContent>
-            </SimpleForm>
+    const renderSearchInput = useCallback(() => {
+        return (
+            <Styled.Input>
+                <Icon className={Styled.searchIcon} icon={<SearchIcon />} />
+                <input
+                    autoFocus
+                    type={"text"}
+                    placeholder="Search blocks..."
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+            </Styled.Input>
         );
-    }
+    }, [search]);
+};
 
+class SearchBarClass extends React.Component<SearchBarProps, SearchBarState> {
     renderSearchInput = () => {
         return (
             <Styled.Input>
