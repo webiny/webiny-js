53e687d963e6cbd16b02b1b0bbfb3c051b225cc6
Adrian Smijulj <adrian1358@gmail.com>
2019-09-13 12:21:23 +0200

    wip: remove withCrud

--- packages/app-admin/src/components/index.js
+++ packages/app-admin/src/components/index.js
@@ -1,6 +1,4 @@
 // @flow
-export { withCrud } from "./withCrud";
-export type { WithCrudProps, WithCrudFormProps, WithCrudListProps } from "./withCrud";
 export { withSnackbar, useSnackbar } from "./withSnackbar";
 export type { WithSnackbarProps } from "./withSnackbar";
 export { withDialog } from "./withDialog";
