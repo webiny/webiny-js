c7f99313ce6b1df24cd78a687f0d7f6b814a9b72
SvenAlHamad <sven@webiny.com>
2019-09-22 13:03:22 +0100

    Form fields and a few bug fixes.

--- packages/app-forms/src/admin/plugins/fields/contact/index.js
+++ packages/app-forms/src/admin/plugins/fields/contact/index.js
@@ -2,5 +2,26 @@ import firstName from "./firstName";
 import lastName from "./lastName";
 import email from "./email";
 import website from "./website";
+import phoneNumber from "./phoneNumber";
+import streetAddress from "./streetAddress";
+import city from "./city";
+import country from "./country";
+import stateRegion from "./stateRegion";
+import companyName from "./companyName";
+import jobTitle from "./jobTitle";
+import postCode from "./postCode";
 
-export default [firstName, lastName, email, website];
+export default [
+    firstName,
+    lastName,
+    email,
+    website,
+    phoneNumber,
+    streetAddress,
+    city,
+    country,
+    stateRegion,
+    companyName,
+    jobTitle,
+    postCode
+];
