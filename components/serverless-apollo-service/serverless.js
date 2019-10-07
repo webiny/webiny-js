e3cf4b8f8fdac89c5bc9d9730446da3e96ab12e6
Pavel Denisjuk <pavel@webiny.com>
2019-10-07 20:43:58 +0200

    fix: track proper method on component removal.

--- components/serverless-apollo-service/serverless.js
+++ components/serverless-apollo-service/serverless.js
@@ -167,7 +167,7 @@ class ApolloService extends Component {
     }
 
     async remove({ track, ...inputs } = {}) {
-        await trackComponent({ track, context: this.context, component });
+        await trackComponent({ track, context: this.context, component, method: "remove" });
         const apiGw = await this.load("@serverless/aws-api-gateway");
         await apiGw.remove(inputs);
 
