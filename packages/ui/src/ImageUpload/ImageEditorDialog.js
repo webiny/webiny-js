47d19bd0a500231db204baefd539dd574c8ff51c
SvenAlHamad <sven@webiny.com>
2019-10-17 11:17:57 +0100

    Fix styles on image editor dialog

--- packages/ui/src/ImageUpload/ImageEditorDialog.js
+++ packages/ui/src/ImageUpload/ImageEditorDialog.js
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { ImageEditor } from "@webiny/ui/ImageEditor";
 import { Tooltip } from "@webiny/ui/Tooltip";
+import { css } from "emotion";
 import {
     Dialog,
     DialogAccept,
@@ -12,6 +13,22 @@ import {
 
 type Props = Object & { options?: Object, src: ?string };
 
+const imageEditorDialog = css({
+    width: "100vw",
+    height: "100vh",
+    ".mdc-dialog__surface": {
+        maxWidth: "100% !important",
+        maxHeight: "100% !important",
+        ".webiny-ui-dialog__content": {
+            maxWidth: "100% !important",
+            maxHeight: "100% !important",
+            width: "100vw",
+            height: "100vh",
+            paddingTop: "0 !important"
+        }
+    }
+});
+
 class ImageEditorDialog extends React.Component<Props, { imageProcessing: boolean }> {
     imageEditor = React.createRef();
 
@@ -19,7 +36,12 @@ class ImageEditorDialog extends React.Component<Props, { imageProcessing: boolea
         const { src, options, onAccept, open, dialogZIndex, ...dialogProps } = this.props;
 
         return (
-            <Dialog style={{ zIndex: dialogZIndex }} open={open} {...dialogProps}>
+            <Dialog
+                className={imageEditorDialog}
+                style={{ zIndex: dialogZIndex }}
+                open={open}
+                {...dialogProps}
+            >
                 {open && (
                     <ImageEditor ref={this.imageEditor} src={src} options={options}>
                         {({ render, activeTool }) => (
