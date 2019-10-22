3b4d07cf8cb6a2c954b84fa175dd12564d1a2eb4
Pavel Denisjuk <pavel@webiny.com>
2019-10-22 11:22:58 +0200

    chore: add prettier rules for md and json.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,19 @@
 module.exports = {
     printWidth: 100,
     parser: "babel",
-    tabWidth: 4
+    tabWidth: 4,
+    overrides: [
+        {
+            files: "*.json",
+            options: {
+                parser: "json"
+            }
+        },
+        {
+            files: "*.md",
+            options: {
+                parser: "markdown"
+            }
+        }
+    ]
 };
