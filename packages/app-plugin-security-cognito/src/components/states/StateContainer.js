39764cf1b097d4f7bf28c856a9ea6ff4b4ccb808
SvenAlHamad <sven@webiny.com>
2019-10-24 15:30:12 +0100

    Updated logo on login component

--- packages/app-plugin-security-cognito/src/components/states/StateContainer.js
+++ packages/app-plugin-security-cognito/src/components/states/StateContainer.js
@@ -1,10 +1,10 @@
-import * as React from "react";
-import { LoginContent, Logo, Wrapper } from "../StyledComponents";
-import logoOrange from "../logo_orange.png";
+import * as React from 'react';
+import {LoginContent, Logo, Wrapper} from '../StyledComponents';
+import logoOrange from '../webiny-orange-logo.svg';
 
-export default ({ children }) => (
-    <Wrapper>
-        <Logo src={logoOrange} />
-        <LoginContent>{children}</LoginContent>
-    </Wrapper>
+export default ({children}) => (
+  <Wrapper>
+    <Logo src={logoOrange} />
+    <LoginContent>{children}</LoginContent>
+  </Wrapper>
 );
