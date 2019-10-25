829e0b94d6dbba3bf428a49091b6f1978622659e
Adrian Smijulj <adrian1358@gmail.com>
2019-10-25 13:56:41 +0200

    wip: install - elements looking good

---
+++ packages/api-page-builder/src/plugins/graphql/installResolver/importData/PbCategory.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        id: "5c436a2b4527ee6f2b8bf3d5",
+        name: "Blog",
+        slug: "blog",
+        url: "/blog/",
+        layout: "blog"
+    }
+];
---
+++ packages/api-page-builder/src/plugins/graphql/installResolver/utils/downloadInstallationFiles.js
@@ -0,0 +1,60 @@
+import download from "download";
+import fs from "fs-extra";
+import extract from "extract-zip";
+import path from "path";
+import rimraf from "rimraf";
+import S3 from "aws-sdk/clients/s3";
+
+function extractZip(zipPath, dir) {
+    return new Promise((resolve, reject) => {
+        extract(zipPath, { dir }, e => {
+            if (e) {
+                reject(e);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+function deleteFile(path) {
+    return new Promise((resolve, reject) => {
+        rimraf(path, e => {
+            if (e) {
+                reject(e);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+const INSTALL_DIR = "/tmp/installation-files";
+const INSTALL_ZIP_PATH = path.join(INSTALL_DIR, "apiPageBuilder.zip");
+const INSTALL_EXTRACT_DIR = path.join(INSTALL_DIR, "apiPageBuilder");
+
+let downloaded = false;
+export default async () => {
+    if (downloaded) {
+        return INSTALL_EXTRACT_DIR;
+    }
+
+    // Start the download of initial Page Builder page / block images.
+    // eslint-disable-next-line
+    const s3 = new S3({ region: process.env.AWS_REGION });
+
+    // 5. ⚠️ Save files.
+    // Before continuing, let's make sure the download has finished.
+    const installationFilesUrl = await s3.getSignedUrlPromise("getObject", {
+        Bucket: "wby-pb-install",
+        Key: "page_builder_install_files.zip"
+    });
+
+    fs.ensureDirSync(INSTALL_DIR);
+    fs.writeFileSync(INSTALL_ZIP_PATH, await download(installationFilesUrl));
+
+    await extractZip(INSTALL_ZIP_PATH, INSTALL_EXTRACT_DIR);
+    await deleteFile(INSTALL_ZIP_PATH);
+
+    return INSTALL_EXTRACT_DIR;
+};
