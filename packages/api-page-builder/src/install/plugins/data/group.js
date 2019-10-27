da9e434bc0ace1ae89bc2207e179d5b4c5660ba0
Pavel Denisjuk <pavel@webiny.com>
2019-10-27 20:28:14 +0100

    chore: remove "install" folder

--- packages/api-page-builder/src/install/plugins/data/group.js
+++
@@ -1,7 +0,0 @@
-// @flow
-export default {
-    name: "Page Builder - Full Access",
-    description: "Grants access to page categories, pages, and menus.",
-    system: true,
-    slug: "pb-full-access"
-};
--- packages/api-page-builder/src/install/plugins/data/index.js
+++
@@ -1,3 +0,0 @@
-// @flow
-export { default as group } from "./group";
-export { default as roles } from "./roles";
