448b695296fc93d2ce18d0acbb21a0e93799033b
Pavel Denisjuk <pavel@webiny.com>
2019-10-27 17:28:24 +0100

    chore: add handling of Network errors.

--- examples/apps/admin/src/config/apollo.js
+++ examples/apps/admin/src/config/apollo.js
@@ -1,12 +1,35 @@
+import React from "react";
 import ApolloClient from "apollo-client";
 import { ApolloLink } from "apollo-link";
 import { BatchHttpLink } from "apollo-link-batch-http";
+import { ErrorLink } from "apollo-link-error";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { useQuery } from "react-apollo";
+import gql from "graphql-tag";
 import { createAuthLink } from "@webiny/app-security/components";
 import { createOmitTypenameLink } from "@webiny/app/graphql";
+import { Alert } from "@webiny/ui/Alert";
+import { Elevation } from "@webiny/ui/Elevation";
+
+const GET_ERROR = gql`
+    {
+        error
+    }
+`;
 
 export default new ApolloClient({
     link: ApolloLink.from([
+        new ErrorLink(({ networkError, operation }) => {
+            if (networkError) {
+                const { cache } = operation.getContext();
+                cache.writeQuery({
+                    query: GET_ERROR,
+                    data: {
+                        error: "Your API seems to be unavailable!"
+                    }
+                });
+            }
+        }),
         createOmitTypenameLink(),
         createAuthLink(),
         new BatchHttpLink({ uri: process.env.REACT_APP_GRAPHQL_API_URL })
@@ -16,3 +39,20 @@ export default new ApolloClient({
         dataIdFromObject: obj => obj.id || null
     })
 });
+
+export const NetworkMonitor = ({ children }) => {
+    const { data } = useQuery(GET_ERROR, { fetchPolicy: "cache-only" });
+
+    return data && data.error ? (
+        <Elevation z={2}>
+            <Alert type={"danger"} title={"Network error"}>
+                {data.error}
+                <br />
+                Make sure your API is available at{" "}
+                <strong>{process.env.REACT_APP_GRAPHQL_API_URL}</strong>
+            </Alert>
+        </Elevation>
+    ) : (
+        children
+    );
+};
--- examples/apps/admin/src/index.js
+++ examples/apps/admin/src/index.js
@@ -6,14 +6,16 @@ import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import App from "./App";
-import apolloClient from "./config/apollo";
+import apolloClient, { NetworkMonitor } from "./config/apollo";
 
 ReactDOM.render(
     <ApolloProvider client={apolloClient}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <App />
-            <Route exact path="/" render={() => <Redirect to="/page-builder/pages" />} />
-        </BrowserRouter>
+        <NetworkMonitor>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <App />
+                <Route exact path="/" render={() => <Redirect to="/page-builder/pages" />} />
+            </BrowserRouter>
+        </NetworkMonitor>
     </ApolloProvider>,
     document.getElementById("root")
 );
