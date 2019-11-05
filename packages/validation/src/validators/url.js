5d77b095acec42e3519ec4087a7bfcef6d941ad3
Adrian Smijulj <adrian1358@gmail.com>
2019-11-05 14:40:21 +0100

    feat: implement "allowRelative" option for "url" validator

--- packages/validation/src/validators/url.js
+++ packages/validation/src/validators/url.js
@@ -1,6 +1,21 @@
 // @flow
 import ValidationError from "./../validationError";
 
+const regex = {
+    base: new RegExp(
+        // eslint-disable-next-line
+        /^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
+    ),
+    ip: new RegExp(
+        // eslint-disable-next-line
+        /^(https?:\/\/)(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+    ),
+    relative: new RegExp(
+        // eslint-disable-next-line
+        /^\/.*$/
+    )
+};
+
 export default (value: any, params: Array<string>) => {
     if (!value) return;
     value = value + "";
@@ -9,22 +24,18 @@ export default (value: any, params: Array<string>) => {
         value = value.replace("//localhost", "//localhost.com");
     }
 
-    const regex = new RegExp(
-        // eslint-disable-next-line
-        /^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
-    );
-
-    const ipRegex = new RegExp(
-        // eslint-disable-next-line
-        /^(https?:\/\/)(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-    );
-
-    if (regex.test(value)) {
+    if (regex.base.test(value)) {
         if (!params.includes("noIp")) {
             return;
         }
 
-        if (!ipRegex.test(value)) {
+        if (!regex.ip.test(value)) {
+            return;
+        }
+    }
+
+    if (params.includes("allowRelative")) {
+        if (regex.relative.test(value)) {
             return;
         }
     }
