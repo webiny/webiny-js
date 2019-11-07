20124065c8945c09adf4f9dc03dba361a03c442d
Pavel Denisjuk <pavel@webiny.com>
2019-11-07 20:14:59 +0100

    fix: handle undefined value for Slate editor.

--- packages/app-page-builder/src/render/components/Slate.js
+++ packages/app-page-builder/src/render/components/Slate.js
@@ -8,7 +8,7 @@ import { usePageBuilder } from "@webiny/app-page-builder/hooks/usePageBuilder";
 const SlateEditor = props => {
     const { theme } = usePageBuilder();
     const plugins = useRef(getPlugins("pb-render-slate-editor").map(pl => pl.slate));
-    const [value] = useState(Value.fromJSON(props.value));
+    const [value] = useState(Value.fromJSON(props.value || {}));
 
     return (
         <Editor
