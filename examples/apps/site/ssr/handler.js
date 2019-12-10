dc3a42d7c9b02803f9d3333a301ca0be1c6022c4
Adrian Smijulj <adrian1358@gmail.com>
2019-12-10 22:43:05 +0100

    refactor: cleanup

--- examples/apps/site/ssr/handler.js
+++ examples/apps/site/ssr/handler.js
@@ -2,8 +2,23 @@
  * It is absolutely mandatory to clear module cache for Apollo to generate the correct state!
  */
 module.exports.handler = async event => {
-    const { url } = event;
+    const path = event.path.replace("/ssr", "");
     delete require.cache[require.resolve("./ssr")];
     const { renderer } = require("./ssr");
-    return await renderer(url);
+    const { html } = await renderer(path);
+
+    try {
+        return {
+            statusCode: 200,
+            body: html,
+            headers: { "Cache-Control": "no-store", "Content-Type": "text/html" }
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            type: "text/html",
+            body: e.stack,
+            headers: { "Cache-Control": "no-store", "Content-Type": "text/html" }
+        };
+    }
 };
