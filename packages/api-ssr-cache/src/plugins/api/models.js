fd87d7368a846f4442302df9d06531a08e8f81f5
Adrian Smijulj <adrian1358@gmail.com>
2019-12-12 23:24:46 +0100

    wip: ssr-generation-url via settings

--- packages/api-ssr-cache/src/plugins/api/models.js
+++ packages/api-ssr-cache/src/plugins/api/models.js
@@ -3,6 +3,7 @@ import { flow } from "lodash";
 import { withStorage, withCrudLogs, withSoftDelete, withFields } from "@webiny/commodo";
 import { withUser } from "@webiny/api-security";
 import ssrCache from "./models/ssrCache.model";
+import ssrCacheSettings from "./models/ssrCacheSettings.model";
 
 export default () => ({
     name: "graphql-context-models",
@@ -27,9 +28,11 @@ export default () => ({
                 withCrudLogs()
             )();
 
-        const SsrCache = ssrCache({ createBase, context });
+        const SsrCacheSettings = ssrCacheSettings({ createBase, context });
+        const SsrCache = ssrCache({ createBase, context, SsrCacheSettings });
 
         context.models = {
+            SsrCacheSettings,
             SsrCache
         };
     }
