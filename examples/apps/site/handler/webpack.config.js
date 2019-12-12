3572bcef6e74797fe299db03f7e0ebc41ac32149
Adrian Smijulj <adrian1358@gmail.com>
2019-12-12 14:40:35 +0100

    wip: use API URL from env variables

--- examples/apps/site/handler/webpack.config.js
+++ examples/apps/site/handler/webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
 const WebpackBar = require("webpackbar");
 
 module.exports = {
     entry: path.resolve("handler", "handler.js"),
     mode: "development",
     devtool: false,
-    plugins: [new WebpackBar({ name: "Site handler" })],
+    plugins: [
+        new WebpackBar({ name: "Site handler" }),
+        new webpack.DefinePlugin({
+            "process.env.REACT_APP_API_URL": JSON.stringify(process.env.REACT_APP_API_URL)
+        })
+    ],
     output: {
         filename: "handler.js",
         path: path.resolve("build"),
