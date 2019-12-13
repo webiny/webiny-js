01d762dbb673d5ee95fb92f544c0569164a2ab06
Adrian Smijulj <adrian1358@gmail.com>
2019-12-13 17:12:51 +0100

    wip: take query params into consideration when caching

--- examples/apps/site/handler/.cloudfnsrc.js
+++ examples/apps/site/handler/.cloudfnsrc.js
@@ -1,8 +1,17 @@
+// BABEL_ENV=development DEBUG=fn:* env-cmd -r .env.json -e default,dev cloudfns build handler
+
+const webpack = require("webpack");
+
 module.exports = {
     name: "site-fn",
     plugins: ["aws-lambda"],
-    webpack: ({ setEntry, setOutput }) => {
+    webpack: ({ setEntry, setOutput, addPlugins }) => {
         setEntry("handler.js");
         setOutput("handler.js");
+        addPlugins([
+            new webpack.DefinePlugin({
+                "process.env.GRAPHQL_API_URL": JSON.stringify(process.env.REACT_APP_GRAPHQL_API_URL)
+            })
+        ]);
     }
 };
