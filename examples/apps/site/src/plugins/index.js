2c398d49129301f2ace5757521a7d460f7caa127
Pavel Denisjuk <pavel@webiny.com>
2019-12-20 13:34:32 +0100

    chore: update example app setups

--- examples/apps/site/src/plugins/index.js
+++ examples/apps/site/src/plugins/index.js
@@ -1,5 +1,5 @@
-// @flow
 import pageBuilderPlugins from "@webiny/app-page-builder/site/plugins";
+import pageBuilderTheme from "@webiny/app-page-builder-theme";
 import { fileUploadPlugin, imagePlugin } from "@webiny/app/plugins";
 import cookiePolicyPlugins from "@webiny/app-cookie-policy/render";
 import typeformPlugins from "@webiny/app-typeform/render";
@@ -8,18 +8,21 @@ import gtmPlugins from "@webiny/app-google-tag-manager/render";
 import i18nPlugins from "@webiny/app-i18n/site/plugins";
 import formsSitePlugins from "@webiny/app-form-builder/site/plugins";
 import formsPbPlugins from "@webiny/app-form-builder/page-builder/site/plugins";
+import formBuilderTheme from "@webiny/app-form-builder-theme";
 
 const plugins = [
     fileUploadPlugin(),
     imagePlugin,
     pageBuilderPlugins,
+    pageBuilderTheme(),
     cookiePolicyPlugins,
     typeformPlugins,
     mailchimpPlugins,
     gtmPlugins,
     i18nPlugins,
     formsSitePlugins,
-    formsPbPlugins
+    formsPbPlugins,
+    formBuilderTheme()
 ];
 
 export default plugins;
