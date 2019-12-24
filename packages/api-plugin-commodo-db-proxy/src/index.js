b8967f3856b4743c8e47238e6b5d5d0df538f4aa
Adrian Smijulj <adrian1358@gmail.com>
2019-12-24 14:50:39 +0100

    wip: pass withId and id to context

--- packages/api-plugin-commodo-db-proxy/src/index.js
+++ packages/api-plugin-commodo-db-proxy/src/index.js
@@ -1,11 +1,14 @@
-import { DbProxyDriver, id } from "@webiny/commodo-fields-storage-db-proxy";
+import { DbProxyDriver, id, isId, withId } from "@webiny/commodo-fields-storage-db-proxy";
 
 export default options => ({
     name: "graphql-context-commodo",
     type: "graphql-context",
     async preApply(context) {
         if (!context.commodo) {
-            context.commodo = {};
+            context.commodo = {
+                isId,
+                withId
+            };
         }
 
         if (!context.commodo.fields) {
