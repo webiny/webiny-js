dae889d81713c6d13efed0fe209394ad90a9de3a
Pavel Denisjuk <pavel@webiny.com>
2019-12-26 22:10:03 +0100

    rename root index file to index.js

--- packages/api-google-tag-manager/index.js
+++ packages/api-google-tag-manager/index.js
@@ -1,4 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var src_1 = require("./src");
-exports.default = src_1.default;
+import src from "./src";
+export default src;
--- packages/api-plugin-security-cognito/index.js
+++ packages/api-plugin-security-cognito/index.js
@@ -1,4 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var src_1 = require("./src");
-exports.default = src_1.default;
+export { default } from "./src";
