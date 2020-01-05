f06ec8101938d3e22f7956b5961ebf983589c068
naokia <hexmap@gmail.com>
2020-01-05 16:22:24 +0900

    fix: set minimum password length to the same as defined in cognito

--- packages/validation/__tests__/password.test.js
+++ packages/validation/__tests__/password.test.js
@@ -8,14 +8,14 @@ describe("password test", () => {
 
     it("should fail - values are too short", () => {
         return Promise.all([
-            validation.validate("12312", "password").should.be.rejected,
+            validation.validate("1231231", "password").should.be.rejected,
             validation.validate("123123", "password:10").should.be.rejected
         ]);
     });
 
     it("should pass - value is long enough", () => {
         return Promise.all([
-            validation.validate("123123", "password").should.become(true),
+            validation.validate("12312312", "password").should.become(true),
             validation.validate("123", "password:3").should.become(true)
         ]);
     });
--- packages/validation/src/validators/password.js
+++ packages/validation/src/validators/password.js
@@ -5,7 +5,7 @@ export default (value: any, params: Array<string> = []) => {
     if (!value) return;
     value = value + "";
 
-    const length = params[0] || 6;
+    const length = params[0] || 8;
     const test = value.match(new RegExp(`^.{${length},}$`));
     if (test === null) {
         throw new ValidationError(`Password must contain at least ${length} characters`);
