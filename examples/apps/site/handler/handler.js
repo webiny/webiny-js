f80fbb014bac1e94877fc5cf08667d2c32bfd2b1
Adrian Smijulj <adrian1358@gmail.com>
2020-01-06 14:46:15 +0100

    fix: cdn cache invalidation file name

--- examples/apps/site/handler/handler.js
+++ examples/apps/site/handler/handler.js
@@ -1,6 +1,6 @@
 import { create } from "@webiny/cloud-function";
 import filesPlugins from "@webiny/cloud-function-files";
 import ssrPlugins from "@webiny/cloud-function-ssr";
-import cdnInvalidationPlugins from "@webiny/cloud-function-ssr/cdnInvalidation";
+import cdnCacheInvalidationPlugins from "@webiny/cloud-function-ssr/cdnCacheInvalidation";
 
-export const handler = create(filesPlugins(), ssrPlugins(), cdnInvalidationPlugins());
+export const handler = create(filesPlugins(), ssrPlugins(), cdnCacheInvalidationPlugins());
