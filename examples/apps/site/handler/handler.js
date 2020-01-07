e5367717d9e6426edd82af9fce7a9271cd009c28
Adrian Smijulj <adrian1358@gmail.com>
2020-01-07 09:51:41 +0100

    fix: add cdn cache invalidation plugins

--- examples/apps/site/handler/handler.js
+++ examples/apps/site/handler/handler.js
@@ -1,6 +1,13 @@
 import { create } from "@webiny/cloud-function";
 import filesPlugins from "@webiny/cloud-function-files";
 import ssrPlugins from "@webiny/cloud-function-ssr";
-import cdnCacheInvalidationPlugins from "@webiny/cloud-function-ssr/cdnCacheInvalidation";
+import cdnSsrCacheInvalidationPlugins from "@webiny/cloud-function-ssr/cdnSsrCacheInvalidation";
 
-export const handler = create(filesPlugins(), ssrPlugins(), cdnCacheInvalidationPlugins());
+export const handler = create(
+    filesPlugins(),
+    ssrPlugins({
+        ssrCacheTtl: 2592000, // 30 days in seconds.
+        ssrCacheTtlState: 20
+    }),
+    cdnSsrCacheInvalidationPlugins()
+);
