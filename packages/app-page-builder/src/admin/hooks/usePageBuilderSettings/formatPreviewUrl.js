4d606fac274a89b3897cc016bd234dba661721d0
Adrian Smijulj <adrian1358@gmail.com>
2020-01-08 21:40:58 +0100

    feat: add ts on the preview urls

--- packages/app-page-builder/src/admin/hooks/usePageBuilderSettings/formatPreviewUrl.js
+++ packages/app-page-builder/src/admin/hooks/usePageBuilderSettings/formatPreviewUrl.js
@@ -1,7 +1,7 @@
 // @flow
 import { trimEnd } from "lodash";
 
-export default ({ page: { url, id }, domain }: Object) => {
+export default ({ page: { url, id }, domain, addTimestamp }: Object) => {
     let previewUrl = "";
 
     if (!domain) {
@@ -18,5 +18,10 @@ export default ({ page: { url, id }, domain }: Object) => {
     previewUrl = trimEnd(previewUrl, "/");
     previewUrl += url;
     previewUrl += "?preview=" + id;
+
+    if (addTimestamp !== false) {
+        previewUrl += "&ts=" + new Date().getTime();
+    }
+
     return previewUrl;
 };
