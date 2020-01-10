ac1ae04438ffd17e6d7d8861419d7dd42e70b257
Adrian Smijulj <adrian1358@gmail.com>
2020-01-10 18:26:55 +0100

    fix: rename "cloud-function" to "http-handler"

--- packages/api-page-builder/src/plugins/useSsrCacheTags.js
+++ packages/api-page-builder/src/plugins/useSsrCacheTags.js
@@ -1,5 +1,5 @@
 import { withHooks } from "@webiny/commodo";
-import SsrApiClient from "@webiny/cloud-function-ssr/Client";
+import SsrApiClient from "@webiny/http-handler-ssr/Client";
 import gql from "graphql-tag";
 import { hasScope } from "@webiny/api-security";
 import { Response, ErrorResponse, NotFoundResponse } from "@webiny/api";
