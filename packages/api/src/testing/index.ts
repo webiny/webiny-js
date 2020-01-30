0f9a77f497258fb84a5367f603a5fdaf5897b828
Pavel Denisjuk <pavel@webiny.com>
2020-01-30 19:27:52 +0100

    feat: add setupContext tool to setup context for testing.

--- packages/api/src/testing/index.ts
+++ packages/api/src/testing/index.ts
@@ -1,5 +1,6 @@
 import { createSchema } from "../index";
 import { PluginsContainer } from "@webiny/plugins/PluginsContainer";
+import { GraphQLContextPlugin } from "@webiny/api/types";
 
 export const setupSchema = async plugins => {
     const pluginsContainer = new PluginsContainer([plugins]);
@@ -12,4 +13,32 @@ export const setupSchema = async plugins => {
         schema,
         context
     };
+};
+
+export const setupContext = async (plugins, baseContext = {}) => {
+    const pluginsContainer = new PluginsContainer([plugins]);
+
+    const context = { ...baseContext, plugins: pluginsContainer };
+
+    // Process `graphql-context` plugins
+    const ctxPlugins = pluginsContainer.byType<GraphQLContextPlugin>("graphql-context");
+    for (let i = 0; i < ctxPlugins.length; i++) {
+        if (typeof ctxPlugins[i].preApply === "function") {
+            await ctxPlugins[i].preApply(context);
+        }
+    }
+
+    for (let i = 0; i < ctxPlugins.length; i++) {
+        if (typeof ctxPlugins[i].apply === "function") {
+            await ctxPlugins[i].apply(context);
+        }
+    }
+
+    for (let i = 0; i < ctxPlugins.length; i++) {
+        if (typeof ctxPlugins[i].postApply === "function") {
+            await ctxPlugins[i].postApply(context);
+        }
+    }
+
+    return context;
 };
