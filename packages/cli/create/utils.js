27296961f37176a1414f01c968f88aeba9f02242
Pavel Denisjuk <pavel@webiny.com>
2020-02-06 12:31:49 +0100

    fix: check tags using semver.

--- packages/cli/create/utils.js
+++ packages/cli/create/utils.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const pRetry = require("p-retry");
+const semver = require("semver");
 
 async function getPackageVersion(name, tag = "latest") {
     const getVersion = async () => {
@@ -7,7 +8,7 @@ async function getPackageVersion(name, tag = "latest") {
         const json = await res.json();
 
         const tagVersion = json["dist-tags"][tag];
-        if (!tagVersion || tagVersion < json["dist-tags"]["latest"]) {
+        if (!tagVersion || semver.lte(tagVersion, json["dist-tags"]["latest"])) {
             return json["dist-tags"]["latest"];
         }
 
