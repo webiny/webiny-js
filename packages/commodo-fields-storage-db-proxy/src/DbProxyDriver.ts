e921529997fed7fa80c00eb09e1029af7dcb9520
Adrian Smijulj <adrian1358@gmail.com>
2020-02-06 09:21:25 +0100

    fix: minor error message correction

--- packages/commodo-fields-storage-db-proxy/src/DbProxyDriver.ts
+++ packages/commodo-fields-storage-db-proxy/src/DbProxyDriver.ts
@@ -32,7 +32,7 @@ class DbProxyClient {
         if (parsedPayload.error) {
             if (MONGO_CONNECTION_ERRORS.includes(parsedPayload.error.name)) {
                 throw new Error(
-                    `Could not connect to MongoDB server, make sure the connection string is correct and that the database server allows outside connections. Check https://docs.webiny.com/docs/get-started/quick-start#3-setup-database-connection for more information.`
+                    `Could not connect to the MongoDB server, make sure the connection string is correct and that the database server allows outside connections. Check https://docs.webiny.com/docs/get-started/quick-start#3-setup-database-connection for more information.`
                 );
             }
             throw new Error(`${parsedPayload.error.name}: ${parsedPayload.error.message}`);
