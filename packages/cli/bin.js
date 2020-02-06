b269d90716a64c07b7310993806d2fa2234001ca
Pavel Denisjuk <pavel@webiny.com>
2020-02-06 12:31:31 +0100

    fix: abort process gracefully for yarn v2.

--- packages/cli/bin.js
+++ packages/cli/bin.js
@@ -3,6 +3,7 @@
 
 const chalk = require("chalk");
 const execa = require("execa");
+const semver = require("semver");
 const { verifyConfig } = require("./config");
 const currentNodeVersion = process.versions.node;
 const majorVersion = parseInt(currentNodeVersion.split(".")[0]);
@@ -13,17 +14,25 @@ const minorVersion = parseInt(currentNodeVersion.split(".")[1]);
         console.error(
             chalk.red(
                 "You are running Node " +
-                currentNodeVersion +
-                ".\n" +
-                "Webiny requires Node 10.14 or higher. \n" +
-                "Please update your version of Node."
+                    currentNodeVersion +
+                    ".\n" +
+                    "Webiny requires Node 10.14 or higher. \n" +
+                    "Please update your version of Node."
             )
         );
         process.exit(1);
     }
 
     try {
-        await execa("yarn", ["--version"]);
+        const { stdout } = await execa("yarn", ["--version"]);
+        if (!semver.satisfies(stdout, "^1")) {
+            console.error(
+                chalk.red(
+                    `"@webiny/cli" currently only works with yarn@^1. yarn@^2 will be supported when the stable release is out.`
+                )
+            );
+            process.exit(1);
+        }
     } catch (err) {
         console.error(
             chalk.red(`"@webiny/cli" depends on "yarn" and its built-in support for workspaces.`)
