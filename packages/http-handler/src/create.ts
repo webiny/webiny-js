4288412e4a2d2267bd994be575daf3a06ff2f254
Adrian Smijulj <adrian1358@gmail.com>
2020-02-06 14:51:24 +0100

    fix: on thrown errors, respond with a nice error message

--- packages/http-handler/src/create.ts
+++ packages/http-handler/src/create.ts
@@ -8,39 +8,49 @@ import {
 } from "./types";
 
 export default (...plugins) => async (...args) => {
-    const context = {
-        plugins: new PluginsContainer(plugins)
-    };
+    try {
+        const context = {
+            plugins: new PluginsContainer(plugins)
+        };
 
-    const contextPlugins = context.plugins.byType<HttpContextPlugin>("context");
-    for (let i = 0; i < contextPlugins.length; i++) {
-        contextPlugins[i].apply({ context, args });
-    }
-
-    const beforeHandle = context.plugins.byType<HttpBeforeHandlePlugin>("before-handle");
-    for (let i = 0; i < beforeHandle.length; i++) {
-        await beforeHandle[i].handle({ context, args });
-    }
-
-    let result;
-    const handlers = context.plugins.byType<HttpHandlerPlugin>("handler");
-    for (let i = 0; i < handlers.length; i++) {
-        const handler = handlers[i];
-        if (handler.canHandle({ context, args })) {
-            result = await handler.handle({ context, args });
+        const contextPlugins = context.plugins.byType<HttpContextPlugin>("context");
+        for (let i = 0; i < contextPlugins.length; i++) {
+            contextPlugins[i].apply({ context, args });
         }
-    }
 
-    const afterHandle = context.plugins.byType<HttpAfterHandlePlugin>("after-handle");
-    for (let i = 0; i < afterHandle.length; i++) {
-        await afterHandle[i].handle({ context, args, result });
-    }
+        const beforeHandle = context.plugins.byType<HttpBeforeHandlePlugin>("before-handle");
+        for (let i = 0; i < beforeHandle.length; i++) {
+            await beforeHandle[i].handle({ context, args });
+        }
 
-    if (!result) {
+        let result;
+        const handlers = context.plugins.byType<HttpHandlerPlugin>("handler");
+        for (let i = 0; i < handlers.length; i++) {
+            const handler = handlers[i];
+            if (handler.canHandle({ context, args })) {
+                result = await handler.handle({ context, args });
+            }
+        }
+
+        const afterHandle = context.plugins.byType<HttpAfterHandlePlugin>("after-handle");
+        for (let i = 0; i < afterHandle.length; i++) {
+            await afterHandle[i].handle({ context, args, result });
+        }
+
+        if (!result) {
+            return createResponse({
+                body: "Request not handled. Please check registered handlers."
+            });
+        }
+
+        return result;
+    } catch (e) {
+        // TODO: Make optional and pluginable.
         return createResponse({
-            body: "Request not handled. Please check registered handlers."
+            statusCode: 404,
+            type: "text/html",
+            body: `<html><h1>An error occurred</h1><p>${e.stack}</p></html>`,
+            headers: { "Cache-Control": "no-store" }
         });
     }
-
-    return result;
 };
