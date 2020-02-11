d330df8fdc35aeacc77772a1ed76cbeb788f111e
Adrian Smijulj <adrian1358@gmail.com>
2020-02-11 12:43:52 +0100

    feat: enable registration of error-handler plugin

--- packages/http-handler/src/types.ts
+++ packages/http-handler/src/types.ts
@@ -23,6 +23,12 @@ export type HttpHandlerPlugin = Plugin & {
     handle(params: { context: HttpContext; args: HttpArgs }): Promise<any>;
 };
 
+export type HttpErrorHandlerPlugin = Plugin & {
+    type: "error-handler";
+    canHandle(params: { context: HttpContext; args: HttpArgs; error: any }): boolean;
+    handle(params: { context: HttpContext; args: HttpArgs; error: any }): Promise<any>;
+};
+
 export type HttpAfterHandlerPlugin = Plugin & {
     type: "after-handler";
     handle(params: { context: HttpContext; args: HttpArgs; result: any }): Promise<void>;
