f6721f35a511eb48b923c407c8014de4e979d07c
Adrian Smijulj <adrian1358@gmail.com>
2020-02-13 18:56:26 +0100

    feat: track "tracking-disabled" activity

--- packages/cli/sls/disableTracking.js
+++ packages/cli/sls/disableTracking.js
@@ -1,6 +1,9 @@
 const { setTracking } = require("./../config");
+const { version } = require(require.resolve("@webiny/cli/package.json"));
+const { trackActivity } = require("@webiny/tracking");
 
 module.exports = async () => {
     setTracking(false);
+    await trackActivity({ type: 'tracking-disabled', cliVersion: version });
     console.log("INFO: tracking of Webiny stats is now DISABLED!");
 };
