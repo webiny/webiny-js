7066d992c8046ff94271d036078760f4e9ae532f
Adrian Smijulj <adrian1358@gmail.com>
2020-02-14 13:47:40 +0100

    fix: add activityId

--- packages/cli/sls/disableTracking.js
+++ packages/cli/sls/disableTracking.js
@@ -1,9 +1,10 @@
 const { setTracking } = require("./../config");
 const { version } = require(require.resolve("@webiny/cli/package.json"));
 const { trackActivity } = require("@webiny/tracking");
+const uniqueId = require('uniqid');
 
 module.exports = async () => {
     setTracking(false);
-    await trackActivity({ type: 'tracking-disabled', cliVersion: version });
+    await trackActivity({ type: 'tracking-disabled', cliVersion: version, activityId: uniqueId() });
     console.log("INFO: tracking of Webiny stats is now DISABLED!");
 };
