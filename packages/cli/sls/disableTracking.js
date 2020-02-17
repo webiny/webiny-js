d9ee4349d23a035e8727fff1bc2cbf170efe2ec1
Adrian Smijulj <adrian1358@gmail.com>
2020-02-17 18:47:47 +0100

    fix: change order of execution

--- packages/cli/sls/disableTracking.js
+++ packages/cli/sls/disableTracking.js
@@ -4,7 +4,7 @@ const { trackActivity } = require("@webiny/tracking");
 const uniqueId = require('uniqid');
 
 module.exports = async () => {
-    setTracking(false);
     await trackActivity({ type: 'tracking-disabled', cliVersion: version, activityId: uniqueId() });
+    setTracking(false);
     console.log("INFO: tracking of Webiny stats is now DISABLED!");
 };
