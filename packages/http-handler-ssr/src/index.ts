6f600e7a8568b14495b40122de8c9f486778d75c
Adrian Smijulj <adrian1358@gmail.com>
2020-02-18 12:11:25 +0100

    fix: revert changes

--- packages/http-handler-ssr/src/index.ts
+++ packages/http-handler-ssr/src/index.ts
@@ -3,33 +3,17 @@ import ssrServe from "./ssrServe";
 import models from "./models";
 import { withFields, boolean, number, string, fields } from "@webiny/commodo/fields";
 
-const Animal = withFields({
-    name: string({
-        validate: value => {
-            if (!value) {
-                throw Error("A pet must have a name!");
-            }
-        }
-    }),
-    age: number(),
-    isAwesome: boolean(),
-    about: fields({
+const OptionsModel = withFields({
+    ssrFunction: string({ value: process.env.SSR_FUNCTION }),
+    cache: fields({
         value: {},
         instanceOf: withFields({
-            type: string({ value: "cat" }),
-            dangerous: boolean({ value: true })
+            enabled: boolean({ value: false }),
+            ttl: number({ value: 80 }),
+            staleTtl: number({ value: 20 })
         })()
     })
 })();
-
-const animal = new Animal();
-animal.populate({ age: "7" }); // Throws data type error, cannot populate a string with number.
-
-animal.populate({ age: 7 });
-await animal.validate(); // Throws a validation error - name must be defined.
-
-animal.name = "Garfield";
-await animal.validate(); // All good.
 
 export default rawOptions => {
     const options = new OptionsModel().populate(rawOptions);
