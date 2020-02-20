4a2c150e25a6b8dacc95999ea81e04d224f7ca8e
Adrian Smijulj <adrian1358@gmail.com>
2020-02-20 18:47:24 +0100

    fix: load default pages via plugins

--- examples/apps/site/src/App.js
+++ examples/apps/site/src/App.js
@@ -4,7 +4,6 @@ import { registerPlugins, getPlugins } from "@webiny/plugins";
 import { PageBuilderProvider } from "@webiny/app-page-builder/contexts/PageBuilder";
 import { UiProvider } from "@webiny/app/contexts/Ui";
 import plugins from "./plugins";
-import { GenericNotFoundPage, GenericErrorPage } from "./pageBuilder";
 import { I18NProvider } from "@webiny/app-i18n/contexts/I18N";
 import "./App.scss";
 
@@ -13,18 +12,11 @@ registerPlugins(plugins);
 // Execute `init` plugins, they may register more plugins dynamically
 getPlugins("webiny-init").forEach(plugin => plugin.callback());
 
-const defaults = {
-    pages: {
-        notFound: GenericNotFoundPage,
-        error: GenericErrorPage
-    }
-};
-
 const App = () => {
     return (
         <I18NProvider>
             <UiProvider>
-                <PageBuilderProvider defaults={defaults}>
+                <PageBuilderProvider>
                     {getPlugins("route").map(pl =>
                         React.cloneElement(pl.route, { key: pl.name, exact: true })
                     )}
