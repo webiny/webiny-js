14e3bcfba37b8ea568a4346ff0ba73e314fe56f3
Adrian Smijulj <adrian1358@gmail.com>
2020-02-21 18:08:34 +0100

    refactor: because of a TS error

--- packages/app-page-builder-theme/src/components/defaultPages/DefaultErrorPage.tsx
+++ packages/app-page-builder-theme/src/components/defaultPages/DefaultErrorPage.tsx
@@ -3,32 +3,36 @@ import DefaultPage from "./DefaultPage";
 
 const getPbNotInstalledErrorMessage = () => {
     // Check if window exists first (does not exist while doing SSR).
-    const isLocalhost = typeof window === 'object' && window.location.hostname === "localhost";
+    const isLocalhost = typeof window === "object" && window.location.hostname === "localhost";
     let adminUi = <a href="/admin">Admin UI</a>;
     if (isLocalhost) {
         adminUi = <strong>Admin UI</strong>;
     }
 
-    return [
-        "Installation incomplete",
-        <>
-            <p>Page Builder is not installed!</p>
-            <p>
-                Before you continue, please open up the {adminUi} and complete the installation
-                wizard.
-            </p>
-        </>
-    ];
+    return {
+        message: "Installation incomplete",
+        title: (
+            <>
+                <p>Page Builder is not installed!</p>
+                <p>
+                    Before you continue, please open up the {adminUi} and complete the installation
+                    wizard.
+                </p>
+            </>
+        )
+    };
 };
 
 export default function DefaultErrorPage(props) {
-    let message = "The link is either broken or the page has been removed.";
-    let title = "An error occurred";
+    let pageProps = {
+        message: "The link is either broken or the page has been removed.",
+        title: <>An error occurred</>
+    };
 
     // Once the Page Builder is installed, this can be safely removed.
     if (props.error.code === "PB_NOT_INSTALLED") {
-        [title, message] = getPbNotInstalledErrorMessage();
+        pageProps = getPbNotInstalledErrorMessage();
     }
 
-    return <DefaultPage message={message} title={title} />;
+    return <DefaultPage {...pageProps} />;
 }
