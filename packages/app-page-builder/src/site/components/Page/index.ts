9479a560e2938b280ce14c3e4b838e0cbaeb8414
Adrian Smijulj <adrian1358@gmail.com>
2020-02-22 18:48:53 +0100

    fix: export graphql

--- packages/app-page-builder/src/site/components/Page/index.ts
+++ packages/app-page-builder/src/site/components/Page/index.ts
@@ -2,3 +2,4 @@ export { default as Layout } from "./Layout";
 export { default as Page } from "./Page";
 export { default as PageLoad } from "./PageLoad";
 export { default as PageRender } from "./PageRender";
+export * from "./graphql";
