c0ffc47a32c85d4589d54bdfa27e159b7c0f5197
Adrian Smijulj <adrian1358@gmail.com>
2020-02-22 18:48:37 +0100

    wip: add types

--- packages/app-page-builder/src/site/components/Page/Page.tsx
+++ packages/app-page-builder/src/site/components/Page/Page.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import PageRender from "./PageRender";
 import PageLoad from "./PageLoad";
 
-const Page = (props) => {
+type PageProps = {
+    url?: string;
+    id?: string;
+    parent?: string;
+    data?: any;
+};
+
+const Page = (props: PageProps) => {
     if (props.data) {
         return <PageRender {...props} />;
     }
 
     if (props.location) {
-        return <PageLoad {...props}/>
+        return <PageLoad {...props} />;
     }
 
     return null;
