de662fe9036dbbae44c1d8ee1bf1c2b4e326b816
Adrian Smijulj <adrian1358@gmail.com>
2020-02-23 07:50:08 +0100

    wip: add PageContent component

--- packages/app-page-builder/src/site/components/Page/index.ts
+++ packages/app-page-builder/src/site/components/Page/index.ts
@@ -1,5 +1,6 @@
 export { default as Layout } from "./Layout";
 export { default as Page } from "./Page";
+export { default as PageContent } from "./PageContent";
 export { default as PageLoad } from "./PageLoad";
 export { default as PageRender } from "./PageRender";
 export * from "./graphql";
