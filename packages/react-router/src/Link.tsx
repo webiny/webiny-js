d40e97ed7c0c1d7dd63678652cca05ca32664f9a
Tim Rexer <tjrexer@gmail.com>
2020-02-22 19:40:03 -0500

    Updated Link based on Slate examples

--- packages/react-router/src/Link.tsx
+++ packages/react-router/src/Link.tsx
@@ -1,17 +1,36 @@
 import React, { useContext, useEffect } from "react";
-import { Link as RouterLink, LinkProps as RouterLinkProps } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import { RouterContext } from "./context/RouterContext";
+import PropTypes from "prop-types";
 
-export type LinkProps = RouterLinkProps;
+export type LinkProps =  {
+    to: string,
+    href: string,
+    noFollow: boolean,
+    newTab: boolean,
+    children: any,
+    className: string,
+    onClick: () => void
+}
 
-function Link({ children, ...props }: LinkProps) {
-    const { onLink } = useContext(RouterContext);
+function Link({children, ...props}: LinkProps) {
+        const { onLink } = useContext(RouterContext)
 
-    useEffect(() => {
-        onLink(props.to as string);
-    }, [props.to]);
+        const isInternal = 'href' in props ? props.href.startsWith("/") : false
+        const LinkComponent = isInternal ? RouterLink : "a"
+        const componentProps = {
+            ...props,
+            rel: props.noFollow ? "nofollow" : null,
+            target: props.newTab ? "_blank" : "_self",
+            [isInternal ? "to" : "href"]: 'href' in props ? props.href : null
+        }
 
-    return <RouterLink {...props}>{children}</RouterLink>;
+        return (
+            <LinkComponent 
+                {...componentProps}>
+                {children}
+            </LinkComponent>
+        );
 }
 
 export { Link };
