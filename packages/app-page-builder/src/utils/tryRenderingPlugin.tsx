1152663d3b94e71c24e623c50743d8c0cd82076c
Fsalker <andreiii500@yahoo.com>
2020-02-25 23:15:58 +0200

    Edited error msg and removed the test error

--- packages/app-page-builder/src/utils/tryRenderingPlugin.tsx
+++ packages/app-page-builder/src/utils/tryRenderingPlugin.tsx
@@ -2,15 +2,12 @@ import React from "react";
 
 const tryRenderingPlugin = fn => {
     try {
-        // if (fn.name === "asd") throw new Error("error1238523");
         return fn();
     } catch (err) {
         console.log(err);
         return (
             <div style={{ paddingTop: "15px", paddingBottom: "15px", color: "rgb(206, 17, 38)" }}>
-                <div style={{ fontSize: "1.5em", paddingBottom: "5px" }}>
-                    An error has occurred: {err.message}
-                </div>
+                <div style={{ fontSize: "1.5em", paddingBottom: "5px" }}>Error: {err.message}</div>
                 <div style={{ fontSize: "1.1em", color: "rgb(206, 17, 38)" }}>
                     Check the console for more information regarding the error.
                 </div>
