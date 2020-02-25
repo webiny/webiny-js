0764bec5a9661e15a7b77a5c458625bd7d90af43
Fsalker <andreiii500@yahoo.com>
2020-02-25 08:02:20 +0200

    Tweaked the styles a little

--- packages/app-page-builder/src/utils/tryRenderingPlugin.tsx
+++ packages/app-page-builder/src/utils/tryRenderingPlugin.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 const tryRenderingPlugin = fn => {
-    // console.log(fn.name);
     try {
         // if (fn.name === "asd") throw new Error("error1238523");
         return fn();
     } catch (err) {
         console.log(err);
         return (
-            <div style={{ padding: "15px", color: "rgb(206, 17, 38)" }}>
-                <div style={{ fontSize: "2.0em", paddingBottom: "5px" }}>
+            <div style={{ paddingTop: "15px", paddingBottom: "15px", color: "rgb(206, 17, 38)" }}>
+                <div style={{ fontSize: "1.5em", paddingBottom: "5px" }}>
                     An error has occurred: {err.message}
                 </div>
-                <div style={{ fontSize: "1.3em", color: "rgb(206, 17, 38)", padding: "10px" }}>
+                <div style={{ fontSize: "1.1em", color: "rgb(206, 17, 38)" }}>
                     Check the console for more information regarding the error.
                 </div>
             </div>
