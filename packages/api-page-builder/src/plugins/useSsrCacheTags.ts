16b35ebb5a8454b41d157787a2459f480b3cfb27
Fsalker <andreiii500@yahoo.com>
2020-02-28 21:34:14 +0200

    Readded a removed thing

--- packages/api-page-builder/src/plugins/useSsrCacheTags.ts
+++ packages/api-page-builder/src/plugins/useSsrCacheTags.ts
@@ -70,6 +70,7 @@ export default () => [
                         } catch {
                             // Do nothing.
                         }
+                        removeCallback();
                     });
                 }
             })(PbSettings);
