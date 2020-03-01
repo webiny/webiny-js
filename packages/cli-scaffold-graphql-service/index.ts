5788a7f31a1b63b36f1dc98d7278bec8e262cf83
Fsalker <andreiii500@yahoo.com>
2020-03-01 13:44:31 +0200

    1234321

---
+++ packages/cli-scaffold-graphql-service/index.ts
@@ -0,0 +1,11 @@
+module.exports = [{
+  name: "",
+  type: "scaffold-template",
+  scaffold: {
+    name: "GraphQL Apollo Service",
+    description: "Creates an /api template for this service. Also fills it in serverless.yml",
+    generate: () => {
+            console.log("HELL YEAH")
+    }
+  }
+}]
