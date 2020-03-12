6259d8b2a19c492d68a5e188f9a16e38c1c5a1cc
Maxim Moroz <m.moroz@users-MBP.softarex.lan>
2020-03-12 16:49:29 +0300

    feat: Add url validation for I18N_API_URL

--- packages/api-i18n/src/plugins/service.ts
+++ packages/api-i18n/src/plugins/service.ts
@@ -29,6 +29,14 @@ export default () => [
                 return localesCache;
             }
 
+            const urlRegEx = /^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+
+            if (!urlRegEx.test(process.env.I18N_API_URL)) {
+                throw Error(
+                    `I18N_API_URL env variable is not a valid URL. Make sure it's specified in the service configuration.`
+                );
+            }
+
             const client = new GraphQLClient(process.env.I18N_API_URL);
 
             const response = await client.request(GET_I18N_INFORMATION);
