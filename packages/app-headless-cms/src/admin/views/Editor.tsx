b7b47e112dac30e3444569f0aacd9bba58071f0a
Adrian Smijulj <adrian1358@gmail.com>
2020-03-12 10:04:06 +0100

    fix: import statement

--- packages/app-headless-cms/src/admin/views/Editor.tsx
+++ packages/app-headless-cms/src/admin/views/Editor.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import Snackbar from "@webiny/app-admin/plugins/Snackbar/Snackbar";
-import { FormEditorApp } from "@webiny/app-headless-cms/admin/components/FormEditor";
+import { FormEditorApp } from "../components/FormEditor";
 
 export default function EditorView() {
     return (
