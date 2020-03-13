fad2fb11669e62ca769e8e361aeb4d58009b6b24
Adrian Smijulj <adrian1358@gmail.com>
2020-03-13 06:34:36 +0100

    fix: remove unnecessary files

--- packages/app-headless-cms/src/admin/plugins/index.ts
+++ packages/app-headless-cms/src/admin/plugins/index.ts
@@ -3,27 +3,16 @@ import menus from "./menus";
 import fields from "./fields";
 import groups from "./groups";
 import validators from "./validators";
-import settings from "./settings";
 
-import formEditorPlugins from "@webiny/app-headless-cms/editor/plugins";
-
-import previewContent from "./formDetails/previewContent";
-import formRevisions from "./formDetails/formRevisions";
-import formSubmissions from "./formDetails/formSubmissions";
-import install from "./install";
+import contentModelEditorPlugins from "../../editor/plugins";
 
 export default [
-    install,
-    settings,
     routes,
     menus,
-    formSubmissions,
-    previewContent,
-    formRevisions,
 
     // Editor
     fields,
     groups,
     validators,
-    formEditorPlugins
+    contentModelEditorPlugins
 ];
