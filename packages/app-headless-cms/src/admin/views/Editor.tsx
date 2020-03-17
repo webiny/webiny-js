ec47c1fdebb15a3db837d48dc51f8fe2dd2c72e0
Adrian Smijulj <adrian1358@gmail.com>
2020-03-17 19:21:24 +0100

    fix: rename "FormEditorApp" to "ContentModelEditorApp"

--- packages/app-headless-cms/src/admin/views/Editor.tsx
+++ packages/app-headless-cms/src/admin/views/Editor.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import Snackbar from "@webiny/app-admin/plugins/Snackbar/Snackbar";
-import { FormEditorApp } from "../components/FormEditor";
+import { ContentModelEditorApp } from "../components/ContentModelEditor";
 
 export default function EditorView() {
     return (
         <DndProvider backend={HTML5Backend}>
-            <FormEditorApp />
+            <ContentModelEditorApp />
             <div style={{ zIndex: 10, position: "absolute" }}>
                 <Snackbar />
             </div>
