fd98bac2385e63f139a0a64db4be8c36612e314d
Fsalker <andreiii500@yahoo.com>
2020-03-18 15:14:32 +0200

    fix: 'storybook' script runs succesfully now: Webpack now correctly fetches the aliases

--- packages/ui/.storybook/webpack.config.js
+++ packages/ui/.storybook/webpack.config.js
@@ -1,39 +1,44 @@
 const path = require("path");
+const aliases = require("@webiny/project-utils/aliases/webpack");
 
 const includePaths = [
-    path.join(__dirname, "../node_modules"),
-    path.join(__dirname, "../../../node_modules"),
-    path.join(__dirname, "../../../node_modules/material-components-web/node_modules")
+          path.join(__dirname, "../node_modules"),
+          path.join(__dirname, "../../../node_modules"),
+          path.join(
+                    __dirname,
+                    "../../../node_modules/material-components-web/node_modules"
+          )
 ];
 
 module.exports = ({ config }) => {
-    config.resolve.extensions.push(".ts", ".tsx");
+          config.resolve.extensions.push(".ts", ".tsx");
+          config.resolve.alias = aliases;
 
-    config.module.rules.push({
-        test: /\.tsx?$/,
-        use: [
-            {
-                loader: require.resolve("babel-loader"),
-                options: {
-                    babelrc: true,
-                    ...require("../.babelrc.js")
-                }
-            },
-            require.resolve("react-docgen-typescript-loader")
-        ]
-    });
+          config.module.rules.push({
+                    test: /\.tsx?$/,
+                    use: [
+                              {
+                                        loader: require.resolve("babel-loader"),
+                                        options: {
+                                                  babelrc: true,
+                                                  ...require("../.babelrc.js")
+                                        }
+                              },
+                              require.resolve("react-docgen-typescript-loader")
+                    ]
+          });
 
-    config.module.rules.unshift({
-        test: /\.scss$/,
-        use: [
-            "style-loader",
-            "css-loader",
-            {
-                loader: "sass-loader",
-                options: { includePaths }
-            }
-        ]
-    });
+          config.module.rules.unshift({
+                    test: /\.scss$/,
+                    use: [
+                              "style-loader",
+                              "css-loader",
+                              {
+                                        loader: "sass-loader",
+                                        options: { includePaths }
+                              }
+                    ]
+          });
 
-    return config;
+          return config;
 };
