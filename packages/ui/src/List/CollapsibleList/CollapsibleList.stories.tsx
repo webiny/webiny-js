77da65093ff185f42feb32ffd8cf0662c7f2372a
Fsalker <andreiii500@yahoo.com>
2020-03-18 15:18:45 +0200

    docs: added CollapsibleList storybook

---
+++ packages/ui/src/List/CollapsibleList/CollapsibleList.stories.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+    Story,
+    StoryReadme,
+    StorySandboxCode,
+    StorySandbox,
+    StorySandboxExample
+} from "@webiny/storybook-utils/Story";
+import readme from "./README.md";
+import { withKnobs, boolean, text, object, array } from "@storybook/addon-knobs";
+
+import { CollapsibleList } from "./index";
+import { SimpleListItem } from "./../List";
+
+const story = storiesOf("Components/List", module);
+story.addDecorator(withKnobs);
+
+story.add(
+    "collapsible list",
+    () => {
+        const name = "Andrei";
+        return (
+            <Story>
+                <StoryReadme>{readme}</StoryReadme>
+
+                <StorySandbox>
+                    <StorySandboxExample>
+                        <CollapsibleList handle={<SimpleListItem text="Tokens (click)" />}>
+                            <SimpleListItem text="Bananas" />
+                            <SimpleListItem text="Grapes" />
+                            <SimpleListItem text="Oranges" />
+                        </CollapsibleList>
+                    </StorySandboxExample>
+                    <StorySandboxCode>
+                        {`
+                         <CollapsibleList handle={<SimpleListItem text="Fruits" />}>
+                            <SimpleListItem text="Bananas" />
+                            <SimpleListItem text="Grapes" />
+                            <SimpleListItem text="Oranges" />
+                        </CollapsibleList>
+                      `}
+                    </StorySandboxCode>
+                </StorySandbox>
+            </Story>
+        );
+    },
+    {
+        info: {
+            propTables: [
+                CollapsibleList,
+                SimpleListItem
+                // List,
+                // ListItem,
+                // ListItemText,
+                // ListItemTextSecondary,
+                // ListItemMeta,
+                // ListItemGraphic
+            ]
+        }
+    }
+);
