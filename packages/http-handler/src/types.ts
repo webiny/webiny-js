015be7098b4d2b357d0e8cccfbdbf2e56be514b2
Adrian Smijulj <adrian1358@gmail.com>
2020-03-22 18:21:49 +0100

    fix: rename "HttpContext" to "HttpHandlerContext"

--- packages/http-handler/src/types.ts
+++ packages/http-handler/src/types.ts
@@ -1,6 +1,6 @@
 import { Plugin, PluginsContainer } from "@webiny/plugins/types";
 
-export type HttpContext = {
+export type HttpHandlerContext = {
     plugins: PluginsContainer;
     [key: string]: any;
 };
@@ -9,27 +9,27 @@ export type HttpArgs = any[];
 
 export type HttpContextPlugin = Plugin & {
     type: "context";
-    apply(params: { context: HttpContext; args: HttpArgs }): void;
+    apply(params: { context: HttpHandlerContext; args: HttpArgs }): void;
 };
 
 export type HttpBeforeHandlerPlugin = Plugin & {
     type: "before-handler";
-    handle(params: { context: HttpContext; args: HttpArgs }): Promise<void>;
+    handle(params: { context: HttpHandlerContext; args: HttpArgs }): Promise<void>;
 };
 
 export type HttpHandlerPlugin = Plugin & {
     type: "handler";
-    canHandle(params: { context: HttpContext; args: HttpArgs }): boolean;
-    handle(params: { context: HttpContext; args: HttpArgs }): Promise<any>;
+    canHandle(params: { context: HttpHandlerContext; args: HttpArgs }): boolean;
+    handle(params: { context: HttpHandlerContext; args: HttpArgs }): Promise<any>;
 };
 
 export type HttpErrorHandlerPlugin = Plugin & {
     type: "error-handler";
-    canHandle(params: { context: HttpContext; args: HttpArgs; error: any }): boolean;
-    handle(params: { context: HttpContext; args: HttpArgs; error: any }): Promise<any>;
+    canHandle(params: { context: HttpHandlerContext; args: HttpArgs; error: any }): boolean;
+    handle(params: { context: HttpHandlerContext; args: HttpArgs; error: any }): Promise<any>;
 };
 
 export type HttpAfterHandlerPlugin = Plugin & {
     type: "after-handler";
-    handle(params: { context: HttpContext; args: HttpArgs; result: any }): Promise<void>;
+    handle(params: { context: HttpHandlerContext; args: HttpArgs; result: any }): Promise<void>;
 };
