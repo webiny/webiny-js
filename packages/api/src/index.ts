7eb2358afd6ccdc919cd75eb534d46b1c680f082
Adrian Smijulj <adrian1358@gmail.com>
2020-03-22 11:00:12 +0100

    fix: remove old files and PluginsContainer from exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,2 @@
-export { createHandler, createSchema } from "./createHandler";
-export { PluginsContainer } from "@webiny/plugins";
 export * from "./graphql/responses";
 export { default as emptyResolver } from "./graphql/emptyResolver";
