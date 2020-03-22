c9de521963b5ed98b5181eacf4c4e67133d5f565
Adrian Smijulj <adrian1358@gmail.com>
2020-03-22 18:14:43 +0100

    feat: export createSchema as a "handler-apollo-server-create-schema" plugin

---
+++ packages/api/src/plugins/httpHandlerApolloServer.ts
@@ -0,0 +1,9 @@
+import createSchema from "./../createSchema";
+
+export default [
+    {
+        type: "handler-apollo-server-create-schema",
+        name: "handler-apollo-server-create-schema",
+        create: createSchema
+    }
+];
