803dea5317fb757412756dc06951fa33a0357ec3
Adrian Smijulj <adrian1358@gmail.com>
2020-03-24 19:58:06 +0100

    fix: use latest handler creation code

--- examples/api/services/headlessCmsHandler/src/index.ts
+++ examples/api/services/headlessCmsHandler/src/index.ts
@@ -4,11 +4,12 @@ import securityService from "@webiny/api-security/plugins/service";
 import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
 import i18nPlugins from "@webiny/api-i18n/plugins/service";
 
+declare const HTTP_HANDLER_APOLLO_SERVER_OPTIONS: any;
 declare const DB_PROXY_OPTIONS: any;
 declare const SECURITY_OPTIONS: any;
 
 export const handler = createHandler(
-    headlessCmsHttpHandlerApolloServerPlugins(),
+    headlessCmsHttpHandlerApolloServerPlugins(HTTP_HANDLER_APOLLO_SERVER_OPTIONS),
     dbProxy(DB_PROXY_OPTIONS),
     securityService(SECURITY_OPTIONS),
     i18nPlugins()
