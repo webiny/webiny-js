2192e923af53ce835242cd11b6e90d80aeba7351
Adrian Smijulj <adrian1358@gmail.com>
2020-03-24 20:18:43 +0100

    fix: correct TS configs

--- examples/api/services/headlessCmsHandler/src/index.ts
+++ examples/api/services/headlessCmsHandler/src/index.ts
@@ -1,7 +1,7 @@
 import { createHandler } from "@webiny/http-handler";
 import headlessCmsHttpHandlerApolloServerPlugins from "@webiny/api-headless-cms/handler";
-import securityService from "@webiny/api-security/plugins/service";
 import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
+import securityService from "@webiny/api-security/plugins/service";
 import i18nPlugins from "@webiny/api-i18n/plugins/service";
 
 declare const HTTP_HANDLER_APOLLO_SERVER_OPTIONS: any;
