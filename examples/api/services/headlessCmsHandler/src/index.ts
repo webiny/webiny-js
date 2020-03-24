0a84c6f65c501369876aa5c2565db7ee6c1c5b83
Adrian Smijulj <adrian1358@gmail.com>
2020-03-24 18:46:44 +0100

    fix: rename folder "headless" to "headlessCmsHandler"

---
+++ examples/api/services/headlessCmsHandler/src/index.ts
@@ -0,0 +1,15 @@
+import { createHandler } from "@webiny/http-handler";
+import headlessCmsHttpHandlerApolloServerPlugins from "@webiny/api-headless-cms/handler";
+import securityService from "@webiny/api-security/plugins/service";
+import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
+import i18nPlugins from "@webiny/api-i18n/plugins/service";
+
+declare const DB_PROXY_OPTIONS: any;
+declare const SECURITY_OPTIONS: any;
+
+export const handler = createHandler(
+    headlessCmsHttpHandlerApolloServerPlugins(),
+    dbProxy(DB_PROXY_OPTIONS),
+    securityService(SECURITY_OPTIONS),
+    i18nPlugins()
+);
