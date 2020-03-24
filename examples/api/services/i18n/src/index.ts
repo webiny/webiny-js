a73351ad5f13bb8366adef012948b31a7e7972f0
Adrian Smijulj <adrian1358@gmail.com>
2020-03-24 18:36:25 +0100

    fix: no need to add "apiHttpHandlerApolloServerPlugins" separately

--- examples/api/services/i18n/src/index.ts
+++ examples/api/services/i18n/src/index.ts
@@ -1,6 +1,5 @@
 import { createHandler } from "@webiny/http-handler";
 import httpHandlerApolloServerPlugins from "@webiny/http-handler-apollo-server";
-import apiHttpHandlerApolloServerPlugins from "@webiny/api/plugins/httpHandlerApolloServer";
 import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
 import securityServicePlugins from "@webiny/api-security/plugins/service";
 import i18nPlugins from "@webiny/api-i18n/plugins";
@@ -11,7 +10,6 @@ declare const SECURITY_OPTIONS: any;
 
 export const handler = createHandler(
     httpHandlerApolloServerPlugins(HTTP_HANDLER_APOLLO_SERVER_OPTIONS),
-    apiHttpHandlerApolloServerPlugins,
     dbProxy(DB_PROXY_OPTIONS),
     securityServicePlugins(SECURITY_OPTIONS),
     i18nPlugins()
