23e2e7eeea49c959ec27ff773103e323308824b6
Fsalker <andreiii500@yahoo.com>
2020-03-24 09:01:48 +0200

    feat: Split "Account" form into 3 "Section" elements

--- packages/app-plugin-security-cognito-theme/src/admin/views/UserAccountForm.tsx
+++ packages/app-plugin-security-cognito-theme/src/admin/views/UserAccountForm.tsx
@@ -3,18 +3,26 @@ import { Cell, Grid } from "@webiny/ui/Grid";
 import { validation } from "@webiny/validation";
 import { Input } from "@webiny/ui/Input";
 import { i18n } from "@webiny/app/i18n";
+import Section from "@webiny/ui/Section";
+import styled from "@emotion/styled";
 
 const t = i18n.ns("app-plugin-security-cognito-theme/users/account");
 
+const RenderSection = styled("div")({
+    backgroundColor: "var(--mdc-theme-background)",
+    height: "100%",
+    padding: 25
+});
+
 const UserAccountForm = ({ Bind, data, fields }) => {
     return (
-        <Grid>
-            <Cell span={3}>
+        <RenderSection>
+            <Section title="Avatar">
                 <Grid>
                     <Cell span={12}>{fields.avatar}</Cell>
                 </Grid>
-            </Cell>
-            <Cell span={9}>
+            </Section>
+            <Section title="Bio">
                 <Grid>
                     <Cell span={12}>{fields.email}</Cell>
                     <Cell span={12}>
@@ -29,10 +37,12 @@ const UserAccountForm = ({ Bind, data, fields }) => {
                     </Cell>
                     <Cell span={12}>{fields.firstName}</Cell>
                     <Cell span={12}>{fields.lastName}</Cell>
-                    <Cell span={12}>{fields.personalAccessTokens}</Cell>
                 </Grid>
-            </Cell>
-        </Grid>
+            </Section>
+            <Section title="Personal Access Tokens">
+                <Cell span={12}>{fields.personalAccessTokens}</Cell>
+            </Section>
+        </RenderSection>
     );
 };
 
