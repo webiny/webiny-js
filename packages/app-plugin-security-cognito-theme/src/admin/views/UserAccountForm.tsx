0e131dabf5c526184a8da27abad25c6db1a9cf32
Fsalker <andreiii500@yahoo.com>
2020-03-24 16:21:30 +0200

    feat: Account settings page now looks much more awesome
    The Avatar's on top & there are Accordions for the other inputs.

--- packages/app-plugin-security-cognito-theme/src/admin/views/UserAccountForm.tsx
+++ packages/app-plugin-security-cognito-theme/src/admin/views/UserAccountForm.tsx
@@ -3,46 +3,46 @@ import { Cell, Grid } from "@webiny/ui/Grid";
 import { validation } from "@webiny/validation";
 import { Input } from "@webiny/ui/Input";
 import { i18n } from "@webiny/app/i18n";
-import Section from "@webiny/ui/Section";
-import styled from "@emotion/styled";
+import { Accordion, AccordionItem } from "@webiny/ui/Accordion";
+import { ReactComponent as SettingsIcon } from "./../assets/icons/settings-24px.svg";
+import { ReactComponent as SecurityIcon } from "./../assets/icons/security-24px.svg";
 
 const t = i18n.ns("app-plugin-security-cognito-theme/users/account");
 
-const RenderSection = styled("div")({
-    backgroundColor: "var(--mdc-theme-background)",
-    height: "100%",
-    padding: 25
-});
-
 const UserAccountForm = ({ Bind, data, fields }) => {
     return (
-        <RenderSection>
-            <Section title="Avatar">
-                <Grid>
-                    <Cell span={12}>{fields.avatar}</Cell>
-                </Grid>
-            </Section>
-            <Section title="Bio">
-                <Grid>
-                    <Cell span={12}>{fields.email}</Cell>
-                    <Cell span={12}>
-                        <Bind name="password" validators={validation.create("password")}>
-                            <Input
-                                autoComplete="off"
-                                description={data.id && t`Type a new password to reset it.`}
-                                type="password"
-                                label={t`Password`}
-                            />
-                        </Bind>
-                    </Cell>
-                    <Cell span={12}>{fields.firstName}</Cell>
-                    <Cell span={12}>{fields.lastName}</Cell>
-                </Grid>
-            </Section>
-            <Section title="Personal Access Tokens">
-                <Cell span={12}>{fields.personalAccessTokens}</Cell>
-            </Section>
-        </RenderSection>
+        <>
+            <Accordion elevation={0}>
+                <AccordionItem
+                    description="Show Account bio information"
+                    title="Bio"
+                    icon={<SettingsIcon />}
+                >
+                    <Grid>
+                        <Cell span={12}>{fields.email}</Cell>
+                        <Cell span={12}>
+                            <Bind name="password" validators={validation.create("password")}>
+                                <Input
+                                    autoComplete="off"
+                                    description={data.id && t`Type a new password to reset it.`}
+                                    type="password"
+                                    label={t`Password`}
+                                />
+                            </Bind>
+                        </Cell>
+                        <Cell span={12}>{fields.firstName}</Cell>
+                        <Cell span={12}>{fields.lastName}</Cell>
+                    </Grid>
+                </AccordionItem>
+                <AccordionItem
+                    description="Show Personal Access Tokens"
+                    title="Personal Access Tokens"
+                    icon={<SecurityIcon />}
+                >
+                    {fields.personalAccessTokens}
+                </AccordionItem>
+            </Accordion>
+        </>
     );
 };
 
