85a48710a61006d5073157aa25920fa8aa9b0b5f
Fsalker <andreiii500@yahoo.com>
2020-03-26 06:21:15 +0200

    feat: Add PATs & Move Avatar above form

--- packages/app-security/src/admin/views/Users/UsersForm.tsx
+++ packages/app-security/src/admin/views/Users/UsersForm.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { getPlugin } from "@webiny/plugins";
 import { i18n } from "@webiny/app/i18n";
 import { Form } from "@webiny/form";
@@ -17,11 +17,36 @@ import {
     SimpleFormFooter,
     SimpleFormContent
 } from "@webiny/app-admin/components/SimpleForm";
+import gql from "graphql-tag";
+import { useApolloClient } from "react-apollo";
+import AccuntTokens from "@webiny/app-security/admin/views/AccountTokens";
 
 const t = i18n.ns("app-security/admin/users/form");
 
+const useForceRender = () => {
+    const [renderIndex, setRenderCount] = useState(0);
+    return () => {
+        console.log("Current render index: " + renderIndex);
+        setRenderCount(Math.random());
+    };
+};
+
+const GET_NEW_PAT = gql`
+    mutation {
+        security {
+            createPAT
+        }
+    }
+`;
+
 const UsersForm = () => {
+    const client = useApolloClient();
     const { form: crudForm } = useCrud();
+    const forceRender = useForceRender(); // [CR note] [Andrei] this form is refreshed 3 times. useCrud() provides "data" only on the 3rd render
+    // we can't use useEffect() because it "violates rules of hooks" or something. so
+    // I'm forcing a render everytime when it's needed... It looks good to me =D
+    //
+    // If you have better ideas, let me know
 
     const auth = getPlugin("security-view-user-form") as SecurityViewUserFormPlugin;
 
@@ -29,63 +54,100 @@ const UsersForm = () => {
         throw Error(`You must register a "security-view-user-form" plugin to render User form!`);
     }
 
+    const deleteToken = removedValue => {
+        const tokenIndex = crudForm.data.personalAccessTokens.findIndex(
+            PAT => PAT.token === removedValue
+        );
+        if (tokenIndex === -1) return;
+
+        crudForm.data.personalAccessTokens.splice(tokenIndex, 1);
+        forceRender();
+    };
+
+    const generateToken = async () => {
+        crudForm.loading = true;
+        forceRender();
+        const queryResponse = await client.mutate({
+            mutation: GET_NEW_PAT
+        });
+
+        const personalAccessToken = { token: queryResponse.data.security.createPAT };
+        crudForm.loading = false;
+        if (!crudForm.data.personalAccessTokens)
+            crudForm.data.personalAccessTokens = [personalAccessToken];
+        else crudForm.data.personalAccessTokens.push(personalAccessToken);
+
+        forceRender();
+    };
+
     return (
         <Form {...crudForm}>
             {({ data, form, Bind }) => (
-                <SimpleForm>
-                    {crudForm.loading && <CircularProgress />}
-                    <SimpleFormHeader title={data.fullName || t`N/A`} />
-                    <SimpleFormContent>
-                        {React.createElement(auth.view, {
-                            Bind,
-                            data,
-                            fields: {
-                                firstName: (
-                                    <Bind
-                                        name="firstName"
-                                        validators={validation.create("required")}
-                                    >
-                                        <Input label={t`First Name`} />
-                                    </Bind>
-                                ),
-                                lastName: (
-                                    <Bind
-                                        name="lastName"
-                                        validators={validation.create("required")}
-                                    >
-                                        <Input label={t`Last name`} />
-                                    </Bind>
-                                ),
-                                email: (
-                                    <Bind
-                                        name="email"
-                                        validators={validation.create("required,email")}
-                                    >
-                                        <Input label={t`E-mail`} />
-                                    </Bind>
-                                ),
-                                avatar: (
-                                    <Bind name="avatar">
-                                        <AvatarImage label={t`Avatar`} />
-                                    </Bind>
-                                ),
-                                groups: (
-                                    <Bind name="groups">
-                                        <GroupsAutocomplete label={t`Groups`} />
-                                    </Bind>
-                                ),
-                                roles: (
-                                    <Bind name="roles">
-                                        <RolesAutocomplete label={t`Roles`} />
-                                    </Bind>
-                                )
-                            }
-                        })}
-                    </SimpleFormContent>
-                    <SimpleFormFooter>
-                        <ButtonPrimary onClick={form.submit}>{t`Save user`}</ButtonPrimary>
-                    </SimpleFormFooter>
-                </SimpleForm>
+                <>
+                    <div style={{ marginBottom: "32px", marginTop: "24px" }}>
+                        <Bind name="avatar">
+                            <AvatarImage />
+                        </Bind>
+                    </div>
+                    <SimpleForm>
+                        {crudForm.loading && <CircularProgress />}
+                        <SimpleFormHeader title={data.fullName || t`N/A`} />
+                        <SimpleFormContent>
+                            {React.createElement(auth.view, {
+                                Bind,
+                                data,
+                                fields: {
+                                    firstName: (
+                                        <Bind
+                                            name="firstName"
+                                            validators={validation.create("required")}
+                                        >
+                                            <Input label={t`First Name`} />
+                                        </Bind>
+                                    ),
+                                    lastName: (
+                                        <Bind
+                                            name="lastName"
+                                            validators={validation.create("required")}
+                                        >
+                                            <Input label={t`Last name`} />
+                                        </Bind>
+                                    ),
+                                    email: (
+                                        <Bind
+                                            name="email"
+                                            validators={validation.create("required,email")}
+                                        >
+                                            <Input label={t`E-mail`} />
+                                        </Bind>
+                                    ),
+                                    groups: (
+                                        <Bind name="groups">
+                                            <GroupsAutocomplete label={t`Groups`} />
+                                        </Bind>
+                                    ),
+                                    roles: (
+                                        <Bind name="roles">
+                                            <RolesAutocomplete label={t`Roles`} />
+                                        </Bind>
+                                    ),
+                                    personalAccessTokens: (
+                                        <AccuntTokens
+                                            deleteToken={deleteToken}
+                                            generateToken={generateToken}
+                                            personalAccessTokens={
+                                                crudForm && crudForm.data.personalAccessTokens
+                                            }
+                                        />
+                                    )
+                                }
+                            })}
+                        </SimpleFormContent>
+                        <SimpleFormFooter>
+                            <ButtonPrimary onClick={form.submit}>{t`Save user`}</ButtonPrimary>
+                        </SimpleFormFooter>
+                    </SimpleForm>
+                </>
             )}
         </Form>
     );
