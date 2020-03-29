b9cae36503b16a71e3f72f57d447e503c488f2e4
Fsalker <andreiii500@yahoo.com>
2020-03-29 23:03:07 +0300

    feat: add 'name' to PAT model

--- packages/api-security/src/plugins/models/securityPersonalAccessToken.model.ts
+++ packages/api-security/src/plugins/models/securityPersonalAccessToken.model.ts
@@ -1,6 +1,7 @@
 import { withFields, string, date } from "@webiny/commodo";
 
 export default withFields({
+    name: string(),
     token: string(),
     createdOn: date({ value: new Date() })
 })();
