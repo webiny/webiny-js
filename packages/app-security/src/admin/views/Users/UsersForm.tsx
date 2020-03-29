8deba5f410eacd9f9bccfc545345326852e0bd8c
Fsalker <andreiii500@yahoo.com>
2020-03-29 23:04:52 +0300

    refactor: pass setValue to update form
    Also removed generateToken() & deleteToken() from the UsersForm component

--- packages/app-security/src/admin/views/Users/UsersForm.tsx
+++ packages/app-security/src/admin/views/Users/UsersForm.tsx
@@ -17,36 +17,13 @@ import {
     SimpleFormFooter,
     SimpleFormContent
 } from "@webiny/app-admin/components/SimpleForm";
-import gql from "graphql-tag";
-import { useApolloClient } from "react-apollo";
 import AccuntTokens from "@webiny/app-security/admin/views/AccountTokens";
 
 const t = i18n.ns("app-security/admin/users/form");
 
-const useForceRender = () => {
-    const [renderIndex, setRenderCount] = useState(0);
-    return () => {
-        console.log("Current render index: " + renderIndex);
-        setRenderCount(Math.random());
-    };
-};
-
-const GET_NEW_PAT = gql`
-    mutation {
-        security {
-            createPAT
-        }
-    }
-`;
-
 const UsersForm = () => {
-    const client = useApolloClient();
+    const [formIsLoading, setFormIsLoading] = useState(false);
     const { form: crudForm } = useCrud();
-    const forceRender = useForceRender(); // [CR note] [Andrei] this form is refreshed 3 times. useCrud() provides "data" only on the 3rd render
-    // we can't use useEffect() because it "violates rules of hooks" or something. so
-    // I'm forcing a render everytime when it's needed... It looks good to me =D
-    //
-    // If you have better ideas, let me know
 
     const auth = getPlugin("security-view-user-form") as SecurityViewUserFormPlugin;
 
@@ -54,35 +31,9 @@ const UsersForm = () => {
         throw Error(`You must register a "security-view-user-form" plugin to render User form!`);
     }
 
-    const deleteToken = removedValue => {
-        const tokenIndex = crudForm.data.personalAccessTokens.findIndex(
-            PAT => PAT.token === removedValue
-        );
-        if (tokenIndex === -1) return;
-
-        crudForm.data.personalAccessTokens.splice(tokenIndex, 1);
-        forceRender();
-    };
-
-    const generateToken = async () => {
-        crudForm.loading = true;
-        forceRender();
-        const queryResponse = await client.mutate({
-            mutation: GET_NEW_PAT
-        });
-
-        const personalAccessToken = { token: queryResponse.data.security.createPAT };
-        crudForm.loading = false;
-        if (!crudForm.data.personalAccessTokens)
-            crudForm.data.personalAccessTokens = [personalAccessToken];
-        else crudForm.data.personalAccessTokens.push(personalAccessToken);
-
-        forceRender();
-    };
-
     return (
         <Form {...crudForm}>
-            {({ data, form, Bind }) => (
+            {({ data, form, Bind, setValue }) => (
                 <>
                     <div style={{ marginBottom: "32px", marginTop: "24px" }}>
                         <Bind name="avatar">
@@ -90,8 +41,8 @@ const UsersForm = () => {
                         </Bind>
                     </div>
                     <SimpleForm>
-                        {crudForm.loading && <CircularProgress />}
-                        <SimpleFormHeader title={data.fullName || t`N/A`} />
+                        {(formIsLoading || crudForm.loading) && <CircularProgress />}
+                        <SimpleFormHeader title={data.fullName || t`New User`} />
                         <SimpleFormContent>
                             {React.createElement(auth.view, {
                                 Bind,
@@ -132,13 +83,14 @@ const UsersForm = () => {
                                         </Bind>
                                     ),
                                     personalAccessTokens: (
-                                        <AccuntTokens
-                                            deleteToken={deleteToken}
-                                            generateToken={generateToken}
-                                            personalAccessTokens={
-                                                crudForm && crudForm.data.personalAccessTokens
-                                            }
-                                        />
+                                        <Bind name="personalAccessTokens">
+                                            <AccuntTokens
+                                                data={data}
+                                                setValue={setValue}
+                                                formIsLoading={formIsLoading}
+                                                setFormIsLoading={setFormIsLoading}
+                                            />
+                                        </Bind>
                                     )
                                 }
                             })}
