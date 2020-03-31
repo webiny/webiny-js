44b1e732988dcaae13c21acefc0b70c0dc0ed3be
Adrian Smijulj <adrian1358@gmail.com>
2020-03-31 17:44:46 +0200

    fix: adapt settings menus to the new structure

--- packages/app-page-builder/src/admin/plugins/settings/index.tsx
+++ packages/app-page-builder/src/admin/plugins/settings/index.tsx
@@ -3,55 +3,74 @@ import { Route } from "@webiny/react-router";
 import { AdminLayout } from "@webiny/app-admin/components/AdminLayout";
 import PageBuilderSettings from "./components/PageBuilderSettings";
 import GeneralSettings from "./components/generalSettings/GeneralSettings";
-import { SettingsPlugin } from "@webiny/app-admin/types";
-import { hasRoles } from "@webiny/app-security";
 import { SecureRoute } from "@webiny/app-security/components";
+import { i18n } from "@webiny/app/i18n";
+import { getPlugins } from "@webiny/plugins";
+import Helmet from "react-helmet";
+import { PbMenuSettingsItem } from "@webiny/app-page-builder/types";
 
-const plugins: SettingsPlugin[] = [
+const t = i18n.ns("app-page-builder/admin/menus");
+
+const plugins = [
     {
-        type: "settings",
-        name: "settings-page-builder",
-        settings: {
-            show: () => {
-                return hasRoles(["pb-settings", "pb-editor"]);
-            },
-            type: "app",
-            name: "Page Builder",
-            route: (
-                <Route
-                    path="/page-builder"
-                    render={() => (
-                        <AdminLayout>
-                            <SecureRoute roles={["pb-settings", "pb-editor"]}>
-                                <PageBuilderSettings />
-                            </SecureRoute>
-                        </AdminLayout>
-                    )}
-                />
-            )
+        type: "route",
+        name: "route-settings-website",
+        route: (
+            <Route
+                path="/settings/page-builder/website"
+                render={() => (
+                    <AdminLayout>
+                        <Helmet title={t`Page Builder - Website Settings`} />
+                        <SecureRoute roles={["pb-settings", "pb-editor"]}>
+                            <PageBuilderSettings />
+                        </SecureRoute>
+                    </AdminLayout>
+                )}
+            />
+        )
+    },
+    {
+        type: "route",
+        name: "route-settings-general",
+        route: (
+            <Route
+                path="/settings/page-builder/general"
+                render={() => (
+                    <AdminLayout>
+                        <Helmet title={t`Page Builder - General Settings`} />
+                        <SecureRoute roles={["pb-settings"]}>
+                            <GeneralSettings />
+                        </SecureRoute>
+                    </AdminLayout>
+                )}
+            />
+        )
+    },
+    {
+        type: "menu-settings",
+        name: "menu-settings-page-builder",
+        render({ Section, Item }) {
+            return (
+                <Section label={t`Page Builder`}>
+                    {getPlugins("menu-settings-page-builder").map(plugin => {
+                        return plugin.render<PbMenuSettingsItem>({ Item });
+                    })}
+                </Section>
+            );
         }
     },
     {
-        type: "settings",
-        name: "settings-general-settings",
-        settings: {
-            show: () => {
-                return hasRoles(["pb-settings"]);
-            },
-            type: "other",
-            name: "General settings",
-            route: (
-                <Route
-                    path="/general"
-                    render={() => (
-                        <AdminLayout>
-                            <SecureRoute roles={["pb-settings"]}>
-                                <GeneralSettings />
-                            </SecureRoute>
-                        </AdminLayout>
-                    )}
-                />
-            )
+        type: "menu-settings-page-builder",
+        name: "menu-settings-website",
+        render({ Item }) {
+            return <Item label={t`Website`} path={"/settings/page-builder/website"} />;
+        }
+    },
+    {
+        type: "menu-settings-page-builder",
+        name: "menu-settings-general",
+        render({ Item }) {
+            return <Item label={t`General`} path={"/settings/page-builder/general"} />;
         }
     }
 ];
