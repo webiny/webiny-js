152b44857fd39cc687fdf85968a96f9fac75c48c
Adrian Smijulj <adrian1358@gmail.com>
2020-03-31 09:49:39 +0200

    feat: create NameSlug component

---
+++ packages/app-headless-cms/src/admin/components/NameSlug.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Cell } from "@webiny/ui/Grid";
+import { Input } from "@webiny/ui/Input";
+import { i18n } from "@webiny/app/i18n";
+import { validation } from "@webiny/validation";
+import slugify from "slugify";
+const t = i18n.ns("app-page-builder/admin/components/name-slug");
+
+const toSlug = text =>
+    slugify(text, {
+        replacement: "-",
+        lower: true,
+        remove: /[*#\?<>_\{\}\[\]+~.()'"!:;@]/g
+    });
+
+const slugValidator = async (value, validateSlugUniqueness, formData) => {
+    if (formData.id) {
+        return;
+    }
+
+    validation.validateSync(value, "required,maxLength:100");
+    await validateSlugUniqueness();
+};
+
+let currentFormData;
+
+function NameSlug({ formData, Bind, setValue, name = {}, slug = {}, validateSlugUniqueness }) {
+
+    // A weird issue is happening here - afterChange always contains the initial "{}" as the "formData" value.
+    // This was a temporary fix 😐
+    currentFormData = formData;
+
+    return (
+        <>
+            <Cell span={6}>
+                <Bind
+                    {...name}
+                    name="name"
+                    validators={validation.create("required,maxLength:100")}
+                    afterChange={value => {
+                        if (!currentFormData.id) {
+                            setValue("slug", toSlug(value));
+                        }
+                    }}
+                >
+                    <Input label={t`Name`} />
+                </Bind>
+            </Cell>
+            <Cell span={6}>
+                <Bind
+                    name="slug"
+                    validators={value => slugValidator(value, validateSlugUniqueness, formData)}
+                >
+                    {bindProps => (
+                        <Input {...bindProps} {...slug} disabled={formData.id} label={t`Slug`} />
+                    )}
+                </Bind>
+            </Cell>
+        </>
+    );
+}
+
+export default NameSlug;
