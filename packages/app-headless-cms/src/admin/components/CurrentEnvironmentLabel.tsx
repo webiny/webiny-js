9b966c1decc59676eb80da6ea91f4c9a0b03c9ae
Adrian Smijulj <adrian1358@gmail.com>
2020-04-02 11:17:23 +0200

    feat: create current environment label component

---
+++ packages/app-headless-cms/src/admin/components/CurrentEnvironmentLabel.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Chips, Chip, ChipIcon } from "@webiny/ui/Chips";
+import { ReactComponent as EnvironmentIcon } from "@webiny/app-headless-cms/admin/icons/call_split-24px.svg";
+import { useCms } from "@webiny/app-headless-cms/admin/hooks";
+import EnvironmentSelectorDialog from "./EnvironmentSelectorDialog";
+
+const CurrentEnvironmentLabel = props => {
+    const {
+        environments: { currentEnvironment }
+    } = useCms();
+
+    const [dialogOpened, setDialogOpened] = useState(false);
+
+    if (!currentEnvironment) {
+        return null;
+    }
+
+    return (
+        <>
+            <Chips {...props} onClick={() => setDialogOpened(true)}>
+                <Chip>
+                    {currentEnvironment.name}
+                    <ChipIcon trailing icon={<EnvironmentIcon />} />
+                </Chip>
+            </Chips>
+
+            <EnvironmentSelectorDialog
+                open={dialogOpened}
+                onClose={() => {
+                    setDialogOpened(false);
+                }}
+            />
+        </>
+    );
+};
+
+export default CurrentEnvironmentLabel;
