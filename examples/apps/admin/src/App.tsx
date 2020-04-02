73c89079ceed7c6697ee216b6edaa9560a261b1a
Adrian Smijulj <adrian1358@gmail.com>
2020-04-02 11:16:09 +0200

    fix: remove "apolloClient" params

--- examples/apps/admin/src/App.tsx
+++ examples/apps/admin/src/App.tsx
@@ -6,8 +6,5 @@ export const App = adminTemplate({
         region: process.env.REACT_APP_USER_POOL_REGION,
         userPoolId: process.env.REACT_APP_USER_POOL_ID,
         userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
-    },
-    apolloClient: {
-        uri: process.env.REACT_APP_GRAPHQL_API_URL
     }
 });
