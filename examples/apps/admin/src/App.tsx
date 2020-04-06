3eb49589d849228bd4119b4775595af6ad0c95cb
Pavel Denisjuk <pavel@webiny.com>
2020-04-06 13:26:59 +0200

    chore: use default export for App component

--- examples/apps/admin/src/App.tsx
+++ examples/apps/admin/src/App.tsx
@@ -1,13 +1,10 @@
 import adminTemplate from "@webiny/app-template-admin-full";
 import "./App.scss";
 
-export const App = adminTemplate({
+export default adminTemplate({
     cognito: {
         region: process.env.REACT_APP_USER_POOL_REGION,
         userPoolId: process.env.REACT_APP_USER_POOL_ID,
         userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
-    },
-    apolloClient: {
-        uri: process.env.REACT_APP_GRAPHQL_API_URL
     }
 });
