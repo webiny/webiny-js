4f5edfe53c29fb0de5aa37f0b57fb3e6371fe4ff
Adrian Smijulj <adrian1358@gmail.com>
2020-04-07 11:04:16 +0200

    fix: load environment and environment alias

--- packages/api-headless-cms/src/handler/plugins/models.ts
+++ packages/api-headless-cms/src/handler/plugins/models.ts
@@ -49,14 +49,15 @@ export default () => {
 
         // Before continuing with the rest of the models, we must load the environment and assign it to the context.
         let environment = null;
+        let environmentAlias = null;
         if (context.cms.environment && typeof context.cms.environment === "string") {
             if (context.commodo.isId(context.cms.environment)) {
                 environment = await context.models.CmsEnvironment.findById(context.cms.environment);
             } else {
-                environment = await context.models.CmsEnvironmentAlias.findOne({
+                environmentAlias = await context.models.CmsEnvironmentAlias.findOne({
                     query: { slug: context.cms.environment }
                 });
-                environment = await context.models.CmsEnvironmentAlias.environment;
+                environment = await environmentAlias.environment;
             }
         }
 
@@ -68,6 +69,7 @@ export default () => {
 
         context.cms.environment = environment.slug;
         context.cms.getEnvironment = () => environment;
+        context.cms.getEnvironmentAlias = () => environmentAlias;
 
         const modifyQueryArgs = (args = {}) => {
             const returnArgs = cloneDeep(args);
