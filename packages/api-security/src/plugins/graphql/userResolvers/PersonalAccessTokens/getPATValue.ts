a5b1ff4ac718a25f4269f156c6fbb8fd45fff24b
Fsalker <andreiii500@yahoo.com>
2020-04-08 15:58:11 +0300

    feat: return {data: string} & refactor

--- packages/api-security/src/plugins/graphql/userResolvers/PersonalAccessTokens/getPATValue.ts
+++ packages/api-security/src/plugins/graphql/userResolvers/PersonalAccessTokens/getPATValue.ts
@@ -1,36 +1,32 @@
-import { NotFoundResponse, ErrorResponse } from "@webiny/commodo-graphql";
-
+import { NotFoundResponse } from "@webiny/api";
+import { ErrorResponse } from "@webiny/commodo-graphql";
 export default async (root, args, context) => {
-    const PersonalAccessToken = context.models.SecurityPersonalAccessToken;
-    const User = context.models.SecurityUser;
+    if (!context.user) {
+        return new NotFoundResponse("Current user not found!");
+    }
 
+    const { SecurityPersonalAccessToken } = context.models;
     const currentUserId = context.user.id;
-    const currentUser = await User.findById(currentUserId);
-
-    const canAssignUser =
-        context.user.access.fullAccess ||
-        context.user.access.scopes.find((scope) => scope === "security:user:crud");
 
     try {
-        const PAT = await PersonalAccessToken.findById(args.id);
-        if (!PAT) return new NotFoundResponse("PAT not found!");
-        const PATUser = await PAT.user;
-
-        if (PATUser.id === currentUserId) {
-            if (!currentUser) return new NotFoundResponse("Current user not found!");
-        } else {
-            if (!canAssignUser)
+        const pat = await SecurityPersonalAccessToken.findById(args.id);
+        if (!pat) {
+            return new NotFoundResponse("Personal Access Token not found!");
+        }
+        const patUser = await pat.user;
+        if (patUser.id !== currentUserId) {
+            const { fullAccess, scopes } = context.user.access;
+            const canGetTokenValue =
+                fullAccess || scopes.find((scope) => scope === "security:user:crud");
+            if (!canGetTokenValue) {
                 return new ErrorResponse({
                     message:
-                        "Current user is not admin! You must be an admin in order to update other users.",
+                        "Cannot get user's personal access token's value - insufficient permissions.",
                 });
+            }
         }
-
-        console.log("");
-        console.log("");
-        console.log(PAT);
-        const token = PAT.token;
-        return token.slice(0, Math.ceil(token.length / 2));
+        const token = pat.token;
+        return { data: token.slice(0, Math.ceil(token.length / 2)) };
     } catch (e) {
         return new ErrorResponse({
             code: e.code,
