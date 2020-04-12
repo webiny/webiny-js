6b5a09b8118fd540eb98047d2aaf0b2c00e3712c
Jeremytijal <40340340+Jeremytijal@users.noreply.github.com>
2020-04-12 06:55:23 +0200

    fix: change "pb-page-element" to "pb-editor-page-element" (#788)
    close #787

--- packages/app-page-builder/src/admin/utils/createElementPlugin.tsx
+++ packages/app-page-builder/src/admin/utils/createElementPlugin.tsx
@@ -5,7 +5,7 @@ import { PbElement, PbEditorPageElementPlugin } from "@webiny/app-page-builder/t
 import Title from "./components/Title";
 
 export default (el: PbElement) => {
-    const plugins = getPlugins("pb-page-element") as PbEditorPageElementPlugin[];
+    const plugins = getPlugins("pb-editor-page-element") as PbEditorPageElementPlugin[];
     const rootPlugin: PbEditorPageElementPlugin = plugins.find(pl => pl.elementType === el.content.type);
 
     if (!rootPlugin) {
