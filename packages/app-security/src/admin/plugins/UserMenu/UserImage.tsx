ec71d1d4bb85f0f31669cf4f7e39f2bba5e6ea9d
Fsalker <andreiii500@yahoo.com>
2020-04-16 15:37:49 +0300

    fix: link to /account when clicking on Account icon

--- packages/app-security/src/admin/plugins/UserMenu/UserImage.tsx
+++ packages/app-security/src/admin/plugins/UserMenu/UserImage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@webiny/react-router";
 import { Avatar } from "@webiny/ui/Avatar";
 import { Image } from "@webiny/app/components";
 import { useSecurity } from "@webiny/app-security/hooks/useSecurity";
@@ -13,13 +14,15 @@ const UserImage = () => {
     const { fullName, avatar } = security.user;
 
     return (
-        <Avatar
-            data-testid="logged-in-user-menu-avatar"
-            src={avatar && avatar.src}
-            alt={fullName}
-            fallbackText={fullName}
-            renderImage={props => <Image {...props} transform={{ width: 100 }} />}
-        />
+        <Link to={"/account"} style={{ textDecoration: "none" }}>
+            <Avatar
+                data-testid="logged-in-user-menu-avatar"
+                src={avatar && avatar.src}
+                alt={fullName}
+                fallbackText={fullName}
+                renderImage={props => <Image {...props} transform={{ width: 100 }} />}
+            />
+        </Link>
     );
 };
 
