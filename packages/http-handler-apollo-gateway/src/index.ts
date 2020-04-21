72e6424eccb6b2f2a24eab130d9b16e8733c7754
Adrian Smijulj <adrian1358@gmail.com>
2020-04-21 14:51:27 +0200

    fix: map over services only if they are actually provided

--- packages/http-handler-apollo-gateway/src/index.ts
+++ packages/http-handler-apollo-gateway/src/index.ts
@@ -1,13 +1,20 @@
 import { HttpHandlerApolloGatewayOptions } from "./types";
 import gatewayHandler from "./gatewayHandler";
 import errorHandler from "./errorHandler";
+import { Plugin } from "@webiny/plugins/types";
 
-export default (options: HttpHandlerApolloGatewayOptions) => [
-    gatewayHandler(options),
-    errorHandler(),
-    options.services.map(service => ({
-        type: "http-handler-apollo-gateway-service",
-        name: "http-handler-apollo-gateway-service-" + service.name,
-        service
-    }))
-];
+export default (options: HttpHandlerApolloGatewayOptions) => {
+    const plugins = [gatewayHandler(options), errorHandler()] as Plugin[];
+
+    if (Array.isArray(options.services)) {
+        plugins.push(
+            ...options.services.map(service => ({
+                type: "http-handler-apollo-gateway-service",
+                name: "http-handler-apollo-gateway-service-" + service.name,
+                service
+            }))
+        );
+    }
+
+    return plugins;
+};
