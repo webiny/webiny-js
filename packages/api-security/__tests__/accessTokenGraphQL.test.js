0edbdefafbd87421eae8c8ba6d05612abd9dcc02
Fsalker <andreiii500@yahoo.com>
2020-04-22 04:25:34 +0300

    feat: add graphql access token tests

---
+++ packages/api-security/__tests__/accessTokenGraphQL.test.js
@@ -0,0 +1,204 @@
+import accessTokenPlugins from "../src/plugins";
+import { createUtils } from "./utils";
+import { mockedUser } from "./mocks";
+import { graphql } from "graphql";
+import { JwtToken } from "../src/plugins/authentication";
+
+const gqlGetCurrentUser = `
+    {
+        security {
+            getCurrentUser {
+                data {
+                    id
+                    personalAccessTokens {
+                        id
+                        name
+                        token
+                    }
+                }
+                error {
+                    code
+                    message
+                    data
+                }
+            }
+        }
+    }
+`;
+
+const gqlCreatePAT = `
+    mutation createPAT($userId: ID, $tokenName: String!) {
+        security {
+            createPAT(name: $tokenName, userId: $userId) {
+                data {
+                    pat {
+                        id
+                        name
+                        token
+                    }
+                    token
+                }
+                error{
+                    message
+                    data
+                }
+            }
+        }
+    }
+`;
+
+const gqlUpdatePAT = `
+    mutation updatePAT($tokenId: ID!, $tokenName: String!) {
+        security {
+            updatePAT(id: $tokenId, data: { name: $tokenName }) {
+                data {
+                    id
+                    name
+                    token
+                }
+                error {
+                    message
+                    data
+                }
+            }
+        }
+    }
+`;
+
+const gqlDeletePAT = `
+    mutation deletePAT($tokenId: ID!) {
+        security {
+            deletePAT(id: $tokenId) {
+                data
+                error {
+                    message
+                    data
+                }
+            }
+        }
+    }
+`;
+
+describe("Personal Access Tokens [GraphQL] test suite", () => {
+    const tokenSecret = "ghsgashdgu";
+    const { useDatabase, useSchema } = createUtils([
+        accessTokenPlugins({ token: { secret: tokenSecret } })
+    ]);
+    const db = useDatabase();
+    let schema;
+    let context;
+    let tokenName1 = "Cool token #1";
+    let tokenName2 = "Cool token #1 - Renamed";
+    let createdToken;
+    let updatedToken;
+
+    beforeAll(async () => {
+        await db.getCollection("SecurityUser").insertOne(mockedUser);
+        delete mockedUser._id;
+
+        const jwt = new JwtToken({ secret: tokenSecret });
+        let payload = {
+            id: mockedUser.id,
+            type: "user",
+            access: {
+                scopes: [],
+                roles: [],
+                fullAccess: true
+            }
+        };
+        const token = await jwt.encode(payload, new Date().getTime() + 999999999);
+
+        ({ schema, context } = await useSchema());
+        context.event = {
+            headers: {
+                authorization: token
+            },
+            httpMethod: "POST"
+        };
+    });
+
+    afterAll(async () => {});
+
+    test("Should get PATs (no tokens)", async () => {
+        const response = await graphql(schema, gqlGetCurrentUser, {}, context);
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const user = response.data.security.getCurrentUser.data;
+        expect(user).toBeTruthy();
+        expect(user.id).toEqual(mockedUser.id);
+        expect(user.personalAccessTokens).toEqual([]);
+    });
+    test("Should create PAT", async () => {
+        const response = await graphql(schema, gqlCreatePAT, {}, context, {
+            userId: mockedUser.id,
+            tokenName: tokenName1
+        });
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const { error, data } = response.data.security.createPAT;
+        if (error) {
+            throw JSON.stringify(error, null, 2);
+        }
+        expect(data.token).toBeTruthy();
+        expect(data.token.length).toEqual(48);
+        expect(data.pat.id).toBeTruthy();
+        expect(data.pat.name).toEqual(tokenName1);
+        expect(data.pat.token).toBeTruthy();
+        expect(data.pat.token.length).toEqual(4);
+        createdToken = data.pat;
+    });
+    test("Should get PATs (1 token)", async () => {
+        const response = await graphql(schema, gqlGetCurrentUser, {}, context);
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const user = response.data.security.getCurrentUser.data;
+        expect(user.personalAccessTokens).toEqual([createdToken]);
+    });
+    test("Should update PAT", async () => {
+        const response = await graphql(schema, gqlUpdatePAT, {}, context, {
+            tokenId: createdToken.id,
+            tokenName: tokenName2
+        });
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const { error, data: token } = response.data.security.updatePAT;
+        if (error) {
+            throw JSON.stringify(error, null, 2);
+        }
+        expect(token.name).toEqual(tokenName2);
+        updatedToken = token;
+    });
+    test("Should get PATs (1 token, new value)", async () => {
+        const response = await graphql(schema, gqlGetCurrentUser, {}, context);
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const user = response.data.security.getCurrentUser.data;
+        expect(user.personalAccessTokens).toEqual([updatedToken]);
+    });
+    test("Should delete PAT", async () => {
+        const response = await graphql(schema, gqlDeletePAT, {}, context, {
+            tokenId: updatedToken.id
+        });
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const { error, data } = response.data.security.deletePAT;
+        if (error) {
+            throw JSON.stringify(error, null, 2);
+        }
+        expect(data).toEqual(true);
+    });
+    test("Should get PATs (no tokens)", async () => {
+        const response = await graphql(schema, gqlGetCurrentUser, {}, context);
+        if (response.errors) {
+            throw JSON.stringify(response.errors, null, 2);
+        }
+        const user = response.data.security.getCurrentUser.data;
+        expect(user.personalAccessTokens).toEqual([]);
+    });
+});
