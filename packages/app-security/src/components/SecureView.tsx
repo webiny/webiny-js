4167a66fb76e9e3afb06ecb832f257d7bca71642
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-24 14:11:41 +0530

    refactor: 📦  Remove `hasRoles` from packages/app-security
    remove checking of `hasRole` function

--- packages/app-security/src/components/SecureView.tsx
+++ packages/app-security/src/components/SecureView.tsx
@@ -1,24 +1,21 @@
 import * as React from "react";
-import { hasScopes, hasRoles } from "@webiny/app-security";
+import { hasScopes } from "@webiny/app-security";
 import { ResourcesType } from "../identity";
 
 export default ({
     children,
-    scopes,
-    roles
+    scopes
 }: {
     children: any;
     scopes?: ResourcesType;
-    roles?: ResourcesType;
 }): React.ReactElement => {
     const checks = {
-        scopes: scopes ? hasScopes(scopes) : true,
-        roles: roles ? hasRoles(roles) : true
+        scopes: scopes ? hasScopes(scopes) : true
     };
 
     if (typeof children === "function") {
         return children(checks);
     }
 
-    return checks.scopes && checks.roles ? children : null;
+    return checks.scopes ? children : null;
 };
