fef6400729bc67b96163c25e1eec4f73a8651158
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-24 17:49:08 +0530

    refactor: 📦  Remove `middlewarePlugin` factory

--- packages/api-security/src/plugins/security.ts
+++ packages/api-security/src/plugins/security.ts
@@ -2,27 +2,25 @@ import { GraphQLContextPlugin } from "@webiny/graphql/types";
 import authenticate from "./authentication/authenticate";
 import { SecurityPlugin } from "@webiny/api-security/types";
 
-const middlewarePlugin = (options): GraphQLContextPlugin => ({
-    type: "graphql-context",
-    name: "graphql-context-security",
-    preApply: async context => {
-        if (!context.event) {
-            return;
-        }
-
-        context.security = options;
-        context.token = null;
-        context.user = null;
-        context.getUser = () => context.user;
-
-        const securityPlugins = context.plugins.byType<SecurityPlugin>("graphql-security");
-        for (let i = 0; i < securityPlugins.length; i++) {
-            await securityPlugins[i].authenticate(context);
-        }
-    }
-});
-
 export default options => [
-    middlewarePlugin(options),
+    {
+        type: "graphql-context",
+        name: "graphql-context-security",
+        preApply: async context => {
+            if (!context.event) {
+                return;
+            }
+
+            context.security = options;
+            context.token = null;
+            context.user = null;
+            context.getUser = () => context.user;
+
+            const securityPlugins = context.plugins.byType<SecurityPlugin>("graphql-security");
+            for (let i = 0; i < securityPlugins.length; i++) {
+                await securityPlugins[i].authenticate(context);
+            }
+        }
+    } as GraphQLContextPlugin,
     { type: "graphql-security", name: "graphql-security", authenticate }
 ];
