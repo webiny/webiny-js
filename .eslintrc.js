77904fba40085569e0a48103c916195191c61aeb
Adrian Smijulj <adrian1358@gmail.com>
2020-04-28 14:44:40 +0200

    chore: add "curly" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,8 @@ module.exports = {
         "@typescript-eslint/ban-types": "off",
         "@typescript-eslint/no-use-before-define": 0,
         "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/no-explicit-any": 0
+        "@typescript-eslint/no-explicit-any": 0,
+        "curly": ["error"]
     },
     settings: {
         react: {
