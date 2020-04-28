7dea6dc432ae7b28c1decc1a7c6f8e43765e1533
Pavel Denisjuk <pavel@webiny.com>
2020-04-28 21:40:17 +0200

    chore: delete obsolete packages

--- packages/api-plugin-create-apollo-gateway/src/types.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin, PluginsContainer } from "@webiny/graphql/types";
-
-export type ApolloGatewayHeaders = Plugin & {
-    buildHeaders(params: { headers: { [key: string]: string }; plugins: PluginsContainer }): void;
-};
