c6e31279a786adb1e00c30af548e6e59a34dc099
Adrian Smijulj <adrian1358@gmail.com>
2020-04-30 14:29:06 +0200

    fix: create content form plugin

---
+++ packages/app-headless-cms/src/admin/plugins/contentDetails/contentForm/index.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { renderPlugins } from "@webiny/app/plugins";
+import { Tab } from "@webiny/ui/Tabs";
+import styled from "@emotion/styled";
+import { Elevation } from "@webiny/ui/Elevation";
+import ContentForm from "./ContentForm";
+import { CircularProgress } from "@webiny/ui/Progress";
+
+const RenderBlock = styled("div")({
+    position: "relative",
+    zIndex: 0,
+    backgroundColor: "var(--mdc-theme-background)",
+    height: "100%",
+    overflow: "scroll",
+    padding: 25
+});
+
+const plugins = [
+    {
+        name: "cms-content-details-revision-content-preview",
+        type: "cms-content-details-revision-content",
+        render(props) {
+            return (
+                <Tab label={"Content"} disabled={props.loading}>
+                    <RenderBlock>
+                        <Elevation z={2}>
+                            <div style={{ position: "relative" }}>
+                                {props.loading && <CircularProgress />}
+                                {renderPlugins(
+                                    "cms-content-details-revision-content-preview",
+                                    props
+                                )}
+                            </div>
+                        </Elevation>
+                    </RenderBlock>
+                </Tab>
+            );
+        }
+    },
+    {
+        name: "cms-content-details-content-form",
+        type: "cms-content-details-revision-content-preview",
+        render(props) {
+            return <ContentForm {...props} />;
+        }
+    }
+];
+
+export default plugins;
