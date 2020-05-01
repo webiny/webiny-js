ca777f070efaaeac34fd1c60cbd3ca71b29b87cd
Fsalker <andreiii500@yahoo.com>
2020-05-01 16:21:42 +0300

    chore: revert "fix"

--- packages/app-security/src/admin/plugins/UserMenu/UserImage.tsx
+++ packages/app-security/src/admin/plugins/UserMenu/UserImage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "@webiny/react-router";
 import { Avatar } from "@webiny/ui/Avatar";
 import { Image } from "@webiny/app/components";
 import { useSecurity } from "@webiny/app-security/hooks/useSecurity";
@@ -14,15 +13,13 @@ const UserImage = () => {
     const { fullName, avatar } = security.user;
 
     return (
-        <Link to={"/account"} style={{ textDecoration: "none" }}>
-            <Avatar
-                data-testid="logged-in-user-menu-avatar"
-                src={avatar && avatar.src}
-                alt={fullName}
-                fallbackText={fullName}
-                renderImage={props => <Image {...props} transform={{ width: 100 }} />}
-            />
-        </Link>
+        <Avatar
+            data-testid="logged-in-user-menu-avatar"
+            src={avatar && avatar.src}
+            alt={fullName}
+            fallbackText={fullName}
+            renderImage={props => <Image {...props} transform={{ width: 100 }} />}
+        />
     );
 };
 
