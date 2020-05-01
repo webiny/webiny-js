e060bf0d73623cf43256cda2be70aa50258f0735
Adrian Smijulj <adrian1358@gmail.com>
2020-05-01 09:23:47 +0200

    fix: create "resolveCreateFrom" resolver

---
+++ packages/api-headless-cms/src/handler/plugins/utils/resolvers/resolveCreateFrom.ts
@@ -0,0 +1,43 @@
+import { GraphQLFieldResolver } from "@webiny/graphql/types";
+import { Response, ErrorResponse } from "@webiny/commodo-graphql";
+import { entryNotFound } from "./entryNotFound";
+import { CmsGraphQLContext } from "@webiny/api-headless-cms/types";
+
+export const resolveCreateFrom = ({
+    model
+}): GraphQLFieldResolver<any, any, CmsGraphQLContext> => async (root, args, context) => {
+    const Model = context.models[model.modelId];
+    const baseRevision = await Model.findOne({ query: { id: args.revision } });
+
+    if (!baseRevision) {
+        return entryNotFound(JSON.stringify(args.where));
+    }
+
+    try {
+        const newRevision = new Model();
+
+        for (let i = 0; i < model.fields.length; i++) {
+            const field = model.fields[i];
+            if (baseRevision[field.fieldId]) {
+                newRevision[field.fieldId] = baseRevision[field.fieldId];
+            }
+        }
+
+        newRevision.meta.parent = baseRevision.meta.parent;
+        newRevision.meta.environment = baseRevision.meta.environment;
+
+        if (args.data) {
+            newRevision.populate(args.data);
+        }
+
+        await newRevision.save();
+
+        return new Response(newRevision);
+    } catch (e) {
+        return new ErrorResponse({
+            code: e.code,
+            message: e.message,
+            data: e.data || null
+        });
+    }
+};
