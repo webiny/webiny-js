d9b8ef0a01944fe4f354e51135c9cbab7683c6ee
Fsalker <andreiii500@yahoo.com>
2020-05-01 04:27:34 +0300

    feat: add our default scaffold plugins + inline demo plugin

--- sample-project/webiny.root.js
+++ sample-project/webiny.root.js
@@ -1,3 +1,20 @@
 module.exports = {
-    projectName: "webiny-js"
+    projectName: "webiny-js",
+    cli: {
+        plugins: [
+            "@webiny/cli-scaffold-graphql-service",
+            "@webiny/cli-scaffold-custom-lambda",
+            {
+                name: "cool-plugin-dinner",
+                type: "scaffold-template",
+                scaffold: {
+                    name: "Dinner making scaffold plugin",
+                    questions: () => [],
+                    generate: async () => {
+                        console.log("Dinner has been prepared!\n");
+                    }
+                }
+            }
+        ]
+    }
 };
