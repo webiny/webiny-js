f23b40273266a1ae15464877e5f0b508d7335213
Adrian Smijulj <adrian1358@gmail.com>
2020-05-02 10:29:07 +0200

    fix: bring back the "graphql-context" plugin

--- packages/api-security/src/plugins/security.ts
+++ packages/api-security/src/plugins/security.ts
@@ -1,8 +1,30 @@
 import authenticateJwt from "./authentication/authenticateJwt";
 import authenticatePat from "./authentication/authenticatePat";
 import { SecurityPlugin } from "@webiny/api-security/types";
+import { GraphQLContextPlugin } from "@webiny/graphql/types";
+
+const contextPlugin = (options): GraphQLContextPlugin => ({
+    type: "graphql-context",
+    name: "graphql-context-security",
+    preApply: async context => {
+        if (!context.event) {
+            return;
+        }
+
+        context.security = options;
+        context.token = null;
+        context.user = null;
+        context.getUser = () => context.user;
+
+        const securityPlugins = context.plugins.byType<SecurityPlugin>("graphql-security");
+        for (let i = 0; i < securityPlugins.length; i++) {
+            await securityPlugins[i].authenticate(context);
+        }
+    }
+});
 
 export default options => [
+    contextPlugin(options),
     {
         type: "graphql-security",
         name: "graphql-security-jwt",
