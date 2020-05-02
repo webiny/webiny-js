2195204b6ec41f63731b17577d989f7bae14c82c
Adrian Smijulj <adrian1358@gmail.com>
2020-05-02 10:15:24 +0200

    fix: extract authenticatePat function into a separate file

---
+++ packages/api-security/src/plugins/authentication/authenticatePat.ts
@@ -0,0 +1,26 @@
+import LambdaClient from "aws-sdk/clients/lambda";
+import { GraphQLContext } from "@webiny/graphql/types";
+
+export default options => async (context: GraphQLContext) => {
+    if (context.user) {
+        return;
+    }
+
+    const { event } = context;
+    const { headers = {} } = event;
+    const authorization = headers["Authorization"] || headers["authorization"] || "";
+
+    if (!authorization) {
+        return;
+    }
+
+    const token = authorization;
+    const Lambda = new LambdaClient({ region: process.env.AWS_REGION });
+    const user = JSON.parse((await Lambda.invoke({
+        FunctionName: options.validateAccessTokenFunction,
+        Payload: JSON.stringify({ PAT: token })
+    }).promise()).Payload as string);
+
+    context.token = token;
+    context.user = user;
+};
--- packages/api-security/src/plugins/security.ts
+++ packages/api-security/src/plugins/security.ts
@@ -1,31 +1,6 @@
-import { GraphQLContext } from "@webiny/graphql/types";
 import authenticateJwt from "./authentication/authenticateJwt";
+import authenticatePat from "./authentication/authenticatePat";
 import { SecurityPlugin } from "@webiny/api-security/types";
-import LambdaClient from "aws-sdk/clients/lambda";
-
-const authenticatePat = options => async (context: GraphQLContext) => {
-    if (context.user) {
-        return;
-    }
-
-    const { event } = context;
-    const { headers = {} } = event;
-    const authorization = headers["Authorization"] || headers["authorization"] || "";
-
-    if (!authorization) {
-        return;
-    }
-
-    const token = authorization;
-    const Lambda = new LambdaClient({ region: process.env.AWS_REGION });
-    const user = JSON.parse((await Lambda.invoke({
-        FunctionName: options.validateAccessTokenFunction,
-        Payload: JSON.stringify({ PAT: token })
-    }).promise()).Payload as string);
-
-    context.token = token;
-    context.user = user;
-};
 
 export default options => [
     {
