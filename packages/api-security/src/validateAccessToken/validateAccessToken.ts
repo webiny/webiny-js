4f2ffdbe3e72c4be5357274ec2b967a344c485f3
Adrian Smijulj <adrian1358@gmail.com>
2020-05-02 10:18:41 +0200

    fix: do not swallow the thrown error

--- packages/api-security/src/validateAccessToken/validateAccessToken.ts
+++ packages/api-security/src/validateAccessToken/validateAccessToken.ts
@@ -1,25 +1,21 @@
 export default async (context, pat) => {
-    try {
-        if (!pat) {
-            return null;
-        }
-
-        pat = pat.toLowerCase();
-        const token = await context.models.SecurityPersonalAccessToken.findOne({
-            query: { token: pat }
-        });
-        const user = await token.user;
-        if (!user) {
-            return null;
-        }
-
-        return {
-            id: user.id,
-            type: "user",
-            access: await user.access
-        };
-
-    } catch (e) {
-        console.log(e);
+    if (!pat || typeof pat !== "string") {
+        return null;
     }
+
+    pat = pat.toLowerCase();
+    const token = await context.models.SecurityPersonalAccessToken.findOne({
+        query: { token: pat }
+    });
+
+    const user = await token.user;
+    if (!user) {
+        return null;
+    }
+
+    return {
+        id: user.id,
+        type: "user",
+        access: await user.access
+    };
 };
