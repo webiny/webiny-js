b58459a72cad38186fd71df4f82c428fee129470
Adrian Smijulj <adrian1358@gmail.com>
2020-05-03 19:51:29 +0200

    chore: add "webiny-i18n" eslint rule (experimental)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: "module"
     },
-    plugins: ["@typescript-eslint", "jest", "import", "react"],
+    plugins: ["@typescript-eslint", "jest", "import", "react", "webiny-i18n"],
     env: {
         jest: true,
         commonjs: true,
@@ -25,7 +25,9 @@ module.exports = {
         "@typescript-eslint/no-use-before-define": 0,
         "@typescript-eslint/no-var-requires": 0,
         "@typescript-eslint/no-explicit-any": 0,
-        "curly": ["error"]
+        "curly": ["error"],
+        "webiny-i18n/namespaces": "error"
+
     },
     settings: {
         react: {
---
+++ eslint/webiny-i18n/index.js
@@ -0,0 +1,43 @@
+const get = require("lodash/get");
+
+const PREFIX = ".*packages/app-headless-cms/src";
+
+const rules = {
+    [`${PREFIX}/admin/plugins/fields.*`]: "app-headless-cms/admin/plugins/fields"
+};
+
+module.exports = {
+    rules: {
+        namespaces: {
+            create: function (context) {
+                return {
+                    VariableDeclarator(node) {
+                        const oName = get(node, "init.callee.object.name");
+                        const pName = get(node, "init.callee.property.name");
+                        if (oName !== "i18n" || pName !== "ns") {
+                            return;
+                        }
+
+                        const filename = context.getFilename();
+                        const [namespace] = node.init.arguments;
+                        if (!namespace || !namespace.value) {
+                            const msg = `I18N namespace not specified.`;
+                            context.report(node, msg);
+                            return;
+                        }
+
+                        for (let rulesKey in rules) {
+                            const regex = new RegExp(rulesKey);
+                            if (filename.match(regex)) {
+                                if (!namespace.value.match(rules[rulesKey])) {
+                                    const msg = `Incorrect I18N namespace specified. Expected "${rulesKey}", received "${namespace.value}".`;
+                                    context.report(namespace, msg);
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    }
+};
