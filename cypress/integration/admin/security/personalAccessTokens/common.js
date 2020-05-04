05662753f96154b6c119d98519173616042c3dab
Adrian Smijulj <adrian1358@gmail.com>
2020-05-04 09:31:41 +0200

    test: simplify PAT test

--- cypress/integration/admin/security/personalAccessTokens/common.js
+++ cypress/integration/admin/security/personalAccessTokens/common.js
@@ -1,83 +1,76 @@
-/* eslint-disable jest/valid-expect */
-export const testPAT = ({ patComponentRoute, runAfterVisitingRoute }) => {
-    const tokenName = "Cool token #1";
-    const tokenName2 = "Cool token #2 - Updated";
-    let initialTokens, newTokens, lastToken, lastTokenId;
+import mdbid from "mdbid";
 
-    const getTokens = body =>
-        Array.from(body[0].querySelectorAll("[data-testid*=pat-token-list-item]"));
+export const testPAT = ({ patComponentRoute, runAfterVisitingRoute }) => {
+    const tokenName = `Test token ${mdbid()}`;
+    const updatedTokenName = tokenName + " Updated";
+    let initialTokensCount;
+
+    const getTokensCount = body =>
+        Array.from(body[0].querySelectorAll("[data-testid=pat-tokens-list-item]")).length;
 
     cy.visit(patComponentRoute);
+
     if (runAfterVisitingRoute) {
         runAfterVisitingRoute();
     }
+
     cy.findByText("Personal Access Tokens")
         .click()
-        .get("body")
-        .then($body => {
-            initialTokens = getTokens($body);
+        .findByTestId("pat-tokens-list")
+        .then(tokensList => {
+            initialTokensCount = getTokensCount(tokensList);
         })
         .findByText("Create Token")
         .click()
-        .findByTestId("create-token-dialog-content")
+        .findByTestId("account-tokens-dialog")
         .within(() => {
-            cy.get("div > input")
+            cy.findByLabelText("Token name")
                 .clear()
-                .type(tokenName);
+                .type(tokenName)
+                .findByText("OK")
+                .click();
         })
-        .findByTestId("accept-generate-token")
-        .click()
-        .wait(500)
-        .findByTestId(`close-created-token-dialog`)
-        .click()
-        .get("body")
-        .then($body => {
-            const currentTokens = getTokens($body);
-            expect(currentTokens.length).to.equal(initialTokens.length + 1);
+        .findByText("Token created successfully!")
+        .should("exist");
 
-            const initialTokenDataTestIds = initialTokens.map(token =>
-                token.getAttribute("data-testid")
-            );
-            newTokens = currentTokens.filter(
-                token => !initialTokenDataTestIds.includes(token.getAttribute("data-testid"))
-            );
-            lastToken = newTokens[0];
-            lastTokenId = lastToken
-                .getAttribute("data-testid")
-                .split("-")
-                .pop();
-            expect(lastToken.textContent).to.equal(tokenName);
+    cy.findByTestId("created-token-dialog")
+        .findByText("Close")
+        .click();
 
-            return lastToken.querySelector(`[data-testid=updateToken-${lastTokenId}]`);
-        })
-        .click()
-        .get("body")
-        .then($body =>
-            $body[0].querySelector(
-                `[data-testid=UpdateTokenDialogContent-${lastTokenId}] > div > input`
-            )
-        )
-        .clear()
-        .type(tokenName2)
-        .get("body")
-        .then($body => $body[0].querySelector(`[data-testid=AcceptUpdateToken-${lastTokenId}]`))
-        .click()
-        .wait(500)
-        .findByText(tokenName2)
-        .get("body")
-        .then($body => $body[0].querySelector(`[data-testid=deleteToken-${lastTokenId}`))
-        .click()
-        .get("body")
-        .then($body => {
-            return $body[0].querySelector(
-                `[data-testid*=DeleteTokenDialog-${lastTokenId}] [data-testid=confirmationdialog-confirm-action]`
-            );
-        })
-        .click()
-        .wait(1000)
-        .get("body")
-        .then($body => {
-            const currentTokens = getTokens($body);
-            expect(currentTokens.length).to.equal(initialTokens.length);
+    cy.findByTestId("pat-tokens-list").within(() => {
+        cy.findAllByTestId("pat-tokens-list-item").should("have.length", initialTokensCount + 1);
+        cy.get("> div:last-child").within(() => {
+            cy.contains(tokenName);
+            cy.findByTestId("update-personal-access-token").click();
         });
+    });
+
+    cy.get(`[data-testid="update-personal-account-token-dialog"]:visible`)
+        .within(() => {
+            cy.findByLabelText("Token name")
+                .clear()
+                .type(updatedTokenName)
+                .findByText("OK")
+                .click();
+        })
+        .findByText("Token updated successfully!")
+        .should("exist");
+
+    cy.findByTestId("pat-tokens-list").within(() => {
+        cy.get("> div:last-child").within(() => {
+            cy.contains(updatedTokenName);
+            cy.findByTestId("delete-personal-access-token").click();
+        });
+    });
+
+    cy.get(`[data-testid="delete-personal-access-token-dialog"]:visible`)
+        .within(() => {
+            cy.findByText("Confirm").click();
+        })
+        .findByText("Token deleted successfully!")
+        .should("exist");
+
+    cy.findByTestId("pat-tokens-list").within(() => {
+        cy.findAllByTestId("pat-tokens-list-item").should("have.length", initialTokensCount);
+    });
 };
