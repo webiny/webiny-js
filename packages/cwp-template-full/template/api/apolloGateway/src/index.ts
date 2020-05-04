d1b4c8687a735ea3f51c7648a3fd837c905dab2c
Pavel Denisjuk <pavel@webiny.com>
2020-05-04 09:05:32 +0200

    fix: rename TS type usage

--- packages/cwp-template-full/template/api/apolloGateway/src/index.ts
+++ packages/cwp-template-full/template/api/apolloGateway/src/index.ts
@@ -1,7 +1,7 @@
 import { createHandler } from "@webiny/handler";
-import { HttpHandlerApolloGatewayOptions } from "@webiny/handler-apollo-gateway/types";
+import { HandlerApolloGatewayOptions } from "@webiny/handler-apollo-gateway/types";
 import apolloGatewayHandler from "@webiny/handler-apollo-gateway";
 
-declare const HTTP_HANDLER_APOLLO_GATEWAY_OPTIONS: HttpHandlerApolloGatewayOptions;
+declare const HTTP_HANDLER_APOLLO_GATEWAY_OPTIONS: HandlerApolloGatewayOptions;
 
 export const handler = createHandler(apolloGatewayHandler(HTTP_HANDLER_APOLLO_GATEWAY_OPTIONS));
