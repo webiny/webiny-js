60530e97d4f4a45d5fa1bfa2587d3995e7830c49
Fsalker <andreiii500@yahoo.com>
2020-05-06 04:16:59 +0300

    fix: remove existing resource if needed

--- packages/cli-scaffold-custom-lambda/transform.js
+++ packages/cli-scaffold-custom-lambda/transform.js
@@ -3,6 +3,10 @@ module.exports = function({ types: t, template: tpl }, { template, resourceName 
         visitor: {
             Identifier(path) {
                 if (path.node.name === "resources") {
+                    path.parent.value.properties = path.parent.value.properties.filter(
+                        val => val.key.name !== resourceName
+                    );
+
                     path.parent.value.properties.push(
                         t.objectProperty(
                             t.identifier(resourceName),
