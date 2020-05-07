5991a289009e7b65b65aa836e4354f600b72d878
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-07 20:03:35 +0530

    feat: ✨  Add `Number` CmsModelFieldToCommodoFieldPlugin
    add `Number` CmsModelFieldToCommodoFieldPlugin to modelFields

--- packages/api-headless-cms/src/handler/plugins/modelFields/index.ts
+++ packages/api-headless-cms/src/handler/plugins/modelFields/index.ts
@@ -2,8 +2,9 @@ import text from "./text";
 import ref from "./ref";
 import integer from "./integer";
 import float from "./float";
+import number from "./number";
 import boolean from "./boolean";
 import datetime from "./datetime";
 import json from "./json";
 
-export default [text, ref, integer, float, boolean, datetime, json];
+export default [text, ref, integer, float, number, boolean, datetime, json];
