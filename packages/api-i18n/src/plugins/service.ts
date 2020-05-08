4713240b39593ef291ec5e3c449b34bfbe6ef567
Adrian Smijulj <adrian1358@gmail.com>
2020-05-08 08:08:04 +0200

    fix: check if "localesFunction" option was passed

--- packages/api-i18n/src/plugins/service.ts
+++ packages/api-i18n/src/plugins/service.ts
@@ -5,31 +5,39 @@ import { Context } from "@webiny/graphql/types";
 
 let localesCache;
 
-export default options => [
-    i18n,
-    {
-        name: "context-i18n-get-locales",
-        type: "context-i18n-get-locales",
-        async resolve() {
-            if (Array.isArray(localesCache)) {
+type ServicePluginsOptions = { localesFunction: string };
+
+export default (options: ServicePluginsOptions) => {
+    if (!options.localesFunction) {
+        throw new Error(`I18N service plugins error - "localesFunction" not specified.`);
+    }
+
+    return [
+        i18n,
+        {
+            name: "context-i18n-get-locales",
+            type: "context-i18n-get-locales",
+            async resolve() {
+                if (Array.isArray(localesCache)) {
+                    return localesCache;
+                }
+
+                const Lambda = new LambdaClient({ region: process.env.AWS_REGION });
+                const { Payload } = await Lambda.invoke({
+                    FunctionName: options.localesFunction
+                }).promise();
+
+                let parsedPayload;
+
+                try {
+                    parsedPayload = JSON.parse(Payload as string);
+                } catch (e) {
+                    throw new Error("Could not JSON.parse DB Proxy's response.");
+                }
+
+                localesCache = parsedPayload;
                 return localesCache;
             }
-
-            const Lambda = new LambdaClient({ region: process.env.AWS_REGION });
-            const { Payload } = await Lambda.invoke({
-                FunctionName: options.localesFunction
-            }).promise();
-
-            let parsedPayload;
-
-            try {
-                parsedPayload = JSON.parse(Payload as string);
-            } catch (e) {
-                throw new Error("Could not JSON.parse DB Proxy's response.");
-            }
-
-            localesCache = parsedPayload;
-            return localesCache;
-        }
-    } as ContextI18NGetLocales<Context>
-];
+        } as ContextI18NGetLocales<Context>
+    ];
+};
