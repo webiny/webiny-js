66ffe7dbe4a64920e04594f909b04f7d56d4a842
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-08 19:25:59 +0530

    refactor: 📦  Update error message and remove todo comment

--- packages/graphql/src/createSchema/NumberScalar.ts
+++ packages/graphql/src/createSchema/NumberScalar.ts
@@ -1,8 +1,6 @@
 import { GraphQLScalarType } from "graphql";
 import { Kind } from "graphql/language";
 
-// TODO: Need to check whether user can send number as string
-// And do we want to parse it in that case or use null
 const getNumber = (value: any): any => {
     if (typeof value !== "number") {
         return null;
@@ -16,11 +14,11 @@ export const Number = new GraphQLScalarType({
     description: "A custom input type to be used with numbers. Supports Int and Float.",
     serialize: getNumber,
     parseValue: getNumber,
-    parseLiteral: ast => {
+    parseLiteral: (ast: any) => {
         if (ast.kind === Kind.INT || ast.kind === Kind.FLOAT) {
             return ast.value;
         }
 
-        throw new Error("Invalid Number value!");
+        throw new Error(`Expected type Number, found {${ast.value}}`);
     }
 });
