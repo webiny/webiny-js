340863a6970d520ed3f0eeb6af76b5b83734bd8c
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-08 16:24:48 +0530

    refactor: 📦  Remove `Float` and `Integer` field from GQL Fields

--- packages/api-headless-cms/src/handler/plugins/graphqlFields/index.ts
+++ packages/api-headless-cms/src/handler/plugins/graphqlFields/index.ts
@@ -1,9 +1,7 @@
 import text from "./text";
 import ref from "./ref";
-import integer from "./integer";
-import float from "./float";
 import number from "./number";
 import boolean from "./boolean";
 import datetime from "./datetime";
 
-export default [text, ref, integer, float, number, datetime, boolean];
+export default [text, ref, number, datetime, boolean];
