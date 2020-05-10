22f5f9a6421430a6070a1b18dc3dd1c4e8217c62
Adrian Smijulj <adrian1358@gmail.com>
2020-05-10 16:47:21 +0200

    chore: create commodo-versions script

--- scripts/commodoVersions.js
+++ scripts/commodoVersions.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const path = require("path");
 const readJson = require("load-json-file");
+const writeJson = require("write-json-file");
 const getPackages = require("get-yarn-workspaces");
-const { green, blue, red } = require("chalk");
+const { red, blue, cyan } = require("chalk");
 const argv = require("yargs").argv;
 const got = require("got");
 
@@ -16,42 +17,114 @@ const COMMODO_PACKAGES = [
     "@commodo/fields-storage-soft-delete"
 ];
 
+const ALLOWED_DIST_TAGS = ["latest", "canary"];
+
+const DIST_TAG = argv.tag || "latest";
+const PREVIEW = argv.preview;
+
 /**
- * This script prints all @webiny packages and shows its @webiny dependencies.
+ *
  */
 (async () => {
-    const DIST_TAG = argv.tag || "latest";
-
-    let npmData = [];
-    for (let i = 0; i < COMMODO_PACKAGES.length; i++) {
-        let p = COMMODO_PACKAGES[i];
-        const response = await got(`https://registry.npmjs.com/${p}`, { json: true });
-        npmData.push({
-            name: response.body._id,
-            distTags: response.body["dist-tags"]
-        });
+    if (!ALLOWED_DIST_TAGS.includes(DIST_TAG)) {
+        console.log(
+            red(
+                `Provided tag "${DIST_TAG}" not allowed (must be one of: ${ALLOWED_DIST_TAGS.join(
+                    ", "
+                )}).`
+            )
+        );
+        process.exit(1);
     }
 
-    const packages = getPackages().filter(p => !p.includes("sample-project/"));
+    if (PREVIEW) {
+        console.log("Running in preview mode, no changes to actual files will be applied.");
+    }
+
+    console.log("⏳ Fetching package data from NPM...");
+    let npm = {};
+    for (let i = 0; i < COMMODO_PACKAGES.length; i++) {
+        let p = COMMODO_PACKAGES[i];
+        const { body } = await got(`https://registry.npmjs.com/${p}`, { json: true });
+        npm[body._id] = body["dist-tags"];
+    }
+
+    console.log();
+
+    if (PREVIEW) {
+        console.log("🐰 These are the differences in package versions:");
+        console.log("==============================================");
+    } else {
+        console.log("🐰️ These are the changes that were applied to package versions:");
+        console.log("============================================================");
+    }
+
+    const packages = getPackages();
 
     for (let i = 0; i < packages.length; i++) {
         const p = packages[i];
+        const packageJsonPath = path.join(p, "package.json");
         try {
-            const json = await readJson(path.join(p, "package.json"));
-            console.log(`${json.name}@${blue(json.version)}`);
-            Object.keys(json.dependencies).forEach(key => {
-                if (!key.startsWith("@commodo")) {
-                    return;
-                }
+            const packageJson = await readJson(packageJsonPath);
 
-                json.dependencies[key] =
-                console.log(`- ${key}@${green(json.dependencies[key])}`);
-            });
+            if (!mustProcess(packageJson)) {
+                continue;
+            }
+
+            console.log(`${cyan(packageJson.name)}`);
+            const depsCount = processDeps({ deps: packageJson.dependencies, npm });
+            const devDepsCount = processDeps({ deps: packageJson.devDependencies, npm });
+
+            if (depsCount || devDepsCount) {
+                !PREVIEW && (await writeJson(packageJsonPath, packageJson));
+            } else {
+                console.log("No changes.");
+            }
+
             console.log();
-        } catch (er) {
-            console.log(`${blue(">")} ${er.message} in ${red(path.basename(p))}!`);
-        }
+        } catch {}
     }
 
     process.exit(0);
 })();
+
+function mustProcess(json) {
+    if (json.dependencies) {
+        for (let key in json.dependencies) {
+            if (COMMODO_PACKAGES.includes(key)) {
+                return true;
+            }
+        }
+    }
+
+    if (json.devDependencies) {
+        for (let key in json.devDependencies) {
+            if (COMMODO_PACKAGES.includes(key)) {
+                return true;
+            }
+        }
+    }
+}
+
+function processDeps({ deps, npm }) {
+    let processedCount = 0;
+
+    for (let key in deps) {
+        if (!COMMODO_PACKAGES.includes(key)) {
+            continue;
+        }
+
+        const currentVersion = deps[key];
+        const newVersion = npm[key];
+
+        if (currentVersion !== newVersion) {
+            console.log(
+                `- ${key}@${blue(currentVersion)} => ${key}@${blue(`^` + newVersion[DIST_TAG])}`
+            );
+            deps[key] = `^${npm[key]}`;
+            processedCount++;
+        }
+    }
+
+    return processedCount;
+}
