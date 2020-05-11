0f36078e5026f8ae6f66be11763b2759663a2169
Adrian Smijulj <adrian1358@gmail.com>
2020-05-11 13:33:54 +0200

    fix: use ButtonDefault instead of ButtonSecondary

--- packages/app-headless-cms/src/admin/plugins/contentDetails/header/localeSelector/LocaleSelector.tsx
+++ packages/app-headless-cms/src/admin/plugins/contentDetails/header/localeSelector/LocaleSelector.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { css } from "emotion";
 import { withRouter } from "@webiny/react-router";
-import { ButtonDefault, ButtonIcon, ButtonSecondary } from "@webiny/ui/Button";
+import { ButtonDefault } from "@webiny/ui/Button";
 import { Icon } from "@webiny/ui/Icon";
 import { ReactComponent as DownButton } from "@webiny/app-headless-cms/admin/icons/round-arrow_drop_down-24px.svg";
 import { ReactComponent as DoneIcon } from "@webiny/app-headless-cms/admin/icons/done-24px.svg";
-import { ReactComponent as I18NIcon } from "@webiny/app-headless-cms/admin/icons/round-translate-24px.svg";
-import { ReactComponent as DropDownIcon } from "@webiny/app-headless-cms/admin/icons/round-arrow_drop_down-24px.svg";
 import { MenuItem } from "@rmwc/menu";
 import { Menu } from "@webiny/ui/Menu";
 import { useI18N } from "@webiny/app-i18n/hooks/useI18N";
-import { i18n } from "@webiny/app/i18n";
 import { ListItemGraphic } from "@webiny/ui/List";
-const t = i18n.ns("app-headless-cms/admin");
 
 const buttonStyle = css({
     "&.mdc-button": {
@@ -33,32 +29,10 @@ const LocaleSelector = ({ getLocale, setLocale, getLoading }) => {
 
     return (
         <Menu
-            handle={
-                <ButtonSecondary>
-                    <ButtonIcon icon={<I18NIcon />} />
-                    {t`Current locale: {locale}`({
-                        locale: i18n.getLocale().code
-                    })}
-                    <DropDownIcon />
-                </ButtonSecondary>
-            }
-        >
-            {i18n.getLocales().map(item => (
-                <MenuItem key={item.id} onClick={() => {}}>
-                    {item.code}
-                </MenuItem>
-            ))}
-        </Menu>
-    );
-
-    return (
-        <Menu
             className={menuStyles}
             handle={
                 <ButtonDefault className={buttonStyle} disabled={getLoading()}>
-                    {t`Locale: {locale}`({
-                        locale: i18n.getLocales().find(item => item.id === getLocale()).code
-                    })}
+                    {i18n.getLocales().find(item => item.id === getLocale()).code}
                     <Icon icon={<DownButton />} />
                 </ButtonDefault>
             }
