8f92df04649e9835a8809cfd5603c735777f3dba
Adrian Smijulj <adrian1358@gmail.com>
2020-05-11 13:33:31 +0200

    fix: remove title

--- packages/app-headless-cms/src/admin/plugins/contentDetails/header/Header.tsx
+++ packages/app-headless-cms/src/admin/plugins/contentDetails/header/Header.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { css } from "emotion";
 import { renderPlugins } from "@webiny/app/plugins";
-import { Typography } from "@webiny/ui/Typography";
 import { Grid, Cell } from "@webiny/ui/Grid";
-import { I18NValue } from "@webiny/app-i18n/components";
-import { i18n } from "@webiny/app/i18n";
 import classNames from "classnames";
-const t = i18n.ns("app-headless-cms/admin/plugins/content-details-header/header");
-
-const titleGrid = css({
-    paddingBottom: "5px !important"
-});
 
 const toolbarGrid = css({
-    paddingTop: "5px !important",
     borderBottom: "1px solid var(--mdc-theme-on-background)"
-});
-
-const contentTitle = css({
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis"
 });
 
 const headerActions = css({
@@ -37,24 +22,8 @@ const headerActionsRight = css({
 });
 
 const Header = props => {
-    const { contentModel, content } = props;
-
-    let title;
-    if (contentModel) {
-        title = t`New {contentModelName}`({ contentModelName: contentModel.name });
-    }
-
-    if (content.id) {
-        title = I18NValue({ value: content.meta.title });
-    }
-
     return (
         <React.Fragment>
-            <Grid className={titleGrid}>
-                <Cell span={12} className={contentTitle}>
-                    <Typography use="headline5">{title}</Typography>
-                </Cell>
-            </Grid>
             <Grid className={toolbarGrid}>
                 <Cell span={6} className={classNames(headerActions, headerActionsRight)}>
                     {renderPlugins("cms-content-details-header-left", props)}
