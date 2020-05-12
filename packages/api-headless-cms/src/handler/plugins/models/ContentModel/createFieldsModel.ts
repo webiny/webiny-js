cffcee3a9adb237d121d8357d40b08a9738c7b4a
Adrian Smijulj <adrian1358@gmail.com>
2020-05-12 09:56:34 +0200

    fix: type, _id, and _fieldId cannot be changed once set

--- packages/api-headless-cms/src/handler/plugins/models/ContentModel/createFieldsModel.ts
+++ packages/api-headless-cms/src/handler/plugins/models/ContentModel/createFieldsModel.ts
@@ -1,25 +1,27 @@
 import { i18nString } from "@webiny/api-i18n/fields";
 import { validation } from "@webiny/validation";
-import { withFields, string, fields, object, boolean } from "@webiny/commodo";
+import {
+    withFields,
+    string,
+    fields,
+    object,
+    boolean,
+    setOnce,
+    pipe,
+    skipOnPopulate,
+    onSet
+} from "@webiny/commodo";
 
 const required = validation.create("required");
 
 export default context =>
-    withFields({
-        _id: string({ validation: required }),
-        fieldId: string({ validation: required }),
+    withFields(instance => ({
+        _id: setOnce()(string({ validation: required })),
+        fieldId: setOnce()(string({ validation: required })),
         label: i18nString({ context, validation: required }),
         helpText: i18nString({ context }),
         placeholderText: i18nString({ context }),
-        options: fields({
-            list: true,
-            value: [],
-            instanceOf: withFields({
-                label: i18nString({ context }),
-                value: string({ value: "" })
-            })()
-        }),
-        type: string({ validation: required }),
+        type: setOnce()(string({ validation: required })),
         unique: boolean({ validation: required, value: false }),
         searchable: boolean({ validation: required, value: false }),
         sortable: boolean({ validation: required, value: false }),
