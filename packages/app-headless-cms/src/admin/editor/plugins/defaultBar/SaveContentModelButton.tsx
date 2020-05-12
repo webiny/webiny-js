10f2aa59bfb7b618e08f579c6e9d5061e53b1ad8
Adrian Smijulj <adrian1358@gmail.com>
2020-05-12 12:09:09 +0200

    fix: check for errors when pressing the "save" button

--- packages/app-headless-cms/src/admin/editor/plugins/defaultBar/SaveContentModelButton.tsx
+++ packages/app-headless-cms/src/admin/editor/plugins/defaultBar/SaveContentModelButton.tsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonPrimary } from "@webiny/ui/Button";
 import { useSnackbar } from "@webiny/app-admin/hooks/useSnackbar";
 import { useContentModelEditor } from "@webiny/app-headless-cms/admin/components/ContentModelEditor/Context";
 
 import { i18n } from "@webiny/app/i18n";
-const t = i18n.namespace("ContentModelEditor.PublishPageButton");
+const t = i18n.namespace("app-headless-cms/admin/editor/top-bar/save-button");
 
 const SaveContentModelButton = () => {
     const { saveContentModel } = useContentModelEditor();
-
+    const [loading, setLoading] = useState(false);
     const { showSnackbar } = useSnackbar();
 
     return (
         <ButtonPrimary
+            disabled={loading}
             onClick={async () => {
-                await saveContentModel();
-                // Let's wait a bit, because we are also redirecting the user.
-                setTimeout(() => {
-                    showSnackbar(t`Your content model was saved successfully!`);
-                }, 500);
+                setLoading(true);
+                const response = await saveContentModel();
+                setLoading(false);
+
+                if (response.error) {
+                    return showSnackbar(response.error.message);
+                }
+
+                showSnackbar(t`Your content model was saved successfully!`);
             }}
         >
             {t`Save`}
