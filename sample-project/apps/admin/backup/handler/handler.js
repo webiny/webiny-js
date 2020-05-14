eb1649a5fb7766bbcdfca1d5d6135d33c2f32d9c
Pavel Denisjuk <pavel@webiny.com>
2020-05-14 15:51:44 +0200

    chore: remove backup folder

--- sample-project/apps/admin/backup/handler/handler.js
+++
@@ -1,5 +0,0 @@
-import { createHandler } from "@webiny/http-handler";
-import filesPlugins from "@webiny/http-handler-files";
-import indexPlugins from "@webiny/http-handler-index";
-
-export const handler = createHandler(filesPlugins(), indexPlugins());
