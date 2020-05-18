eeb4d69daa63dd1c42af143282a875a0afe3ae30
Adrian Smijulj <adrian1358@gmail.com>
2020-05-18 09:07:40 +0200

    fix: add warning message - cannot preview form if no field are present

--- packages/app-headless-cms/src/admin/components/ContentModelEditor/Tabs/PreviewTab/PreviewTab.tsx
+++ packages/app-headless-cms/src/admin/components/ContentModelEditor/Tabs/PreviewTab/PreviewTab.tsx
@@ -3,6 +3,8 @@ import { css } from "emotion";
 import { useContentModelEditor } from "@webiny/app-headless-cms/admin/components/ContentModelEditor/Context";
 import { Elevation } from "@webiny/ui/Elevation";
 import { ContentModelForm } from "@webiny/app-headless-cms/admin/components/ContentModelForm";
+import { i18n } from "@webiny/app/i18n";
+const t = i18n.ns("app-headless-cms/admin/components/editor/tabs/preview");
 
 const formPreviewWrapper = css({
     padding: 40,
@@ -11,12 +13,26 @@ const formPreviewWrapper = css({
     boxSizing: "border-box"
 });
 
+const style = {
+    noFieldsMessage: css({
+        textAlign: "center"
+    })
+};
+
 export const PreviewTab = () => {
     const { data } = useContentModelEditor();
 
+    const { fields } = data;
+
     return (
         <Elevation z={1} className={formPreviewWrapper}>
-            <ContentModelForm contentModel={data} preview />
+            {fields && fields.length ? (
+                <ContentModelForm contentModel={data} preview />
+            ) : (
+                <div className={style.noFieldsMessage}>
+                    {t`Before previewing the form, please add at least one field to the content model.`}
+                </div>
+            )}
         </Elevation>
     );
 };
