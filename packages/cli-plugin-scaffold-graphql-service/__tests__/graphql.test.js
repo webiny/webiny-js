7e01e3c1fd91e28092a932ee7da08ee67d920219
Fsalker <andreiii500@yahoo.com>
2020-05-20 15:07:14 +0300

    feat(cli-plugin-scaffold-graphql-service): add tests for the new schema

--- packages/cli-plugin-scaffold-graphql-service/__tests__/graphql.test.js
+++ packages/cli-plugin-scaffold-graphql-service/__tests__/graphql.test.js
@@ -2,50 +2,169 @@ import { graphql } from "graphql";
 import plugins from "../template/src/plugins";
 import { createUtils } from "./utils";
 
-// TODO: update tests with new schema
-
-describe.skip("Scaffold GraphQL service test", () => {
+describe("Scaffold GraphQL service test", () => {
     const { useSchema } = createUtils([plugins()]);
-    let unicorns;
+    const bookName = "Book #1";
+    const newBookName = "Book #1 [renamed]";
+    let book;
 
-    test("Get all unicorns", async () => {
+    test("List books (0 books)", async () => {
         const query = /* GraphQL */ `
             query {
-                getUnicorns {
-                    id
-                    name
-                    weight
+                books {
+                    listBooks {
+                        data {
+                            id
+                            title
+                            createdOn
+                        }
+                    }
                 }
             }
         `;
+
         const { schema, context } = await useSchema();
         const response = await graphql(schema, query, {}, context);
 
         if (response.errors) {
             throw response.errors;
         }
-        expect(response.data.getUnicorns.length).toBeGreaterThanOrEqual(1);
-        unicorns = response.data.getUnicorns;
+        expect(response.data.books.listBooks.data.length).toEqual(0);
     });
 
-    test("Get unicorn by name", async () => {
+    test("Create book", async () => {
         const query = /* GraphQL */ `
-            query getThatUnicorn($name: String!) {
-                getUnicorn(name: $name) {
-                    id
-                    name
-                    weight
+            mutation createBook($title: String!) {
+                books {
+                    createBook(data: { title: $title }) {
+                        data {
+                            id
+                            title
+                            createdOn
+                        }
+                    }
                 }
             }
         `;
+
         const { schema, context } = await useSchema();
         const response = await graphql(schema, query, {}, context, {
-            name: unicorns[0].name
+            title: bookName
         });
 
         if (response.errors) {
             throw response.errors;
         }
-        expect(response.data.getUnicorn).toEqual(unicorns[0]);
+
+        expect(response.data.books.createBook.data).toMatchObject({
+            title: bookName
+        });
+        book = response.data.books.createBook.data;
+    });
+
+    test("Update book", async () => {
+        const query = /* GraphQL */ `
+            mutation updateBook($title: String!, $bookId: ID!) {
+                books {
+                    updateBook(id: $bookId, data: { title: $title }) {
+                        data {
+                            id
+                            title
+                            createdOn
+                        }
+                    }
+                }
+            }
+        `;
+
+        const { schema, context } = await useSchema();
+        const response = await graphql(schema, query, {}, context, {
+            title: newBookName,
+            bookId: book.id
+        });
+
+        if (response.errors) {
+            throw response.errors;
+        }
+
+        expect(response.data.books.updateBook.data).toMatchObject({
+            title: newBookName
+        });
+    });
+
+    test("Get updated book by ID", async () => {
+        const query = /* GraphQL */ `
+            query getBook($bookId: ID!) {
+                books {
+                    getBook(id: $bookId) {
+                        data {
+                            id
+                            title
+                            createdOn
+                        }
+                    }
+                }
+            }
+        `;
+
+        const { schema, context } = await useSchema();
+        const response = await graphql(schema, query, {}, context, {
+            bookId: book.id
+        });
+
+        if (response.errors) {
+            throw response.errors;
+        }
+
+        expect(response.data.books.getBook.data).toMatchObject({
+            title: newBookName
+        });
+    });
+
+    test("Delete book", async () => {
+        const query = /* GraphQL */ `
+            mutation deleteBook($bookId: ID!) {
+                books {
+                    deleteBook(id: $bookId) {
+                        data
+                    }
+                }
+            }
+        `;
+
+        const { schema, context } = await useSchema();
+        const response = await graphql(schema, query, {}, context, {
+            bookId: book.id
+        });
+
+        if (response.errors) {
+            throw response.errors;
+        }
+
+        expect(response.data.books.deleteBook.data).toEqual(true);
+    });
+
+    test("List books (0 books)", async () => {
+        const query = /* GraphQL */ `
+            query {
+                books {
+                    listBooks {
+                        data {
+                            id
+                            title
+                            createdOn
+                        }
+                    }
+                }
+            }
+        `;
+
+        const { schema, context } = await useSchema();
+        const response = await graphql(schema, query, {}, context);
+
+        if (response.errors) {
+            throw response.errors;
+        }
+        expect(response.data.books.listBooks.data.length).toEqual(0);
     });
 });
