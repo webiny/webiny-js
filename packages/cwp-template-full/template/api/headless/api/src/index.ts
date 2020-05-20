fecff42107768290389ee64ca07942b0a2e2e9fb
Emil Kais <emil.kais94@gmail.com>
2020-05-20 17:13:37 -0400

    fix: 🐛  Fix cwp-template building and compiling errors for api

--- packages/cwp-template-full/template/api/headless/api/src/index.ts
+++ packages/cwp-template-full/template/api/headless/api/src/index.ts
@@ -3,15 +3,9 @@ import headlessCmsHandler from "@webiny/api-headless-cms/handler";
 import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
 import securityServicePlugins from "@webiny/api-security/plugins/service";
 import i18nServicePlugins from "@webiny/api-i18n/plugins/service";
+import dataManagerPlugins from "@webiny/api-headless-cms/dataManager/client";
 
 export const handler = createHandler(
-    headlessCmsHandler({
-        debug: process.env.DEBUG,
-        server: {
-            introspection: process.env.GRAPHQL_INTROSPECTION,
-            playground: process.env.GRAPHQL_PLAYGROUND
-        }
-    }),
     dbProxy({ functionName: process.env.DB_PROXY_FUNCTION }),
     securityServicePlugins({
         token: {
@@ -22,5 +16,13 @@ export const handler = createHandler(
     }),
     i18nServicePlugins({
         localesFunction: process.env.I18N_LOCALES_FUNCTION
-    })
+    }),
+    headlessCmsHandler({
+        debug: process.env.DEBUG,
+        server: {
+            introspection: process.env.GRAPHQL_INTROSPECTION,
+            playground: process.env.GRAPHQL_PLAYGROUND
+        }
+    }),
+    dataManagerPlugins({ dataManagerFunction: process.env.CMS_DATA_MANAGER_FUNCTION })
 );
