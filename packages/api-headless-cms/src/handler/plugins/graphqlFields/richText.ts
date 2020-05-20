b590363ea3c9090f11f02db80df00b42d616b085
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-20 15:48:22 +0530

    refactor: 📦  Remove `createListFilters` for rich text field
    set `isSortable` to false

--- packages/api-headless-cms/src/handler/plugins/graphqlFields/richText.ts
+++ packages/api-headless-cms/src/handler/plugins/graphqlFields/richText.ts
@@ -1,34 +1,11 @@
 
 import { CmsModelFieldToGraphQLPlugin } from "@webiny/api-headless-cms/types";
 
-const createListFilters = ({ field }) => {
-    return `
-        # Matches if the field is equal to the given value
-        ${field.fieldId}: JSON
-
-        # Matches if the field is not equal to the given value
-        ${field.fieldId}_not: JSON
-
-
-        # Matches if the field value equal one of the given values
-        ${field.fieldId}_in: [JSON]
-
-        # Matches if the field value does not equal any of the given values
-        ${field.fieldId}_not_in: [JSON]
-
-        # Matches if given value is a substring of the the field value
-        ${field.fieldId}_contains: JSON
-
-        # Matches if given value is not a substring of the the field value
-        ${field.fieldId}_not_contains: JSON
-    `;
-};
-
 const plugin: CmsModelFieldToGraphQLPlugin = {
     name: "cms-model-field-to-graphql-rich-text",
     type: "cms-model-field-to-graphql",
     fieldType: "rich-text",
-    isSortable: true,
+    isSortable: false,
     read: {
         createTypeField({ field }) {
             const localeArg = "(locale: String)";
@@ -40,7 +17,6 @@ const plugin: CmsModelFieldToGraphQLPlugin = {
             }
             return `${field.fieldId}: JSON`;
         },
-        createListFilters,
         createResolver({ field }) {
             return (instance, args) => {
                 return instance[field.fieldId].value(args.locale);
@@ -48,7 +24,6 @@ const plugin: CmsModelFieldToGraphQLPlugin = {
         }
     },
     manage: {
-        createListFilters,
         createResolver({ field }) {
             return instance => {
                 return instance[field.fieldId];
