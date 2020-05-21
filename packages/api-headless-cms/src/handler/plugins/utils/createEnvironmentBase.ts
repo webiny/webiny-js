54e37fb8a8fd51c2c5ba34a632aa3d4fae7edc67
Bo Yuan <boyuan954@gmail.com>
2020-05-21 22:52:10 +1000

    fix: 🐛  fix the query in return args
    fix the query in return args for api-headless-cms
    
    BREAKING CHANGE:
    none

--- packages/api-headless-cms/src/handler/plugins/utils/createEnvironmentBase.ts
+++ packages/api-headless-cms/src/handler/plugins/utils/createEnvironmentBase.ts
@@ -2,7 +2,7 @@ import { pipe, withFields, withStaticProps, setOnce, withHooks } from "@webiny/c
 import { cloneDeep } from "lodash";
 
 const modifyQueryArgs = (args = {}, environment) => {
-    const returnArgs = cloneDeep(args);
+    const returnArgs: any = cloneDeep(args);
     if (returnArgs.query) {
         returnArgs.query = {
             $and: [{ environment: environment.id }, returnArgs.query]
