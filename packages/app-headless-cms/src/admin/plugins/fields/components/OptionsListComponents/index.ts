2528a8e8101d706837e4b2d20903fb1c4564edab
Adrian Smijulj <adrian1358@gmail.com>
2020-05-21 21:22:00 +0200

    chore: remove old files

--- packages/app-headless-cms/src/admin/plugins/fields/components/OptionsListComponents/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AddOptionInput } from "./AddOptionInput";
-export { default as OptionsListItem } from "./OptionsListItem";
-export { default as EditFieldOptionDialog } from "./EditFieldOptionDialog";
