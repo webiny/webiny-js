a2e387a1e09c1e4deee98f3bcf3ca3c5f3288afe
Adrian Smijulj <adrian1358@gmail.com>
2020-05-21 18:34:07 +0200

    fix: replace icon

--- packages/app-headless-cms/src/admin/plugins/fields/richText.tsx
+++ packages/app-headless-cms/src/admin/plugins/fields/richText.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as NotesIcon } from "./icons/notes-black-24px.svg";
+import { ReactComponent as NotesIcon } from "./icons/round-text_fields-24px.svg";
 import { Grid, Cell } from "@webiny/ui/Grid";
 import { I18NInput } from "@webiny/app-i18n/admin/components";
 import { CmsEditorFieldTypePlugin } from "@webiny/app-headless-cms/types";
