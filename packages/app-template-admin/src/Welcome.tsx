5044f7fefed730325019c37558a3e2795fb0ca1f
Emil Kais <emil.kais94@gmail.com>
2020-05-29 08:48:56 -0400

    feat: ✨  Add css polish for Welcome screen

--- packages/app-template-admin/src/Welcome.tsx
+++ packages/app-template-admin/src/Welcome.tsx
@@ -1,37 +1,97 @@
 import React from "react";
 
+import { Link } from "@webiny/react-router";
 import {
     SimpleForm,
     SimpleFormHeader,
-    SimpleFormContent
+    SimpleFormContent,
+    SimpleFormFooter
 } from "@webiny/app-admin/components/SimpleForm";
-
+import { Typography } from "@webiny/ui/Typography";
 import { Cell, Grid } from "@webiny/ui/Grid";
 import { ButtonPrimary } from "@webiny/ui/Button";
+import GithubIcon from "./assets/icons/github-logo.svg";
+import SlackIcon from "./assets/icons/slack-logo.svg";
+
+import { css } from "emotion";
+
+
+const linkStyle = css({
+    textDecoration: "none",
+    "&:hover": {
+        textDecoration: "none"
+    },
+    marginTop: "6px"
+});
+
+const noteStyle = css({
+    marginLeft: "20px",
+    marginTop: "20px"
+});
+
+const imageStyle = css({
+    width: "30px",
+    height: "30px"
+});
+
+const communityStyle = css({
+    textAlign: "left",
+});
 
 const Welcome = () => {
     return (
         <Grid>
-            <Cell span={3} />
-            <Cell span={6}>
+            <Cell span={2} />
+            <Cell span={8}>
                 <SimpleForm>
-                    <SimpleFormHeader title={"Welcome to Webiny!"} />
+                    <SimpleFormHeader title={"Welcome!"} />
                     <SimpleFormContent>
-                        Welcome to Webiny!
-                        You are logged in as the first administrator of this application.
+                        <Typography use={"headline6"}>
+                            <p className={noteStyle}>Get started by creating your first Content Model below:</p>
+                        </Typography>
                         <Grid>
-                            <Cell span={3}>
-                                <ButtonPrimary>
-                                    Create Content Model
-                                </ButtonPrimary>
-                            </Cell>
-                            <Cell span={3}>
-                                <ButtonPrimary>
-                                    Join Community
-                                </ButtonPrimary>
+                            <Cell span={6}>
+                                <Link to="cms/content-models" className={linkStyle}>
+                                    <ButtonPrimary>
+                                        Create Content Model
+                                    </ButtonPrimary>                                    
+                                </Link>
                             </Cell>
                         </Grid>
                     </SimpleFormContent>
+                    <SimpleFormFooter>
+                        <Grid>
+                            <Cell span={12}>
+                                <Typography use={"headline6"} className={communityStyle}>
+                                    <p>Join us on:</p>
+                                </Typography>
+                            </Cell>
+                            <Cell span={1}>
+                                <img
+                                    className={imageStyle}
+                                    src={SlackIcon}
+                                    align="left"
+                                />
+                            </Cell>
+                                <Cell span={1} align="middle">
+                                    <Link to="https://www.webiny.com/slack/" className={linkStyle}>
+                                        Slack
+                                    </Link>
+                                </Cell>
+                            <Cell span={1}>
+                                <img
+                                    className={imageStyle}
+                                    src={GithubIcon}
+                                    align="left"
+                                />
+                            </Cell>
+                            <Cell span={1} align="middle">
+                                <Link to="https://github.com/webiny/webiny-js" className={linkStyle}>
+                                    Github
+                                </Link>
+                            </Cell>
+                        </Grid>
+                    </SimpleFormFooter>
                 </SimpleForm>
             </Cell>
         </Grid>
