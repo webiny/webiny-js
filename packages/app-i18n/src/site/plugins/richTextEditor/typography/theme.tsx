a0145b357f5a7b453bfd4638c42d2236f62df177
Pavel Denisjuk <pavel@webiny.com>
2020-05-29 18:33:48 +0200

    fix: add missing files

---
+++ packages/app-i18n/src/site/plugins/richTextEditor/typography/theme.tsx
@@ -0,0 +1,51 @@
+const theme = {
+    typography: {
+        h1: {
+            label: "Heading 1",
+            component: "h1",
+            className: "webiny-rich-typography-h1"
+        },
+        h2: {
+            label: "Heading 2",
+            component: "h2",
+            className: "webiny-rich-typography-h2"
+        },
+        h3: {
+            label: "Heading 3",
+            component: "h3",
+            className: "webiny-rich-typography-h3"
+        },
+        h4: {
+            label: "Heading 4",
+            component: "h4",
+            className: "webiny-rich-typography-h4"
+        },
+        h5: {
+            label: "Heading 5",
+            component: "h5",
+            className: "webiny-rich-typography-h5"
+        },
+        h6: {
+            label: "Heading 6",
+            component: "h6",
+            className: "webiny-rich-typography-h6"
+        },
+        paragraph: {
+            label: "Paragraph",
+            component: "p",
+            className: "webiny-rich-typography-body"
+        },
+        description: {
+            label: "Description",
+            component: "p",
+            className: "webiny-rich-typography-description"
+        },
+        primaryColorText: {
+            label: "Primary color text",
+            component: "p",
+            className: "webiny-rich-typography-body webiny-rich-typography--primary"
+        }
+    }
+};
+
+export default theme;
