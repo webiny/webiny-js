6584f542ae340ba797ace2835583d056755e6e67
Ashutosh <bhardwajashu96@hotmail.com>
2020-06-01 12:46:07 +0530

    refactor: 📦  Remove `fileSettings` service

--- packages/api-files/src/types.ts
+++ packages/api-files/src/types.ts
@@ -1,11 +1,6 @@
-import { Plugin, Context } from "@webiny/graphql/types";
+import { Plugin } from "@webiny/graphql/types";
 
 export type FilesResolverListTagsPlugin = Plugin & {
     name: "files-resolver-list-tags";
     resolve(params: { context: any }): any;
-};
-
-export type ContextFilesGetSettings<T = Context> = Plugin & {
-    name: "context-files-get-settings";
-    resolve(params: { context: T }): Promise<any[]>;
 };
