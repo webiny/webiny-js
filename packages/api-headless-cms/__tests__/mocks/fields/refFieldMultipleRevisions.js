555b290838d3f6782bd59c82fa35f1dcd8e22372
Adrian Smijulj <adrian1358@gmail.com>
2020-06-03 12:18:21 +0200

    test: test creating multiple revisions with ref field

---
+++ packages/api-headless-cms/__tests__/mocks/fields/refFieldMultipleRevisions.js
@@ -0,0 +1,201 @@
+import { locales } from "@webiny/api-i18n/testing";
+
+const mocks = {
+    authorContentModel: ({ contentModelGroupId }) => ({
+        name: "Author",
+        group: contentModelGroupId,
+        fields: [
+            {
+                _id: "vqk-UApa0",
+                fieldId: "title",
+                type: "text",
+                label: {
+                    values: [
+                        {
+                            locale: locales.en.id,
+                            value: "Title"
+                        },
+                        {
+                            locale: locales.de.id,
+                            value: "Titel"
+                        }
+                    ]
+                }
+            },
+            {
+                _id: "id-books",
+                fieldId: "books",
+                type: "ref",
+                multipleValues: true,
+                settings: {
+                    modelId: "book"
+                },
+                label: {
+                    values: [
+                        {
+                            locale: locales.en.id,
+                            value: "Books"
+                        },
+                        {
+                            locale: locales.de.id,
+                            value: "Bücher"
+                        }
+                    ]
+                }
+            }
+        ]
+    }),
+    bookContentModel: ({ contentModelGroupId }) => ({
+        name: "Book",
+        group: contentModelGroupId,
+        fields: [
+            {
+                _id: "vqk-UApa0",
+                fieldId: "title",
+                type: "text",
+                label: {
+                    values: [
+                        {
+                            locale: locales.en.id,
+                            value: "Title"
+                        },
+                        {
+                            locale: locales.de.id,
+                            value: "Titel"
+                        }
+                    ]
+                }
+            }
+        ]
+    }),
+    book1: {
+        data: {
+            title: {
+                values: [
+                    {
+                        locale: locales.en.id,
+                        value: "Book1-en"
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: "Book1-de"
+                    }
+                ]
+            }
+        }
+    },
+    book2: {
+        data: {
+            title: {
+                values: [
+                    {
+                        locale: locales.en.id,
+                        value: "Book2-en"
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: "Book2-de"
+                    }
+                ]
+            }
+        }
+    },
+    author1: ({ book1Id, book2Id }) => ({
+        data: {
+            title: {
+                values: [
+                    {
+                        locale: locales.en.id,
+                        value: "Author1-en"
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: "Author2-de"
+                    }
+                ]
+            },
+            books: {
+                values: [
+                    {
+                        locale: locales.en.id,
+                        value: [book1Id]
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: [book2Id]
+                    }
+                ]
+            }
+        }
+    }),
+    author1Revision2: ({ authorId, book1Id, book2Id }) => ({
+        revision: authorId,
+        data: {
+            books: {
+                values: [
+                    {
+                        locale: locales.en.id,
+                        value: []
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: [book1Id, book2Id]
+                    },
+                    {
+                        locale: locales.de.id,
+                        value: [book2Id]
+                    }
+                ]
+            }
+        }
+    }),
+    newAuthorRevisionSearchEntries: ({ environmentId, authorRev1Id, authorRev2Id, book1Id, book2Id }) => [
+        {
+            locale: locales.en.id,
+            deleted: false,
+            entry1: authorRev1Id,
+            entry2: book1Id,
+            entry1ClassId: "CmsAuthor",
+            entry2ClassId: "CmsBook",
+            environment: environmentId
+        },
+        {
+            locale: locales.de.id,
+            deleted: false,
+            entry1: authorRev1Id,
+            entry2: book2Id,
+            entry1ClassId: "CmsAuthor",
+            entry2ClassId: "CmsBook",
+            environment: environmentId
+        },
+        {
+            locale: locales.de.id,
+            deleted: false,
+            entry1: authorRev2Id,
+            entry2: book1Id,
+            entry1ClassId: "CmsAuthor",
+            entry2ClassId: "CmsBook",
+            environment: environmentId
+        },
+        {
+            locale: locales.de.id,
+            deleted: false,
+            entry1: authorRev2Id,
+            entry2: book2Id,
+            entry1ClassId: "CmsAuthor",
+            entry2ClassId: "CmsBook",
+            environment: environmentId
+        },
+        {
+            locale: locales.de.id,
+            deleted: false,
+            entry1: authorRev2Id,
+            entry2: book2Id,
+            entry1ClassId: "CmsAuthor",
+            entry2ClassId: "CmsBook",
+            environment: environmentId
+        }
+    ]
+};
+
+export default mocks;
