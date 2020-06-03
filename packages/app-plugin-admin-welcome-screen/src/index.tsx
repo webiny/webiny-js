7668a1a669ead2220da714fba387dc01f2f19e08
Fsalker <andreiii500@yahoo.com>
2020-06-03 11:49:35 +0300

    fix(app-plugin-admin-welcome-screen): destructure Helmet correctly

--- packages/app-plugin-admin-welcome-screen/src/index.tsx
+++ packages/app-plugin-admin-welcome-screen/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { Route } from "@webiny/react-router";
 import { AdminLayout } from "@webiny/app-admin/components/AdminLayout";
 import Welcome from "./components/Welcome";
