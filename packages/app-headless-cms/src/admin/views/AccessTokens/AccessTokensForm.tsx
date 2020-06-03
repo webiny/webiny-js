a298e37cb240ff5bcb3e4a7678b0ddb7b27fff34
Fsalker <andreiii500@yahoo.com>
2020-06-03 15:57:17 +0300

    style(app-headless-cms): rename prop

--- packages/app-headless-cms/src/admin/views/AccessTokens/AccessTokensForm.tsx
+++ packages/app-headless-cms/src/admin/views/AccessTokens/AccessTokensForm.tsx
@@ -90,7 +90,7 @@ function EnvironmentAliasesForm() {
                                                 >
                                                     <CopyButton
                                                         value={data.token}
-                                                        showSnackbar={showSnackbar}
+                                                        onCopy={showSnackbar}
                                                     />
                                                 </span>
                                             </div>
