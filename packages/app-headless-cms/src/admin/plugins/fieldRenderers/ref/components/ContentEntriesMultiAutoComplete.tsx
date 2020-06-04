096e717508d37934f2313032597c96d79605d25c
Adrian Smijulj <adrian1358@gmail.com>
2020-06-04 11:58:33 +0200

    chore: reformat code

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/components/ContentEntriesMultiAutoComplete.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/components/ContentEntriesMultiAutoComplete.tsx
@@ -8,85 +8,93 @@ import debounce from "lodash/debounce";
 import { createListQuery, GET_CONTENT_MODEL } from "./graphql";
 
 function ContentEntriesMultiAutocomplete({ bind, field }) {
-  // Value can be an array of object (received from API) or an array of ID (set by the Autocomplete component).
-  const value = bind.value.map(item => {
-    return get(item, "id", item);
-  });
-  const [search, setSearch] = useState("");
-
-  // Format value coming from API
-  useEffect(() => {
-    if (bind.value.some(v => typeof v !== "string")) {
-      // We only need IDs to send back in request to API
-      bind.onChange(bind.value.map(item => {
+    // Value can be an array of object (received from API) or an array of ID (set by the Autocomplete component).
+    const value = bind.value.map(item => {
         return get(item, "id", item);
-      }));
-    }
-  }, [bind.value]);
+    });
+    const [search, setSearch] = useState("");
 
-  const { getValue } = useI18N();
+    // Format value coming from API
+    useEffect(() => {
+        if (bind.value.some(v => typeof v !== "string")) {
+            // We only need IDs to send back in request to API
+            bind.onChange(
+                bind.value.map(item => {
+                    return get(item, "id", item);
+                })
+            );
+        }
+    }, [bind.value]);
 
-  // Fetch ref content model data, so that we can its title field.
-  const refContentModelQuery = useQuery(GET_CONTENT_MODEL, {
-    variables: { where: { modelId: field.settings.modelId } }
-  });
+    const { getValue } = useI18N();
 
-  const refContentModel = get(refContentModelQuery, `data.getContentModel.data`, {});
+    // Fetch ref content model data, so that we can its title field.
+    const refContentModelQuery = useQuery(GET_CONTENT_MODEL, {
+        variables: { where: { modelId: field.settings.modelId } }
+    });
 
-  // Once we have the refContentModel loaded, this will construct proper list and get queries.
-  const { LIST_CONTENT } = useMemo(
-    () => ({
-      LIST_CONTENT: createListQuery(refContentModel)
-    }),
-    [field.settings.modelId, refContentModel.id]
-  );
+    const refContentModel = get(refContentModelQuery, `data.getContentModel.data`, {});
 
-  // Once the query in the input has changed, this query will be triggered.
-  const { titleFieldId } = refContentModel;
-  const listContentQuery = useQuery(LIST_CONTENT, {
-    skip: !search || !titleFieldId,
-    variables: { where: { [`${titleFieldId}_contains`]: search } }
-  });
+    // Once we have the refContentModel loaded, this will construct proper list and get queries.
+    const { LIST_CONTENT } = useMemo(
+        () => ({
+            LIST_CONTENT: createListQuery(refContentModel)
+        }),
+        [field.settings.modelId, refContentModel.id]
+    );
 
-  // Once we have a valid IDs, we load the data.
-  const listContentQueryFilterById = useQuery(LIST_CONTENT, {
-    skip: !value || !titleFieldId,
-    variables: { where: { ['id_in']: value } }
-  });
+    // Once the query in the input has changed, this query will be triggered.
+    const { titleFieldId } = refContentModel;
+    const listContentQuery = useQuery(LIST_CONTENT, {
+        skip: !search || !titleFieldId,
+        variables: { where: { [`${titleFieldId}_contains`]: search } }
+    });
 
-  // Format options for the Autocomplete component.
-  const options = get(listContentQuery, "data.content.data", []).map(item => ({
-    id: item.id,
-    name: getValue(item.meta.title)
-  }));
+    // Once we have a valid IDs, we load the data.
+    const listContentQueryFilterById = useQuery(LIST_CONTENT, {
+        skip: !value || !titleFieldId,
+        variables: { where: { ["id_in"]: value } }
+    });
 
-  // Format value prop for the Autocomplete component.
-  const valueForAutoComplete = get(listContentQueryFilterById, "data.content.data", []).map(item => ({
-    id: item.id,
-    name: getValue(item.meta.title)
-  }));
+    // Format options for the Autocomplete component.
+    const options = get(listContentQuery, "data.content.data", []).map(item => ({
+        id: item.id,
+        name: getValue(item.meta.title)
+    }));
 
-  // Calculate loading prop for the Autocomplete component.
-  const loading =
-    listContentQuery.loading || refContentModelQuery.loading || listContentQueryFilterById.loading;
+    // Format value prop for the Autocomplete component.
+    const valueForAutoComplete = get(listContentQueryFilterById, "data.content.data", []).map(
+        item => ({
+            id: item.id,
+            name: getValue(item.meta.title)
+        })
+    );
 
-  return (
-    <MultiAutoComplete
-      {...bind}
-      onChange={values => {
-        // We only need IDs to send back in request to API
-        bind.onChange(values.map(item => {
-          return get(item, "id", item);
-        }));
-      }}
-      loading={loading}
-      value={valueForAutoComplete}
-      options={options}
-      label={<I18NValue value={field.label} />}
-      description={<I18NValue value={field.helpText} />}
-      onInput={debounce(search => search && setSearch(search), 250)}
-    />
-  );
+    // Calculate loading prop for the Autocomplete component.
+    const loading =
+        listContentQuery.loading ||
+        refContentModelQuery.loading ||
+        listContentQueryFilterById.loading;
+
+    return (
+        <MultiAutoComplete
+            {...bind}
+            onChange={values => {
+                // We only need IDs to send back in request to API
+                bind.onChange(
+                    values.map(item => {
+                        return get(item, "id", item);
+                    })
+                );
+            }}
+            loading={loading}
+            value={valueForAutoComplete}
+            options={options}
+            label={<I18NValue value={field.label} />}
+            description={<I18NValue value={field.helpText} />}
+            onInput={debounce(search => search && setSearch(search), 250)}
+        />
+    );
 }
 
 export default ContentEntriesMultiAutocomplete;
