d1aa7c334e681340ac14fd2b3d83fa583a8e5fc8
Pavel Denisjuk <pavel@webiny.com>
2020-06-04 15:44:32 +0200

    fix(cwp-template-cms): add missing api hook

--- packages/cwp-template-cms/template/webiny.root.js
+++ packages/cwp-template-cms/template/webiny.root.js
@@ -5,8 +5,13 @@ module.exports = {
         plugins: [
             require("@webiny/cli-plugin-deploy-components")({
                 hooks: {
-                    api: ["@webiny/cwp-template-cms/hooks/api"],
-                    apps: ["@webiny/cwp-template-cms/hooks/apps"]
+                    api: [
+                        "@webiny/cwp-template-cms/hooks/api",
+                        "./apps/admin/webiny.config.js",
+                    ],
+                    apps: [
+                        "@webiny/cwp-template-cms/hooks/apps",
+                    ]
                 }
             }),
             "@webiny/cli-plugin-scaffold",
