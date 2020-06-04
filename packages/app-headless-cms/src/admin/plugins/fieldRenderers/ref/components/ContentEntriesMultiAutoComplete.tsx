9a971f32b4ddc0b26d4d6577600be28485e7188c
Ashutosh <bhardwajashu96@hotmail.com>
2020-06-04 11:26:04 +0530

    refactor: 📦  Simplify `refInputs` renderer
    add `ContentEntriesMultiAutocomplete` component

---
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/components/ContentEntriesMultiAutoComplete.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useMemo } from "react";
+import { I18NValue } from "@webiny/app-i18n/components";
+import { MultiAutoComplete } from "@webiny/ui/AutoComplete";
+import { useQuery } from "@webiny/app-headless-cms/admin/hooks";
+import { useI18N } from "@webiny/app-i18n/hooks/useI18N";
+import get from "lodash/get";
+import debounce from "lodash/debounce";
+import { createListQuery, GET_CONTENT_MODEL } from "./graphql";
+
+function ContentEntriesMultiAutocomplete({ bind, field }) {
+  // Value can be an array of object (received from API) or an array of ID (set by the Autocomplete component).
+  const value = bind.value.map(item => {
+    return get(item, "id", item);
+  });
+  const [search, setSearch] = useState("");
+  const { getValue } = useI18N();
+
+  // Fetch ref content model data, so that we can its title field.
+  const refContentModelQuery = useQuery(GET_CONTENT_MODEL, {
+    variables: { where: { modelId: field.settings.modelId } }
+  });
+
+  const refContentModel = get(refContentModelQuery, `data.getContentModel.data`, {});
+
+  // Once we have the refContentModel loaded, this will construct proper list and get queries.
+  const { LIST_CONTENT } = useMemo(
+    () => ({
+      LIST_CONTENT: createListQuery(refContentModel)
+    }),
+    [field.settings.modelId, refContentModel.id]
+  );
+
+  // Once the query in the input has changed, this query will be triggered.
+  const { titleFieldId } = refContentModel;
+  const listContentQuery = useQuery(LIST_CONTENT, {
+    skip: !search || !titleFieldId,
+    variables: { where: { [`${titleFieldId}_contains`]: search } }
+  });
+
+  // Once we have a valid IDs, we load the data.
+  const listContentQueryFilterById = useQuery(LIST_CONTENT, {
+    skip: !value || !titleFieldId,
+    variables: { where: { ['id_in']: value } }
+  });
+
+  // Format options for the Autocomplete component.
+  const options = get(listContentQuery, "data.content.data", []).map(item => ({
+    id: item.id,
+    name: getValue(item.meta.title)
+  }));
+
+  // Format value prop for the Autocomplete component.
+  const valueForAutoComplete = get(listContentQueryFilterById, "data.content.data", []).map(item => ({
+    id: item.id,
+    name: getValue(item.meta.title)
+  }));
+
+  // Calculate loading prop for the Autocomplete component.
+  const loading =
+    listContentQuery.loading || refContentModelQuery.loading || listContentQueryFilterById.loading;
+
+  return (
+    <MultiAutoComplete
+      {...bind}
+      onChange={values => {
+        // We only need IDs to send back in request to API
+        bind.onChange(values.map(item => {
+          return get(item, "id", item);
+        }));
+      }}
+      loading={loading}
+      value={valueForAutoComplete}
+      options={options}
+      label={<I18NValue value={field.label} />}
+      description={<I18NValue value={field.helpText} />}
+      onInput={debounce(search => search && setSearch(search), 250)}
+    />
+  );
+}
+
+export default ContentEntriesMultiAutocomplete;
--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/refInputs.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/refInputs.tsx
@@ -1,92 +1,9 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
-import gql from "graphql-tag";
+import React from "react";
 import { CmsEditorFieldRendererPlugin } from "@webiny/app-headless-cms/types";
-import { I18NValue } from "@webiny/app-i18n/components";
-import { CircularProgress } from "@webiny/ui/Progress";
-import { MultiAutoComplete } from "@webiny/ui/AutoComplete";
-import { useQuery } from "@webiny/app-headless-cms/admin/hooks";
-import get from "lodash/get";
-import upperFirst from "lodash/upperFirst";
-import pluralize from "pluralize";
-import debounce from "lodash/debounce";
-import { extractIdsFromValue, GET_CONTENT_MODEL } from "./utils";
+import ContentEntriesMultiAutocomplete from "./components/ContentEntriesMultiAutoComplete";
 
 import { i18n } from "@webiny/app/i18n";
 const t = i18n.ns("app-headless-cms/admin/fields/ref");
-
-const MultiAutoCompleteContainer = ({ field, value, onChange, titleFieldId }) => {
-    const [title, setTitle] = useState([]);
-    // Initialize values
-    useEffect(() => {
-        if (Array.isArray(value) && value.length !== 0) {
-            const onlyIds = value.map(i => i.id);
-            onChange(onlyIds);
-        }
-    }, [])
-
-    // Reset title
-    useEffect(() => {
-        if (Array.isArray(value) && value.length !== 0) {
-            const titles = [];
-            value.forEach(v => {
-                if (v && v.meta && v.meta.title.value) {
-                    titles.push(v.meta.title.value);
-                }
-            })
-            if (titles.length) {
-                setTitle(titles);
-            }
-        }
-    }, [value])
-
-    const typeName = upperFirst(field.settings.modelId);
-
-    const LIST_CONTENT_ENTRIES = gql`
-                query HeadlessCmsListContentEntries($where: ${typeName}ListWhereInput, $sort: [${typeName}ListSorter], $after: String, $before: String, $limit: Int) {
-                    list${pluralize(typeName)}(where: $where, sort: $sort, after: $after, before: $before, limit: $limit) {
-                        data {
-                            id
-                            meta {
-                                model
-                                title {
-                                    value
-                                }
-                            }
-                        }
-                    }
-                }
-            `;
-
-    const { data } = useQuery(LIST_CONTENT_ENTRIES, { variables: { where: { [`${titleFieldId}_in`]: title } } });
-
-    const contentEntries = get(data, `list${pluralize(typeName)}.data`, []).map(item => {
-        return { id: item.id, name: item.meta.title.value };
-    });
-
-    const handleOnInput = useCallback(inputValue => {
-        if (inputValue.length) {
-            setTitle([...title, inputValue]);
-        }
-    }, []);
-
-    const debounceOnInput = useMemo(() => debounce(handleOnInput, 250), []);
-
-    const label = I18NValue({ value: field.label });
-    const description = I18NValue({ value: field.helpText });
-
-    return (
-        <MultiAutoComplete
-            value={extractIdsFromValue(value, contentEntries)}
-            onChange={(value) => {
-                onChange(value.map(item => item.id));
-            }}
-            onInput={debounceOnInput}
-            options={contentEntries}
-            label={label}
-            description={description}
-        />
-    );
-}
 
 const plugin: CmsEditorFieldRendererPlugin = {
     type: "cms-editor-field-renderer",
@@ -98,25 +15,12 @@ const plugin: CmsEditorFieldRendererPlugin = {
         canUse({ field }) {
             return field.type === "ref" && field.multipleValues;
         },
-        render({ field, getBind }) {
-            const Bind = getBind();
-
-            const { data, loading } = useQuery(GET_CONTENT_MODEL, { variables: { where: { modelId: field.settings.modelId } } });
-
-            if (loading) {
-                return <CircularProgress />;
-            }
-
-            const { titleFieldId } = get(data, `getContentModel.data`, {});
-
+        render(props) {
+            const Bind = props.getBind();
             return (
                 <Bind>
                     {bind => (
-                        <MultiAutoCompleteContainer
-                            {...bind}
-                            field={field}
-                            titleFieldId={titleFieldId}
-                        />
+                        <ContentEntriesMultiAutocomplete {...props} bind={bind} />
                     )}
                 </Bind>
             );
