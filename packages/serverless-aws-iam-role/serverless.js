e6cbfa32eb36fc813dc7e19226bd6123c373e7e7
Pavel Denisjuk <pavel@webiny.com>
2020-06-06 18:13:59 +0200

    feat(serverless-aws-iam-role): add component to deploy AWS IAM roles

---
+++ packages/serverless-aws-iam-role/serverless.js
@@ -0,0 +1,94 @@
+const { Component } = require("@webiny/serverless-component");
+const IAM = require("aws-sdk/clients/iam");
+const isEqual = require("lodash.isequal");
+
+const {
+    createRole,
+    deleteRole,
+    addRolePolicy,
+    removeRolePolicy,
+    updateAssumeRolePolicy
+} = require("./utils");
+
+class ServerlessAwsIamRole extends Component {
+    async default(inputs = {}) {
+        const iam = new IAM({
+            region: inputs.region
+        });
+
+        const state = this.state.output || {};
+
+        const name = state.name || this.context.instance.getResourceName();
+
+        const output = {
+            name,
+            service: inputs.service,
+            policy: inputs.policy,
+            region: inputs.region
+        };
+
+        // If policy is defined inline, remove ARN
+        if (inputs.policy.Version && inputs.policy.Statement) {
+            delete inputs.policy.arn;
+        }
+
+        if (!state.arn) {
+            this.context.instance.debug(`Creating role %o.`, name);
+            output.arn = await createRole({ iam, name, ...inputs });
+        } else {
+            if (!isEqual(this.state.inputs, inputs)) {
+                if (state.service !== inputs.service) {
+                    this.context.instance.debug(`Updating service for role %o.`, name);
+                    await updateAssumeRolePolicy({ iam, ...inputs });
+                }
+                if (!isEqual(state.policy, inputs.policy)) {
+                    this.context.instance.debug(`Updating policy for role %o.`, name);
+                    await removeRolePolicy({ iam, ...inputs });
+                    await addRolePolicy({ iam, ...inputs });
+                }
+            }
+        }
+
+        this.state.inputs = inputs;
+        this.state.output = output;
+
+        await this.save();
+
+        this.context.instance.debug(
+            `Role %o was successfully deployed to region %o.`,
+            name,
+            inputs.region
+        );
+
+        return this.state.output;
+    }
+
+    async remove() {
+        this.context.instance.status(`Removing`);
+
+        if (!this.state.output || !this.state.output.name) {
+            return;
+        }
+
+        const iam = new IAM({ region: this.state.inputs.region });
+
+        this.context.instance.debug(
+            `Removing role %o from region %o.`,
+            this.state.output.name,
+            this.state.output.region
+        );
+
+        await deleteRole({ iam, ...this.state.output });
+
+        this.context.instance.debug(
+            `Role %o successfully removed from region %o.`,
+            this.state.output.name,
+            this.state.output.region
+        );
+
+        this.state = {};
+        await this.save();
+    }
+}
+
+module.exports = ServerlessAwsIamRole;
---
+++ packages/serverless-aws-iam-role/utils.js
@@ -0,0 +1,113 @@
+const addRolePolicy = async ({ iam, name, policy }) => {
+    if (policy.hasOwnProperty("arn")) {
+        await iam
+            .attachRolePolicy({
+                RoleName: name,
+                PolicyArn: policy.arn
+            })
+            .promise();
+    } else if (Object.keys(policy).length > 0) {
+        await iam
+            .putRolePolicy({
+                RoleName: name,
+                PolicyName: `${name}-policy`,
+                PolicyDocument: JSON.stringify(policy)
+            })
+            .promise();
+    }
+};
+
+const removeRolePolicy = async ({ iam, name, policy }) => {
+    if (policy.hasOwnProperty("arn")) {
+        return await iam
+            .detachRolePolicy({
+                RoleName: name,
+                PolicyArn: policy.arn
+            })
+            .promise();
+    }
+
+    await iam
+        .deleteRolePolicy({
+            RoleName: name,
+            PolicyName: `${name}-policy`
+        })
+        .promise();
+};
+
+const createRole = async ({ iam, name, service, policy }) => {
+    const assumeRolePolicyDocument = {
+        Version: "2012-10-17",
+        Statement: {
+            Effect: "Allow",
+            Principal: {
+                Service: service
+            },
+            Action: "sts:AssumeRole"
+        }
+    };
+    const roleRes = await iam
+        .createRole({
+            RoleName: name,
+            Path: "/",
+            AssumeRolePolicyDocument: JSON.stringify(assumeRolePolicyDocument)
+        })
+        .promise();
+
+    await addRolePolicy({
+        iam,
+        name,
+        policy
+    });
+
+    return roleRes.Role.Arn;
+};
+
+const deleteRole = async ({ iam, name, policy }) => {
+    try {
+        await removeRolePolicy({
+            iam,
+            name,
+            policy
+        });
+        await iam
+            .deleteRole({
+                RoleName: name
+            })
+            .promise();
+    } catch (error) {
+        if (
+            error.message !== `Policy ${policy.arn} was not found.` &&
+            error.code !== "NoSuchEntity"
+        ) {
+            throw error;
+        }
+    }
+};
+
+const updateAssumeRolePolicy = async ({ iam, name, service }) => {
+    const assumeRolePolicyDocument = {
+        Version: "2012-10-17",
+        Statement: {
+            Effect: "Allow",
+            Principal: {
+                Service: service
+            },
+            Action: "sts:AssumeRole"
+        }
+    };
+    await iam
+        .updateAssumeRolePolicy({
+            RoleName: name,
+            PolicyDocument: JSON.stringify(assumeRolePolicyDocument)
+        })
+        .promise();
+};
+
+module.exports = {
+    createRole,
+    deleteRole,
+    addRolePolicy,
+    removeRolePolicy,
+    updateAssumeRolePolicy
+};
