a5a35d562d1cdbfd5c78af8014528b6e93b018e6
Ashutosh <bhardwajashu96@hotmail.com>
2020-06-08 16:54:22 +0530

    feat(app-headless-cms): add `dateTimeFields` components
    add `dateTimeFields` components for field with multipleValues true

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/dateTime/dateTimeFields.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/dateTime/dateTimeFields.tsx
@@ -7,37 +7,60 @@ import Time from "./Time";
 import { i18n } from "@webiny/app/i18n";
 const t = i18n.ns("app-headless-cms/admin/fields/date-time");
 import get from "lodash/get";
+import {ReactComponent as DeleteIcon} from "@webiny/app-headless-cms/admin/icons/close.svg";
+import DynamicListMultipleValues from "@webiny/app-headless-cms/admin/plugins/fieldRenderers/DynamicListMultipleValues";
 
 const plugin: CmsEditorFieldRendererPlugin = {
     type: "cms-editor-field-renderer",
-    name: "cms-editor-field-renderer-date-time",
+    name: "cms-editor-field-renderer-date-times",
     renderer: {
-        rendererName: "date-time-input",
+        rendererName: "date-time-inputs",
         name: t`Date/Time Inputs`,
         description: t`Renders inputs for various formats of dates and times.`,
         canUse({ field }) {
-            return field.type === "datetime" && !field.multipleValues && !get(field, "predefinedValues.enabled");
+            return field.type === "datetime" && field.multipleValues && !get(field, "predefinedValues.enabled");
         },
-        render({ field, getBind }) {
-            const Bind = getBind();
+        render(props) {
+            const field = props.field;
 
-            return (
-                <Bind>
-                    {bind => {
-                        if (field.settings.type === "dateTimeWithoutTimezone") {
-                            return <DateTimeWithoutTimezone field={field} bind={bind} />;
-                        }
-                        if (field.settings.type === "dateTimeWithTimezone") {
-                            return <DateTimeWithTimezone field={field} bind={bind} />;
-                        }
-                        if (field.settings.type === "time") {
-                            return <Time field={field} bind={bind} />;
-                        }
+            return <DynamicListMultipleValues {...props}>
+                {({ bind, index }) => {
+                    const trailingIcon = index > 0  && {
+                            icon: <DeleteIcon />,
+                            onClick: bind.index.removeValue
+                    };
 
-                        return <Input {...bind} type={field.settings.type} />;
-                    }}
-                </Bind>
-            );
+                    if (field.settings.type === "dateTimeWithoutTimezone") {
+                        return <DateTimeWithoutTimezone
+                            field={field}
+                            bind={bind.index}
+                            trailingIcon={trailingIcon}
+                        />;
+                    }
+                    if (field.settings.type === "dateTimeWithTimezone") {
+                        return <DateTimeWithTimezone
+                            field={field}
+                            bind={bind.index}
+                            trailingIcon={trailingIcon}
+                        />;
+                    }
+                    if (field.settings.type === "time") {
+                        return <Time
+                            field={field}
+                            bind={bind.index}
+                            label={t`Value {number}`({ number: index + 1 })}
+                            trailingIcon={trailingIcon}
+                        />;
+                    }
+
+                    return <Input
+                        {...bind.index}
+                        type={field.settings.type}
+                        label={t`Value {number}`({ number: index + 1 })}
+                        trailingIcon={trailingIcon}
+                    />;
+                }}
+            </DynamicListMultipleValues>
         }
     }
 };
