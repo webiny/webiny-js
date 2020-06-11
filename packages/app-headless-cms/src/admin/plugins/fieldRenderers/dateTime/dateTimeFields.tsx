7d70dd8e092938fea8860efc642d4af5eb9c8415
Ashutosh <bhardwajashu96@hotmail.com>
2020-06-11 20:44:21 +0530

    style: fix styles with prettier

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/dateTime/dateTimeFields.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/dateTime/dateTimeFields.tsx
@@ -7,7 +7,7 @@ import Time from "./Time";
 import { i18n } from "@webiny/app/i18n";
 const t = i18n.ns("app-headless-cms/admin/fields/date-time");
 import get from "lodash/get";
-import {ReactComponent as DeleteIcon} from "@webiny/app-headless-cms/admin/icons/close.svg";
+import { ReactComponent as DeleteIcon } from "@webiny/app-headless-cms/admin/icons/close.svg";
 import DynamicListMultipleValues from "@webiny/app-headless-cms/admin/plugins/fieldRenderers/DynamicListMultipleValues";
 
 const plugin: CmsEditorFieldRendererPlugin = {
@@ -18,49 +18,63 @@ const plugin: CmsEditorFieldRendererPlugin = {
         name: t`Date/Time Inputs`,
         description: t`Renders inputs for various formats of dates and times.`,
         canUse({ field }) {
-            return field.type === "datetime" && field.multipleValues && !get(field, "predefinedValues.enabled");
+            return (
+                field.type === "datetime" &&
+                field.multipleValues &&
+                !get(field, "predefinedValues.enabled")
+            );
         },
         render(props) {
             const field = props.field;
 
-            return <DynamicListMultipleValues {...props}>
-                {({ bind, index }) => {
-                    const trailingIcon = index > 0  && {
+            return (
+                <DynamicListMultipleValues {...props}>
+                    {({ bind, index }) => {
+                        const trailingIcon = index > 0 && {
                             icon: <DeleteIcon />,
                             onClick: bind.index.removeValue
-                    };
+                        };
 
-                    if (field.settings.type === "dateTimeWithoutTimezone") {
-                        return <DateTimeWithoutTimezone
-                            field={field}
-                            bind={bind.index}
-                            trailingIcon={trailingIcon}
-                        />;
-                    }
-                    if (field.settings.type === "dateTimeWithTimezone") {
-                        return <DateTimeWithTimezone
-                            field={field}
-                            bind={bind.index}
-                            trailingIcon={trailingIcon}
-                        />;
-                    }
-                    if (field.settings.type === "time") {
-                        return <Time
-                            field={field}
-                            bind={bind.index}
-                            label={t`Value {number}`({ number: index + 1 })}
-                            trailingIcon={trailingIcon}
-                        />;
-                    }
+                        if (field.settings.type === "dateTimeWithoutTimezone") {
+                            return (
+                                <DateTimeWithoutTimezone
+                                    field={field}
+                                    bind={bind.index}
+                                    trailingIcon={trailingIcon}
+                                />
+                            );
+                        }
+                        if (field.settings.type === "dateTimeWithTimezone") {
+                            return (
+                                <DateTimeWithTimezone
+                                    field={field}
+                                    bind={bind.index}
+                                    trailingIcon={trailingIcon}
+                                />
+                            );
+                        }
+                        if (field.settings.type === "time") {
+                            return (
+                                <Time
+                                    field={field}
+                                    bind={bind.index}
+                                    label={t`Value {number}`({ number: index + 1 })}
+                                    trailingIcon={trailingIcon}
+                                />
+                            );
+                        }
 
-                    return <Input
-                        {...bind.index}
-                        type={field.settings.type}
-                        label={t`Value {number}`({ number: index + 1 })}
-                        trailingIcon={trailingIcon}
-                    />;
-                }}
-            </DynamicListMultipleValues>
+                        return (
+                            <Input
+                                {...bind.index}
+                                type={field.settings.type}
+                                label={t`Value {number}`({ number: index + 1 })}
+                                trailingIcon={trailingIcon}
+                            />
+                        );
+                    }}
+                </DynamicListMultipleValues>
+            );
         }
     }
 };
