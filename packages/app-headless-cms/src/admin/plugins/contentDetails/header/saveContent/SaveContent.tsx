a12708946f73c35e7f5e7000e1ced2d6754399fe
Ashutosh <bhardwajashu96@hotmail.com>
2020-06-15 19:34:54 +0530

    feat(app-headless-cms): add `ContentFormOptionsMenu`
    add `ContentFormOptionsMenu` to content form header

--- packages/app-headless-cms/src/admin/plugins/contentDetails/header/saveContent/SaveContent.tsx
+++ packages/app-headless-cms/src/admin/plugins/contentDetails/header/saveContent/SaveContent.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { i18n } from "@webiny/app/i18n";
-import { ButtonPrimary } from "@webiny/ui/Button";
+import { ButtonSecondary } from "@webiny/ui/Button";
 const t = i18n.ns("app-headless-cms/admin/plugins/content-details/header/publish-revision");
 
 const SaveContentButton = ({ state }) => {
-
-    return <ButtonPrimary onClick={() => state.contentForm.submit()}>{t`Save`}</ButtonPrimary>;
+    return <ButtonSecondary onClick={() => state.contentForm.submit()}>{t`Save`}</ButtonSecondary>;
 };
 
 export default SaveContentButton;
