d72cb76550e6932088373cec63cd49175b86d9f4
Fsalker <andreiii500@yahoo.com>
2020-06-23 20:59:35 +0300

    feat(api-security): validate ATs for /read and /preview cms routes

--- packages/api-security/src/hasScope.ts
+++ packages/api-security/src/hasScope.ts
@@ -1,9 +1,10 @@
 import { GraphQLFieldResolver } from "@webiny/graphql/types";
 import SecurityError from "./SecurityError";
+import { validateAccessToken } from "@webiny/api-headless-cms/content/plugins";
 
 export default (scope: string) => {
     return (resolver: GraphQLFieldResolver) => {
-        return (parent, args, ctx, info) => {
+        return async (parent, args, ctx, info) => {
             if (process.env.NODE_ENV === "test") {
                 return resolver(parent, args, ctx, info);
             }
@@ -14,6 +15,15 @@ export default (scope: string) => {
 
             if (access) {
                 allowAccess = access.fullAccess || access.scopes.includes(scope);
+            }
+
+            if (ctx.cms.READ || ctx.cms.PREVIEW) {
+                try {
+                    await validateAccessToken({ context: ctx });
+                    allowAccess = true;
+                } catch (e) {
+                    return new SecurityError(e);
+                }
             }
 
             if (allowAccess) {
