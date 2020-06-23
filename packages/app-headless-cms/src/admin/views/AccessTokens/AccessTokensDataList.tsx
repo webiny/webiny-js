9bddab7dd50b0235b5d7130533e74731e1a9b254
Emil Kais <emil.kais94@gmail.com>
2020-06-23 16:45:12 -0400

    wip(headless): add api information dialog to accessTokensDataList

--- packages/app-headless-cms/src/admin/views/AccessTokens/AccessTokensDataList.tsx
+++ packages/app-headless-cms/src/admin/views/AccessTokens/AccessTokensDataList.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { i18n } from "@webiny/app/i18n";
 import { ConfirmationDialog } from "@webiny/ui/ConfirmationDialog";
 import { DeleteIcon } from "@webiny/ui/List/DataList/icons";
 import { useCrud } from "@webiny/app-admin/hooks/useCrud";
 import { Typography } from "@webiny/ui/Typography";
+import { ReactComponent as InformationIcon } from "../../icons/info.svg";
+import { css } from "emotion";
 
 import {
     DataList,
@@ -15,11 +17,38 @@ import {
     ListActions
 } from "@webiny/ui/List";
 import { Link } from "@webiny/react-router";
+import EnvironmentInfoDialog from "../../components/EnvironmentInfoDialog";
 
 const t = i18n.ns("app-headless-cms/admin/environmentAliases/data-list");
 
+const style = {
+    informationLabel: css({
+        color: "var(--mdc-theme-primary)"
+    }),
+    icon: css({
+        width: 16,
+        height: 16,
+        marginTop: "4px",
+        marginLeft: "10px"
+    }),
+    environmentText: css({
+        display: "flex",
+        flexDirection: "row"
+    })
+};
+
+
 const EnvironmentAliasesDataList = () => {
     const { actions, list } = useCrud();
+    const [infoOpened, setInfoOpened] = useState(false);
+    const [selectedInfo, setSelectedInfo] = useState({
+        name: t`N\A`,
+        url: {
+            manage: "",
+            preview: "",
+            read: ""
+        },
+    });
 
     return (
         <DataList
@@ -49,7 +78,25 @@ const EnvironmentAliasesDataList = () => {
                     {data.map(item => (
                         <ListItem key={item.id} selected={isSelected(item)}>
                             <ListItemText onClick={() => select(item)}>
-                                {item.name}{" "}
+                                <div className={style.environmentText}>
+                                    {item.name}{" "}
+                                    <Typography use={"caption"} className={style.informationLabel}>
+                                            <div onClick={e => {
+                                                e.preventDefault();
+                                                e.stopPropagation();
+                                                setInfoOpened(true);
+                                                setSelectedInfo(item);
+                                            }}>
+                                                <InformationIcon className={style.icon}/>
+                                            </div>
+                                    </Typography>
+                                    <EnvironmentInfoDialog
+                                        open={infoOpened}
+                                        onClose={() => setInfoOpened(false)}
+                                        name={selectedInfo.name}
+                                        url={selectedInfo.url}
+                                    />
+                                </div>
                                 {item.default && (
                                     <Typography use={"overline"}>{t`(default)`}</Typography>
                                 )}
