a2e476b0c44eb4bd00cbb13fcd55ca2eb142f303
Fsalker <andreiii500@yahoo.com>
2020-06-26 14:57:05 +0300

    chore(api-security): remove comment

--- packages/api-security/src/plugins/service.ts
+++ packages/api-security/src/plugins/service.ts
@@ -1,5 +1,4 @@
 import security from "./security";
 import { SecurityOptions } from "../types";
 
-// TODO [Andrei]: add PAT & JWT authorization plugins here
 export default (options: SecurityOptions) => [security(options)];
