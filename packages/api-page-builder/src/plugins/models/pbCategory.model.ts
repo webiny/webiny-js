e4c661761104039de972cb762d71676d06e1f92a
Tommy Jackson <tom.jackson50@gmail.com>
2020-06-26 23:56:22 +0100

    fix: created base to catch cateogories with linked pages

--- packages/api-page-builder/src/plugins/models/pbCategory.model.ts
+++ packages/api-page-builder/src/plugins/models/pbCategory.model.ts
@@ -6,6 +6,16 @@ import { withFields, string, withName } from "@webiny/commodo";
 export default ({ createBase }) =>
     flow(
         withName("PbCategory"),
+        withHooks({
+            async beforeDelete() {
+                const Model = context.models[this.modelId];
+                if (await Model.findOne()) {
+                    throw new Error(
+                        "Cannot delete category because some pages are linked to it."
+                    );
+                }
+            }
+        }),
         withFields({
             name: string({ validation: validation.create("required") }),
             slug: string({ validation: validation.create("required") }),
