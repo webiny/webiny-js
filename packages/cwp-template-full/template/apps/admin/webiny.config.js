447ab884dd6ed3bfd8dbae786b70a07596385e1a
Pavel Denisjuk <pavel@webiny.com>
2020-06-27 14:08:11 +0200

    chore: remove unused REACT_APP_FILES_PROXY ENV variable

--- packages/cwp-template-full/template/apps/admin/webiny.config.js
+++ packages/cwp-template-full/template/apps/admin/webiny.config.js
@@ -5,7 +5,6 @@ const map = {
     REACT_APP_USER_POOL_REGION: "${cognito.userPool.Region}",
     REACT_APP_GRAPHQL_API_URL: "${cdn.url}/graphql",
     REACT_APP_API_URL: "${cdn.url}",
-    REACT_APP_FILES_PROXY: "${cdn.url}",
     REACT_APP_USER_POOL_ID: "${cognito.userPool.Id}",
     REACT_APP_USER_POOL_WEB_CLIENT_ID: "${cognito.appClients[0].ClientId}"
 };
