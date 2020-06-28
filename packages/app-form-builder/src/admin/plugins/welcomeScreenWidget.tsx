346811b15d04a1146d892d76655a0fc05107c509
Tommy Jackson <tom.jackson50@gmail.com>
2020-06-28 21:01:31 +0100

    fix: added remaining scopes and removed secureView from widgets

--- packages/app-form-builder/src/admin/plugins/welcomeScreenWidget.tsx
+++ packages/app-form-builder/src/admin/plugins/welcomeScreenWidget.tsx
@@ -4,9 +4,6 @@ import { Link } from "@webiny/react-router";
 import { AdminWelcomeScreenWidgetPlugin } from "@webiny/app-plugin-admin-welcome-screen/types";
 import { ButtonSecondary } from "@webiny/ui/Button";
 import { css } from "emotion";
-import { SecureView } from "@webiny/app-security/components";
-
-const ROLE_FORMS_EDITOR = ["forms:settings"];
 
 const linkStyle = css({
     textDecoration: "none",
@@ -22,14 +19,12 @@ const buttonStyle = css({
 const plugin: AdminWelcomeScreenWidgetPlugin = {
     type: "admin-welcome-screen-widget",
     name: "admin-welcome-screen-widget-form-builder",
-    scopes: ROLE_FORMS_EDITOR,
+    scopes: ["forms:settings"],
     widget: {
         cta: (
-            <SecureView scopes={ROLE_FORMS_EDITOR}>
-                <Link to="/forms" className={linkStyle}>
-                    <ButtonSecondary className={buttonStyle}>Create a new Form</ButtonSecondary>
-                </Link>
-            </SecureView>
+            <Link to="/forms" className={linkStyle}>
+                <ButtonSecondary className={buttonStyle}>Create a new Form</ButtonSecondary>
+            </Link>
         ),
         description: "Create forms using a drag and drop interface and track conversions.",
         title: "Form Builder"
