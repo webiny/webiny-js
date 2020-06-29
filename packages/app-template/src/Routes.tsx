9b5841b15c2b3103d13dd80ed09446a9a047039b
Pavel Denisjuk <pavel@webiny.com>
2020-06-29 16:56:44 +0200

    fix(app-template): sort routes and handle catch-all route paths

--- packages/app-template/src/Routes.tsx
+++ packages/app-template/src/Routes.tsx
@@ -5,16 +5,19 @@ import { RoutePlugin } from "@webiny/app/types";
 
 export const Routes = () => {
     const plugins = getPlugins<RoutePlugin>("route").sort((a, b) => {
-        if (a.name === "route-not-found" || a.name === "route-root") {
+        const pathA = a.route.props.path || "*";
+        const pathB = b.route.props.path || "*";
+
+        if (pathA === "*" || pathA === "/") {
             return 1;
         }
 
-        if (b.name === "route-not-found" || b.name === "route-root") {
+        if (pathB === "*" || pathB === "/") {
             return -1;
         }
 
         return 0;
     });
-
+    
     return <Switch>{plugins.map(pl => React.cloneElement(pl.route, { key: pl.name }))}</Switch>;
 };
