6df5d779efe657595d056470593bc7147e9dbaaf
Fsalker <andreiii500@yahoo.com>
2020-06-30 05:14:28 +0300

    refactor(api-security): allow authorization by multiple plugins

--- packages/api-security/src/hasScope.ts
+++ packages/api-security/src/hasScope.ts
@@ -1,40 +1,21 @@
 import { GraphQLFieldResolver } from "@webiny/graphql/types";
 import SecurityError from "./SecurityError";
-// import { validateAccessToken } from "@webiny/api-headless-cms/content/plugins";
 
 export default (scope: string) => {
     return (resolver: GraphQLFieldResolver) => {
-        return async (parent, args, ctx, info) => {
+        return async (parent, args, context, info) => {
             if (process.env.NODE_ENV === "test") {
-                return resolver(parent, args, ctx, info);
+                return resolver(parent, args, context, info);
             }
 
-            let allowAccess = false;
-
-            const access = ctx.user && ctx.user.access;
-
-            // TODO [Andrei]: get authorization plugins (AT, PAT, JWT) and call them here
-            // const authorizationScopesPlugin = ctx.plugins.ByType(...)
-            // for(let plugin of authorizationScopesPlugin) {
-            //     if(await plugin.hasScope({context: ctx, scope})) {
-            //         return  resolver(parent, args, ctx, info);
-            //     }
-            // }
-
-            if (access) {
-                // TODO [Andrei]: move this inside the correct authorization plugin (JWT)
-                allowAccess = access.fullAccess || access.scopes.includes(scope);
+            const scopeAuthorizationPlugins = context.plugins.byType("authorization");
+            for (let plugin of scopeAuthorizationPlugins) {
+                if (await plugin.hasScope({ context, scope })) {
+                    return resolver(parent, args, context, info);
+                }
             }
 
-            // if (ctx.cms.READ || ctx.cms.PREVIEW) {
-            //     try {
-            //         await validateAccessToken({ context: ctx });
-            //         allowAccess = true;
-            //     } catch (e) {
-            //         return new SecurityError(e);
-            //     }
-            // }
-            return new SecurityError();
+            return new SecurityError(); // The scopes couldn't be authorized with the given JWT / PAT / AT
         };
     };
 };
