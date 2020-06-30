4db91d73d800573dc4acff58a7d493aa4266cb50
Pavel Denisjuk <pavel@webiny.com>
2020-06-30 10:57:52 +0200

    fix(app-template): sort routes so * is always the last item

--- packages/app-template/src/Routes.tsx
+++ packages/app-template/src/Routes.tsx
@@ -8,16 +8,23 @@ export const Routes = () => {
         const pathA = a.route.props.path || "*";
         const pathB = b.route.props.path || "*";
 
+        // This will sort paths at the very bottom of the list
+        if (pathA === "/" && pathB === "*") {
+            return -1;
+        }
+
+        // This will push * and / to the bottom of the list
         if (pathA === "*" || pathA === "/") {
             return 1;
         }
 
+        // This will push * and / to the bottom of the list
         if (pathB === "*" || pathB === "/") {
             return -1;
         }
 
         return 0;
     });
-    
+
     return <Switch>{plugins.map(pl => React.cloneElement(pl.route, { key: pl.name }))}</Switch>;
 };
