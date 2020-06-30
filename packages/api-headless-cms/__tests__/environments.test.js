a3bb9489b0227f3f387f32a106f538c098a2cd4b
Fsalker <andreiii500@yahoo.com>
2020-06-30 05:13:47 +0300

    feat(api-headless-cms): add slug test & [WIP] contentModels test

--- packages/api-headless-cms/__tests__/environments.test.js
+++ packages/api-headless-cms/__tests__/environments.test.js
@@ -32,6 +32,7 @@ const GET_ENVIRONMENT = /* GraphQL */ `
                         id
                         name
                     }
+                    slug
                 }
             }
         }
@@ -45,10 +46,14 @@ const LIST_ENVIRONMENTS = /* GraphQL */ `
                 data {
                     id
                     name
+                    contentModels {
+                        modelId
+                    }
                     createdFrom {
                         id
                         name
                     }
+                    slug
                 }
             }
         }
@@ -60,13 +65,21 @@ describe("Environments test", () => {
     const { invoke } = useApolloHandler();
     const { getCollection } = useDatabase();
     const initialEnvironment = { id: mdbid() };
+    const modelId = { id: mdbid() };
 
     beforeAll(async () => {
+        await getCollection("CmsContentModel").insertOne({
+            id: modelId,
+            environment: initialEnvironment,
+            name: "Test Model"
+        });
+
         await getCollection("CmsEnvironment").insertOne({
             id: initialEnvironment.id,
             name: "Initial Environment",
             description: "This is the initial environment.",
-            createdFrom: null
+            createdFrom: null,
+            slug: "test slug"
         });
     });
 
@@ -76,6 +89,7 @@ describe("Environments test", () => {
                 query: CREATE_ENVIRONMENT,
                 variables: {
                     data: {
+                        slug: "666",
                         name: "new-environment-1"
                     }
                 }
@@ -92,6 +106,7 @@ describe("Environments test", () => {
                 variables: {
                     data: {
                         name: "new-environment-1",
+                        slug: "test-slug",
                         createdFrom: initialEnvironment.id
                     }
                 }
@@ -100,6 +115,25 @@ describe("Environments test", () => {
 
         expect(body.data.cms.createEnvironment.data.id).toBeTruthy();
         expect(body.data.cms.createEnvironment.data.createdFrom.id).toBeTruthy();
+    });
+
+    it("should not create two environments with the same slug", async () => {
+        const [body] = await invoke({
+            body: {
+                query: CREATE_ENVIRONMENT,
+                variables: {
+                    data: {
+                        name: "new-environment-1",
+                        slug: "test-slug",
+                        createdFrom: initialEnvironment.id
+                    }
+                }
+            }
+        });
+
+        expect(body.data.cms.createEnvironment.error.message).toEqual(
+            expect.stringMatching(/already exists/)
+        );
     });
 
     it(`should utilize "where" filtering correctly`, async () => {
@@ -115,6 +149,7 @@ describe("Environments test", () => {
         });
 
         expect(body.data.cms.getEnvironment.data.id).toBeTruthy();
+        expect(body.data.cms.getEnvironment.data.slug).toBeTruthy();
         expect(body.data.cms.getEnvironment.data.createdFrom).toBeNull();
     });
 
@@ -146,5 +181,8 @@ describe("Environments test", () => {
         });
 
         expect(body.data.cms.listEnvironments.data.length).toBe(initialListLength + 1);
+        // console.log(body.data.cms.listEnvironments.data.map(x => x.contentModels));
+        // TODO [Andrei] [now]: fix this test
+        // expect(body.data.cms.listEnvironments.data[0].contentModels).toBeTruthy();
     });
 });
