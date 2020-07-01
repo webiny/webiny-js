25d6edc868294344969052f26954aef0dbd63987
Adrian Smijulj <adrian1358@gmail.com>
2020-07-01 09:40:48 +0200

    test: upgrade test (add additional test case)

--- packages/api-headless-cms/__tests__/mocks/fields/refInvalidReferences.js
+++ packages/api-headless-cms/__tests__/mocks/fields/refInvalidReferences.js
@@ -2,96 +2,139 @@ import { locales } from "@webiny/api-i18n/testing";
 
 const mocks = {
     bookContentModel: ({ contentModelGroupId }) => ({
-        name: "Book",
-        group: contentModelGroupId,
-        fields: []
+        data: {
+            name: "Book",
+            group: contentModelGroupId,
+            fields: []
+        }
     }),
     authorContentModel: ({ contentModelGroupId }) => ({
-        name: "Author",
-        group: contentModelGroupId,
-        fields: [
-            {
-                _id: "vqk-UApa0",
-                fieldId: "title",
-                type: "text",
-                label: {
-                    values: [
-                        {
-                            locale: locales.en.id,
-                            value: "Title"
-                        },
-                        {
-                            locale: locales.de.id,
-                            value: "Titel"
-                        }
-                    ]
-                }
-            },
-            {
-                _id: "id-favorite-book",
-                fieldId: "favoriteBook",
-                type: "ref",
-                settings: {
-                    modelId: "book"
+        data: {
+            name: "Author",
+            group: contentModelGroupId,
+            fields: [
+                {
+                    _id: "vqk-UApa0",
+                    fieldId: "title",
+                    type: "text",
+                    label: {
+                        values: [
+                            {
+                                locale: locales.en.id,
+                                value: "Title"
+                            },
+                            {
+                                locale: locales.de.id,
+                                value: "Titel"
+                            }
+                        ]
+                    }
                 },
-                label: {
-                    values: [
-                        {
-                            locale: locales.en.id,
-                            value: "Favorite Book"
-                        },
-                        {
-                            locale: locales.de.id,
-                            value: "Lieblingsbuch"
-                        }
-                    ]
-                }
-            },
-            {
-                _id: "id-other-books",
-                fieldId: "otherBooks",
-                type: "ref",
-                multipleValues: true,
-                settings: {
-                    modelId: "book"
+                {
+                    _id: "id-favorite-book",
+                    fieldId: "favoriteBook",
+                    type: "ref",
+                    settings: {
+                        modelId: "book"
+                    },
+                    label: {
+                        values: [
+                            {
+                                locale: locales.en.id,
+                                value: "Favorite Book"
+                            },
+                            {
+                                locale: locales.de.id,
+                                value: "Lieblingsbuch"
+                            }
+                        ]
+                    }
                 },
-                label: {
-                    values: [
-                        {
-                            locale: locales.en.id,
-                            value: "Other Books"
-                        },
-                        {
-                            locale: locales.de.id,
-                            value: "Andere Bücher"
-                        }
-                    ]
+                {
+                    _id: "id-other-books",
+                    fieldId: "otherBooks",
+                    type: "ref",
+                    multipleValues: true,
+                    settings: {
+                        modelId: "book"
+                    },
+                    label: {
+                        values: [
+                            {
+                                locale: locales.en.id,
+                                value: "Other Books"
+                            },
+                            {
+                                locale: locales.de.id,
+                                value: "Andere Bücher"
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
+            ]
+        }
     }),
     authorWithoutBookRefFields: ({ contentModelGroupId }) => ({
-        name: "Author",
-        group: contentModelGroupId,
-        fields: [
-            {
-                _id: "vqk-UApa0",
-                fieldId: "title",
-                type: "text",
-                label: {
-                    values: [
-                        {
-                            locale: locales.en.id,
-                            value: "Title"
-                        },
-                        {
-                            locale: locales.de.id,
-                            value: "Titel"
-                        }
-                    ]
+        data: {
+            name: "Author",
+            group: contentModelGroupId,
+            fields: [
+                {
+                    _id: "vqk-UApa0",
+                    fieldId: "title",
+                    type: "text",
+                    label: {
+                        values: [
+                            {
+                                locale: locales.en.id,
+                                value: "Title"
+                            },
+                            {
+                                locale: locales.de.id,
+                                value: "Titel"
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
+            ]
+        }
+    }),
+    modelA: ({ contentModelGroupId }) => ({
+        data: {
+            name: "A",
+            group: contentModelGroupId,
+            fields: []
+        }
+    }),
+    modelBCreate: ({ contentModelGroupId }) => ({
+        data: {
+            name: "B",
+            group: contentModelGroupId,
+            fields: []
+        }
+    }),
+    modelBUpdate: ({ modelId }) => ({
+        id: modelId,
+        data: {
+            fields: [
+                {
+                    _id: "id-model-a-ref",
+                    fieldId: "a",
+                    type: "ref",
+                    settings: {
+                        modelId: "a"
+                    },
+                    label: {
+                        values: [
+                            {
+                                locale: locales.en.id,
+                                value: "A"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
     })
 };
 
