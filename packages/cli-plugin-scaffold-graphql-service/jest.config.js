86bb40c38c9d66073bca69f3be46769fe3a2e663
Adrian Smijulj <adrian1358@gmail.com>
2020-07-02 12:17:28 +0200

    fix: remove unneeded jest-mongodb

--- packages/cli-plugin-scaffold-graphql-service/jest.config.js
+++ packages/cli-plugin-scaffold-graphql-service/jest.config.js
@@ -1,6 +1,5 @@
-const mongoDbPreset = require("@shelf/jest-mongodb/jest-preset");
 const base = require("../../jest.config.base");
 
 module.exports = {
-    ...base({ path: __dirname }, [mongoDbPreset])
+    ...base({ path: __dirname })
 };
