ded5adf1b013993bddf6103fadb93d13bbb1d0d4
Tommy Jackson <tom.jackson50@gmail.com>
2020-07-02 22:54:45 +0100

    fix: removed fake PbContext reference

--- packages/api-page-builder/src/plugins/models/pbCategory.model.ts
+++ packages/api-page-builder/src/plugins/models/pbCategory.model.ts
@@ -3,9 +3,10 @@ import { validation } from "@webiny/validation";
 
 import { withFields, string, withHooks, withName } from "@webiny/commodo";
 
-import { PbContext } from "@webiny/api-page-builder/types";
+/*import { PbContext } from "@webiny/api-page-builder/types";
 
-export default ({ createBase, context } : { createBase: Function, context: PbContext }) =>
+export default ({ createBase, context } : { createBase: Function, context: PbContext }) =>*/
+export default ({ createBase, context }) =>
     flow(
         withName("PbCategory"),
         withHooks({
