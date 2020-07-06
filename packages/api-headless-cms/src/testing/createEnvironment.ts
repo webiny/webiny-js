aef6925214961f6d50654494968dbbcbbd0eceee
Adrian Smijulj <adrian1358@gmail.com>
2020-07-06 12:47:10 +0200

    fix: add slug

--- packages/api-headless-cms/src/testing/createEnvironment.ts
+++ packages/api-headless-cms/src/testing/createEnvironment.ts
@@ -2,6 +2,7 @@ export default ({ database }) =>
     database.collection("CmsEnvironment").insert({
         id: "e1e1e1e1e1e1e1e1e1e1e1e1",
         name: "Initial Environment",
+        slug: "initial-environment",
         description: "This is the initial environment.",
         createdFrom: null
     });
