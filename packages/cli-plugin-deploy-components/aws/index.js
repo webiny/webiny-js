49e27b9d9b956c7cad16340fcb3487f69a9a25ac
Tommy Jackson <tom.jackson50@gmail.com>
2020-07-07 22:48:42 +0100

    fix: impoting sts from aws and trying to call getCallerIdentity

--- packages/cli-plugin-deploy-components/aws/index.js
+++ packages/cli-plugin-deploy-components/aws/index.js
@@ -1,10 +1,25 @@
+//const execa = require('execa');
+//const AWS_CREDENTIALS = execa("aws sts get-caller-identity"); \
+const sts = require("aws-sdk/clients/sts");
+
 module.exports = {
     type: "hook-before-deploy",
     name: "cli-command-aws",
     async hook(params, context) {
         // check AWS credentials
+        console.log("sts:::::::::::::::");
+        console.log(sts);
         try {
-            let awsCred; //to equal command to call aws sts get-caller-identity and receive object data
+            //to equal command to call aws sts get-caller-identity and receive object data
+            await sts.getCallerIdentity(params, function(err, data) {
+                if (err) {
+                    console.log("ERROR::::::::::::::::::");
+                    console.log(err, err.stack); //an error occurred
+                } else {
+                    console.log("DATA::::::::::::::::::::");
+                    console.log(data); //successful response
+                }
+            });
         } catch (err) {
             throw err; //replace with a statement on failure to find aws creds
         }
