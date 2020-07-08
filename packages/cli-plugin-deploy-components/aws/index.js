5a7af7fb2339838ea6c376bdea0d7a2497214416
Tommy Jackson <tom.jackson50@gmail.com>
2020-07-08 22:53:15 +0100

    fix: changed plugin name, error message and brought back try and catch

--- packages/cli-plugin-deploy-components/aws/index.js
+++ packages/cli-plugin-deploy-components/aws/index.js
@@ -1,27 +1,19 @@
-//const execa = require('execa');
-//const AWS_CREDENTIALS = execa("aws sts get-caller-identity"); \
 const STS = require("aws-sdk/clients/sts");
 
 module.exports = {
     type: "hook-before-deploy",
-    name: "cli-command-aws",
+    name: "hook-before-deploy-aws-credentials",
     async hook() {
         // check AWS credentials
         const sts = new STS();
-        const stsResults = await sts
-            .getCallerIdentity({})
-            .promise()
-            .then((result, err) => {
-                if (err) {
-                    console.err(`The following error occured: ${err}.`);
-                    console.log(`Please to refer to the following link for setting up credentials:
-                    Please to refer to the following link for setting up aws credentials:
-                    https://docs.webiny.com/docs/guides/aws-credentials/
-                    `);
-                } else {
-                    console.log("Your AWS data is set!");
-                    console.log(result);
-                }
-            });
+        try {
+            await sts.getCallerIdentity({}).promise();
+        } catch (err) {
+            console.log("Looks like your AWS credentials are not configured correctly!");
+            console.log(
+                "Please to refer to the following link for setting them up: https://docs.webiny.com/docs/guides/aws-credentials/"
+            );
+            process.exit(1);
+        }
     }
 };
