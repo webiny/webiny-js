9bb00d89ed24ba65253ca6fd1ebb0df63b08f250
Tommy Jackson <tom.jackson50@gmail.com>
2020-07-10 19:04:43 +0100

    fix: updated console message, removed comment and deleted aws.js file

--- packages/cli-plugin-deploy-components/aws/index.js
+++ packages/cli-plugin-deploy-components/aws/index.js
@@ -4,14 +4,13 @@ module.exports = {
     type: "hook-before-deploy",
     name: "hook-before-deploy-aws-credentials",
     async hook() {
-        // check AWS credentials
         const sts = new STS();
         try {
             await sts.getCallerIdentity({}).promise();
         } catch (err) {
             console.log("Looks like your AWS credentials are not configured correctly!");
             console.log(
-                "Check the AWS credentials documentation: https://docs.webiny.com/docs/guides/aws-credentials/"
+                "To learn how to configure your AWS credentials, visit https://docs.webiny.com/docs/guides/aws-credentials"
             );
             process.exit(1);
         }
