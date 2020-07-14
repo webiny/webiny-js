a33fb2a714f20c3068c4e11d45505338cccf5cca
Adrian Smijulj <adrian1358@gmail.com>
2020-07-14 06:26:36 +0200

    fix: add "*" scope

--- packages/api-headless-cms/src/testing/createAccessToken.ts
+++ packages/api-headless-cms/src/testing/createAccessToken.ts
@@ -3,5 +3,6 @@ export default ({ database }) =>
         id: "a1a1a1a1a1a1a1a1a1a1a1a1",
         name: "Default Access Token",
         description: "This is the initial environment.",
-        token: "some-random-token"
+        token: "some-random-token",
+        scopes: ['*']
     });
