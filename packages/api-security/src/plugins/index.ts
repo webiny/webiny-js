73e10fc1cbece524da223bf30468a62e2871a24c
Adrian Smijulj <adrian1358@gmail.com>
2020-07-14 06:25:21 +0200

    wip: use new security plugins structure

--- packages/api-security/src/plugins/index.ts
+++ packages/api-security/src/plugins/index.ts
@@ -3,4 +3,13 @@ import graphql from "./graphql";
 import security from "./security";
 import { SecurityOptions } from "../types";
 
-export default (options: SecurityOptions) => [models(), graphql, security(options)];
+export default (options: SecurityOptions) => [
+    models(),
+    graphql,
+    security(options),
+    securityAuthenticationPlugins(), // context da
+    securityAuthJwtPlugins({
+        // ovdje je has-scope plugin + authN
+        secret: JWT_TOKEN_SIGN_SECRET
+    })
+];
