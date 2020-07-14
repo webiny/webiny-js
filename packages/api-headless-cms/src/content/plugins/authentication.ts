a192ab1510f9b9d7a9b72bfe88da1b018b3fee96
Adrian Smijulj <adrian1358@gmail.com>
2020-07-14 06:26:01 +0200

    fix: remove environments (already included in the scope)

--- packages/api-headless-cms/src/content/plugins/authentication.ts
+++ packages/api-headless-cms/src/content/plugins/authentication.ts
@@ -1,47 +1,20 @@
-import { withStorage, withFields, string, withName, pipe } from "@webiny/commodo";
 import { SecurityIdentity } from "@webiny/api-security/utils";
 
 const getAuthorizationToken = context => {
-    const { headers } = context.event;
+    const [event] = context.args;
+    const { headers = {} } = event;
     return headers.authorization || headers.Authorization;
 };
-
-const createBase = context =>
-    pipe(
-        withFields({
-            id: context.commodo.fields.id()
-        }),
-        withStorage({ driver: context.commodo.driver })
-    )();
-
-const createModels = context => ({
-    CmsAccessToken: pipe(withName("CmsAccessToken"))(createBase(context)),
-    CmsEnvironment2AccessToken: pipe(
-        withName("CmsEnvironment2AccessToken"),
-        withFields(() => ({
-            environment: string()
-        }))
-    )(createBase(context))
-});
 
 export default {
     name: "authentication-access-token",
     type: "authentication",
     authenticate: async context => {
-        if (!context.event) {
-            return;
-        }
-
-        if (context.event.isMetaQuery) {
-            // Allow "getMeta" query.
-            return new SecurityIdentity();
-        }
-
-        const { CmsAccessToken, CmsEnvironment2AccessToken } = createModels(context);
+        const { CmsAccessToken } = context.models;
 
         const accessToken = getAuthorizationToken(context);
         if (!accessToken) {
-            return false;
+            return;
         }
 
         const token = await CmsAccessToken.findOne({
@@ -49,19 +22,14 @@ export default {
         });
 
         if (!token) {
-            return false;
+            return;
         }
 
-        const allowedEnvironments = await CmsEnvironment2AccessToken.find({
-            query: { accessToken: token.id }
+        return new SecurityIdentity({
+            id: token.id,
+            displayName: token.name,
+            email: "",
+            scopes: token.scopes
         });
-        const currentEnvironment = context.cms.getEnvironment();
-        if (!allowedEnvironments.find(env => env.id === currentEnvironment.id)) {
-            return false;
-        }
-
-        context.security.user = token;
-
-        return true;
     }
 };
