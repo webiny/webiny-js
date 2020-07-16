b4c56962aebc5617075d45649ec01cbe22afb8d0
Fsalker <andreiii500@yahoo.com>
2020-07-16 14:39:19 +0300

    chore(scaffolding): remove unused tests

--- packages/cli-plugin-scaffold-react-package/__tests__/utils/useContext.js
+++
@@ -1,5 +0,0 @@
-import { setupContext } from "@webiny/graphql/testing";
-
-export default plugins => async (contextPlugins = [], baseContext = {}) => {
-    return await setupContext([...plugins, ...contextPlugins], baseContext);
-};
