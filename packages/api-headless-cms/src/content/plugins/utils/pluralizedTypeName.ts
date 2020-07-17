fc0f8209167fc6d9ee556db563cba00b92a8d818
Emil Kais <emil.kais94@gmail.com>
2020-07-17 08:03:42 -0400

    style(headless): used prettier for pluralized type name

--- packages/api-headless-cms/src/content/plugins/utils/pluralizedTypeName.ts
+++ packages/api-headless-cms/src/content/plugins/utils/pluralizedTypeName.ts
@@ -1,6 +1,6 @@
 import pluralize from "pluralize";
 
-// This will make it so if the content model name is a single capitalized letter 
+// This will make it so if the content model name is a single capitalized letter
 // Ex. A, pluralize will not capitalize the name to AS, and instead make it As
 
 export const pluralizedTypeName = (typeName: string) => {
