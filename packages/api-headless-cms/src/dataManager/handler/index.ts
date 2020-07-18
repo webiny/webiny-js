61101ffcef4116f9b88d7064fca0126640f5bab1
Pavel Denisjuk <pavel@webiny.com>
2020-07-18 18:38:57 +0200

    feat(api-headless-cms): add support for data manager hooks

--- packages/api-headless-cms/src/dataManager/handler/index.ts
+++ packages/api-headless-cms/src/dataManager/handler/index.ts
@@ -1,18 +1,32 @@
 import { applyContextPlugins } from "@webiny/graphql";
-import { HandlerPlugin } from "@webiny/handler/types";
+import { HandlerContext, HandlerPlugin } from "@webiny/handler/types";
 import { createResponse } from "@webiny/handler";
 import headlessPlugins from "../../content/plugins";
 import { generateContentModelIndexes } from "./generateContentModelIndexes";
 import { generateRevisionIndexes } from "./generateRevisionIndexes";
+import { deleteRevisionIndexes } from "./deleteRevisionIndexes";
 import { deleteEnvironmentData } from "./deleteEnvironmentData";
 import { Action } from "../types";
 import { copyEnvironment } from "./copyEnvironment";
+import { CmsDataManagerHookPlugin } from "@webiny/api-headless-cms/dataManager/types";
 
 // Setup plugins for given environment
 async function setupEnvironment(context, environment) {
     context.plugins.register(await headlessPlugins({ type: "manage", environment }));
     await applyContextPlugins(context);
 }
+
+const processHooks = async (payload, context: HandlerContext) => {
+    const plugins = context.plugins.byType<CmsDataManagerHookPlugin>("cms-data-manager-hook");
+    for (let i = 0; i < plugins.length; i++) {
+        const plugin = plugins[i];
+        try {
+            await plugin.hook(payload, context);
+        } catch {
+            // We completely ignore errors in hooks, since those are 3rd party plugins.
+        }
+    }
+};
 
 export default () => [
     {
@@ -44,6 +58,39 @@ export default () => [
                         await setupEnvironment(context, environment);
 
                         result = await generateRevisionIndexes({ context, ...params });
+
+                        await processHooks(
+                            {
+                                type: "entry-update",
+                                environment,
+                                contentModel: params.contentModel,
+                                entry: result
+                            },
+                            context
+                        );
+
+                        break;
+                    case "deleteRevisionIndexes":
+                        if (!params.contentModel || !params.revision) {
+                            throw Error(
+                                `[${action}] Missing required parameters "contentModel" and "revision"!`
+                            );
+                        }
+
+                        await setupEnvironment(context, environment);
+
+                        result = await deleteRevisionIndexes({ context, ...params });
+
+                        await processHooks(
+                            {
+                                type: "entry-delete",
+                                environment,
+                                contentModel: params.contentModel,
+                                entry: result
+                            },
+                            context
+                        );
+
                         break;
                     case "copyEnvironment":
                         if (!params.copyFrom || !params.copyTo) {
