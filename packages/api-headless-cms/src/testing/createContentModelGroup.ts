e3ae87f2e31cd888994ce4c6870b5a26d1794d87
Adrian Smijulj <adrian1358@gmail.com>
2020-07-19 18:10:25 +0200

    fix: avoid hardcoded string

--- packages/api-headless-cms/src/testing/createContentModelGroup.ts
+++ packages/api-headless-cms/src/testing/createContentModelGroup.ts
@@ -1,6 +1,7 @@
 export const CONTENT_MODEL_GROUP_ID = "c1c1c1c1c1c1c1c1c1c1c1c1";
+import { environmentId as defaultEnvironmentId } from "./createEnvironment";
 
-export default ({ database, environmentId = "e1e1e1e1e1e1e1e1e1e1e1e1" }) =>
+export default ({ database, environmentId = defaultEnvironmentId }) =>
     database.collection("CmsContentModelGroup").insert({
         id: CONTENT_MODEL_GROUP_ID,
         name: "Ungrouped",
