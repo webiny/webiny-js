79792208392c4e30a73fa4bc5ba1a64d9f92a2d4
Adrian Smijulj <adrian1358@gmail.com>
2020-07-21 10:42:33 +0200

    test: refactor tests

--- packages/api-security/src/validateAccessToken/index.ts
+++ packages/api-security/src/validateAccessToken/index.ts
@@ -1,17 +1,15 @@
-import { applyContextPlugins } from "@webiny/graphql";
 import securityModels from "../plugins/models";
 import validateAccessToken from "./validateAccessToken";
 
 export default () => [
+    securityModels(),
     {
         type: "handler",
         name: "handler-validate-access-token",
-        async handle({ context, args }) {
-            context.plugins.register(securityModels());
+        async handle({ context }) {
+            const [event] = context.args;
 
-            await applyContextPlugins(context);
-
-            return await validateAccessToken(context, args[0].PAT);
+            return await validateAccessToken(context, event.pat);
         }
     }
 ];
