3902be0b4a05d6f362219f45be162efc70181702
Fsalker <andreiii500@yahoo.com>
2020-07-23 06:05:25 +0300

    feat(scaffolding): simplify scaffolded plugin

--- packages/cli-plugin-scaffold-node-package/template/src/plugins/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default {
-    name: "utility-plugins-remove-vowels",
-    type: "utility-plugins",
-    removeVowels: str => {
-        return str.replace(/[aeiouAEIOU]/g, "");
-    }
-};
