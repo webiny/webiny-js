f8635d4e2f568ebb28f02cceb6ac6b4eff208e85
Adrian Smijulj <adrian1358@gmail.com>
2020-07-24 21:10:13 +0200

    chore: create waitToDeploy script

--- scripts/waitToDeploy.js
+++ scripts/waitToDeploy.js
@@ -11,28 +11,36 @@ const ping = async () => {
     const appsStateFile = getState("apps");
     const { url } = appsStateFile.outputs.cdn;
 
+    let totalTries = 0;
     while (true) {
+        totalTries++;
+        if (totalTries > 100) {
+            console.log(
+                `🚩 Even after a hundred "${url}" pings, a successful HTTP response wasn't received. Something must be wrong, exiting... `
+            );
+            process.exit(1);
+        }
+
         try {
             console.log(`⏱ Pinging ${url}...`);
             await got(url);
-
-            // Success, let's still wait a bit, just in case.
-            await wait();
             break;
         } catch (e) {
-            if (e.code !== "ENOTFOUND") {
-                // Success, let's still wait a bit, just in case.
+            // Weird, but only "ENOTFOUND" code means the CDN URL is not publicly available yet.
+            if (e.code === "ENOTFOUND") {
+                console.log("ℹ️ Site not ready yet, will wait a bit and ping again...");
                 await wait();
-                break;
+                continue;
             }
 
-            console.log("ℹ️ Site not ready yet, will wait a bit and ping again...");
-            await wait();
+            break;
         }
     }
 
-    console.log("ode");
-    return true;
+    // Success, let's still wait a bit, just in case.
+    await wait();
+    console.log("✅ Site deployed and ready!");
+    process.exit(0);
 };
 
 ping();
