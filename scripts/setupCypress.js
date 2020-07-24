dc447f2396079e1d9ad679c322a133430888abd2
Adrian Smijulj <adrian1358@gmail.com>
2020-07-24 14:47:24 +0200

    chore: introduce "setup-cypress" script

---
+++ scripts/setupCypress.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const loadJson = require("load-json-file");
+const writeJson = require("write-json-file");
+const fs = require("fs");
+const { green } = require("chalk");
+
+const PROJECT_FOLDER = ".";
+
+(async () => {
+    const cypressExampleConfigPath = path.resolve(PROJECT_FOLDER, "example.cypress.json");
+    const cypressConfigPath = path.resolve(PROJECT_FOLDER, "cypress.json");
+    if (fs.existsSync(cypressConfigPath)) {
+        console.log(`⚠️  ${green("cypress.json")} already exists, skipping.`);
+        return;
+    } else {
+        fs.copyFileSync(cypressExampleConfigPath, cypressConfigPath);
+    }
+
+    const cypressConfig = await loadJson.sync(cypressConfigPath);
+
+    // TODO: parametrize this.
+    const ENV = "dev";
+    const apiStatePath = path.resolve(
+        PROJECT_FOLDER,
+        path.join(".webiny", "state", "api", ENV, "Webiny.json")
+    );
+    const apiStateFile = await loadJson.sync(apiStatePath);
+
+    const appsStatePath = path.resolve(
+        PROJECT_FOLDER,
+        path.join(".webiny", "state", "apps", ENV, "Webiny.json")
+    );
+    const appsStateFile = await loadJson.sync(appsStatePath);
+
+    cypressConfig.baseUrl = appsStateFile.outputs.cdn.url;
+    cypressConfig.env.SITE_URL = appsStateFile.outputs.cdn.url;
+    cypressConfig.env.ADMIN_URL = appsStateFile.outputs.cdn.url + "/admin";
+    cypressConfig.env.API_URL = apiStateFile.outputs.cdn.url;
+    cypressConfig.env.GRAPHQL_API_URL = apiStateFile.outputs.cdn.url + "/graphql";
+
+    cypressConfig.env.AWS_COGNITO_USER_POOL_ID = apiStateFile.outputs.cognito.userPool.Id;
+    cypressConfig.env.AWS_COGNITO_CLIENT_ID = apiStateFile.outputs.cognito.appClients[0].ClientId;
+
+    await writeJson(cypressConfigPath, cypressConfig);
+
+    console.log(
+        `✅️ Created cypress.json config file! To open Cypress, just run "cypress open" in your terminal.`
+    );
+})();
