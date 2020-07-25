4d0ba9bb9f1f38430a9cee5c37eced01b9d2642f
Adrian Smijulj <adrian1358@gmail.com>
2020-07-25 09:56:26 +0200

    fix: remove default authorization plugin

--- packages/api-security/src/plugins/authentication.ts
+++ packages/api-security/src/plugins/authentication.ts
@@ -1,6 +1,5 @@
-import { SecurityAuthenticationPlugin, SecurityAuthorizationPlugin } from "../types";
+import { SecurityAuthenticationPlugin } from "../types";
 import { ContextPlugin } from "@webiny/graphql/types";
-import { SecurityIdentity } from "@webiny/api-security/utils";
 
 export default () => [
     {
@@ -20,20 +19,11 @@ export default () => [
 
             for (let i = 0; i < securityPlugins.length; i++) {
                 const identity = await securityPlugins[i].authenticate(context);
-                if (identity instanceof SecurityIdentity) {
+                if (identity) {
                     context.security.identity = identity;
                     return;
                 }
             }
         }
-    } as ContextPlugin,
-    // We want to provide the default "authorization" plugin, which just checks scopes in the "SecurityIdentity" instance.
-    // Feel free to create your own if needed, but this one will probably suffice for a lot of cases.
-    {
-        type: "authorization",
-        name: "authorization-identity-scopes",
-        hasScope: ({ context, scope }) => {
-            return context.security.getIdentity() && context.security.getIdentity().hasScope(scope);
-        }
-    } as SecurityAuthorizationPlugin
+    } as ContextPlugin
 ];
