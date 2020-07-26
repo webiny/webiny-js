d2c3acac1dc3cb16367cb4ab61c2ba2031fdda71
Tommy Jackson <tom.jackson50@gmail.com>
2020-07-26 16:45:31 +0100

    fix: removed space from advanced settings file

--- packages/app-page-builder/src/editor/plugins/elementSettings/advanced/AdvancedSettings.tsx
+++ packages/app-page-builder/src/editor/plugins/elementSettings/advanced/AdvancedSettings.tsx
@@ -66,7 +66,7 @@ const AdvancedSettings = props => {
     useEffect(() => {
         open ? addKeyHandler("escape", closeDialog) : removeKeyHandler("escape");
     });
-    
+
     return (
         <Dialog
             open={open}
