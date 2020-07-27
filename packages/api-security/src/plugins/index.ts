d6659c5a52927dde3d08796749c0e95b55b2af7e
Adrian Smijulj <adrian1358@gmail.com>
2020-07-27 19:47:53 +0200

    fix: correct import path

--- packages/api-security/src/plugins/index.ts
+++ packages/api-security/src/plugins/index.ts
@@ -1,13 +1,13 @@
 import models from "./models";
 import graphql from "./graphql";
-import securityAuthenticationPlugins from "@webiny/api-security/plugins/authentication";
+import securityAuthPlugins from "@webiny/api-security/plugins/auth";
 import securityAuthJwtPlugins, { JwtAuthOptions } from "@webiny/api-security/plugins/auth/jwt";
 import securityAuthPatPlugins from "@webiny/api-security/plugins/auth/pat";
 
 export default (options: JwtAuthOptions) => [
     models(),
     graphql,
-    securityAuthenticationPlugins(),
+    securityAuthPlugins(),
     securityAuthJwtPlugins(options),
     securityAuthPatPlugins()
 ];
