54041b00564e4deac847dbdb3b479e6b39e4ffe4
Adrian Smijulj <adrian1358@gmail.com>
2020-07-28 12:08:31 +0200

    fix: only register "models" and "graphql" plugins

--- packages/api-security/src/plugins/index.ts
+++ packages/api-security/src/plugins/index.ts
@@ -1,13 +1,4 @@
 import models from "./models";
 import graphql from "./graphql";
-import securityAuthPlugins from "@webiny/api-security/plugins/auth";
-import securityAuthJwtPlugins, { JwtAuthOptions } from "@webiny/api-security/plugins/auth/jwt";
-import securityAuthPatPlugins from "@webiny/api-security/plugins/auth/pat";
 
-export default (options: JwtAuthOptions) => [
-    models(),
-    graphql,
-    securityAuthPlugins(),
-    securityAuthJwtPlugins(options),
-    securityAuthPatPlugins()
-];
+export default () => [models(), graphql];
