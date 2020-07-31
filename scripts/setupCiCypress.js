3a28e666d95d39563fd004a378d354d915659585
Adrian Smijulj <adrian1358@gmail.com>
2020-07-31 12:01:43 +0200

    ci: apply "skipLibCheck" to the TS config

--- scripts/setupCiCypress.js
+++ scripts/setupCiCypress.js
@@ -14,5 +14,11 @@ const PROJECT_FOLDER = ".";
     envJson.default.MONGODB_SERVER = process.env.MONGODB_SERVER;
     await writeJson(envJsonPath, envJson);
 
+    // Add "skipLibCheck" param to root tsconfig.build.file so the build happens faster.
+    const tsConfigJsonPath = path.resolve(PROJECT_FOLDER, "tsconfig.build.json");
+    const tsConfigJson = await loadJson.sync(tsConfigJsonPath);
+    tsConfigJson.compilerOptions.skipLibCheck = true;
+    await writeJson(tsConfigJsonPath, tsConfigJson);
+
     console.log(`✅️ Applied changes needed for running Webiny in CI.`);
 })();
