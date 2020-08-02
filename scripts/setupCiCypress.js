12bf97a40c52518c0983746a0f95c877b4025f42
Adrian Smijulj <adrian1358@gmail.com>
2020-08-02 18:08:48 +0200

    ci: add post-run MongoDB database removal

--- scripts/setupCiCypress.js
+++ scripts/setupCiCypress.js
@@ -1,14 +1,41 @@
 const path = require("path");
 const loadJson = require("load-json-file");
 const writeJson = require("write-json-file");
+const { argv } = require("yargs");
+const { MongoClient } = require("mongodb");
+
+let db;
+async function getDatabase(name) {
+    if (db) {
+        return db;
+    }
+
+    const client = await MongoClient.connect(
+        "mongodb+srv://adrian:YDzr8E1bWDAHC7aF@adriantest-kvgdz.mongodb.net/test?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+
+    return client.db(name);
+}
 
 const PROJECT_FOLDER = ".";
 
 (async () => {
-    require("./setupEnvFiles");
-
     const envJsonPath = path.resolve(PROJECT_FOLDER, ".env.json");
     const envJson = await loadJson.sync(envJsonPath);
+
+    if (argv.postRun) {
+        const db = await getDatabase(envJson.default.MONGODB_NAME);
+        await db.dropDatabase();
+        console.log(`✅️ Dropped "${envJson.default.MONGODB_NAME}" MongoDB database.`);
+        process.exit(0);
+    }
+
+    require("./setupEnvFiles");
+
     envJson.default.AWS_REGION = "eu-central-1";
     envJson.default.MONGODB_NAME = "webiny-cy-test-" + new Date().getTime();
     envJson.default.MONGODB_SERVER = process.env.MONGODB_SERVER;
