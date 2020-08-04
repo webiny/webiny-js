68b457791e5cf6b8aa6ac0eec51f983b5dd8fbfc
bhavesh <bhavesh.shrimali@empower-solutions.co.in>
2020-08-04 15:17:57 +0530

    fix: renamed basic to admin

--- packages/cwp-template-admin/template/webiny.root.js
+++ packages/cwp-template-admin/template/webiny.root.js
@@ -4,8 +4,8 @@ module.exports = {
     cli: {
         plugins: [
             require("@webiny/cli-plugin-deploy-components")(),
-            require("@webiny/cwp-template-basic/hooks/api")(),
-            require("@webiny/cwp-template-basic/hooks/apps")(),
+            require("@webiny/cwp-template-admin/hooks/api")(),
+            require("@webiny/cwp-template-admin/hooks/apps")(),
             require("@webiny/cli-plugin-scaffold"),
             require("@webiny/cli-plugin-scaffold-graphql-service"),
             require("@webiny/cli-plugin-scaffold-lambda")
