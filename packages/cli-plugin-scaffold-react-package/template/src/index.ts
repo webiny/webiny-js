c73ac7112249339e0f23576f7510af68cea8dad1
Adrian Smijulj <adrian1358@gmail.com>
2020-08-10 20:01:49 +0200

    fix: correct paths and updating of root package.json's "workspaces" property

--- packages/cli-plugin-scaffold-react-package/template/src/index.ts
+++
@@ -1,3 +0,0 @@
-export default () => {
-    // TODO: implement your package
-};
---
+++ packages/cli-plugin-scaffold-react-package/template/src/index.tsx
@@ -0,0 +1,6 @@
+/**
+ * Your code goes here.
+ * Reference it from other packages via the following import statement:
+ * import { NewComponent } from "PACKAGE_NAME";
+ */
+export const NewComponent = function() {};
