64eac14ce6a6dcc855ebf0eaf8095021fa053536
Adrian Smijulj <adrian1358@gmail.com>
2020-08-10 19:31:37 +0200

    fix: make initial index.ts nicer

--- packages/cli-plugin-scaffold-node-package/template/src/index.ts
+++ packages/cli-plugin-scaffold-node-package/template/src/index.ts
@@ -1,3 +1,6 @@
-export default () => {
-    // TODO: implement your package
-};
+/**
+ * Your code goes here.
+ * Reference it from other packages via the following import statement:
+ * import { PACKAGE_NAME_CAMEL_CASED } from "PACKAGE_NAME";
+ */
+export const PACKAGE_NAME_CAMEL_CASED = function() {};
