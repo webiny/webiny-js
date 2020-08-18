2fb1c6827fad5cc2a4360bd72ceaef19aaa75488
Adrian Smijulj <adrian1358@gmail.com>
2020-08-18 12:09:57 +0200

    fix(cli-plugin-scaffold-lambda): improve messaging

--- packages/cli-plugin-scaffold-lambda/index.js
+++ packages/cli-plugin-scaffold-lambda/index.js
@@ -5,6 +5,8 @@ const ncp = util.promisify(require("ncp").ncp);
 const findUp = require("find-up");
 const camelCase = require("lodash.camelcase");
 const kebabCase = require("lodash.kebabcase");
+const { green } = require("chalk");
+const indentString = require("indent-string");
 
 module.exports = [
     {
@@ -38,7 +40,7 @@ module.exports = [
                     }
                 ];
             },
-            generate: async ({ input }) => {
+            generate: async ({ input, wait, oraSpinner }) => {
                 const { location } = input;
                 const fullLocation = path.resolve(location);
                 const rootResourcesPath = findUp.sync("resources.js", {
@@ -58,6 +60,9 @@ module.exports = [
                 if (fs.existsSync(location)) {
                     throw new Error(`Package ${packageName} already exists!`);
                 }
+
+                oraSpinner.start(`Creating Lambda function files in ${green(fullLocation)}...`);
+                await wait();
 
                 await fs.mkdirSync(location, { recursive: true });
 
@@ -80,7 +85,17 @@ module.exports = [
                 packageJson = packageJson.replace("[PACKAGE_NAME]", packageName);
                 fs.writeFileSync(packageJsonPath, packageJson);
 
+                oraSpinner.stopAndPersist({
+                    symbol: green("✔"),
+                    text: `Lambda function files created in ${green(fullLocation)}.`
+                });
+
                 // Inject resource into closest resources.js
+                oraSpinner.start(
+                    `Adding ${green(resourceName)} resource in ${green(rootResourcesPath)}...`
+                );
+                await wait();
+
                 const { transform } = require("@babel/core");
                 const source = fs.readFileSync(rootResourcesPath, "utf8");
                 let resourceTpl = fs.readFileSync(path.join(__dirname, "resource.tpl"), "utf8");
@@ -88,6 +103,11 @@ module.exports = [
 
                 const { code } = await transform(source, {
                     plugins: [[__dirname + "/transform", { template: resourceTpl, resourceName }]]
+                });
+
+                oraSpinner.stopAndPersist({
+                    symbol: green("✔"),
+                    text: `Resource ${green(resourceName)} added in ${green(rootResourcesPath)}.`
                 });
 
                 // Update tsconfig "extends" path
@@ -102,9 +122,49 @@ module.exports = [
                 const formattedCode = prettier.format(code, { ...prettierConfig, parser: "babel" });
 
                 fs.writeFileSync(rootResourcesPath, formattedCode);
+            },
+            onSuccess({ input }) {
+                const { location } = input;
+                const fullLocation = path.resolve(location);
+                const indexTsLocation = path.join(fullLocation, "/src/index.ts");
+                const projectRootPath = path.dirname(
+                    findUp.sync("webiny.root.js", {
+                        cwd: fullLocation
+                    })
+                );
 
+                const rootResourcesPath = findUp.sync("resources.js", {
+                    cwd: fullLocation
+                });
+
+                const rootResourcesDir = path.dirname(rootResourcesPath);
+
+                const deployCommandStackPath = rootResourcesDir
+                    .replace(projectRootPath, "")
+                    .replace("/", "");
+
+                console.log(`The next steps:`);
+                console.log(indentString(`1. Put your code in ${green(indexTsLocation)}.`, 2));
                 console.log(
-                    `\nIMPORTANT: make sure you add an endpoint in your API Gateway resource to point to this new Lambda!\n\n`
+                    indentString(
+                        `2. Open ${green(
+                            rootResourcesPath
+                        )} and in your API Gateway resource, define a new endpoint over which your new Lambda function will be invoked.`,
+                        2
+                    )
+                );
+                console.log(
+                    indentString(
+                        `3. Finally, deploy the ${green(
+                            deployCommandStackPath
+                        )} stack by running ${green(
+                            `webiny deploy ${deployCommandStackPath} --env local`
+                        )}.`,
+                        2
+                    )
+                );
+                console.log(
+                    `Learn more about API development at https://docs.webiny.com/docs/api-development/introduction`
                 );
             }
         }
