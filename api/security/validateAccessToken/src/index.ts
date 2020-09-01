7b4177a1a63299d723cf142f815831a0c0b73fdd
Ashutosh <bhardwajashu96@hotmail.com>
2020-09-01 18:22:56 +0530

    fix: update import in `validateAccessToken`

--- api/security/validateAccessToken/src/index.ts
+++ api/security/validateAccessToken/src/index.ts
@@ -1,8 +1,9 @@
 import { createHandler } from "@webiny/handler";
-import validateAccessToken from "@webiny/api-security/validateAccessToken";
+import validateAccessToken from "@webiny/api-security-user-management/personalAccessToken/validator";
 import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
 
 export const handler = createHandler(
     dbProxy({ functionName: process.env.DB_PROXY_FUNCTION }),
+    // TODO: We'll revisit this later
     validateAccessToken()
 );
