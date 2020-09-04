408852ee9966e9cf7891f648392a6026f4cbc4cb
Tommy Jackson <tom.jackson50@gmail.com>
2020-09-04 19:24:36 +0100

    feat: removed original new package app from root directory packages

--- packages/app-plugin-page-builder-menu-element/src/index.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Your code goes here.
- * Reference it from other packages via the following import statement:
- * import { NewComponent } from "app-plugin-page-builder-menu-element";
- */
-export const NewComponent = function() {};
