8c65b9920fcfab07e980ee541486e33c49f84281
Adrian Smijulj <adrian1358@gmail.com>
2020-09-08 20:27:29 +0200

    fix: add package build step

--- packages/cli-plugin-scaffold-node-package/index.js
+++ packages/cli-plugin-scaffold-node-package/index.js
@@ -71,7 +71,6 @@ module.exports = [
                     throw new Error(`Destination folder ${location} already exists!`);
                 }
 
-
                 oraSpinner.start(`Creating Node package files in ${green(fullLocation)}...`);
                 await wait();
 
@@ -159,7 +158,9 @@ module.exports = [
                 });
 
                 // Update root package.json - update "workspaces.packages" section.
-                oraSpinner.start(`Adding ${green(input.location)} workspace in root ${green(`package.json`)}..`);
+                oraSpinner.start(
+                    `Adding ${green(input.location)} workspace in root ${green(`package.json`)}..`
+                );
                 await wait();
 
                 const rootPackageJsonPath = path.join(projectRootPath, "package.json");
@@ -171,7 +172,9 @@ module.exports = [
 
                 oraSpinner.stopAndPersist({
                     symbol: green("✔"),
-                    text: `Workspace ${green(input.location)} added in root ${green(`package.json`)}.`
+                    text: `Workspace ${green(input.location)} added in root ${green(
+                        `package.json`
+                    )}.`
                 });
 
                 // Once everything is done, run `yarn` so the new packages are automatically installed.
@@ -181,6 +184,13 @@ module.exports = [
                     oraSpinner.stopAndPersist({
                         symbol: green("✔"),
                         text: "Dependencies installed."
+                    });
+
+                    oraSpinner.start(`Building package...`);
+                    await execa("yarn", ["build"], { cwd: fullLocation });
+                    oraSpinner.stopAndPersist({
+                        symbol: green("✔"),
+                        text: "Package built."
                     });
                 } catch (err) {
                     throw new Error(
