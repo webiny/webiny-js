6db3931f534b6a0a32a6e76f0d5fba1e10bf914f
Adrian Smijulj <adrian1358@gmail.com>
2020-09-09 06:10:52 +0200

    feat: improve created file's initial content

--- packages/cli-plugin-scaffold-lambda/template/src/index.ts
+++ packages/cli-plugin-scaffold-lambda/template/src/index.ts
@@ -1,3 +1,16 @@
-export const handler = async event => {
-    return "Hello from Webiny!";
+/**
+ * Implement your function here. To make it accessible on the internet, add the following entry to the
+ * "api.inputs.endpoints" array, in the "/api/resources.js" file:
+ *
+ * {
+ *   path: "/some-route",
+ *   method: "ANY",
+ *   function: "${FUNCTION_NAME.arn}",
+ * }
+ */
+export const handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello from my new function!", event, context })
+    };
 };
