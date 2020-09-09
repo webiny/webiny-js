d5dc43a227a0c402a5726c662af5881fcf4cfdc2
Adrian Smijulj <adrian1358@gmail.com>
2020-09-09 11:59:51 +0200

    test(file-manager): add dialogs visibility check

---
+++ cypress/integration/admin/fileManager/dialogsVisibility.spec.js
@@ -0,0 +1,35 @@
+context("Account Module", () => {
+    beforeEach(() => cy.login());
+
+    it("should be able to correctly show image editor and delete image dialogs", () => {
+        cy.visit("/account")
+            .findByText("Select an image")
+            .click();
+
+        cy.findByTestId("fm-list-wrapper").dropFile("sample.jpeg", "image/jpeg");
+        cy.findByText("File upload complete.").should("exist");
+
+        cy.findByTestId("fm-list-wrapper").within(() => {
+            cy.findAllByTestId("fm-list-wrapper-file")
+                .first()
+                .within(() => {
+                    cy.findByTestId("fm-file-wrapper-file-info-icon").click({ force: true });
+                });
+        });
+
+        cy.findByTestId("fm-edit-image-button").click();
+        cy.findByTestId("fm-image-editor-dialog")
+            .should("be.visible")
+            .within(() => {
+                cy.findByText("Cancel").click();
+            });
+
+        cy.findByTestId("fm-delete-file-button").click();
+
+        cy.findByTestId("fm-delete-file-confirmation-dialog").within(() => {
+            cy.findByText("Confirm").click();
+        });
+
+        cy.findByText("File deleted successfully.");
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,2 +1,3 @@
 import "./login";
+import "./dropFile";
 import "./reloadUntil";
