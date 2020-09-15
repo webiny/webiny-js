380d818496b547f20ddabb293ee30bdbe7bcadf5
Bruno Zorić <bruno.zoric@gmail.com>
2020-09-15 11:33:53 +0200

    refactor: remove eslint disable and return something

--- packages/cli-plugin-scaffold-node-package/template/src/index.ts
+++ packages/cli-plugin-scaffold-node-package/template/src/index.ts
@@ -3,5 +3,6 @@
  * Reference it from other packages via the following import statement:
  * import { PACKAGE_NAME_CAMEL_CASED } from "PACKAGE_NAME";
  */
-/* eslint-disable @typescript-eslint/no-empty-function */
-export const PACKAGE_NAME_CAMEL_CASED = function() {};
+export const PACKAGE_NAME_CAMEL_CASED = function() {
+    return Math.random();
+};
--- packages/cli-plugin-scaffold-react-package/template/src/index.tsx
+++ packages/cli-plugin-scaffold-react-package/template/src/index.tsx
@@ -3,5 +3,6 @@
  * Reference it from other packages via the following import statement:
  * import { NewComponent } from "PACKAGE_NAME";
  */
-/* eslint-disable @typescript-eslint/no-empty-function */
-export const NewComponent = function() {};
+export const NewComponent = function() {
+    return Math.random();
+};
