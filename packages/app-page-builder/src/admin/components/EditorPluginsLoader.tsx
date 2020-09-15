789c7497c70e8d891fba878aa42fa539f78cefb0
Adrian Smijulj <adrian1358@gmail.com>
2020-09-15 19:00:49 +0200

    fix: use the newly introduced `skipExisting` plugin registration option

--- packages/app-page-builder/src/admin/components/EditorPluginsLoader.tsx
+++ packages/app-page-builder/src/admin/components/EditorPluginsLoader.tsx
@@ -4,6 +4,9 @@ import { CircularProgress } from "@webiny/ui/Progress";
 
 const globalState = { render: false, editor: false };
 
+// Since these plugins are loaded asynchronously, and some overrides might've been registered
+// already by the developer (e.g. in the main App.tsx file), we only register new plugins.
+// In other words, if the plugin with a particular name already exists, we skip its registration.
 export function EditorPluginsLoader({ children, location }) {
     const [loaded, setLoaded] = useReducer(
         (state, newState) => ({ ...state, ...newState }),
@@ -14,7 +17,9 @@ export function EditorPluginsLoader({ children, location }) {
         // If we are on pages list route, import plugins required to render the page content.
         if (location.pathname.startsWith("/page-builder/pages") && !loaded.render) {
             const renderPlugins = await import("@webiny/app-page-builder/render/presets/default");
-            registerPlugins(renderPlugins.default());
+
+            // "skipExisting" will ensure existing plugins (with the same name) are not overridden.
+            registerPlugins(renderPlugins.default(), { skipExisting: true });
 
             globalState.render = true;
             setLoaded({ render: true });
@@ -30,7 +35,12 @@ export function EditorPluginsLoader({ children, location }) {
                         : null
                 ].filter(Boolean)
             );
-            registerPlugins(plugins.map(p => p.default()));
+
+            // "skipExisting" will ensure existing plugins (with the same name) are not overridden.
+            registerPlugins(
+                plugins.map(p => p.default()),
+                { skipExisting: true }
+            );
 
             globalState.editor = true;
             globalState.render = true;
