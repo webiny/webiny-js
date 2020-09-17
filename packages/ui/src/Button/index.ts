63a691b9cc1e219addf13fe230105bb3bddbe764
Adrian Smijulj <adrian1358@gmail.com>
2020-09-17 10:33:16 +0200

    fix: export using `*`, instead of doing named exports

--- packages/ui/src/Button/index.ts
+++ packages/ui/src/Button/index.ts
@@ -1,12 +1,3 @@
-export {
-    ButtonProps,
-    ButtonFloatingProps,
-    ButtonDefault,
-    ButtonPrimary,
-    ButtonSecondary,
-    ButtonFloating,
-    ButtonIcon
-} from "./Button";
-
+export * from "./Button";
 export * from "./IconButton/IconButton";
 export * from "./CopyButton/CopyButton";
