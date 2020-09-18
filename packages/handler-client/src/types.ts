32fccfc5f7e5126987f67800567d1e3c7e26300c
Adrian Smijulj <adrian1358@gmail.com>
2020-09-18 13:10:56 +0200

    fix: `InvokeResult` must be `Promise`

--- packages/handler-client/src/types.ts
+++ packages/handler-client/src/types.ts
@@ -1,4 +1,5 @@
 import HandlerClient from "./HandlerClient";
+import { Plugin } from "@webiny/plugins/types";
 
 export type InvokeParams = { name: string; payload?: { [key: string]: any }; await?: boolean };
 export type InvokeResult = { [key: string]: any };
@@ -6,7 +7,7 @@ export type InvokeResult = { [key: string]: any };
 export type HandlerClientPlugin = Plugin & {
     type: "handler-client";
     name: "handler-client";
-    invoke: (params: InvokeParams) => InvokeResult;
+    invoke: (params: InvokeParams) => Promise<InvokeResult>;
 };
 
 export type HandlerClientContext = {
