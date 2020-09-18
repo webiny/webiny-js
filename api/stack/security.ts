0fb1fbb9b9a9e818cf810bb13eaa618b6e72cd45
Adrian Smijulj <adrian1358@gmail.com>
2020-09-18 08:38:19 +0200

    fix: add permissions manager

--- api/stack/security.ts
+++ api/stack/security.ts
@@ -2,7 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 class Security {
-    functions: { graphql: aws.lambda.Function; validateAccessToken: aws.lambda.Function };
+    functions: {
+        graphql: aws.lambda.Function;
+        validateAccessToken: aws.lambda.Function;
+        permissionsManager: aws.lambda.Function;
+    };
     constructor({
         role,
         dbProxy,
@@ -12,6 +16,23 @@ class Security {
         dbProxy: aws.lambda.Function;
         env: { [key: string]: any };
     }) {
+        const permissionsManager = new aws.lambda.Function("security-permissions-manager", {
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            role: role.arn,
+            timeout: 30,
+            memorySize: 512,
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("./code/security/permissionsManager/build")
+            }),
+            environment: {
+                variables: {
+                    DB_PROXY_FUNCTION: dbProxy.arn,
+                    DEBUG: String(process.env.DEBUG)
+                }
+            }
+        });
+
         const validateAccessToken = new aws.lambda.Function("security-validate-access-token", {
             runtime: "nodejs12.x",
             handler: "handler.handler",
@@ -24,6 +45,7 @@ class Security {
             environment: {
                 variables: {
                     DB_PROXY_FUNCTION: dbProxy.arn,
+                    PERMISSIONS_MANAGER_FUNCTION: permissionsManager.arn,
                     DEBUG: String(process.env.DEBUG)
                 }
             }
@@ -41,14 +63,16 @@ class Security {
             environment: {
                 variables: {
                     ...env,
-                    VALIDATE_ACCESS_TOKEN_FUNCTION: validateAccessToken.arn
+                    VALIDATE_ACCESS_TOKEN_FUNCTION: validateAccessToken.arn,
+                    PERMISSIONS_MANAGER_FUNCTION: permissionsManager.arn
                 }
             }
         });
 
         this.functions = {
             validateAccessToken,
-            graphql
+            graphql,
+            permissionsManager
         };
     }
 }
