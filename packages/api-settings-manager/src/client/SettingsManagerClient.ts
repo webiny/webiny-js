0a8c4d5cd425f533ff6edf5769f86827f4b7ca36
Adrian Smijulj <adrian1358@gmail.com>
2020-09-20 14:14:11 +0200

    fix: remove direct aws-sdk/Lambda client usage

--- packages/api-settings-manager/src/client/SettingsManagerClient.ts
+++ packages/api-settings-manager/src/client/SettingsManagerClient.ts
@@ -1,5 +1,6 @@
-import LambdaClient from "aws-sdk/clients/lambda";
 import { Action } from "../types";
+import { HandlerClientContext } from "@webiny/handler-client/types";
+import { HandlerContext } from "@webiny/handler/types";
 
 interface SettingsManagerOperation {
     action: Action;
@@ -28,11 +29,12 @@ class SettingsManagerClientCache {
 
 export class SettingsManagerClient {
     settingsManagerFunction: string;
-    lambda: LambdaClient;
     cache: SettingsManagerClientCache;
-    constructor({ functionName }) {
+    context: HandlerContext & HandlerClientContext;
+    constructor({ context, functionName }) {
         this.settingsManagerFunction = functionName;
         this.cache = new SettingsManagerClientCache();
+        this.context = context;
     }
 
     private async invoke(operation: SettingsManagerOperation) {
@@ -41,18 +43,10 @@ export class SettingsManagerClient {
             return cached;
         }
 
-        if (!this.lambda) {
-            this.lambda = new LambdaClient({ region: process.env.AWS_REGION });
-        }
-
-        const { Payload } = await this.lambda
-            .invoke({
-                FunctionName: this.settingsManagerFunction,
-                Payload: JSON.stringify(operation)
-            })
-            .promise();
-
-        const { error, data } = JSON.parse(Payload as string);
+        const { error, data } = await this.context.handlerClient.invoke({
+            name: this.settingsManagerFunction,
+            payload: operation
+        })
 
         if (error) {
             throw Error(error);
