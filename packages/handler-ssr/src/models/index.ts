e04e8233ed3d8ae49f60a7e0ba6c728df00a767a
Adrian Smijulj <adrian1358@gmail.com>
2020-09-22 13:23:46 +0200

    fix: add Context types

--- packages/handler-ssr/src/models/index.ts
+++ packages/handler-ssr/src/models/index.ts
@@ -2,13 +2,13 @@ import { withStorage } from "@webiny/commodo/fields-storage";
 import { pipe } from "@webiny/commodo/pipe";
 import ssrCache from "./ssrCache.model";
 import { withId, DbProxyDriver } from "@webiny/commodo-fields-storage-db-proxy";
-import { HandlerContext, HandlerContextPlugin } from "@webiny/handler/types";
+import { HandlerContextPlugin } from "@webiny/handler/types";
 import { HandlerClientContext } from "@webiny/handler-client/types";
 
-export default (options): HandlerContextPlugin => ({
+export default (options): HandlerContextPlugin<HandlerClientContext> => ({
     type: "context",
     name: "context-models",
-    apply(context: HandlerContext & HandlerClientContext) {
+    apply(context) {
         const createBase = () =>
             pipe(
                 withId(),
