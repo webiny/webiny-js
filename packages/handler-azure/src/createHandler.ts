b167b68c5e1e5b8bfc0edd08a459d9bbbcee5665
Adrian Smijulj <adrian1358@gmail.com>
2020-09-22 13:13:03 +0200

    wip: remove package

--- packages/handler-azure/src/createHandler.ts
+++
@@ -1,5 +0,0 @@
-import handlerClient from "./plugins/handlerClient";
-import handlerHttp from "./plugins/handlerHttp";
-import handlerArgs from "./plugins/handlerArgs";
-
-export default () => [handlerClient, handlerArgs, handlerHttp];
--- packages/handler-azure/src/index.ts
+++
@@ -1,8 +0,0 @@
-import handlerClient from "./plugins/handlerClient";
-import handlerHttp from "./plugins/handlerHttp";
-import handlerArgs from "./plugins/handlerArgs";
-import { createHandler as createDefaultHandler } from "@webiny/handler";
-
-export const createHandler = (...plugins) => {
-    return createDefaultHandler(handlerClient, handlerArgs, handlerHttp, ...plugins);
-};
