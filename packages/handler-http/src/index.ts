72db54df873e6f74953c99d47a805641873c399a
Adrian Smijulj <adrian1358@gmail.com>
2020-09-22 13:18:02 +0200

    feat: add error handler

--- packages/handler-http/src/index.ts
+++ packages/handler-http/src/index.ts
@@ -1,8 +1,31 @@
 import { HandlerHttpContext } from "@webiny/handler-http/types";
+import { HandlerContextPlugin, HandlerErrorPlugin } from "@webiny/handler/types";
 
-export default () => ({
-    type: "context",
-    apply(context: HandlerHttpContext) {
-        context.http = null;
-    }
-});
+export default () => [
+    {
+        type: "context",
+        apply(context) {
+            context.http = null;
+        }
+    } as HandlerContextPlugin<HandlerHttpContext>,
+    {
+        type: "handler-error",
+        handle: (context, error) => {
+            if (!context.http || typeof context.http.response !== "function") {
+                return error;
+            }
+
+            return context.http.response({
+                statusCode: 500,
+                body: JSON.stringify({
+                    error: {
+                        name: error.constructor.name,
+                        message: error.message,
+                        stack: error.stack
+                    }
+                }),
+                headers: { "Cache-Control": "no-store", "Content-Type": "application/json" }
+            });
+        }
+    } as HandlerErrorPlugin<HandlerHttpContext>
+];
