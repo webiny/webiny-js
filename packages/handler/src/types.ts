ef16e56d90086f91c7ec710ebc428b122ed3752e
Adrian Smijulj <adrian1358@gmail.com>
2020-09-22 13:20:09 +0200

    feat: enable easier context specifying

--- packages/handler/src/types.ts
+++ packages/handler/src/types.ts
@@ -8,20 +8,54 @@ export type HandlerContext = {
     [key: string]: any;
 };
 
-export type HandlerContextPlugin = Plugin & {
+export type HandlerContextPlugin<
+    C0 = HandlerContext,
+    C1 = HandlerContext,
+    C2 = HandlerContext,
+    C3 = HandlerContext,
+    C4 = HandlerContext,
+    C5 = HandlerContext,
+    C6 = HandlerContext,
+    C7 = HandlerContext,
+    C8 = HandlerContext,
+    C9 = HandlerContext
+> = Plugin & {
     type: "context";
-    apply(context: HandlerContext): void;
+    apply(context: C0 & C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8 & C9): void;
 };
 
-export type HandlerPlugin = Plugin & {
+export type HandlerPlugin<
+    C0 = HandlerContext,
+    C1 = HandlerContext,
+    C2 = HandlerContext,
+    C3 = HandlerContext,
+    C4 = HandlerContext,
+    C5 = HandlerContext,
+    C6 = HandlerContext,
+    C7 = HandlerContext,
+    C8 = HandlerContext,
+    C9 = HandlerContext
+> = Plugin & {
     type: "handler";
-    handle(context: HandlerContext, next: Function): Promise<any>;
+    handle(context: C0 & C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8 & C9, next: Function): Promise<any>;
 };
 
-export type ErrorHandlerPlugin = Plugin & {
-    type: "error-handler";
+export type HandlerErrorPlugin<
+    C0 = HandlerContext,
+    C1 = HandlerContext,
+    C2 = HandlerContext,
+    C3 = HandlerContext,
+    C4 = HandlerContext,
+    C5 = HandlerContext,
+    C6 = HandlerContext,
+    C7 = HandlerContext,
+    C8 = HandlerContext,
+    C9 = HandlerContext
+> = Plugin & {
+    type: "handler-error";
     handle(
-        context: HandlerContext,
+        context: C0 & C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8 & C9,
+        error: any,
         next: Function
     ): Promise<any>;
 };
