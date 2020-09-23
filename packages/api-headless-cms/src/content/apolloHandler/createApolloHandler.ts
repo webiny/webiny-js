3d50b9d14e4171d4f205968c8fdde1f2a7e66955
Adrian Smijulj <adrian1358@gmail.com>
2020-09-23 14:14:09 +0200

    fix: optimize `handler-apollo-gateway` - remove redundant code

--- packages/api-headless-cms/src/content/apolloHandler/createApolloHandler.ts
+++ packages/api-headless-cms/src/content/apolloHandler/createApolloHandler.ts
@@ -1,17 +1,10 @@
-import { ApolloServer } from "apollo-server-lambda";
-import { boolean } from "boolean";
 import { CreateSchemaPlugin } from "@webiny/handler-apollo-server/types";
 import { get } from "lodash";
 import { HandlerContext } from "@webiny/handler/types";
-
-function normalizeEvent(event) {
-    // In AWS, when enabling binary support, received body gets base64 encoded. Did not find a way to solve this
-    // correctly, so for now we "normalize" the event before passing it to the handler. It would be nice if
-    // we could resolve this issue better / smarter in the future (configure integrations correctly?).
-    if (event.isBase64Encoded) {
-        event.body = Buffer.from(event.body, "base64").toString("utf-8");
-    }
-}
+import { runHttpQuery } from "apollo-server-core/dist/runHttpQuery";
+import { generateSchemaHash } from "apollo-server-core/dist/utils/schemaHash";
+import { Headers } from "apollo-server-env";
+import buildCorsHeaders from "./buildCorsHeaders";
 
 type CreateApolloHandlerParams = {
     context: HandlerContext;
@@ -21,11 +14,8 @@ type CreateApolloHandlerParams = {
 
 export default async function createApolloHandler({
     context,
-    options,
     meta = null
 }: CreateApolloHandlerParams) {
-    const { server = {}, handler = {} } = options;
-
     const createSchemaPlugin = context.plugins.byName<CreateSchemaPlugin>(
         "handler-apollo-server-create-schema"
     );
@@ -34,26 +24,10 @@ export default async function createApolloHandler({
         throw Error(`"handler-apollo-server-create-schema" plugin is not configured!`);
     }
 
+    const { http } = context;
+
     const { schema } = await createSchemaPlugin.create(context);
-
-    const apolloServer = new ApolloServer({
-        // @ts-ignore Not sure why it doesn't work, "boolean" function does return a boolean value.
-        introspection: boolean(server.introspection),
-        // @ts-ignore Not sure why it doesn't work, "boolean" function does return a boolean value.
-        playground: boolean(server.playground),
-        debug: boolean(process.env.DEBUG),
-        ...server,
-        schema,
-        context: ({ context }) => context
-    });
-
-    const apolloHandler = apolloServer.createHandler({
-        cors: {
-            origin: "*",
-            methods: "GET,HEAD,POST",
-            ...(handler.cors || {})
-        }
-    });
+    const schemaHash = generateSchemaHash(schema);
 
     const output = {
         meta,
@@ -61,18 +35,13 @@ export default async function createApolloHandler({
             // Invoke handler and check if environment / environment alias has changed.
             const response = await output.handler(
                 {
-                    isMetaQuery: true,
-                    httpMethod: "POST",
-                    headers: {},
-                    body: JSON.stringify({
-                        query: /* GraphQL */ `
-                            {
-                                getMeta {
-                                    cacheKey
-                                }
+                    query: /* GraphQL */ `
+                        {
+                            getMeta {
+                                cacheKey
                             }
-                        `
-                    })
+                        }
+                    `
                 },
                 context
             );
@@ -81,16 +50,23 @@ export default async function createApolloHandler({
             return get(body, "data.getMeta") || {};
         },
         schema,
-        handler: (event, context): any => {
-            normalizeEvent(event);
-            return new Promise((resolve, reject) => {
-                apolloHandler(event, context, (error, data) => {
-                    if (error) {
-                        return reject(error);
-                    }
+        handler: async function(query, context) {
+            const { graphqlResponse } = await runHttpQuery([], {
+                method: "POST",
+                query,
+                options: { schema, schemaHash, context },
+                request: {
+                    url: "/graphql",
+                    method: "POST",
+                    headers: new Headers(http.headers)
+                }
+            });
 
-                    resolve(data);
-                });
+            return http.response({
+                body: graphqlResponse,
+                headers: buildCorsHeaders({
+                    "Content-Type": "application/json"
+                })
             });
         }
     };
