74c9bb8ff0505b048dc2a686f762a3c5c9554939
Bruno Zorić <bruno.zoric@gmail.com>
2020-09-24 11:35:27 +0200

    fix: image list display in preview mode

--- packages/app-page-builder/src/render/plugins/elements/imagesList/ImagesList.tsx
+++ packages/app-page-builder/src/render/plugins/elements/imagesList/ImagesList.tsx
@@ -3,14 +3,14 @@ import warning from "warning";
 import { getPlugins } from "@webiny/plugins";
 import { PbPageElementImagesListComponentPlugin } from "@webiny/app-page-builder/types";
 
-const ImagesList = props => {
-    const { data = {} } = props;
+const ImagesList = ({ data, theme }) => {
     const { component } = data;
     const plugins = getPlugins<PbPageElementImagesListComponentPlugin>(
         "pb-page-element-images-list-component"
     );
 
     const pageList = plugins.find(cmp => cmp.componentName === component);
+
     if (!pageList) {
         warning(false, `Pages list component "${component}" is missing!`);
         return null;
@@ -23,7 +23,7 @@ const ImagesList = props => {
         return null;
     }
 
-    return null;
+    return <ListComponent data={data.images || []} theme={theme} />;
 };
 
 export default React.memo(ImagesList);
