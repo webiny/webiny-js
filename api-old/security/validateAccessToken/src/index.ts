c270223756e8714b4f73776144413f0901d6a206
Adrian Smijulj <adrian1358@gmail.com>
2020-09-28 15:19:56 +0200

    chore: remove `api-old` package

--- api-old/security/validateAccessToken/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createHandler } from "@webiny/handler";
-import validateAccessToken from "@webiny/api-security-user-management/personalAccessToken/validator";
-import dbProxy from "@webiny/api-plugin-commodo-db-proxy";
-
-export const handler = createHandler(
-    dbProxy({ functionName: process.env.DB_PROXY_FUNCTION }),
-    // TODO: We'll revisit this later
-    validateAccessToken()
-);
