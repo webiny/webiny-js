89b585793f212f8d5221bdec859da512c53457af
Adrian Smijulj <adrian1358@gmail.com>
2020-09-29 11:03:44 +0200

    fix: adapt to newest project structure changes

--- scripts/setupEnvFiles.js
+++ scripts/setupEnvFiles.js
@@ -3,7 +3,14 @@ const path = require("path");
 const { green } = require("chalk");
 const loadJson = require("load-json-file");
 const writeJson = require("write-json-file");
-const uniqid = require("uniqid");
+const crypto = require("crypto");
+
+function random(length = 5) {
+    return crypto
+        .randomBytes(Math.ceil(length / 2))
+        .toString("hex")
+        .slice(0, length);
+}
 
 const PROJECT_FOLDER = ".";
 
@@ -18,63 +25,54 @@ const PROJECT_FOLDER = ".";
         fs.copyFileSync(rootExampleEnvJsonPath, rootEnvJsonPath);
 
         const rootEnvJson = await loadJson.sync(rootEnvJsonPath);
-        rootEnvJson.default.PULUMI_CONFIG_PASSPHRASE = uniqid() + uniqid();
+        rootEnvJson.default.PULUMI_CONFIG_PASSPHRASE = random();
         await writeJson(rootEnvJsonPath, rootEnvJson);
 
         console.log(`✅️ ${green(".env.json")} was created successfully!`);
     }
 
     // Create API .env.json
-    const envJsonPath = path.resolve(PROJECT_FOLDER, "api", ".env.json");
     const exampleEnvJsonPath = path.resolve(PROJECT_FOLDER, "api", "example.env.json");
+    const envJsonPath = path.resolve(PROJECT_FOLDER, "api", ".env.json");
     if (fs.existsSync(envJsonPath)) {
         console.log(`⚠️  ${green("api/.env.json")} already exists, skipping.`);
     } else {
         fs.copyFileSync(exampleEnvJsonPath, envJsonPath);
 
         const envJson = await loadJson.sync(envJsonPath);
-        envJson.default.S3_BUCKET = `webiny-${uniqid()
+        envJson.default.S3_BUCKET = `webiny-${random()
             .split("-")
             .shift()}`;
 
-        envJson.local.DOCUMENT_DB_USERNAME = uniqid() + uniqid();
-        envJson.local.DOCUMENT_DB_PASSWORD = uniqid() + uniqid();
-        envJson.local.DOCUMENT_DB_DATABASE = "webiny-" + uniqid();
+        envJson.local.DOCUMENT_DB_USERNAME = `user${random()}`;
+        envJson.local.DOCUMENT_DB_PASSWORD = random(32);
+        envJson.local.DOCUMENT_DB_DATABASE = `webiny-${random()}`;
 
         await writeJson(envJsonPath, envJson);
         console.log(`✅️ ${green("api/.env.json")} was created successfully!`);
     }
 
     // Create `admin` .env.json
-    const adminEnvJsonPath = path.resolve(PROJECT_FOLDER, "apps", "admin", "code", ".env.json");
-    const exampleAdminEnvJsonPath = path.resolve(
-        PROJECT_FOLDER,
-        "apps",
-        "admin",
-        "code",
-        "example.env.json"
-    );
+    const adminRoot = path.join("apps", "admin", "code");
+    const exampleAdminEnvJsonPath = path.resolve(PROJECT_FOLDER, adminRoot, "example.env.json");
+    const adminEnvJsonPath = path.resolve(PROJECT_FOLDER, adminRoot, ".env.json");
 
     if (fs.existsSync(adminEnvJsonPath)) {
-        console.log(`⚠️  ${green("apps/admin/code/.env.json")} already exists, skipping.`);
+        console.log(`⚠️  ${green(path.join(adminRoot, ".env.json"))} already exists, skipping.`);
     } else {
         fs.copyFileSync(exampleAdminEnvJsonPath, adminEnvJsonPath);
-        console.log(`✅️ ${green("apps/admin/code/.env.json")} was created successfully!`);
+        console.log(`✅️ ${green(path.join(adminRoot, ".env.json"))} was created successfully!`);
     }
 
     // Create `site` .env.json
-    const siteEnvJsonPath = path.resolve(
-        PROJECT_FOLDER,
-        "apps",
-        "site",
-        "code",
-        ".env.json"
-    );
-    const exampleSiteEnvJsonPath = path.resolve(PROJECT_FOLDER, "apps", "site", "code", "example.env.json");
+    const siteRoot = path.join("apps", "site", "code");
+    const exampleSiteEnvJsonPath = path.resolve(PROJECT_FOLDER, siteRoot, "example.env.json");
+    const siteEnvJsonPath = path.resolve(PROJECT_FOLDER, siteRoot, ".env.json");
+
     if (fs.existsSync(siteEnvJsonPath)) {
-        console.log(`⚠️  ${green("apps/site/code/.env.json")} already exists, skipping.`);
+        console.log(`⚠️  ${green(path.join(siteRoot, ".env.json"))} already exists, skipping.`);
     } else {
         fs.copyFileSync(exampleSiteEnvJsonPath, siteEnvJsonPath);
-        console.log(`✅️ ${green("apps/site/code/.env.json")} was created successfully!`);
+        console.log(`✅️ ${green(path.join(siteRoot, ".env.json"))} was created successfully!`);
     }
 })();
