2ca671f94722f06e0c911cb67a4c9d7506872407
Bruno Zorić <bruno.zoric@gmail.com>
2020-09-30 18:12:00 +0200

    build(app-plugin-security-cognito-theme): add webiny/plugins to packages

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,3 +1,4 @@
+import {CognitoViewLogoPlugin} from "@webiny/app-plugin-security-cognito-theme/admin/types";
 import adminTemplate from "@webiny/app-template-admin-full";
 import "./App.scss";
 
@@ -6,5 +7,12 @@ export default adminTemplate({
         region: process.env.REACT_APP_USER_POOL_REGION,
         userPoolId: process.env.REACT_APP_USER_POOL_ID,
         userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
-    }
+    },
+    plugins: [
+        {
+            type: "cognito-view",
+            name: "cognito-view-logo",
+            src: "some_string.jpg",
+        } as CognitoViewLogoPlugin,
+    ],
 });
