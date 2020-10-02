8f87866e3a28c2a79e7abe1dc1079827d0a03846
aloks98 <aloksahoo9819@gmail.com>
2020-10-02 22:06:27 +0530

    fix: fixed Not Authorized page style
    Changed Not Authorized view and access from one line to a page beautified with link to homepage
    
    BREAKING CHANGE:
    na

--- packages/app-security/src/admin/plugins/secureRouteError.tsx
+++ packages/app-security/src/admin/plugins/secureRouteError.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import { Plugin } from "@webiny/plugins/types";
 import Helmet from "react-helmet";
+import { css } from "emotion"
+import authErrorImg from "../assets/images/SecureRouteError.svg"
+
+const notAuthCss = css({
+    display: "block",
+    padding: 25,
+    textAlign: "center",
+    margin: "auto"
+})
 
 type SecureRouteErrorPlugin = Plugin & { render: () => React.ReactNode };
 
@@ -9,10 +18,13 @@ const plugin: SecureRouteErrorPlugin = {
     name: "secure-route-error",
     render() {
         return (
-            <>
+            <div className={notAuthCss}>    
                 <Helmet title={"Not authorized"} />
-                <span>You are not authorized to access this route.</span>
-            </>
+                <img src={authErrorImg} alt="Not Authorized" width="40%"/>
+                <p>You are not authorized to view this route.</p>
+                <p>Please contact your administrator to request access.</p>
+                <a href="/">Take me back.</a>
+            </div>
         );
     }
 };
--- packages/app-security/src/components/SecureRoute.tsx
+++ packages/app-security/src/components/SecureRoute.tsx
@@ -3,6 +3,18 @@ import { hasScopes } from "@webiny/app-security";
 import { getPlugin } from "@webiny/plugins";
 import { ResourcesType } from "../identity";
 import { SecureRouteErrorPlugin } from "@webiny/app-security/types";
+import { css } from "emotion";
+import Helmet from "react-helmet";
+
+
+import authErrorImg from "../admin/assets/images/SecureRouteError.svg"
+
+const notAuthCss = css({
+    display: "block",
+    padding: 25,
+    textAlign: "center",
+    margin: "auto"
+})
 
 export default ({
     children,
@@ -19,7 +31,15 @@ export default ({
 
     const plugin = getPlugin<SecureRouteErrorPlugin>("secure-route-error");
     if (!plugin) {
-        return <span>You are not authorized to view this route.</span>;
+        return (
+            <div className={notAuthCss}>    
+                <Helmet title={"Not authorized"} />
+                <img src={authErrorImg} alt="Not Authorized" width="40%"/>
+                <p>You are not authorized to view this route.</p>
+                <p>Please contact your administrator to request access.</p>
+                <a href="/">Take me back.</a>
+            </div>
+        );
     }
     return plugin.render();
 };
