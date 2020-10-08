c2193cdc5e1bdc2bf5ca096510d1169c0ac252be
Adrian Smijulj <adrian1358@gmail.com>
2020-10-08 19:51:38 +0200

    fix: remove `handler-files`

--- apps/site/code/app/webiny.config.js
+++ apps/site/code/app/webiny.config.js
@@ -1,8 +1,6 @@
 const {
     startApp,
     buildApp,
-    buildAppHandlerWithSSR,
-    buildAppSSR
 } = require("@webiny/project-utils");
 const path = require("path");
 const { setEnvironmentFromState } = require("@webiny/cli-plugin-deploy-pulumi/utils");
@@ -37,24 +35,6 @@ module.exports = {
 
             // Bundle app for deployment
             await buildApp(options, context);
-
-            // Build Lambda handler which will serve files to CDN
-            await buildAppHandlerWithSSR(options, context);
-        },
-        async buildSsr({ env, ...options }, context) {
-            // Load .env.json from current directory.
-            await context.loadEnv(path.resolve(__dirname, ".env.json"), env);
-
-            // Set environment variables for given project environment and stack.
-            // This will load state values using the provided map and
-            // populate process.env, overwriting existing values.
-            await setEnvironmentFromState({ env, stack: "api", map }, context);
-
-            // Build app for deployment
-            await buildApp(options, context);
-
-            // Build SSR bundle using app build output
-            await buildAppSSR({ ...options, app: __dirname }, context);
         }
     }
 };
--- packages/project-utils/bundling/appHandler/handler.js
+++ packages/project-utils/bundling/appHandler/handler.js
@@ -1,5 +1,4 @@
 import "source-map-support/register";
 import { createHandler } from "@webiny/handler";
-import filesPlugins from "@webiny/handler-files";
 
-export const handler = createHandler(filesPlugins());
+export const handler = createHandler();
