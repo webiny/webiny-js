c50ffab15c396cb70ce6bd39ab94c28b8cfb0a4a
Bruno Zorić <bruno.zoric@gmail.com>
2020-10-09 14:24:21 +0200

    fix: add value to SlateEditorProps

--- packages/app-page-builder/src/editor/components/ConnectedSlate.tsx
+++ packages/app-page-builder/src/editor/components/ConnectedSlate.tsx
@@ -4,18 +4,14 @@ import Slate from "./Slate";
 import { elementByIdSelectorFamily } from "./recoil";
 import { useRecoilValue } from "recoil";
 
-type Props = SlateEditorProps & {
+type Props = Omit<SlateEditorProps, "value"> & {
     elementId: string;
 };
 const ConnectedSlate: React.FunctionComponent<Props> = props => {
     const { elementId } = props;
     const element = useRecoilValue(elementByIdSelectorFamily(elementId));
     const value = element?.data?.text;
-    const slateProps = {
-        ...props,
-        value
-    };
-    return <Slate {...slateProps} />;
+    return <Slate {...props} value={value} />;
 };
 
 export default React.memo(ConnectedSlate);
