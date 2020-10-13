f72e8241ba2ad3507d45990c5e9805e9594d152a
Adrian Smijulj <adrian1358@gmail.com>
2020-10-13 16:25:31 +0200

    fix: add DynamoDB table

--- api/stack/i18n.ts
+++ api/stack/i18n.ts
@@ -5,7 +5,18 @@ import defaultLambdaRole from "./defaultLambdaRole";
 
 class I18N {
     functions: { locales: aws.lambda.Function; graphql: aws.lambda.Function };
+    dynamoDbTable: aws.dynamodb.Table;
     constructor({ env, dbProxy }: { env: { [key: string]: any }; dbProxy: aws.lambda.Function }) {
+        this.dynamoDbTable = new aws.dynamodb.Table("I18N", {
+            attributes: [
+                { name: "PK", type: "S" },
+                { name: "SK", type: "S" }
+            ],
+            billingMode: "PAY_PER_REQUEST",
+            hashKey: "PK",
+            rangeKey: "SK"
+        });
+
         const graphql = new aws.lambda.Function("i18n-graphql", {
             role: defaultLambdaRole.role.arn,
             runtime: "nodejs12.x",
@@ -16,7 +27,7 @@ class I18N {
                 ".": new pulumi.asset.FileArchive("./code/i18n/graphql/build")
             }),
             environment: {
-                variables: env
+                variables: { ...env, STORAGE_NAME: this.dynamoDbTable.name }
             },
             vpcConfig: {
                 subnetIds: vpc.subnets.private.map(subNet => subNet.id),
