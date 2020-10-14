37134a8fbbd4d5388195525c25690c5d31adc2c2
Adrian Smijulj <adrian1358@gmail.com>
2020-10-14 10:08:32 +0200

    chore: remove old file

--- packages/app-plugin-security-cognito-theme/src/admin/types.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Plugin } from "@webiny/app/types";
-
-export type CognitoViewLogoPlugin = Plugin & {
-    name: "cognito-view-logo";
-    type: "cognito-view";
-    component?: React.FunctionComponent;
-    src?: string;
-};
