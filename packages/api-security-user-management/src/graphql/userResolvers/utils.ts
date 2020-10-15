289afe13cabbcf43ceaf3b431a56d7375b26dfdc
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-15 14:16:54 +0530

    feat: update "updateCurrentUser"

--- packages/api-security-user-management/src/graphql/userResolvers/utils.ts
+++ packages/api-security-user-management/src/graphql/userResolvers/utils.ts
@@ -34,3 +34,31 @@ export const createSecurityUser = ({ Model, user }) => {
 
     return batch.execute();
 };
+
+export const updateSecurityUser = async ({ Model, modelInstance, user }) => {
+    const PK = `${PK_USER}#${user.id}`;
+
+    // Instead of creating new instances of "Security Model" we are reusing
+    const securityRecordPrimary = new Model();
+    await securityRecordPrimary.populate(modelInstance);
+
+    securityRecordPrimary.PK = PK;
+    securityRecordPrimary.SK = SK_USER;
+    securityRecordPrimary.GSI1_SK = `login#${user.email}`;
+
+    const securityRecordSecondary = new Model();
+    await securityRecordSecondary.populate(modelInstance);
+
+    securityRecordSecondary.PK = PK;
+    securityRecordSecondary.SK = "createdOn";
+    securityRecordSecondary.GSI1_SK = `createdOn#${user.createdOn}`;
+
+    const batch = new Batch(
+        // User item - A
+        [securityRecordPrimary, "save"],
+        // User item - createdOn
+        [securityRecordSecondary, "save"]
+    );
+
+    await batch.execute();
+};
