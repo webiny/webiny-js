39bcfef7d9a591dfb4e5f623ca0b0c349875d330
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-19 19:38:10 +0530

    refactor: reuse `getIdentityData`

--- apps/admin/code/src/plugins/security.ts
+++ apps/admin/code/src/plugins/security.ts
@@ -1,17 +1,14 @@
 import security from "@webiny/app-plugin-security-cognito";
 import cognitoUserManagement from "@webiny/app-plugin-security-cognito/userManagement";
 import userManagement from "@webiny/app-security-user-management/plugins";
-import { LOGIN } from "@webiny/app-security-user-management/graphql";
+import { getIdentityData } from "../components/Authentication";
 
 export default [
     security({
         region: process.env.REACT_APP_USER_POOL_REGION,
         userPoolId: process.env.REACT_APP_USER_POOL_ID,
         userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
-        async getIdentityData({ client }) {
-            const { data } = await client.mutate({ mutation: LOGIN });
-            return data.security.login.data;
-        }
+        getIdentityData
     }),
     userManagement(),
     cognitoUserManagement()
