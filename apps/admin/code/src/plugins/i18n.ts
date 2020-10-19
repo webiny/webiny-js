6c12b3b9b376ddcce0197af0ac646d008c56b640
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-19 19:38:33 +0530

    feat: eject `I18N` plugins

--- apps/admin/code/src/plugins/i18n.ts
+++ apps/admin/code/src/plugins/i18n.ts
@@ -1,3 +1,24 @@
-import i18n from "@webiny/app-i18n/admin/plugins";
+import { AdminGlobalSearchPreventHotkeyPlugin } from "@webiny/app-admin/types";
+import install from "@webiny/app-i18n/admin/plugins/install";
+import routes from "@webiny/app-i18n/admin/plugins/routes";
+import menus from "@webiny/app-i18n/admin/plugins/menus";
+import richTextEditor from "@webiny/app-i18n/admin/plugins/richTextEditor";
+// I18N site plugins
+// TODO: Check why the site plugin is needed in "admin" app
+import i18nSitePlugins from "@webiny/app-i18n/site/plugins";
 
-export default i18n();
+/**
+ * Prevents opening global search menu when pressing "/" inside of I18N Rich Text Editor.
+ * @type {{shouldOpen(*): boolean, name: string, type: string}}
+ */
+const globalSearchHotkey: AdminGlobalSearchPreventHotkeyPlugin = {
+    type: "admin-global-search-prevent-hotkey",
+    name: "global-search-prevent-hotkey-slate-editor",
+    preventOpen(e) {
+        if ((e.target as any).dataset.slateEditor === "true") {
+            return true;
+        }
+    }
+};
+
+export default [i18nSitePlugins(), globalSearchHotkey, install, routes, menus, richTextEditor];
