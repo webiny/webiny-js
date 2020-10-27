439e642af386b11b35ce570112ef011baf5cb698
Alok Kumar Sahoo <45600289+aloks98@users.noreply.github.com>
2020-10-27 10:35:47 +0530

    feat: show published version of webiny in admin app (#1321)
    show published version of webiny in navigation sidebar footer in admin app

--- packages/project-utils/bundling/app/index.js
+++ packages/project-utils/bundling/app/index.js
@@ -1,6 +1,9 @@
+const { version } = require("@webiny/project-utils/package.json");
+
 module.exports.buildApp = options => {
     process.env.NODE_ENV = "production";
     process.env.REACT_APP_ENV = "browser";
+    process.env.REACT_APP_WEBINY_VERSION = version;
     process.env.REACT_APP_USER_ID = require("@webiny/cli/config").getId();
 
     return require("./createProdConfig")(options);
@@ -9,6 +12,7 @@ module.exports.buildApp = options => {
 module.exports.startApp = options => {
     process.env.NODE_ENV = "development";
     process.env.REACT_APP_ENV = "browser";
+    process.env.REACT_APP_WEBINY_VERSION = version;
     process.env.REACT_APP_USER_ID = require("@webiny/cli/config").getId();
 
     return require("./createDevConfig")(options);
