d137735f73095a6e03f81102f1d2712acd65eeb8
Adrian Smijulj <adrian1358@gmail.com>
2020-10-28 22:03:45 +0100

    feat: make `Plugin` type generic

--- packages/plugins/src/types.ts
+++ packages/plugins/src/types.ts
@@ -1,10 +1,10 @@
 export { PluginsContainer } from "./PluginsContainer";
 
-export type Plugin = {
+export type Plugin<T = Record<string, any>> = {
     type: string;
     name?: string;
     init?: () => void;
     [key: string]: any;
-};
+} & T;
 
 export type PluginCollection = (Plugin | PluginCollection)[];
