72baddbe1ff89a0cff0047508183f0678988bb53
Pavel Denisjuk <pavel@webiny.com>
2020-10-29 14:13:18 +0100

    feat: add ES service role creation plugin

--- api/stack/elasticSearch.ts
+++ api/stack/elasticSearch.ts
@@ -7,48 +7,30 @@ class ElasticSearch {
     constructor() {
         const domainName = "webiny-js";
 
-        const dependsOn = aws.iam
-            .getRole({ name: "AWSServiceRoleForAmazonElasticsearchService" })
-            .then(role => {
-                if (!role) {
-                    return [
-                        new aws.iam.ServiceLinkedRole("esServiceLinkedRole", {
-                            awsServiceName: "es.amazonaws.com"
-                        })
-                    ];
-                }
-
-                return [];
-            });
-
-        this.domain = new aws.elasticsearch.Domain(
-            domainName,
-            {
-                elasticsearchVersion: "7.7",
-                clusterConfig: {
-                    instanceType: "t3.small.elasticsearch"
-                },
-                vpcOptions: {
-                    subnetIds: [vpc.subnets.private[0].id],
-                    securityGroupIds: [vpc.vpc.defaultSecurityGroupId]
-                },
-                ebsOptions: {
-                    ebsEnabled: true,
-                    volumeSize: 10,
-                    volumeType: "gp2"
-                },
-                advancedOptions: {
-                    "rest.action.multi.allow_explicit_index": "true"
-                },
-                snapshotOptions: {
-                    automatedSnapshotStartHour: 23
-                },
-                tags: {
-                    Domain: "TestDomain"
-                }
+        this.domain = new aws.elasticsearch.Domain(domainName, {
+            elasticsearchVersion: "7.7",
+            clusterConfig: {
+                instanceType: "t3.small.elasticsearch"
             },
-            { dependsOn }
-        );
+            vpcOptions: {
+                subnetIds: [vpc.subnets.private[0].id],
+                securityGroupIds: [vpc.vpc.defaultSecurityGroupId]
+            },
+            ebsOptions: {
+                ebsEnabled: true,
+                volumeSize: 10,
+                volumeType: "gp2"
+            },
+            advancedOptions: {
+                "rest.action.multi.allow_explicit_index": "true"
+            },
+            snapshotOptions: {
+                automatedSnapshotStartHour: 23
+            },
+            tags: {
+                Domain: "TestDomain"
+            }
+        });
 
         new aws.elasticsearch.DomainPolicy(`${domainName}-policy`, {
             domainName: this.domain.domainName.apply(v => `${v}`),
