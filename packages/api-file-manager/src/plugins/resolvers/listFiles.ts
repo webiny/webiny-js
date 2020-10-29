7905761afd32bec177174a11f1c6f2e6d2467716
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-29 20:38:02 +0530

    feat(api-file-manager): implement `listFiles` using elastic search

--- packages/api-file-manager/src/plugins/resolvers/listFiles.ts
+++ packages/api-file-manager/src/plugins/resolvers/listFiles.ts
@@ -3,6 +3,13 @@ import { GraphQLFieldResolver } from "@webiny/graphql/types";
 import { NotAuthorizedResponse } from "@webiny/api-security";
 import hasRwd from "./utils/hasRwd";
 
+const sorters = {
+    CREATED_ON_ASC: { createdOn: "asc" },
+    CREATED_ON_DESC: { createdOn: "desc" },
+    SIZE_ASC: { size: "asc" },
+    SIZE_DESC: { size: "desc" }
+};
+
 const resolver: GraphQLFieldResolver = async (root, args, context) => {
     try {
         // If permission has "rwd" property set, but "r" is not part of it, bail.
@@ -11,9 +18,66 @@ const resolver: GraphQLFieldResolver = async (root, args, context) => {
             return new NotAuthorizedResponse();
         }
 
-        const files = context.files;
+        const { limit = 40, sort, search = "", types = [], tags = [], ids = [] } = args;
 
-        let list = await files.list();
+        const must = [];
+        // We'll see about it
+        // must.push({ "meta.private": { $ne: true } }); // Files created by the system, eg. installation files.
+        if (Array.isArray(types) && types.length) {
+            must.push({ terms: { "type.keyword": types } });
+        }
+
+        if (search) {
+            must.push({
+                bool: {
+                    should: [
+                        { wildcard: { name: `*${search}*` } },
+                        { terms: { tags: search.toLowerCase().split(" ") } }
+                    ]
+                }
+            });
+        }
+
+        if (Array.isArray(tags) && tags.length > 0) {
+            must.push({
+                terms: { "tags.keyword": tags.map(tag => tag.toLowerCase()) }
+            });
+        }
+
+        if (Array.isArray(ids) && ids.length > 0) {
+            must.push({
+                terms: { "id.keyword": ids }
+            });
+        }
+
+        const response = await context.elasticSearch.search({
+            index: "file-manager",
+            type: "_doc",
+            body: {
+                query: {
+                    // eslint-disable-next-line @typescript-eslint/camelcase
+                    constant_score: {
+                        filter: {
+                            bool: {
+                                // `must` means `and`;
+                                // all conditions must be satisfied for a record to be present in the result
+                                must: must
+                            }
+                        }
+                    }
+                },
+                size: limit,
+                sort: [sorters[sort] || sorters.CREATED_ON_ASC]
+            }
+        });
+
+        let list = response?.body?.hits?.hits?.map(item => item._source);
+
+        if (!Array.isArray(list)) {
+            const files = context.files;
+
+            list = await files.list();
+        }
 
         // If user can only manage own records, let's check if he owns the loaded one.
         if (filesFilePermission?.own === true) {
