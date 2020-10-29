8a6754cc0072beaa17a571f17fe18b4a94d75e27
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-29 16:39:18 +0530

    feat: add `dbPlugins` & `elasticSearch` plugins

--- api/code/fileManager/graphql/src/index.ts
+++ api/code/fileManager/graphql/src/index.ts
@@ -1,12 +1,15 @@
 import { createHandler } from "@webiny/handler-aws";
 import apolloServerPlugins from "@webiny/handler-apollo-server";
-import dbProxyPlugins from "@webiny/api-plugin-commodo-db-proxy";
 import settingsManagerPlugins from "@webiny/api-settings-manager/client";
 import filesPlugins from "@webiny/api-file-manager/plugins";
 import filesResolvers from "@webiny/api-plugin-files-resolvers-mongodb";
 import securityPlugins from "@webiny/api-security/authenticator";
 import permissionsManager from "@webiny/api-security-permissions-manager/client";
 import cognitoAuthentication from "@webiny/api-plugin-security-cognito/authentication";
+import dbPlugins from "@webiny/handler-db";
+import { DynamoDbDriver } from "@webiny/db-dynamodb/index";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import elasticSearch from "@webiny/api-plugin-elastic-search-client";
 
 export const handler = createHandler(
     apolloServerPlugins({
@@ -16,7 +19,16 @@ export const handler = createHandler(
             playground: process.env.GRAPHQL_PLAYGROUND
         }
     }),
-    dbProxyPlugins({ functionName: process.env.DB_PROXY_FUNCTION }),
+    elasticSearch({ endpoint: `https://${process.env.ELASTIC_SEARCH_ENDPOINT}` }),
+    dbPlugins({
+        table: process.env.DB_TABLE,
+        driver: new DynamoDbDriver({
+            documentClient: new DocumentClient({
+                convertEmptyValues: true,
+                region: process.env.AWS_REGION
+            })
+        })
+    }),
     settingsManagerPlugins({ functionName: process.env.SETTINGS_MANAGER_FUNCTION }),
     // Adds a context plugin to process `security` plugins for authentication
     securityPlugins(),
