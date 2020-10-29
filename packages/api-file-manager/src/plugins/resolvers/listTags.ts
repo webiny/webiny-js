b185c61630573e2291a4e978252d3b2ef9d429d2
Ashutosh <bhardwajashu96@hotmail.com>
2020-10-29 20:15:30 +0530

    fix: add error handling

--- packages/api-file-manager/src/plugins/resolvers/listTags.ts
+++ packages/api-file-manager/src/plugins/resolvers/listTags.ts
@@ -1,19 +1,28 @@
 import { GraphQLFieldResolver } from "@webiny/graphql/types";
+import { ErrorResponse } from "@webiny/graphql";
 
 const resolver: GraphQLFieldResolver = async (root, args, context) => {
-    const response = await context.elasticSearch.search({
-        index: "file-manager",
-        body: {
-            size: 0,
-            aggs: {
-                listTags: {
-                    terms: { field: "tags.keyword" }
+    try {
+        const response = await context.elasticSearch.search({
+            index: "file-manager",
+            body: {
+                size: 0,
+                aggs: {
+                    listTags: {
+                        terms: { field: "tags.keyword" }
+                    }
                 }
             }
-        }
-    });
+        });
 
-    return response?.body?.aggregations?.listTags?.buckets.map(item => item.key);
+        return response?.body?.aggregations?.listTags?.buckets.map(item => item.key) || [];
+    } catch (e) {
+        return new ErrorResponse({
+            code: e.code,
+            message: e.message,
+            data: e.data
+        });
+    }
 };
 
 export default resolver;
