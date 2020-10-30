9cb42399573d40a5e133b04249cf867e4f68b2c4
Adrian Smijulj <adrian1358@gmail.com>
2020-10-30 09:14:34 +0100

    fix: remove `canSave`

--- packages/app-page-builder/src/admin/plugins/menuItems/pageList/PageListForm.tsx
+++ packages/app-page-builder/src/admin/plugins/menuItems/pageList/PageListForm.tsx
@@ -16,7 +16,7 @@ const menuPageFormStyle = {
     backgroundColor: "var(--mdc-theme-background) !important"
 };
 
-const LinkForm = ({ data, onSubmit, onCancel, canSave }) => {
+const LinkForm = ({ data, onSubmit, onCancel }) => {
     return (
         <Elevation z={4} css={menuPageFormStyle}>
             <Form data={data} onSubmit={onSubmit}>
