a73851ba79b08d44094e5c310f2d3bb01902f28d
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-02 19:42:09 +0530

    feat(api-file-manager): use "createInBatch" & "bulk" indexing

--- packages/api-file-manager/src/plugins/resolvers/createFiles.ts
+++ packages/api-file-manager/src/plugins/resolvers/createFiles.ts
@@ -2,6 +2,17 @@ import { Response, ErrorResponse } from "@webiny/graphql";
 import { GraphQLFieldResolver } from "@webiny/graphql/types";
 import { BATCH_CREATE_MAX_FILES } from "./utils/constants";
 
+const getFileDoc = file => ({
+    id: file.id,
+    createdOn: file.createdOn,
+    key: file.key,
+    size: file.size,
+    type: file.type,
+    name: file.name,
+    tags: file.tags,
+    createdBy: file.createdBy
+});
+
 const resolver: GraphQLFieldResolver = async (root, args, context) => {
     const { data: filesData } = args;
     if (!Array.isArray(filesData)) {
@@ -25,31 +36,41 @@ const resolver: GraphQLFieldResolver = async (root, args, context) => {
         });
     }
 
-    const data = [];
-    for (let i = 0; i < filesData.length; i++) {
-        const fileData = filesData[i];
+    try {
+        const data = await context.files.createInBatch(filesData);
 
-        const file = await context.files.create(fileData);
-        data.push(file);
-        // Index file in "Elastic Search"
-        await context.elasticSearch.create({
-            id: file.id,
-            index: "file-manager",
-            type: "_doc",
-            body: {
-                id: file.id,
-                createdOn: file.createdOn,
-                key: file.key,
-                size: file.size,
-                type: file.type,
-                name: file.name,
-                tags: file.tags,
-                createdBy: file.createdBy
-            }
+        const body = data.flatMap(doc => [{ index: { _index: "file-manager" } }, getFileDoc(doc)]);
+
+        const { body: bulkResponse } = await context.elasticSearch.bulk({ body });
+        if (bulkResponse.errors) {
+            const erroredDocuments = [];
+            // The items array has the same order of the dataset we just indexed.
+            // The presence of the `error` key indicates that the operation
+            // that we did for the document has failed.
+            bulkResponse.items.forEach((action, i) => {
+                const operation = Object.keys(action)[0];
+                if (action[operation].error) {
+                    erroredDocuments.push({
+                        // If the status is 429 it means that you can retry the document,
+                        // otherwise it's very likely a mapping error, and you should
+                        // fix the document before to try it again.
+                        status: action[operation].status,
+                        error: action[operation].error,
+                        operation: body[i * 2],
+                        document: body[i * 2 + 1]
+                    });
+                }
+            });
+        }
+
+        return new Response(data);
+    } catch (e) {
+        return new ErrorResponse({
+            code: e.code,
+            message: e.message,
+            data: e.data
         });
     }
-
-    return new Response(data);
 };
 
 export default resolver;
