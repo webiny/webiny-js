360ea2732a1c98a068e99e3812ec9a199165d277
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-02 18:57:39 +0530

    refactor(api-security-user-management): update "permissions manager"
    update "permissions manager" using `CRUD` helpers

--- packages/api-security-user-management/src/permissionsManager/index.ts
+++ packages/api-security-user-management/src/permissionsManager/index.ts
@@ -1,39 +1,30 @@
-import models from "../models";
-import {
-    PK_USER,
-    SK_USER
-} from "@webiny/api-security-user-management/models/securityUserData.model";
-import { GSI1_PK_GROUP } from "@webiny/api-security-user-management/models/securityGroupData.model";
+import crud from "../crud";
 
 export default () => [
-    models(),
+    crud(),
     {
         type: "permissions-manager-middleware",
         async getPermissions({ identity }, context) {
-            const Model = context.models.SECURITY;
+            const { users, groups } = context;
             if (identity) {
-                const securityRecord = await Model.findOne({
-                    query: { PK: `${PK_USER}#${identity}`, SK: SK_USER }
-                });
-                const user = securityRecord.data;
+                const user = await users.get(identity);
+
                 if (!user) {
                     throw Error(`User "${identity}" was not found!`);
                 }
 
-                return user.permissions;
+                const group = await groups.get(user.group);
+                return group?.data?.permissions;
             }
 
             // Identity is "anonymous", and we need to load permissions from the "anonymous" group.
+            const group = await groups.getBySlug("anonymous");
 
-            const securityRecord = await Model.findOne({
-                query: { GSI1_PK: GSI1_PK_GROUP, GSI1_SK: `slug#anonymous` }
-            });
-            const group = securityRecord.data;
             if (!group) {
                 return [];
             }
 
-            return group.permissions;
+            return group?.GSI_DATA?.permissions;
         }
     }
 ];
