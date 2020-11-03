d31ea1a2bbf356a7b576ff404ec056281bf29ee1
Adrian Smijulj <adrian1358@gmail.com>
2020-11-03 09:51:34 +0100

    feat: add `security-authorization` plugin

---
+++ packages/api-security-user-management/src/authorization/index.ts
@@ -0,0 +1,37 @@
+import {
+    PK_USER,
+    SK_USER
+} from "@webiny/api-security-user-management/models/securityUserData.model";
+import { GSI1_PK_GROUP } from "@webiny/api-security-user-management/models/securityGroupData.model";
+
+export default () => [
+    {
+        type: "security-authorization",
+        async getPermissions({ identity, models }) {
+            const Model = models.SECURITY;
+            if (identity) {
+                const securityRecord = await Model.findOne({
+                    query: { PK: `${PK_USER}#${identity}`, SK: SK_USER }
+                });
+                const user = securityRecord.data;
+                if (!user) {
+                    throw Error(`User "${identity}" was not found!`);
+                }
+
+                return user.permissions;
+            }
+
+            // Identity is "anonymous", and we need to load permissions from the "anonymous" group.
+            const securityRecord = await Model.findOne({
+                query: { GSI1_PK: GSI1_PK_GROUP, GSI1_SK: `slug#anonymous` }
+            });
+
+            const group = securityRecord?.data;
+            if (!group) {
+                return [];
+            }
+
+            return group.permissions;
+        }
+    }
+];
