8d30569be14ee6ae02d550bf17b2cd42562e48a0
Adrian Smijulj <adrian1358@gmail.com>
2020-11-03 09:54:13 +0100

    fix: remove `permissionsManager`

--- api/code/api/src/index.ts
+++ api/code/api/src/index.ts
@@ -1,10 +1,10 @@
 import { createHandler } from "@webiny/handler-aws";
 import apolloServerPlugins from "@webiny/handler-apollo-server";
 import securityPlugins from "@webiny/api-security/authenticator";
-import permissionsManager from "@webiny/api-security-permissions-manager/client";
 import cognitoAuthentication from "@webiny/api-plugin-security-cognito/authentication";
 import cognitoUserManagement from "@webiny/api-plugin-security-cognito/userManagement";
 import userManagement from "@webiny/api-security-user-management";
+import userManagementAuthorization from "@webiny/api-security-user-management/authorization";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import i18nPlugins from "@webiny/api-i18n/plugins";
 import i18nContentPlugins from "@webiny/api-i18n-content/plugins";
@@ -31,7 +31,7 @@ export const handler = createHandler(
         })
     }),
     dynamoDb({
-        tableName: process.env.STORAGE_NAME,
+        tableName: process.env.DB_TABLE,
         documentClient: new DocumentClient({
             convertEmptyValues: true,
             region: process.env.AWS_REGION
@@ -39,16 +39,20 @@ export const handler = createHandler(
     }),
     // Adds a context plugin to process `security` plugins for authentication
     securityPlugins(),
-    // Adds an Permissions Manager Client to perform permission checks
-    permissionsManager({ functionName: process.env.PERMISSIONS_MANAGER_FUNCTION }),
+
     // Add Cognito plugins for authentication
     cognitoAuthentication({
         region: process.env.COGNITO_REGION,
         userPoolId: process.env.COGNITO_USER_POOL_ID,
         identityType: "admin"
     }),
+
     // Add user management
     userManagement(),
+
+    // Contains the "security-authorization" plugin.
+    userManagementAuthorization(),
+
     // Add Cognito plugins for user management
     cognitoUserManagement({
         region: process.env.COGNITO_REGION,
