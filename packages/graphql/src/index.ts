1ef7d03bb8fa8d590a33f5c987a1cff17b5f4f11
Adrian Smijulj <adrian1358@gmail.com>
2020-11-04 13:10:03 +0100

    fix: remove `applyContextPlugins`

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -2,4 +2,3 @@ export * from "./responses";
 export { default as emptyResolver } from "./emptyResolver";
 export { default as createSchema } from "./createSchema";
 export { default as requiresTotalCount } from "./requiresTotalCount";
-export { applyContextPlugins } from "./createSchema/contextPlugins";
