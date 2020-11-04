12e6427e0054403bbfc585fbe81b78c3ad49aebd
Adrian Smijulj <adrian1358@gmail.com>
2020-11-04 10:31:08 +0100

    fix: remove `pipe`

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -2,5 +2,4 @@ export * from "./responses";
 export { default as emptyResolver } from "./emptyResolver";
 export { default as createSchema } from "./createSchema";
 export { default as requiresTotalCount } from "./requiresTotalCount";
-export { default as pipe } from "./pipe";
 export { applyContextPlugins } from "./createSchema/contextPlugins";
--- packages/graphql/src/pipe.ts
+++
@@ -1,1 +0,0 @@
-export default (...fns) => x => fns.reduce((v, f) => f(v), x);
