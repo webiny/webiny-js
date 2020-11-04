b02354fcfe3addd83635120d92bf8cb3d0dadd62
Adrian Smijulj <adrian1358@gmail.com>
2020-11-04 10:30:33 +0100

    chore: introduce getWorkspaces helper fn

---
+++ scripts/utils/getWorkspaces.js
@@ -0,0 +1,38 @@
+const readJson = require("load-json-file");
+const getWorkspaces = require("get-yarn-workspaces");
+const { yellow } = require("chalk");
+
+module.exports = () => {
+    return getWorkspaces()
+        .map(path => {
+            const packageJsonPath = path + "/package.json";
+            const tsConfigJsonPath = path + "/tsconfig.json";
+            const tsConfigBuildJsonPath = path + "/tsconfig.build.json";
+
+            let packageJson, tsConfigJson, tsConfigBuildJson;
+            packageJson = readJson.sync(packageJsonPath);
+
+            try {
+                tsConfigJson = readJson.sync(tsConfigJsonPath);
+                tsConfigBuildJson = readJson.sync(tsConfigBuildJsonPath);
+            } catch {
+                // Do nothing.
+            }
+
+            try {
+                return {
+                    packageFolder: path,
+                    packageJsonPath,
+                    tsConfigJsonPath,
+                    tsConfigBuildJsonPath,
+                    packageJson,
+                    tsConfigJson,
+                    tsConfigBuildJson
+                };
+            } catch {
+                console.log(yellow(`Ignoring ${path}/package.json`));
+                return null;
+            }
+        })
+        .filter(Boolean);
+};
