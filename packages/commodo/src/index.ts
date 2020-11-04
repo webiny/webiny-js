a31a275dfffa09e887a06418e2ea073c95e710fb
Adrian Smijulj <adrian1358@gmail.com>
2020-11-04 10:32:16 +0100

    fix: remove redundant dependencies

--- packages/commodo/src/index.ts
+++ packages/commodo/src/index.ts
@@ -2,14 +2,10 @@ export * from "repropose";
 export * from "@commodo/name";
 export * from "@commodo/fields";
 export * from "@commodo/hooks";
-export * from "@commodo/fields-storage";
-export * from "@commodo/fields-storage-ref";
-export * from "@commodo/fields-storage-soft-delete";
 export * from "commodo-fields-int";
 export * from "commodo-fields-float";
 export * from "commodo-fields-date";
 export * from "commodo-fields-object";
-export * from "commodo-fields-storage-crud-logs";
 
 export { default as pipe } from "ramda/src/pipe";
 export { default as compose } from "ramda/src/compose";
