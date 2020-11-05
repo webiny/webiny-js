3bd6f4d534ea0b6fc99e2b262dff3e1621128e22
Adrian Smijulj <adrian1358@gmail.com>
2020-11-05 11:01:40 +0100

    fix: update config

--- .adiorc.js
+++ .adiorc.js
@@ -1,25 +1,12 @@
 const get = require("lodash.get");
-const LOAD_COMPONENTS_PREFIX = ["@webiny/", "@serverless/"];
 
 module.exports = {
     parser: {
         plugins: ["jsx", "classProperties", "dynamicImport", "throwExpressions", "typescript"]
     },
     traverse: ({ path, push }) => {
-        // We try to detect "this.load("...")" calls inside of Serverless components.
         const { node } = path;
         if (node.type === "CallExpression") {
-            if (get(node, "callee.property.name") === "load") {
-                const possiblePackage = get(node, "arguments.0.value");
-                if (typeof possiblePackage === "string") {
-                    LOAD_COMPONENTS_PREFIX.forEach(prefix => {
-                        if (possiblePackage.startsWith(prefix)) {
-                            return push(possiblePackage);
-                        }
-                    });
-                }
-            }
-
             if (
                 get(node, "callee.property.name") === "resolve" &&
                 get(node, "callee.object.name") === "require"
@@ -38,16 +25,5 @@ module.exports = {
         peerDependencies: true
     },
     ignoreDirs: ["node_modules/", "dist/", "build/"],
-    packages: [
-        "packages/*",
-        "api/apolloGateway",
-        "api/settingsManager",
-        "api/databaseProxy",
-        "api/files/*",
-        "api/formBuilder",
-        "api/cms/*",
-        "api/i18n/*",
-        "api/pageBuilder",
-        "api/security/*"
-    ]
+    packages: ["packages/*", "api/code/api", "apps/admin/code", "apps/site/code"]
 };
