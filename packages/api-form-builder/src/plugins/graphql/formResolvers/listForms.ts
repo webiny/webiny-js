404ed7b053832d9da58ab4f1299cee7ef5e08d8e
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-12 22:44:49 +0530

    feat(api-form-builder): add `listForms` resolver

--- packages/api-form-builder/src/plugins/graphql/formResolvers/listForms.ts
+++ packages/api-form-builder/src/plugins/graphql/formResolvers/listForms.ts
@@ -1,32 +1,75 @@
-import { ListResponse, requiresTotalCount } from "@webiny/graphql";
+import { ErrorResponse, ListResponse } from "@webiny/graphql";
 import { GraphQLFieldResolver } from "@webiny/graphql/types";
 
 const resolver: GraphQLFieldResolver = async (root, args, context, info) => {
-    const { Form } = context.models;
+    const forms = context?.formBuilder?.crud?.forms;
+    const {
+        // sort = null,
+        search = null,
+        parent = null,
+        limit = 10
+        // after,
+        // before,
+    } = args;
 
-    const { limit = 10, after, before, sort = null, search = null, parent = null } = args;
+    const must: any = [{ term: { latestVersion: true } }];
 
-    const query: any = {
-        latestVersion: true
-    };
-
-    if (parent) {
-        query.parent = parent;
+    if (search) {
+        must.push({
+            bool: {
+                should: [
+                    { wildcard: { "name.keyword": `*${search}*` } },
+                    { wildcard: { name: `*${search}*` } },
+                    { wildcard: { "slug.keyword": `*${search}*` } },
+                    { wildcard: { slug: `*${search}*` } }
+                ]
+            }
+        });
     }
 
-    const findArgs = {
-        sort,
-        limit,
-        search,
-        query,
-        after,
-        before,
-        totalCount: requiresTotalCount(info)
-    };
+    if (parent) {
+        if (Array.isArray(parent)) {
+            must.push({ terms: { parent: parent } });
+        } else {
+            must.push({ term: { parent: parent } });
+        }
+    }
 
-    const forms = await Form.find(findArgs);
+    try {
+        // Get "latest" form revisions from Elasticsearch.
+        const response = await context.elasticSearch.search({
+            index: "form-builder",
+            type: "_doc",
+            body: {
+                query: {
+                    // eslint-disable-next-line @typescript-eslint/camelcase
+                    constant_score: {
+                        filter: {
+                            bool: {
+                                must
+                            }
+                        }
+                    }
+                },
+                size: limit
+            }
+        });
 
-    return new ListResponse(forms, forms.getMeta());
+        let list = response?.body?.hits?.hits?.map(item => item._source);
+        // Get complete form data for returned list.
+        if (list?.length) {
+            const formIds = list.map(item => item.id);
+            list = await forms.listFormsInBatch({ ids: formIds });
+        }
+
+        return new ListResponse(list);
+    } catch (e) {
+        return new ErrorResponse({
+            message: e.message,
+            code: e.code,
+            data: e.data
+        });
+    }
 };
 
 export default resolver;
