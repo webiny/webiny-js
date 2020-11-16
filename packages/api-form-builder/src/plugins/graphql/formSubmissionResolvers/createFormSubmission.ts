d42df5a5501456849499152f8ca3264c6e0e4fee
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-16 15:52:59 +0530

    refactor(api-form-builder): update FormSubmission "resolvers"
    use updated "CRUD"

--- packages/api-form-builder/src/plugins/graphql/formSubmissionResolvers/createFormSubmission.ts
+++ packages/api-form-builder/src/plugins/graphql/formSubmissionResolvers/createFormSubmission.ts
@@ -1,15 +1,19 @@
 import { ErrorResponse, NotFoundResponse, Response } from "@webiny/graphql";
+import { FormsCRUD } from "../../../types";
 
 export default async (root: any, args: { [key: string]: any }, context: { [key: string]: any }) => {
-    const forms = context?.formBuilder?.crud?.forms;
-    const form = await forms.get(args.id);
+    const forms: FormsCRUD = context?.formBuilder?.crud?.forms;
+
+    const { id, data, reCaptchaResponseToken, meta } = args;
+
+    const form = await forms.getForm(id);
 
     if (!form || !form.published) {
         return new NotFoundResponse(`Form with id "${args.id}" was not found!`);
     }
 
     try {
-        const formSubmission = await forms.submit({ ...args, form });
+        const formSubmission = await forms.submit({ data, reCaptchaResponseToken, meta, form });
         return new Response(formSubmission);
     } catch (e) {
         return new ErrorResponse({
