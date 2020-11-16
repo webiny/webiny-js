b43cba697b2e853ac8fbbf8c7c752eb4aff8561e
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-16 15:43:18 +0530

    feat(api-form-builder): add `types` for Forms, FormSubmissions and FormBuilderSettings

--- packages/api-form-builder/src/types.ts
+++ packages/api-form-builder/src/types.ts
@@ -41,3 +41,161 @@ export type FbFormTriggerHandlerPlugin = Plugin & {
     trigger: string;
     handle: (args: FbFormTriggerHandlerParams) => Promise<void>;
 };
+
+export type Form = {
+    id: string;
+    savedOn: string;
+    createdOn: string;
+    name: string;
+    slug: string;
+    version: number;
+    parent: string;
+    locked: boolean;
+    published: boolean;
+    publishedOn: string;
+    latestVersion: boolean;
+    status: string;
+    fields: Record<string, any>[];
+    layout: [string[]];
+    stats: Record<string, any>;
+    settings: Record<string, any>;
+    triggers: Record<string, any>;
+};
+
+export type CreatedBy = {
+    id: string;
+    displayName: string;
+    type: string;
+};
+
+export type FormCreateData = {
+    name: string;
+};
+
+export type FormUpdateData = {
+    name: string;
+    fields: Record<string, any>[];
+    layout: [string[]];
+    settings: Record<string, any>;
+    triggers: Record<string, any>;
+};
+
+export type Sort = {
+    SK: 1 | -1;
+};
+
+export type FormsCRUD = {
+    getForm(id: string): Promise<Form>;
+    listAllForms(sort: Sort): Promise<Form[]>;
+    listFormsBeginsWithId({
+        id,
+        sort
+    }: {
+        id: string;
+        sort?: Sort;
+        [key: string]: any;
+    }): Promise<Form[]>;
+    listFormsInBatch(ids: string[]): Promise<Form[]>;
+    createForm(data: FormCreateData): Promise<Form>;
+    updateForm(id: string, data: Partial<FormUpdateData>): Promise<boolean>;
+    deleteForm(id: string): Promise<boolean>;
+    deleteForms(ids: string[]): Promise<boolean>;
+    publishForm(id: string): Promise<boolean>;
+    unPublishForm(id: string): Promise<boolean>;
+    createFormRevision(revision: Form): Promise<Form>;
+    getNextVersion(id: string): Promise<number>;
+    markPreviousLatestVersion({
+        parentId,
+        version,
+        latestVersion
+    }: {
+        parentId: string;
+        version: number;
+        latestVersion: boolean;
+    }): Promise<boolean>;
+    saveFormStats(id: string, stats: any): Promise<boolean>;
+    submit({
+        form,
+        reCaptchaResponseToken,
+        data,
+        meta
+    }: {
+        form: Form;
+        reCaptchaResponseToken: string;
+        data: any;
+        meta: any;
+    }): Promise<FormSubmission>;
+};
+
+export type FormSubmission = {
+    id: string;
+    data: Record<string, any>;
+    meta: Record<string, any>;
+    form: {
+        parent: string;
+        revision: string;
+    };
+    logs: Record<string, any>[];
+};
+
+export type FormSubmissionCreateData = {
+    form: {
+        parent: string;
+        revision: string;
+    };
+    data: Record<string, any>;
+    reCaptchaResponseToken: string;
+    meta: Record<string, any>;
+};
+
+export type FormSubmissionsCRUD = {
+    getSubmission({
+        formId,
+        submissionId
+    }: {
+        formId: string;
+        submissionId: string;
+    }): Promise<FormSubmission>;
+    listAllSubmissions({ formId, sort }: { formId: string; sort: Sort }): Promise<FormSubmission[]>;
+    createSubmission(data: FormSubmissionCreateData): Promise<FormSubmission>;
+    updateSubmission({ formId, data }: { formId: string; data: FormSubmission }): Promise<boolean>;
+    deleteSubmission({
+        formId,
+        submissionId
+    }: {
+        formId: string;
+        submissionId: string;
+    }): Promise<boolean>;
+    addLog(formSubmission: FormSubmission, log: Record<string, any>): FormSubmission;
+};
+
+export type FormSubmissionUpdateData = {
+    logs: Record<string, any>;
+};
+
+export type FormBuilderSettings = {
+    key: "form-builder";
+    installed: boolean;
+    domain: string;
+    reCaptcha: {
+        enabled: boolean;
+        siteKey: string;
+        secretKey: string;
+    };
+};
+
+export type FormBuilderSettingsUpdateData = {
+    domain: string;
+    reCaptcha: {
+        enabled: boolean;
+        siteKey: string;
+        secretKey: string;
+    };
+};
+
+export type FormBuilderSettingsCRUD = {
+    getSettings(): Promise<FormBuilderSettings>;
+    createSettings(data: FormBuilderSettings): Promise<FormBuilderSettings>;
+    updateSettings(data: FormBuilderSettingsUpdateData): Promise<boolean>;
+    deleteSettings(): Promise<boolean>;
+};
