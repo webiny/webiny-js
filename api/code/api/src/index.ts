ee6b2ee874710c3decdf42e7028d473362e1008f
Adrian Smijulj <adrian1358@gmail.com>
2020-11-16 12:15:23 +0100

    fix: use new `@webiny/handler-graphql`

--- api/code/api/src/index.ts
+++ api/code/api/src/index.ts
@@ -1,5 +1,5 @@
 import { createHandler } from "@webiny/handler-aws";
-import apolloServerPlugins from "@webiny/handler-apollo-server";
+import apolloServerPlugins from "@webiny/handler-graphql";
 import securityPlugins from "@webiny/api-security/authenticator";
 import cognitoAuthentication from "@webiny/api-plugin-security-cognito/authentication";
 import cognitoUserManagement from "@webiny/api-plugin-security-cognito/userManagement";
