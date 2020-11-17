b31dbb46b865d777045d110b7d5b3382c0a21ff6
Adrian Smijulj <adrian1358@gmail.com>
2020-11-17 17:01:44 +0100

    feat: add `compose` / `pipe` functions

--- packages/handler-graphql/src/index.ts
+++ packages/handler-graphql/src/index.ts
@@ -1,22 +1,14 @@
 import { HandlerGraphQLOptions } from "./types";
 import createGraphQLHandler from "./createGraphQLHandler";
 import { Context } from "@webiny/handler/types";
-
-export const composeResolvers = <
-    TSource = Record<string, any>,
-    TArgs = Record<string, any>,
-    TContext = Context
->(
-    ...fns: ((source: TSource, args: TArgs, context: TContext) => any)[]
-): any => {
-    return async (source: TSource, args: TArgs, context: TContext) => {
-        let result;
-        for (let i = 0; i < fns.length; i++) {
-            const fn = fns[i];
-            result = await fn(source, args, context);
-        }
-        return result;
-    };
-};
+import { GraphQLFieldResolver } from "./types";
 
 export default (options: HandlerGraphQLOptions = {}) => [createGraphQLHandler(options)];
+
+export const pipe = <TSource = any, TArgs = Record<string, any>, TContext = Context>(...fns) => (
+    resolver: GraphQLFieldResolver<TSource, TArgs, TContext>
+) => fns.reduce((v, f) => f(v), resolver);
+
+export const compose = <TSource = any, TArgs = Record<string, any>, TContext = Context>(...fns) => (
+    resolver: GraphQLFieldResolver<TSource, TArgs, TContext>
+) => fns.reduceRight((v, f) => f(v), resolver);
