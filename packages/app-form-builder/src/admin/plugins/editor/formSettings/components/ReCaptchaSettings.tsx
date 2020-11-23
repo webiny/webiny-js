4c7138e68a66e3e6fc15b7cf6d2b594b7c14ca48
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-23 14:35:31 +0530

    refactor: replace `Query` with `useQuery`
    remove `get` usage

--- packages/app-form-builder/src/admin/plugins/editor/formSettings/components/ReCaptchaSettings.tsx
+++ packages/app-form-builder/src/admin/plugins/editor/formSettings/components/ReCaptchaSettings.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { Grid, Cell } from "@webiny/ui/Grid";
 import { Switch } from "@webiny/ui/Switch";
-import { Query } from "react-apollo";
-import { get } from "lodash";
+import { useQuery } from "react-apollo";
 import { GET_RECAPTCHA_SETTINGS } from "./graphql";
 import ReCaptchaSettingsDialog from "./ReCaptchaSettingsDialog";
 import { Alert } from "@webiny/ui/Alert";
@@ -15,69 +14,63 @@ const ReCaptchaSettings = ({ Bind, formData }) => {
 
     const [settingsLoaded, setSettingsLoaded] = useState(false);
 
+    const { data, refetch } = useQuery(GET_RECAPTCHA_SETTINGS, {
+        onCompleted: () => setSettingsLoaded(true)
+    });
+
+    const settings = data?.forms?.getSettings?.data?.reCaptcha || {};
+    const reCaptchaEnabledInSettings = settings.enabled && settings.siteKey && settings.secretKey;
+    const reCaptchaEnabled = formData?.reCaptcha?.enabled;
+
     return (
-        <Query query={GET_RECAPTCHA_SETTINGS} onCompleted={() => setSettingsLoaded(true)}>
-            {({ data, refetch }) => {
-                const settings = get(data, "forms.getSettings.data.reCaptcha") || {};
-                const reCaptchaEnabledInSettings =
-                    settings.enabled && settings.siteKey && settings.secretKey;
-                const reCaptchaEnabled = get(formData, "reCaptcha.enabled");
+        <Bind name={"reCaptcha.enabled"}>
+            {({ onChange, ...rest }) => (
+                <Grid>
+                    <Cell span={12}>
+                        <Switch
+                            onChange={value => {
+                                if (value && !reCaptchaEnabledInSettings) {
+                                    openDialog();
+                                    return;
+                                }
 
-                return (
-                    <Bind name={"reCaptcha.enabled"}>
-                        {({ onChange, ...rest }) => (
-                            <Grid>
-                                <Cell span={12}>
-                                    <Switch
-                                        onChange={value => {
-                                            if (value && !reCaptchaEnabledInSettings) {
-                                                openDialog();
-                                                return;
-                                            }
+                                onChange(value);
+                            }}
+                            {...rest}
+                            label={"Enabled"}
+                            description={`Will require users to "confirm they are human" by clicking on a special checkbox.`}
+                        />
+                    </Cell>
+                    <Cell span={12}>
+                        <Bind name={"reCaptcha.errorMessage"}>
+                            <Input disabled={!reCaptchaEnabled} label={"Error message"} />
+                        </Bind>
+                    </Cell>
 
-                                            onChange(value);
-                                        }}
-                                        {...rest}
-                                        label={"Enabled"}
-                                        description={`Will require users to "confirm they are human" by clicking on a special checkbox.`}
-                                    />
-                                </Cell>
-                                <Cell span={12}>
-                                    <Bind name={"reCaptcha.errorMessage"}>
-                                        <Input
-                                            disabled={!reCaptchaEnabled}
-                                            label={"Error message"}
-                                        />
-                                    </Bind>
-                                </Cell>
-
-                                {settingsLoaded && !reCaptchaEnabledInSettings && (
-                                    <Cell span={12}>
-                                        <Alert type="danger" title="Google reCAPTCHA not enabled.">
-                                            Click{" "}
-                                            <a href="#" onClick={openDialog}>
-                                                here
-                                            </a>{" "}
-                                            to update your Google reCAPTCHA settings.
-                                        </Alert>
-                                    </Cell>
-                                )}
-                                <ReCaptchaSettingsDialog
-                                    reCaptchaSettings={settings}
-                                    open={dialogOpened}
-                                    onClose={closeDialog}
-                                    onSubmit={() => {
-                                        refetch();
-                                        onChange(true);
-                                        closeDialog();
-                                    }}
-                                />
-                            </Grid>
-                        )}
-                    </Bind>
-                );
-            }}
-        </Query>
+                    {settingsLoaded && !reCaptchaEnabledInSettings && (
+                        <Cell span={12}>
+                            <Alert type="danger" title="Google reCAPTCHA not enabled.">
+                                Click{" "}
+                                <a href="#" onClick={openDialog}>
+                                    here
+                                </a>{" "}
+                                to update your Google reCAPTCHA settings.
+                            </Alert>
+                        </Cell>
+                    )}
+                    <ReCaptchaSettingsDialog
+                        reCaptchaSettings={settings}
+                        open={dialogOpened}
+                        onClose={closeDialog}
+                        onSubmit={() => {
+                            refetch();
+                            onChange(true);
+                            closeDialog();
+                        }}
+                    />
+                </Grid>
+            )}
+        </Bind>
     );
 };
 
