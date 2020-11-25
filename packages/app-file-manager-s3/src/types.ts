3230cc5861cd9f303f05e6b32ede142e29adcb46
Ashutosh <bhardwajashu96@hotmail.com>
2020-11-25 13:04:57 +0530

    feat: add `app-file-manager-s3` package

---
+++ packages/app-file-manager-s3/src/types.ts
@@ -0,0 +1,8 @@
+import { Plugin } from "@webiny/plugins/types";
+import { ApolloClient } from "apollo-client";
+
+export type AppFileManagerStorageS3 = Plugin & {
+    type: "app-file-manager-storage";
+    name: "app-file-manager-storage-s3";
+    upload(file: File, options: { apolloClient: ApolloClient<Record<string, any>> }): Promise<any>;
+};
