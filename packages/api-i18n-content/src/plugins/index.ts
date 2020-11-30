938f270b51f5748d068b0ab051a7cd39ce6e549f
Adrian Smijulj <adrian1358@gmail.com>
2020-11-30 10:51:46 +0100

    feat: add `hasI18NContentPermission` and `checkI18NContentPermission` methods to `context.i18nContent`

--- packages/api-i18n-content/src/plugins/index.ts
+++ packages/api-i18n-content/src/plugins/index.ts
@@ -1,11 +1,33 @@
 import { ContextPlugin } from "@webiny/handler/types";
 import { I18NContext } from "@webiny/api-i18n/types";
 import { I18NContentContext } from "../types";
+import { SecurityContext } from "@webiny/api-security/types";
+import { NotAuthorizedError } from "@webiny/api-security";
 
 export default () =>
     ({
         type: "context",
         apply(context) {
-            context.i18nContent = { locale: context.i18n.getCurrentLocale("content") };
+            context.i18nContent = {
+                locale: context.i18n.getCurrentLocale("content"),
+                hasI18NContentPermission: async () => {
+                    const contentPermission = await context.security.getPermission("content.i18n");
+                    if (!contentPermission) {
+                        return false;
+                    }
+
+                    return (
+                        !Array.isArray(contentPermission.locales) ||
+                        contentPermission.locales.includes(context?.i18nContent?.locale?.code)
+                    );
+                },
+                checkI18NContentPermission: async () => {
+                    if (await context.i18nContent.hasI18NContentPermission()) {
+                        return;
+                    }
+
+                    throw new NotAuthorizedError();
+                }
+            };
         }
-    } as ContextPlugin<I18NContext, I18NContentContext>);
+    } as ContextPlugin<I18NContext, I18NContentContext, SecurityContext>);
