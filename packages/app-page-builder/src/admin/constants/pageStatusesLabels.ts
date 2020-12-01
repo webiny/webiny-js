83c1fb8c6afc3f529c1052d1f04ad31e8c876918
Adrian Smijulj <adrian1358@gmail.com>
2020-12-01 10:35:54 +0100

    fix: encapsulate page status labels in a single constant

---
+++ packages/app-page-builder/src/admin/constants/pageStatusesLabels.ts
@@ -0,0 +1,11 @@
+import { i18n } from "@webiny/app/i18n";
+const t = i18n.ns("app-page-builder/admin/pages/statuses");
+
+
+export default {
+    draft: t`Draft`,
+    published: t`Published`,
+    unpublished: t`Unpublished`,
+    changesRequested: t`Changes Requested`,
+    reviewRequested: t`Review Requested`
+};
