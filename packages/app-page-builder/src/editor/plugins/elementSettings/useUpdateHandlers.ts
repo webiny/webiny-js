7b5c4c58bfbc4e71182af4432b4cd37bd1beafdc
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-02 15:20:28 +0530

    feat: add `postModifyElement` to "useUpdateHandlers"

--- packages/app-page-builder/src/editor/plugins/elementSettings/useUpdateHandlers.ts
+++ packages/app-page-builder/src/editor/plugins/elementSettings/useUpdateHandlers.ts
@@ -6,10 +6,15 @@ import { useMemo } from "react";
 import lodashSet from "lodash/set";
 import lodashMerge from "lodash/merge";
 import { useHandler } from "@webiny/app/hooks/useHandler";
-
+export type PostModifyElementArgs = {
+    name: string;
+    element: PbShallowElement | PbElement;
+    newElement: PbShallowElement | PbElement;
+};
 type UpdateHandlersPropsType = {
     element: PbShallowElement | PbElement;
     dataNamespace: string;
+    postModifyElement?: (args: PostModifyElementArgs) => void;
 };
 type HandlerUpdateCallableType = (name: string) => (value: any) => void;
 type UseUpdateHandlersType = (
@@ -23,12 +28,16 @@ const useUpdateHandlers: UseUpdateHandlersType = props => {
     const updateElement = (args: UpdateElementActionArgsType) => {
         handler.trigger(new UpdateElementActionEvent(args));
     };
-    const updateSettings = useHandler(props, ({ element, dataNamespace }) => {
+    const updateSettings = useHandler(props, ({ element, dataNamespace, postModifyElement }) => {
         const historyUpdated = {};
         return (name: string, newValue: any, history = false) => {
             const propName = `${dataNamespace}.${name}`;
 
             const newElement = lodashMerge({}, element, lodashSet({}, propName, newValue));
+            // post modify the element.
+            if (typeof postModifyElement === "function") {
+                postModifyElement({ name, newElement, element });
+            }
 
             if (!history) {
                 updateElement({
