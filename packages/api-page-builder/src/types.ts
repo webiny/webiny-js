1cc2cf6fff7d90db788630f881f7e9eb429e0763
Adrian Smijulj <adrian1358@gmail.com>
2020-12-03 08:39:48 +0100

    fix: add `PbContext` type

--- packages/api-page-builder/src/types.ts
+++ packages/api-page-builder/src/types.ts
@@ -1,8 +1,23 @@
 import { Plugin } from "@webiny/plugins/types";
-import { Context } from "@webiny/handler/types";
+import { Context, ContextPlugin } from "@webiny/handler/types";
+import { I18NContentContext } from "@webiny/api-i18n-content/types";
+import { DbContext } from "@webiny/handler-db/types";
+import { SecurityContext } from "@webiny/api-security/types";
+import { TenancyContext } from "@webiny/api-security-tenancy/types";
+import { I18NContext } from "@webiny/api-i18n/types";
 
 export type PbInstallPlugin = Plugin & {
     name: "pb-install";
     before: (params: { context: Context; data: any }) => void;
     after: (params: { context: Context; data: any }) => void;
 };
+
+export type PbContext = Context<
+    I18NContentContext,
+    I18NContext,
+    DbContext,
+    SecurityContext,
+    TenancyContext
+>;
+
+export type PbContextPlugin = ContextPlugin<PbContext>;
