f2f41487977ec6a94daa8cb978eca63bda0895e1
Adrian Smijulj <adrian1358@gmail.com>
2020-12-04 12:48:39 +0100

    chore: undo changes

--- packages/api-page-builder/jest.config.js
+++ packages/api-page-builder/jest.config.js
@@ -3,12 +3,5 @@ const esPreset = require("@shelf/jest-elasticsearch/jest-preset");
 const base = require("../../jest.config.base");
 
 module.exports = {
-    ...base(
-        {
-            path: __dirname,
-            collectCoverageFrom: [`src/**/*.ts`],
-            coverageReporters: ["text", "lcov", "html"]
-        },
-        [dynamoDbPreset]
-    )
+    ...base({ path: __dirname }, [dynamoDbPreset, esPreset])
 };
