0d536da0e34b31e8c0e3f5c459ea2bcc535b2e3a
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-07 15:00:29 +0530

    feat: update "RTE" tools config

--- apps/admin/code/src/plugins/pageBuilder/richTextEditor.ts
+++ apps/admin/code/src/plugins/pageBuilder/richTextEditor.ts
@@ -1,9 +1,10 @@
 import Delimiter from "@editorjs/delimiter";
-import Header from "@editorjs/header";
 import Quote from "@editorjs/quote";
 import List from "@editorjs/list";
 import Underline from "@editorjs/underline";
 import Image from "@webiny/app-admin/components/RichTextEditor/tools/image";
+import TextColor from "@webiny/app-admin/components/RichTextEditor/tools/textColor";
+import Header from "@webiny/app-admin/components/RichTextEditor/tools/header";
 
 /**
  * This file contains a RichTextEditor configuration used in Page Builder app.
@@ -18,6 +19,7 @@ export default {
             },
             header: {
                 class: Header,
+                inlineToolbar: ["bold", "italic", "color"],
                 config: {
                     levels: [1, 2, 3, 4]
                 }
@@ -33,6 +35,10 @@ export default {
             },
             underline: {
                 class: Underline
+            },
+            color: {
+                class: TextColor,
+                shortcut: "CMD+M"
             }
         }
     }
