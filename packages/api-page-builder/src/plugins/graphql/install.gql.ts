86e6b137ea687ad4f698d9a5fb44639db2ae6f53
Adrian Smijulj <adrian1358@gmail.com>
2020-12-07 19:07:58 +0100

    fix: add `install` resolvers

--- packages/api-page-builder/src/plugins/graphql/install.gql.ts
+++ packages/api-page-builder/src/plugins/graphql/install.gql.ts
@@ -10,29 +10,64 @@ const plugin: GraphQLSchemaPlugin<PbContext> = {
                 domain: String
                 name: String!
             }
-            type PbBooleanResponse {
+            type PbInstallResponse {
                 data: Boolean
                 error: PbError
             }
 
             extend type PbQuery {
                 # Is Page Builder installed?
-                isInstalled: PbBooleanResponse
+                isInstalled: PbInstallResponse
             }
 
             extend type PbMutation {
                 # Install Page Builder (there are x steps because the process takes a long time).
-                install(step: Int!, data: PbInstallInput!): PbBooleanResponse
+                install(data: PbInstallInput!): PbInstallResponse
             }
         `,
         resolvers: {
             PbQuery: {
-                isInstalled: () => {
-                    return new Response(true);
+                isInstalled: async (_, args, context) => {
+                    const { i18nContent, security } = context;
+                    if (!security.getTenant() || !i18nContent.getLocale()) {
+                        return false;
+                    }
+
+                    const settings = await context.pageBuilder.settings.get({
+                        auth: false
+                    });
+                    return new Response(settings.installed);
                 }
             },
             PbMutation: {
-                install: () => new Response(true)
+                install: async (_, args, context) => {
+                    // 1. Create initial page category.
+                    const staticCategory = await context.pageBuilder.categories.get("static");
+                    if (!staticCategory) {
+                        await context.pageBuilder.categories.create({
+                            name: "Static",
+                            slug: "static",
+                            url: "/static/",
+                            layout: "static"
+                        });
+                    }
+
+                    // 2. Create page blocks.
+
+                    // 3. Create initial menu.
+
+                    // 4. Create sample pages.
+
+                    // 5. Mark the Page Builder app as installed.
+                    const settings = await context.pageBuilder.settings.get();
+                    if (!settings.installed) {
+                        await context.pageBuilder.settings.update({
+                            installed: true
+                        });
+                    }
+
+                    return new Response(true);
+                }
             }
         }
     }
