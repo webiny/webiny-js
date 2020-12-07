02d57abe873edc226e1e21effd5561402b43f664
Adrian Smijulj <adrian1358@gmail.com>
2020-12-07 14:40:19 +0100

    wip: add delete functionality

--- packages/app-page-builder/src/admin/plugins/pageDetails/header/deletePage/DeletePage.tsx
+++ packages/app-page-builder/src/admin/plugins/pageDetails/header/deletePage/DeletePage.tsx
@@ -1,64 +1,95 @@
-import React from "react";
-import dot from "dot-prop-immutable";
+import React, { useCallback } from "react";
 import { useApolloClient } from "react-apollo";
 import { useRouter } from "@webiny/react-router";
-import { get } from "lodash";
-import { useHandler } from "@webiny/app/hooks/useHandler";
 import { useSnackbar } from "@webiny/app-admin/hooks/useSnackbar";
 import { useConfirmationDialog } from "@webiny/app-admin/hooks/useConfirmationDialog";
 import { useDialog } from "@webiny/app-admin/hooks/useDialog";
 import { IconButton } from "@webiny/ui/Button";
 import { Tooltip } from "@webiny/ui/Tooltip";
 import { ReactComponent as DeleteIcon } from "@webiny/app-page-builder/admin/assets/delete.svg";
-import { DELETE_PAGE } from "@webiny/app-page-builder/admin/graphql/pages";
+import { DELETE_PAGE, LIST_PAGES } from "@webiny/app-page-builder/admin/graphql/pages";
+import { i18n } from "@webiny/app/i18n";
+
+const t = i18n.ns("app-headless-cms/app-page-builder/page-details/header/delete-page");
 
 const DeletePage = props => {
+    const { page } = props;
     const client = useApolloClient();
     const { showSnackbar } = useSnackbar();
     const { history } = useRouter();
     const { showDialog } = useDialog();
 
-    const title = get(props, "pageDetails.page.title", "N/A");
-
     const { showConfirmation } = useConfirmationDialog({
-        title: "Delete page",
+        title: t`Delete page`,
         message: (
             <p>
-                You are about to delete the entire page and all of its revisions! <br />
-                Are you sure you want to permanently delete the page <strong>{title}</strong>?
+                {t`You are about to delete the entire page and all of its revisions!`}
+                <br />
+                {t`Are you sure you want to permanently delete the page {title}?`({
+                    title: <strong>{page.title}</strong>
+                })}
             </p>
         )
     });
 
-    const confirmDelete = useHandler(
-        { ...props, showConfirmation },
-        ({ pageDetails: { page }, showConfirmation }) => () => {
+    const confirmDelete = useCallback(
+        () =>
             showConfirmation(async () => {
                 const { data: res } = await client.mutate({
                     mutation: DELETE_PAGE,
-                    variables: { id: page.parent },
-                    refetchQueries: ["PbListPages"]
+                    variables: { id: page.id },
+                    update: (cache: any, { data }) => {
+                        // Don't do anything if there was an error during publishing!
+                        if (data.pageBuilder.deletePage.error) {
+                            return;
+                        }
+
+                        const inMemoryCacheData: Record<string, any> = cache.data.data;
+
+                        console.log("prije deletea", inMemoryCacheData);
+                        // Delete the page from cached responses.
+                        for (const key in inMemoryCacheData) {
+                            if (!key.includes("pageBuilder.listPages")) {
+                                continue;
+                            }
+
+                            if (Array.isArray(inMemoryCacheData[key].data)) {
+                                const data = inMemoryCacheData[key].data.filter(item => {
+                                    item.id !== page.id;
+                                });
+
+                                console.log('dejta',data, page.id, inMemoryCacheData[key].data)
+
+                                inMemoryCacheData[key].data = data;
+                            }
+                        }
+
+                        console.log("poslije delete-a", inMemoryCacheData);
+                        return;
+                    }
                 });
 
-                const { error } = dot.get(res, "pageBuilder.deletePage");
+                const { error } = res?.pageBuilder?.deletePage;
                 if (error) {
-                    return showDialog(error.message, { title: "Could not delete page" });
+                    return showDialog(error.message, { title: t`Could not delete page.` });
                 }
 
                 showSnackbar(
                     <span>
-                        The page{" "}
-                        <strong>
-                            {page.title.substr(0, 20)}
-                            ...
-                        </strong>{" "}
-                        was deleted successfully!
+                        {t`The page {title} was deleted successfully.`({
+                            title: (
+                                <strong>
+                                    {page.title.substr(0, 20)}
+                                    ...
+                                </strong>
+                            )
+                        })}
                     </span>
                 );
 
                 history.push("/page-builder/pages");
-            });
-        }
+            }),
+        [page.id]
     );
 
     return (
