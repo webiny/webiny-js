ef3175a169f7d18e2085cf57069ba8ebb27cb42c
Adrian Smijulj <adrian1358@gmail.com>
2020-12-10 10:39:22 +0100

    chore: put a temporary ts-ignore

--- packages/react-graphql-client/src/index.tsx
+++ packages/react-graphql-client/src/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+// @ts-ignore
 import { GraphQLClient } from "@webiny/graphql-client";
 import { DocumentNode } from "graphql/language/ast";
 
