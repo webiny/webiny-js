cc3f49202a29e34faab5d6117bc001c294977988
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-10 14:10:49 +0530

    feat: add `useRenderEmptyEmbed` hook
    PB sidebar

---
+++ packages/app-page-builder/src/editor/plugins/elements/utils/oembed/useRenderEmptyEmbed.ts
@@ -0,0 +1,12 @@
+import { useCallback } from "react";
+import { plugins } from "@webiny/plugins";
+import { PbElement } from "../../../../../types";
+
+export default (element: PbElement) => {
+    return useCallback(() => {
+        const [pageElementPlugin] = plugins
+            .byType("pb-editor-page-element")
+            .filter(pl => pl.elementType === element.type);
+        return pageElementPlugin.toolbar.preview();
+    }, [element]);
+};
