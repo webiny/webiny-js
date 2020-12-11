ddd840d39f35be9ad852e7c91c25eea60411fb52
Adrian Smijulj <adrian1358@gmail.com>
2020-12-11 09:40:24 +0100

    feat: add simple logging ability

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -18,38 +18,91 @@ export interface DbDriver {
     read: <T = Record<string, any>>(args: Args) => Promise<Result<T[]>>;
     update: (args: Args) => Promise<Result<true>>;
     delete: (args: Args) => Promise<Result<true>>;
+
+    // Logging functions.
+    createLog: (operation: string, args: Args, logTable: string) => Promise<Result<true>>;
+    readLogs: <T = Record<string, any>>(logTable: string) => Promise<Result<T[]>>;
 }
 
 export type OperationType = "create" | "read" | "update" | "delete";
 export type Operation = [OperationType, Args];
 
+export type ConstructorArgs = {
+    driver: DbDriver;
+    table?: string;
+    logTable?: string;
+};
+
 class Db {
     driver: DbDriver;
     table: string;
-    constructor({ driver, table }) {
+    logTable?: string;
+    constructor({ driver, table, logTable }: ConstructorArgs) {
         this.driver = driver;
         this.table = table;
+        this.logTable = logTable;
     }
 
     async create(args: Args): Promise<Result<true>> {
-        return this.driver.create({ ...args, table: args.table || this.table });
+        const createArgs = { ...args, table: args.table || this.table };
+        await this.createLog("create", createArgs);
+        return this.driver.create(createArgs);
     }
 
     async read<T = Record<string, any>>(args: Args): Promise<Result<T[]>> {
-        return this.driver.read({ ...args, table: args.table || this.table });
+        const readArgs = { ...args, table: args.table || this.table };
+        await this.createLog("read", readArgs);
+        return this.driver.read(readArgs);
     }
 
     async update(args: Args): Promise<Result<true>> {
-        return this.driver.update({ ...args, table: args.table || this.table });
+        const updateArgs = { ...args, table: args.table || this.table };
+        await this.createLog("update", updateArgs);
+        return this.driver.update(updateArgs);
     }
+
     async delete(args: Args): Promise<Result<true>> {
-        return this.driver.delete({ ...args, table: args.table || this.table });
+        const deleteArgs = { ...args, table: args.table || this.table };
+        await this.createLog("delete", deleteArgs);
+        return this.driver.delete(deleteArgs);
+    }
+
+    // Logging functions.
+    async createLog(operation, args: Args): Promise<Result<true>> {
+        if (!this.logTable) {
+            return;
+        }
+
+        const data = getCreateLogData(args);
+        return this.driver.createLog(operation, data, this.logTable);
+    }
+
+    async readLogs<T = Record<string, any>>(): Promise<Result<T[]>> {
+        if (!this.logTable) {
+            return;
+        }
+
+        return this.driver.readLogs(this.logTable);
     }
 
     batch() {
         return new Batch(this);
     }
 }
+
+const getCreateLogData = (args: Args) => {
+    const { table, meta, limit, sort, data, query, keys } = args;
+
+    const logData = { table, meta, limit, sort, data, query, keys, batch: null };
+    if (args.__batch) {
+        logData.batch = {
+            id: args.__batch.instance.id,
+            type: args.__batch.instance.type
+        };
+    }
+
+    return logData;
+};
 
 class Batch {
     db: Db;
