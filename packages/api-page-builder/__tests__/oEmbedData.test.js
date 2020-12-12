86e8f9ba93e8b523a3410002cd9f4194af73036f
Adrian Smijulj <adrian1358@gmail.com>
2020-12-12 18:15:00 +0100

    test: ease the test (some data keeps changing)

--- packages/api-page-builder/__tests__/oEmbedData.test.js
+++ packages/api-page-builder/__tests__/oEmbedData.test.js
@@ -5,28 +5,19 @@ describe("oEmbed test", () => {
 
     test("YouTube test", async () => {
         const [response] = await oEmbedData({ url: "https://www.youtube.com/watch?v=sF4HRS9nE3s" });
-        expect(response).toEqual({
+        expect(response).toMatchObject({
             data: {
                 pageBuilder: {
                     oembedData: {
                         data: {
                             author_name: "Webiny",
                             author_url: "https://www.youtube.com/c/Webiny",
-                            height: 270,
-                            html:
-                                '<iframe width="480" height="270" src="https://www.youtube.com/embed/sF4HRS9nE3s?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
                             provider_name: "YouTube",
                             provider_url: "https://www.youtube.com/",
                             source: {
                                 url: "https://www.youtube.com/watch?v=sF4HRS9nE3s"
                             },
-                            thumbnail_height: 360,
-                            thumbnail_url: "https://i.ytimg.com/vi/sF4HRS9nE3s/hqdefault.jpg",
-                            thumbnail_width: 480,
                             title: "Webiny Serverless Apps",
-                            type: "video",
-                            version: "1.0",
-                            width: 480
                         },
                         error: null
                     }
