0fa0e24a80f863ff32c4626b03e19de58e1ce782
Adrian Smijulj <adrian1358@gmail.com>
2020-12-16 17:47:22 +0100

    fix: use `Page` type in `RenderPagesArgs` type

--- packages/api-page-builder/src/graphql/types.ts
+++ packages/api-page-builder/src/graphql/types.ts
@@ -145,7 +145,7 @@ export type ListMeta = {
 
 // Pages CRUD.
 export type Tag = { id?: string; class?: string };
-export type RenderPagesArgs = { tags?: Tag[]; path?: string; queue?: boolean; async?: boolean };
+export type RenderPagesArgs = { tags?: Tag[]; pages?: Page[] };
 
 export type PagesCrud = {
     get(id: string): Promise<Page>;
