0011d3ecdb4dd6019b509eab38f2aec7b11ff5c4
Adrian Smijulj <adrian1358@gmail.com>
2020-12-16 17:52:20 +0100

    fix: extract Commodo models into a separate file

--- packages/api-page-builder/src/graphql/crud/pages.crud.ts
+++ packages/api-page-builder/src/graphql/crud/pages.crud.ts
@@ -1,8 +1,5 @@
 import { ContextPlugin } from "@webiny/handler/types";
 import mdbid from "mdbid";
-import { withFields, string, fields } from "@commodo/fields";
-import { object } from "commodo-fields-object";
-import { validation } from "@webiny/validation";
 import defaults from "./utils/defaults";
 import uniqid from "uniqid";
 import { NotAuthorizedError } from "@webiny/api-security";
@@ -16,6 +13,8 @@ import createListMeta from "./utils/createListMeta";
 import checkBasePermissions from "./utils/checkBasePermissions";
 import checkOwnPermissions from "./utils/checkOwnPermissions";
 import executeHookCallbacks from "./utils/executeHookCallbacks";
+import path from "path";
+import { CreateDataModel, UpdateSettingsModel, UpdateDataModel } from "./pages/models";
 
 const STATUS_CHANGES_REQUESTED = "changesRequested";
 const STATUS_REVIEW_REQUESTED = "reviewRequested";
@@ -24,28 +23,6 @@ const STATUS_PUBLISHED = "published";
 const STATUS_UNPUBLISHED = "unpublished";
 
 const DEFAULT_EDITOR = "page-builder";
-
-const CreateDataModel = withFields({
-    category: string({ validation: validation.create("required,maxLength:100") })
-})();
-
-const UpdateDataModel = withFields({
-    title: string({
-        validation: validation.create("maxLength:150")
-    }),
-    url: string({
-        validation: value => {
-            if (value) {
-                validation.validateSync(value, "maxLength:100");
-                if (!value.startsWith("/")) {
-                    throw new Error('Page path must start with forward slash ("/").');
-                }
-            }
-        }
-    }),
-    category: string({ validation: validation.create("maxLength:100") }),
-    content: object()
-})();
 
 const getZeroPaddedVersionNumber = number => String(number).padStart(4, "0");
 
@@ -61,88 +38,6 @@ type PagePublishedUrl = {
     url: string;
     id: string;
 };
-
-const UpdateSettingsModel = withFields({
-    general: fields({
-        value: {},
-        instanceOf: withFields({
-            tags: string({
-                list: true,
-                validation: value => {
-                    if (!Array.isArray(value)) {
-                        return;
-                    }
-
-                    if (value.length > 30) {
-                        throw new Error("Cannot store more than 30 tags.");
-                    }
-
-                    for (let i = 0; i < value.length; i++) {
-                        validation.validateSync(value[i], "maxLength:50");
-                    }
-                }
-            }),
-            snippet: string({ validation: validation.create("maxLength:500") }),
-            layout: string({ validation: validation.create("maxLength:50") }),
-            image: object()
-        })()
-    }),
-    seo: fields({
-        value: {},
-        instanceOf: withFields({
-            title: string({ validation: validation.create("maxLength:500") }),
-            description: string({ validation: validation.create("maxLength:500") }),
-            meta: fields({
-                list: true,
-                value: [],
-                validation: value => {
-                    if (!Array.isArray(value)) {
-                        return;
-                    }
-
-                    if (value.length > 30) {
-                        throw new Error("Cannot store more than 30 SEO tags.");
-                    }
-                    for (let i = 0; i < value.length; i++) {
-                        validation.validateSync(value[i], "maxLength:50");
-                    }
-                },
-                instanceOf: withFields({
-                    name: string({ validation: validation.create("maxLength:100") }),
-                    content: string({ validation: validation.create("maxLength:200") })
-                })()
-            })
-        })()
-    }),
-    social: fields({
-        value: {},
-        instanceOf: withFields({
-            meta: fields({
-                value: [],
-                list: true,
-                validation: value => {
-                    if (!Array.isArray(value)) {
-                        return;
-                    }
-
-                    if (value.length > 30) {
-                        throw new Error("Cannot store more than 30 social tags.");
-                    }
-                    for (let i = 0; i < value.length; i++) {
-                        validation.validateSync(value[i], "maxLength:50");
-                    }
-                },
-                instanceOf: withFields({
-                    property: string({ validation: validation.create("maxLength:100") }),
-                    content: string({ validation: validation.create("maxLength:200") })
-                })()
-            }),
-            title: string({ validation: validation.create("maxLength:500") }),
-            description: string({ validation: validation.create("maxLength:500") }),
-            image: object()
-        })()
-    })
-})();
 
 const TYPE_PAGE = "pb.page";
 const TYPE_PAGE_LATEST = TYPE_PAGE + ".l";
