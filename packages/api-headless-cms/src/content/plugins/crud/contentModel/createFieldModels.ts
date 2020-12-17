b2a4a079f58286ff7324460a430ed42732314bb4
Pavel Denisjuk <pavel@webiny.com>
2020-12-17 21:04:59 +0100

    fix: store all field properties to DB

--- packages/api-headless-cms/src/content/plugins/crud/contentModel/createFieldModels.ts
+++ packages/api-headless-cms/src/content/plugins/crud/contentModel/createFieldModels.ts
@@ -9,22 +9,11 @@ export const createFieldModels = async (
     model: CmsContentModelType,
     data: CmsContentModelUpdateInputType
 ): Promise<CmsContentModelFieldType[]> => {
-    const fields = [];
+    const fields: CmsContentModelFieldType[] = [];
     for (const field of data.fields) {
         const fieldData = new ContentModelFieldModel().populate(field);
         await fieldData.validate();
-
-        const obj: CmsContentModelFieldType = {
-            id: field.id,
-            fieldId: field.fieldId,
-            label: field.label,
-            helpText: field.helpText,
-            multipleValues: field.multipleValues,
-            settings: field.settings,
-            type: field.type,
-            validation: field.validation
-        };
-        fields.push(obj);
+        fields.push(await fieldData.toJSON());
     }
     return fields;
 };
