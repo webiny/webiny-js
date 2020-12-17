b1395bbb46817e9719ee1146dafd699a4781e8e7
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-17 14:32:51 +0530

    fix: replace `TextRenderer` with RTE data renderer plugin

--- packages/app-page-builder/src/render/plugins/elements/text/Text.tsx
+++ packages/app-page-builder/src/render/plugins/elements/text/Text.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { ElementRoot } from "@webiny/app-page-builder/render/components/ElementRoot";
 import { PbElement } from "@webiny/app-page-builder/types";
-import TextRenderer from "../RichTextEditorOutputRenderer";
+import { plugins } from "@webiny/plugins";
 
 type TextPropsType = {
     element: PbElement;
 };
 const Text: React.FunctionComponent<TextPropsType> = ({ element }) => {
+    const [rendererPlugin] = plugins
+        .byType("rte-data-renderer")
+        .filter(pl => pl.outputType === "jsx");
+
     return (
         <ElementRoot element={element} className={className}>
-            <TextRenderer data={element.data.text} />
+            {rendererPlugin.render(element.data.text)}
         </ElementRoot>
     );
 };
