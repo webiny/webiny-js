fe1e8ea8445b580a86dcd1fc38a430b962391f84
Pavel Denisjuk <pavel@webiny.com>
2020-12-18 15:42:57 +0100

    chore: remove cms template and unused CLI plugins

--- packages/cwp-template-cms/template/api/files/download/webiny.config.js
+++
@@ -1,7 +0,0 @@
-const { buildFunction } = require("@webiny/serverless-files/functions/download/bundle");
-
-module.exports = {
-    commands: {
-        build: buildFunction
-    }
-};
--- packages/cwp-template-cms/template/api/files/manage/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { createHandler } from "@webiny/handler";
-import manageFilePlugins from "@webiny/serverless-files/functions/manage";
-
-export const handler = createHandler(manageFilePlugins());
