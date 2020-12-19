adaf59cd306ebc0c905e93e332996ffeea6fa89a
Adrian Smijulj <adrian1358@gmail.com>
2020-12-19 13:47:50 +0100

    wip: add `hook-after-deploy-pb-update-settings` plugin

--- webiny.root.js
+++ webiny.root.js
@@ -1,3 +1,6 @@
+const { Pulumi } = require("@webiny/pulumi-sdk");
+const path = require("path");
+
 module.exports = {
     projectName: "webiny-js",
     cli: {
@@ -12,7 +15,37 @@ module.exports = {
             require("@webiny/cli-plugin-scaffold-react-package"),
             require("@webiny/cli-plugin-scaffold-react-app"),
             require("@webiny/cli-plugin-build")(),
-            require("@webiny/cli-plugin-deploy-pulumi")()
+            require("@webiny/cli-plugin-deploy-pulumi")(),
+            {
+                type: "hook-after-deploy",
+                name: "hook-after-deploy-pb-update-settings",
+                async hook(args, context) {
+                    if (args.stack !== "site") {
+                        return;
+                    }
+
+                    // When the "site" stack is deployed, let's make sure we update Page Builder app's settings
+                    // with necessary pieces of information. This way the user doesn't have to do this manually.
+                    const pulumi = new Pulumi({
+                        execa: {
+                            cwd: path.join(context.paths.projectRoot, "apps", "site")
+                        }
+                    });
+
+                    await pulumi.run({ command: ["stack", "select", args.env] });
+                    const { stdout } = await pulumi.run({
+                        command: ["stack", "output"],
+                        args: {
+                            stack: args.env,
+                            json: true
+                        }
+                    });
+
+                    const state = JSON.parse(stdout);
+                    console.log("TODO: send this to PB app:");
+                    console.log(JSON.stringify(state, null, 2));
+                }
+            }
         ]
     }
 };
