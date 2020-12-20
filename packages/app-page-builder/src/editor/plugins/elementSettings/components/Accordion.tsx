87bffa88f2ce026f4fcfe64f8195986b25120cb1
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-20 16:11:50 +0530

    fix: color picker position in sidebar settings
    PB sidebar UX

--- packages/app-page-builder/src/editor/plugins/elementSettings/components/Accordion.tsx
+++ packages/app-page-builder/src/editor/plugins/elementSettings/components/Accordion.tsx
@@ -6,6 +6,11 @@ import { Typography } from "@webiny/ui/Typography";
 
 const classes = {
     accordionWrapper: css({
+        /**
+         * We're using position: "relative" here for "Popping Out of Hidden Overflow" https://css-tricks.com/popping-hidden-overflow/
+         * so that, we can use absolute positioned element inside without "overflow: hidden" being a problem
+         */
+        position: "relative",
         width: "100%",
         borderBottom: `1px solid ${COLORS.gray}`
     }),
