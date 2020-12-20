7934986aa33f86a605ee6814bbc5307072acb529
Adrian Smijulj <adrian1358@gmail.com>
2020-12-20 10:25:14 +0100

    feat: introduce `api-prerendering-service`

---
+++ packages/api-prerendering-service/README.md
@@ -0,0 +1,5 @@
+# @webiny/api-prerendering-service
+[![](https://img.shields.io/npm/dw/@webiny/api-prerendering-service.svg)](https://www.npmjs.com/package/@webiny/api-prerendering-service) 
+[![](https://img.shields.io/npm/v/@webiny/api-prerendering-service.svg)](https://www.npmjs.com/package/@webiny/api-prerendering-service)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
--- packages/api-prerendering-service/src/render/index.ts
+++ packages/api-prerendering-service/src/render/index.ts
@@ -3,92 +3,90 @@ import { ArgsContext } from "@webiny/handler-args/types";
 import renderPage from "./renderPage";
 import path from "path";
 import S3 from "aws-sdk/clients/s3";
-import { DbContext } from "@webiny/handler-db/types";
-import defaults from "@webiny/api-page-builder/utils/defaults";
 
 const s3 = new S3({ region: process.env.AWS_REGION });
 
-export type Page = {
-    path: string;
-    tenant: string;
-    locale: string;
-};
-
 export type Configuration = {
-    storageName: string;
+    website?: {
+        url?: string;
+    };
+    storage?: {
+        name?: string;
+        folder?: string;
+    };
 };
 
-export default (
-    configuration: Configuration
-): HandlerPlugin<
-    DbContext,
-    ArgsContext<{
-        paths: Page[];
-    }>
-> => ({
+export type Args = {
+    configuration?: Configuration;
+    url?: string;
+    path?: string;
+};
+
+export type HandlerArgs = Args | Args[];
+export type HandlerResponse = { data: Record<string, any>; error: Record<string, any> };
+
+export default (configuration?: Configuration): HandlerPlugin<ArgsContext<HandlerArgs>> => ({
     type: "handler",
-    async handle(context) {
-        const { invocationArgs: args, db } = context;
-        if (!Array.isArray(args.paths)) {
-            return;
-        }
-
-        const { storageName } = configuration;
-
-        const __settings = {};
-        const getSettings = async (tenant, locale) => {
-            // T#root#L#en-US#PB#SETTINGS
-            const PK = `T#${tenant}#L#${locale}#PB#SETTINGS`;
-
-            if (PK in __settings) {
-                return __settings[PK];
-            }
-
-            const [[settings]] = await db.read({
-                ...defaults.db,
-                query: { PK, SK: "default" },
-                limit: 1
-            });
-
-            __settings[PK] = settings;
-            return __settings[PK];
-        };
+    async handle(context): Promise<HandlerResponse> {
+        const { invocationArgs } = context;
+        const handlerArgs = Array.isArray(invocationArgs) ? invocationArgs : [invocationArgs];
 
         const promises = [];
-        for (let i = 0; i < args.paths.length; i++) {
-            const current = args.paths[i];
-            const settings = await getSettings(current.tenant, current.locale);
 
-            promises.push(
-                new Promise(async resolve => {
-                    const files = await renderPage(settings.domain + current.path);
-                    for (let j = 0; j < files.length; j++) {
-                        const file = files[j];
+        try {
+            for (let i = 0; i < handlerArgs.length; i++) {
+                const args = handlerArgs[i];
 
-                        const key = path.join(
-                            current.tenant,
-                            current.locale,
-                            current.path,
-                            file.name
-                        );
+                promises.push(
+                    new Promise(async resolve => {
+                        const url = getRenderUrl(args, configuration);
+                        const files = await renderPage(url);
 
-                        await storeFile({
-                            key,
-                            body: file.body,
-                            contentType: file.type,
-                            storageName
-                        });
-                    }
-                    resolve();
-                })
-            );
+                        const storageName = getStorageName(args, configuration);
+                        const storageFolder = getStorageFolder(args, configuration);
+
+                        for (let j = 0; j < files.length; j++) {
+                            const file = files[j];
+
+                            const key = path.join(storageFolder, file.name);
+
+                            await storeFile({
+                                storageName,
+                                key,
+                                body: file.body,
+                                contentType: file.type
+                            });
+                        }
+                        resolve();
+                    })
+                );
+            }
+
+            await Promise.all(promises);
+
+            return { data: null, error: null };
+        } catch (e) {
+            return { data: null, error: e };
         }
-
-        await Promise.all(promises);
-
-        return {};
     }
 });
+
+const getRenderUrl = (args: Args, configuration: Configuration) => {
+    if (args.url) {
+        return args.url;
+    }
+
+    const websiteUrl = args?.configuration?.website.url || configuration?.website?.url;
+    return path.join(websiteUrl, args.path);
+};
+
+const getStorageName = (args: Args, configuration: Configuration) => {
+    return args?.configuration?.storage.name || configuration?.storage.name;
+};
+
+const getStorageFolder = (args: Args, configuration: Configuration) => {
+    return args?.configuration?.storage.folder || configuration?.storage.folder;
+};
 
 const storeFile = ({ key, contentType, body, storageName }) => {
     return s3
