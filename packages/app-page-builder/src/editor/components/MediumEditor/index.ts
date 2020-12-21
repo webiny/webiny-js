8d832e227623f4dc1fb42740dd14993b9b3f3f8c
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-21 23:21:11 +0530

    feat: add `ReactMediumEditor`
    PB editor

---
+++ packages/app-page-builder/src/editor/components/MediumEditor/index.ts
@@ -0,0 +1,80 @@
+import React from "react";
+import MediumEditor from "medium-editor";
+import { css } from "emotion";
+// FIXME: Move to appropriate location
+// load theme styles with webpack
+require("medium-editor/dist/css/medium-editor.css");
+require("medium-editor/dist/css/themes/default.css");
+
+const editorClass = css({
+    "&:focus": {
+        outline: "none"
+    },
+    "& b": {
+        fontWeight: "bold"
+    },
+    "& u": {
+        textDecoration: "underline"
+    },
+    "& i": {
+        fontStyle: "italic"
+    },
+    "& h1,h2,h3,h4,h5,h6": {
+        fontSize: "unset"
+    }
+});
+
+type ReactMediumEditorProps = {
+    value: string;
+    onChange: (value: string) => void;
+    tag?: string;
+    options?: any;
+    [key: string]: any;
+};
+
+const ReactMediumEditor = ({ tag = "div", value, options, ...props }: ReactMediumEditorProps) => {
+    const elementRef = React.useRef();
+    const editorRef = React.useRef();
+
+    React.useEffect(() => {
+        if (!elementRef && !elementRef.current) {
+            return;
+        }
+
+        editorRef.current = new MediumEditor(elementRef.current, options);
+
+        const handleChange = (data, editable) => {
+            if (props.onChange) {
+                // @ts-ignore
+                editorRef.current.restoreSelection();
+                props.onChange(editable.innerHTML);
+            }
+        };
+
+        // Subscribe to onchange
+        // @ts-ignore
+        editorRef.current.subscribe("blur", handleChange);
+
+        return () => {
+            // UnSubscribe to onchange
+            // @ts-ignore
+            editorRef.current.unsubscribe("blur", handleChange);
+            // @ts-ignore
+            editorRef.current.destroy();
+        };
+    }, [props, options]);
+
+    if (editorRef.current) {
+        // @ts-ignore
+        editorRef.current.saveSelection();
+    }
+
+    return React.createElement(tag, {
+        ...props,
+        dangerouslySetInnerHTML: { __html: value },
+        ref: elementRef,
+        className: editorClass
+    });
+};
+
+export default React.memo(ReactMediumEditor);
