e8c7df34b4e8c68623a1c840160eb9e2612ac69b
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-21 16:06:17 +0530

    feat: use `"pb-config"` plugin in editor provider
    PB editor

--- packages/app-page-builder/src/editor/provider.tsx
+++ packages/app-page-builder/src/editor/provider.tsx
@@ -1,3 +1,8 @@
+import React, { createContext, useContext } from "react";
+import { useApolloClient } from "react-apollo";
+import { plugins } from "@webiny/plugins";
+import merge from "lodash/merge";
+import { PbConfigPluginType, PbConfigType } from "../types";
 import { EventActionHandler } from "@webiny/app-page-builder/editor/recoil/eventActions";
 import {
     contentAtom,
@@ -9,8 +14,15 @@ import {
 } from "@webiny/app-page-builder/editor/recoil/modules";
 import { connectedAtomValue } from "@webiny/app-page-builder/editor/recoil/modules/connected";
 import { PbState } from "@webiny/app-page-builder/editor/recoil/modules/types";
-import React, { createContext, useContext } from "react";
-import { useApolloClient } from "react-apollo";
+
+const createConfiguration = (plugins: PbConfigPluginType[]): PbConfigType => {
+    return plugins.reduce(
+        (acc, pl) => {
+            return merge(acc, pl.config());
+        },
+        { maxEventActionsNesting: 5 }
+    );
+};
 
 type ProviderType = {
     eventActionHandler: EventActionHandler;
@@ -20,9 +32,14 @@ export const EditorProvider: React.FunctionComponent<any> = props => {
     const apolloClient = useApolloClient();
     const handlerRef = React.useRef<EventActionHandler>(null);
     if (!handlerRef.current) {
-        handlerRef.current = new EventActionHandler(["content"], {
-            client: apolloClient
-        });
+        const config = createConfiguration(plugins.byType("pb-config"));
+        handlerRef.current = new EventActionHandler(
+            ["content"],
+            {
+                client: apolloClient
+            },
+            config
+        );
     }
     const provider: ProviderType = {
         eventActionHandler: handlerRef.current
