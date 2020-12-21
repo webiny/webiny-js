75c03720e1214ab8be803af199b7bfdfdf67fee7
Adrian Smijulj <adrian1358@gmail.com>
2020-12-21 11:44:12 +0100

    fix: replace `string` with `"string"`

--- packages/api-page-builder/src/graphql/crud/pages/normalizePath.ts
+++ packages/api-page-builder/src/graphql/crud/pages/normalizePath.ts
@@ -1,7 +1,7 @@
 import trim from "lodash/trim";
 
 export default (value: string) => {
-    if (typeof value === string) {
+    if (typeof value === "string") {
         return "/" + trim(value, "/");
     }
     return null;
