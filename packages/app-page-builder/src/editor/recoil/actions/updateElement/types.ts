0ae63a26048e1ffcf972b342652bfaed74a8cd4a
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-21 14:31:53 +0530

    feat: make `history` required in UpdateElementActionArgsType

--- packages/app-page-builder/src/editor/recoil/actions/updateElement/types.ts
+++ packages/app-page-builder/src/editor/recoil/actions/updateElement/types.ts
@@ -3,5 +3,5 @@ import { PbElement } from "@webiny/app-page-builder/types";
 export type UpdateElementActionArgsType = {
     element: PbElement;
     merge?: boolean;
-    history?: boolean;
+    history: boolean;
 };
