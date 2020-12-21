22c66f6c4e1e7a5659fbd615c7df4597faec2b08
Adrian Smijulj <adrian1358@gmail.com>
2020-12-21 11:52:47 +0100

    chore: add comments

--- packages/api-i18n-content/src/index.ts
+++ packages/api-i18n-content/src/index.ts
@@ -6,11 +6,16 @@ import { Context } from "@webiny/handler/types";
 export const hasI18NContentPermission = () => {
     return (resolver: GraphQLFieldResolver) => {
         return async (parent, args, context: Context<I18NContentContext>, info) => {
+            // If `content.i18n` permission is not present, immediately throw.
             const contentPermission = await context.security.getPermission("content.i18n");
             if (!contentPermission) {
                 return new NotAuthorizedResponse();
             }
 
+            // Otherwise, let's check if the identity has access to current content locale.
+            // If `contentPermission.locales` array is present, that means identity's access is restricted
+            // to the locales listed in it. If it's not present, that means there are no restrictions, or
+            // in other words - identity can access all locales.
             const hasLocaleAccess =
                 !Array.isArray(contentPermission.locales) ||
                 contentPermission.locales.includes(context?.i18nContent?.locale?.code);
