601077d712741072d39096e11171c36cdc2f433f
Adrian Smijulj <adrian1358@gmail.com>
2020-12-22 13:53:58 +0100

    wip: add `after-deploy` hook for `site` stack

--- webiny.root.js
+++ webiny.root.js
@@ -1,4 +1,5 @@
 const { Pulumi } = require("@webiny/pulumi-sdk");
+const LambdaClient = require("aws-sdk/clients/lambda");
 const path = require("path");
 
 module.exports = {
@@ -26,24 +27,67 @@ module.exports = {
 
                     // When the "site" stack is deployed, let's make sure we update Page Builder app's settings
                     // with necessary pieces of information. This way the user doesn't have to do this manually.
-                    const pulumi = new Pulumi({
-                        execa: {
-                            cwd: path.join(context.paths.projectRoot, "apps", "site")
-                        }
+                    const siteStackDir = path.join(context.paths.projectRoot, "apps", "site");
+                    const apiStackDir = path.join(context.paths.projectRoot, "api");
+
+                    const pulumi = new Pulumi();
+
+                    // 1. Get exports from `site` stack, for `args.env` environment.
+                    await pulumi.run({
+                        command: ["stack", "select", args.env],
+                        args: { cwd: siteStackDir }
                     });
 
-                    await pulumi.run({ command: ["stack", "select", args.env] });
-                    const { stdout } = await pulumi.run({
-                        command: ["stack", "output"],
-                        args: {
-                            stack: args.env,
-                            json: true
-                        }
+                    const siteExports = await pulumi
+                        .run({
+                            command: ["stack", "output"],
+                            args: {
+                                stack: args.env,
+                                cwd: siteStackDir,
+                                json: true
+                            }
+                        })
+                        .then(({ stdout }) => JSON.parse(stdout));
+
+                    // 2. Get exports from `api` stack, again, for `args.env` environment.
+                    await pulumi.run({
+                        command: ["stack", "select", args.env],
+                        args: { cwd: apiStackDir }
                     });
 
-                    const state = JSON.parse(stdout);
-                    console.log("TODO: send this to PB app:");
-                    console.log(JSON.stringify(state, null, 2));
+                    const apiExports = await pulumi
+                        .run({
+                            command: ["stack", "output"],
+                            args: {
+                                stack: args.env,
+                                cwd: apiStackDir,
+                                json: true
+                            }
+                        })
+                        .then(({ stdout }) => JSON.parse(stdout));
+
+                    // 3. Let's update relevant Page Builde app's URLs, by invoking the `updateSettings` function,
+                    // which has been exported from the `api` stack for this exact purpose.
+                    const lambdaClient = new LambdaClient({ region: apiExports.region });
+                    const response = await lambdaClient
+                        .invoke({
+                            FunctionName: apiExports.updatePageBuilderSettingsFunction,
+                            Payload: JSON.stringify({
+                                data: {
+                                    prerendering: {
+                                        app: {
+                                            url: siteExports.appURL
+                                        },
+                                        storage: {
+                                            name: siteExports.deliveryStorage
+                                        }
+                                    }
+                                }
+                            })
+                        })
+                        .promise();
+
+                    console.log('dobeo response', response)
                 }
             }
         ]
