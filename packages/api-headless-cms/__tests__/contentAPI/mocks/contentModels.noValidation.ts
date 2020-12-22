083c35974bbc2a6e2b481fce768de915e2ab4887
Pavel Denisjuk <pavel@webiny.com>
2020-12-22 22:44:23 +0100

    test: add a model mock without field validation

---
+++ packages/api-headless-cms/__tests__/contentAPI/mocks/contentModels.noValidation.ts
@@ -0,0 +1,63 @@
+import shortId from "shortid";
+import contentModelGroup from "./contentModelGroup";
+import { CmsContentModelType } from "@webiny/api-headless-cms/types";
+
+const ids = {
+    field11: shortId.generate(),
+    field12: shortId.generate()
+};
+
+const models: CmsContentModelType[] = [
+    {
+        createdOn: new Date(),
+        savedOn: new Date(),
+        titleFieldId: "title",
+        lockedFields: [],
+        name: "Category",
+        description: "Product category",
+        modelId: "category",
+        group: {
+            id: contentModelGroup.id,
+            name: contentModelGroup.name
+        },
+        layout: [[ids.field11], [ids.field12]],
+        fields: [
+            {
+                id: ids.field11,
+                multipleValues: false,
+                helpText: "",
+                label: "Title",
+                type: "text",
+                fieldId: "title",
+                validation: [],
+                placeholderText: "placeholder text",
+                predefinedValues: {
+                    enabled: false,
+                    values: []
+                },
+                renderer: {
+                    name: "renderer"
+                }
+            },
+            {
+                id: ids.field12,
+                multipleValues: false,
+                helpText: "",
+                label: "Slug",
+                type: "text",
+                fieldId: "slug",
+                validation: [],
+                placeholderText: "placeholder text",
+                predefinedValues: {
+                    enabled: false,
+                    values: []
+                },
+                renderer: {
+                    name: "renderer"
+                }
+            }
+        ]
+    }
+];
+
+export default models;
