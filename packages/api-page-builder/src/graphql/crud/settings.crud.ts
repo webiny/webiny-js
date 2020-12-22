816b5c43adcd74c762e0b63d973d7f5ce8c90535
Adrian Smijulj <adrian1358@gmail.com>
2020-12-22 10:18:31 +0100

    fix: import settings model from utils folder

--- packages/api-page-builder/src/graphql/crud/settings.crud.ts
+++ packages/api-page-builder/src/graphql/crud/settings.crud.ts
@@ -1,56 +1,11 @@
 import { ContextPlugin } from "@webiny/handler/types";
 import defaults from "./utils/defaults";
-import { withFields, string, fields, boolean, onSet } from "@commodo/fields";
-import { object } from "commodo-fields-object";
 import getPKPrefix from "./utils/getPKPrefix";
 import { PbContext, Settings, SettingsHookPlugin } from "@webiny/api-page-builder/types";
 import { NotAuthorizedError } from "@webiny/api-security";
 import DataLoader from "dataloader";
 import executeHookCallbacks from "./utils/executeHookCallbacks";
-import trimEnd from "lodash/trimEnd";
-
-// We don't want trailing slashes in Page Builder app's important URLs (website URL, website preview URL, app URL).
-const trimTrailingSlashes = value => trimEnd(value, "/");
-
-const SettingsModel = withFields({
-    installed: boolean({ value: false }),
-    name: string({ validation: "required,maxLength:500" }),
-    websiteUrl: onSet(trimTrailingSlashes)(string({ validation: "url,maxLength:500" })),
-    websitePreviewUrl: onSet(trimTrailingSlashes)(string({ validation: "url,maxLength:500" })),
-    favicon: object({}),
-    logo: object({}),
-    prerendering: fields({
-        instanceOf: withFields({
-            app: fields({
-                instanceOf: withFields({
-                    url: onSet(trimTrailingSlashes)(string({ validation: "url" }))
-                })()
-            }),
-            storage: fields({
-                instanceOf: withFields({
-                    name: string({ validation: "maxLength:500" })
-                })()
-            })
-        })()
-    }),
-    social: fields({
-        value: {},
-        instanceOf: withFields({
-            facebook: string({ validation: "url,maxLength:500" }),
-            twitter: string({ validation: "url,maxLength:500" }),
-            instagram: string({ validation: "url,maxLength:500" }),
-            image: object({})
-        })()
-    }),
-    pages: fields({
-        value: {},
-        instanceOf: withFields({
-            home: string(),
-            notFound: string(),
-            error: string()
-        })()
-    })
-})();
+import { SettingsModel } from "@webiny/api-page-builder/utils/models";
 
 const TYPE = "pb.settings";
 
