3194ed247d6e5e28454769a353b8fecb681dac32
Adrian Smijulj <adrian1358@gmail.com>
2020-12-22 10:18:49 +0100

    fix: enable generation without locale/tenant prefixes

--- packages/api-page-builder/src/graphql/crud/utils/getPKPrefix.ts
+++ packages/api-page-builder/src/graphql/crud/utils/getPKPrefix.ts
@@ -1,16 +1,40 @@
 import { I18NContentContext } from "@webiny/api-i18n-content/types";
 import { TenancyContext } from "@webiny/api-security-tenancy/types";
 import { Context } from "@webiny/handler/types";
+import Error from "@webiny/error";
 
-export default (context: Context<TenancyContext, I18NContentContext>) => {
-    const { security, i18nContent } = context;
-    if (!security.getTenant()) {
-        throw new Error("Tenant missing.");
+type Options = {
+    includeTenant?: boolean;
+    includeLocale?: boolean;
+};
+
+export default (context: Context<TenancyContext, I18NContentContext>, options: Options = {}) => {
+    let prefix = "PB#";
+    if (!options.includeTenant && !options.includeLocale) {
+        return prefix;
     }
 
-    if (!i18nContent.getLocale()) {
-        throw new Error("Locale missing.");
+    if (options.includeLocale !== false) {
+        const { i18nContent } = context;
+        if (!i18nContent.getLocale()) {
+            throw new Error(
+                `Cannot construct primary key - locale missing.`,
+                "PAGE_BUILDER_CANNOT_CONSTRUCT_PK"
+            );
+        }
+        prefix = `L#${i18nContent.getLocale().code}#${prefix}`;
     }
 
-    return `T#${security.getTenant().id}#L#${i18nContent.getLocale().code}#PB#`;
+    if (options.includeTenant !== false) {
+        const { security } = context;
+        if (!security.getTenant()) {
+            throw new Error(
+                `Cannot construct primary key - tenant missing.`,
+                "PAGE_BUILDER_CANNOT_CONSTRUCT_PK"
+            );
+        }
+        prefix = `T#${security.getTenant().id}#${prefix}`;
+    }
+
+    return prefix;
 };
