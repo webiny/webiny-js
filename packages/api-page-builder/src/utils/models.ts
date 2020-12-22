d81eb8a7b2adfe15a3daf726cf5aa438a14f43eb
Adrian Smijulj <adrian1358@gmail.com>
2020-12-22 16:46:07 +0100

    fix: correct name

--- packages/api-page-builder/src/utils/models.ts
+++ packages/api-page-builder/src/utils/models.ts
@@ -1,2 +1,2 @@
 export { default as DefaultSettingsModel } from "./models/DefaultSettings.model";
-export { default as InstallSettings } from "./models/InstallSettings.model";
+export { default as InstallSettingsModel } from "./models/InstallSettings.model";
