a62d5bf15e135514fcfff13201992580b8ffb7a5
Adrian Smijulj <adrian1358@gmail.com>
2020-12-23 19:57:34 +0100

    test: prevent unpublish if page set as special

--- packages/api-page-builder/__tests__/graphql/specialPages.test.js
+++ packages/api-page-builder/__tests__/graphql/specialPages.test.js
@@ -1,11 +1,27 @@
 import useGqlHandler from "./useGqlHandler";
 
+const CANNOT_UNPUBLISH_RESPONSE = type => ({
+    data: {
+        pageBuilder: {
+            unpublishPage: {
+                data: null,
+                error: {
+                    code: "",
+                    data: null,
+                    message: `Cannot unpublish page because it's set as ${type}`
+                }
+            }
+        }
+    }
+});
+
 describe("Settings Test", () => {
     const {
         createCategory,
         createPage,
         updateSettings,
         publishPage,
+        unpublishPage,
         getPublishedPage,
         deleteElasticSearchIndex
     } = useGqlHandler();
@@ -32,22 +48,20 @@ describe("Settings Test", () => {
 
         await getPublishedPage({ path: "/" }).then(([res]) =>
             expect(res).toEqual({
-                "data": {
-                    "pageBuilder": {
-                        "getPublishedPage": {
-                            "data": null,
-                            "error": {
-                                "code": "NOT_FOUND",
-                                "data": null,
-                                "message": "Page not found."
+                data: {
+                    pageBuilder: {
+                        getPublishedPage: {
+                            data: null,
+                            error: {
+                                code: "NOT_FOUND",
+                                data: null,
+                                message: "Page not found."
                             }
                         }
                     }
                 }
             })
         );
-
-        const [pid] = page.id.split("#");
 
         await updateSettings({
             data: {
@@ -59,20 +73,111 @@ describe("Settings Test", () => {
 
         await getPublishedPage({ path: "/" }).then(([res]) =>
             expect(res).toMatchObject({
-                "data": {
-                    "pageBuilder": {
-                        "getPublishedPage": {
-                            "data": {
-                                "category": {
-                                    "slug": "category"
+                data: {
+                    pageBuilder: {
+                        getPublishedPage: {
+                            data: {
+                                category: {
+                                    slug: "category"
                                 },
-                                "locked": true,
-                                "path": /^some-url\/untitled-/,
-                                "status": "published",
-                                "title": "Untitled",
-                                "version": 1
+                                locked: true,
+                                path: /^some-url\/untitled-/,
+                                status: "published",
+                                title: "Untitled",
+                                version: 1
                             },
-                            "error": null
+                            error: null
+                        }
+                    }
+                }
+            })
+        );
+    });
+
+    test("prevent unpublish if page is special", async () => {
+        await createCategory({
+            data: {
+                slug: `category`,
+                name: `name`,
+                url: `/some-url/`,
+                layout: `layout`
+            }
+        });
+
+        let p1 = await createPage({ category: "category" }).then(
+            ([res]) => res.data.pageBuilder.createPage.data
+        );
+
+        let p2 = await createPage({ category: "category" }).then(
+            ([res]) => res.data.pageBuilder.createPage.data
+        );
+
+        await publishPage({ id: p1.id });
+        await publishPage({ id: p2.id });
+
+        await updateSettings({
+            data: {
+                pages: {
+                    home: p1.id,
+                    error: p1.id,
+                    notFound: p1.id
+                }
+            }
+        });
+
+        await unpublishPage({ id: p1.id }).then(([res]) =>
+            expect(res).toEqual(CANNOT_UNPUBLISH_RESPONSE("home"))
+        );
+
+        await updateSettings({
+            data: {
+                pages: {
+                    home: p2.id,
+                    error: p1.id,
+                    notFound: p1.id
+                }
+            }
+        });
+        await unpublishPage({ id: p1.id }).then(([res]) =>
+            expect(res).toEqual(CANNOT_UNPUBLISH_RESPONSE("notFound"))
+        );
+
+        await updateSettings({
+            data: {
+                pages: {
+                    home: p2.id,
+                    error: p2.id,
+                    notFound: p1.id
+                }
+            }
+        });
+        await unpublishPage({ id: p1.id }).then(([res]) =>
+            expect(res).toEqual(CANNOT_UNPUBLISH_RESPONSE("notFound"))
+        );
+
+        await updateSettings({
+            data: {
+                pages: {
+                    home: p2.id,
+                    error: p2.id,
+                    notFound: p2.id
+                }
+            }
+        });
+
+        await unpublishPage({ id: p1.id }).then(([res]) =>
+            expect(res).toMatchObject({
+                data: {
+                    pageBuilder: {
+                        unpublishPage: {
+                            data: {
+                                locked: true,
+                                path: /^some-url\/untitled-/,
+                                status: "unpublished",
+                                title: "Untitled",
+                                version: 1
+                            },
+                            error: null
                         }
                     }
                 }
