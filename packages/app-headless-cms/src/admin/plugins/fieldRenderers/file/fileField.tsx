4067bd9b3b88a1c481120ea3e65781f3386dd38c
Pavel Denisjuk <pavel@webiny.com>
2020-12-23 22:50:27 +0100

    fix: migrate file(s) field renderer

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/file/fileField.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/file/fileField.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { CmsEditorFieldRendererPlugin } from "@webiny/app-headless-cms/types";
 import { i18n } from "@webiny/app/i18n";
 import { Cell, Grid } from "@webiny/ui/Grid";
-import SingleFile from "./SingleFile";
+import { FileManager } from "@webiny/app-admin/components";
+import File from "./File";
 
 const t = i18n.ns("app-headless-cms/admin/fields/file");
 
@@ -24,12 +25,27 @@ const plugin: CmsEditorFieldRendererPlugin = {
                     <Cell span={12}>
                         <Label>{field.label}</Label>
                         <Bind>
-                            {bind => (
-                                <SingleFile
-                                    field={field}
-                                    bind={bind}
-                                    description={field.helpText}
-                                />
+                            {({ value, onChange }) => (
+                                <FileManager multiple={false}>
+                                    {({ showFileManager }) => {
+                                        const selectFile = () => {
+                                            showFileManager(newFile => {
+                                                if (newFile !== null) {
+                                                    onChange(newFile.src);
+                                                }
+                                            });
+                                        };
+
+                                        return (
+                                            <File
+                                                url={value}
+                                                onRemove={() => onChange(null)}
+                                                showFileManager={selectFile}
+                                                placeholder={t`Select a file"`}
+                                            />
+                                        );
+                                    }}
+                                </FileManager>
                             )}
                         </Bind>
                     </Cell>
