c148ba1d33735e2e942771c9ede853de515cf6a1
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-23 15:21:51 +0530

    feat(app-form-builder): update `Form` element advanced settings
    update API queries logic

--- packages/app-form-builder/src/page-builder/admin/plugins/components/FormElementAdvancedSettings.tsx
+++ packages/app-form-builder/src/page-builder/admin/plugins/components/FormElementAdvancedSettings.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { Query } from "react-apollo";
-import { get } from "lodash";
+import React, { useMemo } from "react";
+import { useLazyQuery, useQuery } from "react-apollo";
+import get from "lodash/get";
 import { Grid, Cell } from "@webiny/ui/Grid";
 import { Alert } from "@webiny/ui/Alert";
 import { AutoComplete } from "@webiny/ui/AutoComplete";
@@ -12,120 +12,132 @@ import {
     SimpleButton,
     classes
 } from "@webiny/app-page-builder/editor/plugins/elementSettings/components/StyledComponents";
-import { LIST_FORMS } from "./graphql";
+import { LIST_FORMS, GET_FORM } from "./graphql";
 
 const FormOptionsWrapper = styled("div")({
     minHeight: 250
 });
 
-const getOptions = ({ gqlResponse, data }) => {
-    const output = {
-        parents: {
-            options: [],
-            value: null
-        },
-        publishedRevisions: {
-            options: [],
-            value: null
-        }
-    };
-
-    const selected = {
-        parent: get(data, "settings.form.parent"),
-        revision: get(data, "settings.form.revision")
-    };
-
-    const parentsList = get(gqlResponse, "data.formBuilder.listForms.data") || [];
-
-    output.parents.options = parentsList.map(({ parent, name }) => ({ id: parent, name }));
-    output.parents.value = output.parents.options.find(item => item.id === selected.parent) || null;
-
-    const parent = parentsList.find(item => item.parent === selected.parent);
-    if (parent) {
-        output.publishedRevisions.options = parent.publishedRevisions.map(item => ({
-            id: item.id,
-            name: `${item.name} (version ${item.version})`
-        }));
-        if (output.publishedRevisions.options.length > 0) {
-            output.publishedRevisions.options.unshift({
-                id: "latest",
-                name: "Latest published revision"
-            });
-        }
-
-        output.publishedRevisions.value =
-            output.publishedRevisions.options.find(item => item.id === selected.revision) || null;
-    }
-
-    return output;
-};
-
 const FormElementAdvancedSettings = ({ Bind, submit, data }) => {
+    const listQuery = useQuery(LIST_FORMS, { fetchPolicy: "network-only" });
+
+    const selectedForm = useMemo(() => {
+        return {
+            parent: get(data, "settings.form.parent"),
+            revision: get(data, "settings.form.revision")
+        };
+    }, [data]);
+
+    const [getForm, getQuery] = useLazyQuery(GET_FORM, {
+        variables: {
+            id: selectedForm.parent
+        }
+    });
+
+    const latestRevisions = useMemo(() => {
+        const output = {
+            options: [],
+            value: null
+        };
+        if (listQuery.data) {
+            const parentsList = get(listQuery, "data.formBuilder.listForms.data") || [];
+
+            output.options = parentsList.map(({ id, name }) => ({ id, name }));
+            output.value = output.options.find(item => item.id === selectedForm.parent) || null;
+        }
+
+        return output;
+    }, [listQuery, selectedForm]);
+
+    const publishedRevisions = useMemo(() => {
+        const output = {
+            options: [],
+            value: null
+        };
+
+        if (getQuery.data) {
+            const publishedRevisions = get(
+                getQuery,
+                "data.formBuilder.getForm.data.revisions"
+            ).filter(revision => revision.published);
+            output.options = publishedRevisions.map(item => ({
+                id: item.id,
+                name: `${item.name} (version ${item.version})`
+            }));
+
+            if (output.options.length > 0) {
+                output.options.unshift({
+                    id: "latest",
+                    name: "Latest published revision"
+                });
+            }
+
+            output.value = output.options.find(item => item.id === selectedForm.revision) || null;
+        }
+
+        return output;
+    }, [getQuery, selectedForm]);
+
     return (
         <Accordion title={"Form"} defaultValue={true}>
             <FormOptionsWrapper>
-                <Query query={LIST_FORMS} fetchPolicy="network-only">
-                    {gqlResponse => {
-                        const options = getOptions({ gqlResponse, data });
-                        return (
-                            <Grid className={classes.simpleGrid}>
-                                <Cell span={12}>
-                                    <Bind
-                                        name={"settings.form.parent"}
-                                        validators={validation.create("required")}
-                                    >
-                                        {({ onChange }) => (
-                                            <AutoComplete
-                                                options={options.parents.options}
-                                                value={options.parents.value}
-                                                onChange={onChange}
-                                                label={"Form"}
-                                            />
-                                        )}
-                                    </Bind>
-                                </Cell>
-                                <Cell span={12}>
-                                    <Bind
-                                        name={"settings.form.revision"}
-                                        validators={validation.create("required")}
-                                    >
-                                        {({ onChange }) => {
-                                            const parentSelected = !!options.parents.value;
-                                            const noPublished =
-                                                options.publishedRevisions.options.length === 0;
+                <Grid className={classes.simpleGrid}>
+                    <Cell span={12}>
+                        <Bind
+                            name={"settings.form.parent"}
+                            validators={validation.create("required")}
+                        >
+                            {({ onChange }) => (
+                                <AutoComplete
+                                    options={latestRevisions.options}
+                                    value={latestRevisions.value}
+                                    onChange={value => {
+                                        onChange(value);
+                                        getForm();
+                                    }}
+                                    label={"Form"}
+                                />
+                            )}
+                        </Bind>
+                    </Cell>
+                    <Cell span={12}>
+                        <Bind
+                            name={"settings.form.revision"}
+                            validators={validation.create("required")}
+                        >
+                            {({ onChange }) => {
+                                const parentSelected = !!latestRevisions.value;
+                                const noPublished = publishedRevisions.options.length === 0;
 
-                                            const description = "Choose a published revision.";
-                                            if (parentSelected && noPublished) {
-                                                return (
-                                                    <Alert type="danger" title="Form not published">
-                                                        Please publish the form and then you can
-                                                        insert it into your page.
-                                                    </Alert>
-                                                );
-                                            } else {
-                                                return (
-                                                    <AutoComplete
-                                                        label={"Revision"}
-                                                        description={description}
-                                                        disabled={!parentSelected || noPublished}
-                                                        options={options.publishedRevisions.options}
-                                                        value={options.publishedRevisions.value}
-                                                        onChange={onChange}
-                                                    />
-                                                );
-                                            }
-                                        }}
-                                    </Bind>
-                                </Cell>
-                                <Cell span={12}>
-                                    <ButtonContainer>
-                                        <SimpleButton onClick={submit}>Save</SimpleButton>
-                                    </ButtonContainer>
-                                </Cell>
-                            </Grid>
-                        );
-                    }}
-                </Query>
+                                const description = "Choose a published revision.";
+                                if (parentSelected && noPublished) {
+                                    return (
+                                        <Alert type="danger" title="Form not published">
+                                            Please publish the form and then you can insert it into
+                                            your page.
+                                        </Alert>
+                                    );
+                                } else {
+                                    return (
+                                        <AutoComplete
+                                            label={"Revision"}
+                                            description={description}
+                                            disabled={!parentSelected || noPublished}
+                                            options={publishedRevisions.options}
+                                            value={publishedRevisions.value}
+                                            onChange={onChange}
+                                        />
+                                    );
+                                }
+                            }}
+                        </Bind>
+                    </Cell>
+                    <Cell span={12}>
+                        <ButtonContainer>
+                            <SimpleButton onClick={submit}>Save</SimpleButton>
+                        </ButtonContainer>
+                    </Cell>
+                </Grid>
             </FormOptionsWrapper>
         </Accordion>
     );
