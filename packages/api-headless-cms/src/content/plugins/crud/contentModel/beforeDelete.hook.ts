50037ac10ecf9efd3655582c4316ec859f56c19d
Bruno Zorić <bruno.zoric@gmail.com>
2020-12-24 10:33:57 +0100

    fix: model before delete hook entries search

--- packages/api-headless-cms/src/content/plugins/crud/contentModel/beforeDelete.hook.ts
+++ packages/api-headless-cms/src/content/plugins/crud/contentModel/beforeDelete.hook.ts
@@ -2,7 +2,7 @@ import { CmsContext } from "@webiny/api-headless-cms/types";
 
 export const beforeDeleteHook = async (context: CmsContext, modelId: string) => {
     const manager = await context.cms.getModel(modelId);
-    const entries = await manager.list({
+    const [entries] = await manager.list({
         limit: 1
     });
     if (entries.length === 0) {
