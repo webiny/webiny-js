3024cdb662b106ca81d111460ea65c55ab511dce
Adrian Smijulj <adrian1358@gmail.com>
2020-12-24 09:17:40 +0100

    fix: move HTTP request data to `request` path

--- packages/handler-http/src/types.ts
+++ packages/handler-http/src/types.ts
@@ -17,15 +17,6 @@ export type HttpRequestObject = {
 };
 
 export type HttpObject = {
-    method: "POST" | "GET" | "DELETE" | "OPTIONS";
-    body: string;
-    headers: { [key: string]: any };
-    cookies: string[];
-    path: {
-        base: string;
-        parameters: { [key: string]: any };
-        query: { [key: string]: any };
-    };
     response: (args: ResponseArgs) => { [key: string]: any };
     request: HttpRequestObject;
 };
