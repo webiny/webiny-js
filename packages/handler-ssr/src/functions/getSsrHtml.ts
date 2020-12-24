973c3607d6b7b994c3196e232563e40f0420abfe
Adrian Smijulj <adrian1358@gmail.com>
2020-12-24 09:48:00 +0100

    fix: remove `handler-ssr` package

--- packages/handler-ssr/src/functions/getSsrHtml.ts
+++
@@ -1,8 +0,0 @@
-export default async (context, ssrFunction, { path }) => {
-    const payload = context.handlerClient.invoke({ name: ssrFunction, payload: { path } });
-    if (payload.error) {
-        throw new Error(payload.body);
-    }
-
-    return payload.body;
-};
