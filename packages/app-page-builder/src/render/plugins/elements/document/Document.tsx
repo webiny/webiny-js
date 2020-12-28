fa9f3bf3344720e54684ca490cd0a89bcd813c85
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-28 12:56:05 +0530

    refactor: remove redundant code

--- packages/app-page-builder/src/render/plugins/elements/document/Document.tsx
+++ packages/app-page-builder/src/render/plugins/elements/document/Document.tsx
@@ -1,60 +1,13 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import kebabCase from "lodash/kebabCase";
-import { plugins } from "@webiny/plugins";
-import RenderElement from "@webiny/app-page-builder/render/components/Element";
-import { PbElement, PbRenderResponsiveModePlugin } from "@webiny/app-page-builder/types";
+import React from "react";
+import { PbElement } from "../../../../types";
+import RenderElement from "../../../components/Element";
 
 const Document = ({ element }: { element: PbElement }) => {
-    const [editorMode, setEditorMode] = useState("desktop");
-    const pagePreviewRef = useRef();
-    const responsiveModeConfigs = useMemo(() => {
-        return plugins
-            .byType<PbRenderResponsiveModePlugin>("pb-render-responsive-mode")
-            .map(pl => pl.config);
-    }, []);
-
-    const resizeObserver = useMemo(() => {
-        // @ts-ignore
-        return new ResizeObserver(entries => {
-            for (const entry of entries) {
-                const { width, height } = entry.contentRect;
-                handlerResize({ width, height });
-            }
-        });
-    }, []);
-    // Set resize observer
-    useEffect(() => {
-        // webiny-pb-editor-content-preview
-        if (pagePreviewRef.current) {
-            // Add resize observer
-            resizeObserver.observe(pagePreviewRef.current);
-        }
-    }, []);
-    // Handle document resize
-    const handlerResize = useCallback(
-        ({ width }) => {
-            let mode = "desktop";
-            responsiveModeConfigs.forEach(config => {
-                if (width <= config.minWidth) {
-                    mode = config.name;
-                }
-            });
-
-            setEditorMode(mode);
-        },
-        [responsiveModeConfigs]
-    );
-
     if (!element || Array.isArray(element)) {
         return null;
     }
     return (
-        <div
-            ref={pagePreviewRef}
-            className={`webiny-pb-page-document webiny-pb-page-document-mode--${kebabCase(
-                editorMode
-            )}`}
-        >
+        <div className={"webiny-pb-page-document"}>
             {element.elements.map(element => (
                 <RenderElement key={element.id} element={element} />
             ))}
