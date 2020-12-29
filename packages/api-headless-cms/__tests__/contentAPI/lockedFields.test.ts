961895e51bde0701ec8d498efcb62b715cd3fe93
Bruno Zorić <bruno.zoric@gmail.com>
2020-12-29 09:35:29 +0100

    test: locked fields test

---
+++ packages/api-headless-cms/__tests__/contentAPI/lockedFields.test.ts
@@ -0,0 +1,159 @@
+import { useContentGqlHandler } from "../utils/useContentGqlHandler";
+import {
+    CmsContentEntryType,
+    CmsContentModelGroupType,
+    CmsContentModelType
+} from "@webiny/api-headless-cms/types";
+import models from "./mocks/contentModels";
+import { useCategoryManageHandler } from "../utils/useCategoryManageHandler";
+import { useProductManageHandler } from "../utils/useProductManageHandler";
+
+describe("Content model locked fields", () => {
+    const esCmsIndex = "root-headless-cms";
+
+    const manageOpts = { path: "manage/en-US" };
+
+    const {
+        elasticSearch,
+        createContentModelMutation,
+        updateContentModelMutation,
+        createContentModelGroupMutation
+    } = useContentGqlHandler(manageOpts);
+
+    // This function is not directly within `beforeEach` as we don't always setup the same content model.
+    // We call this function manually at the beginning of each test, where needed.
+    const setupContentModelGroup = async (): Promise<CmsContentModelGroupType> => {
+        const [createCMG] = await createContentModelGroupMutation({
+            data: {
+                name: "Group",
+                slug: "group",
+                icon: "ico/ico",
+                description: "description"
+            }
+        });
+        return createCMG.data.createContentModelGroup.data;
+    };
+
+    const setupCategoryModel = async (contentModelGroup: CmsContentModelGroupType) => {
+        const model = models.find(m => m.modelId === "category");
+        // Create initial record
+        const [create] = await createContentModelMutation({
+            data: {
+                name: model.name,
+                modelId: model.modelId,
+                group: contentModelGroup.id
+            }
+        });
+
+        if (create.errors) {
+            console.error(`[beforeEach] ${create.errors[0].message}`);
+            process.exit(1);
+        }
+
+        const [update] = await updateContentModelMutation({
+            modelId: create.data.createContentModel.data.modelId,
+            data: {
+                fields: model.fields,
+                layout: model.layout
+            }
+        });
+        return update.data.updateContentModel.data;
+    };
+
+    beforeEach(async () => {
+        try {
+            await elasticSearch.indices.create({ index: esCmsIndex });
+        } catch {}
+    });
+
+    afterEach(async () => {
+        try {
+            await elasticSearch.indices.delete({ index: esCmsIndex });
+        } catch {}
+    });
+
+    test("must mark fields as used and prevent changes on it, as soon as the first entry is saved", async () => {
+        const { createCategory } = useCategoryManageHandler({
+            ...manageOpts
+        });
+        const contentModelGroup = await setupContentModelGroup();
+
+        const categoryContentModel = await setupCategoryModel(contentModelGroup);
+
+        const [createCategoryResponse] = await createCategory({
+            data: {
+                title: "Vegetables",
+                slug: "vegetables"
+            }
+        });
+        const category = createCategoryResponse.data.createCategory.data as CmsContentEntryType;
+
+        const productModel = models.find(m => m.modelId === "product");
+        const [createResponse] = await createContentModelMutation({
+            data: {
+                name: productModel.name,
+                modelId: productModel.modelId,
+                group: contentModelGroup.id
+            }
+        });
+
+        const contentModel = createResponse.data.createContentModel.data as CmsContentModelType;
+
+        await updateContentModelMutation({
+            modelId: contentModel.modelId,
+            data: {
+                fields: productModel.fields,
+                layout: productModel.layout
+            }
+        });
+
+        // create a entry so fields get locked
+        const { createProduct } = useProductManageHandler({
+            ...manageOpts
+        });
+
+        await createProduct({
+            data: {
+                title: "Potato",
+                price: 100,
+                availableOn: "2020-12-25T16:37:00Z.000",
+                color: "white",
+                availableSizes: ["s", "m"],
+                image: "file.jpg",
+                category: {
+                    modelId: categoryContentModel.modelId,
+                    entryId: category.id
+                }
+            }
+        });
+
+        // when removing field one by one error must be thrown that given field cannot be removed
+        const fieldsToRemove = productModel.fields.filter(field => field.fieldId !== "title");
+        for (const field of fieldsToRemove) {
+            const targetFields = productModel.fields.filter(f => f.id !== field.id);
+            const [removedFieldResponse] = await updateContentModelMutation({
+                modelId: contentModel.modelId,
+                data: {
+                    titleFieldId: null,
+                    fields: targetFields,
+                    layout: targetFields.map(f => {
+                        return [f.id];
+                    })
+                }
+            });
+
+            expect(removedFieldResponse).toEqual({
+                data: {
+                    updateContentModel: {
+                        data: null,
+                        error: {
+                            code: "ENTRY_FIELD_USED",
+                            data: null,
+                            message: `Cannot remove the field "${field.fieldId}" because it's already in use in created content.`
+                        }
+                    }
+                }
+            });
+        }
+    });
+});
