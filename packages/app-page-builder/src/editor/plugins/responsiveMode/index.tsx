716bfdff9e4ce81433527db80f5f90ee4e6de525
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-29 17:34:20 +0530

    feat: update PbEditorResponsiveModePlugin config

--- packages/app-page-builder/src/editor/plugins/responsiveMode/index.tsx
+++ packages/app-page-builder/src/editor/plugins/responsiveMode/index.tsx
@@ -5,6 +5,7 @@ import { PbEditorResponsiveModePlugin, DisplayMode } from "../../../types";
 import { ReactComponent as DesktopIcon } from "./icons/laptop_mac.svg";
 import { ReactComponent as TabletIcon } from "./icons/tablet_mac.svg";
 import { ReactComponent as MobileIcon } from "./icons/phone_iphone.svg";
+import { ReactComponent as StarIcon } from "./icons/star_rate.svg";
 
 const rotateStyle = css({
     transform: "rotate(90deg)"
@@ -16,7 +17,13 @@ export default () => [
         name: "pb-editor-responsive-mode-desktop",
         config: {
             displayMode: DisplayMode.DESKTOP,
-            label: "Desktop base breakpoint",
+            toolTip: {
+                title: "Desktop",
+                subTitle: "Base breakpoint",
+                body: `Desktop styles apply at all breakpoints, unless they're
+                       edited at a smaller breakpoint. Start your styling here.`,
+                subTitleIcon: <StarIcon />
+            },
             icon: <DesktopIcon />
         }
     } as PbEditorResponsiveModePlugin,
@@ -25,7 +32,12 @@ export default () => [
         name: "pb-editor-responsive-mode-tablet",
         config: {
             displayMode: DisplayMode.TABLET,
-            label: "Tablet 991px and down",
+            toolTip: {
+                title: "Tablet",
+                subTitle: "991px and down",
+                body: `Styles added here will apply at 991px and down, unless they're
+                       edited at a smaller breakpoint.`
+            },
             icon: <TabletIcon />
         }
     } as PbEditorResponsiveModePlugin,
@@ -34,7 +46,12 @@ export default () => [
         name: "pb-editor-responsive-mode-mobile-landscape",
         config: {
             displayMode: DisplayMode.MOBILE_LANDSCAPE,
-            label: "Mobile landscape 767px and down",
+            toolTip: {
+                title: "Mobile landscape",
+                subTitle: "767px and down",
+                body: `Styles added here will apply at 767px and down, unless they're
+                       edited at a smaller breakpoint.`
+            },
             icon: <MobileIcon className={rotateStyle} />
         }
     } as PbEditorResponsiveModePlugin,
@@ -43,7 +60,11 @@ export default () => [
         name: "pb-editor-responsive-mode-mobile-portrait",
         config: {
             displayMode: DisplayMode.MOBILE_PORTRAIT,
-            label: "Mobile portrait 478px and down",
+            toolTip: {
+                title: "Mobile portrait",
+                subTitle: "478px and down",
+                body: `Styles added here will apply at 478px and down.`
+            },
             icon: <MobileIcon />
         }
     } as PbEditorResponsiveModePlugin
