d06b5489acc2f462fd83e5c96fb6ec1a17bf7403
Ashutosh <bhardwajashu96@hotmail.com>
2020-12-31 13:23:03 +0530

    feat: temporary disabled `visibility` style settings

--- packages/app-page-builder/src/editor/plugins/elementSettings/visibility/index.tsx
+++ packages/app-page-builder/src/editor/plugins/elementSettings/visibility/index.tsx
@@ -5,7 +5,6 @@ import VisibilitySettings from "./VisibilitySettings";
 export default {
     name: "pb-editor-page-element-style-settings-visibility",
     type: "pb-editor-page-element-style-settings",
-    elements: true,
     render({ options }) {
         return <VisibilitySettings options={options} />;
     }
