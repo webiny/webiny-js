84fb403f65df3c191a93b85174e027677d7bd525
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-01 15:49:33 +0530

    chore: add missing files from merge

--- packages/api-i18n/src/graphql/index.ts
+++ packages/api-i18n/src/graphql/index.ts
@@ -1,4 +1,23 @@
-export { default as I18NStringValueType } from "./I18NStringValueType";
-export { default as I18NJSONValueType } from "./I18NJSONValueType";
-export { default as I18NStringValueInput } from "./I18NStringValueInput";
-export { default as I18NJSONValueInput } from "./I18NJSONValueInput";
+import graphql from "./graphql";
+import i18nContext from "./context";
+import crud from "./crud";
+import localeContexts from "./localeContexts";
+
+export default () => [
+    crud,
+    localeContexts,
+    graphql,
+    i18nContext,
+    {
+        name: "context-i18n-get-locales",
+        type: "context-i18n-get-locales",
+        async resolve({ context }) {
+            const { locales } = context;
+            const list = await locales.list();
+            return list.map(locale => ({
+                code: locale.code,
+                default: locale.default
+            }));
+        }
+    }
+];
