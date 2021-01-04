2010cd2e410771125a62f6f3ab88d350477476c4
Adrian Smijulj <adrian1358@gmail.com>
2021-01-04 13:31:23 +0100

    fix: add `build` package

--- packages/cwp-template-full/template/webiny.root.js
+++ packages/cwp-template-full/template/webiny.root.js
@@ -3,6 +3,7 @@ module.exports = {
     projectName: "[PROJECT_NAME]",
     cli: {
         plugins: [
+            require("@webiny/cli-plugin-build")(),
             require("@webiny/cli-plugin-deploy-pulumi")(),
             require("@webiny/api-page-builder/cli")(),
             require("@webiny/cwp-template-full/hooks/api")(),
