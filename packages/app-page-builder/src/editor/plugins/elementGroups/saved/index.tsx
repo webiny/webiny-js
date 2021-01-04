1d66a89e7b2eb7e9087f0c510e4586ec741b1cd7
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-04 15:53:35 +0530

    feat(app-page-builder): add `emptyView` in Saved element group

--- packages/app-page-builder/src/editor/plugins/elementGroups/saved/index.tsx
+++ packages/app-page-builder/src/editor/plugins/elementGroups/saved/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { ReactComponent as SavedIcon } from "@webiny/app-page-builder/editor/assets/icons/round-favorite-24px.svg";
 import { PbEditorPageElementGroupPlugin } from "@webiny/app-page-builder/types";
+import EmptyElementGroupView from "../../../components/EmptyElementGroupView";
+import { ReactComponent as ContentIcon } from "../../../assets/icons/insights.svg";
 
 export default {
     name: "pb-editor-element-group-saved",
     type: "pb-editor-page-element-group",
     group: {
         title: "Saved",
-        icon: <SavedIcon />
+        icon: <SavedIcon />,
+        emptyView: (
+            <EmptyElementGroupView
+                icon={<ContentIcon />}
+                title={"Bring your elements to life."}
+                body={`Let's start by saving some great elements. Simply select an element and click
+            Save icon in Element tab.`}
+            />
+        )
     }
 } as PbEditorPageElementGroupPlugin;
