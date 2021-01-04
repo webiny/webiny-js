5035dfbb0462ad16705345b3c20931a48ffa9830
Adrian Smijulj <adrian1358@gmail.com>
2021-01-04 11:23:51 +0100

    fix: temporary remove scaffold CLI plugins

--- packages/cli-plugin-scaffold-node-package/template/jest.config.js
+++
@@ -1,3 +0,0 @@
-const base = require("[JEST_CONFIG_BASE_PATH]");
-
-module.exports = base({ path: __dirname });
--- packages/cli-plugin-scaffold-react-app/template-site/src/App.tsx
+++
@@ -1,4 +0,0 @@
-import createSite from "@webiny/app-template-site";
-import "./App.scss";
-
-export default createSite({});
