16164284fef2d27aa4844911020ef8cdaae0551a
Adrian Smijulj <adrian1358@gmail.com>
2021-01-04 11:59:28 +0100

    fix: update CLI plugins

--- packages/cwp-template-full/template/webiny.root.js
+++ packages/cwp-template-full/template/webiny.root.js
@@ -3,16 +3,10 @@ module.exports = {
     projectName: "[PROJECT_NAME]",
     cli: {
         plugins: [
-            require("@webiny/cli-plugin-deploy-components")(),
+            require("@webiny/cli-plugin-deploy-pulumi")(),
+            require("@webiny/api-page-builder/cli")(),
             require("@webiny/cwp-template-full/hooks/api")(),
             require("@webiny/cwp-template-full/hooks/apps")(),
-            require("@webiny/cli-plugin-scaffold"),
-            require("@webiny/cli-plugin-scaffold-graphql-service"),
-            require("@webiny/cli-plugin-scaffold-lambda"),
-            require("@webiny/cli-plugin-scaffold-admin-app-module"),
-            require("@webiny/cli-plugin-scaffold-node-package"),
-            require("@webiny/cli-plugin-scaffold-react-package"),
-            require("@webiny/cli-plugin-scaffold-react-app")
         ]
     }
 };
