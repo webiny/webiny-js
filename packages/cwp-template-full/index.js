801010851eea81a3be06c2c2db92c589530d59a7
Adrian Smijulj <adrian1358@gmail.com>
2021-01-04 14:10:03 +0100

    fix: generate  `PULUMI_CONFIG_PASSPHRASE`

--- packages/cwp-template-full/index.js
+++ packages/cwp-template-full/index.js
@@ -3,6 +3,16 @@
 const fs = require("fs-extra");
 const path = require("path");
 const execa = require("execa");
+const loadJson = require("load-json-file");
+const writeJson = require("write-json-file");
+const crypto = require("crypto");
+
+function random(length = 5) {
+    return crypto
+        .randomBytes(Math.ceil(length / 2))
+        .toString("hex")
+        .slice(0, length);
+}
 
 module.exports = async ({ appName, root }) => {
     const { name, version } = require("./package.json");
@@ -18,6 +28,11 @@ module.exports = async ({ appName, root }) => {
     // Commit .gitignore.
     execa.sync("git", ["add", ".gitignore"], { cwd: root });
     execa.sync("git", ["commit", "-m", `chore: initialize .gitignore`], { cwd: root });
+
+    const rootEnvJsonPath = path.join(root, ".env.json");
+    const rootEnvJson = await loadJson.sync(rootEnvJsonPath);
+    rootEnvJson.default.PULUMI_CONFIG_PASSPHRASE = random();
+    await writeJson(rootEnvJsonPath, rootEnvJson);
 
     let webinyRoot = fs.readFileSync(path.join(root, "webiny.root.js"), "utf-8");
     webinyRoot = webinyRoot.replace("[PROJECT_NAME]", appName);
