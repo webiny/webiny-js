da1dee85c9d11ca7370ad607350e51df9d41f006
Adrian Smijulj <adrian1358@gmail.com>
2021-01-06 12:15:34 +0100

    fix: update setting of root `.env` file

--- packages/cwp-template-full/index.js
+++ packages/cwp-template-full/index.js
@@ -1,10 +1,6 @@
-"use strict";
-
 const fs = require("fs-extra");
 const path = require("path");
 const execa = require("execa");
-const loadJson = require("load-json-file");
-const writeJson = require("write-json-file");
 const crypto = require("crypto");
 
 function random(length = 32) {
@@ -29,10 +25,11 @@ module.exports = async ({ appName, root }) => {
     execa.sync("git", ["add", ".gitignore"], { cwd: root });
     execa.sync("git", ["commit", "-m", `chore: initialize .gitignore`], { cwd: root });
 
-    const rootEnvJsonPath = path.join(root, ".env.json");
-    const rootEnvJson = await loadJson.sync(rootEnvJsonPath);
-    rootEnvJson.default.PULUMI_CONFIG_PASSPHRASE = random();
-    await writeJson(rootEnvJsonPath, rootEnvJson);
+    const rootEnvFilePath = path.join(root, ".env");
+    let content = fs.readFileSync(rootEnvFilePath).toString();
+    content = content.replace("{REGION}", "us-east-1");
+    content = content.replace("{PULUMI_CONFIG_PASSPHRASE}", random());
+    fs.writeFileSync(rootEnvFilePath, content);
 
     let webinyRoot = fs.readFileSync(path.join(root, "webiny.root.js"), "utf-8");
     webinyRoot = webinyRoot.replace("[PROJECT_NAME]", appName);
