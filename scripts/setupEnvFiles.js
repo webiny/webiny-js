bc5b3e071f52f03e91124698fb0f45a85cde8670
Adrian Smijulj <adrian1358@gmail.com>
2021-01-06 11:27:00 +0100

    fix: remove creation of env files

--- scripts/setupEnvFiles.js
+++ scripts/setupEnvFiles.js
@@ -27,28 +27,4 @@ const PROJECT_FOLDER = ".";
         fs.writeFileSync(rootEnvFilePath, content);
         console.log(`✅️ ${green(".env")} was created successfully!`);
     }
-
-    // Create `apps/admin` .env
-    const adminRoot = path.join("apps", "admin", "code");
-    const exampleAdminEnvFilePath = path.resolve(PROJECT_FOLDER, adminRoot, "example.env");
-    const adminEnvFilePath = path.resolve(PROJECT_FOLDER, adminRoot, ".env");
-
-    if (fs.existsSync(adminEnvFilePath)) {
-        console.log(`⚠️  ${green(path.join(adminRoot, ".env"))} already exists, skipping.`);
-    } else {
-        fs.copyFileSync(exampleAdminEnvFilePath, adminEnvFilePath);
-        console.log(`✅️ ${green(path.join(adminRoot, ".env"))} was created successfully!`);
-    }
-
-    // Create `apps/site` .env
-    const siteRoot = path.join("apps", "site", "code");
-    const exampleSiteEnvFilePath = path.resolve(PROJECT_FOLDER, siteRoot, "example.env");
-    const siteEnvFilePath = path.resolve(PROJECT_FOLDER, siteRoot, ".env");
-
-    if (fs.existsSync(siteEnvFilePath)) {
-        console.log(`⚠️  ${green(path.join(siteRoot, ".env"))} already exists, skipping.`);
-    } else {
-        fs.copyFileSync(exampleSiteEnvFilePath, siteEnvFilePath);
-        console.log(`✅️ ${green(path.join(siteRoot, ".env"))} was created successfully!`);
-    }
 })();
