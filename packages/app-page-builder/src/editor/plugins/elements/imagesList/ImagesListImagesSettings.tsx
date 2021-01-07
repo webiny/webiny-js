82233eb1e54899f9e8f5b9749ed1244d6e95e147
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-07 20:12:22 +0530

    fix(app-page-builder): use clone value of "images"

--- packages/app-page-builder/src/editor/plugins/elements/imagesList/ImagesListImagesSettings.tsx
+++ packages/app-page-builder/src/editor/plugins/elements/imagesList/ImagesListImagesSettings.tsx
@@ -37,50 +37,58 @@ const ImagesListImagesSettings = props => {
             <Grid className={classes.simpleGrid}>
                 <Cell span={12}>
                     <Bind name={"images"} afterChange={submit}>
-                        {({ onChange, value }) => (
-                            <FileManager
-                                images
-                                multiple
-                                onChange={files => {
-                                    Array.isArray(value)
-                                        ? onChange([...value, ...files])
-                                        : onChange([...files]);
-                                }}
-                            >
-                                {({ showFileManager }) => (
-                                    <>
-                                        <ul className="sortable-list">
-                                            {Array.isArray(value) &&
-                                                value.map((item, i) => (
-                                                    <SortableItem
-                                                        key={i}
-                                                        onSortItems={onChange}
-                                                        items={value}
-                                                        sortId={i}
-                                                    >
-                                                        <File
-                                                            file={item}
-                                                            onRemove={() => {
-                                                                // Remove the image at index i
-                                                                const updatedValue = [
-                                                                    ...value.slice(0, i),
-                                                                    ...value.slice(i + 1)
-                                                                ];
-                                                                onChange(updatedValue);
-                                                            }}
-                                                        />
-                                                    </SortableItem>
-                                                ))}
-                                        </ul>
-                                        <ButtonContainer>
-                                            <SimpleButton onClick={showFileManager}>
-                                                Add images...
-                                            </SimpleButton>
-                                        </ButtonContainer>
-                                    </>
-                                )}
-                            </FileManager>
-                        )}
+                        {({ onChange, value: images }) => {
+                            /**
+                             * We're creating a fresh copy of value here because all of sudden
+                             * dragging a "SortableItem" started throwing TypeError: "Cannot assign to read only property"
+                             * which means the state is being mutated by "Sortable" somehow.
+                             */
+                            const value = [...images];
+                            return (
+                                <FileManager
+                                    images
+                                    multiple
+                                    onChange={files => {
+                                        Array.isArray(value)
+                                            ? onChange([...value, ...files])
+                                            : onChange([...files]);
+                                    }}
+                                >
+                                    {({ showFileManager }) => (
+                                        <>
+                                            <ul className="sortable-list">
+                                                {Array.isArray(value) &&
+                                                    value.map((item, i) => (
+                                                        <SortableItem
+                                                            key={i}
+                                                            onSortItems={onChange}
+                                                            items={value}
+                                                            sortId={i}
+                                                        >
+                                                            <File
+                                                                file={item}
+                                                                onRemove={() => {
+                                                                    // Remove the image at index i
+                                                                    const updatedValue = [
+                                                                        ...value.slice(0, i),
+                                                                        ...value.slice(i + 1)
+                                                                    ];
+                                                                    onChange(updatedValue);
+                                                                }}
+                                                            />
+                                                        </SortableItem>
+                                                    ))}
+                                            </ul>
+                                            <ButtonContainer>
+                                                <SimpleButton onClick={showFileManager}>
+                                                    Add images...
+                                                </SimpleButton>
+                                            </ButtonContainer>
+                                        </>
+                                    )}
+                                </FileManager>
+                            );
+                        }}
                     </Bind>
                 </Cell>
             </Grid>
