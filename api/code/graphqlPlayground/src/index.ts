a5b6a3a865c2bbe001529f397867a453de593849
Pavel Denisjuk <pavel@webiny.com>
2021-01-07 23:53:53 +0100

    chore: remove graphql playground lambda

--- api/code/graphqlPlayground/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { createHandler } from "@webiny/handler-aws";
-import graphqlPlaygroundHandler from "@webiny/handler-graphql-playground";
-
-export const handler = createHandler(graphqlPlaygroundHandler());
