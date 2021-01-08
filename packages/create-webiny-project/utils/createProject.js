7c3be611edf92ae956f54c625ccfce8a3c3428f1
Adrian Smijulj <adrian1358@gmail.com>
2021-01-08 11:16:40 +0100

    wip: move code to `cwp-template-aws`

--- packages/create-webiny-project/utils/createProject.js
+++ packages/create-webiny-project/utils/createProject.js
@@ -6,13 +6,8 @@ const Listr = require("listr");
 const os = require("os");
 const path = require("path");
 const indentString = require("indent-string");
-const fg = require("fast-glob");
-const findUp = require("find-up");
-const loadJsonFile = require("load-json-file");
 const { sendEvent } = require("@webiny/tracking");
-const writeJsonFile = require("write-json-file");
 const rimraf = require("rimraf");
-const getPackageVersion = require("./getPackageVersion");
 const getPackageJson = require("./getPackageJson");
 
 const checkProjectName = require("./checkProjectName");
@@ -83,7 +78,8 @@ module.exports = async function createProject({ projectName, template, tag, log 
             task: async context => {
                 let templateName = `@webiny/cwp-template-${template}`;
                 if (template.startsWith(".") || template.startsWith("file:")) {
-                    templateName = "file:" + path.relative(projectName, template.replace("file:", ""));
+                    templateName =
+                        "file:" + path.relative(projectName, template.replace("file:", ""));
                 } else {
                     templateName = `${templateName}@${tag}`;
                 }
@@ -110,40 +106,7 @@ module.exports = async function createProject({ projectName, template, tag, log 
                 );
             }
         },
-        /*{
-            title: "Set up project dependencies",
-            task: () => {
-                const appPackage = require(path.join(root, "package.json"));
-                const projectDeps = require(path.join(root, "dependencies.json"));
-
-                Object.assign(appPackage.dependencies, projectDeps.dependencies);
-
-                if (appPackage.devDependencies) {
-                    Object.assign(appPackage.devDependencies, projectDeps.devDependencies);
-                } else {
-                    appPackage.devDependencies = Object.assign({}, projectDeps.devDependencies);
-                }
-
-                if (appPackage.workspaces) {
-                    Object.assign(appPackage.workspaces, projectDeps.workspaces);
-                } else {
-                    appPackage.workspaces = Object.assign({}, projectDeps.workspaces);
-                }
-
-                if (appPackage.scripts) {
-                    Object.assign(appPackage.scripts, projectDeps.scripts);
-                } else {
-                    appPackage.scripts = Object.assign({}, projectDeps.scripts);
-                }
-
-                fs.writeFileSync(
-                    path.join(root, "package.json"),
-                    JSON.stringify(appPackage, null, 2) + os.EOL
-                );
-                fs.unlinkSync(path.join(root, "dependencies.json"));
-            }
-        },*/
-        /* {
+        {
             title: `Initialize git`,
             task: (ctx, task) => {
                 try {
@@ -154,113 +117,14 @@ module.exports = async function createProject({ projectName, template, tag, log 
                     task.skip("Git repo not initialized", err);
                 }
             }
-        },*/
-        /* {
-            title: "Get proper package versions",
-            task: async () => {
-                // Set proper @webiny package versions
-                const workspaces = await fg(["**!/package.json"], {
-                    ignore: ["**!/dist/!**", "**!/node_modules/!**", root],
-                    cwd: root
-                });
-                const latestVersion = await getPackageVersion("@webiny/cli", tag);
-
-                for (let i = 0; i < workspaces.length; i++) {
-                    const jsonPath = path.join(root, workspaces[i]);
-                    const json = await loadJsonFile(jsonPath);
-                    const keys = Object.keys(json.dependencies).filter(k =>
-                        k.startsWith("@webiny")
-                    );
-                    const currentDir = jsonPath.match(/(.*)[\/\\]/)[1] || "";
-
-                    const baseTsConfigPath = path
-                        .relative(
-                            currentDir,
-                            findUp.sync("tsconfig.json", {
-                                cwd: root
-                            })
-                        )
-                        .replace(/\\/g, "/");
-
-                    // We don't want to modify tsconfig file in the root of the project
-                    if (workspaces[i] !== "package.json") {
-                        const tsConfigPath = path.join(currentDir, "tsconfig.json");
-                        const tsconfig = require(tsConfigPath);
-                        tsconfig.extends = baseTsConfigPath;
-                        fs.writeFileSync(tsConfigPath, JSON.stringify(tsconfig, null, 2));
-                    }
-
-                    keys.forEach(name => {
-                        if (tag.startsWith(".")) {
-                            // This means `tag` points to the location of all @webiny packages
-                            json.dependencies[name] =
-                                "link:" +
-                                path.relative(
-                                    path.dirname(jsonPath),
-                                    path.join(process.cwd(), tag, name)
-                                );
-                        } else {
-                            // Skip packages that are already set to use file: protocol
-                            if (json.dependencies[name].startsWith("file:")) {
-                                return;
-                            }
-
-                            // Use version of @webiny/cli package (we have fixed package versioning)
-                            json.dependencies[name] = `^` + latestVersion;
-                        }
-                    });
-                    await writeJsonFile(jsonPath, json);
-                }
+        },
+        {
+            title: "Setup template",
+            task: async context => {
+                await require(context.templatePath)({ projectName, root });
             }
         },
         {
-            title: "Resolve packages",
-            task: async () => {
-                // Add package resolutions if `tag` is pointing to a local folder.
-
-                // Why? This is very useful when you're testing packages that are not yet published to `npm`.
-                // Just run `create-webiny-project my-project --tag=./local/webiny/project/node_modules` and the whole project
-                // will be set up to use your local packages via symlinks, so you can develop and test immediately.
-                try {
-                    if (tag.startsWith(".")) {
-                        const webinyPackages = await fg(["@webiny/!*"], {
-                            cwd: path.join(process.cwd(), tag),
-                            onlyDirectories: true
-                        });
-
-                        if (webinyPackages.length) {
-                            const rootPkg = path.join(root, "package.json");
-                            const rootPkgJson = await loadJsonFile(rootPkg);
-                            if (!rootPkgJson.resolutions) {
-                                rootPkgJson.resolutions = {};
-                            }
-
-                            webinyPackages
-                                .filter(name => {
-                                    // Do not include private packages
-                                    const pkgJson = require(path.join(
-                                        process.cwd(),
-                                        tag,
-                                        name,
-                                        "package.json"
-                                    ));
-                                    return !pkgJson.private;
-                                })
-                                .forEach(name => {
-                                    rootPkgJson.dependencies[name] = rootPkgJson.resolutions[name] =
-                                        "link:" +
-                                        path.relative(root, path.join(process.cwd(), tag, name));
-                                });
-
-                            await writeJsonFile(rootPkg, rootPkgJson);
-                        }
-                    }
-                } catch (err) {
-                    throw new Error("Unable to resolve packages: " + err.message);
-                }
-            }
-        },*/
-        /*{
             title: "Install dependencies",
             task: async context => {
                 try {
@@ -281,12 +145,6 @@ module.exports = async function createProject({ projectName, template, tag, log 
                 } catch (err) {
                     throw new Error("Unable to install the necessary packages: " + err.message);
                 }
-            }
-        },*/
-        {
-            title: "Run template-specific actions",
-            task: async context => {
-                await require(context.templatePath)({ projectName, root });
             }
         }
     ]);
