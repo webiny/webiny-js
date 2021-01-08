6724b391aa80f0b0211b391877ab5359bfed7818
Adrian Smijulj <adrian1358@gmail.com>
2021-01-08 10:08:27 +0100

    fix: add `renames` utility file

---
+++ packages/cwp-template-aws/setup/renames.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        prev: "example.env",
+        next: ".env"
+    },
+    {
+        prev: "example.gitignore",
+        next: ".gitignore"
+    },
+    {
+        prev: "example.gitattributes",
+        next: ".gitattributes"
+    },
+    {
+        prev: "example.babel.node.js",
+        next: ".babel.node.js"
+    },
+    {
+        prev: "example.babel.react.js",
+        next: ".babel.react.js"
+    },
+    {
+        prev: "example.prettierrc.js",
+        next: ".prettierrc.js"
+    },
+    {
+        prev: "example.eslintignore",
+        next: ".eslintignore"
+    },
+    {
+        prev: "example.eslintrc.js",
+        next: ".eslintrc.js"
+    }
+];
