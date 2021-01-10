aad5a0b064ba6bed3015d08613e1657fb0207f8e
Adrian Smijulj <adrian1358@gmail.com>
2021-01-10 18:42:45 +0100

    test: add root `.env` test

---
+++ packages/cwp-template-aws/__tests__/rootEnv.test.js
@@ -0,0 +1,42 @@
+const setup = require("../setup");
+const path = require("path");
+const fs = require("fs-extra");
+const writeJsonFile = require("write-json-file");
+const getPackageJson = require("create-webiny-project/utils/getPackageJson");
+const dotenv = require("dotenv");
+
+const PROJECT_NAME = "test-123";
+const PROJECT_ROOT = path.join(__dirname, PROJECT_NAME);
+
+describe("root .env file generation test", () => {
+    beforeAll(async () => {
+        fs.ensureDirSync(PROJECT_ROOT);
+
+        const packageJson = getPackageJson(PROJECT_NAME);
+        await writeJsonFile(path.join(PROJECT_ROOT, "package.json"), packageJson);
+
+        await setup({
+            projectName: PROJECT_NAME,
+            projectRoot: PROJECT_ROOT,
+            vpc: true
+        });
+    });
+
+    afterAll(() => {
+        fs.removeSync(PROJECT_ROOT);
+    });
+
+    test("root .env file should exist", async () => {
+        expect(fs.pathExistsSync(path.join(PROJECT_ROOT, ".env"))).toBe(true);
+    });
+
+    test("should update root .env file correctly", async () => {
+        const config = dotenv.parse(fs.readFileSync(path.join(PROJECT_ROOT, ".env")));
+        expect(config).toMatchObject({
+            AWS_REGION: "us-east-1",
+            PULUMI_SECRETS_PROVIDER: "passphrase"
+        });
+
+        expect(config.PULUMI_CONFIG_PASSPHRASE.length).toBe(32);
+    });
+});
