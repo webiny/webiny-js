698c90f9c98690edfecdf8b13863ec15a5af6c61
Adrian Smijulj <adrian1358@gmail.com>
2021-01-11 13:00:04 +0100

    fix: add `workspaces` plugins

--- packages/cwp-template-aws/template/webiny.root.js
+++ packages/cwp-template-aws/template/webiny.root.js
@@ -3,6 +3,7 @@ module.exports = {
     projectName: "[PROJECT_NAME]",
     cli: {
         plugins: [
+            require("@webiny/cli-plugin-workspaces")(),
             require("@webiny/cli-plugin-deploy-pulumi")(),
             require("@webiny/api-page-builder/cli")(),
             require("@webiny/cwp-template-aws/cli")()
