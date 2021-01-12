c4958159199334ef605a6680f54b452c78a305fc
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-12 20:15:43 +0530

    refactor(app-security-tenancy): remove `FloatingActionButton` from view

--- packages/app-security-tenancy/src/views/ApiKeys/ApiKeys.tsx
+++ packages/app-security-tenancy/src/views/ApiKeys/ApiKeys.tsx
@@ -1,28 +1,18 @@
 import * as React from "react";
 import { SplitView, LeftPanel, RightPanel } from "@webiny/app-admin/components/SplitView";
-import { FloatingActionButton } from "@webiny/app-admin/components/FloatingActionButton";
 import ApiKeysDataList from "./ApiKeysDataList";
 import ApiKeyForm from "./ApiKeyForm";
-import { useRouter } from "@webiny/react-router";
 
 const ApiKeys = ({ formProps, listProps }: any) => {
-    const { history } = useRouter();
-
     return (
-        <>
-            <SplitView>
-                <LeftPanel>
-                    <ApiKeysDataList {...listProps} />
-                </LeftPanel>
-                <RightPanel>
-                    <ApiKeyForm {...formProps} />
-                </RightPanel>
-            </SplitView>
-            <FloatingActionButton
-                data-testid="new-record-button"
-                onClick={() => history.push("/security/api-keys")}
-            />
-        </>
+        <SplitView>
+            <LeftPanel>
+                <ApiKeysDataList {...listProps} />
+            </LeftPanel>
+            <RightPanel>
+                <ApiKeyForm {...formProps} />
+            </RightPanel>
+        </SplitView>
     );
 };
 
