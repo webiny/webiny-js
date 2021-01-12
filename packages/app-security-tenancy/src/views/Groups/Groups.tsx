abd4be0cd6ec245d64775d91fbb767e913b0e40d
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-12 20:11:06 +0530

    refactor(app-security-tenancy): remove `FloatingActionButton`

--- packages/app-security-tenancy/src/views/Groups/Groups.tsx
+++ packages/app-security-tenancy/src/views/Groups/Groups.tsx
@@ -1,28 +1,18 @@
 import * as React from "react";
 import { SplitView, LeftPanel, RightPanel } from "@webiny/app-admin/components/SplitView";
-import { FloatingActionButton } from "@webiny/app-admin/components/FloatingActionButton";
 import GroupsDataList from "./GroupsDataList";
 import GroupsForm from "./GroupsForm";
-import { useRouter } from "@webiny/react-router";
 
 const Groups = ({ formProps, listProps }: any) => {
-    const { history } = useRouter();
-
     return (
-        <>
-            <SplitView>
-                <LeftPanel>
-                    <GroupsDataList {...listProps} />
-                </LeftPanel>
-                <RightPanel>
-                    <GroupsForm {...formProps} />
-                </RightPanel>
-            </SplitView>
-            <FloatingActionButton
-                data-testid="new-record-button"
-                onClick={() => history.push("/security/groups")}
-            />
-        </>
+        <SplitView>
+            <LeftPanel>
+                <GroupsDataList {...listProps} />
+            </LeftPanel>
+            <RightPanel>
+                <GroupsForm {...formProps} />
+            </RightPanel>
+        </SplitView>
     );
 };
 
