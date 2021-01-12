880df900821ca3f95452b2dd31b47a727c0c7372
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-12 20:44:13 +0530

    refactor(app-i18n): remove `FloatingActionButton`

--- packages/app-i18n/src/admin/views/I18NLocales/I18NLocales.tsx
+++ packages/app-i18n/src/admin/views/I18NLocales/I18NLocales.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { SplitView, LeftPanel, RightPanel } from "@webiny/app-admin/components/SplitView";
-import { FloatingActionButton } from "@webiny/app-admin/components/FloatingActionButton";
 import I18NLocalesDataList from "./I18NLocalesDataList";
 import I18NLocalesForm from "./I18NLocalesForm";
-import { useRouter } from "@webiny/react-router";
 
 export default function I18NLocales() {
-    const { history } = useRouter();
     return (
-        <>
-            <SplitView>
-                <LeftPanel>
-                    <I18NLocalesDataList />
-                </LeftPanel>
-                <RightPanel>
-                    <I18NLocalesForm />
-                </RightPanel>
-            </SplitView>
-            <FloatingActionButton onClick={() => history.push("/i18n/locales")} />
-        </>
+        <SplitView>
+            <LeftPanel>
+                <I18NLocalesDataList />
+            </LeftPanel>
+            <RightPanel>
+                <I18NLocalesForm />
+            </RightPanel>
+        </SplitView>
     );
 }
