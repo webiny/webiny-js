54f250cea007dc68006ef3c776479ebd107cf1a9
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-12 21:11:06 +0530

    feat(app-i18n): update `I18NLocalesDataList` layout
    add new "sort and search" UI

--- packages/app-i18n/src/admin/views/I18NLocales/I18NLocalesDataList.tsx
+++ packages/app-i18n/src/admin/views/I18NLocales/I18NLocalesDataList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { i18n } from "@webiny/app/i18n";
 import { useI18N } from "@webiny/app-i18n/hooks/useI18N";
 import { useRouter } from "@webiny/react-router";
@@ -6,7 +6,7 @@ import { useQuery, useMutation } from "react-apollo";
 import { LIST_LOCALES, DELETE_LOCALE } from "./graphql";
 import { useSnackbar } from "@webiny/app-admin/hooks/useSnackbar";
 import { useConfirmationDialog } from "@webiny/app-admin/hooks/useConfirmationDialog";
-
+import orderBy from "lodash/orderBy";
 import {
     DataList,
     ScrollList,
@@ -18,10 +18,26 @@ import {
 } from "@webiny/ui/List";
 
 import { DeleteIcon } from "@webiny/ui/List/DataList/icons";
+import { ButtonIcon, ButtonSecondary } from "@webiny/ui/Button";
+import SearchUI from "@webiny/app-admin/components/SearchUI";
+import { ReactComponent as AddIcon } from "../../assets/icons/add-18px.svg";
 
 const t = i18n.ns("app-i18n/admin/locales/data-list");
 
+const SORTERS = [
+    {
+        label: t`Code A-Z`,
+        sorters: { code: "asc" }
+    },
+    {
+        label: t`Code Z-A`,
+        sorters: { code: "desc" }
+    }
+];
+
 const I18NLocalesDataList = () => {
+    const [filter, setFilter] = useState("");
+    const [sort, setSort] = useState(null);
     const { refetchLocales } = useI18N();
     const { history } = useRouter();
     const { showSnackbar } = useSnackbar();
@@ -31,6 +47,24 @@ const I18NLocalesDataList = () => {
     });
 
     const { showConfirmation } = useConfirmationDialog();
+
+    const filterLocales = useCallback(
+        ({ code }) => {
+            return code.toLowerCase().includes(filter);
+        },
+        [filter]
+    );
+
+    const sortLocaleList = useCallback(
+        locales => {
+            if (!sort) {
+                return locales;
+            }
+            const [[key, value]] = Object.entries(sort);
+            return orderBy(locales, [key], [value]);
+        },
+        [sort]
+    );
 
     const data = listQuery.loading ? [] : listQuery.data.i18n.listI18NLocales.data;
     const code = new URLSearchParams(location.search).get("code");
@@ -61,12 +95,32 @@ const I18NLocalesDataList = () => {
 
     const loading = [listQuery, deleteMutation].find(item => item.loading);
 
+    const filteredData = filter === "" ? data : data.filter(filterLocales);
+    const localeList = sortLocaleList(filteredData);
+
     return (
         <DataList
             loading={Boolean(loading)}
-            data={data}
+            actions={
+                <ButtonSecondary
+                    data-testid="new-record-button"
+                    onClick={() => history.push("/i18n/locales?new=true")}
+                >
+                    <ButtonIcon icon={<AddIcon />} /> {t`Add Locale`}
+                </ButtonSecondary>
+            }
+            data={localeList}
             title={t`Locales`}
             refresh={listQuery.refetch}
+            search={
+                <SearchUI
+                    value={filter}
+                    onChange={setFilter}
+                    inputPlaceholder={t`Search locales`}
+                />
+            }
+            sorters={SORTERS}
+            setSorters={sorter => setSort(sorter)}
         >
             {({ data }) => (
                 <ScrollList>
