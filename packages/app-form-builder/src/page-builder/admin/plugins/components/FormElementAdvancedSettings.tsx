0fcf5c206fa7aa36783a98f40064d5f017df1f6e
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-13 14:07:19 +0530

    fix(app-form-builder): update `latestRevisions` selection logic
    update FormElementAdvancedSettings component

--- packages/app-form-builder/src/page-builder/admin/plugins/components/FormElementAdvancedSettings.tsx
+++ packages/app-form-builder/src/page-builder/admin/plugins/components/FormElementAdvancedSettings.tsx
@@ -40,10 +40,18 @@ const FormElementAdvancedSettings = ({ Bind, submit, data }) => {
             value: null
         };
         if (listQuery.data) {
-            const parentsList = get(listQuery, "data.formBuilder.listForms.data") || [];
+            const latestFormRevisionsList = get(listQuery, "data.formBuilder.listForms.data") || [];
 
-            output.options = parentsList.map(({ id, name }) => ({ id, name }));
-            output.value = output.options.find(item => item.id === selectedForm.parent) || null;
+            output.options = latestFormRevisionsList.map(({ id, name }) => ({ id, name }));
+            output.value =
+                output.options.find(item => {
+                    if (typeof item.id !== "string" || typeof selectedForm.parent !== "string") {
+                        return;
+                    }
+                    // Get selected form's "baseId", i.e without the revision number suffix.
+                    const [baseId] = selectedForm.parent.split("#");
+                    return item.id.includes(baseId);
+                }) || null;
         }
 
         return output;
