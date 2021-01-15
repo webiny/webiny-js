66ca1df937f30e51cc92cc92c8fbdd43f0029e81
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-15 18:14:57 +0530

    fix(app-admin): revert `leftPanel` styles

--- packages/app-admin/src/components/SplitView/SplitView.tsx
+++ packages/app-admin/src/components/SplitView/SplitView.tsx
@@ -30,10 +30,8 @@ const leftPanel = css({
     ">.webiny-data-list": {
         display: "flex",
         flexDirection: "column",
-        height: "calc(100vh - 70px)",
+        maxHeight: "calc(100vh - 70px)",
         ".mdc-list": {
-            position: "relative",
-            height: "100%",
             overflow: "auto"
         }
     },
