490dac277bdc65d2e1f67fb453c10c8b0ac98314
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-15 18:08:07 +0530

    feat(app-page-builder): update CategoryDataList layout
    add "sort and search" UI

--- packages/app-page-builder/src/admin/views/Categories/CategoriesDataList.tsx
+++ packages/app-page-builder/src/admin/views/Categories/CategoriesDataList.tsx
@@ -1,10 +1,13 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { css } from "emotion";
+import classNames from "classnames";
 import { i18n } from "@webiny/app/i18n";
 import { useRouter } from "@webiny/react-router";
 import { useQuery, useMutation } from "react-apollo";
 import { LIST_CATEGORIES, DELETE_CATEGORY } from "./graphql";
 import { useSnackbar } from "@webiny/app-admin/hooks/useSnackbar";
 import { useConfirmationDialog } from "@webiny/app-admin/hooks/useConfirmationDialog";
+import orderBy from "lodash/orderBy";
 
 import {
     DataList,
@@ -16,18 +19,78 @@ import {
     ListItemTextSecondary
 } from "@webiny/ui/List";
 
-import { DeleteIcon } from "@webiny/ui/List/DataList/icons";
+import { DeleteIcon, FilterIcon } from "@webiny/ui/List/DataList/icons";
+import { Form } from "@webiny/form";
+import { Cell, Grid } from "@webiny/ui/Grid";
+import { Select } from "@webiny/ui/Select";
 import { useSecurity } from "@webiny/app-security";
+import { ButtonIcon, ButtonSecondary } from "@webiny/ui/Button";
+import SearchUI from "@webiny/app-admin/components/SearchUI";
+import { SimpleModal } from "@webiny/app-admin/components/SimpleModal";
+import { ReactComponent as AddIcon } from "@webiny/app-admin/assets/icons/add-18px.svg";
+import { serializeSorters, deserializeSorters } from "../utils";
 
 const t = i18n.ns("app-page-builder/admin/categories/data-list");
+const activeIcon = css({
+    "& svg": {
+        color: "var(--mdc-theme-primary)"
+    }
+});
 
-const PageBuilderCategoriesDataList = () => {
+const SORTERS = [
+    {
+        label: t`Newest to oldest`,
+        sorters: { createdOn: "desc" }
+    },
+    {
+        label: t`Oldest to newest`,
+        sorters: { createdOn: "asc" }
+    },
+    {
+        label: t`Name A-Z`,
+        sorters: { name: "asc" }
+    },
+    {
+        label: t`Name Z-A`,
+        sorters: { name: "desc" }
+    }
+];
+
+type PageBuilderCategoriesDataListProps = {
+    canCreate: boolean;
+};
+const PageBuilderCategoriesDataList = ({ canCreate }: PageBuilderCategoriesDataListProps) => {
+    const [filter, setFilter] = useState("");
+    const [sort, setSort] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const { history } = useRouter();
     const { showSnackbar } = useSnackbar();
     const listQuery = useQuery(LIST_CATEGORIES);
     const [deleteIt, deleteMutation] = useMutation(DELETE_CATEGORY, {
         refetchQueries: [{ query: LIST_CATEGORIES }]
     });
+
+    const filterData = useCallback(
+        ({ slug, name, url }) => {
+            return (
+                slug.toLowerCase().includes(filter) ||
+                name.toLowerCase().includes(filter) ||
+                url.toLowerCase().includes(filter)
+            );
+        },
+        [filter]
+    );
+
+    const sortData = useCallback(
+        users => {
+            if (!sort) {
+                return users;
+            }
+            const [[key, value]] = Object.entries(sort);
+            return orderBy(users, [key], [value]);
+        },
+        [sort]
+    );
 
     const { showConfirmation } = useConfirmationDialog();
 
@@ -75,12 +138,77 @@ const PageBuilderCategoriesDataList = () => {
 
     const loading = [listQuery, deleteMutation].find(item => item.loading);
 
+    const renderModal = useMemo(
+        () => (
+            <SimpleModal isOpen={isModalOpen} onDismiss={() => setIsModalOpen(false)}>
+                <Form
+                    data={{ sort: serializeSorters(sort) }}
+                    onChange={({ sort }) => {
+                        if (typeof sort === "string") {
+                            const newSort = deserializeSorters(sort);
+                            // @ts-ignore
+                            setSort(newSort);
+                        }
+                    }}
+                >
+                    {({ Bind }) => (
+                        <Grid>
+                            <Cell span={12}>
+                                <Bind name={"sort"}>
+                                    <Select label={t`Sort by`} description={"Sort pages by"}>
+                                        {SORTERS.map(({ label, sorters }) => {
+                                            return (
+                                                <option
+                                                    key={label}
+                                                    value={serializeSorters(sorters)}
+                                                >
+                                                    {label}
+                                                </option>
+                                            );
+                                        })}
+                                    </Select>
+                                </Bind>
+                            </Cell>
+                        </Grid>
+                    )}
+                </Form>
+            </SimpleModal>
+        ),
+        [isModalOpen]
+    );
+
+    const filteredData = filter === "" ? data : data.filter(filterData);
+    const categoryList = sortData(filteredData);
+
     return (
         <DataList
-            loading={Boolean(loading)}
-            data={data}
             title={t`Categories`}
-            refresh={listQuery.refetch}
+            loading={Boolean(loading)}
+            data={categoryList}
+            actions={
+                canCreate ? (
+                    <ButtonSecondary
+                        data-testid="new-record-button"
+                        onClick={() => history.push("/page-builder/categories?new=true")}
+                    >
+                        <ButtonIcon icon={<AddIcon />} /> {t`Add Category`}
+                    </ButtonSecondary>
+                ) : null
+            }
+            search={
+                <SearchUI
+                    value={filter}
+                    onChange={setFilter}
+                    inputPlaceholder={t`Search categories`}
+                />
+            }
+            modal={renderModal}
+            modalAction={
+                <FilterIcon
+                    className={classNames({ [activeIcon]: sort })}
+                    onClick={() => setIsModalOpen(!isModalOpen)}
+                />
+            }
         >
             {({ data }) => (
                 <ScrollList>
