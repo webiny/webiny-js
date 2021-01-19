7518fdf6968a630004f3836aee57d941b6ac0b5f
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-19 09:07:19 +0530

    feat(app-headless-cms): add `canCreate` "permission" check
    remove `FloatingActionButton`

--- packages/app-headless-cms/src/admin/views/ContentModels/ContentModels.tsx
+++ packages/app-headless-cms/src/admin/views/ContentModels/ContentModels.tsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { SplitView, LeftPanel } from "@webiny/app-admin/components/SplitView";
-import { FloatingActionButton } from "@webiny/app-admin/components/FloatingActionButton";
+import { useSecurity } from "@webiny/app-security";
 import ContentModelsDataList from "./ContentModelsDataList";
 import NewContentModelDialog from "./NewContentModelDialog";
 
 function ContentModels() {
     const [newContentModelDialogOpened, openNewContentModelDialog] = React.useState(false);
 
+    const { identity } = useSecurity();
+
+    const canCreate = useMemo(() => {
+        const permission = identity.getPermission("cms.contentModel");
+        if (!permission) {
+            return false;
+        }
+
+        if (typeof permission.rwd !== "string") {
+            return true;
+        }
+
+        return permission.rwd.includes("w");
+    }, []);
+
+    const onCreate = useCallback(() => openNewContentModelDialog(true), []);
+    const onClose = useCallback(() => openNewContentModelDialog(false), []);
+
     return (
         <>
-            <NewContentModelDialog
-                open={newContentModelDialogOpened}
-                onClose={() => openNewContentModelDialog(false)}
-            />
+            <NewContentModelDialog open={newContentModelDialogOpened} onClose={onClose} />
 
             <SplitView>
                 <LeftPanel span={12}>
-                    <ContentModelsDataList />
+                    <ContentModelsDataList canCreate={canCreate} onCreate={onCreate} />
                 </LeftPanel>
             </SplitView>
-            <FloatingActionButton
-                data-testid="new-record-button"
-                onClick={() => openNewContentModelDialog(true)}
-            />
         </>
     );
 }
