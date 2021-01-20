123e5c83434cb6ae62526c51a691ba2568130125
Pavel Denisjuk <pavel@webiny.com>
2021-01-20 19:13:12 +0100

    fix(handler-graphql): cache graphql schema per handler instance

--- packages/handler-graphql/src/createGraphQLHandler.ts
+++ packages/handler-graphql/src/createGraphQLHandler.ts
@@ -19,123 +19,127 @@ const DEFAULT_HEADERS = {
     "Access-Control-Allow-Methods": "OPTIONS,POST"
 };
 
-let schema;
-
-export default (options: HandlerGraphQLOptions = {}): HandlerPlugin => ({
-    type: "handler",
-    name: "handler-graphql",
-    async handle(context: Context & HttpContext, next) {
-        const { http } = context;
-        if (!http) {
-            return next();
-        }
-
-        if (http.request.method === "OPTIONS") {
-            return http.response({
-                statusCode: 204,
-                headers: DEFAULT_HEADERS
-            });
-        }
-
-        if (http.request.method !== "POST") {
-            return next();
-        }
-
-        if (!schema) {
-            const scalars = context.plugins
-                .byType<GraphQLScalarPlugin>("graphql-scalar")
-                .map(item => item.scalar);
-
-            const typeDefs = [
-                gql`
-                    type Query
-                    type Mutation
-                    ${scalars.map(scalar => `scalar ${scalar.name}`).join(" ")}
-                    scalar JSON
-                    scalar Long
-                    scalar DateTime
-                    scalar RefInput
-                    scalar Number
-                    scalar Any
-                `
-            ];
-
-            const resolvers = [
-                {
-                    ...scalars.reduce((acc, s) => {
-                        acc[s.name] = s;
-                        return acc;
-                    }, {}),
-                    JSON: GraphQLJSON,
-                    DateTime: GraphQLDateTime,
-                    Long: GraphQLLong,
-                    RefInput,
-                    Number,
-                    Any
-                }
-            ];
-
-            const gqlPlugins = context.plugins.byType("graphql-schema");
-            for (let i = 0; i < gqlPlugins.length; i++) {
-                const plugin = gqlPlugins[i];
-                typeDefs.push(plugin.schema.typeDefs);
-                resolvers.push(plugin.schema.resolvers);
+export default (options: HandlerGraphQLOptions = {}): HandlerPlugin => {
+    let schema;
+    
+    return {
+        type: "handler",
+        name: "handler-graphql",
+        async handle(context: Context & HttpContext, next) {
+            const { http } = context;
+            if (!http) {
+                return next();
             }
 
-            schema = makeExecutableSchema({
-                typeDefs,
-                resolvers
-            });
-        }
-
-        try {
-            const body = JSON.parse(http.request.body);
-            let result;
-            if (Array.isArray(body)) {
-                const promises = [];
-                for (let i = 0; i < body.length; i++) {
-                    const { query, variables, operationName } = body[i];
-                    promises.push(graphql(schema, query, {}, context, variables, operationName));
-                }
-
-                result = await Promise.all(promises);
-            } else {
-                const { query, variables, operationName } = body;
-                result = await graphql(schema, query, {}, context, variables, operationName);
-            }
-
-            return http.response({
-                body: JSON.stringify(result),
-                statusCode: 200,
-                headers: DEFAULT_HEADERS
-            });
-        } catch (e) {
-            const report = {
-                error: {
-                    name: e.constructor.name,
-                    message: e.message,
-                    stack: e.stack
-                }
-            };
-
-            console.log(
-                "[@webiny/handler-graphql] An error occurred:",
-                JSON.stringify(report, null, 2)
-            );
-
-            if (boolean(options.debug)) {
-                return context.http.response({
-                    statusCode: 500,
-                    body: JSON.stringify(report, null, 2),
-                    headers: {
-                        ...DEFAULT_HEADERS,
-                        "Cache-Control": "no-store",
-                        "Content-Type": "text/json"
-                    }
+            if (http.request.method === "OPTIONS") {
+                return http.response({
+                    statusCode: 204,
+                    headers: DEFAULT_HEADERS
                 });
             }
 
-            throw e;
+            if (http.request.method !== "POST") {
+                return next();
+            }
+
+            if (!schema) {
+                const scalars = context.plugins
+                    .byType<GraphQLScalarPlugin>("graphql-scalar")
+                    .map(item => item.scalar);
+
+                const typeDefs = [
+                    gql`
+                        type Query
+                        type Mutation
+                        ${scalars.map(scalar => `scalar ${scalar.name}`).join(" ")}
+                        scalar JSON
+                        scalar Long
+                        scalar DateTime
+                        scalar RefInput
+                        scalar Number
+                        scalar Any
+                    `
+                ];
+
+                const resolvers = [
+                    {
+                        ...scalars.reduce((acc, s) => {
+                            acc[s.name] = s;
+                            return acc;
+                        }, {}),
+                        JSON: GraphQLJSON,
+                        DateTime: GraphQLDateTime,
+                        Long: GraphQLLong,
+                        RefInput,
+                        Number,
+                        Any
+                    }
+                ];
+
+                const gqlPlugins = context.plugins.byType("graphql-schema");
+                for (let i = 0; i < gqlPlugins.length; i++) {
+                    const plugin = gqlPlugins[i];
+                    typeDefs.push(plugin.schema.typeDefs);
+                    resolvers.push(plugin.schema.resolvers);
+                }
+
+                schema = makeExecutableSchema({
+                    typeDefs,
+                    resolvers
+                });
+            }
+
+            try {
+                const body = JSON.parse(http.request.body);
+                let result;
+                if (Array.isArray(body)) {
+                    const promises = [];
+                    for (let i = 0; i < body.length; i++) {
+                        const { query, variables, operationName } = body[i];
+                        promises.push(
+                            graphql(schema, query, {}, context, variables, operationName)
+                        );
+                    }
+
+                    result = await Promise.all(promises);
+                } else {
+                    const { query, variables, operationName } = body;
+                    result = await graphql(schema, query, {}, context, variables, operationName);
+                }
+
+                return http.response({
+                    body: JSON.stringify(result),
+                    statusCode: 200,
+                    headers: DEFAULT_HEADERS
+                });
+            } catch (e) {
+                const report = {
+                    error: {
+                        name: e.constructor.name,
+                        message: e.message,
+                        stack: e.stack
+                    }
+                };
+
+                console.log(
+                    "[@webiny/handler-graphql] An error occurred:",
+                    JSON.stringify(report, null, 2)
+                );
+
+                if (boolean(options.debug)) {
+                    return context.http.response({
+                        statusCode: 500,
+                        body: JSON.stringify(report, null, 2),
+                        headers: {
+                            ...DEFAULT_HEADERS,
+                            "Cache-Control": "no-store",
+                            "Content-Type": "text/json"
+                        }
+                    });
+                }
+
+                throw e;
+            }
         }
-    }
-});
+    };
+};
