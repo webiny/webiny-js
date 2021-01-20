fec0beca775966840bc0dd7f7cacb6c7ec952a80
Pavel Denisjuk <pavel@webiny.com>
2021-01-20 17:22:52 +0100

    feat(cwp-template-aws): print all environments if --env parameter is not provided

--- packages/cwp-template-aws/cli/info/index.js
+++ packages/cwp-template-aws/cli/info/index.js
@@ -1,6 +1,60 @@
 const getStackOutput = require("@webiny/cli-plugin-deploy-pulumi/utils/getStackOutput");
 const { green } = require("chalk");
 
+const line = `-------------------------`;
+
+const printEnvOutput = async (env, context) => {
+    console.log(line);
+    console.log(`Environment: ${green(env)}`);
+    console.log(line);
+
+    let stacksDeployedCount = 0;
+    let output = await getStackOutput("api", env);
+    if (output) {
+        stacksDeployedCount++;
+        console.log(
+            [
+                `➜ Main GraphQL API: ${green(output.apiUrl + "/graphql")}`,
+                `➜ Headless CMS GraphQL API:`,
+                `   - Manage API: ${green(output.apiUrl + "/cms/manage/{LOCALE_CODE}")}`,
+                `   - Read API: ${green(output.apiUrl + "/cms/read/{LOCALE_CODE}")}`,
+                `   - Preview API: ${green(output.apiUrl + "/cms/preview/{LOCALE_CODE}")}`
+            ].join("\n")
+        );
+    } else {
+        context.info(`Stack ${green("api")} not deployed yet.`);
+    }
+
+    output = await getStackOutput("apps/admin", env);
+    if (output) {
+        stacksDeployedCount++;
+        console.log([`➜ Admin app: ${green(output.appUrl)}`].join("\n"));
+    } else {
+        context.info(`Stack ${green("apps/admin")} not deployed yet.`);
+    }
+
+    output = await getStackOutput("apps/website", env);
+    if (output) {
+        stacksDeployedCount++;
+        console.log(
+            [
+                `➜ Public website:`,
+                `   - Website URL: ${green(output.deliveryUrl)}`,
+                `   - Website preview URL: ${green(output.appUrl)}`
+            ].join("\n")
+        );
+    } else {
+        context.info(`Stack ${green("apps/website")} not deployed yet.`);
+    }
+
+    if (stacksDeployedCount === 0) {
+        context.info("It seems none of the stacks were deployed, so no info could be provided.");
+        context.info(`Please check if the provided environment ${green(env)} is correct.`);
+    }
+
+    console.log(line + "\n");
+};
+
 module.exports = {
     type: "cli-command",
     name: "cli-command-info",
@@ -12,7 +66,7 @@ module.exports = {
                 yargs.option("env", {
                     describe: `Environment`,
                     type: "string",
-                    required: true
+                    required: false
                 });
                 yargs.option("debug", {
                     describe: `Debug`,
@@ -22,58 +76,31 @@ module.exports = {
             async args => {
                 const { env } = args;
 
-                let stacksDeployedCount = 0;
-                let output = await getStackOutput("api", env);
-                if (output) {
-                    stacksDeployedCount++;
-                    console.log(
-                        [
-                            `➜ Main GraphQL API: ${green(output.apiUrl + "/graphql")}`,
-                            `➜ Headless CMS GraphQL API:`,
-                            `   - Manage API: ${green(
-                                output.apiUrl + "/cms/manage/{LOCALE_CODE}"
-                            )}`,
-                            `   - Read API: ${green(output.apiUrl + "/cms/read/{LOCALE_CODE}")}`,
-                            `   - Preview API: ${green(
-                                output.apiUrl + "/cms/preview/{LOCALE_CODE}"
-                            )}`
-                        ].join("\n")
-                    );
-                } else {
-                    context.info(`Stack ${green("api")} not deployed yet.`);
+                if (!env) {
+                    // Get all existing environments
+                    const glob = require("fast-glob");
+                    const pulumiFiles = await glob(["**/Pulumi.*.yaml"], {
+                        cwd: context.paths.projectRoot,
+                        onlyFiles: true,
+                        ignore: ["**/node_modules/**"]
+                    });
+
+                    const existingEnvs = new Set();
+                    const regex = /Pulumi\.(\w+)\.yaml/;
+                    pulumiFiles.forEach(file => {
+                        if (file.match(regex)) {
+                            existingEnvs.add(RegExp.$1);
+                        }
+                    });
+
+                    for (const env of existingEnvs) {
+                        await printEnvOutput(env, context);
+                    }
+
+                    return;
                 }
 
-                output = await getStackOutput("apps/admin", env);
-                if (output) {
-                    stacksDeployedCount++;
-                    console.log([`➜ Admin app: ${green(output.appUrl)}`].join("\n"));
-                } else {
-                    context.info(`Stack ${green("apps/admin")} not deployed yet.`);
-                }
-
-                output = await getStackOutput("apps/website", env);
-                if (output) {
-                    stacksDeployedCount++;
-                    console.log(
-                        [
-                            `➜ Public website:`,
-                            `   - Website URL: ${green(output.deliveryUrl)}`,
-                            `   - Website preview URL: ${green(output.appUrl)}`
-                        ].join("\n")
-                    );
-                } else {
-                    context.info(`Stack ${green("apps/website")} not deployed yet.`);
-                }
-
-                if (stacksDeployedCount === 0) {
-                    console.log();
-                    context.info(
-                        "It seems none of the stacks were deployed, so no info could be provided."
-                    );
-                    context.info(
-                        `Please check if the provided environment ${green(env)} is correct.`
-                    );
-                }
+                await printEnvOutput(env, context);
             }
         );
     }
