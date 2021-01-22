0430d98703fb2a1adbb419b9ea44088447c57b42
Pavel Denisjuk <pavel@webiny.com>
2021-01-22 12:46:10 +0100

    fix(handler-aws): change the order of plugins

--- packages/handler-aws/src/index.ts
+++ packages/handler-aws/src/index.ts
@@ -3,16 +3,14 @@ import defaultHandlerHttp from "@webiny/handler-http";
 import defaultHandlerArgs from "@webiny/handler-args";
 import handlerClient from "./plugins/handlerClient";
 import handlerHttp from "./plugins/handlerHttp";
-import handlerArgs from "./plugins/handlerArgs";
 import { createHandler as createDefaultHandler } from "@webiny/handler";
 
 export const createHandler = (...plugins) => {
     return createDefaultHandler(
         defaultHandlerClient(),
-        defaultHandlerHttp(),
         defaultHandlerArgs(),
+        defaultHandlerHttp(),
         handlerClient,
-        handlerArgs,
         handlerHttp,
         ...plugins
     );
