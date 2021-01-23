411aab77bfeb7e8e17ac552f8c37f9da498b1992
Ashutosh <bhardwajashu96@hotmail.com>
2021-01-23 08:34:48 +0530

    fix(app-page-builder): move `CellContainer` border to "pseudo after"

--- packages/app-page-builder/src/editor/plugins/elements/cell/CellContainer.tsx
+++ packages/app-page-builder/src/editor/plugins/elements/cell/CellContainer.tsx
@@ -21,9 +21,17 @@ const CellContainerStyle = styled("div")({
     boxSizing: "border-box",
     flexGrow: 1,
     width: `100%`,
-    border: "1px dashed gray",
     " > div": {
         width: "100%"
+    },
+    "&::after": {
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        border: "1px dashed gray"
     }
 });
 const addIcon = css({
