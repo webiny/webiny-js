d5d934ef8c9211c69038aafe8ab0cea00b2eb59e
Adrian Smijulj <adrian1358@gmail.com>
2021-01-29 09:40:55 +0100

    fix: reword VPC question

--- packages/cwp-template-aws/index.js
+++ packages/cwp-template-aws/index.js
@@ -45,14 +45,14 @@ function runInquirer(cwp) {
                 name: "vpc",
                 default: false,
                 message:
-                    "Do you want to have your API deployed into a Virtual Private Cloud (VPC)?",
+                    "Do you want to have your API project application deployed into a custom Virtual Private Cloud (VPC)?",
                 choices: [
                     {
-                        name: "Yes (more secure, includes a NAT Gateway, incurs cost)",
+                        name: "Yes (includes private subnets and a NAT Gateway, more secure, incurs cost)",
                         value: true
                     },
                     {
-                        name: "No (less secure, all deployed resources are free tier eligible)",
+                        name: "No, use the default VPC (less secure, all deployed resources are free tier eligible)",
                         value: false
                     }
                 ]
