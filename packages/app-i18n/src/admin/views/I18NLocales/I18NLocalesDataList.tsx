41fd86cc81a24115197e439b8fe2f06510b376ba
Ashutosh <bhardwajashu96@hotmail.com>
2021-02-02 13:26:39 +0530

    fix(app-i18n): reload page after add locale and delete locale

--- packages/app-i18n/src/admin/views/I18NLocales/I18NLocalesDataList.tsx
+++ packages/app-i18n/src/admin/views/I18NLocales/I18NLocalesDataList.tsx
@@ -110,6 +110,8 @@ const I18NLocalesDataList = () => {
                 }
 
                 refetchLocales();
+                // Reload page
+                window.location.reload();
             });
         },
         [code]
