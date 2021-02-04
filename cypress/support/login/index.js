8f7fd99bc716d2b68d75254ed9db2497c72c1fd2
Adrian Smijulj <adrian1358@gmail.com>
2021-02-04 09:53:17 +0100

    test: export the `login` function

--- cypress/support/login/index.js
+++ cypress/support/login/index.js
@@ -3,9 +3,11 @@ import authenticateWithCognito from "./authenticateWithCognito";
 const DEFAULT_USERNAME = Cypress.env("DEFAULT_ADMIN_USER_USERNAME");
 const DEFAULT_PASSWORD = Cypress.env("DEFAULT_ADMIN_USER_PASSWORD");
 
-Cypress.Commands.add(
-    "login",
-    ({ username, password } = { username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD }) => {
-        return authenticateWithCognito({ username, password });
-    }
-);
+const DEFAULT_LOGIN = { username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD };
+
+const login = ({ username, password } = DEFAULT_LOGIN) =>
+    authenticateWithCognito({ username, password });
+
+Cypress.Commands.add("login", login);
+
+export default login;
