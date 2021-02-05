691471dda8e4fb5775491ab8d5fb342d83f1293b
Adrian Smijulj <adrian1358@gmail.com>
2021-02-05 08:13:42 +0100

    test: cache logged in user

--- cypress/support/login/index.js
+++ cypress/support/login/index.js
@@ -5,9 +5,15 @@ const DEFAULT_PASSWORD = Cypress.env("DEFAULT_ADMIN_USER_PASSWORD");
 
 const DEFAULT_LOGIN = { username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD };
 
-const login = ({ username, password } = DEFAULT_LOGIN) =>
-    authenticateWithCognito({ username, password });
+const cache = {};
 
-Cypress.Commands.add("login", login);
+Cypress.Commands.add("login", ({ username, password } = DEFAULT_LOGIN) => {
+    if (cache[username + password]) {
+        return cache[username + password];
+    }
 
-export default login;
+    return authenticateWithCognito({ username, password }).then(response => {
+        cache[username + password] = response;
+        return cache[username + password];
+    });
+});
