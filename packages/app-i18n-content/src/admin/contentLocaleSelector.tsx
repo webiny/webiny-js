85e3d62bc52b421a25fca4670c1001d81d5164d9
Ashutosh <bhardwajashu96@hotmail.com>
2021-02-09 17:24:18 +0530

    fix(app-i18n-content): filter locales as per permission

--- packages/app-i18n-content/src/admin/contentLocaleSelector.tsx
+++ packages/app-i18n-content/src/admin/contentLocaleSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { AdminHeaderRightPlugin } from "@webiny/app-admin/types";
 import { useI18N } from "@webiny/app-i18n/hooks/useI18N";
 import { Menu, MenuItem } from "@webiny/ui/Menu";
@@ -6,6 +6,7 @@ import { ButtonPrimary, ButtonIcon } from "@webiny/ui/Button";
 import { css } from "emotion";
 import { ReactComponent as DoneIcon } from "./assets/done-24px.svg";
 import { ReactComponent as TranslateIcon } from "./assets/round-translate-24px.svg";
+import { useSecurity } from "@webiny/app-security";
 
 const menuList = css({
     width: 160,
@@ -21,10 +22,19 @@ const plugin: AdminHeaderRightPlugin = {
     type: "admin-header-right",
     render() {
         const { setCurrentLocale, getCurrentLocale, getLocales } = useI18N();
+        const { identity } = useSecurity();
+
+        const contentI18NPermission = useMemo(() => identity.getPermission("content.i18n"), []);
 
         const locales = getLocales();
+        const localeList = locales.filter(locale => {
+            if (!contentI18NPermission || !Array.isArray(contentI18NPermission.locales)) {
+                return true;
+            }
+            return contentI18NPermission.locales.includes(locale.code);
+        });
 
-        if (locales.length === 1) {
+        if (localeList.length === 1) {
             return null;
         }
 
@@ -39,7 +49,7 @@ const plugin: AdminHeaderRightPlugin = {
                     </ButtonPrimary>
                 }
             >
-                {locales.map(locale => (
+                {localeList.map(locale => (
                     <MenuItem
                         key={locale.code}
                         onClick={() => {
