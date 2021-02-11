2dd0957c541a2e48d0bac2ed97a76bd9c6c81fbe
Pavel Denisjuk <pavel@webiny.com>
2021-02-11 14:19:55 +0100

    fix(cli): restore telemetry control (#1430)

--- packages/cli/config/index.js
+++ packages/cli/config/index.js
@@ -2,6 +2,7 @@ const os = require("os");
 const path = require("path");
 const readJson = require("load-json-file");
 const writeJson = require("write-json-file");
+const publicIp = require("public-ip");
 
 const configPath = path.join(os.homedir(), ".webiny", "config");
 
@@ -15,7 +16,6 @@ const verifyConfig = async () => {
             throw Error("Invalid Webiny config!");
         }
     } catch (e) {
-        const publicIp = require("public-ip");
         // A new config file is written if it doesn't exist or is invalid.
         config = { id: await publicIp.v4() };
         await writeJson(configPath, config);
@@ -25,7 +25,21 @@ const verifyConfig = async () => {
 };
 
 const getId = () => {
-    return `${process.env.WEBINY_TRACKING_PREFIX || ""}${config.id}`;
+    return config.id;
 };
 
-module.exports = { verifyConfig, getId };
+const setTracking = async enabled => {
+    try {
+        const config = readJson.sync(configPath);
+        if (!config.id) {
+            config.id = await publicIp.v4();
+        }
+        config.tracking = enabled;
+        writeJson.sync(configPath, config);
+    } catch (e) {
+        // A new config file is written if it doesn't exist.
+        writeJson.sync(configPath, { id: await publicIp.v4(), tracking: enabled });
+    }
+};
+
+module.exports = { verifyConfig, getId, setTracking };
