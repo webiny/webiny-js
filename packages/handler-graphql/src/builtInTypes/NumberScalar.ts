7aa17197c78b2d0a076eb0a61c60d480cc8dc7ad
Bruno Zorić <bruno.zoric@gmail.com>
2021-02-17 20:26:01 +0100

    refactor(handler-graphql): tests and parsing for number

--- packages/handler-graphql/src/builtInTypes/NumberScalar.ts
+++ packages/handler-graphql/src/builtInTypes/NumberScalar.ts
@@ -1,26 +1,42 @@
 import { GraphQLScalarType } from "graphql";
 import { Kind } from "graphql/language";
+import WebinyError from "@webiny/error";
 
-const getNumber = (value: any): any => {
-    if (typeof value !== "number") {
+const parseValue = (value: any) => {
+    if (String(value).match(/^0x/) !== null) {
+        throw new WebinyError("Value sent must be a non-hex number.", "INVALID_VALUE", {
+            value
+        });
+    } else if (typeof value === "number") {
+        return value;
+    } else if (value === null || value === undefined) {
         return null;
+    } else if (isNaN(value) === true) {
+        throw new WebinyError("Value sent must be a number.", "INVALID_VALUE", {
+            value
+        });
     }
-
-    return value;
+    return parseFloat(value);
 };
 
 export const Number = new GraphQLScalarType({
     name: "Number",
     description: "A custom input type to be used with numbers. Supports Int and Float.",
-    serialize: (value: string | number) => {
-        if (typeof value === "number") {
-            return value;
-        } else if (value === null || value === undefined || isNaN(value as any) === true) {
+    serialize: (value: any) => {
+        try {
+            return parseValue(value);
+        } catch (ex) {
+            console.log({
+                message: "Value sent must be a number.",
+                code: "INVALID_VALUE",
+                data: {
+                    value
+                }
+            });
             return null;
         }
-        return parseFloat(value);
     },
-    parseValue: getNumber,
+    parseValue,
     parseLiteral: (ast: any) => {
         if (ast.kind === Kind.INT || ast.kind === Kind.FLOAT) {
             return ast.value;
