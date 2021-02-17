be1b0ca1b6a13ec5faa13b47fd3ad94712615b2c
Bruno Zorić <bruno.zoric@gmail.com>
2021-02-17 09:43:06 +0100

    feat(api-headless-cms): import system upgrades into admin graphql

--- packages/api-headless-cms/src/plugins/index.ts
+++ packages/api-headless-cms/src/plugins/index.ts
@@ -2,5 +2,6 @@ import graphql from "./graphql";
 import crud from "./crud";
 import context from "./context";
 import contentModelGroup from "../content/plugins/crud/contentModelGroup.crud";
+import systemUpgrades from "../upgrades";
 
-export default () => [context(), graphql(), crud(), contentModelGroup()];
+export default () => [context(), graphql(), crud(), contentModelGroup(), systemUpgrades()];
