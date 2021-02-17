712838f7dcf9aeb26f9d8ba02711789d6fac0dde
Adrian Smijulj <adrian1358@gmail.com>
2021-02-17 18:40:20 +0100

    test: update tests

--- cypress/support/login/index.js
+++ cypress/support/login/index.js
@@ -7,13 +7,29 @@ const DEFAULT_LOGIN = { username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD }
 
 const cache = {};
 
+// A trivial approach. Upgrade if needed.
+const hasLoginDataInLocalStorage = () => {
+    for (const key in localStorage) {
+        if (key.startsWith("CognitoIdentityServiceProvider")) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
 Cypress.Commands.add("login", ({ username, password } = DEFAULT_LOGIN) => {
-    if (cache[username + password]) {
+    console.log(localStorage);
+    console.log(process.env);
+    window.sajpres = Cypress;
+    if (cache[username + password] && hasLoginDataInLocalStorage()) {
         return cache[username + password];
     }
 
     return authenticateWithCognito({ username, password }).then(response => {
         cache[username + password] = response;
+        console.log(localStorage);
+
         return cache[username + password];
     });
 });
