2fe6c819df2eaba54fe65e8d33f658b2e0fbaf21
Bruno Zorić <bruno.zoric@gmail.com>
2021-02-19 10:23:21 +0100

    fix(app-headless-cms): item click clears the ref field (#1455)

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/components/ContentEntriesAutocomplete.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/components/ContentEntriesAutocomplete.tsx
@@ -12,8 +12,7 @@ const label = t`Selected content entry is not published. Make sure to {publishIt
 function ContentEntriesAutocomplete({ bind, field }) {
     const { options, setSearch, value, loading, onChange } = useReference({
         bind,
-        field,
-        assignValueEntry: true
+        field
     });
 
     // Currently we only support 1 model in the `ref` field, so we use index 0 (this will be upgraded in the future).
