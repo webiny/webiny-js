443ae742cc183f3e0b9eeb00bb2ab10ccc92ccc1
Adrian Smijulj <adrian1358@gmail.com>
2021-02-20 13:02:55 +0100

    fix: remove redundant login call

--- packages/cli-plugin-deploy-pulumi/app/deploy.js
+++ packages/cli-plugin-deploy-pulumi/app/deploy.js
@@ -81,10 +81,6 @@ module.exports = async (inputs, context) => {
         }
     });
 
-    await pulumi.run({
-        command: ["login", login]
-    });
-
     let stackExists = true;
     try {
         await pulumi.run(
