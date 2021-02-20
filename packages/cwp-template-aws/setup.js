5fb8e03cab9311dd842c679a8da0b74cf57b986f
Adrian Smijulj <adrian1358@gmail.com>
2021-02-20 08:38:14 +0100

    fix: remove "iac" option

--- packages/cwp-template-aws/setup.js
+++ packages/cwp-template-aws/setup.js
@@ -3,16 +3,13 @@ const path = require("path");
 const execa = require("execa");
 const crypto = require("crypto");
 const renames = require("./setup/renames");
-const get = require("lodash/get");
 const merge = require("lodash/merge");
-const trimEnd = require("lodash/trimEnd");
 const writeJsonFile = require("write-json-file");
 const loadJsonFile = require("load-json-file");
 const getPackages = require("get-yarn-workspaces");
 const { green } = require("chalk");
 
 const IS_TEST = process.env.NODE_ENV === "test";
-const DEFAULT_BACKEND_URL = "file://";
 
 // Automatic detection could be added here.
 function getDefaultRegion() {
@@ -31,7 +28,6 @@ const setup = async args => {
     const {
         vpc = false,
         region = getDefaultRegion(),
-        iac = ["pulumi", { backend: { url: DEFAULT_BACKEND_URL } }]
     } = templateOptions;
 
     fs.copySync(path.join(__dirname, "template"), projectRoot);
@@ -93,23 +89,6 @@ const setup = async args => {
         path.join(projectRoot, "api", "pulumi"),
         { overwrite: true }
     );
-
-    // Set the appropriate backend.url value in Pulumi.yaml files.
-    const [, iacOptions = {}] = iac;
-
-    const pulumiYamlFolders = ["api", "apps/admin", "apps/website"];
-    for (let i = 0; i < pulumiYamlFolders.length; i++) {
-        const pulumiYamlFolder = pulumiYamlFolders[i];
-        const pulumiYamlPath = path.join(projectRoot, pulumiYamlFolder, "Pulumi.yaml");
-        const content = fs.readFileSync(pulumiYamlPath, "utf-8");
-
-        let backendUrl = get(iacOptions, "backend.url") || DEFAULT_BACKEND_URL;
-        if (backendUrl !== DEFAULT_BACKEND_URL) {
-            backendUrl = trimEnd(backendUrl, "/") + "/" + pulumiYamlFolder;
-        }
-
-        fs.writeFileSync(pulumiYamlPath, content.replace("{BACKEND_URL}", backendUrl));
-    }
 
     // Adjust versions - change them from `latest` to current one.
     const latestVersion = version;
