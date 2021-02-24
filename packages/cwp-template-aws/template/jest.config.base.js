c786c65a9e165bc185814b27c3b6b5b22557cc90
Pavel Denisjuk <pavel@webiny.com>
2021-02-24 23:16:12 +0100

    fix(cwp-template-aws): add new ENV variables

--- packages/cwp-template-aws/template/jest.config.base.js
+++ packages/cwp-template-aws/template/jest.config.base.js
@@ -1,6 +1,10 @@
 const { basename } = require("path");
 const merge = require("merge");
 const tsPreset = require("ts-jest/presets/js-with-babel/jest-preset");
+const { version } = require("@webiny/cli/package.json");
+
+process.env.DB_TABLE_ELASTICSEARCH = "ElasticSearchStream";
+process.env.WEBINY_VERSION = version;
 
 module.exports = ({ path }, presets = []) => {
     const name = basename(path);
