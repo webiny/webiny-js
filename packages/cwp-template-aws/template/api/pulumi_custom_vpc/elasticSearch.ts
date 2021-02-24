a0057109134b11d7a1d9e930edec10698ec102ba
Pavel Denisjuk <pavel@webiny.com>
2021-02-24 23:06:54 +0100

    feat: add api-dynamodb-to-elasticsearch handler

--- packages/cwp-template-aws/template/api/pulumi_custom_vpc/elasticSearch.ts
+++ packages/cwp-template-aws/template/api/pulumi_custom_vpc/elasticSearch.ts
@@ -1,8 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import vpc from "./vpc";
+import defaultLambdaRole from "./defaultLambdaRole";
 
 class ElasticSearch {
     domain: aws.elasticsearch.Domain;
+    table: aws.dynamodb.Table;
 
     constructor() {
         const domainName = "webiny-js";
@@ -26,9 +29,6 @@ class ElasticSearch {
             },
             snapshotOptions: {
                 automatedSnapshotStartHour: 23
-            },
-            tags: {
-                Domain: "TestDomain"
             }
         });
 
@@ -47,6 +47,62 @@ class ElasticSearch {
                     }
                 ]
             }
+        });
+
+        /**
+         * Create a table for Elasticsearch records. All ES records are stored in this table to dramatically improve
+         * performance and stability on write operations (especially massive data imports). This table also serves as a backup and
+         * a single source of truth for your Elasticsearch domain. Streaming is enabled on this table, and it will
+         * allow asynchronous synchronization of data with Elasticsearch domain.
+         */
+        this.table = new aws.dynamodb.Table("webiny-es", {
+            attributes: [
+                { name: "PK", type: "S" },
+                { name: "SK", type: "S" }
+            ],
+            streamEnabled: true,
+            streamViewType: "NEW_AND_OLD_IMAGES",
+            billingMode: "PAY_PER_REQUEST",
+            hashKey: "PK",
+            rangeKey: "SK",
+            readCapacity: 1,
+            writeCapacity: 1
+        });
+
+        /**
+         * This Lambda will process the stream events from DynamoDB table that contains Elasticsearch items.
+         * Elasticsearch can't take large amount of individual writes in a short period of time, so this way
+         * we store data for Elasticsearch in a DynamoDB table, and asynchronously insert it into Elasticsearch
+         * using batching.
+         */
+        const streamTarget = new aws.lambda.Function("dynamo-to-elastic", {
+            role: defaultLambdaRole.role.arn,
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            timeout: 600,
+            memorySize: 512,
+            environment: {
+                variables: {
+                    ELASTIC_SEARCH_ENDPOINT: this.domain.endpoint
+                }
+            },
+            description: "Process DynamoDB Stream.",
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("../code/dynamoToElastic/build")
+            }),
+            vpcConfig: {
+                subnetIds: vpc.subnets.private.map(subNet => subNet.id),
+                securityGroupIds: [vpc.vpc.defaultSecurityGroupId]
+            }
+        });
+
+        new aws.lambda.EventSourceMapping("dynamo-to-elastic", {
+            eventSourceArn: this.table.streamArn,
+            functionName: streamTarget.arn,
+            startingPosition: "LATEST",
+            maximumRetryAttempts: 3,
+            batchSize: 100,
+            maximumBatchingWindowInSeconds: 3
         });
     }
 }
