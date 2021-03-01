b4b7b65ada5b52e8cb7067d43c3c56c0fd7ff4b5
Pavel Denisjuk <pavel@webiny.com>
2021-03-01 19:02:59 +0100

    chore: format code with prettier

--- packages/cwp-template-aws/template/api/pulumi/dev/prerenderingService.ts
+++ packages/cwp-template-aws/template/api/pulumi/dev/prerenderingService.ts
@@ -84,9 +84,7 @@ class PageBuilder {
             },
             description: "Processes all jobs added to the prerendering queue.",
             code: new pulumi.asset.AssetArchive({
-                ".": new pulumi.asset.FileArchive(
-                    "../code/prerenderingService/queue/process/build"
-                )
+                ".": new pulumi.asset.FileArchive("../code/prerenderingService/queue/process/build")
             })
         });
 
