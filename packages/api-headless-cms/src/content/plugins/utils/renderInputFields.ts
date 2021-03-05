8eb3456882dcbea441f8426d4782fc463b434da1
Adrian Smijulj <adrian1358@gmail.com>
2021-03-05 13:44:49 +0100

    fix: do not throw if a plugin is missing

--- packages/api-headless-cms/src/content/plugins/utils/renderInputFields.ts
+++ packages/api-headless-cms/src/content/plugins/utils/renderInputFields.ts
@@ -1,4 +1,5 @@
 import { CmsFieldTypePlugins, CmsContentModel } from "../../../types";
+import get from "lodash/get";
 
 interface RenderInputFields {
     (params: { model: CmsContentModel; fieldTypePlugins: CmsFieldTypePlugins }): string;
@@ -7,7 +8,18 @@ interface RenderInputFields {
 export const renderInputFields: RenderInputFields = ({ model, fieldTypePlugins }) => {
     return model.fields
         .map(f => {
-            return fieldTypePlugins[f.type].manage.createInputField({ model, field: f });
+            // Every time a client updates content model's fields, we check the type of each field. If a field plugin
+            // for a particular "field.type" doesn't exist on the backend yet, we throw an error. But still, we also
+            // want to be careful when accessing the field plugin here too. It is still possible to have a content model
+            // that contains a field, for which we don't have a plugin registered on the backend. For example, user
+            // could've just removed the plugin from the backend.
+            const createInputField = get(fieldTypePlugins, `${f.type}.manage.createInputField`);
+            if (createInputField) {
+                return createInputField({ model, field: f });
+            }
+
+            return null;
         })
+        .filter(Boolean)
         .join("\n");
 };
