f5ff02be118243699511ede20eaa0c8d10de0cc8
Pavel Denisjuk <pavel@webiny.com>
2021-03-15 13:59:53 +0100

    fix: add missing path variable

--- .babel.node.js
+++ .babel.node.js
@@ -26,6 +26,6 @@ module.exports = ({ path }) => {
                     }
                 }
             ]
-        ].filter(Boolean)
+        ]
     };
 };
--- .babel.react.js
+++ .babel.react.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({ path }) => ({
     presets: [
         [
             "@babel/preset-env",
