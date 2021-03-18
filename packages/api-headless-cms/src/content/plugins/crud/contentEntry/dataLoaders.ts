fb6b6b9c750481d69e37f7b9dafeec9ade288840
Adrian Smijulj <adrian1358@gmail.com>
2021-03-18 13:38:59 +0100

    fix: ensure maximum of 100 items are requested via the `batchGet` call

--- packages/api-headless-cms/src/content/plugins/crud/contentEntry/dataLoaders.ts
+++ packages/api-headless-cms/src/content/plugins/crud/contentEntry/dataLoaders.ts
@@ -1,6 +1,7 @@
 import DataLoader from "dataloader";
 import { CmsContentEntry, CmsContext } from "../../../../types";
 import * as utils from "../../../../utils";
+import chunk from "lodash/chunk";
 
 export const getAllEntryRevisions = (context: CmsContext, { PK_ENTRY }) => {
     return new DataLoader<string, CmsContentEntry[]>(async keys => {
@@ -24,67 +25,110 @@ export const getAllEntryRevisions = (context: CmsContext, { PK_ENTRY }) => {
 
 export const getRevisionById = (context: CmsContext, { PK_ENTRY }) => {
     return new DataLoader<string, CmsContentEntry>(async keys => {
-        const queries = keys.map(id => {
-            const [entryId, version] = id.split("#");
-            return {
-                ...utils.defaults.db(),
-                query: {
-                    PK: PK_ENTRY(entryId),
-                    SK: `REV#${version}`
-                }
-            };
+        // DynamoDB allows getting a maximum of 100 items in a single database call.
+        // So, we are creating chunks that consist of a maximum of 100 keys.
+        const keysChunks = chunk(keys, 100);
+        const promises = [];
+
+        keysChunks.forEach(chunk => {
+            promises.push(
+                new Promise(async resolve => {
+                    const queries = chunk.map(id => {
+                        const [entryId, version] = id.split("#");
+                        return {
+                            ...utils.defaults.db(),
+                            query: {
+                                PK: PK_ENTRY(entryId),
+                                SK: `REV#${version}`
+                            }
+                        };
+                    });
+
+                    context.db
+                        .batch()
+                        .read(...queries)
+                        .execute()
+                        .then(results => {
+                            resolve(
+                                results.map(result => {
+                                    const items = result[0];
+                                    return items[0];
+                                })
+                            );
+                        });
+                })
+            );
         });
 
-        const results = (await context.db
-            .batch()
-            .read(...queries)
-            .execute()) as [CmsContentEntry[]][];
-
-        // We're not filtering empty values here as we must return the same amount of items as the number of "keys".
-        const items = results.map(result => {
-            const items = result[0];
-
-            return items[0];
-        });
-
-        return items;
+        const entriesChunks = await Promise.all(promises);
+        return entriesChunks.reduce((current, item) => current.concat(item), []);
     });
 };
 
 export const getPublishedRevisionByEntryId = (context: CmsContext, { PK_ENTRY, SK_PUBLISHED }) => {
     return new DataLoader<string, CmsContentEntry>(async keys => {
-        const entries = await context.db
-            .batch()
-            .read(
-                ...keys.map(id => ({
-                    ...utils.defaults.db(),
-                    query: {
-                        PK: PK_ENTRY(id),
-                        SK: SK_PUBLISHED()
-                    }
-                }))
-            )
-            .execute();
+        // DynamoDB allows getting a maximum of 100 items in a single database call.
+        // So, we are creating chunks that consist of a maximum of 100 keys.
+        const keysChunks = chunk(keys, 100);
+        const promises = [];
 
-        return entries.map(result => result[0][0]);
+        keysChunks.forEach(chunk => {
+            promises.push(
+                new Promise(resolve =>
+                    context.db
+                        .batch()
+                        .read(
+                            ...chunk.map(id => ({
+                                ...utils.defaults.db(),
+                                query: {
+                                    PK: PK_ENTRY(id),
+                                    SK: SK_PUBLISHED()
+                                }
+                            }))
+                        )
+                        .execute()
+                        .then(entries => {
+                            resolve(entries.map(result => result[0][0]));
+                        })
+                )
+            );
+        });
+
+        const entriesChunks = await Promise.all(promises);
+        return entriesChunks.reduce((current, item) => current.concat(item), []);
     });
 };
 
 export const getLatestRevisionByEntryId = (context: CmsContext, { PK_ENTRY, SK_LATEST }) => {
     return new DataLoader<string, CmsContentEntry>(async keys => {
-        const entries = await context.db
-            .batch()
-            .read(
-                ...keys.map(id => ({
-                    ...utils.defaults.db(),
-                    query: {
-                        PK: PK_ENTRY(id),
-                        SK: SK_LATEST()
-                    }
-                }))
-            )
-            .execute();
+        // DynamoDB allows getting a maximum of 100 items in a single database call.
+        // So, we are creating chunks that consist of a maximum of 100 keys.
+        const keysChunks = chunk(keys, 100);
+        const promises = [];
 
-        return entries.map(result => result[0][0]);
+        keysChunks.forEach(chunk => {
+            promises.push(
+                new Promise(resolve =>
+                    context.db
+                        .batch()
+                        .read(
+                            ...chunk.map(id => ({
+                                ...utils.defaults.db(),
+                                query: {
+                                    PK: PK_ENTRY(id),
+                                    SK: SK_LATEST()
+                                }
+                            }))
+                        )
+                        .execute()
+                        .then(entries => {
+                            resolve(entries.map(result => result[0][0]));
+                        })
+                )
+            );
+        });
+
+        const entriesChunks = await Promise.all(promises);
+        return entriesChunks.reduce((current, item) => current.concat(item), []);
     });
 };
