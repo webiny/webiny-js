62a8c80fdfcfdacb9fb362c9c42e094d8c64ebf4
Ashutosh <bhardwajashu96@hotmail.com>
2021-03-19 16:15:19 +0530

    fix: add missing permission check (#1511)
    * fix(api-file-manager-s3): add permission check in resolvers
    
    * chore(api-file-manager-s3): add @webiny/api-file-manager as dependency
    
    * fix(api-file-manager-s3): use type from api-file-manager
    
    * chore: update yarn lock

--- packages/api-file-manager-s3/src/plugins/graphqlFileStorageS3.ts
+++ packages/api-file-manager-s3/src/plugins/graphqlFileStorageS3.ts
@@ -1,7 +1,8 @@
 import { GraphQLSchemaPlugin } from "@webiny/handler-graphql/types";
 import { ErrorResponse, Response } from "@webiny/handler-graphql/responses";
+import checkBasePermissions from "@webiny/api-file-manager/plugins/crud/utils/checkBasePermissions";
+import { FileManagerContext } from "@webiny/api-file-manager/types";
 import getPresignedPostPayload from "../utils/getPresignedPostPayload";
-import { FileManagerContext } from "../types";
 
 const BATCH_UPLOAD_MAX_FILES = 20;
 
@@ -52,6 +53,8 @@ const plugin: GraphQLSchemaPlugin<FileManagerContext> = {
             FmQuery: {
                 getPreSignedPostPayload: async (root, args, context) => {
                     try {
+                        await checkBasePermissions(context, { rwd: "w" });
+
                         const { data } = args;
                         const settings = await context.fileManager.settings.getSettings();
                         const response = await getPresignedPostPayload(data, settings);
@@ -66,6 +69,8 @@ const plugin: GraphQLSchemaPlugin<FileManagerContext> = {
                     }
                 },
                 getPreSignedPostPayloads: async (root, args, context) => {
+                    await checkBasePermissions(context, { rwd: "w" });
+
                     const { data: files } = args;
                     if (!Array.isArray(files)) {
                         return new ErrorResponse({
