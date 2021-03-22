c6eb6b6f479d7c7a83206be04ae17fbdcea5fb7e
Adrian Smijulj <adrian1358@gmail.com>
2021-03-22 11:31:49 +0100

    fix: update link

--- packages/cli-plugin-deploy-pulumi/plugins/pendingOperationsInfo.js
+++ packages/cli-plugin-deploy-pulumi/plugins/pendingOperationsInfo.js
@@ -5,8 +5,8 @@ module.exports = {
     type: "cli-command-error",
     handle: ({ context, error }) => {
         const { message } = error;
-        const isError = typeof message === "string" && message.includes(MATCH_STRING);
-        if (!isError) {
+        const hasError = typeof message === "string" && message.includes(MATCH_STRING);
+        if (!hasError) {
             return;
         }
 
@@ -15,7 +15,7 @@ module.exports = {
                 `The ${blue(
                     "pending operations"
                 )} Pulumi error you've just experienced can occur if one of the previous deployments has been interrupted, or another deployment is already in progress.`,
-                "To learn more here, please visit https://docs.webiny.com/docs/how-to-guides/deployment/deploy-your-project#the-deploy-command."
+                "To learn more here, please visit https://docs.webiny.com/docs/how-to-guides/deployment/deploy-your-project#the-current-deployment-has-x-resources-with-pending-operations."
             ].join(" ")
         );
     }
