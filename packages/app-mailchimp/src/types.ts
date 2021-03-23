f2769228d1c5cb7101f8146f22fb02e61f73ad1e
Ashutosh <bhardwajashu96@hotmail.com>
2021-03-23 17:58:46 +0530

    chore: remove cookie-policy, google-tag-manager and mailchimp packages (#1525)
    * chore(api-headless-cms): use pipe from @ramda/pipe and object from commodo-fields-object
    
    remove @webiny/commodo usage
    
    * chore(api-headless-cms): add @ramda/pipe and commodo-fields-object as dependency
    
    remove @webiny/commodo
    
    * chore(api-headless-cms): update yarn lock
    
    * chore: remove cookie-policy, google-tag-manager and mailchimp packages
    
    * chore: replace pipe with flow
    
    * chore: update yarn lock
    
    * chore: update yarn lock
    
    * chore: remove @webiny/commodo from tsconfig
    
    * chore: update yarn lock
    
    * chore: update yarn lock
    
    * chore: update package.json and yarn lock

--- packages/app-mailchimp/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from "@webiny/app/types";
-import { ComponentType } from "react";
-
-export type PbPageElementMailchimpComponentPlugin = Plugin & {
-    type: "pb-page-element-mailchimp-component";
-    title: string;
-    componentName: string;
-    component: ComponentType<any>;
-};
