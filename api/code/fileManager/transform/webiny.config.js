3aea32e7afc08c08f37fffc5d13b4ac663623287
Adrian Smijulj <adrian1358@gmail.com>
2021-03-30 06:39:44 +0200

    feat: add  "watch" command

--- api/code/fileManager/transform/webiny.config.js
+++ api/code/fileManager/transform/webiny.config.js
@@ -1,7 +1,9 @@
 const { buildFunction } = require("@webiny/api-file-manager/handlers/transform/bundle");
+const { watchFunction } = require("@webiny/project-utils");
 
 module.exports = {
     commands: {
-        build: buildFunction
+        build: buildFunction,
+        watch: watchFunction
     }
 };
