aae0e9de2741e18d7e468638d908ecda180c3890
Adrian Smijulj <adrian1358@gmail.com>
2021-03-30 14:36:17 +0200

    feat: create "ws tree" command

--- packages/cli-plugin-workspaces/index.js
+++ packages/cli-plugin-workspaces/index.js
@@ -8,18 +8,18 @@ module.exports = (options = {}) => ({
             wsCommand => {
                 wsCommand.command(
                     "list",
-                    `List all project workspaces.`,
+                    `List all project workspaces`,
                     command => {
                         command.option("json", {
                             required: false,
-                            describe: `Output as JSON.`,
+                            describe: `Output as JSON`,
                             type: "boolean",
                             default: false
                         });
 
                         command.option("withPath", {
                             required: false,
-                            describe: `Includes full workspace path in the output.`,
+                            describe: `Includes full workspace path in the output`,
                             type: "boolean",
                             default: false
                         });
@@ -33,33 +33,74 @@ module.exports = (options = {}) => ({
                 );
 
                 wsCommand.command(
-                    "run <script>",
-                    "Run a script on one or several project workspaces.",
+                    "tree",
+                    `Returns dependencies tree for specified workspaces`,
                     command => {
-                        command.positional("script", {
+                        command.option("json", {
                             required: false,
-                            describe: `Name of the script to run.`,
-                            type: "string"
+                            describe: `Output as JSON`,
+                            type: "boolean",
+                            default: false
                         });
 
                         command.option("scope", {
-                            describe: `Include only workspaces that match the given names or globs.`,
+                            describe: `Include only workspaces that match the given names or globs`,
                             type: "string"
                         });
 
                         command.option("folder", {
-                            describe: `Include only workspaces found in the specified folder.`,
+                            describe: `Include only workspaces found in the specified folder`,
+                            type: "string",
+                        });
+
+                        command.option("depth", {
+                            describe: `Set tree depth`,
+                            type: "number",
+                            default: 2
+                        });
+
+                        command.option("distinct", {
+                            describe: `Return an array of distinct packages found in the tree`,
+                            type: "boolean",
+                            default: false
+                        });
+
+                        command.example("$0 workspaces tree --scope @webiny/handler-graphql");
+                    },
+                    async argv => {
+                        await require("./commands/tree")({ ...argv, options }, context);
+                        process.exit(0);
+                    }
+                );
+
+                wsCommand.command(
+                    "run <script>",
+                    "Run a script on one or several project workspaces",
+                    command => {
+                        command.positional("script", {
+                            required: false,
+                            describe: `Name of the script to run`,
+                            type: "string"
+                        });
+
+                        command.option("scope", {
+                            describe: `Include only workspaces that match the given names or globs`,
+                            type: "string"
+                        });
+
+                        command.option("folder", {
+                            describe: `Include only workspaces found in the specified folder`,
                             type: "string"
                         });
 
                         command.option("stream", {
-                            describe: `Stream output to the terminal.`,
+                            describe: `Stream output to the terminal`,
                             type: "bool",
                             default: true
                         });
 
                         command.option("parallel", {
-                            describe: `Run script in parallel, ignoring cross-package dependencies.`,
+                            describe: `Run script in parallel, ignoring cross-package dependencies`,
                             type: "bool"
                         });
 
