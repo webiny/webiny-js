05e89e15b53eca4be245c7cc25741f02ef6e1c23
Adrian Smijulj <adrian1358@gmail.com>
2021-03-31 08:11:24 +0200

    fix: calculate project app's root against Pulumi.yaml file

--- packages/cli-plugin-deploy-pulumi/utils/getProjectApplication.js
+++ packages/cli-plugin-deploy-pulumi/utils/getProjectApplication.js
@@ -2,7 +2,12 @@ const { join, dirname } = require("path");
 const findUp = require("find-up");
 
 module.exports = path => {
-    const absPath = path.startsWith("/") ? path : join(process.cwd(), path);
+    let absPath = path;
+    if (!absPath.startsWith("/")) {
+        absPath = join(process.cwd(), path);
+    }
+
+    absPath = dirname(findUp.sync("Pulumi.yaml", { cwd: absPath }));
 
     const projectPath = dirname(findUp.sync("webiny.root.js"));
     const relPath = absPath.replace(projectPath, "").substr(1);
