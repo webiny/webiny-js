bc2a299a1bece0e28d6346c4258ec0ee23cb9432
Adrian Smijulj <adrian1358@gmail.com>
2021-04-01 13:55:01 +0200

    feat: add "webiny.application.js"

---
+++ apps/admin/webiny.application.js
@@ -0,0 +1,14 @@
+/**
+ * For more information on the Admin Area project application, please see:
+ * https://www.webiny.com/docs/key-topics/cloud-infrastructure/admin/introduction
+ */
+module.exports = {
+    id: "admin",
+    name: "Admin Area",
+    description: "Your project's admin area.",
+    config: {
+        watch: {
+            deploy: false
+        }
+    }
+};
---
+++ apps/website/webiny.application.js
@@ -0,0 +1,15 @@
+/**
+ * For more information on the Website project application, please see:
+ * https://www.webiny.com/docs/key-topics/cloud-infrastructure/website/introduction
+ */
+
+module.exports = {
+    id: "website",
+    name: "Website",
+    description: "Your project's public website.",
+    config: {
+        watch: {
+            deploy: false
+        }
+    }
+};
