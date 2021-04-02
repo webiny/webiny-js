56179a822d6f05ad75f0f532ea08bdfe31705d0b
Adrian Smijulj <adrian1358@gmail.com>
2021-04-02 17:05:59 +0200

    fix(cwp-template-aws): update template (WEBINY_LOGS_FORWARD_URL)

--- packages/cwp-template-aws/template/api/pulumi/dev/index.ts
+++ packages/cwp-template-aws/template/api/pulumi/dev/index.ts
@@ -44,7 +44,8 @@ export default () => {
             PRERENDERING_FLUSH_HANDLER: prerenderingService.functions.flush.arn,
             PRERENDERING_QUEUE_ADD_HANDLER: prerenderingService.functions.queue.add.arn,
             PRERENDERING_QUEUE_PROCESS_HANDLER: prerenderingService.functions.queue.process.arn,
-            S3_BUCKET: fileManager.bucket.id
+            S3_BUCKET: fileManager.bucket.id,
+            WEBINY_LOGS_FORWARD_URL: String(process.env.WEBINY_LOGS_FORWARD_URL)
         }
     });
 
@@ -56,7 +57,8 @@ export default () => {
             DB_TABLE_ELASTICSEARCH: elasticSearch.table.name,
             DEBUG: String(process.env.DEBUG),
             ELASTIC_SEARCH_ENDPOINT: elasticSearch.domain.endpoint,
-            S3_BUCKET: fileManager.bucket.id
+            S3_BUCKET: fileManager.bucket.id,
+            WEBINY_LOGS_FORWARD_URL: String(process.env.WEBINY_LOGS_FORWARD_URL)
         }
     });
 
