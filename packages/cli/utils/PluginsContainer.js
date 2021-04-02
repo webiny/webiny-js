b3daa707adbfbc21f6874cfef08870a619154421
Adrian Smijulj <adrian1358@gmail.com>
2021-04-02 16:56:29 +0200

    fix(cli): simplify PluginsContainer

--- packages/cli/utils/PluginsContainer.js
+++ packages/cli/utils/PluginsContainer.js
@@ -1,22 +1,13 @@
 const uniqid = require("uniqid");
 
-const isOptionsObject = item => item && !Array.isArray(item) && !item.type && !item.name;
-const normalizeArgs = args => {
-    let options = {};
-
-    // Check if last item in the plugins array is actually an options object.
-    if (isOptionsObject(args[args.length - 1])) {
-        [options] = args.splice(-1, 1);
-    }
-
-    return [args, options];
-};
-
-const assign = (plugins, options, target) => {
+// Since the Webiny CLI can rely on "@webiny/plugins" package (chicken-egg problem), then
+// we need to make a copy of its PluginsContainer class. We removed all of the extra
+// features that are in reality not needed for the Webiny CLI.
+const assign = (plugins, target) => {
     for (let i = 0; i < plugins.length; i++) {
         const plugin = plugins[i];
         if (Array.isArray(plugin)) {
-            assign(plugin, options, target);
+            assign(plugin, target);
             continue;
         }
 
@@ -25,18 +16,12 @@ const assign = (plugins, options, target) => {
             plugin.name = name = uniqid(plugin.type + "-");
         }
 
-        // If skip existing was set to true, and a plugin with the same name was already registered, skip registration.
-        if (!options.skipExisting || !target[name]) {
-            target[name] = plugin;
-            plugin.init && plugin.init();
-        }
+        target[name] = plugin;
     }
 };
 
 module.exports = class PluginsContainer {
     plugins = {};
-    _byTypeCache = {};
-
     constructor(...args) {
         this.register(...args);
     }
@@ -46,50 +31,15 @@ module.exports = class PluginsContainer {
     }
 
     byType(type) {
-        if (this._byTypeCache[type]) {
-            return Array.from(this._byTypeCache[type]);
-        }
         const plugins = this.findByType(type);
-        this._byTypeCache[type] = plugins;
         return Array.from(plugins);
     }
 
-    atLeastOneByType(type) {
-        const list = this.byType(type);
-        if (list.length === 0) {
-            throw new Error(`There are no plugins by type "${type}".`);
-        }
-        return list;
-    }
-
-    oneByType(type) {
-        const list = this.atLeastOneByType(type);
-        if (list.length > 1) {
-            throw new Error(
-                `There is a requirement for plugin of type "${type}" to be only one registered.`
-            );
-        }
-        return list[0];
-    }
-
-    all() {
-        return Object.values(this.plugins);
-    }
-
     register(...args) {
-        // reset the cache when adding new plugins
-        this._byTypeCache = {};
-        const [plugins, options] = normalizeArgs(args);
-        assign(plugins, options, this.plugins);
+        assign(...args, this.plugins);
     }
 
     unregister(name) {
-        // reset the cache when removing a plugin
-        this._byTypeCache = {};
         delete this.plugins[name];
-    }
-
-    findByType(type) {
-        return Object.values(this.plugins).filter(pl => pl.type === type);
     }
 };
