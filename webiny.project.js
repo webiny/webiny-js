8cb7f18cd3ce0163e4f3db31ae6668e18be43884
Adrian Smijulj <adrian1358@gmail.com>
2021-04-02 15:07:44 +0200

    fix: catch require statements

--- webiny.project.js
+++ webiny.project.js
@@ -1,15 +1,22 @@
-module.exports = {
-    name: "webiny-js",
-    cli: {
-        plugins: [
-            require("@webiny/cli-plugin-workspaces")(),
-            require("@webiny/cli-plugin-deploy-pulumi")(),
-            require("@webiny/api-page-builder/cli")(),
-            require("@webiny/cwp-template-aws/cli")(),
-            require("@webiny/cli-plugin-scaffold").default(),
-            require("@webiny/cli-plugin-scaffold-graphql-service").default(),
-            require("@webiny/cli-plugin-scaffold-admin-app-module").default(),
-            require("@webiny/cli-plugin-scaffold-react-component").default()
-        ]
-    }
-};
+try {
+    module.exports = {
+        name: "webiny-js",
+        cli: {
+            plugins: [
+                require("@webiny/cli-plugin-workspaces")(),
+                require("@webiny/cli-plugin-deploy-pulumi")(),
+                require("@webiny/api-page-builder/cli")(),
+                require("@webiny/cwp-template-aws/cli")(),
+                require("@webiny/cli-plugin-scaffold").default(),
+                require("@webiny/cli-plugin-scaffold-graphql-service").default(),
+                require("@webiny/cli-plugin-scaffold-admin-app-module").default(),
+                require("@webiny/cli-plugin-scaffold-react-component").default()
+            ]
+        }
+    };
+} catch {
+    // This try / catch is not needed in real projects, only for "webiny-js" repository.
+    // This is because packages are built using "yarn webiny run build" command, which
+    // invoke the Webiny CLI, and which requires file. As we can see, above we have
+    // requires of allegedly built packages, but on first build, that's not true.
+}
