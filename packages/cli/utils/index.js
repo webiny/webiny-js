e9a29c0c8f440e9e6f571ae5a38daa5192d7c1ce
Adrian Smijulj <adrian1358@gmail.com>
2021-04-02 16:51:40 +0200

    fix: export PluginsContainer

--- packages/cli/utils/index.js
+++ packages/cli/utils/index.js
@@ -1,5 +1,6 @@
 const getProject = require("./getProject");
 const getProjectApplication = require("./getProjectApplication");
 const log = require("./log");
+const PluginsContainer = require("./PluginsContainer");
 
-module.exports = { getProject, getProjectApplication, log };
+module.exports = { getProject, getProjectApplication, log, PluginsContainer };
