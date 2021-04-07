b64c42889357d20d171e87b10d6600ba718040cb
Bruno Zorić <bruno.zoric@gmail.com>
2021-04-07 11:45:09 +0200

    feat(api-security-tenancy): cache permissions in anon and apiKey

--- packages/api-security-tenancy/src/authorization/apiKey.ts
+++ packages/api-security-tenancy/src/authorization/apiKey.ts
@@ -1,22 +1,32 @@
-import { SecurityAuthorizationPlugin } from "@webiny/api-security/types";
+import { SecurityAuthorizationPlugin, SecurityPermission } from "@webiny/api-security/types";
 
 type APIKeyAuthorization = {
     identityType?: string;
 };
 
-export default (config: APIKeyAuthorization = {}): SecurityAuthorizationPlugin => ({
-    type: "security-authorization",
-    name: "security-authorization-api-key",
-    async getPermissions({ security }) {
-        const identityType = config.identityType || "api-key";
+export default (config: APIKeyAuthorization = {}): SecurityAuthorizationPlugin => {
+    const permissionCache = new Map<string, SecurityPermission[] | null>();
+    return {
+        type: "security-authorization",
+        name: "security-authorization-api-key",
+        async getPermissions({ security }) {
+            const identityType = config.identityType || "api-key";
 
-        const identity = security.getIdentity();
+            const identity = security.getIdentity();
 
-        if (!identity || identity.type !== identityType) {
-            return;
+            if (!identity || identity.type !== identityType) {
+                return;
+            }
+            if (permissionCache.has(identity.id)) {
+                return permissionCache.get(identity.id);
+            }
+            // We can expect `permissions` to exist on the identity, because api-key authentication
+            // plugin sets them on the identity instance to avoid loading them from DB here.
+            const permissions = Array.isArray(identity.permissions) ? identity.permissions : [];
+
+            permissionCache.set(identity.id, permissions);
+
+            return permissions;
         }
-        // We can expect `permissions` to exist on the identity, because api-key authentication
-        // plugin sets them on the identity instance to avoid loading them from DB here.
-        return Array.isArray(identity.permissions) ? identity.permissions : [];
-    }
-});
+    };
+};
