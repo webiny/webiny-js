1c8059c3e55ae465c3c46c42aee627ff80228afd
Adrian Smijulj <adrian1358@gmail.com>
2021-04-08 11:39:50 +0200

    fix: remove upload of React app files

--- apps/website/pulumi/app.ts
+++ apps/website/pulumi/app.ts
@@ -1,8 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as fs from "fs";
-import * as mime from "mime";
-import * as path from "path";
 
 class App {
     cloudfront: aws.cloudfront.Distribution;
@@ -15,26 +11,6 @@ class App {
                 indexDocument: "index.html",
                 errorDocument: "index.html"
             }
-        });
-
-        // Sync the contents of the source directory with the S3 bucket, which will in-turn show up on the CDN.
-        const webContentsRootPath = path.join(__dirname, "..", "code", "build");
-        App.crawlDirectory(webContentsRootPath, (filePath: string) => {
-            const relativeFilePath = filePath.replace(webContentsRootPath + "/", "");
-            new aws.s3.BucketObject(
-                relativeFilePath,
-                {
-                    key: relativeFilePath,
-                    acl: "public-read",
-                    bucket: this.bucket,
-                    contentType: mime.getType(filePath) || undefined,
-                    source: new pulumi.asset.FileAsset(filePath),
-                    cacheControl: "max-age=31536000"
-                },
-                {
-                    parent: this.bucket
-                }
-            );
         });
 
         this.cloudfront = new aws.cloudfront.Distribution("app", {
@@ -81,20 +57,6 @@ class App {
                 cloudfrontDefaultCertificate: true
             }
         });
-    }
-
-    static crawlDirectory(dir: string, f: (_: string) => void) {
-        const files = fs.readdirSync(dir);
-        for (const file of files) {
-            const filePath = `${dir}/${file}`;
-            const stat = fs.statSync(filePath);
-            if (stat.isDirectory()) {
-                App.crawlDirectory(filePath, f);
-            }
-            if (stat.isFile()) {
-                f(filePath);
-            }
-        }
     }
 }
 
