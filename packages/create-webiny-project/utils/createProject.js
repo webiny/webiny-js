7819e80ea96089758320fbcb43b1a54ebc812119
Adrian Smijulj <adrian1358@gmail.com>
2021-04-12 17:06:11 +0200

    fix: revert change (use berry a version)

--- packages/create-webiny-project/utils/createProject.js
+++ packages/create-webiny-project/utils/createProject.js
@@ -93,7 +93,6 @@ module.exports = async function createProject({
                 title: "Setup yarn@^2",
                 task: async () => {
                     await execa("yarn", ["set", "version", "berry"], { cwd: projectRoot });
-                    await execa("yarn", ["set", "version", "2.4.1"], { cwd: projectRoot });
 
                     const yamlPath = path.join(projectRoot, ".yarnrc.yml");
                     const parsedYaml = yaml.load(fs.readFileSync(yamlPath, "utf-8"));
