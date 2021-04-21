a11109b0a34af5d062ad60f4ddb8d975d0aa1ee2
Pavel Denisjuk <pavel@webiny.com>
2021-04-21 11:40:03 +0200

    feat(react-rich-text-renderer): create a Rich Text renderer component (#1566)

--- packages/app-form-builder/src/admin/plugins/editor/formFieldValidators/patternPlugins/lowerCase.ts
+++ packages/app-form-builder/src/admin/plugins/editor/formFieldValidators/patternPlugins/lowerCase.ts
@@ -1,7 +1,7 @@
 import { FbFormFieldPatternValidatorPlugin } from "../../../../../types";
 
 export default {
-    type: "form-editor-field-validator-pattern",
+    type: "fb-form-field-validator-pattern",
     name: "form-editor-field-validator-pattern-lower-case",
     pattern: {
         message: "Only lower case characters are allowed.",
