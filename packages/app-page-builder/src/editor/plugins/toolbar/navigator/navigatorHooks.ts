6f333a809b9bdae8c1cb6a5dad4eda0d70fbe568
Ashutosh <bhardwajashu96@hotmail.com>
2021-04-27 15:46:24 +0530

    fix(app-page-builder): replace hover event with drop

--- packages/app-page-builder/src/editor/plugins/toolbar/navigator/navigatorHooks.ts
+++ packages/app-page-builder/src/editor/plugins/toolbar/navigator/navigatorHooks.ts
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useRecoilValue } from "recoil";
-import { useDrag, useDrop, DropTargetMonitor } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
 import { elementByIdSelector, rootElementAtom } from "~/editor/recoil/modules";
 import { MoveBlockActionArgsType } from "~/editor/recoil/actions/moveBlock/types";
 import { MoveBlockActionEvent } from "~/editor/recoil/actions";
@@ -39,11 +39,6 @@ export const useMoveBlock = elementId => {
     };
 };
 
-interface XYCoord {
-    x: number;
-    y: number;
-}
-
 interface DragItem {
     index: number;
     id: string;
@@ -75,51 +70,25 @@ export const useSortableList = ({
                 handlerId: monitor.getHandlerId()
             };
         },
-        hover(item: DragItem, monitor: DropTargetMonitor) {
+        drop(item: DragItem) {
             if (!ref.current) {
                 return;
             }
             const dragIndex = item.index;
-            const hoverIndex = index;
+            const dropIndex = index;
 
             // Don't replace items with themselves
-            if (dragIndex === hoverIndex) {
-                return;
-            }
-
-            // Determine rectangle on screen
-            const hoverBoundingRect = ref.current && ref.current.getBoundingClientRect();
-
-            // Get vertical middle
-            const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-            // Determine mouse position
-            const clientOffset = monitor.getClientOffset();
-
-            // Get pixels to the top
-            const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
-
-            // Only perform the move when the mouse has crossed half of the items height
-            // When dragging downwards, only move when the cursor is below 50%
-            // When dragging upwards, only move when the cursor is above 50%
-
-            // Dragging downwards
-            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-                return;
-            }
-
-            // Dragging upwards
-            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+            if (dragIndex === dropIndex) {
                 return;
             }
 
             // Time to actually perform the action
-            move(hoverIndex, dragIndex);
+            move(dragIndex, dropIndex);
             // Note: we're mutating the monitor item here!
             // Generally it's better to avoid mutations,
             // but it's good here for the sake of performance
             // to avoid expensive index searches.
-            item.index = hoverIndex;
+            item.index = dropIndex;
         }
     });
 
