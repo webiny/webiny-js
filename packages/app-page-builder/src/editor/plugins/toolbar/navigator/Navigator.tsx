3a3e406a2554e9bfc31af474d322353dd368162c
Ashutosh <bhardwajashu96@hotmail.com>
2021-04-27 11:04:45 +0530

    feat(app-page-builder): create NavigatorContext

--- packages/app-page-builder/src/editor/plugins/toolbar/navigator/Navigator.tsx
+++ packages/app-page-builder/src/editor/plugins/toolbar/navigator/Navigator.tsx
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { Typography } from "@webiny/ui/Typography";
 import { useEventActionHandler } from "~/editor/hooks/useEventActionHandler";
 import { NavigatorTitle } from "./StyledComponents";
 import { TreeView } from "./TreeView";
 
+export const NavigatorContext = createContext({ refresh: () => null });
+
 const Navigator = () => {
     const [elementTree, setElementTree] = useState(null);
     const { getElementTree } = useEventActionHandler();
+
+    const refreshElementTree = async () => {
+        try {
+            const elementTree = await getElementTree();
+            setElementTree(elementTree);
+        } catch (e) {
+            console.log("Failed!");
+        }
+    };
 
     // Get initial element tree.
     useEffect(() => {
@@ -14,23 +25,16 @@ const Navigator = () => {
             return;
         }
         // Load element tree.
-        (async () => {
-            try {
-                const elementTree = await getElementTree();
-                setElementTree(elementTree);
-            } catch (e) {
-                console.log("Failed!");
-            }
-        })();
+        refreshElementTree();
     });
 
     return (
-        <>
+        <NavigatorContext.Provider value={{ refresh: refreshElementTree }}>
             <NavigatorTitle>
                 <Typography use={"subtitle1"}>Navigator</Typography>
             </NavigatorTitle>
             {elementTree && <TreeView element={elementTree} level={0} />}
-        </>
+        </NavigatorContext.Provider>
     );
 };
 
