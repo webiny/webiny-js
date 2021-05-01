8695c76786ab8a287f5330e30bb83b4de9bf023a
Pavel Denisjuk <pavel@webiny.com>
2021-05-01 16:22:38 +0200

    refactor: use pirates to add TS handling

--- packages/cli/utils/importModule.js
+++ packages/cli/utils/importModule.js
@@ -1,36 +1,33 @@
 const fs = require("fs");
 const path = require("path");
-const vm = require("vm");
+const { addHook } = require("pirates");
 
 /**
  * Add support for TS
  */
-require.extensions[".ts"] = module => {
-    const ts = require("typescript");
-    const { outputText } = ts.transpileModule(fs.readFileSync(module.filename, "utf8"), {
-        compilerOptions: {
-            target: "es6",
-            allowJs: true,
-            allowSyntheticDefaultImports: true,
-            esModuleInterop: true,
-            outDir: "bin",
-            moduleResolution: "node",
-            module: "commonjs"
-        }
-    });
 
-    const context = Object.assign({}, global);
-    context.require = module.require.bind(module);
-    context.exports = module.exports;
-    context.__filename = path.basename(module.filename);
-    context.__dirname = path.dirname(module.filename);
-    context.module = module;
-    context.process = process;
-    context.global = context;
-    context.console = console;
+addHook(
+    code => {
+        const ts = require("typescript");
+        const { outputText } = ts.transpileModule(code, {
+            compilerOptions: {
+                target: "es6",
+                allowJs: true,
+                allowSyntheticDefaultImports: true,
+                esModuleInterop: true,
+                outDir: "bin",
+                moduleResolution: "node",
+                module: "commonjs"
+            }
+        });
 
-    return vm.runInNewContext(outputText, vm.createContext(context));
-};
+        return outputText;
+    },
+    {
+        exts: [".ts"],
+        matcher: () => true
+    }
+);
 
 module.exports.importModule = configPath => {
     if (!fs.existsSync(configPath)) {
