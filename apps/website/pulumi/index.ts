5214ba843113db78bfd29e5b76a4d0f1ececadd2
Pavel Denisjuk <pavel@webiny.com>
2021-05-01 16:59:13 +0200

    fix: tag AWS resources in React apps

--- apps/website/pulumi/index.ts
+++ apps/website/pulumi/index.ts
@@ -1,20 +1,29 @@
 import App from "./app";
 import Delivery from "./delivery";
+import { tagResources } from "@webiny/cli-plugin-deploy-pulumi/utils";
 
-const app = new App();
-const delivery = new Delivery({ appS3Bucket: app.bucket });
+export = async () => {
+    // Add tags to all resources that support tagging.
+    tagResources({
+        WbyProjectName: process.env.WEBINY_PROJECT_NAME as string,
+        WbyEnvironment: process.env.WEBINY_ENV as string
+    });
 
-// Cloudfront and S3 bucket used to host the single-page application (SPA). The URL of the distribution is mainly
-// utilized by the Page Builder app's prerendering engine, over which it access the SPA and saves the generated HTML.
-// The files that are generated in that process will be stored in the below deliveryStorage S3 bucket.
-export const appId = app.cloudfront.id;
-export const appStorage = app.bucket.id;
-export const appUrl = app.cloudfront.domainName.apply(value => `https://${value}`);
+    const app = new App();
+    const delivery = new Delivery({ appS3Bucket: app.bucket });
 
-// Cloudfront and S3 bucket that will deliver static pages to actual website visitors. Except the fact that this
-// S3 bucket is utilized by the Page Builder app's prerendering engine, in order to store the files that were
-// generated in the prerendering process (as mentioned above), note that these still relies on the app's S3 bucket
-// defined above, for serving static assets (JS, CSS, images).
-export const deliveryId = delivery.cloudfront.id;
-export const deliveryStorage = delivery.bucket.id;
-export const deliveryUrl = delivery.cloudfront.domainName.apply(value => `https://${value}`);
+    return {
+        // Cloudfront and S3 bucket used to host the single-page application (SPA). The URL of the distribution is mainly
+        // utilized by the Page Builder app's prerendering engine. Using this URL, it accesses the SPA and creates HTML snapshots.
+        // The files that are generated in that process are stored in the `deliveryStorage` S3 bucket further below.
+        appId: app.cloudfront.id,
+        appStorage: app.bucket.id,
+        appUrl: app.cloudfront.domainName.apply(value => `https://${value}`),
+        // These are the Cloudfront and S3 bucket that will deliver static pages to the actual website visitors. 
+        // The static HTML snapshots delivered from them still rely on the app's S3 bucket
+        // defined above, for serving static assets (JS, CSS, images).
+        deliveryId: delivery.cloudfront.id,
+        deliveryStorage: delivery.bucket.id,
+        deliveryUrl: delivery.cloudfront.domainName.apply(value => `https://${value}`)
+    };
+};
