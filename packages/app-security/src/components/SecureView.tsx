c5cec7ea7d1e1c51c9d10a7a2cde8f04b928eaf9
Adrian Smijulj <adrian1358@gmail.com>
2021-05-02 11:02:54 +0200

    fix: remove "scopes" and deprecation warning

--- packages/app-security/src/components/SecureView.tsx
+++ packages/app-security/src/components/SecureView.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { useSecurity } from "..";
 import { SecurityPermission } from "../types";
 
-let warned = false;
-
 interface ChildrenRenderFunctionArgs<T extends SecurityPermission> {
     hasPermission: boolean;
     permission: T;
@@ -11,26 +9,13 @@ interface ChildrenRenderFunctionArgs<T extends SecurityPermission> {
 
 interface Props<T extends SecurityPermission> {
     children: ((args: ChildrenRenderFunctionArgs<T>) => React.ReactElement) | React.ReactElement;
-    scopes?: string[];
     permission?: string;
 }
 
 function SecureView<T extends SecurityPermission>({
     children,
-    scopes,
     permission
 }: Props<T>): React.ReactElement {
-    if (!permission && scopes) {
-        !warned &&
-            console.warn(
-                `DEPRECATION WARNING: <SecureView> "scopes" prop is deprecated. Please upgrade to "permission" prop! [${scopes.join(
-                    ","
-                )}]`
-            );
-        warned = true;
-        permission = scopes[0];
-    }
-
     const { identity } = useSecurity();
     const matchedPermission = identity.getPermission<T>(permission);
 
