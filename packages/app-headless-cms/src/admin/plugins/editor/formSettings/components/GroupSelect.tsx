ffee762380c3c71c2b23ff109967264e340c1d6a
Pavel Denisjuk <pavel@webiny.com>
2021-05-04 19:57:25 +0200

    fix(app-headless-cms): add content model group selector

---
+++ packages/app-headless-cms/src/admin/plugins/editor/formSettings/components/GroupSelect.tsx
@@ -0,0 +1,26 @@
+import React, { useMemo } from "react";
+import { Select } from "@webiny/ui/Select";
+import { FormComponentProps } from "@webiny/ui/types";
+import { LIST_MENU_CONTENT_GROUPS_MODELS } from "~/admin/viewsGraphql";
+import { useQuery } from "~/admin/hooks";
+
+export default function GroupSelect({ value, ...props }: FormComponentProps) {
+    const { data, loading } = useQuery(LIST_MENU_CONTENT_GROUPS_MODELS);
+
+    const groups = loading && !data ? [] : data.listContentModelGroups.data;
+    const options = useMemo(() => {
+        return groups.map(item => ({ value: item.id, label: item.name }));
+    }, [groups]);
+
+    const selectValue = typeof value === "string" ? value : value.id;
+
+    return (
+        <Select
+            {...props}
+            value={loading ? "" : selectValue}
+            description={"Choose a content model group"}
+            label={"Content model group"}
+            options={options}
+        />
+    );
+}
