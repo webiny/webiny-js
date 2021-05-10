02613a0a00b6eba2d1ea1bdca4deff97e1177f3d
Bruno Zorić <bruno.zoric@gmail.com>
2021-05-10 14:33:22 +0200

    style: prettier run

--- webiny.project.ts
+++ webiny.project.ts
@@ -19,17 +19,19 @@ export default {
                     import("@webiny/cli-plugin-scaffold-react-component")
                 ]);
 
-                return modules.map(m => {
-                    // Use only "fulfilled" imports.
-                    if (m.status === "fulfilled") {
-                        try {
-                            return m.value.default();
-                        } catch {
-                            // This one is most like not built yet.
-                            return null;
+                return modules
+                    .map(m => {
+                        // Use only "fulfilled" imports.
+                        if (m.status === "fulfilled") {
+                            try {
+                                return m.value.default();
+                            } catch {
+                                // This one is most like not built yet.
+                                return null;
+                            }
                         }
-                    }
-                }).filter(Boolean);
+                    })
+                    .filter(Boolean);
             } catch (e) {
                 // If the whole promise fails, act as if there are no plugins.
                 return [];
