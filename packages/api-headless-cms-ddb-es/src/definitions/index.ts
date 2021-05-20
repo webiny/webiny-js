cc5c212483594b78fcf8557ad655aa0fcea41e25
Bruno Zorić <bruno.zoric@gmail.com>
2021-05-20 14:05:42 +0200

    fix(app-headless-cms-ddb-es): failing upgrade due to different entity formulation

--- packages/api-headless-cms-ddb-es/src/definitions/index.ts
+++ packages/api-headless-cms-ddb-es/src/definitions/index.ts
@@ -2,10 +2,12 @@ import defineTable from "./table";
 import defineElasticsearchTable from "./tableElasticsearch";
 import defineModel from "./model";
 import defineEntry from "./entry";
+import defineElasticsearchEntry from "./elasticsearchEntry";
 
 export default {
     defineTable,
     defineElasticsearchTable,
     defineModel,
-    defineEntry
+    defineEntry,
+    defineElasticsearchEntry
 };
--- packages/api-headless-cms-ddb-es/src/upgrades/v5.7.0/index.ts
+++ packages/api-headless-cms-ddb-es/src/upgrades/v5.7.0/index.ts
@@ -5,6 +5,7 @@ import { TYPE_ENTRY_LATEST } from "../../operations/entry/CmsContentEntryDynamoE
 import WebinyError from "@webiny/error";
 import lodashChunk from "lodash.chunk";
 import definitions from "../../definitions";
+import { Entity } from "dynamodb-toolbox";
 
 interface EntryRecordData {
     /**
@@ -23,6 +24,13 @@ interface EntryRecordData {
      * Locale code.
      */
     locale: string;
+}
+
+interface ElasticCmsContentEntry {
+    index: string;
+    PK: string;
+    SK: string;
+    data: CmsContentEntry;
 }
 
 const sleep = async (ms = 2000): Promise<void> => {
@@ -51,6 +59,15 @@ const extractEntryId = (id: string): string => {
 };
 
 const createPartitionKey = (data: EntryRecordData) => {
+    if (!data.id) {
+        throw new WebinyError(
+            `Missing ID in the EntryRecordData: ${JSON.stringify(data)}`,
+            "RECORD_DATA_ID_ERROR",
+            {
+                data
+            }
+        );
+    }
     const entryId = extractEntryId(data.id);
     if (!data.tenant) {
         throw new WebinyError("Missing tenant on entry record data.", "TENANT_ERROR", {
@@ -87,7 +104,7 @@ const executeQuery = async <T>(query: Promise<any>): Promise<T[]> => {
      * Otherwise a result object is returned and we loop it.
      * This works only if autoParse is not false (it is true by default).
      */
-    while ((result = previousResult.next())) {
+    while (typeof previousResult.next === "function" && (result = previousResult.next())) {
         if (!result || Array.isArray(result.Items) === false || result.Items.length === 0) {
             return items;
         }
@@ -95,6 +112,35 @@ const executeQuery = async <T>(query: Promise<any>): Promise<T[]> => {
         previousResult = result;
     }
     return items;
+};
+
+interface FetchEntriesArgs {
+    records: Record<string, EntryRecordData>;
+    entity: Entity<any>;
+}
+
+const fetchEntries = async <T>(args: FetchEntriesArgs): Promise<T[]> => {
+    const { records, entity } = args;
+    const queries = Object.values(records).map(record => {
+        const partitionKey = createPartitionKey(record);
+        return executeQuery<T>(entity.query(partitionKey));
+    });
+    try {
+        const results = await Promise.all(queries);
+
+        return results.reduce((items, result) => {
+            return items.concat(result);
+        }, []);
+    } catch (ex) {
+        throw new WebinyError(
+            `Fetching the entries from "${entity.table.name}" table the error.`,
+            "FETCH_ENTRIES_ERROR",
+            {
+                table: entity.table.name,
+                entity: entity.name
+            }
+        );
+    }
 };
 
 export default (): UpgradePlugin<CmsContext> => ({
@@ -113,7 +159,7 @@ export default (): UpgradePlugin<CmsContext> => ({
         const elasticTable = definitions.defineElasticsearchTable(context);
         const modelEntity = definitions.defineModel(table);
         const entryEntity = definitions.defineEntry(table);
-        const entryElasticsearchEntity = definitions.defineEntry(elasticTable);
+        const entryElasticsearchEntity = definitions.defineElasticsearchEntry(elasticTable);
 
         const entryRecords: Record<string, EntryRecordData> = {};
         const esIndices: { esIndex: string; localeCode: string }[] = [];
@@ -191,9 +237,23 @@ export default (): UpgradePlugin<CmsContext> => ({
                 const { hits } = response.body.hits;
 
                 for (const hit of hits) {
-                    const entryId = extractEntryId(hit.id);
+                    /**
+                     * _source should be an property on the hit, but we check it just in case something wrong happens.
+                     * There are no Elasticsearch typings for this.
+                     */
+                    const source = hit._source || {};
+                    if (!source.id) {
+                        throw new WebinyError(
+                            `Missing ID in source: ${JSON.stringify(source)}`,
+                            "SOURCE_ID_ERROR",
+                            {
+                                source
+                            }
+                        );
+                    }
+                    const entryId = extractEntryId(source.id);
                     entryRecords[entryId] = {
-                        id: hit.id,
+                        id: source.id,
                         entryId,
                         tenant: context.security.getTenant().id,
                         locale: localeCode
@@ -216,23 +276,24 @@ export default (): UpgradePlugin<CmsContext> => ({
          * We can use a scan but at that point all the records in the table would be read,
          * which is possibly expensive if there are a lot of records from the other applications (file manager, form builder, ...)
          */
-        const regularQueries: Promise<CmsContentEntry[]>[] = [];
-        for (const entryPartitionKey in entryRecords) {
-            if (!entryRecords.hasOwnProperty(entryPartitionKey)) {
-                continue;
-            }
-            const entryRecord = entryRecords[entryPartitionKey];
-            const partitionKey = createPartitionKey(entryRecord);
-            regularQueries.push(executeQuery<CmsContentEntry>(entryEntity.query(partitionKey)));
-        }
-        const regularEntries = (await Promise.all(regularQueries)).reduce((items, result) => {
-            return items.concat(result);
-        }, []);
+        const regularEntries = await fetchEntries<CmsContentEntry>({
+            records: entryRecords,
+            entity: entryEntity
+        });
         /**
          * Then we create updates to the regular DynamoDB records.
          */
         const updates = [];
         for (const entry of regularEntries) {
+            if (!entry.id) {
+                throw new WebinyError(
+                    `Missing ID in regular entry: ${JSON.stringify(entry)}`,
+                    "ENTRY_ID_ERROR",
+                    {
+                        entry
+                    }
+                );
+            }
             const entryId = extractEntryId(entry.id);
             if (!entryRecords[entryId]) {
                 continue;
@@ -246,33 +307,54 @@ export default (): UpgradePlugin<CmsContext> => ({
                 })
             );
         }
-        await table.batchWrite(updates);
+        try {
+            const chunks = lodashChunk(updates, 25);
+            for (const key in chunks) {
+                if (!chunks.hasOwnProperty(key)) {
+                    continue;
+                }
+                const chunk = chunks[key];
+                await table.batchWrite(chunk);
+            }
+        } catch (ex) {
+            throw new WebinyError(
+                "Error while writing a batch of records to regular table.",
+                "REGULAR_TABLE_WRITE_ERROR",
+                {
+                    length: updates.length,
+                    ex
+                }
+            );
+        }
 
         console.log(`[DYNAMODB] Table updated records: ${updates.length}`);
         /**
          * Time to go for the Elasticsearch data.
-         * It is same as for the DynamoDB table data.
          */
-        const elasticQueries = [];
-        for (const entryPartitionKey in entryRecords) {
-            if (!entryRecords.hasOwnProperty(entryPartitionKey)) {
-                continue;
-            }
-            const entryRecord = entryRecords[entryPartitionKey];
-            const partitionKey = createPartitionKey(entryRecord);
-            elasticQueries.push(
-                executeQuery<CmsContentEntry>(entryElasticsearchEntity.query(partitionKey))
-            );
-        }
-        const elasticEntries = (await Promise.all(elasticQueries)).reduce((items, result) => {
-            return items.concat(result);
-        }, []);
+        const elasticEntries = await fetchEntries<ElasticCmsContentEntry>({
+            records: entryRecords,
+            entity: entryElasticsearchEntity
+        });
         /**
          * Then we create the updates to the Elasticsearch records.
          */
         const elasticUpdates = [];
         for (const entry of elasticEntries) {
-            const entryId = extractEntryId(entry.id);
+            const data = entry.data;
+            if (!data) {
+                throw new WebinyError("Missing data in elastic entry.", "DATA_ERROR", {
+                    entry
+                });
+            } else if (!data.id) {
+                throw new WebinyError(
+                    `Missing ID in elastic entry data: ${JSON.stringify(entry)}`,
+                    "DATA_ID_ERROR",
+                    {
+                        entry
+                    }
+                );
+            }
+            const entryId = extractEntryId(data.id);
             if (!entryRecords[entryId]) {
                 continue;
             }
@@ -280,14 +362,18 @@ export default (): UpgradePlugin<CmsContext> => ({
             elasticUpdates.push(
                 entryElasticsearchEntity.putBatch({
                     ...entry,
-                    entryId,
-                    tenant
+                    data: {
+                        ...data,
+                        entryId,
+                        tenant
+                    }
                 })
             );
         }
 
         const breakMs = 200;
-        const recordsInABatch = 50;
+        // max dynamodb limit
+        const recordsInABatch = 25;
         /**
          * Updating the Elasticsearch table is a bit tricky because it can break if overwhelmed.
          * We will take breakMs ms break between each recordsInABatch records
@@ -297,7 +383,18 @@ export default (): UpgradePlugin<CmsContext> => ({
             `[ELASTICSEARCH] Total chunks to be written with ${recordsInABatch} records in a batch: ${elasticUpdatesChunks.length}`
         );
         for (const elasticUpdateChunk of elasticUpdatesChunks) {
-            await elasticTable.batchWrite(elasticUpdateChunk);
+            try {
+                await elasticTable.batchWrite(elasticUpdateChunk);
+            } catch (ex) {
+                throw new WebinyError(
+                    "Error writing to Elasticsearch stream table.",
+                    "STREAM_TABLE_WRITE_ERROR",
+                    {
+                        ex,
+                        message: ex.message
+                    }
+                );
+            }
             await sleep(breakMs);
         }
         console.log(`[ELASTICSEARCH] Streaming table updated records: ${elasticUpdates.length}`);
