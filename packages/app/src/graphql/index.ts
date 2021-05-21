a0e0e8a88cdb1dc03b46134cbe384ee1173b13cc
Ashutosh <bhardwajashu96@hotmail.com>
2021-05-21 11:51:20 +0530

    Improve error handling (#1622)
    * fix(handler-http): update headers in handler-error plugin
    
    * refactor(handler-graphql): remove redundant try-catch
    
    * feat(handler-http): use debug flag to return conditional error response
    
    * feat(handler-http): add HandlerHttpOptions to type
    
    * feat(handler-aws): add support for http options
    
    * feat(api): use http option in createHandler
    
    * feat(cwp-template-aws): use http option in createHandler and set DEBUG flag
    
    * chore(handler-http): add boolean as dependency
    
    * chore: update yarn lock
    
    * feat(app): add createErrorLogger
    
    * feat(app/admin): add createErrorLogger to apollo links
    
    * feat(app): add ErrorOverlay in createErrorLogger
    
    * feat(project-utils): add REACT_APP_DEBUG variable
    
    remove SKIP_PREFLIGHT_CHECK variable
    
    * chore(app): add missing dependencies
    
    * chore(app): ran prettier
    
    * refactor(app): rename createErrorLogger to createNetworkErrorLink
    
    * feat(cwp-template-aws): add createNetworkErrorLink to apollo links
    
    * refactor(apps/admin): use createNetworkErrorLink
    
    * fix(app): update overlay styles
    
    * refactor(project-utils): set REACT_APP_DEBUG true in startApp
    
    remove REACT_APP_DEBUG from applyDefaults
    
    * fix(app): update text copy

--- packages/app/src/graphql/index.ts
+++ packages/app/src/graphql/index.ts
@@ -1,3 +1,4 @@
 export { default as createOmitTypenameLink } from "./createOmitTypenameLink";
 export { default as createSetContextLink } from "./createSetContextLink";
 export { default as createConsoleLink } from "./createConsoleLink";
+export { default as createNetworkErrorLink } from "./createNetworkErrorLink";
