3acb7fac224ef8408830afbde888d972bc26bb50
Pavel Denisjuk <pavel@webiny.com>
2021-05-23 21:32:05 +0200

    fix(app): update ApolloLinkPlugin to use static type property

--- packages/app/src/plugins/ApolloLinkPlugin.ts
+++ packages/app/src/plugins/ApolloLinkPlugin.ts
@@ -2,10 +2,7 @@ import { ApolloLink } from "apollo-link";
 import { Plugin } from "@webiny/plugins";
 
 export abstract class ApolloLinkPlugin extends Plugin {
-    constructor() {
-        super();
-        this.type = "apollo-link";
-    }
+    public static readonly type = "apollo-link";
 
     abstract createLink(): ApolloLink;
 }
