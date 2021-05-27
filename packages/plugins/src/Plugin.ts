3a95846339a04ab5ca6dbda973087a8c263cd2c5
Pavel Denisjuk <pavel@webiny.com>
2021-05-27 18:12:58 +0200

    fix(plugins): make name property public

--- packages/plugins/src/Plugin.ts
+++ packages/plugins/src/Plugin.ts
@@ -1,19 +1,11 @@
 export abstract class Plugin {
     public static readonly type: string;
-    private _name: string;
+    public name: string;
 
     constructor() {
         if (!this.type) {
             throw Error(`Missing "type" definition in "${this.constructor.name}"!`);
         }
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(value) {
-        this._name = value;
     }
 
     get type() {
