03d933e5021e8b592c30e994b6ec8b79f215f96b
ashu96 <bhardwajashu96@hotmail.com>
2021-05-28 13:05:10 +0530

    test: add test cases for File Manager app

--- cypress/integration/admin/fileManager/uploadImage.spec.js
+++ cypress/integration/admin/fileManager/uploadImage.spec.js
@@ -1,13 +1,18 @@
-context("Account Module", () => {
-    beforeEach(() => cy.login());
+context("File Manager View - CRUD", () => {
+    beforeEach(() => {
+        cy.login();
+        cy.visit("/");
+        // Open drawer
+        cy.findByTestId("apps-menu").click();
+        // Open "File Manage" view
+        cy.findByTestId("admin-drawer-footer-menu-file-manager").click();
+    });
 
-    it("should be able to correctly show image editor and delete image dialogs", () => {
-        cy.visit("/account");
-        cy.findByText("Select an image").click();
-
+    it("should upload, edit and delete image", () => {
+        // Drop file
         cy.findByTestId("fm-list-wrapper").dropFile("sample.jpeg", "image/jpeg");
         cy.findByText("File upload complete.").should("exist");
-
+        // Open file details
         cy.findByTestId("fm-list-wrapper").within(() => {
             cy.findAllByTestId("fm-list-wrapper-file")
                 .first()
@@ -15,20 +20,18 @@ context("Account Module", () => {
                     cy.findByTestId("fm-file-wrapper-file-info-icon").click({ force: true });
                 });
         });
-
+        // Edit file
         cy.findByTestId("fm-edit-image-button").click();
         cy.findByTestId("fm-image-editor-dialog")
             .should("be.visible")
             .within(() => {
                 cy.findByText("Cancel").click();
             });
-
+        // Delete file
         cy.findByTestId("fm-delete-file-button").click();
-
         cy.findByTestId("fm-delete-file-confirmation-dialog").within(() => {
             cy.findByText("Confirm").click();
         });
-
         cy.findByText("File deleted successfully.");
     });
 });
