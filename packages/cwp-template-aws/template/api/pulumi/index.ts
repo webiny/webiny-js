68f9a2b07fbebb9300a3c7bdfa9e3880503ca120
Adrian Smijulj <adrian1358@gmail.com>
2021-05-31 08:42:53 +0200

    fix: add comment about the "staging" environment

--- packages/cwp-template-aws/template/api/pulumi/index.ts
+++ packages/cwp-template-aws/template/api/pulumi/index.ts
@@ -7,6 +7,9 @@ export = async () => {
         WbyEnvironment: process.env.WEBINY_ENV as string
     });
 
+    // By default, we only deploy "prod" stack when the WEBINY_ENV is equal to "prod". But, we would
+    // probably want to deploy this stack into our staging environments as well.
+    // https://www.webiny.com/docs/key-topics/ci-cd/environments/staging-prod-deployments
     if (process.env.WEBINY_ENV === "prod") {
         // Import "prod" resources config and initialize resources.
         return await import("./prod").then(module => module.default());
