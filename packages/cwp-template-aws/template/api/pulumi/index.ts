2d9e11a661952b59d0b72c3b308b184e744a74ef
Adrian Smijulj <adrian1358@gmail.com>
2021-05-31 09:34:52 +0200

    fix: add comment about the "staging" environment

--- packages/cwp-template-aws/template/api/pulumi/index.ts
+++ packages/cwp-template-aws/template/api/pulumi/index.ts
@@ -1,5 +1,11 @@
 import { tagResources } from "@webiny/cli-plugin-deploy-pulumi/utils";
 
+// By default, we only deploy "prod" stack when the WEBINY_ENV is equal to "prod". But do note that it's
+// recommended to we deploy both application code and cloud infrastructure resources into staging environments
+// as well. For cost reasons, by default, this is disabled, but feel free to add "staging" if needed.
+// https://www.webiny.com/docs/key-topics/ci-cd/environments/staging-prod-deployments
+const prodStackEnvironments = ["prod" /*"staging"*/];
+
 export = async () => {
     // Add tags to all resources that support tagging.
     tagResources({
@@ -7,10 +13,8 @@ export = async () => {
         WbyEnvironment: process.env.WEBINY_ENV as string
     });
 
-    // By default, we only deploy "prod" stack when the WEBINY_ENV is equal to "prod". But, we would
-    // probably want to deploy this stack into our staging environments as well.
-    // https://www.webiny.com/docs/key-topics/ci-cd/environments/staging-prod-deployments
-    if (process.env.WEBINY_ENV === "prod") {
+    const environment = process.env.WEBINY_ENV as string;
+    if (prodStackEnvironments.includes(environment)) {
         // Import "prod" resources config and initialize resources.
         return await import("./prod").then(module => module.default());
     }
