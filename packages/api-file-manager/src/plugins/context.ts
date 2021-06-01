a78c8983fc33c7bce614a19e985ef5fe693504d2
Pavel Denisjuk <pavel@webiny.com>
2021-06-01 21:00:05 +0200

    refactor(api-file-manager): use ContextPlugin class

--- packages/api-file-manager/src/plugins/context.ts
+++ packages/api-file-manager/src/plugins/context.ts
@@ -1,26 +1,24 @@
-import { ContextPlugin } from "@webiny/handler/types";
+import { ContextPlugin } from "@webiny/handler/plugins/ContextPlugin";
 import filesCRUD from "./crud/files.crud";
 import settingsCRUD from "./crud/settings.crud";
 import systemCRUD from "./crud/system.crud";
 import { FileStorage } from "./FileStorage";
 import { FileManagerContext } from "../types";
 
-export default {
-    type: "context",
-    apply: async context => {
-        const { i18nContent, security } = context;
-        if (!security.getTenant() || !i18nContent.getLocale()) {
-            return;
-        }
-
-        context.fileManager = {
-            ...context.fileManager,
-            files: filesCRUD(context),
-            system: systemCRUD(context),
-            settings: settingsCRUD(context),
-            storage: new FileStorage({
-                context
-            })
-        };
+export default new ContextPlugin<FileManagerContext>(context => {
+    const { i18nContent, security } = context;
+    
+    if (!security.getTenant() || !i18nContent.getLocale()) {
+        return;
     }
-} as ContextPlugin<FileManagerContext>;
+
+    context.fileManager = {
+        ...context.fileManager,
+        files: filesCRUD(context),
+        system: systemCRUD(context),
+        settings: settingsCRUD(context),
+        storage: new FileStorage({
+            context
+        })
+    };
+});
