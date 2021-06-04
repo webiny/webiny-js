01d48b8c97fce23b0156dcd299da50832f57f76d
Pavel Denisjuk <pavel@webiny.com>
2021-06-04 14:29:14 +0200

    fix(api-headless-cms-ddb-es): move beforeInstall code to a dedicated plugin

--- packages/api-headless-cms-ddb-es/src/operations/system/CmsSystemDynamoElastic.ts
+++ packages/api-headless-cms-ddb-es/src/operations/system/CmsSystemDynamoElastic.ts
@@ -1,6 +1,6 @@
+import WebinyError from "@webiny/error";
 import { CmsContext, CmsSystem, CmsSystemStorageOperations } from "@webiny/api-headless-cms/types";
 import configurations from "../../configurations";
-import WebinyError from "@webiny/error";
 
 interface ConstructorArgs {
     context: CmsContext;
@@ -86,53 +86,6 @@ export default class CmsSystemDynamoElastic implements CmsSystemStorageOperation
                     error: ex,
                     data
                 }
-            );
-        }
-    }
-
-    public async beforeInstall(): Promise<void> {
-        const { elasticSearch } = this.context;
-        try {
-            await elasticSearch.indices.putTemplate({
-                name: "headless-cms-entries-index",
-                body: {
-                    index_patterns: ["*headless-cms*"],
-                    settings: {
-                        analysis: {
-                            analyzer: {
-                                lowercase_analyzer: {
-                                    type: "custom",
-                                    filter: ["lowercase", "trim"],
-                                    tokenizer: "keyword"
-                                }
-                            }
-                        }
-                    },
-                    mappings: {
-                        properties: {
-                            property: {
-                                type: "text",
-                                fields: {
-                                    keyword: {
-                                        type: "keyword",
-                                        ignore_above: 256
-                                    }
-                                },
-                                analyzer: "lowercase_analyzer"
-                            },
-                            rawValues: {
-                                type: "object",
-                                enabled: false
-                            }
-                        }
-                    }
-                }
-            });
-        } catch (err) {
-            console.log(err);
-            throw new WebinyError(
-                "Index template creation failed!",
-                "CMS_INSTALLATION_INDEX_TEMPLATE_ERROR"
             );
         }
     }
