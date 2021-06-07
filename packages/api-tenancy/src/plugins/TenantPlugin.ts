425afca10454ec257948205253b78844b80ecbe9
Pavel Denisjuk <pavel@webiny.com>
2021-06-07 21:30:26 +0200

    fix(api-tenancy): simplify TenantPlugin types

--- packages/api-tenancy/src/plugins/TenantPlugin.ts
+++ packages/api-tenancy/src/plugins/TenantPlugin.ts
@@ -1,88 +1,76 @@
 import { Plugin } from "@webiny/plugins";
-import { TenancyContext, Tenant } from "../types";
+import { TenancyContext, Tenant as BaseTenant } from "../types";
 
 type PromiseVoid = void | Promise<void>;
 
-export interface UpdateInput {
-    // TODO: add fields from GraphQL
-    [key: string]: any;
+export interface CreateParams<Tenant, Context> {
+    context: Context;
+    tenant: Tenant;
 }
 
-export interface CreateParams<TTenant, TContext> {
-    context: TContext;
-    tenant: TTenant;
-}
-
-export interface BeforeUpdateParams<TTenant, TContext> {
-    context: TContext;
-    tenant: TTenant;
+export interface BeforeUpdateParams<Tenant, Context> {
+    context: Context;
+    tenant: Tenant;
     inputData: Record<string, any>;
-    updateData: Partial<TTenant>;
+    updateData: Partial<Tenant>;
 }
 
-export interface AfterUpdateParams<TTenant, TContext> {
-    context: TContext;
-    tenant: TTenant;
+export interface AfterUpdateParams<Tenant, Context> {
+    context: Context;
+    tenant: Tenant;
     inputData: Record<string, any>;
 }
 
-export interface DeleteParams<TTenant, TContext> {
-    context: TContext;
-    tenant: TTenant;
+export interface DeleteParams<Tenant, Context> {
+    context: Context;
+    tenant: Tenant;
 }
 
-interface ITenantPlugin<
-    TTenant extends Tenant = Tenant,
-    TContext extends TenancyContext = TenancyContext
+interface Config<
+    Tenant extends BaseTenant = BaseTenant,
+    Context extends TenancyContext = TenancyContext
 > {
-    beforeCreate(params: CreateParams<TTenant, TContext>): PromiseVoid;
-    afterCreate(params: CreateParams<TTenant, TContext>): PromiseVoid;
-    beforeUpdate(params: BeforeUpdateParams<TTenant, TContext>): PromiseVoid;
-    afterUpdate(params: AfterUpdateParams<TTenant, TContext>): PromiseVoid;
-    beforeDelete(params: DeleteParams<TTenant, TContext>): PromiseVoid;
-    afterDelete(params: DeleteParams<TTenant, TContext>): PromiseVoid;
+    beforeCreate?: (params: CreateParams<Tenant, Context>) => PromiseVoid;
+    afterCreate?: (params: CreateParams<Tenant, Context>) => PromiseVoid;
+    beforeUpdate?: (params: BeforeUpdateParams<Tenant, Context>) => PromiseVoid;
+    afterUpdate?: (params: AfterUpdateParams<Tenant, Context>) => PromiseVoid;
+    beforeDelete?: (params: DeleteParams<Tenant, Context>) => PromiseVoid;
+    afterDelete?: (params: DeleteParams<Tenant, Context>) => PromiseVoid;
 }
-
-type Config<
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    TTenant extends Tenant = Tenant,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    TContext extends TenancyContext = TenancyContext
-> = Partial<ITenantPlugin>;
 
 export class TenantPlugin<
-    TTenant extends Tenant = Tenant,
-    TContext extends TenancyContext = TenancyContext
-> extends Plugin implements ITenantPlugin {
+    Tenant extends BaseTenant = BaseTenant,
+    Context extends TenancyContext = TenancyContext
+> extends Plugin {
     public static readonly type = "tenancy.tenant";
-    private readonly _config: Partial<Config>;
+    private readonly _config: Config<Tenant, Context>;
 
-    constructor(config?: Config<TTenant, TContext>) {
+    constructor(config?: Config<Tenant, Context>) {
         super();
         this._config = config || {};
     }
 
-    beforeCreate(params: CreateParams<TTenant, TContext>): void | Promise<void> {
+    beforeCreate(params: CreateParams<Tenant, Context>): PromiseVoid {
         return this._execute("beforeCreate", params);
     }
 
-    afterCreate(params: CreateParams<TTenant, TContext>): void | Promise<void> {
+    afterCreate(params: CreateParams<Tenant, Context>): PromiseVoid {
         return this._execute("afterCreate", params);
     }
 
-    afterDelete(params: DeleteParams<Tenant, TenancyContext>): void | Promise<void> {
+    afterDelete(params: DeleteParams<Tenant, Context>): PromiseVoid {
         return this._execute("afterDelete", params);
     }
 
-    afterUpdate(params: AfterUpdateParams<Tenant, TenancyContext>): void | Promise<void> {
+    afterUpdate(params: AfterUpdateParams<Tenant, Context>): PromiseVoid {
         return this._execute("afterUpdate", params);
     }
 
-    beforeDelete(params: DeleteParams<Tenant, TenancyContext>): void | Promise<void> {
+    beforeDelete(params: DeleteParams<Tenant, Context>): PromiseVoid {
         return this._execute("beforeDelete", params);
     }
 
-    beforeUpdate(params: BeforeUpdateParams<Tenant, TenancyContext>): void | Promise<void> {
+    beforeUpdate(params: BeforeUpdateParams<Tenant, Context>): PromiseVoid {
         return this._execute("beforeUpdate", params);
     }
 
