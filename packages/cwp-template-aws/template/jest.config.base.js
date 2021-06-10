de5b421fa7b621467da6908b5209c709ad9c7d3a
Adrian Smijulj <adrian1358@gmail.com>
2021-06-10 09:46:04 +0200

    fix: update tests configuration

--- packages/cwp-template-aws/template/jest.config.base.js
+++ packages/cwp-template-aws/template/jest.config.base.js
@@ -8,7 +8,7 @@ module.exports = ({ path }, presets = []) => {
         name: name,
         displayName: name,
         modulePaths: [`${path}/src`],
-        testMatch: [`${path}/__tests__/**/*.test.[jt]s?(x)`],
+        testMatch: [`${path}/**/__tests__/**/*.test.[jt]s?(x)`],
         transform: {
             "^.+\\.(ts|tsx)$": "ts-jest"
         },
