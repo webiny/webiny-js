302b744bbb49eecb53dea459a833926461748178
Adrian Smijulj <adrian1358@gmail.com>
2021-06-16 21:20:03 +0200

    chore: format code

--- api/code/graphql/jest.config.js
+++ api/code/graphql/jest.config.js
@@ -8,16 +8,17 @@ const { getStackOutput } = require("@webiny/cli-plugin-deploy-pulumi/utils");
  */
 const stackOutput = getStackOutput({ folder: "api", env: "dev" });
 
-stackOutput && Object.assign(process.env, {
-    // We assign `region`, `dynamoDbTable`, and `apiUrl` as AWS_REGION, DB_TABLE, and API_URL
-    // environment variables. If needed, you can export additional values from the mentioned
-    // `api/pulumi/dev/index.ts` file and assign them here.
-    AWS_REGION: stackOutput.region,
-    DB_TABLE: stackOutput.dynamoDbTable,
-    API_URL: stackOutput.apiUrl,
+stackOutput &&
+    Object.assign(process.env, {
+        // We assign `region`, `dynamoDbTable`, and `apiUrl` as AWS_REGION, DB_TABLE, and API_URL
+        // environment variables. If needed, you can export additional values from the mentioned
+        // `api/pulumi/dev/index.ts` file and assign them here.
+        AWS_REGION: stackOutput.region,
+        DB_TABLE: stackOutput.dynamoDbTable,
+        API_URL: stackOutput.apiUrl,
 
-    // Can be of use while writing tests, for example to distinguish test data from non-test data.
-    TEST_RUN_ID: new Date().getTime()
-});
+        // Can be of use while writing tests, for example to distinguish test data from non-test data.
+        TEST_RUN_ID: new Date().getTime()
+    });
 
 module.exports = { ...base({ path: __dirname }) };
