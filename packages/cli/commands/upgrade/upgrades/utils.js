3eb1ac578d2e3791a134be75d070b1dd9d9bf7e5
Pavel Denisjuk <pavel@webiny.com>
2021-06-26 13:29:58 +0200

    feat(cli): add prettierFormat utility

--- packages/cli/commands/upgrade/upgrades/utils.js
+++ packages/cli/commands/upgrade/upgrades/utils.js
@@ -1,5 +1,7 @@
 const tsMorph = require("ts-morph");
+const path = require("path");
 const fs = require("fs");
+const prettier = require("prettier");
 const loadJson = require("load-json-file");
 const writeJson = require("write-json-file");
 const semverCoerce = require("semver/functions/coerce");
@@ -106,10 +108,35 @@ const createMorphProject = files => {
     return project;
 };
 
+const prettierFormat = async (files, context) => {
+    try {
+        context.info("Running prettier...");
+        for (const file of files) {
+            const filePath = path.join(process.cwd(), file);
+            const options = await prettier.resolveConfig(filePath);
+            const fileContentRaw = fs.readFileSync(filePath).toString("utf8");
+            const fileContentFormatted = prettier.format(fileContentRaw, {
+                ...options,
+                filepath: filePath
+            });
+            fs.writeFileSync(filePath, fileContentFormatted);
+        }
+
+        context.info("Finished formatting files.");
+    } catch (ex) {
+        console.log(context.error.hl("Prettier failed."));
+        context.error(ex.message);
+        if (ex.stdout) {
+            console.log(ex.stdout);
+        }
+    }
+};
+
 module.exports = {
     insertImport,
     addPackagesToDependencies,
     addPackagesToDevDependencies,
     addPackagesToPeerDependencies,
-    createMorphProject
+    createMorphProject,
+    prettierFormat
 };
