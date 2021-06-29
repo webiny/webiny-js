65b66e7ed5b3d9b60f2a6d25db562c09bd17308a
ashu96 <bhardwajashu96@hotmail.com>
2021-06-30 00:56:48 +0530

    feat(api-headless-cms-ddb-es): add CmsModelFieldToElasticsearchPlugin for long-text field

--- packages/api-headless-cms-ddb-es/src/elasticsearch/indexing/index.ts
+++ packages/api-headless-cms-ddb-es/src/elasticsearch/indexing/index.ts
@@ -2,10 +2,12 @@ import richTextIndexing from "./richTextIndexing";
 import defaultFieldIndexing from "./defaultFieldIndexing";
 import dateTimeIndexing from "./dateTimeIndexing";
 import numberIndexing from "./numberIndexing";
+import longTextIndexing from "./longTextIndexing";
 
 export default () => [
     dateTimeIndexing(),
     richTextIndexing(),
     defaultFieldIndexing(),
-    numberIndexing()
+    numberIndexing(),
+    longTextIndexing()
 ];
---
+++ packages/api-headless-cms-ddb-es/src/elasticsearch/indexing/longTextIndexing.ts
@@ -0,0 +1,50 @@
+import { CmsModelFieldToElasticsearchPlugin } from "../../types";
+
+export default (): CmsModelFieldToElasticsearchPlugin => ({
+    type: "cms-model-field-to-elastic-search",
+    name: "cms-model-field-to-elastic-search-long-text",
+    fieldType: "long-text",
+    toIndex(args) {
+        const { toIndexEntry, field, originalEntry } = args;
+        const values = toIndexEntry.values;
+        const value = values[field.fieldId];
+        // Save the original entry value to make the field searchable
+        values[field.fieldId] = originalEntry[field.fieldId];
+
+        //
+        return {
+            values,
+            rawValues: {
+                ...(toIndexEntry.rawValues || {}),
+                [field.fieldId]: value
+            }
+        };
+    },
+    fromIndex(args) {
+        const { field, entry } = args;
+        const rawValues = entry.rawValues || {};
+        const value = rawValues[field.fieldId];
+        // we want to remove rawValues so next plugin does not run some action because of it
+        delete rawValues[field.fieldId];
+        /**
+         * To handle backward compatibility;
+         * there might be some entries where the indexed entry doesn't have any "rawValues"
+         */
+        if (!value) {
+            return {
+                values: {
+                    ...(entry.values || {})
+                },
+                rawValues
+            };
+        }
+
+        return {
+            values: {
+                ...(entry.values || {}),
+                [field.fieldId]: value
+            },
+            rawValues
+        };
+    }
+});
