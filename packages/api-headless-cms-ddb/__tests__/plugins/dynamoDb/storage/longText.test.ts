d78b2f1bfd721dc470c5e5abf281325ebfc15fa1
ashu96 <bhardwajashu96@hotmail.com>
2021-07-01 18:07:43 +0530

    test: remove hard-coded compressed string

--- packages/api-headless-cms-ddb/__tests__/plugins/dynamoDb/storage/longText.test.ts
+++ packages/api-headless-cms-ddb/__tests__/plugins/dynamoDb/storage/longText.test.ts
@@ -1,4 +1,5 @@
 import longTextStoragePlugin from "../../../../src/dynamoDb/storage/longText";
+import { gzip } from "@webiny/api-headless-cms/utils";
 
 const defaultArgs = {
     field: {
@@ -14,7 +15,10 @@ const defaultArgs = {
 
 const testValue = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
-const expectedCompressedValue = `H4sIAAAAAAAAEzWQwXFDMQhEW9kCPL+K5JZrCiCI7zAjCVkCj8sPyk9uQsCy+z5sSoOOFQ3Fqk0sdVATv4GtL2EXjwkqOnSx9jukajaXlFyAaKxmBS5t5LJ21qIluiMclb5SHuKXtKDRvROo6iPowKdDurbURtP9eGZJ7YZH6EK35TMK5CWT1cnVOqJWamyX8h7SpfvSr6SOHIZQGm/pya4AecoPvG1JChfojHRyZdWOKWPKt/QiM4Pnx9NqjDwnaSeTQtYSsNb6TygDBc64Kzn6NoRBM4uYB95fLMMlNsZkYMwknHMcQwv53sgUY5oW6ZviJpVHOeqgnRt2nspKKLJk7m6zum3QBqSJY/1xjXb8AL3Fspi9AQAA`;
+const getExpectedValue = async () => {
+    const expectedCompressed = await gzip(testValue);
+    return expectedCompressed.toString("base64");
+};
 
 describe("longTextStoragePlugin", () => {
     test("toStorage should transform value for storage", async () => {
@@ -27,11 +31,12 @@ describe("longTextStoragePlugin", () => {
 
         expect(result).toEqual({
             compression: "gzip",
-            value: expectedCompressedValue
+            value: await getExpectedValue()
         });
     });
 
     test("fromStorage should transform value for output", async () => {
+        const expectedCompressedValue = await getExpectedValue();
         const value = {
             compression: "gzip",
             value: expectedCompressedValue
@@ -47,6 +52,7 @@ describe("longTextStoragePlugin", () => {
     });
 
     test("toStorage should transform value for storage multipleValues", async () => {
+        const expectedCompressedValue = await getExpectedValue();
         const plugin = longTextStoragePlugin();
 
         const result = await plugin.toStorage({
@@ -61,6 +67,7 @@ describe("longTextStoragePlugin", () => {
     });
 
     test("fromStorage should transform value for output multipleValues", async () => {
+        const expectedCompressedValue = await getExpectedValue();
         const value = {
             compression: "gzip",
             value: [expectedCompressedValue, expectedCompressedValue]
