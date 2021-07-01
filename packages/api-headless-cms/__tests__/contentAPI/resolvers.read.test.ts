64cf349e3a159f026cf1d0a3b660021e7e3987a0
ashu96 <bhardwajashu96@hotmail.com>
2021-07-01 19:46:04 +0530

    test(api-headless-cms): wait between operations to test filter by date

--- packages/api-headless-cms/__tests__/contentAPI/resolvers.read.test.ts
+++ packages/api-headless-cms/__tests__/contentAPI/resolvers.read.test.ts
@@ -48,6 +48,10 @@ const categoryManagerHelper = async manageOpts => {
         }
     });
     const fruits = fruitsResponse.data.createCategory.data;
+    /**
+     * Intentionally wait between "create" operations so that we can test filter by date
+     */
+    await sleep(15);
     const [vegetablesResponse] = await createCategory({
         data: {
             title: "Vegetables",
@@ -55,6 +59,10 @@ const categoryManagerHelper = async manageOpts => {
         }
     });
     const vegetables = vegetablesResponse.data.createCategory.data;
+    /**
+     * Intentionally wait between "create" operations so that we can test filter by date
+     */
+    await sleep(15);
     const [animalsResponse] = await createCategory({
         data: {
             title: "Animals",
@@ -1052,9 +1060,9 @@ describe("READ - Resolvers", () => {
         const { listCategories } = useCategoryReadHandler(readOpts);
 
         const from = new Date(vegetables.savedOn);
-        from.setTime(from.getTime() - 10);
+        from.setTime(from.getTime() - 5);
         const to = new Date(vegetables.savedOn);
-        to.setTime(to.getTime() + 10);
+        to.setTime(to.getTime() + 5);
         // If this `until` resolves successfully, we know entry is accessible via the "read" API
         const result = await until(
             () =>
