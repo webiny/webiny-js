7ee8cba113c6dc592a16ba285f3ef1c3aaa49745
Bruno Zorić <bruno.zoric@gmail.com>
2021-07-04 21:04:33 +0200

    fix(api-plugin-elastic-search-client): package rename

---
+++ packages/api-plugin-elastic-search-client/src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Since we removed api-plugin-elastic-search-client the 'yarn up "@webiny/*@5.10.0"' will not upgrade
+ * the existing packages due to this package is missing.
+ */
+export default () => [];
--- packages/api-plugin-elastic-search/src/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Since we removed api-plugin-elastic-search the 'yarn up "@webiny/*@5.10.0"' will not upgrade
- * the existing packages due to this package is missing.
- */
-export default () => [];
