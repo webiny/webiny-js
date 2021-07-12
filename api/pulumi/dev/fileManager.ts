3a78f451330a613ad051c0725b503238b60d94e7
Ashutosh <bhardwajashu96@hotmail.com>
2021-07-12 11:43:05 +0530

    Feat: Use fine-grained policies for AWS resources (#1741)
    * chore: add custom policy doc
    
    * chore: update custom policy doc
    
    * chore: add permission for each lambda role
    
    * feat(api): add lambda policies for prod environment
    
    * feat(api): add lambda policies for dev environment
    
    * chore: add EC2 and logs permission to customPolicyForProgrammaticAccess
    
    * chore: add resources to policy document
    
    * chore: update IAM policies
    
    * feat(api): update getApiGraphqlLambdaPolicy
    
    * feat(api): update policy resources
    
    * feat(api): update policy resources
    
    * chore: rename policy document
    
    * docs: add cwp policy cloudformation template
    
    * docs: update cwp policy cloudformation template

--- api/pulumi/dev/fileManager.ts
+++ api/pulumi/dev/fileManager.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import policies from "./policies";
 
 // @ts-ignore
 import { getLayerArn } from "@webiny/aws-layers";
@@ -9,12 +10,12 @@ class FileManager {
     manageS3LambdaPermission?: aws.lambda.Permission;
     bucketNotification?: aws.s3.BucketNotification;
     role: aws.iam.Role;
-    policy: aws.iam.RolePolicyAttachment;
     functions: {
         manage: aws.lambda.Function;
         transform: aws.lambda.Function;
         download: aws.lambda.Function;
     };
+
     constructor() {
         this.bucket = new aws.s3.Bucket("fm-bucket", {
             acl: "private",
@@ -28,8 +29,8 @@ class FileManager {
                 }
             ]
         });
-
-        this.role = new aws.iam.Role("fm-lambda-role", {
+        const roleName = "fm-lambda-role";
+        this.role = new aws.iam.Role(roleName, {
             assumeRolePolicy: {
                 Version: "2012-10-17",
                 Statement: [
@@ -44,9 +45,16 @@ class FileManager {
             }
         });
 
-        this.policy = new aws.iam.RolePolicyAttachment("fm-lambda-role-policy", {
+        const policy = policies.getFileManagerLambdaPolicy(this.bucket);
+
+        new aws.iam.RolePolicyAttachment(`${roleName}-FileManagerLambdaPolicy`, {
             role: this.role,
-            policyArn: "arn:aws:iam::aws:policy/AdministratorAccess"
+            policyArn: policy.arn.apply(arn => arn)
+        });
+
+        new aws.iam.RolePolicyAttachment(`${roleName}-AWSLambdaBasicExecutionRole`, {
+            role: this.role,
+            policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole
         });
 
         const transform = new aws.lambda.Function("fm-image-transformer", {
@@ -61,9 +69,7 @@ class FileManager {
             }),
             layers: [getLayerArn("sharp")],
             environment: {
-                variables: {
-                    S3_BUCKET: this.bucket.id
-                }
+                variables: { S3_BUCKET: this.bucket.id }
             }
         });
 
@@ -78,9 +84,7 @@ class FileManager {
                 ".": new pulumi.asset.FileArchive("../code/fileManager/manage/build")
             }),
             environment: {
-                variables: {
-                    S3_BUCKET: this.bucket.id
-                }
+                variables: { S3_BUCKET: this.bucket.id }
             }
         });
 
