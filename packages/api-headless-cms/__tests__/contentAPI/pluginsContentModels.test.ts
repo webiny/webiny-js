1d2077339f66fb759193f7881d86908e61a6c5c8
Adrian Smijulj <adrian1358@gmail.com>
2021-07-16 13:44:02 +0200

    test: ensure basic CRUD methods are working

--- packages/api-headless-cms/__tests__/contentAPI/pluginsContentModels.test.ts
+++ packages/api-headless-cms/__tests__/contentAPI/pluginsContentModels.test.ts
@@ -1,38 +1,86 @@
 import { useContentGqlHandler } from "../utils/useContentGqlHandler";
 import { ContentModelPlugin } from "@webiny/api-headless-cms/content/plugins/ContentModelPlugin";
-import { introspectionQuery } from "graphql/utilities";
 
 const contentModelPlugin = new ContentModelPlugin({
-    contentModel: {
-        name: "Product",
-        modelId: "product",
-        locale: "en-us",
-        group: {
-            id: "ecommerce",
-            name: "E-Commerce"
+    name: "Product",
+    modelId: "product",
+    locale: "en-us",
+    group: {
+        id: "ecommerce",
+        name: "E-Commerce"
+    },
+    fields: [
+        {
+            id: "name",
+            fieldId: "name",
+            type: "text",
+            label: "Product Name"
         },
-        fields: [
-            {
-                id: "firstName",
-                fieldId: "firstName",
-                type: "text",
-                label: "First Name"
-            },
-            {
-                id: "lastName",
-                fieldId: "lastName",
-                type: "text",
-                label: "Last Name"
-            }
-        ],
-        layout: [],
-        lockedFields: [],
-        titleFieldId: "asd"
-    }
+        {
+            id: "sku",
+            fieldId: "sku",
+            type: "text",
+            label: "SKU"
+        },
+        {
+            id: "price",
+            fieldId: "price",
+            type: "number",
+            label: "Price"
+        }
+    ],
+    layout: [["name"], ["sku", "price"]],
+    titleFieldId: "name"
 });
 
+const FIELDS_FRAGMENT = /* GraphQL */ `
+    fragment ProductFields on Product {
+        name
+        sku
+        price
+    }
+`;
+
+const ERROR_FRAGMENT = /* GraphQL */ `
+    fragment ErrorFields on CmsError {
+        data
+        code
+        message
+    }
+`;
+
+const CREATE_PRODUCT = /* GraphQL */ `
+    ${FIELDS_FRAGMENT}
+    ${ERROR_FRAGMENT}
+    mutation CreateProduct($data: ProductInput!) {
+        createProduct(data: $data) {
+            data {
+                ...ProductFields
+            }
+            error {
+                ...ErrorFields
+            }
+        }
+    }
+`;
+
+const LIST_PRODUCTS = /* GraphQL */ `
+    ${FIELDS_FRAGMENT}
+    ${ERROR_FRAGMENT}
+    query ListProducts {
+        listProducts {
+            data {
+                ...ProductFields
+            }
+            error {
+                ...ErrorFields
+            }
+        }
+    }
+`;
+
 describe("content model plugins", () => {
-    test("content model-specific query and mutation operations should be present in the schema", async () => {
+    test("must be able to perform basic CRUD operations with content models registered via plugin", async () => {
         const { invoke } = useContentGqlHandler(
             {
                 path: "manage/en-US"
@@ -40,7 +88,51 @@ describe("content model plugins", () => {
             [contentModelPlugin]
         );
 
-        const reza = await invoke({ body: { query: introspectionQuery } });
-        const aa = 123;
+        // Let's get a string schema JSON and do text-based assertions.
+        const products = [];
+        for (let i = 0; i < 3; i++) {
+            products.push(
+                await invoke({
+                    body: {
+                        query: CREATE_PRODUCT,
+                        variables: {
+                            data: { name: `product-${i}`, sku: `sku-${i}`, price: i * 100 }
+                        }
+                    }
+                }).then(([response]) => response)
+            );
+        }
+
+        await invoke({
+            body: {
+                query: LIST_PRODUCTS,
+                variables: {}
+            }
+        }).then(([response]) =>
+            expect(response).toEqual({
+                data: {
+                    listProducts: {
+                        data: [
+                            {
+                                name: "product-2",
+                                price: 200,
+                                sku: "sku-2"
+                            },
+                            {
+                                name: "product-1",
+                                price: 100,
+                                sku: "sku-1"
+                            },
+                            {
+                                name: "product-0",
+                                price: 0,
+                                sku: "sku-0"
+                            }
+                        ],
+                        error: null
+                    }
+                }
+            })
+        );
     });
 });
