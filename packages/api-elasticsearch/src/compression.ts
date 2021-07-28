1640d7998aeff15d5037084bc26109e3fad332a5
Bruno Zorić <bruno.zoric@gmail.com>
2021-07-28 15:46:39 +0200

    feat(api-elasticsearch): gzip compression as a plugin

--- packages/api-elasticsearch/src/compression.ts
+++ packages/api-elasticsearch/src/compression.ts
@@ -1,82 +1,51 @@
-import zlib from "zlib";
+import { ContextInterface } from "@webiny/handler/types";
+import { CompressionPlugin } from "~/plugins/definition/CompressionPlugin";
 
-export const gzip = (input: zlib.InputType, options?: zlib.ZlibOptions): Promise<Buffer> => {
-    return new Promise(function(resolve, reject) {
-        zlib.gzip(input, options, function(error, result) {
-            if (!error) {
-                resolve(result);
-            } else {
-                reject(error);
-            }
-        });
-    });
+/**
+ * Get the compression plugins, in reverse order, because we want to use the last one added - first.
+ */
+const getCompressionPlugins = (context: ContextInterface): CompressionPlugin[] => {
+    return context.plugins.byType<CompressionPlugin>(CompressionPlugin.type).reverse();
 };
-export const ungzip = (input: zlib.InputType, options?: zlib.ZlibOptions): Promise<Buffer> => {
-    return new Promise(function(resolve, reject) {
-        zlib.gunzip(input, options, function(error, result) {
-            if (!error) {
-                resolve(result);
-            } else {
-                reject(error);
-            }
-        });
-    });
-};
-
-const GZIP = "gzip";
-const TO_STORAGE_ENCODING = "base64";
-const FROM_STORAGE_ENCODING = "utf8";
-
-const convertToBuffer = value => {
-    if (typeof value === "string") {
-        return Buffer.from(value, TO_STORAGE_ENCODING);
-    }
-    return value;
-};
-
-export interface CompressedData {
-    compression: string;
-    value: string;
-}
 /**
  * Method to compress the elasticsearch data that is going to be stored into the DynamoDB table that is meant for elasticsearch.
  */
-export const compress = async (data: Record<string, any>): Promise<CompressedData> => {
-    /**
-     * If already compressed, skip this.
-     */
-    if (data.compression) {
-        return data as CompressedData;
+export const compress = async (
+    context: ContextInterface,
+    data: Record<string, any>
+): Promise<Record<string, any>> => {
+    const plugins = getCompressionPlugins(context);
+    if (plugins.length === 0) {
+        return data;
     }
-
-    const value = await gzip(JSON.stringify(data));
-
-    return {
-        compression: GZIP,
-        value: value.toString(TO_STORAGE_ENCODING)
-    };
+    for (const plugin of plugins) {
+        if (plugin.canCompress(data) === false) {
+            continue;
+        }
+        return await plugin.compress(data);
+    }
+    /**
+     * Possibly no plugins that can compress, just return the data.
+     */
+    return data;
 };
 
 export const decompress = async (
-    data: CompressedData | Record<string, any>
+    context: ContextInterface,
+    data: Record<string, any>
 ): Promise<Record<string, any>> => {
-    /**
-     * Possibly it is decompressed already or it never was compressed?
-     */
-    if (!data || !data.compression) {
+    const plugins = getCompressionPlugins(context);
+    if (plugins.length === 0) {
         return data;
-    } else if (data.compression !== GZIP) {
-        console.log(
-            `Could not decompress given data since its compression is not "${GZIP}". It is "${data.compression}".`
-        );
-        return null;
     }
-    const buf = await ungzip(convertToBuffer(data.value));
-
-    try {
-        const value = buf.toString(FROM_STORAGE_ENCODING);
-        return JSON.parse(value);
-    } catch (ex) {
-        return null;
+    for (const plugin of plugins) {
+        if (plugin.canDecompress(data) === false) {
+            continue;
+        }
+        return await plugin.decompress(data);
     }
+    /**
+     * Possibly no plugins that can decompress, just return the data.
+     */
+    return data;
 };
---
+++ packages/api-elasticsearch/src/plugins/GzipCompression.ts
@@ -0,0 +1,95 @@
+import { CompressionPlugin } from "~/plugins/definition/CompressionPlugin";
+import zlib from "zlib";
+
+export const gzip = (input: zlib.InputType, options?: zlib.ZlibOptions): Promise<Buffer> => {
+    return new Promise(function(resolve, reject) {
+        zlib.gzip(input, options, function(error, result) {
+            if (!error) {
+                resolve(result);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+export const ungzip = (input: zlib.InputType, options?: zlib.ZlibOptions): Promise<Buffer> => {
+    return new Promise(function(resolve, reject) {
+        zlib.gunzip(input, options, function(error, result) {
+            if (!error) {
+                resolve(result);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+
+const GZIP = "gzip";
+const TO_STORAGE_ENCODING = "base64";
+const FROM_STORAGE_ENCODING = "utf8";
+
+const convertToBuffer = value => {
+    if (typeof value === "string") {
+        return Buffer.from(value, TO_STORAGE_ENCODING);
+    }
+    return value;
+};
+
+export interface CompressedData {
+    compression: string;
+    value: string;
+}
+
+interface OriginalData {
+    [key: string]: any;
+}
+
+class GzipCompression extends CompressionPlugin {
+    public canCompress(data: any): boolean {
+        /**
+         * If already compressed, skip this.
+         */
+        if (data.compression) {
+            if (data.compression !== "GZIP") {
+                console.log(`Data is already compressed with "${data.compression}".`);
+            }
+            return false;
+        }
+        return true;
+    }
+    public async compress(data) {
+        const value = await gzip(JSON.stringify(data));
+
+        return {
+            compression: GZIP,
+            value: value.toString(TO_STORAGE_ENCODING)
+        };
+    }
+
+    public canDecompress(data: CompressedData | Record<string, any>): boolean {
+        if (!data || !data.compression) {
+            return false;
+        } else if (data.compression !== GZIP) {
+            console.log(
+                `Could not decompress given data since its compression is not "${GZIP}". It is "${data.compression}".`
+            );
+            return false;
+        }
+        return true;
+    }
+
+    public async decompress(data: CompressedData): Promise<OriginalData | null> {
+        const buf = await ungzip(convertToBuffer(data.value));
+
+        try {
+            const value = buf.toString(FROM_STORAGE_ENCODING);
+            return JSON.parse(value);
+        } catch (ex) {
+            return null;
+        }
+    }
+}
+
+export default () => {
+    return new GzipCompression();
+};
