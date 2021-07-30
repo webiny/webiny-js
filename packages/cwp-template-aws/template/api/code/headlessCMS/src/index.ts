a5056176e8a70e7c5e477db68b171bb3c2bf4970
Bruno Zorić <bruno.zoric@gmail.com>
2021-07-30 10:22:39 +0200

    feat(cwp-template-aws): gzip es data compression in user project

--- packages/cwp-template-aws/template/api/code/headlessCMS/src/index.ts
+++ packages/cwp-template-aws/template/api/code/headlessCMS/src/index.ts
@@ -11,6 +11,7 @@ import securityPlugins from "./security";
 import headlessCmsDynamoDbElasticStorageOperation from "@webiny/api-headless-cms-ddb-es";
 import logsPlugins from "@webiny/handler-logs";
 import securityAdminUsersDynamoDbStorageOperations from "@webiny/api-security-admin-users-so-ddb";
+import elasticsearchDataGzipCompression from "@webiny/api-elasticsearch/plugins/GzipCompression";
 
 // Imports plugins created via scaffolding utilities.
 import scaffoldsPlugins from "./plugins/scaffolds";
@@ -39,7 +40,8 @@ export const handler = createHandler({
         i18nContentPlugins(),
         headlessCmsPlugins({ debug }),
         headlessCmsDynamoDbElasticStorageOperation(),
-        scaffoldsPlugins()
+        scaffoldsPlugins(),
+        elasticsearchDataGzipCompression()
     ],
     http: { debug }
 });
