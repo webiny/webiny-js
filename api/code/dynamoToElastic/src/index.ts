495740f6affe00779ac31d122ae077db498105ac
Bruno Zorić <bruno.zoric@gmail.com>
2021-07-30 10:23:12 +0200

    fix(api): add plugins to createHandler in dynamo to elastic lambda

--- api/code/dynamoToElastic/src/index.ts
+++ api/code/dynamoToElastic/src/index.ts
@@ -4,9 +4,11 @@ import dynamoDBToElastic from "@webiny/api-dynamodb-to-elasticsearch/handler";
 import logsPlugins from "@webiny/handler-logs";
 import elasticsearchDataGzipCompression from "@webiny/api-elasticsearch/plugins/GzipCompression";
 
-export const handler = createHandler(
-    logsPlugins(),
-    elasticSearch({ endpoint: `https://${process.env.ELASTIC_SEARCH_ENDPOINT}` }),
-    elasticsearchDataGzipCompression(),
-    dynamoDBToElastic()
-);
+export const handler = createHandler({
+    plugins: [
+        logsPlugins(),
+        elasticSearch({ endpoint: `https://${process.env.ELASTIC_SEARCH_ENDPOINT}` }),
+        elasticsearchDataGzipCompression(),
+        dynamoDBToElastic()
+    ]
+});
