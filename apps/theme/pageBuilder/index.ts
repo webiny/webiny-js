cc8c19ab3c62c306ed3cc3fcfbb7c27ecda5eef4
ashu96 <bhardwajashu96@hotmail.com>
2021-08-04 13:23:51 +0530

    feat(theme): update theme

--- apps/theme/pageBuilder/index.ts
+++ apps/theme/pageBuilder/index.ts
@@ -11,7 +11,10 @@ export default [
                 secondary: "var(--webiny-theme-color-secondary)",
                 background: "var(--webiny-theme-color-background)",
                 surface: "var(--webiny-theme-color-surface)",
-                textPrimary: "var(--webiny-theme-color-text-primary)"
+                textPrimary: "var(--webiny-theme-color-text-primary)",
+                purple: "var(--webiny-theme-color-purple)",
+                yellow: "var(--webiny-theme-color-yellow)",
+                darkGray: "var(--webiny-theme-color-dark-gray)"
             },
             elements: {
                 button: {
@@ -21,23 +24,42 @@ export default [
                         { className: "secondary", label: "Secondary" },
                         { className: "outline-primary", label: "Outline Primary" },
                         { className: "outline-secondary", label: "Outline Secondary" },
+                        { className: "purple", label: "Purple" },
+                        { className: "yellow", label: "Yellow" },
+                        { className: "dark", label: "Dark" },
                         { className: "simple", label: "Simple" }
                     ]
                 },
                 heading: {
-                    types: [{ className: "webiny-pb-typography-heading", label: "Default" }]
+                    types: [
+                        { className: "webiny-pb-typography-heading", label: "Default" },
+                        { className: "webiny-pb-typography-heading--1", label: "Heading 1" },
+                        { className: "webiny-pb-typography-heading--2", label: "Heading 2" },
+                        { className: "webiny-pb-typography-heading--3", label: "Heading 3" },
+                        { className: "webiny-pb-typography-heading--4", label: "Heading 4" },
+                        { className: "webiny-pb-typography-heading--5", label: "Heading 5" },
+                        { className: "webiny-pb-typography-heading--6", label: "Heading 6" }
+                    ]
                 },
                 paragraph: {
                     types: [
                         { className: "webiny-pb-typography-body", label: "Body" },
-                        { className: "webiny-pb-typography-description", label: "Description" }
+                        { className: "webiny-pb-typography-body-xl", label: "Body xl" },
+                        { className: "webiny-pb-typography-description", label: "Description" },
+                        { className: "webiny-pb-typography-title", label: "Title" },
+                        { className: "webiny-pb-typography-subtitle", label: "Subtitle" }
                     ]
                 },
                 list: {
                     types: [
                         { className: "", label: "Default" },
                         { className: "webiny-pb-typography-list--primary", label: "Primary" },
-                        { className: "webiny-pb-typography-list--secondary", label: "Secondary" }
+                        { className: "webiny-pb-typography-list--secondary", label: "Secondary" },
+                        { className: "webiny-pb-typography-list--checkbox", label: "Checkbox" },
+                        {
+                            className: "webiny-pb-typography-list--big-numbers",
+                            label: "Big Numbers"
+                        }
                     ]
                 },
                 quote: {
