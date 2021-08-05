fc2613088d5d68d4da1b8a6ef39fac65ad87d030
Pavel Denisjuk <pavel@webiny.com>
2021-08-05 02:22:35 +0200

    fix(api-page-builder): explicitly enforce settings meta array

--- packages/api-page-builder/src/graphql/crud/pages.validation.ts
+++ packages/api-page-builder/src/graphql/crud/pages.validation.ts
@@ -2,7 +2,7 @@ import { UpdateDataModel, UpdateSettingsModel } from "./pages/models";
 import { PagePlugin } from "~/plugins/PagePlugin";
 
 export default new PagePlugin({
-    async beforeUpdate({ inputData, existingPage }) {
+    async beforeUpdate({ inputData, existingPage, updateData }) {
         const updateDataModel = new UpdateDataModel().populate(inputData);
         await updateDataModel.validate();
 
@@ -11,5 +11,13 @@ export default new PagePlugin({
             .populate(inputData.settings);
 
         await updateSettingsModel.validate();
+
+        if (!Array.isArray(updateData.settings.seo.meta)) {
+            updateData.settings.seo.meta = [];
+        }
+
+        if (!Array.isArray(updateData.settings.social.meta)) {
+            updateData.settings.social.meta = [];
+        }
     }
 });
