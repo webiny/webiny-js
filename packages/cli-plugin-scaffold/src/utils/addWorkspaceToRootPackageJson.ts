2d3ff1de68886fc220f570c90531d23b4b0bc780
Adrian Smijulj <adrian1358@gmail.com>
2021-08-05 16:59:35 +0200

    wip: create GQL API scaffold

---
+++ packages/cli-plugin-scaffold/src/utils/addWorkspaceToRootPackageJson.ts
@@ -0,0 +1,12 @@
+import readJson from "load-json-file";
+import writeJson from "write-json-file";
+
+import { PackageJson} from "@webiny/cli-plugin-scaffold/types";
+
+export default async (packageJsonPath, pathToAdd) => {
+    const rootPackageJson = await readJson<PackageJson>(packageJsonPath);
+    if (!rootPackageJson.workspaces.packages.includes(pathToAdd)) {
+        rootPackageJson.workspaces.packages.push(pathToAdd);
+        await writeJson(packageJsonPath, rootPackageJson);
+    }
+};
