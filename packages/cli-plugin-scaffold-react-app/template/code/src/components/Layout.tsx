7ab26274c0d6b5deb7e4e65d55bfd240575de1d0
Adrian Smijulj <adrian1358@gmail.com>
2021-08-11 14:39:40 +0200

    fix: put more code into parent Layout component

--- packages/cli-plugin-scaffold-react-app/template/code/src/components/Layout.tsx
+++ packages/cli-plugin-scaffold-react-app/template/code/src/components/Layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import transparentBackground from "../images/inner-transparent-bg.svg";
+import { Link } from "@webiny/react-router";
 
 interface Props {
     children: React.ReactNode;
@@ -12,7 +14,36 @@ interface Props {
 export default function Layout(props: Props) {
     return (
         <div className={"layout"}>
-            <div className={props.className}>{props.children}</div>
+            <div className={"menu"}>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/target-data-models"}>Target Data Models</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a
+                            target="_blank"
+                            rel={"noreferrer"}
+                            href={"https://github.com/webiny/webiny-js"}
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel={"noreferrer"} href={"https://www.webiny.com/slack"}>
+                            Community Slack
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className={props.className}>
+                <img alt={"Webiny"} className={"transparent-bg"} src={transparentBackground} />
+                <div className={"inner"}>{props.children}</div>
+            </div>
         </div>
     );
 }
