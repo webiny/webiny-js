9669664a296794a7bb4abe93eb075761711c7d28
Adrian Smijulj <adrian1358@gmail.com>
2021-08-11 17:40:38 +0200

    chore: delete test package

--- my-full-stack-application/api/code/graphql/src/plugins/scaffolds/books/resolvers/index.ts
+++
@@ -1,5 +0,0 @@
-import BooksMutation from "./BooksMutation";
-import BooksQuery from "./BooksQuery";
-
-// Check the exported classes for more information.
-export default { BooksMutation, BooksQuery };
--- my-full-stack-application/api/code/graphql/src/plugins/scaffolds/index.ts
+++
@@ -1,4 +0,0 @@
-// This file is automatically updated via various scaffolding utilities.
-import books from "./books";
-
-export default () => [books];
