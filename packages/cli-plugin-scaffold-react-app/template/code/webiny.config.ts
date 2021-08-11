6fe74f7a3ca1ba70d396184eb0cd74f3a9cd2047
Adrian Smijulj <adrian1358@gmail.com>
2021-08-11 14:35:40 +0200

    fix: clean up config file

--- packages/cli-plugin-scaffold-react-app/template/code/webiny.config.ts
+++ packages/cli-plugin-scaffold-react-app/template/code/webiny.config.ts
@@ -1,34 +1,17 @@
-import invariant from "invariant";
 import { startApp, buildApp } from "@webiny/project-utils";
-import { getStackOutput } from "@webiny/cli-plugin-deploy-pulumi/utils";
 
-const MAP = {
-    REACT_APP_GRAPHQL_API_URL: "${apiUrl}/graphql",
-    REACT_APP_API_URL: "${apiUrl}"
-};
-
-const NO_ENV_MESSAGE = `Please specify the environment via the "--env" argument, for example: "--env dev".`;
-
+// Exports basic start (watch) and build commands.
+// Need to inject environment variables, for example an HTTP API URL or maybe a token?
+// See https://github.com/webiny/webiny-js
 export default {
     commands: {
         async start(options, context) {
-            invariant(options.env, NO_ENV_MESSAGE);
-
-            const output = await getStackOutput("api", options.env, MAP);
-
-            Object.assign(process.env, output);
-
-            // Start local development
+            // Starts local development.
             await startApp(options, context);
         },
         async build(options, context) {
-            invariant(options.env, NO_ENV_MESSAGE);
-
-            const output = await getStackOutput("api", options.env, MAP);
-
-            Object.assign(process.env, output);
-
-            // Bundle app for deployment
+            // Creates a production build of your application, ready to be deployed to
+            // a hosting provider of your choice, for example Amazon S3.
             await buildApp(options, context);
         }
     }
