bade8aaf93e21fd458c6f18707b4142a8c69a47d
Adrian Smijulj <adrian1358@gmail.com>
2021-08-12 17:54:34 +0200

    fix: add new scaffolds

--- packages/cwp-template-aws/template/webiny.project.ts
+++ packages/cwp-template-aws/template/webiny.project.ts
@@ -2,9 +2,14 @@ import cliWorkspaces from "@webiny/cli-plugin-workspaces";
 import cliPulumiDeploy from "@webiny/cli-plugin-deploy-pulumi";
 import cliPageBuilder from "@webiny/api-page-builder/cli";
 import cliAwsTemplate from "@webiny/cwp-template-aws/cli";
+
+// Scaffolds.
 import cliScaffold from "@webiny/cli-plugin-scaffold";
-import cliScaffoldGraphql from "@webiny/cli-plugin-scaffold-graphql-service";
+import cliScaffoldFullStackApp from "@webiny/cli-plugin-scaffold-full-stack-app";
+import cliScaffoldGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-api";
+import cliScaffoldExtendGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-service";
 import cliScaffoldAdminModule from "@webiny/cli-plugin-scaffold-admin-app-module";
+import cliScaffoldReactApp from "@webiny/cli-plugin-scaffold-react-app";
 import cliScaffoldReactComponent from "@webiny/cli-plugin-scaffold-react-component";
 import cliScaffoldCiCd from "@webiny/cli-plugin-scaffold-ci";
 
@@ -17,9 +22,14 @@ export default {
             cliPulumiDeploy(),
             cliPageBuilder(),
             cliAwsTemplate(),
+
+            // Scaffolds.
             cliScaffold(),
+            cliScaffoldFullStackApp(),
+            cliScaffoldGraphQlApi(),
+            cliScaffoldExtendGraphQlApi(),
             cliScaffoldAdminModule(),
-            cliScaffoldGraphql(),
+            cliScaffoldReactApp(),
             cliScaffoldReactComponent(),
             cliScaffoldCiCd()
         ]
