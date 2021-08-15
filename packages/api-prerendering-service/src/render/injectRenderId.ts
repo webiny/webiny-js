5aef42f65ca62161a33b7acfd0dfb94dd266bba3
Adrian Smijulj <adrian1358@gmail.com>
2021-08-15 10:34:57 +0200

    fix: ensure data is injected at the top of the HTML document

--- packages/api-prerendering-service/src/render/injectRenderId.ts
+++ packages/api-prerendering-service/src/render/injectRenderId.ts
@@ -2,11 +2,8 @@ export default ({ id }) =>
     async tree => {
         console.log("Injecting render hash (__PS_RENDER_ID__) into HTML.");
 
-        const apolloScript = `<script>window.__PS_RENDER_ID__ = "${id}";</script>`;
-
-        tree.match({ tag: "body" }, node => {
-            const index = node.content.findIndex(n => n.tag === "div" && n.attrs.id === "root");
-            node.content.splice(index + 1, 0, apolloScript);
+        tree.match({ tag: "head" }, node => {
+            node.content.push(`<script>window.__PS_RENDER_ID__ = "${id}";</script>`);
             return node;
         });
     };
