760c83a0bfccde42e8b0a746b88956ae8cc65b25
Adrian Smijulj <adrian1358@gmail.com>
2021-08-16 07:30:39 +0200

    fix: improve comment

--- api/code/graphql/src/types.ts
+++ api/code/graphql/src/types.ts
@@ -12,9 +12,12 @@ import { PrerenderingServiceClientContext } from "@webiny/api-prerendering-servi
 import { FileManagerContext } from "@webiny/api-file-manager/types";
 import { FormBuilderContext } from "@webiny/api-form-builder/types";
 
-// The context object that is passed to your GraphQL resolver functions.
-// Feel free to extend it with additional context interfaces, if needed.
-// Note: do not rename it, as existing scaffolding utilities may rely on it.
+// When working with the `context` object (for example while defining a new GraphQL resolver function),
+// you can import this interface and assign it to it. This will give you full autocomplete functionality
+// and type safety. The easiest way to import it would be via the following import statement:
+// import { Context } from "~/types";
+// Feel free to extend it with additional context interfaces, if needed. Also, please do not change the
+// name of the interface, as existing scaffolding utilities may rely on it during the scaffolding process.
 export interface Context
     extends HandlerContext,
         HttpContext,
