bf829052497363c15f28058c2ef4a1bb15ed8245
Adrian Smijulj <adrian1358@gmail.com>
2021-08-17 09:44:02 +0200

    fix: update links

--- packages/cli-plugin-scaffold-react-app/template/code/src/plugins/routes/home.tsx
+++ packages/cli-plugin-scaffold-react-app/template/code/src/plugins/routes/home.tsx
@@ -17,11 +17,17 @@ function Home() {
             <h2>Welcome to your new React application!</h2>
             <div>Here are some useful links for you &#8595;</div>
             <UsefulLinks>
-                <UsefulLink url={"/docs/#"} title={"Create Custom Application Tutorial"}>
+                <UsefulLink
+                    url={"/docs/tutorials/create-custom-application/introduction"}
+                    title={"Create Custom Application Tutorial"}
+                >
                     Learn how to create a completely custom application that&apos;s interacting with
                     a standalone GraphQL API.
                 </UsefulLink>
-                <UsefulLink url={"/docs/#"} title={"The New React Application Scaffold"}>
+                <UsefulLink
+                    url={"/docs/how-to-guides/webiny-cli/scaffolding/new-react-application"}
+                    title={"The New React Application Scaffold"}
+                >
                     Learn more about the New React Application scaffold that was used to create this
                     React application.
                 </UsefulLink>
@@ -39,14 +45,14 @@ function Home() {
                     Webiny CLI.
                 </UsefulLink>
                 <UsefulLink
-                    url={"/how-to-guides/webiny-cli/use-watch-command"}
+                    url={"/docs/how-to-guides/webiny-cli/use-watch-command"}
                     title={"Use the Watch Command"}
                 >
                     Learn how to continuously rebuild and redeploy your code using the{" "}
                     <code>webiny watch</code> command.
                 </UsefulLink>
                 <UsefulLink
-                    url={"/how-to-guides/deployment/connect-custom-domain"}
+                    url={"/docs/how-to-guides/deployment/connect-custom-domain"}
                     title={"Connect Custom Domain"}
                 >
                     Learn how to link an existing domain with your new React application.
