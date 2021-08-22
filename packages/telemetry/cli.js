d0babffeb1cb3a7a796caa8a25f3f9a2ba9359d7
Adrian Smijulj <adrian1358@gmail.com>
2021-08-22 14:10:39 +0200

    feat: create `sendEvent` function for CLI environment

---
+++ packages/telemetry/cli.js
@@ -0,0 +1,19 @@
+const { sendEvent: baseSendEvent } = require("./index");
+const { globalConfig } = require("./utils");
+
+module.exports.sendEvent = ({ event, user, version, properties, extraPayload }) => {
+    const config = globalConfig.get();
+
+    // Check both `telemetry` and `tracking` for backwards compatibility
+    if (config.telemetry === false || config.tracking === false) {
+        return;
+    }
+
+    return baseSendEvent({
+        event,
+        user: user || config.id,
+        version: version || require("./package.json").version,
+        properties,
+        extraPayload
+    });
+};
