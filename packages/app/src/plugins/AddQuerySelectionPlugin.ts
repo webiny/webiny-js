7d30bd317300a03906d3bf34d15357e2615aadde
Pavel Denisjuk <pavel@webiny.com>
2021-08-26 15:39:27 +0200

    feat: custom Apollo InMemoryCache (#1884)

--- packages/app/src/plugins/AddQuerySelectionPlugin.ts
+++ packages/app/src/plugins/AddQuerySelectionPlugin.ts
@@ -22,26 +22,37 @@ export class AddQuerySelectionPlugin extends ApolloLinkPlugin {
                 return forward(operation);
             }
 
-            const { addSelection, selectionPath } = this.config;
-            // @ts-ignore
-            let tree = operation.query.definitions[0].selectionSet.selections;
-            const fields = selectionPath.split(".");
-
-            fieldLoop: for (const field of fields) {
-                for (const selection of tree) {
-                    if (selection.name.value === field) {
-                        tree = selection.selectionSet.selections;
-                        continue fieldLoop;
-                    }
-                }
-                // If we get here, it means we didn't find the necessary selection
-                return;
-            }
-
-            // @ts-ignore
-            tree.push(...addSelection.definitions[0].selectionSet.selections);
+            this.addSelectionToQuery(operation.operationName, operation.query);
 
             return forward(operation);
         });
+    }
+
+    addSelectionToQuery(operationName: string, query: DocumentNode) {
+        if (operationName !== this.config.operationName) {
+            return;
+        }
+
+        const { addSelection, selectionPath } = this.config;
+
+        // TODO: check if the selection is already in the query and don't add it again if not necessary.
+
+        // @ts-ignore
+        let tree = query.definitions[0].selectionSet.selections;
+        const fields = selectionPath.split(".");
+
+        fieldLoop: for (const field of fields) {
+            for (const selection of tree) {
+                if (selection.name.value === field) {
+                    tree = selection.selectionSet.selections;
+                    continue fieldLoop;
+                }
+            }
+            // If we get here, it means we didn't find the necessary selection
+            return;
+        }
+
+        // @ts-ignore
+        tree.push(...addSelection.definitions[0].selectionSet.selections);
     }
 }
