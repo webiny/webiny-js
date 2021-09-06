001475a7c406e2418c513a3ea604169af4c5a11c
Bruno Zorić <bruno.zoric@gmail.com>
2021-09-06 15:24:31 +0200

    feat(api): add prerendering and ddb+es plugins

--- api/code/graphql/src/index.ts
+++ api/code/graphql/src/index.ts
@@ -7,6 +7,7 @@ import i18nContentPlugins from "@webiny/api-i18n-content/plugins";
 import adminUsersPlugins from "@webiny/api-security-admin-users";
 import securityAdminUsersDynamoDbStorageOperations from "@webiny/api-security-admin-users-so-ddb";
 import pageBuilderPlugins from "@webiny/api-page-builder/graphql";
+import pageBuilderDynamoDbElasticsearchPlugins from "@webiny/api-page-builder-so-ddb-es";
 import pageBuilderPrerenderingPlugins from "@webiny/api-page-builder/prerendering";
 import prerenderingServicePlugins from "@webiny/api-prerendering-service/client";
 import dbPlugins from "@webiny/handler-db";
@@ -64,6 +65,7 @@ export const handler = createHandler({
         adminUsersPlugins(),
         securityAdminUsersDynamoDbStorageOperations(),
         pageBuilderPlugins(),
+        pageBuilderDynamoDbElasticsearchPlugins(),
         pageBuilderPrerenderingPlugins(),
         formBuilderPlugins(),
         headlessCmsPlugins(),
