9d1498617fa11cb0e1e86eba74f7790fcf248a04
Bruno Zorić <bruno.zoric@gmail.com>
2021-09-08 17:35:37 +0200

    refactor(api-page-builder-so-ddb-es): jest es configs

--- packages/api-page-builder-so-ddb-es/jest-dynalite-config.js
+++ packages/api-page-builder-so-ddb-es/jest-dynalite-config.js
@@ -1,19 +1,7 @@
 module.exports = {
     tables: [
         {
-            TableName: `PageBuilder`,
-            KeySchema: [
-                { AttributeName: "PK", KeyType: "HASH" },
-                { AttributeName: "SK", KeyType: "RANGE" }
-            ],
-            AttributeDefinitions: [
-                { AttributeName: "PK", AttributeType: "S" },
-                { AttributeName: "SK", AttributeType: "S" }
-            ],
-            ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
-        },
-        {
-            TableName: `PageBuilderLogs`,
+            TableName: "PageBuilder",
             KeySchema: [
                 { AttributeName: "PK", KeyType: "HASH" },
                 { AttributeName: "SK", KeyType: "RANGE" }
--- packages/api-page-builder-so-ddb-es/jest.config.js
+++ packages/api-page-builder-so-ddb-es/jest.config.js
@@ -1,4 +1,7 @@
 const dynamoDbPreset = require("jest-dynalite/jest-preset");
 const base = require("../../jest.config.base");
+const os = require("os");
+const esPreset = os.platform() === "win32" ? {} : require("@shelf/jest-elasticsearch/jest-preset");
+const localElastic = !!process.env.LOCAL_ELASTICSEARCH;
 
-module.exports = base({ path: __dirname }, [dynamoDbPreset]);
+module.exports = base({ path: __dirname }, [dynamoDbPreset, localElastic ? {} : esPreset]);
