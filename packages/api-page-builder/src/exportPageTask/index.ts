651d64f6a683c9ade98506a4221c0ab1353b0296
ashu96 <bhardwajashu96@hotmail.com>
2021-09-08 12:27:29 +0530

    feat(api-page-builder): update export page handler

--- packages/api-page-builder/src/exportPageTask/index.ts
+++ packages/api-page-builder/src/exportPageTask/index.ts
@@ -4,13 +4,13 @@ import { ArgsContext } from "@webiny/handler-args/types";
 import { DbContext } from "@webiny/handler-db/types";
 import kebabCase from "lodash/kebabCase";
 import { extractFilesFromPageData } from "./utils";
-import ZipHandler from "./zipHandler";
 import { s3StreamHandler } from "./s3StreamHandler";
 import defaults from "~/graphql/crud/utils/defaults";
-import { ExportTaskStatus } from "~/types";
+import { ExportTaskStatus, Page } from "~/types";
+import zip2Handler from "./zip2Handler";
 
 export type HandlerArgs = {
-    page: any;
+    pages: Page[];
     taskId: string;
     PK: string;
 };
@@ -30,46 +30,52 @@ export default (): HandlerPlugin<DbContext, ArgsContext<HandlerArgs>> => ({
     async handle(context): Promise<HandlerResponse> {
         try {
             const { invocationArgs: args, db } = context;
-            const { page, taskId, PK } = args;
+            const { pages, taskId, PK } = args;
 
-            // Extract all files
-            const files = extractFilesFromPageData(page.content);
-            // Filter files
-            const filesAvailableForDownload = [];
-            for (let i = 0; i < files.length; i++) {
-                const file = files[i];
-                // Check file accessibility
-                if (await s3StreamHandler.isFileAccessible(file.key)) {
-                    filesAvailableForDownload.push(file);
+            const exportInfo = [];
+            for (let i = 0; i < pages.length; i++) {
+                const page = pages[i];
+
+                // Extract all files
+                const files = extractFilesFromPageData(page.content);
+                // Filter files
+                const filesAvailableForDownload = [];
+                for (let i = 0; i < files.length; i++) {
+                    const file = files[i];
+                    // Check file accessibility
+                    if (await s3StreamHandler.isFileAccessible(file.key)) {
+                        filesAvailableForDownload.push(file);
+                    }
                 }
+
+                // Extract the page data in a json file and upload it to S3
+                const pageData = {
+                    page: {
+                        content: page.content
+                    },
+                    files: filesAvailableForDownload
+                };
+                const fileBuffer = Buffer.from(JSON.stringify(pageData));
+                const pageUploadDataKey = uniqueId("", `-${kebabCase(page.title)}.json`);
+                /**
+                 * We're not using FileManager storage to upload the file,
+                 * because we'll not access it via the FileManager in either API or APP.
+                 */
+                const pageDataUpload = await s3StreamHandler.upload({
+                    Key: pageUploadDataKey,
+                    ContentType: "application/json",
+                    Body: fileBuffer
+                });
+
+                exportInfo.push({
+                    pageDataUploadKey: pageDataUpload.Key,
+                    files: filesAvailableForDownload,
+                    pageTitle: page.title
+                });
             }
 
-            // Extract the page data in a json file and upload it to S3
-            const file = {
-                page: {
-                    content: page.content
-                },
-                files: filesAvailableForDownload
-            };
-            const fileBuffer = Buffer.from(JSON.stringify(file));
-            const pageUploadDataKey = uniqueId("", `-${kebabCase(page.title)}.json`);
-            /**
-             * We're not using FileManager storage to upload the file,
-             * because we'll not access it via the FileManager in either API or APP.
-             */
-            const pageDataUpload = await s3StreamHandler.upload({
-                Key: pageUploadDataKey,
-                ContentType: "application/json",
-                Body: fileBuffer
-            });
-            const archiveFileName = uniqueId("", `-${kebabCase(page.title)}-export.zip`);
-            // TODO: Improve signature
-            // Prepare zip and upload it to S3
-            const zipHandler = new ZipHandler({
-                files: filesAvailableForDownload,
-                archiveFileName,
-                s3FileKey: pageDataUpload.Key,
-                filesDirName: "assets"
+            const zipHandler = new zip2Handler({
+                exportInfo
             });
 
             const { Location, Key } = await zipHandler.process();
