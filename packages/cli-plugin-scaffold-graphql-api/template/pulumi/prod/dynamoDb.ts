5be4039ca7328040c6dc5e1f267813b385ff2840
Adrian Smijulj <adrian1358@gmail.com>
2021-09-12 08:49:38 +0200

    fix: add `protect: true`

--- packages/cli-plugin-scaffold-graphql-api/template/pulumi/prod/dynamoDb.ts
+++ packages/cli-plugin-scaffold-graphql-api/template/pulumi/prod/dynamoDb.ts
@@ -3,16 +3,20 @@ import * as aws from "@pulumi/aws";
 class DynamoDB {
     table: aws.dynamodb.Table;
     constructor() {
-        this.table = new aws.dynamodb.Table("project-application-name", {
-            attributes: [
-                { name: "PK", type: "S" },
-                { name: "SK", type: "S" }
-            ],
-            billingMode: "PAY_PER_REQUEST",
-            hashKey: "PK",
-            rangeKey: "SK",
-            globalSecondaryIndexes: []
-        });
+        this.table = new aws.dynamodb.Table(
+            "project-application-name",
+            {
+                attributes: [
+                    { name: "PK", type: "S" },
+                    { name: "SK", type: "S" }
+                ],
+                billingMode: "PAY_PER_REQUEST",
+                hashKey: "PK",
+                rangeKey: "SK",
+                globalSecondaryIndexes: []
+            },
+            { protect: true }
+        );
     }
 }
 
