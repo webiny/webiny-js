21226b03f3fcc69a4f7f5b9cbe4dcc9cbaa7c761
Adrian Smijulj <adrian1358@gmail.com>
2021-09-27 18:15:52 +0200

    chore: clean up redundant files

--- apps/website/code/src/App.tsx
+++ apps/website/code/src/App.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-components";
 import { BrowserRouter, Switch, Route } from "@webiny/react-router";
 import { PageBuilderProvider } from "@webiny/app-page-builder/contexts/PageBuilder";
-import { PageElementsProvider } from "page-builder-elements";
 import { createApolloClient } from "./components/apolloClient";
 import Page from "./components/Page";
 
 export const App = () => (
     <ApolloProvider client={createApolloClient()}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <PageElementsProvider>
-                <PageBuilderProvider>
-                    <Switch>
-                        <Route path={"*"} component={Page} />
-                    </Switch>
-                </PageBuilderProvider>
-            </PageElementsProvider>
+            <PageBuilderProvider>
+                <Switch>
+                    <Route path={"*"} component={Page} />
+                </Switch>
+            </PageBuilderProvider>
         </BrowserRouter>
     </ApolloProvider>
 );
