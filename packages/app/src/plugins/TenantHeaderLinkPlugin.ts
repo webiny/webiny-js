ec0b49e25ecd3e8de95e70bc273967417052d61c
Adrian Smijulj <adrian1358@gmail.com>
2021-09-28 16:11:27 +0200

    fix: check if `headers` exists before using the `in` operator

--- packages/app/src/plugins/TenantHeaderLinkPlugin.ts
+++ packages/app/src/plugins/TenantHeaderLinkPlugin.ts
@@ -28,7 +28,7 @@ export class TenantHeaderLinkPlugin extends ApolloLinkPlugin {
     createLink() {
         return setContext((_, { headers }) => {
             // If tenant header is already set, do not overwrite it.
-            if ("x-tenant" in headers) {
+            if (headers && "x-tenant" in headers) {
                 return { headers };
             }
 
