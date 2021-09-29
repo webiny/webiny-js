3a2facb072af70b1f4d647ab23219a4c42e1bf74
Bruno Zorić <bruno.zoric@gmail.com>
2021-09-29 10:41:25 +0200

    feat(pubsub): add pub/sub package (#1936)

---
+++ packages/pubsub/src/types.ts
@@ -0,0 +1,15 @@
+export interface Subscriber<TEvent> {
+    (event: TEvent): void | Promise<void>;
+}
+
+export type Event = Record<string, any>;
+
+export interface Topic<TEvent = Event> {
+    getTopicName(): string;
+
+    subscribe(cb: (event: TEvent) => void | Promise<void>): void;
+
+    getSubscribers(): Subscriber<TEvent>[];
+
+    publish(event?: TEvent): Promise<void>;
+}
