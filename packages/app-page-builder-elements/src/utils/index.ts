672ba33940f0429802a71ac675f118d08868edca
Adrian Smijulj <adrian1358@gmail.com>
2021-10-11 13:39:38 +0200

    refactor: enable passing custom styles/classNames callbacks

--- packages/app-page-builder-elements/src/utils/index.ts
+++ packages/app-page-builder-elements/src/utils/index.ts
@@ -1,7 +1,7 @@
 // Provides a way to check whether the `PageElementsProvider` React component was mounted or not,
 // in a non-React context. In React contexts, it's strongly recommended the value of `usePageElements`
 // React hook is checked instead (a `null` value means the provider React component wasn't mounted).
-import { Breakpoint, StylesObjects } from "~/types";
+import {Breakpoint, ElementStylesCallback, StylesCallback, StylesObjects, ThemeStylesCallback} from "~/types";
 import { CSSObject } from "@emotion/css";
 
 let usingPageElementsFlag = false;
@@ -52,3 +52,47 @@ export const assignStyles = (args: {
 
     return assignTo;
 };
+
+export const defaultElementStylesCallback: ElementStylesCallback = ({
+    element,
+    modifiers,
+    renderers,
+    theme
+}) => {
+    const styles = {};
+
+    for (const modifierName in modifiers.styles) {
+        assignStyles({
+            breakpoints: theme.breakpoints,
+            assignTo: styles,
+            styles: modifiers.styles[modifierName]({
+                element,
+                theme,
+                renderers,
+                modifiers
+            })
+        });
+    }
+
+    return [styles];
+};
+
+export const defaultThemeStylesCallback: ThemeStylesCallback = ({ theme, getStyles }) => {
+    let themeStyles = {};
+    try {
+        themeStyles = getStyles(theme);
+    } catch (e) {
+        // Do nothing.
+        console.warn("Could not load theme styles:");
+        console.log(e);
+    }
+
+    const styles = assignStyles({
+        breakpoints: theme.breakpoints,
+        styles: themeStyles
+    });
+
+    return [styles];
+};
+
+export const defaultStylesCallback: StylesCallback = ({ styles }) => [styles];
