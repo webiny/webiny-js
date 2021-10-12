9beddef549c3804cfc2ac588e4825abe0bade539
ashu96 <bhardwajashu96@hotmail.com>
2021-10-12 14:41:41 +0530

    feat(cwp-template-aws): add exportPages and importPages packages in api code

---
+++ packages/cwp-template-aws/template/api/code/pageBuilder/exportPages/process/src/index.ts
@@ -0,0 +1,64 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { createHandler } from "@webiny/handler-aws";
+import i18nPlugins from "@webiny/api-i18n/graphql";
+import i18nDynamoDbStorageOperations from "@webiny/api-i18n-ddb";
+import i18nContentPlugins from "@webiny/api-i18n-content/plugins";
+import adminUsersPlugins from "@webiny/api-security-admin-users";
+import securityAdminUsersDynamoDbStorageOperations from "@webiny/api-security-admin-users-so-ddb";
+import pageBuilderPlugins from "@webiny/api-page-builder/graphql";
+import pageBuilderDynamoDbElasticsearchPlugins from "@webiny/api-page-builder-so-ddb-es";
+import pageBuilderImportExportPlugins from "@webiny/api-page-builder-import-export/graphql";
+import { createStorageOperations } from "@webiny/api-page-builder-import-export-so-ddb";
+import exportPagesProcessPlugins from "@webiny/api-page-builder-import-export/exportPages/process";
+import dbPlugins from "@webiny/handler-db";
+import { DynamoDbDriver } from "@webiny/db-dynamodb";
+import dynamoDbPlugins from "@webiny/db-dynamodb/plugins";
+import elasticSearch from "@webiny/api-elasticsearch";
+import fileManagerPlugins from "@webiny/api-file-manager/plugins";
+import fileManagerDynamoDbElasticStorageOperation from "@webiny/api-file-manager-ddb-es";
+import logsPlugins from "@webiny/handler-logs";
+import fileManagerS3 from "@webiny/api-file-manager-s3";
+import securityPlugins from "./security";
+
+const documentClient = new DocumentClient({
+    convertEmptyValues: true,
+    region: process.env.AWS_REGION
+});
+
+const debug = process.env.DEBUG === "true";
+
+export const handler = createHandler({
+    plugins: [
+        dynamoDbPlugins(),
+        logsPlugins(),
+        elasticSearch({ endpoint: `https://${process.env.ELASTIC_SEARCH_ENDPOINT}` }),
+        dbPlugins({
+            table: process.env.DB_TABLE,
+            driver: new DynamoDbDriver({
+                documentClient
+            })
+        }),
+        securityPlugins(),
+        i18nPlugins(),
+        i18nDynamoDbStorageOperations(),
+        i18nContentPlugins(),
+        fileManagerPlugins(),
+        fileManagerDynamoDbElasticStorageOperation(),
+        // Add File storage S3 plugin for API file manager.
+        fileManagerS3(),
+        adminUsersPlugins(),
+        securityAdminUsersDynamoDbStorageOperations(),
+        pageBuilderPlugins(),
+        pageBuilderDynamoDbElasticsearchPlugins(),
+        pageBuilderImportExportPlugins({
+            storageOperations: createStorageOperations({ documentClient })
+        }),
+        exportPagesProcessPlugins({
+            handlers: {
+                process: process.env.AWS_LAMBDA_FUNCTION_NAME,
+                combine: process.env.EXPORT_PAGE_COMBINE_HANDLER
+            }
+        })
+    ],
+    http: { debug }
+});
