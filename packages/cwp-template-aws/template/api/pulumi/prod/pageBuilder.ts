3ab1ca2dea17c99d54c08a86d1b6b42317eddc67
ashu96 <bhardwajashu96@hotmail.com>
2021-10-12 14:43:13 +0530

    feat(cwp-template-aws): add resources for page import export

--- packages/cwp-template-aws/template/api/pulumi/prod/pageBuilder.ts
+++ packages/cwp-template-aws/template/api/pulumi/prod/pageBuilder.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import vpc from "./vpc";
-import policies from "./policies";
+import policies, { EsDomain } from "./policies";
 
 //@ts-ignore
 import { createInstallationZip } from "@webiny/api-page-builder/installation";
@@ -11,15 +11,35 @@ interface PageBuilderParams {
     env: Record<string, any>;
     bucket: aws.s3.Bucket;
     primaryDynamodbTable: aws.dynamodb.Table;
+    elasticsearchDynamodbTable: aws.dynamodb.Table;
+    elasticsearchDomain: EsDomain;
+    cognitoUserPool: aws.cognito.UserPool;
 }
 
 class PageBuilder {
     role: aws.iam.Role;
+    exportPagesLambdaRole: aws.iam.Role;
+    importPagesLambdaRole: aws.iam.Role;
     functions: {
         updateSettings: aws.lambda.Function;
+        importPages: {
+            create: aws.lambda.Function;
+            process: aws.lambda.Function;
+        };
+        exportPages: {
+            process: aws.lambda.Function;
+            combine: aws.lambda.Function;
+        };
     };
 
-    constructor({ env, bucket, primaryDynamodbTable }: PageBuilderParams) {
+    constructor({
+        env,
+        bucket,
+        primaryDynamodbTable,
+        elasticsearchDynamodbTable,
+        elasticsearchDomain,
+        cognitoUserPool
+    }: PageBuilderParams) {
         const pbInstallationZipPath = path.join(path.resolve(), ".tmp", "pbInstallation.zip");
 
         // Will create "pbInstallation.zip" and save it in the `pbInstallationZipPath` path.
@@ -91,8 +111,153 @@ class PageBuilder {
             }
         });
 
+        this.exportPagesLambdaRole = new aws.iam.Role("pb-export-pages-lambda-role", {
+            assumeRolePolicy: {
+                Version: "2012-10-17",
+                Statement: [
+                    {
+                        Action: "sts:AssumeRole",
+                        Principal: {
+                            Service: "lambda.amazonaws.com"
+                        },
+                        Effect: "Allow"
+                    }
+                ]
+            }
+        });
+
+        const exportPagesLambdaPolicy = policies.getPbExportPagesLambdaPolicy(
+            primaryDynamodbTable,
+            bucket
+        );
+
+        new aws.iam.RolePolicyAttachment(`pb-export-pages-lambda-role-policy-attachment`, {
+            role: this.exportPagesLambdaRole,
+            policyArn: exportPagesLambdaPolicy.arn.apply(arn => arn)
+        });
+
+        new aws.iam.RolePolicyAttachment(`pb-export-pages-lambda-AWSLambdaBasicExecutionRole`, {
+            role: this.exportPagesLambdaRole,
+            policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole
+        });
+
+        const exportPagesCombineLambda = new aws.lambda.Function("pb-export-pages-combine", {
+            role: this.exportPagesLambdaRole.arn,
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            timeout: 60,
+            memorySize: 128,
+            description: "Handle page export's combine workflow",
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("../code/pageBuilder/exportPages/combine/build")
+            }),
+            environment: {
+                variables: {
+                    ...env,
+                    S3_BUCKET: bucket.id
+                }
+            }
+        });
+
+        const exportPagesProcessLambda = new aws.lambda.Function("pb-export-pages-process", {
+            role: this.exportPagesLambdaRole.arn,
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            timeout: 60,
+            memorySize: 128,
+            description: "Handle page export's process workflow",
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("../code/pageBuilder/exportPages/process/build")
+            }),
+            environment: {
+                variables: {
+                    ...env,
+                    S3_BUCKET: bucket.id,
+                    EXPORT_PAGE_COMBINE_HANDLER: exportPagesCombineLambda.arn
+                }
+            }
+        });
+
+        this.importPagesLambdaRole = new aws.iam.Role("pb-import-page-lambda-role", {
+            assumeRolePolicy: {
+                Version: "2012-10-17",
+                Statement: [
+                    {
+                        Action: "sts:AssumeRole",
+                        Principal: {
+                            Service: "lambda.amazonaws.com"
+                        },
+                        Effect: "Allow"
+                    }
+                ]
+            }
+        });
+
+        const importPageLambdaPolicy = policies.getImportPagesLambdaPolicy({
+            primaryDynamodbTable,
+            elasticsearchDomain,
+            elasticsearchDynamodbTable,
+            cognitoUserPool,
+            bucket
+        });
+
+        new aws.iam.RolePolicyAttachment(`pb-import-page-lambda-role-policy-attachment`, {
+            role: this.importPagesLambdaRole,
+            policyArn: importPageLambdaPolicy.arn.apply(arn => arn)
+        });
+
+        new aws.iam.RolePolicyAttachment(`pb-import-page-lambda-AWSLambdaBasicExecutionRole`, {
+            role: this.importPagesLambdaRole,
+            policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole
+        });
+
+        const importPagesQueueProcess = new aws.lambda.Function("pb-import-page-queue-process", {
+            role: this.importPagesLambdaRole.arn,
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            timeout: 60,
+            memorySize: 512,
+            description: "Handle import page queue process workflow",
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("../code/pageBuilder/importPages/process/build")
+            }),
+            environment: {
+                variables: {
+                    ...env,
+                    S3_BUCKET: bucket.id
+                }
+            }
+        });
+
+        const importPagesQueueCreate = new aws.lambda.Function("pb-import-page-queue-create", {
+            role: this.importPagesLambdaRole.arn,
+            runtime: "nodejs12.x",
+            handler: "handler.handler",
+            timeout: 60,
+            memorySize: 512,
+            description: "Handle import page queue create workflow",
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive("../code/pageBuilder/importPages/create/build")
+            }),
+            environment: {
+                variables: {
+                    ...env,
+                    S3_BUCKET: bucket.id,
+                    IMPORT_PAGE_QUEUE_PROCESS_HANDLER: importPagesQueueProcess.arn
+                }
+            }
+        });
+
         this.functions = {
-            updateSettings
+            updateSettings,
+            exportPages: {
+                process: exportPagesProcessLambda,
+                combine: exportPagesCombineLambda
+            },
+            importPages: {
+                process: importPagesQueueProcess,
+                create: importPagesQueueCreate
+            }
         };
     }
 }
