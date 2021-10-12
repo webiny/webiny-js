89ad98c2cea271c2d27af7bd12065db55ef003b2
ashu96 <bhardwajashu96@hotmail.com>
2021-10-12 10:42:16 +0530

    fix(project-utils): update postcss-preset-env options in webpack.config

--- packages/project-utils/bundling/app/config/webpack.config.js
+++ packages/project-utils/bundling/app/config/webpack.config.js
@@ -123,7 +123,11 @@ module.exports = function (webpackEnv, { paths, babelCustomizer }) {
                             autoprefixer: {
                                 flexbox: "no-2009"
                             },
-                            stage: 3
+                            stage: 3,
+                            // Necessary for "@material" to work with dart implementation of sass
+                            features: {
+                                "custom-properties": false
+                            }
                         }),
                         // Adds PostCSS Normalize as the reset css with default options,
                         // so that it honors browserslist config in package.json
