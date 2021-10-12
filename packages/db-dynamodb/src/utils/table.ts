75e70a237e90849d4de9d076e301adae7d295fe4
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-12 16:28:06 +0200

    refactor(db-dynamodb): readme file updates

--- packages/db-dynamodb/src/utils/table.ts
+++ packages/db-dynamodb/src/utils/table.ts
@@ -1,6 +1,11 @@
 import WebinyError from "@webiny/error";
 import { DbContext } from "@webiny/handler-db/types";
 
+/**
+ * Will be removed in favor of passing the table name directly to the storage operations.
+ *
+ * @deprecated
+ */
 export const getTable = <T extends DbContext>(context: T): string => {
     if (!context.db) {
         throw new WebinyError("Missing db on context.", "DB_ERROR");
