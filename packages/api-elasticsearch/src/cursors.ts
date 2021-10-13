292dc0a9a4d4242f5d4725afa09b0dbec96aa875
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-13 11:07:57 +0200

    fix(api-elasticsearch): encode HTML special chars for cursor (#1958)

--- packages/api-elasticsearch/src/cursors.ts
+++ packages/api-elasticsearch/src/cursors.ts
@@ -1,10 +1,12 @@
 /**
  * Encode a received cursor value into something that can be passed on to the user.
  */
-export const encodeCursor = (cursor?: string): string | undefined => {
+export const encodeCursor = (cursor?: string | string[]): string | undefined => {
     if (!cursor) {
         return undefined;
     }
+
+    cursor = Array.isArray(cursor) ? cursor.map(encodeURIComponent) : encodeURIComponent(cursor);
 
     try {
         return Buffer.from(JSON.stringify(cursor)).toString("base64");
@@ -17,12 +19,14 @@ export const encodeCursor = (cursor?: string): string | undefined => {
  * Decode a received value into a Elasticsearch cursor.
  * If no value is received or is not decodable, return undefined.
  */
-export const decodeCursor = (cursor?: string): string | undefined => {
+export const decodeCursor = (cursor?: string): string[] | string | undefined => {
     if (!cursor) {
         return undefined;
     }
     try {
-        return JSON.parse(Buffer.from(cursor, "base64").toString("ascii"));
+        const value = JSON.parse(Buffer.from(cursor, "base64").toString("ascii"));
+
+        return Array.isArray(value) ? value.map(decodeURIComponent) : decodeURIComponent(value);
     } catch (ex) {
         console.error(ex.message);
     }
