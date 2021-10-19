10640b26138101de619751d6eeff3a8f71d5869b
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-19 11:28:58 +0200

    refactor(cli): upgrade prerendering services in 5.17.0

--- packages/cli/commands/upgrade/upgrades/utils.js
+++ packages/cli/commands/upgrade/upgrades/utils.js
@@ -6,6 +6,7 @@ const loadJson = require("load-json-file");
 const writeJson = require("write-json-file");
 const semverCoerce = require("semver/functions/coerce");
 const execa = require("execa");
+const fsExtra = require("fs-extra");
 /**
  *
  * @param pkg {string}
@@ -311,6 +312,214 @@ const addImportsToSource = ({ context, source, imports, file }) => {
     }
 };
 
+/**
+ * Timestamp in seconds. Remove the milliseconds.
+ * @return {number}
+ */
+const createCurrentTimestamp = () => {
+    return Math.floor(Date.now() / 1000);
+};
+
+const validateFileExistence = files => {
+    for (const initialFile of files) {
+        const file = createFullFile(initialFile);
+        if (!fs.existsSync(file)) {
+            /**
+             * We throw error because if any of the files does not exist, it should not go any further.
+             */
+            throw new Error(`There is no file "${file}".`);
+        }
+    }
+};
+
+const createBackupFileName = file => {
+    const ext = `.${file.split(".").pop()}`;
+
+    const now = createCurrentTimestamp();
+
+    const backup = file.replace(new RegExp(`${ext}$`), `.${now}${ext}`);
+
+    const backupFile = createFullFile(backup);
+    if (!fs.existsSync(backupFile)) {
+        return backup;
+    }
+    throw new Error(`Backup file "${backupFile}" already exists.`);
+};
+
+const createFullFile = file => {
+    return path.join(process.cwd(), file);
+};
+/**
+ *
+ * @param context {CliContext}
+ * @param initialTargets {{source: string, destination: string}[]}
+ */
+const copyFiles = (context, initialTargets) => {
+    context.info("Copying files...");
+    /**
+     * First check if source and target files exist and create a backup file name.
+     * @type {{source: string, destination: string, backup: string}[]}
+     */
+    const targets = [];
+    for (const target of initialTargets) {
+        /**
+         *
+         */
+        validateFileExistence([target.source, target.destination]);
+        let backup;
+        try {
+            backup = createBackupFileName(target.destination);
+        } catch (ex) {
+            context.error(ex.message);
+            process.exit(1);
+        }
+
+        targets.push({
+            source: target.source,
+            destination: target.destination,
+            backup
+        });
+    }
+    /**
+     * Then:
+     * - make backups of the targets files
+     * - copy new files to their destinations
+     */
+    const backups = [];
+    context.info("Creating backups...");
+    for (const target of targets) {
+        try {
+            fs.copyFileSync(createFullFile(target.destination), createFullFile(target.backup));
+            context.info(`Backed up "${target.destination}" to "${target.backup}".`);
+            backups.push(target.backup);
+        } catch (ex) {
+            context.error(`Could not create backup "${target.destination}" to "${target.backup}".`);
+            for (const backup of backups) {
+                context.info(`Removing created backup "${backup}".`);
+                fs.unlinkSync(createFullFile(backup));
+            }
+            process.exit(1);
+        }
+    }
+
+    const files = [];
+    context.info("Copying new files...");
+    for (const target of targets) {
+        try {
+            fs.copyFileSync(createFullFile(target.source), createFullFile(target.destination));
+            context.info(`Copying new file "${target.source}" to "${target.destination}".`);
+            files.push({
+                destination: target.destination,
+                backup: target.backup
+            });
+        } catch (ex) {
+            context.error(`Could not copy new file "${target.source}" to "${target.destination}".`);
+            for (const file of files) {
+                context.info(`Restoring backup file "${file.backup}" to "${file.destination}".`);
+                fs.copyFileSync(createFullFile(file.backup), createFullFile(file.destination));
+                fs.unlinkSync(createFullFile(file.backup));
+            }
+            process.exit(1);
+        }
+    }
+    context.info("File copying complete!");
+};
+
+/**
+ * @param context {CliContext}
+ * @param targets {{source: string, destination: string}[]}
+ */
+const copyFolders = (context, targets) => {
+    context.info(`Copy folders...`);
+    /**
+     * First we need to backup existing folders.
+     */
+    for (const target of targets) {
+        const folders = target.destination.split("/");
+        const folderName = folders.pop();
+        const now = createCurrentTimestamp();
+        const backupDestination = folders.concat([`${folderName}_${now}`]).join("/");
+        if (fs.existsSync(target.destination) === false) {
+            context.info(`No destination "${target.destination}" to backup.`);
+            continue;
+        }
+        fsExtra.copySync(target.destination, backupDestination);
+        context.info(`Backed up "${target.destination}" to ${backupDestination}.`);
+    }
+    /**
+     * Delete existing folders.
+     */
+    for (const target of targets) {
+        fsExtra.removeSync(target.destination);
+    }
+    /**
+     * And in the end we need to copy new folders to their destinations.
+     */
+    for (const target of targets) {
+        fsExtra.copySync(target.source, target.destination);
+    }
+};
+
+/**
+ *
+ * @param context {CliContext}
+ * @param initialTargets {{source: string, destination: string}[]}
+ */
+const assignPackageVersions = (context, initialTargets, targetVersion) => {
+    const targets = initialTargets
+        .filter(target => target.destination.match(/package\.json$/) !== null)
+        .map(target => target.destination);
+    if (targets.length === 0) {
+        return;
+    }
+    context.info("Assigning proper package versions...");
+    for (const target of targets) {
+        const file = path.join(process.cwd(), target);
+        try {
+            const json = JSON.parse(fs.readFileSync(file).toString());
+            /**
+             *
+             * @type {{}}
+             */
+            json.dependencies = Object.keys(json.dependencies).reduce((dependencies, key) => {
+                if (key.match(/^@webiny\//) === null) {
+                    dependencies[key] = json.dependencies[key];
+                    return dependencies;
+                } else if (json.dependencies[key] === "latest") {
+                    dependencies[key] = `^${targetVersion}`;
+                } else {
+                    dependencies[key] = json.dependencies[key];
+                }
+
+                return dependencies;
+            }, {});
+            /**
+             *
+             */
+            if (json.devDependencies) {
+                json.devDependencies = Object.keys(json.devDependencies).reduce(
+                    (dependencies, key) => {
+                        if (key.match(/^@webiny\//) === null) {
+                            dependencies[key] = json.devDependencies[key];
+                            return dependencies;
+                        } else if (json.devDependencies[key] === "latest") {
+                            dependencies[key] = `^${targetVersion}`;
+                        } else {
+                            dependencies[key] = json.devDependencies[key];
+                        }
+
+                        return dependencies;
+                    },
+                    {}
+                );
+            }
+            fs.writeFileSync(file, JSON.stringify(json));
+        } catch (ex) {
+            console.error(ex.message);
+        }
+    }
+};
+
 module.exports = {
     insertImport,
     addPackagesToDependencies,
@@ -319,5 +528,8 @@ module.exports = {
     createMorphProject,
     prettierFormat,
     yarnInstall,
-    addImportsToSource
+    addImportsToSource,
+    assignPackageVersions,
+    copyFiles,
+    copyFolders
 };
