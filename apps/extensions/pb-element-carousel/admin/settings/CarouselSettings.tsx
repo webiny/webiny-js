ec930ed82b7d89786f3b429883d6d3d4d81383c3
ashu96 <bhardwajashu96@hotmail.com>
2021-10-21 18:12:03 +0530

    refactor(apps/extension): remove redundant code

--- apps/extensions/pb-element-carousel/admin/settings/CarouselSettings.tsx
+++ apps/extensions/pb-element-carousel/admin/settings/CarouselSettings.tsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { css } from "emotion";
 import dotProp from "dot-prop-immutable";
-import { useRecoilValue } from "recoil";
 import { Grid, Cell } from "@webiny/ui/Grid";
-import {
-    PbEditorPageElementSettingsRenderComponentProps,
-    PbEditorElement
-} from "@webiny/app-page-builder/types";
+import { PbEditorPageElementSettingsRenderComponentProps } from "@webiny/app-page-builder/types";
 import { useEventActionHandler } from "@webiny/app-page-builder/editor/hooks/useEventActionHandler";
 import { createElement } from "@webiny/app-page-builder/editor/helpers";
 import { UpdateElementActionEvent } from "@webiny/app-page-builder/editor/recoil/actions";
-import {
-    activeElementAtom,
-    elementWithChildrenByIdSelector
-} from "@webiny/app-page-builder/editor/recoil/modules";
 // Components
 import {
     ContentWrapper,
     SimpleButton
 } from "@webiny/app-page-builder/editor/plugins/elementSettings/components/StyledComponents";
 import Accordion from "@webiny/app-page-builder/editor/plugins/elementSettings/components/Accordion";
+import { useActiveElement } from "@webiny/app-page-builder/editor/hooks/useActiveElement";
 
 const classes = {
     grid: css({
@@ -36,82 +29,70 @@ const classes = {
     })
 };
 
-export const CarouselSettings: React.FunctionComponent<PbEditorPageElementSettingsRenderComponentProps> = ({
-    defaultAccordionValue
-}) => {
-    const handler = useEventActionHandler();
-    const activeElementId = useRecoilValue(activeElementAtom);
-    const element = (useRecoilValue(
-        elementWithChildrenByIdSelector(activeElementId)
-    ) as unknown) as PbEditorElement;
+export const CarouselSettings: React.FunctionComponent<PbEditorPageElementSettingsRenderComponentProps> =
+    ({ defaultAccordionValue }) => {
+        const handler = useEventActionHandler();
+        const [activeElement] = useActiveElement();
 
-    const deleteItem = index => {
-        if (index >= element.elements.length || index < 0) {
-            return;
-        }
-        handler.trigger(
-            new UpdateElementActionEvent({
-                element: {
-                    ...element,
+        const deleteItem = index => {
+            if (index >= activeElement.elements.length || index < 0) {
+                return;
+            }
+            const newElement = dotProp.set(
+                activeElement,
+                "elements",
+                dotProp.delete(activeElement.elements, index)
+            );
+            handler.trigger(
+                new UpdateElementActionEvent({
+                    element: newElement,
+                    history: true
+                })
+            );
+        };
+
+        const removeElementFromEnd = () => {
+            deleteItem(activeElement.elements.length - 1);
+        };
+
+        const addNewElementToEnd = () => {
+            const newElement = dotProp.set(activeElement, "elements", [
+                ...activeElement.elements,
+                createElement("cell", {
                     data: {
-                        ...element.data,
                         settings: {
-                            ...(element.data.settings || {})
-                        }
-                    },
-                    elements: dotProp.delete(element.elements, index)
-                },
-                history: true
-            })
-        );
-    };
-
-    const removeElementFromEnd = () => {
-        deleteItem(element.elements.length - 1);
-    };
-
-    const addNewElementToEnd = () => {
-        handler.trigger(
-            new UpdateElementActionEvent({
-                element: {
-                    ...element,
-                    data: {
-                        ...element.data,
-                        settings: {
-                            ...(element.data.settings || {})
-                        }
-                    },
-                    elements: dotProp.set(
-                        element.elements,
-                        element.elements.length,
-                        createElement("cell", {
-                            data: {
-                                settings: {
-                                    grid: {
-                                        size: 12
-                                    }
-                                }
+                            grid: {
+                                size: 12
                             }
-                        })
-                    )
-                },
-                history: true
-            })
+                        }
+                    }
+                })
+            ]);
+            handler.trigger(
+                new UpdateElementActionEvent({
+                    element: newElement,
+                    history: true
+                })
+            );
+        };
+
+        return (
+            <Accordion title={"Carousel"} defaultValue={defaultAccordionValue}>
+                <ContentWrapper direction={"column"}>
+                    <Grid className={classes.grid}>
+                        <Cell span={6}>
+                            <SimpleButton
+                                disabled={activeElement.elements.length === 0}
+                                onClick={removeElementFromEnd}
+                            >
+                                Remove Item
+                            </SimpleButton>
+                        </Cell>
+                        <Cell span={6}>
+                            <SimpleButton onClick={addNewElementToEnd}>Add Item</SimpleButton>
+                        </Cell>
+                    </Grid>
+                </ContentWrapper>
+            </Accordion>
         );
     };
-
-    return (
-        <Accordion title={"Carousel"} defaultValue={defaultAccordionValue}>
-            <ContentWrapper direction={"column"}>
-                <Grid className={classes.grid}>
-                    <Cell span={6}>
-                        <SimpleButton onClick={removeElementFromEnd}>Remove Item</SimpleButton>
-                    </Cell>
-                    <Cell span={6}>
-                        <SimpleButton onClick={addNewElementToEnd}>Add Item</SimpleButton>
-                    </Cell>
-                </Grid>
-            </ContentWrapper>
-        </Accordion>
-    );
-};
