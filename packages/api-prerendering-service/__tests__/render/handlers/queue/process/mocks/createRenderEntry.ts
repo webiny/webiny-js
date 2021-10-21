b9b1c32f62ddcdf4e66ec7db8b42ccac462dc79d
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-21 20:41:33 +0200

    test(api-prerendering-service): write tests using storage operations

--- packages/api-prerendering-service/__tests__/render/handlers/queue/process/mocks/createRenderEntry.ts
+++ packages/api-prerendering-service/__tests__/render/handlers/queue/process/mocks/createRenderEntry.ts
@@ -1,3 +1,5 @@
+import { Render } from "~/types";
+
 const defaults = {
     configuration: {
         meta: {
@@ -6,7 +8,7 @@ const defaults = {
             }
         },
         db: {
-            namespace: "T#root"
+            namespace: "root"
         },
         storage: {
             name: "s3-bucket-name",
@@ -15,25 +17,9 @@ const defaults = {
     }
 };
 
-type File = {
-    name: string;
-    type: string;
-    meta: { tags: Array<{ key: string; value: string }> } & Record<string, any>;
-};
-
-type Entry = Record<string, any> & {
-    PK: string;
-    SK: string;
-    TYPE: string;
-    url: string;
-    files: File[];
-};
-
-export default function (args): Entry {
+export default function (args): Render {
     return {
-        PK: "T#root#PS#RENDER",
-        SK: args.url,
-        TYPE: "ps.render",
+        namespace: args.namespace,
         url: args.url,
         args: {
             url: args.url,
--- packages/api-prerendering-service/__tests__/render/handlers/queue/process/useHandler.ts
+++ packages/api-prerendering-service/__tests__/render/handlers/queue/process/useHandler.ts
@@ -1,48 +1,22 @@
-import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { createHandler } from "@webiny/handler";
-import dbPlugins from "@webiny/handler-db";
-import { DynamoDbDriver } from "@webiny/db-dynamodb";
-import queueProcessPlugins from "@webiny/api-prerendering-service/queue/process";
+import queueProcessPlugins from "~/queue/process";
 import handlerClient from "@webiny/handler-client";
-
-const defaults = {
-    db: {
-        table: "PrerenderingService",
-        keys: [
-            {
-                primary: true,
-                unique: true,
-                name: "primary",
-                fields: [{ name: "PK" }, { name: "SK" }]
-            }
-        ]
-    }
-};
+import { getStorageOperations } from "../../../../storageOperations";
 
 export default (...plugins) => {
-    const dynamoDbDriver = new DynamoDbDriver({
-        documentClient: new DocumentClient({
-            convertEmptyValues: true,
-            endpoint: process.env.MOCK_DYNAMODB_ENDPOINT,
-            sslEnabled: false,
-            region: "local"
-        })
-    });
+    const storageOperations = getStorageOperations();
 
     const handler = createHandler(
         ...plugins,
         handlerClient(),
         queueProcessPlugins({
+            storageOperations,
             handlers: {
                 render: "handler-client-handler-render-handler",
                 flush: "handler-client-handler-flush-handler"
             }
-        }),
-        dbPlugins({
-            table: "PrerenderingService",
-            driver: dynamoDbDriver
         })
     );
 
-    return { handler, dynamoDbDriver, defaults };
+    return { handler, storageOperations };
 };
