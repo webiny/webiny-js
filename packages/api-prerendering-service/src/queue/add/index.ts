d4a7cb434b6685198b5f977649517b510e4cdca5
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-21 18:56:48 +0200

    feat(api-prerendering-service): implement queue add storage operations

--- packages/api-prerendering-service/src/queue/add/index.ts
+++ packages/api-prerendering-service/src/queue/add/index.ts
@@ -1,62 +1,70 @@
-import { HandlerPlugin, QueueAddHookPlugin } from "./types";
-import { TYPE, DbQueueJob } from "../../types";
-import { HandlerResponse } from "../../types";
 import mdbid from "mdbid";
+import { HandlerPlugin, QueueAddHookPlugin } from "./types";
+import { PrerenderingServiceStorageOperations, QueueJob, HandlerResponse } from "../../types";
 
-export default (): HandlerPlugin => ({
-    type: "handler",
-    async handle(context): Promise<HandlerResponse> {
-        const log = console.log;
-        const { invocationArgs } = context;
-        const handlerArgs = Array.isArray(invocationArgs) ? invocationArgs : [invocationArgs];
-        const handlerHookPlugins = context.plugins.byType<QueueAddHookPlugin>("ps-queue-add-hook");
+export interface Params {
+    storageOperations: PrerenderingServiceStorageOperations;
+}
 
-        log("Received args: ", JSON.stringify(invocationArgs));
+export default (params: Params): HandlerPlugin => {
+    const { storageOperations } = params;
 
-        try {
-            for (let i = 0; i < handlerArgs.length; i++) {
-                const args = handlerArgs[i];
+    return {
+        type: "handler",
+        async handle(context): Promise<HandlerResponse> {
+            const log = console.log;
+            const { invocationArgs } = context;
+            const handlerArgs = Array.isArray(invocationArgs) ? invocationArgs : [invocationArgs];
+            const handlerHookPlugins =
+                context.plugins.byType<QueueAddHookPlugin>("ps-queue-add-hook");
 
-                for (let j = 0; j < handlerHookPlugins.length; j++) {
-                    const plugin = handlerHookPlugins[j];
-                    if (typeof plugin.beforeAdd === "function") {
-                        await plugin.beforeAdd({
-                            context,
-                            args,
-                            log
-                        });
+            log("Received args: ", JSON.stringify(invocationArgs));
+
+            try {
+                for (let i = 0; i < handlerArgs.length; i++) {
+                    const args = handlerArgs[i];
+
+                    for (let j = 0; j < handlerHookPlugins.length; j++) {
+                        const plugin = handlerHookPlugins[j];
+                        if (typeof plugin.beforeAdd === "function") {
+                            await plugin.beforeAdd({
+                                context,
+                                args,
+                                log
+                            });
+                        }
+                    }
+
+                    log("Saving new queue job.");
+
+                    const queueJob: QueueJob = {
+                        id: mdbid(),
+                        args
+                    };
+
+                    await storageOperations.createQueueJob({
+                        queueJob
+                    });
+
+                    log("Queue job saved.", JSON.stringify(invocationArgs));
+
+                    for (let j = 0; j < handlerHookPlugins.length; j++) {
+                        const plugin = handlerHookPlugins[j];
+                        if (typeof plugin.afterAdd === "function") {
+                            await plugin.afterAdd({
+                                context,
+                                args,
+                                log
+                            });
+                        }
                     }
                 }
 
-                log("Saving new queue job.");
-
-                const data: DbQueueJob = {
-                    PK: "PS#Q#JOB",
-                    SK: mdbid(),
-                    TYPE: TYPE.DbQueueJob,
-                    args
-                };
-
-                await context.db.create({ data });
-
-                log("Queue job saved.", JSON.stringify(invocationArgs));
-
-                for (let j = 0; j < handlerHookPlugins.length; j++) {
-                    const plugin = handlerHookPlugins[j];
-                    if (typeof plugin.afterAdd === "function") {
-                        await plugin.afterAdd({
-                            context,
-                            args,
-                            log
-                        });
-                    }
-                }
+                return { data: null, error: null };
+            } catch (e) {
+                log("An error occurred while trying to add to prerendering queue...", e);
+                return { data: null, error: e };
             }
-
-            return { data: null, error: null };
-        } catch (e) {
-            log("An error occurred while trying to add to prerendering queue...", e);
-            return { data: null, error: e };
         }
-    }
-});
+    };
+};
