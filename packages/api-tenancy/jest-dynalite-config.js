bf866571bda15a31b5adec4679c2dab4a1c1a4fe
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-26 08:41:47 +0200

    test(api-prerendering-service): switch string for process.env

--- packages/api-tenancy/jest-dynalite-config.js
+++ packages/api-tenancy/jest-dynalite-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     tables: [
         {
-            TableName: `Tenancy`,
+            TableName: process.env.DB_TABLE,
             KeySchema: [
                 { AttributeName: "PK", KeyType: "HASH" },
                 { AttributeName: "SK", KeyType: "RANGE" }
