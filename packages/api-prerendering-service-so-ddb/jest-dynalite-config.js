a8950b31aed4349a114092e4b965fad191692599
Bruno Zorić <bruno.zoric@gmail.com>
2021-10-27 09:11:53 +0200

    feat(api-prerendering-service): add storage ops - no tests enabled yet

---
+++ packages/api-prerendering-service-so-ddb/jest-dynalite-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    tables: [
+        {
+            TableName: process.env.DB_TABLE,
+            KeySchema: [
+                { AttributeName: "PK", KeyType: "HASH" },
+                { AttributeName: "SK", KeyType: "RANGE" }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: "PK", AttributeType: "S" },
+                { AttributeName: "SK", AttributeType: "S" },
+                { AttributeName: "GSI1_PK", AttributeType: "S" },
+                { AttributeName: "GSI1_SK", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+        }
+    ],
+    basePort: 8000
+};
---
+++ packages/api-prerendering-service-so-ddb/src/definitions/queueJob.ts
@@ -0,0 +1,34 @@
+import { Entity, Table } from "dynamodb-toolbox";
+import { Attributes } from "~/types";
+
+export interface Params {
+    table: Table;
+    entityName: string;
+    attributes: Attributes;
+}
+
+export const createQueueJobEntity = (params: Params): Entity<any> => {
+    const { entityName, attributes, table } = params;
+    return new Entity({
+        name: entityName,
+        table,
+        attributes: {
+            PK: {
+                partitionKey: true
+            },
+            SK: {
+                sortKey: true
+            },
+            TYPE: {
+                type: "string"
+            },
+            id: {
+                type: "string"
+            },
+            args: {
+                type: "map"
+            },
+            ...(attributes || {})
+        }
+    });
+};
