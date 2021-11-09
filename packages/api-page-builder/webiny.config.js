493d5d9d36923288be299788956b80e3ae3cc668
Adrian Smijulj <adrian1358@gmail.com>
2021-11-09 09:15:35 +0100

    fix: undo changes

--- packages/api-page-builder/webiny.config.js
+++ packages/api-page-builder/webiny.config.js
@@ -1,17 +1,8 @@
 const { createWatchPackage, createBuildPackage } = require("@webiny/project-utils");
-const path = require("path");
-const util = require("util");
-const ncpBase = require("ncp");
-const ncp = util.promisify(ncpBase.ncp);
 
 module.exports = {
     commands: {
-        build: async (...args) => {
-            await createBuildPackage({ cwd: __dirname })(...args);
-            const from = path.join(__dirname, "src", "installation", "files");
-            const to = path.join(__dirname, "dist", "installation", "files");
-            await ncp(from, to);
-        },
+        build: createBuildPackage({ cwd: __dirname }),
         watch: createWatchPackage({ cwd: __dirname })
     }
 };
