c1e417c41b2661ea045cf9ecf9be30e6c4c2dc67
Adrian Smijulj <adrian1358@gmail.com>
2021-11-09 08:22:35 +0100

    fix: copy installation files

--- packages/api-page-builder/webiny.config.js
+++ packages/api-page-builder/webiny.config.js
@@ -1,8 +1,17 @@
 const { createWatchPackage, createBuildPackage } = require("@webiny/project-utils");
+const path = require("path");
+const util = require("util");
+const ncpBase = require("ncp");
+const ncp = util.promisify(ncpBase.ncp);
 
 module.exports = {
     commands: {
-        build: createBuildPackage({ cwd: __dirname }),
+        build: async (...args) => {
+            await createBuildPackage({ cwd: __dirname })(...args);
+            const from = path.join(__dirname, "src", "installation", "files");
+            const to = path.join(__dirname, "dist", "installation", "files");
+            await ncp(from, to);
+        },
         watch: createWatchPackage({ cwd: __dirname })
     }
 };
