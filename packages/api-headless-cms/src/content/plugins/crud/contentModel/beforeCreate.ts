6453d069c67bcf5bd87e39528af87fc35d8bee8b
Bruno Zorić <bruno.zoric@gmail.com>
2021-11-18 10:41:54 +0100

    feat(api-headless-cms): check on before create for disallowed model ids

--- packages/api-headless-cms/src/content/plugins/crud/contentModel/beforeCreate.ts
+++ packages/api-headless-cms/src/content/plugins/crud/contentModel/beforeCreate.ts
@@ -1,20 +1,30 @@
+import WebinyError from "@webiny/error";
+import camelCase from "lodash/camelCase";
+import pluralize from "pluralize";
 import { BeforeModelCreateTopicParams, CmsModel, HeadlessCmsStorageOperations } from "~/types";
 import { Topic } from "@webiny/pubsub/types";
 import { PluginsContainer } from "@webiny/plugins";
 import { CmsModelPlugin } from "~/content/plugins/CmsModelPlugin";
-import WebinyError from "@webiny/error";
-import camelCase from "lodash/camelCase";
-import pluralize from "pluralize";
 
-const MAX_MODEL_ID_SEARCH_AMOUNT = 50;
+const disallowedModelIdList: string[] = [
+    "contentModel",
+    "contentModels",
+    "contentModelGroup",
+    "contentModelGroups"
+];
+/**
+ * This list is to disallow creating models that might interfere with GraphQL schema creation.
+ * Add more if required.
+ */
+const disallowedModelIdEndingList: string[] = ["Response", "List", "Meta", "Input", "Sorter"];
 
 /**
  * Checks for the uniqueness of provided modelId, against the provided list of models.
  * It also takes plural / singular forms of the provided modelId into account.
  */
-const checkModelIdUniqueness = (models: string[], modelId: string) => {
-    if (models.includes(modelId) === true) {
-        throw Error(`Content model with modelId "${modelId}" already exists.`);
+const checkModelIdUniqueness = (modelIdList: string[], modelId: string) => {
+    if (modelIdList.includes(modelId) === true) {
+        throw new WebinyError(`Content model with modelId "${modelId}" already exists.`);
     }
     /**
      * Additionally, check if the plural form of the received modelId exists too. This prevents users
@@ -22,8 +32,8 @@ const checkModelIdUniqueness = (models: string[], modelId: string) => {
      * 1. First check if user wants to create the "event" model, but the "events" model already exists.
      */
     const pluralizedModelIdCamelCase = pluralize(modelId);
-    if (models.includes(pluralizedModelIdCamelCase) === true) {
-        throw Error(
+    if (modelIdList.includes(pluralizedModelIdCamelCase) === true) {
+        throw new WebinyError(
             `Content model with modelId "${modelId}" does not exist, but a model with modelId "${pluralizedModelIdCamelCase}" does.`
         );
     }
@@ -32,67 +42,51 @@ const checkModelIdUniqueness = (models: string[], modelId: string) => {
      * 2. Then check if user wants to create the "events" model, but the "event" model already exists.
      */
     const singularizedModelIdCamelCase = pluralize.singular(modelId);
-    if (models.includes(singularizedModelIdCamelCase) === true) {
-        throw Error(
+    if (modelIdList.includes(singularizedModelIdCamelCase) === true) {
+        throw new WebinyError(
             `Content model with modelId "${modelId}" does not exist, but a model with modelId "${singularizedModelIdCamelCase}" does.`
         );
     }
 };
 
-/**
- * Also used to check uniqueness of the provided modelId, although this one just returns a simple boolean value.
- */
-const isUniqueModelId = (models: string[], modelId: string) => {
-    try {
-        checkModelIdUniqueness(models, modelId);
-        return true;
-    } catch {
-        /**
-         * If an error has been thrown - we return false.
-         */
-        return false;
+const checkModelIdAllowed = (modelId: string): void => {
+    if (disallowedModelIdList.includes(modelId) === false) {
+        return;
     }
+    throw new WebinyError(`Provided model ID "${modelId}" is not allowed.`);
 };
 
-const DISALLOWED_MODEL_IDS = [
-    "contentModel",
-    "contentModels",
-    "contentModelGroup",
-    "contentModelGroups"
-];
-
-const checkModelIdAllowed = (modelId: string) => {
-    if (DISALLOWED_MODEL_IDS.includes(modelId)) {
-        throw new Error(`Provided model ID "${modelId}" is not allowed.`);
-    }
-};
-
-const isAllowedModelId = (modelId: string) => {
-    return !DISALLOWED_MODEL_IDS.includes(modelId);
-};
-
-const createNewModelId = (existingModels: string[], model: CmsModel): string => {
-    const modelIdCamelCase = camelCase(model.name);
-    let counter = 0;
-    while (true) {
-        if (counter > MAX_MODEL_ID_SEARCH_AMOUNT) {
-            throw new Error(
-                `While loop reached #${MAX_MODEL_ID_SEARCH_AMOUNT} when checking for unique "modelId".`
-            );
+const checkModelIdEndingAllowed = (modelId: string): void => {
+    for (const ending of disallowedModelIdEndingList) {
+        const re = new RegExp(`${ending}$`, "i");
+        const matched = modelId.match(re);
+        if (matched === null) {
+            continue;
         }
-
-        /**
-         * Let's try generating a new modelId and immediately check for its uniqueness.
-         */
-        const generatedModelId = `${modelIdCamelCase}${counter || ""}`;
-        if (
-            isAllowedModelId(generatedModelId) &&
-            isUniqueModelId(existingModels, generatedModelId)
-        ) {
-            return generatedModelId;
-        }
-        counter++;
+        throw new WebinyError(
+            `ModelId that ends with "${ending}" is not allowed.`,
+            "MODEL_ID_NOT_ALLOWED",
+            {
+                modelId
+            }
+        );
     }
+};
+
+const getModelId = (model: CmsModel): string => {
+    const { modelId, name } = model;
+    if (modelId) {
+        return camelCase(modelId.trim());
+    } else if (name) {
+        return camelCase(name.trim());
+    }
+    throw new WebinyError(
+        `There is no "modelId" or "name" passed into the create model method.`,
+        "MISSING_MODEL_DATA",
+        {
+            model
+        }
+    );
 };
 
 export interface Params {
@@ -100,15 +94,18 @@ export interface Params {
     storageOperations: HeadlessCmsStorageOperations;
     plugins: PluginsContainer;
 }
+
 export const assignBeforeModelCreate = (params: Params) => {
     const { onBeforeCreate, storageOperations, plugins } = params;
 
     onBeforeCreate.subscribe(async params => {
         const { model } = params;
 
+        const modelId = getModelId(model);
+
         const modelPlugin: CmsModelPlugin = plugins
             .byType<CmsModelPlugin>(CmsModelPlugin.type)
-            .find((item: CmsModelPlugin) => item.contentModel.modelId === model.modelId);
+            .find((item: CmsModelPlugin) => item.contentModel.modelId === modelId);
 
         if (modelPlugin) {
             throw new WebinyError(
@@ -128,21 +125,15 @@ export const assignBeforeModelCreate = (params: Params) => {
         });
         const modelIdList = models.map(m => m.modelId);
 
-        const currentId = (model.modelId || "").trim();
-
         /**
-         * If there is a modelId assigned, check if it's unique ...
+         * We need to check for:
+         *  - is that exact modelId allowed
+         *  - is modelId unique
+         *  - is model ending allowed
          */
-        if (currentId) {
-            const modelIdCamelCase = camelCase(currentId);
-            checkModelIdAllowed(modelIdCamelCase);
-            checkModelIdUniqueness(modelIdList, modelIdCamelCase);
-            model.modelId = modelIdCamelCase;
-            return;
-        }
-        /**
-         * ... otherwise, assign a unique modelId automatically.
-         */
-        model.modelId = createNewModelId(modelIdList, model);
+        checkModelIdAllowed(modelId);
+        checkModelIdEndingAllowed(modelId);
+        checkModelIdUniqueness(modelIdList, modelId);
+        model.modelId = modelId;
     });
 };
