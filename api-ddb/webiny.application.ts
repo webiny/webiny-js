014feb704285889959255c6137fb1009e08df40b
Pavel Denisjuk <pavel@webiny.com>
2021-11-23 11:27:44 +0100

    chore: add api-ddb setup

---
+++ api-ddb/webiny.application.ts
@@ -0,0 +1,9 @@
+/**
+ * For more information on the API project application, please see:
+ * https://www.webiny.com/docs/key-topics/cloud-infrastructure/api/introduction
+ */
+export default {
+    id: "api",
+    name: "API",
+    description: "Your GraphQL API and all of the backend services."
+};
