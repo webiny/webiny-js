7695eeeb0717c78b79dae11d42136897cefd901f
Bruno Zorić <bruno.zoric@gmail.com>
2021-12-01 14:27:52 +0100

    fix(api-headless-cms): system version check (#2070)

--- packages/api-headless-cms/src/content/plugins/utils/renderSortEnum.ts
+++ packages/api-headless-cms/src/content/plugins/utils/renderSortEnum.ts
@@ -15,21 +15,14 @@ export const renderSortEnum: RenderSortEnum = ({ model, fieldTypePlugins }) => {
         "createdOn_DESC"
     ];
 
-    const fieldIds = model.fields
-        .filter(f => {
-            // Every time a client updates content model's fields, we check the type of each field. If a field plugin
-            // for a particular "field.type" doesn't exist on the backend yet, we throw an error. But still, we also
-            // want to be careful when accessing the field plugin here too. It is still possible to have a content model
-            // that contains a field, for which we don't have a plugin registered on the backend. For example, user
-            // could've just removed the plugin from the backend.
-            return get(fieldTypePlugins, `${f.type}.isSortable`);
-        })
-        .map(f => f.fieldId);
-
-    fieldIds.forEach(fieldId => {
-        sorters.push(`${fieldId}_ASC`);
-        sorters.push(`${fieldId}_DESC`);
-    });
+    for (const field of model.fields) {
+        const isSortable = get(fieldTypePlugins, `${field.type}.isSortable`);
+        if (!isSortable) {
+            continue;
+        }
+        sorters.push(`${field.fieldId}_ASC`);
+        sorters.push(`${field.fieldId}_DESC`);
+    }
 
     return sorters.join("\n");
 };
