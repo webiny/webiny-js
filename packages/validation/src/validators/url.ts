80aa2494e72099cd927c05a3a6d8bed0def30711
Carsten Jaksch <carsten.jaksch@icloud.com>
2021-12-01 12:17:25 +0100

    fix(@webiny/validation): add exclamation mark to url validation regexp

--- packages/validation/src/validators/url.ts
+++ packages/validation/src/validators/url.ts
@@ -3,7 +3,7 @@ import ValidationError from "./../validationError";
 const regex = {
     base: new RegExp(
         // eslint-disable-next-line
-        /^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
+        /^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=!-]*)?(\#[-a-z\d_]*)?$/i
     ),
     ip: new RegExp(
         // eslint-disable-next-line
