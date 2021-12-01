dc4a25d5e9a6824b4a4f7f6de70aee6d8a5f1844
ashu96 <bhardwajashu96@hotmail.com>
2021-12-01 11:36:50 +0530

    refactor(api-apw): extract contentReview model's fields

--- packages/api-apw/src/plugins/models/contentReview.model.ts
+++ packages/api-apw/src/plugins/models/contentReview.model.ts
@@ -1,3 +1,219 @@
+import { createModelField } from "../utils";
+
+const contentField = () =>
+    createModelField({ label: "Content", parent: "contentReview", type: "text" });
+
+const requestedByField = () => ({
+    id: "contentReview_reviewRequestedBy",
+    fieldId: "reviewRequestedBy",
+    label: "Review requested by",
+    type: "text",
+    settings: {},
+    listValidation: [],
+    validation: [],
+    multipleValues: false,
+    predefinedValues: {
+        values: [],
+        enabled: false
+    }
+});
+
+const stepStatusField = () => ({
+    multipleValues: false,
+    listValidation: [],
+    renderer: {
+        name: "radio-buttons"
+    },
+    predefinedValues: {
+        enabled: true,
+        values: [
+            {
+                value: "done",
+                label: "Steps done"
+            },
+            {
+                value: "active",
+                label: "Step active"
+            },
+            {
+                value: "inactive",
+                label: "Step inactive"
+            }
+        ]
+    },
+    label: "Status",
+    id: "contentReview_steps_status",
+    type: "text",
+    validation: [
+        {
+            name: "required",
+            message: "Value is required."
+        }
+    ],
+    fieldId: "status"
+});
+
+const stepSlugField = () => ({
+    renderer: {
+        name: "text-input"
+    },
+    label: "Slug",
+    id: "contentReview_steps_slug",
+    type: "text",
+    validation: [
+        {
+            name: "required",
+            message: "Value is required."
+        }
+    ],
+    fieldId: "slug"
+});
+
+const stepsField = fields => ({
+    id: "contentReview_steps",
+    label: "Steps",
+    type: "object",
+    settings: {
+        fields,
+        layout: fields.map(field => [field.fieldId])
+    },
+    listValidation: [],
+    validation: [],
+    fieldId: "steps",
+    multipleValues: true,
+    predefinedValues: {
+        values: [],
+        enabled: false
+    }
+});
+
+const changeRequestedTitleField = () => ({
+    renderer: {
+        name: "text-input"
+    },
+    label: "Title",
+    id: "contentReview_changeRequested_title",
+    type: "text",
+    validation: [
+        {
+            name: "required",
+            message: "Value is required."
+        }
+    ],
+    fieldId: "title"
+});
+
+const changeRequestedBodyField = () => ({
+    renderer: {
+        name: "rich-text-input"
+    },
+    label: "Body",
+    id: "contentReview_changeRequested_body",
+    type: "rich-text",
+    validation: [],
+    fieldId: "body"
+});
+
+const changeRequestedMediaField = () => ({
+    renderer: {
+        name: "file-input"
+    },
+    label: "Media",
+    id: "contentReview_changeRequested_media",
+    type: "file",
+    validation: [],
+    fieldId: "media"
+});
+
+const changeRequestedStepField = () => ({
+    renderer: {
+        name: "text-input"
+    },
+    label: "Step",
+    id: "contentReview_changeRequested_step",
+    type: "text",
+    validation: [
+        {
+            name: "required",
+            message: "Value is required."
+        }
+    ],
+    fieldId: "step"
+});
+
+const changeRequestedResolvedField = () => ({
+    renderer: {
+        name: "boolean-input"
+    },
+    label: "Resolved",
+    id: "contentReview_changeRequested__resolved",
+    type: "boolean",
+    validation: [],
+    fieldId: "resolved"
+});
+
+const changeRequestedCommentField = fields => ({
+    label: "Comments",
+    id: "contentReview_changeRequested_comment",
+    type: "object",
+    validation: [],
+    fieldId: "comments",
+    multipleValues: true,
+    settings: {
+        fields: fields,
+        layout: fields.map(field => [field.fieldId])
+    },
+    listValidation: [],
+    renderer: {
+        name: "objects"
+    }
+});
+
+const commentBodyField = () => ({
+    renderer: {
+        name: "rich-text-input"
+    },
+    label: "Body",
+    id: "contentReview_changeRequested_comment_body",
+    type: "rich-text",
+    validation: [],
+    fieldId: "body"
+});
+
+const commentAuthorField = () => ({
+    renderer: {
+        name: "text-input"
+    },
+    label: "Author",
+    id: "contentReview_changeRequested_comment_author",
+    type: "text",
+    validation: [
+        {
+            name: "required",
+            message: "Value is required."
+        }
+    ],
+    fieldId: "author"
+});
+
+const changeRequestedField = fields => ({
+    id: "contentReview_changeRequested",
+    fieldId: "changeRequested",
+    label: "Change Requested",
+    type: "object",
+    settings: {
+        fields: fields,
+        layout: fields.map(field => [field.fieldId])
+    },
+    listValidation: [],
+    validation: [],
+    multipleValues: true,
+    predefinedValues: {
+        values: [],
+        enabled: false
+    }
+});
+
 export const contentReviewModelDefinition = {
     name: "APW - Content Review",
     modelId: "apwContentReviewModelDefinition",
@@ -9,230 +225,16 @@ export const contentReviewModelDefinition = {
         ["contentReview_changeRequested"]
     ],
     fields: [
-        {
-            id: "contentReview_content",
-            fieldId: "content",
-            label: "Content",
-            type: "text",
-            settings: {},
-            listValidation: [],
-            validation: [],
-            multipleValues: false,
-            predefinedValues: {
-                values: [],
-                enabled: false
-            }
-        },
-        {
-            id: "contentReview_reviewRequestedBy",
-            fieldId: "reviewRequestedBy",
-            label: "Review requested by",
-            type: "text",
-            settings: {},
-            listValidation: [],
-            validation: [],
-            multipleValues: false,
-            predefinedValues: {
-                values: [],
-                enabled: false
-            }
-        },
-        {
-            id: "contentReview_steps",
-            label: "Steps",
-            type: "object",
-            settings: {
-                fields: [
-                    {
-                        multipleValues: false,
-                        listValidation: [],
-                        renderer: {
-                            name: "radio-buttons"
-                        },
-                        predefinedValues: {
-                            enabled: true,
-                            values: [
-                                {
-                                    value: "done",
-                                    label: "Steps done"
-                                },
-                                {
-                                    value: "active",
-                                    label: "Step active"
-                                },
-                                {
-                                    value: "inactive",
-                                    label: "Step inactive"
-                                }
-                            ]
-                        },
-                        label: "Status",
-                        id: "contentReview_steps_status",
-                        type: "text",
-                        validation: [
-                            {
-                                name: "required",
-                                message: "Value is required."
-                            }
-                        ],
-                        fieldId: "status"
-                    },
-                    {
-                        renderer: {
-                            name: "text-input"
-                        },
-                        label: "Slug",
-                        id: "contentReview_steps_slug",
-                        type: "text",
-                        validation: [
-                            {
-                                name: "required",
-                                message: "Value is required."
-                            }
-                        ],
-                        fieldId: "slug"
-                    }
-                ],
-                layout: [["contentReview_steps_status"], ["contentReview_steps_slug"]]
-            },
-            listValidation: [],
-            validation: [],
-            fieldId: "steps",
-            multipleValues: true,
-            predefinedValues: {
-                values: [],
-                enabled: false
-            }
-        },
-        {
-            id: "contentReview_changeRequested",
-            fieldId: "changeRequested",
-            label: "Change Requested",
-            type: "object",
-            settings: {
-                fields: [
-                    {
-                        renderer: {
-                            name: "text-input"
-                        },
-                        label: "Title",
-                        id: "contentReview_changeRequested_title",
-                        type: "text",
-                        validation: [
-                            {
-                                name: "required",
-                                message: "Value is required."
-                            }
-                        ],
-                        fieldId: "title"
-                    },
-                    {
-                        renderer: {
-                            name: "rich-text-input"
-                        },
-                        label: "Body",
-                        id: "contentReview_changeRequested_body",
-                        type: "rich-text",
-                        validation: [],
-                        fieldId: "body"
-                    },
-                    {
-                        renderer: {
-                            name: "file-input"
-                        },
-                        label: "Media",
-                        id: "contentReview_changeRequested_media",
-                        type: "file",
-                        validation: [],
-                        fieldId: "media"
-                    },
-                    {
-                        renderer: {
-                            name: "text-input"
-                        },
-                        label: "Step",
-                        id: "contentReview_changeRequested_step",
-                        type: "text",
-                        validation: [
-                            {
-                                name: "required",
-                                message: "Value is required."
-                            }
-                        ],
-                        fieldId: "step"
-                    },
-                    {
-                        renderer: {
-                            name: "boolean-input"
-                        },
-                        label: "Resolved",
-                        id: "contentReview_changeRequested__resolved",
-                        type: "boolean",
-                        validation: [],
-                        fieldId: "resolved"
-                    },
-                    {
-                        label: "Comments",
-                        id: "contentReview_changeRequested_comment",
-                        type: "object",
-                        validation: [],
-                        fieldId: "comments",
-                        multipleValues: true,
-                        settings: {
-                            fields: [
-                                {
-                                    renderer: {
-                                        name: "rich-text-input"
-                                    },
-                                    label: "Body",
-                                    id: "contentReview_changeRequested_comment_body",
-                                    type: "rich-text",
-                                    validation: [],
-                                    fieldId: "body"
-                                },
-                                {
-                                    renderer: {
-                                        name: "text-input"
-                                    },
-                                    label: "Author",
-                                    id: "contentReview_changeRequested_comment_author",
-                                    type: "text",
-                                    validation: [
-                                        {
-                                            name: "required",
-                                            message: "Value is required."
-                                        }
-                                    ],
-                                    fieldId: "author"
-                                }
-                            ],
-                            layout: [
-                                ["contentReview_changeRequested_comment_body"],
-                                ["contentReview_changeRequested_comment_author"]
-                            ]
-                        },
-                        listValidation: [],
-                        renderer: {
-                            name: "objects"
-                        }
-                    }
-                ],
-                layout: [
-                    ["contentReview_changeRequested_title"],
-                    ["UXN-contentReview_changeRequested_body"],
-                    ["contentReview_changeRequested_media"],
-                    ["contentReview_changeRequested_step"],
-                    ["contentReview_changeRequested_resolved"],
-                    ["contentReview_changeRequested_comment"]
-                ]
-            },
-            listValidation: [],
-            validation: [],
-            multipleValues: true,
-            predefinedValues: {
-                values: [],
-                enabled: false
-            }
-        }
+        contentField(),
+        requestedByField(),
+        stepsField([stepStatusField(), stepSlugField()]),
+        changeRequestedField([
+            changeRequestedTitleField(),
+            changeRequestedBodyField(),
+            changeRequestedMediaField(),
+            changeRequestedStepField(),
+            changeRequestedResolvedField(),
+            changeRequestedCommentField([commentBodyField(), commentAuthorField()])
+        ])
     ]
 };
