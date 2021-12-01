f1bac00db43a4ef7ac28c34eb86a04502f5ab966
ashu96 <bhardwajashu96@hotmail.com>
2021-12-01 17:58:56 +0530

    feat(api-apw): add support for value transform in generateFieldResolvers helper

--- packages/api-apw/src/utils/fieldResolver.ts
+++ packages/api-apw/src/utils/fieldResolver.ts
@@ -1,12 +1,40 @@
+import { entryFieldFromStorageTransform } from "@webiny/api-headless-cms/content/plugins/utils/entryStorage";
+import { FieldResolversParams } from "~/types";
+
 const resolveFieldValue = (parent, _, __, info) => {
     const { fieldName } = info;
     return parent.values[fieldName];
 };
 
-export const generateFieldResolvers = (fieldIds: string[]) => {
+const resolveFieldValueWithTransform =
+    ({ getModel, getField, isRoot }) =>
+    async (parent, _, context, info) => {
+        const { fieldName } = info;
+        const model = await getModel(context);
+
+        const field = getField(model, fieldName);
+
+        // Get transformed value (eg. data decompression)
+        return await entryFieldFromStorageTransform({
+            context,
+            model,
+            field,
+            value: isRoot ? parent.values[fieldName] : parent[fieldName]
+        });
+    };
+
+export const generateFieldResolvers = (fieldIds: Array<string | FieldResolversParams>) => {
     return fieldIds.reduce((previousValue, currentValue) => {
-        if (typeof previousValue[currentValue] !== "function") {
-            previousValue[currentValue] = resolveFieldValue;
+        const fieldId = typeof currentValue !== "string" ? currentValue.fieldId : currentValue;
+
+        if (typeof previousValue[fieldId] !== "function") {
+            if (typeof currentValue !== "string") {
+                previousValue[fieldId] = resolveFieldValueWithTransform({
+                    ...currentValue
+                });
+            } else {
+                previousValue[fieldId] = resolveFieldValue;
+            }
         }
         return previousValue;
     }, {});
