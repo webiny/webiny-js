3ec3e1ec4d8ce6dfe3510ecaa3f9322ff9c93c28
Bruno Zorić <bruno.zoric@gmail.com>
2021-12-02 13:47:19 +0100

    fix(app-headless-cms): date time with timezone validation (#2081)

--- packages/app-headless-cms/src/admin/plugins/transformers/dateTransformer.ts
+++ packages/app-headless-cms/src/admin/plugins/transformers/dateTransformer.ts
@@ -1,14 +1,117 @@
 import { CmsFieldValueTransformer } from "~/types";
 import WebinyError from "@webiny/error";
 
-const transformDate = (value: string, type: "date" | "datetime" | "time") => {
-    if (type === "date") {
-        return value.substr(0, 10);
-    }
-    return value;
+interface ThrowTransformErrorParams {
+    ex: WebinyError;
+    type: string;
+    value: string;
+}
+const throwTransformError = (params: ThrowTransformErrorParams): void => {
+    const { type, value, ex: error } = params;
+    throw new WebinyError(`Could not transform value to a date.`, "TRANSFORM_ERROR", {
+        error,
+        type,
+        value
+    });
 };
 
-const excludeTypesTransformation = ["time", "dateTimeWithTimezone"];
+const dateOnly = (value: string): string => {
+    try {
+        const date = new Date(value).toISOString();
+        return date.substr(0, 10);
+    } catch (ex) {
+        throwTransformError({
+            ex,
+            value,
+            type: "date"
+        });
+    }
+};
+
+const extractTimeZone = (value: string): [string, string] => {
+    let result: string[];
+    if (value.includes("+")) {
+        result = value.split("+");
+    } else {
+        result = value.split("-");
+    }
+    if (!result || result.length !== 2) {
+        throw new WebinyError(
+            `Could not determine time and timezone out of given value.`,
+            "TIME_ZONE_ERROR",
+            {
+                value
+            }
+        );
+    }
+    return result as [string, string];
+};
+
+const extractTime = (value: string): string => {
+    if (value.includes(":") === false) {
+        throw new WebinyError("Time value is missing : separators.", "TIME_ERROR", {
+            value
+        });
+    }
+    const result = value.split(":");
+    if (result.length === 3) {
+        return value;
+    } else if (result.length === 2) {
+        return `${value}:00`;
+    }
+};
+
+const dateTimeWithTimezone = (value: string): string => {
+    if (value.includes("T") === false) {
+        return value;
+    }
+
+    const [initialDate, initialTimeZone] = value.split("T");
+
+    let date: string;
+
+    try {
+        const dateObj = new Date(initialDate);
+        date = dateObj.toISOString().substr(0, 10);
+    } catch (ex) {
+        throw new WebinyError(
+            "Could not transform received date into Date object.",
+            "DATE_TRANSFORM_ERROR",
+            {
+                value,
+                date: initialDate,
+                time: initialTimeZone
+            }
+        );
+    }
+    const [initialTime] = extractTimeZone(initialTimeZone);
+
+    const time = extractTime(initialTime);
+
+    return value.replace(initialDate, date).replace(initialTime, time);
+};
+
+const dateTimeWithoutTimezone = (value: string): string => {
+    if (value.includes(" ") === false) {
+        return value;
+    }
+    try {
+        return new Date(`${value.replace(" ", "T")}.000Z`).toISOString();
+    } catch (ex) {
+        throwTransformError({
+            ex,
+            value,
+            type: "dateTimeWithoutTimezone"
+        });
+    }
+};
+
+const transformers = {
+    time: null,
+    date: dateOnly,
+    dateTimeWithoutTimezone,
+    dateTimeWithTimezone
+};
 
 export default (): CmsFieldValueTransformer => ({
     type: "cms-field-value-transformer",
@@ -20,22 +123,13 @@ export default (): CmsFieldValueTransformer => ({
         if (!value) {
             console.log(`Field "${field.fieldId}" has no value.`);
             return null;
-        } else if (excludeTypesTransformation.includes(type) === true) {
+        }
+        const transform = transformers[type];
+
+        if (!transform) {
             return value;
-        } else if (type === "dateTimeWithoutTimezone" && value.includes(" ")) {
-            /**
-             * Need to replace space and add .000Z to datetime value because it can look like: 2021-02-02 16:34:56
-             */
-            value = `${value.replace(" ", "T")}.000Z`;
         }
-        try {
-            const result = new Date(value).toISOString();
-            return transformDate(result, type);
-        } catch (ex) {
-            throw new WebinyError(`Could not transform "${value}" to a date.`, "TRANSFORM_ERROR", {
-                message: ex.message,
-                code: ex.code
-            });
-        }
+
+        return transform(value);
     }
 });
