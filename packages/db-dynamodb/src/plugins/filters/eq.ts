b3999e3fb80733b2f20e84d2732ad003c1400f4b
Bruno Zorić <bruno.zoric@gmail.com>
2021-12-03 10:05:22 +0100

    fix(api-headless-cms): multiple values in ref field when searching (#2090)

--- packages/db-dynamodb/src/plugins/filters/eq.ts
+++ packages/db-dynamodb/src/plugins/filters/eq.ts
@@ -3,6 +3,18 @@ import { ValueFilterPlugin } from "../definitions/ValueFilterPlugin";
 export default new ValueFilterPlugin({
     operation: "eq",
     matches: ({ value, compareValue }) => {
+        /**
+         * Possibility that either input value or one from the system is array.
+         */
+        if (Array.isArray(value) === true) {
+            return value.some(v => {
+                return Array.isArray(compareValue) ? compareValue.includes(v) : compareValue === v;
+            });
+        } else if (Array.isArray(compareValue) === true) {
+            return compareValue.every(v => {
+                return value === v;
+            });
+        }
         return value === compareValue;
     }
 });
