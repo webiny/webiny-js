b0c5dd9f5960ea7eb8975ec4d416e09ac0845e72
Bruno Zorić <bruno.zoric@gmail.com>
2021-12-06 13:32:34 +0100

    fix(db-dynamodb): batch read in chunks of max 100

--- packages/db-dynamodb/src/utils/batchRead.ts
+++ packages/db-dynamodb/src/utils/batchRead.ts
@@ -1,4 +1,6 @@
 import { Table } from "dynamodb-toolbox";
+import lodashChunk from "lodash/chunk";
+import WebinyError from "@webiny/error";
 
 interface Item {
     Table: Table;
@@ -9,6 +11,8 @@ interface Params {
     items: Item[];
 }
 
+const MAX_BATCH_ITEMS = 100;
+
 const flatten = (responses: Record<string, any[]>): any[] => {
     const entries = [];
     const values = Object.values(responses);
@@ -17,35 +21,66 @@ const flatten = (responses: Record<string, any[]>): any[] => {
     }
     return entries;
 };
+
+interface BatchReadAllChunkParams {
+    table: Table;
+    items: Item[];
+}
+const batchReadAllChunk = async <T = any>(params: BatchReadAllChunkParams): Promise<T[]> => {
+    const { table, items } = params;
+    const records: T[] = [];
+
+    const result = await table.batchGet(items);
+    if (!result || !result.Responses) {
+        return records;
+    }
+    records.push(...flatten(result.Responses));
+    if (!result.next || typeof result.next !== "function") {
+        return records;
+    }
+    let previous = result;
+    while (typeof previous.next === "function") {
+        const nextResult = await previous.next();
+        if (!nextResult) {
+            return records;
+        }
+        records.push(...flatten(nextResult.Responses));
+        previous = nextResult;
+    }
+    return records;
+};
 /**
  * This helper function is meant to be used to batch read from one table.
  * It will fetch all results, as there is a next() method call built in.
  */
-export const batchReadAll = async <T = any>(params: Params): Promise<T[]> => {
+export const batchReadAll = async <T = any>(
+    params: Params,
+    maxChunk = MAX_BATCH_ITEMS
+): Promise<T[]> => {
     if (params.items.length === 0) {
         return [];
-    }
-    const items: T[] = [];
-    const result = await params.table.batchGet(params.items);
-
-    if (result.Responses) {
-        items.push(...flatten(result.Responses));
-    }
-
-    if (typeof result.next === "function") {
-        let previous = result;
-        let nextResult;
-        while ((nextResult = await previous.next())) {
-            if (!nextResult) {
-                return items;
+    } else if (maxChunk > MAX_BATCH_ITEMS) {
+        throw new WebinyError(
+            `Cannot set to load more than ${MAX_BATCH_ITEMS} items from the DynamoDB at once.`,
+            "DYNAMODB_MAX_BATCH_GET_LIMIT_ERROR",
+            {
+                maxChunk
             }
-            items.push(...flatten(nextResult.Responses));
-            if (!nextResult || typeof nextResult.next !== "function") {
-                return items;
-            }
-            previous = nextResult;
-        }
+        );
     }
 
-    return items;
+    const records: T[] = [];
+
+    const chunkItemsList: Item[][] = lodashChunk(params.items, maxChunk);
+
+    for (const chunkItems of chunkItemsList) {
+        const results = await batchReadAllChunk<T>({
+            table: params.table,
+            items: chunkItems
+        });
+
+        records.push(...results);
+    }
+
+    return records;
 };
