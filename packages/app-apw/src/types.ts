db59521cdfe943a187a803494053a7c752e7bdd5
ashu96 <bhardwajashu96@hotmail.com>
2021-12-22 17:06:24 +0530

    feat(app-apw): add types

---
+++ packages/app-apw/src/types.ts
@@ -0,0 +1,44 @@
+export enum WorkflowScopeTypes {
+    DEFAULT = "default",
+    PB = "pb",
+    CMS = "cms"
+}
+
+export enum ApwContentReviewStepStatus {
+    DONE = "done",
+    ACTIVE = "active",
+    INACTIVE = "inactive"
+}
+
+export enum ApwWorkflowApplications {
+    PB = "pageBuilder",
+    CMS = "cms"
+}
+
+export interface ApwWorkflowScope {
+    type: WorkflowScopeTypes;
+    data: {
+        categories?: string[];
+        pages?: string[];
+        models?: string[];
+        entries?: string[];
+    };
+}
+
+export enum ApwWorkflowStepTypes {
+    MANDATORY_BLOCKING = "mandatoryBlocking",
+    MANDATORY_NON_BLOCKING = "mandatoryNonBlocking",
+    NON_MANDATORY = "notMandatory"
+}
+
+export enum ApwContentReviewStatus {
+    UNDER_REVIEW = "underReview",
+    READY_TO_BE_PUBLISHED = "readyToBePublished",
+    PUBLISHED = "published"
+}
+
+export interface ApwWorkflowStep {
+    title: string;
+    type: ApwWorkflowStepTypes;
+    reviewers: any[];
+}
