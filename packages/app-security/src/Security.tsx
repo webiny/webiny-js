40db68f61937f17ff754b173245dfccb33a44b6f
Pavel Denisjuk <pavel@webiny.com>
2022-01-02 13:39:50 +0100

    fix: resolve circular dep between packages

--- packages/app-security/src/Security.tsx
+++ packages/app-security/src/Security.tsx
@@ -1,4 +1,7 @@
-import { Provider } from "@webiny/app-admin";
+// Importing from `app-admin-core` and NOT from `app-admin`, to avoid circular dependency.
+// This can be resolved in a different way, by changing the location of `AppInstaller` component (currently in `app-admin`).
+// But this is a faster solution, as I'm really short on time :)
+import { Provider } from "@webiny/app-admin-core";
 import React from "react";
 import { SecurityProvider as ContextProvider } from "./contexts/Security";
 
