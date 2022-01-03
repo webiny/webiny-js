d1901bef13c96608b1924e56f9184384d8936e3b
Bruno Zorić <bruno.zoric@gmail.com>
2022-01-03 12:22:19 +0100

    fix(app-headless-cms): unique validator value param

--- packages/app-headless-cms/src/admin/plugins/fieldValidators/unique.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldValidators/unique.tsx
@@ -1,12 +1,14 @@
 import { CmsEditorFieldValidatorPlugin } from "~/types";
 
-export default {
-    type: "cms-editor-field-validator",
-    name: "cms-editor-field-validator-unique",
-    validator: {
-        name: "unique",
-        label: "Unique",
-        description: "You won't be able to submit the form if this field is not unique",
-        defaultMessage: "Value must be unique."
-    }
-} as CmsEditorFieldValidatorPlugin;
+export default (): CmsEditorFieldValidatorPlugin => {
+    return {
+        type: "cms-editor-field-validator",
+        name: "cms-editor-field-validator-unique",
+        validator: {
+            name: "unique",
+            label: "Unique",
+            description: "You won't be able to submit the form if this field is not unique",
+            defaultMessage: "Value must be unique."
+        }
+    };
+};
--- packages/app-headless-cms/src/admin/plugins/validators/unique.ts
+++ packages/app-headless-cms/src/admin/plugins/validators/unique.ts
@@ -1,16 +1,25 @@
 import { CmsModelFieldValidatorPlugin } from "~/types";
+import { validation } from "@webiny/validation";
+import ValidationError from "@webiny/validation/validationError";
 
 /**
  * In the UI we will pretend that field is unique.
  * When saving the entry, API call will check for the uniqueness.
  */
-export default {
-    type: "cms-model-field-validator",
-    name: "cms-model-field-validator-unique",
-    validator: {
-        name: "unique",
-        validate: async () => {
-            return true;
+export default (): CmsModelFieldValidatorPlugin => {
+    return {
+        type: "cms-model-field-validator",
+        name: "cms-model-field-validator-unique",
+        validator: {
+            name: "unique",
+            validate: async value => {
+                try {
+                    await validation.validate(value, "required");
+                } catch (ex) {
+                    throw new ValidationError("Value cannot be empty.", "unique", value);
+                }
+                return true;
+            }
         }
-    }
-} as CmsModelFieldValidatorPlugin;
+    };
+};
