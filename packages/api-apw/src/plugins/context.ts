30014a773555fa58fb77bafd2689519b95ff15e5
ashu96 <bhardwajashu96@hotmail.com>
2022-01-18 16:05:13 +0530

    refactor(api-apw): register cms related plugins in apw context setup

--- packages/api-apw/src/plugins/context.ts
+++ packages/api-apw/src/plugins/context.ts
@@ -3,10 +3,20 @@ import { ApwContentTypes, ApwContext, PageWithWorkflow } from "~/types";
 import { createApw } from "~/createApw";
 import apwHooks from "./hooks";
 import { createStorageOperations } from "~/storageOperations";
+import { createManageCMSPlugin } from "~/plugins/createManageCMSPlugin";
+import { createContentHeadlessCmsContext } from "@webiny/api-headless-cms";
 
 export default () => [
     new ContextPlugin<ApwContext>(async context => {
         const { tenancy, security, i18nContent } = context;
+
+        const contentHeadlessCmsContextPlugins = createContentHeadlessCmsContext({
+            storageOperations: context.cms.storageOperations
+        });
+        /**
+         * Register cms plugins required by `api-apw` package.
+         */
+        context.plugins.register([createManageCMSPlugin(), ...contentHeadlessCmsContextPlugins]);
 
         const getLocale = () => {
             // TODO: Check which locale do we need here?
