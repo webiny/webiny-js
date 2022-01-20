bbb69043dbcbb07a4da7872e964ad49cdd707497
ashu96 <bhardwajashu96@hotmail.com>
2022-01-20 17:24:14 +0530

    feat(api): add api-apw plugins in main graphQL lambda

--- api/code/graphql/src/index.ts
+++ api/code/graphql/src/index.ts
@@ -30,6 +30,7 @@ import { createStorageOperations as createHeadlessCmsStorageOperations } from "@
 import headlessCmsModelFieldToGraphQLPlugins from "@webiny/api-headless-cms/content/plugins/graphqlFields";
 import securityPlugins from "./security";
 import tenantManager from "@webiny/api-tenant-manager";
+import { createApwContext, createApwGraphQL } from "@webiny/api-apw";
 
 // Imports plugins created via scaffolding utilities.
 import scaffoldsPlugins from "./plugins/scaffolds";
@@ -90,6 +91,8 @@ export const handler = createHandler({
                 modelFieldToGraphQLPlugins: headlessCmsModelFieldToGraphQLPlugins()
             })
         }),
+        createApwGraphQL(),
+        createApwContext(),
         scaffoldsPlugins()
     ],
     http: { debug }
