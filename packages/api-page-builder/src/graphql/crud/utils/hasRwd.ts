78cdc3c830fcf5c21d174434d281f50c180435b3
Bruno Zorić <bruno.zoric@gmail.com>
2022-01-25 07:11:34 +0100

    feat: add types to api-page-builder

--- packages/api-page-builder/src/graphql/crud/utils/hasRwd.ts
+++ packages/api-page-builder/src/graphql/crud/utils/hasRwd.ts
@@ -1,4 +1,6 @@
-export default (permission, rwd) => {
+import { SecurityPermission } from "@webiny/api-security/types";
+
+export default (permission: SecurityPermission, rwd?: string) => {
     if (typeof permission.rwd !== "string") {
         return true;
     }
