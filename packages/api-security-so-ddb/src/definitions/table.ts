db88f62da07d1eff437b9a5134a5d87e4217abd8
Bruno Zorić <bruno.zoric@gmail.com>
2022-01-25 10:16:00 +0100

    feat: add types to api-security-so-ddb

--- packages/api-security-so-ddb/src/definitions/table.ts
+++ packages/api-security-so-ddb/src/definitions/table.ts
@@ -1,12 +1,12 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { Table } from "dynamodb-toolbox";
 
-export interface Params {
+export interface CreateTableParams {
     table: string;
     documentClient: DocumentClient;
 }
 
-export const createTable = ({ table, documentClient }: Params) => {
+export const createTable = ({ table, documentClient }: CreateTableParams) => {
     return new Table({
         name: table || process.env.DB_TABLE,
         partitionKey: "PK",
