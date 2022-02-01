bb997794d5754116fcf320947beb381f1b38741f
ashu96 <bhardwajashu96@hotmail.com>
2022-02-01 18:01:07 +0530

    refactor(api-apw): remove usage of getWorkflowGetter

--- packages/api-apw/src/plugins/hooks/initializeContentReviewSteps.ts
+++ packages/api-apw/src/plugins/hooks/initializeContentReviewSteps.ts
@@ -1,30 +1,14 @@
 import lodashSet from "lodash/set";
-import { AdvancedPublishingWorkflow, ApwContentTypes, LifeCycleHookCallbackParams } from "~/types";
+import { LifeCycleHookCallbackParams } from "~/types";
 import { getContentReviewStepInitialStatus } from "~/plugins/utils";
 import { NotFoundError } from "@webiny/handler-graphql";
-
-export const getWorkflowIdFromContent = async (
-    apw: AdvancedPublishingWorkflow,
-    params: { type: ApwContentTypes; id: string; settings: Record<string, any> }
-): Promise<string> => {
-    switch (params.type) {
-        case ApwContentTypes.PAGE:
-            const getWorkflowFromPage = apw.getWorkflowGetter(ApwContentTypes.PAGE);
-            return getWorkflowFromPage(params.id, {});
-
-        case ApwContentTypes.CMS_ENTRY:
-            const getWorkflowFromCmsEntry = apw.getWorkflowGetter(ApwContentTypes.CMS_ENTRY);
-            return getWorkflowFromCmsEntry(params.id, params.settings);
-    }
-    return null;
-};
 
 export const initializeContentReviewSteps = ({ apw }: LifeCycleHookCallbackParams) => {
     apw.contentReview.onBeforeContentReviewCreate.subscribe(async ({ input }) => {
         /**
          * Let's initialize the "ContentReview" steps.
          */
-        const workflowId = await getWorkflowIdFromContent(apw, input.content);
+        const { workflowId } = input.content;
 
         if (!workflowId) {
             throw new NotFoundError(`Unable to initiate a "Content review". No workflow found!`);
