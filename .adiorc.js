80904af4b927f5d11a4621a665caae665ef4f778
Ewan <jojothegreat694@hotmail.com>
2022-02-03 15:10:51 -0500

    chore: add cwd to function input

--- .adiorc.js
+++ .adiorc.js
@@ -25,6 +25,7 @@ module.exports = {
             "http",
             "path",
             "https",
+            "follow-redirects",
             "os",
             "fs",
             "util",
--- packages/project-utils/bundling/function/telemetryFunction.js
+++ packages/project-utils/bundling/function/telemetryFunction.js
@@ -108,7 +108,7 @@ async function handler(args) {
 }
 
 module.exports = {
-  handler,
-  localData,
-  postTelemetryData
+    handler,
+    localData,
+    postTelemetryData
 };
