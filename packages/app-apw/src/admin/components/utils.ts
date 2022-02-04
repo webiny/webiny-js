c4b5a634f16a005b1ee178fbb51e0b77a7a3a151
ashu96 <bhardwajashu96@hotmail.com>
2022-02-04 12:24:50 +0530

    feat(app-apw): add dayjs helper methods

--- packages/app-apw/src/admin/components/utils.ts
+++ packages/app-apw/src/admin/components/utils.ts
@@ -1,4 +1,18 @@
 import { customAlphabet } from "nanoid";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import relativeTime from "dayjs/plugin/relativeTime";
+// Extend dayjs format.
+dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
 
 const ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 export const getNanoid = customAlphabet(ALPHANUMERIC, 10);
+
+export const formatDate = (date: string): string => {
+    return dayjs(date).format("MMM Do, YYYY");
+};
+
+export const fromNow = (date: string): string => {
+    return dayjs(date).fromNow();
+};
