be5206b53e79f2befb1ba987ccb375b4507abb3f
ashu96 <bhardwajashu96@hotmail.com>
2022-02-08 19:30:20 +0530

    fix(api-apw): add existing content review check in beforePagePublish hook
    remove onAfterPageRequestReview usage

--- packages/api-apw/src/plugins/pageBuilder/triggerContentReview.ts
+++ packages/api-apw/src/plugins/pageBuilder/triggerContentReview.ts
@@ -1,50 +1,34 @@
 import get from "lodash/get";
 import Error from "@webiny/error";
-import {
-    ApwContentTypes,
-    ApwOnBeforePagePublishTopicParams,
-    ApwOnBeforePageRequestReviewTopicParams
-} from "~/types";
+import { ApwOnBeforePagePublishTopicParams } from "~/types";
 import { InitiateContentReviewParams } from ".";
 
-export default ({ pageBuilder, apw }: InitiateContentReviewParams) => {
+export default ({ pageBuilder }: InitiateContentReviewParams) => {
     pageBuilder.onBeforePagePublish.subscribe<ApwOnBeforePagePublishTopicParams>(
         async ({ page }) => {
+            const contentReviewId = get(page, "settings.apw.contentReviewId");
+            if (contentReviewId) {
+                throw new Error(
+                    `A peer review for this content has been already requested.`,
+                    "REVIEW_ALREADY_EXIST",
+                    {
+                        contentReviewId,
+                        page
+                    }
+                );
+            }
+
             const workflowId = get(page, "settings.apw.workflowId");
 
             if (workflowId) {
                 throw new Error(
-                    "This content requires peer review approval before it can be published",
+                    "This content requires peer review approval before it can be published.",
                     "REVIEW_REQUIRED",
                     {
                         workflowId,
                         page
                     }
                 );
-            }
-        }
-    );
-
-    pageBuilder.onAfterPageRequestReview.subscribe<ApwOnBeforePageRequestReviewTopicParams>(
-        async ({ page }) => {
-            /*
-             * TODO: @ashutosh add check to determine if a content review already exist for requested page.
-             *  If it is so, terminate with an "ALREADY_EXISTS" error.
-             */
-
-            const contentReview = await apw.contentReview.create({
-                content: {
-                    id: page.id,
-                    type: ApwContentTypes.PAGE,
-                    settings: null
-                }
-            });
-            /**
-             * As we don't want the "RequestReview" process to continue,
-             * we're using error in this lifecycle hook as a mechanism of flow control.
-             */
-            if (contentReview) {
-                throw new Error(`A content review has been initiated for page "${page.id}"`);
             }
         }
     );
