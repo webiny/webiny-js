625d41fe625ed07a1b9ed544f7dbc61c8450c348
Bruno Zorić <bruno.zoric@gmail.com>
2022-02-09 10:18:23 +0100

    fix(api-prerendering-service): possible undefined json stringify

--- packages/api-prerendering-service/src/render/injectApolloState.ts
+++ packages/api-prerendering-service/src/render/injectApolloState.ts
@@ -3,8 +3,8 @@ export default ({ render }) =>
         console.log("Injecting Apollo state into HTML.");
 
         tree.match({ tag: "head" }, node => {
-            const script = `<script>window.__APOLLO_STATE__ = ${JSON.stringify(
-                render.meta.apolloState || {}
+            const script = `<script>window.__APOLLO_STATE__ = ${(
+                JSON.stringify(render.meta.apolloState) || ""
             ).replace(/</g, "\\u003c")};</script>`;
 
             node.content.push(script);
