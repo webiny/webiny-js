fdda24823d6051d249d7f5e2512076f32cbea49a
Bruno Zorić <bruno.zoric@gmail.com>
2022-02-09 10:43:56 +0100

    fix(handler): export context params (#2197)

--- packages/api-prerendering-service/src/render/injectApolloState.ts
+++ packages/api-prerendering-service/src/render/injectApolloState.ts
@@ -1,11 +1,24 @@
+const stringifyApolloState = (state?: any): string | undefined => {
+    if (state === undefined || state === null) {
+        return state;
+    }
+    try {
+        return JSON.stringify(state).replace(/</g, "\\u003c");
+    } catch (ex) {
+        console.log("Could not stringify state.");
+        console.log(ex.message);
+    }
+    return undefined;
+};
+
 export default ({ render }) =>
     async tree => {
         console.log("Injecting Apollo state into HTML.");
 
         tree.match({ tag: "head" }, node => {
-            const script = `<script>window.__APOLLO_STATE__ = ${JSON.stringify(
+            const script = `<script>window.__APOLLO_STATE__ = ${stringifyApolloState(
                 render.meta.apolloState
-            ).replace(/</g, "\\u003c")};</script>`;
+            )};</script>`;
 
             node.content.push(script);
             return node;
