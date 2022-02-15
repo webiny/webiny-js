761f9112a81a62dc7a393ac697b28b1e2cb09f9f
Adrian Smijulj <adrian1358@gmail.com>
2022-02-15 16:19:13 +0100

    fix: ensure `null` values are returned

--- packages/api-authentication-cognito/src/index.ts
+++ packages/api-authentication-cognito/src/index.ts
@@ -15,17 +15,17 @@ export interface Config extends CognitoConfig {
 export const getIdentity: GetIdentity = ({ identityType, token }) => {
     // We ensure `undefined` doesn't end up in the `displayName` property.
     // If first name and last name is not present, we end up with an empty string.
-    const firstName = token.given_name || "";
-    const lastName = token.family_name || "";
-    const displayName = `${firstName} ${lastName}`.trim();
+    const { given_name = null, family_name = null, id = null, email = null } = token;
+
+    const displayName = [given_name, family_name].filter(Boolean).join(" ").trim() || null;
 
     return {
-        id: token.sub,
+        id,
         type: identityType,
         displayName,
-        email: token.email,
-        firstName,
-        lastName
+        email,
+        firstName: given_name,
+        lastName: family_name
     };
 };
 
