548012b0d9c31c1e97ae3e2c8b52dbb1904663cd
Adrian Smijulj <adrian1358@gmail.com>
2022-02-15 16:00:01 +0100

    fix: create and export the default `getIdentity` function

--- packages/api-authentication-cognito/src/index.ts
+++ packages/api-authentication-cognito/src/index.ts
@@ -12,6 +12,23 @@ export interface Config extends CognitoConfig {
     getIdentity?: GetIdentity;
 }
 
+export const getIdentity: GetIdentity = ({ identityType, token }) => {
+    // We ensure `undefined` doesn't end up in the `displayName` property.
+    // If first name and last name is not present, we end up with an empty string.
+    const firstName = token.given_name || "";
+    const lastName = token.family_name || "";
+    const displayName = `${firstName} ${lastName}`.trim();
+
+    return {
+        id: token.sub,
+        type: identityType,
+        displayName,
+        email: token.email,
+        firstName,
+        lastName
+    };
+};
+
 export default (config: Config) => {
     const cognitoAuthenticator = createAuthenticator({
         region: config.region,
@@ -30,20 +47,7 @@ export default (config: Config) => {
                 return config.getIdentity({ identityType: config.identityType, token: tokenObj });
             }
 
-            // We ensure `undefined` doesn't end up in the `displayName` property.
-            // If first name and last name is not present, we end up with an empty string.
-            const firstName = tokenObj.given_name || "";
-            const lastName = tokenObj.family_name || "";
-            const displayName = `${firstName} ${lastName}`.trim();
-
-            return {
-                id: tokenObj.sub,
-                type: config.identityType,
-                displayName,
-                email: tokenObj.email,
-                firstName,
-                lastName
-            };
+            return getIdentity({ identityType: config.identityType, token: tokenObj });
         });
     });
 };
