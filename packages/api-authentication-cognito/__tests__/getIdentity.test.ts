b1044335d84474dfab6124f0613ee5668c54d2ef
Adrian Smijulj <adrian1358@gmail.com>
2022-02-15 16:21:01 +0100

    test: ensure newly exported `getIdentity` works as expected

---
+++ packages/api-authentication-cognito/__tests__/getIdentity.test.ts
@@ -0,0 +1,80 @@
+import { getIdentity } from "@webiny/api-authentication-cognito";
+
+describe("getIdentity Test", () => {
+    test("full identity information should be returned correctly", async () => {
+        expect(
+            getIdentity({
+                token: {
+                    id: "xyz",
+                    family_name: "Family",
+                    given_name: "Given",
+                    email: "family@given.com"
+                },
+                identityType: "user"
+            })
+        ).toEqual({
+            displayName: "Given Family",
+            email: "family@given.com",
+            firstName: "Given",
+            id: "xyz",
+            lastName: "Family",
+            type: "user"
+        });
+    });
+
+    test(`we must not have "undefined" string in any of the identity properties`, async () => {
+        expect(
+            getIdentity({
+                token: {},
+                identityType: "user"
+            })
+        ).toEqual({
+            displayName: null,
+            email: null,
+            firstName: null,
+            id: null,
+            lastName: null,
+            type: "user"
+        });
+    });
+
+    test(`when constructing displayName, if lastName is missing, only use firstName and vice versa`, async () => {
+        // Last name (family_name) missing - use only first name (given_name).
+        expect(
+            getIdentity({
+                token: {
+                    id: "xyz",
+                    given_name: "Given",
+                    email: "family@given.com"
+                },
+                identityType: "user"
+            })
+        ).toEqual({
+            displayName: "Given",
+            email: "family@given.com",
+            firstName: "Given",
+            id: "xyz",
+            lastName: null,
+            type: "user"
+        });
+
+        // First name (given_name) missing - use only last name (family_name).
+        expect(
+            getIdentity({
+                token: {
+                    id: "xyz",
+                    family_name: "Family",
+                    email: "family@given.com"
+                },
+                identityType: "user"
+            })
+        ).toEqual({
+            displayName: "Family",
+            email: "family@given.com",
+            firstName: null,
+            id: "xyz",
+            lastName: "Family",
+            type: "user"
+        });
+    });
+});
