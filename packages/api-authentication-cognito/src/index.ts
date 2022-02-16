db15576a284814e6d9aca17d2a425cd9da041c75
Adrian Smijulj <adrian1358@gmail.com>
2022-02-16 21:52:47 +0100

    fix: read `id` from `sub` token property

--- packages/api-authentication-cognito/src/index.ts
+++ packages/api-authentication-cognito/src/index.ts
@@ -15,7 +15,7 @@ export interface Config extends CognitoConfig {
 export const getIdentity: GetIdentity = ({ identityType, token }) => {
     // We ensure `undefined` doesn't end up in the `displayName` property.
     // If first name and last name is not present, we end up with an empty string.
-    const { given_name = null, family_name = null, id = null, email = null } = token;
+    const { given_name = null, family_name = null, sub: id = null, email = null } = token;
 
     const displayName = [given_name, family_name].filter(Boolean).join(" ").trim() || null;
 
