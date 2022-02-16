f6b00a121500b003b1f3f726511ebcece9f8798a
Pavel Denisjuk <pavel@webiny.com>
2022-02-16 09:01:55 +0100

    fix(ui): override rmwc/base package

---
+++ packages/ui/rmwc/base/code/utils/strings.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toCamel = function (str) {
+    return str.replace(/(-[a-z])/g, function ($1) {
+        return $1.toUpperCase().replace("-", "");
+    });
+};
+exports.toDashCase = function (str) {
+    return str.replace(/([A-Z])/g, function ($1) {
+        return "-" + $1.toLowerCase();
+    });
+};
