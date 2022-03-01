d916d4521d8fdf918492ca2069d86ffa0af554cc
ashu96 <bhardwajashu96@hotmail.com>
2022-03-01 16:01:07 +0530

    feat(db-dynamodb): add startsWithFilter
    update tests

---
+++ packages/db-dynamodb/src/plugins/filters/startsWith.ts
@@ -0,0 +1,20 @@
+import { ValueFilterPlugin } from "../definitions/ValueFilterPlugin";
+
+export default new ValueFilterPlugin({
+    operation: "startsWith",
+    matches: ({ value, compareValue }) => {
+        /**
+         * We do "case-insensitive" comparison.
+         */
+        const compareValueInLowerCase = compareValue.toLowerCase();
+
+        if (typeof value !== "string") {
+            if (Array.isArray(value) === true) {
+                return value.some(v => v.toLowerCase().startsWith(compareValueInLowerCase));
+            }
+            return false;
+        }
+
+        return value.toLowerCase().startsWith(compareValueInLowerCase);
+    }
+});
