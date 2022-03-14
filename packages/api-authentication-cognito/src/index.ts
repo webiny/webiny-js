1b4fe5e0e532b40be14714067b7281e8840a6327
Pavel Denisjuk <pavel@webiny.com>
2022-03-14 18:19:41 +0100

    fix(api-authentication-cognito): use preferred_username if exists

--- packages/api-authentication-cognito/src/index.ts
+++ packages/api-authentication-cognito/src/index.ts
@@ -15,7 +15,13 @@ export interface Config extends CognitoConfig {
 export const getIdentity: GetIdentity = ({ identityType, token }) => {
     // We ensure `undefined` doesn't end up in the `displayName` property.
     // If first name and last name is not present, we end up with an empty string.
-    const { given_name = null, family_name = null, sub: id = null, email = null } = token;
+    const {
+        given_name = null,
+        family_name = null,
+        sub: id = null,
+        email = null,
+        preferred_username = null
+    } = token;
 
     const displayName = [given_name, family_name].filter(Boolean).join(" ").trim() || null;
 
@@ -23,7 +29,7 @@ export const getIdentity: GetIdentity = ({ identityType, token }) => {
         id,
         type: identityType,
         displayName,
-        email,
+        email: preferred_username || email,
         firstName: given_name,
         lastName: family_name
     };
