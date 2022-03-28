05af13dead665f7f36c2788b2a78e4b9dddc7eed
Bruno Zorić <bruno.zoric@gmail.com>
2022-03-28 09:04:49 +0200

    test(api-headless-cms): do not run cms hooks on options request

--- packages/api-headless-cms/__tests__/contentAPI/httpOptions.test.ts
+++ packages/api-headless-cms/__tests__/contentAPI/httpOptions.test.ts
@@ -1,7 +1,20 @@
+import { ContextPlugin } from "@webiny/handler";
+import { CmsContext } from "~/types";
 import { useCategoryManageHandler } from "../utils/useCategoryManageHandler";
 
 describe("HTTP Options request", () => {
-    const manageOpts = { path: "manage/en-US" };
+    const manageOpts = {
+        path: "manage/en-US",
+        plugins: [
+            new ContextPlugin(async context => {
+                context.waitFor<CmsContext>(["cms"], ctx => {
+                    ctx.cms.onBeforeGroupCreate.subscribe(async () => {
+                        throw new Error("This should not fire or register.");
+                    });
+                });
+            })
+        ]
+    };
 
     test(`http options`, async () => {
         const { invoke } = useCategoryManageHandler(manageOpts);
