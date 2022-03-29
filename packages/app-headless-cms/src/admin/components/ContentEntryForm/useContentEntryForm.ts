c92681308f6b0b27a1be4aeb323848647b7ed4a4
Bruno Zorić <bruno.zoric@gmail.com>
2022-03-29 14:42:31 +0200

    fix(app-headless-cms): memoize default values so form does not rerender

--- packages/app-headless-cms/src/admin/components/ContentEntryForm/useContentEntryForm.ts
+++ packages/app-headless-cms/src/admin/components/ContentEntryForm/useContentEntryForm.ts
@@ -279,65 +279,69 @@ export function useContentEntryForm(params: UseContentEntryFormParams): UseConte
         return createContentFrom(entry.id, gqlData);
     };
 
-    const getDefaultValues = (overrides: Record<string, any> = {}): Record<string, any> => {
-        const values: Record<string, any> = {};
-        /**
-         * Assign the default values:
-         * * check the settings.defaultValue
-         * * check the predefinedValues for selected value
-         */
-        for (const field of contentModel.fields) {
+    const defaultValues = useMemo(
+        (overrides: Record<string, any> = {}): Record<string, any> => {
+            const values: Record<string, any> = {};
             /**
-             * When checking if defaultValue is set in settings, we do the undefined check because it can be null, 0, empty string, false, etc...
+             * Assign the default values:
+             * * check the settings.defaultValue
+             * * check the predefinedValues for selected value
              */
-            const { settings, multipleValues = false } = field;
-            if (settings && settings.defaultValue !== undefined) {
+            for (const field of contentModel.fields) {
                 /**
-                 * Special type of field is the boolean one.
-                 * We MUST set true/false for default value.
+                 * When checking if defaultValue is set in settings, we do the undefined check because it can be null, 0, empty string, false, etc...
                  */
-                values[field.fieldId] = convertDefaultValue(field, settings.defaultValue);
-                continue;
-            }
-            /**
-             * No point in going further if predefined values are not enabled.
-             */
-            const { predefinedValues } = field;
-            if (
-                !predefinedValues ||
-                !predefinedValues.enabled ||
-                Array.isArray(predefinedValues.values) === false
-            ) {
-                continue;
-            }
-            /**
-             * When field is not a multiple values one, we find the first possible default selected value and set it as field value.
-             */
-            if (!multipleValues) {
-                const selectedValue = predefinedValues.values.find(({ selected }) => {
-                    return !!selected;
-                });
-                if (selectedValue) {
-                    values[field.fieldId] = convertDefaultValue(field, selectedValue.value);
+                const { settings, multipleValues = false } = field;
+                if (settings && settings.defaultValue !== undefined) {
+                    /**
+                     * Special type of field is the boolean one.
+                     * We MUST set true/false for default value.
+                     */
+                    values[field.fieldId] = convertDefaultValue(field, settings.defaultValue);
+                    continue;
                 }
-                continue;
+                /**
+                 * No point in going further if predefined values are not enabled.
+                 */
+                const { predefinedValues } = field;
+                if (
+                    !predefinedValues ||
+                    !predefinedValues.enabled ||
+                    Array.isArray(predefinedValues.values) === false
+                ) {
+                    continue;
+                }
+                /**
+                 * When field is not a multiple values one, we find the first possible default selected value and set it as field value.
+                 */
+                if (!multipleValues) {
+                    const selectedValue = predefinedValues.values.find(({ selected }) => {
+                        return !!selected;
+                    });
+                    if (selectedValue) {
+                        values[field.fieldId] = convertDefaultValue(field, selectedValue.value);
+                    }
+                    continue;
+                }
+                /**
+                 *
+                 */
+                values[field.fieldId] = predefinedValues.values
+                    .filter(({ selected }) => !!selected)
+                    .map(({ value }) => {
+                        return convertDefaultValue(field, value);
+                    });
             }
-            /**
-             *
-             */
-            values[field.fieldId] = predefinedValues.values
-                .filter(({ selected }) => !!selected)
-                .map(({ value }) => {
-                    return convertDefaultValue(field, value);
-                });
-        }
-        return { ...values, ...overrides };
-    };
+            return { ...values, ...overrides };
+        },
+        [contentModel.modelId]
+    );
+
     return {
         /**
          * If entry is not set or entry.id does not exist, it means that form is for the new entry, so fetch default values.
          */
-        data: entry && entry.id ? entry : getDefaultValues(),
+        data: entry && entry.id ? entry : defaultValues,
         loading,
         setLoading,
         onChange,
