2adafd06f3da186b4af3dd4a8951e1cee263db15
Ewan <jojothegreat694@hotmail.com>
2022-03-30 15:30:39 -0500

    fix: update tests to work with refactored telemetryFunction

--- packages/project-utils/__tests__/bundling/function/telemetryFunction.test.ts
+++ packages/project-utils/__tests__/bundling/function/telemetryFunction.test.ts
@@ -2,7 +2,6 @@ import * as fs from "fs";
 import { getProject } from "@webiny/cli/utils";
 import { getTelemetryFunctionDownloadPath } from "../../../bundling/function/utils";
 import { downloadTelemetryFunction } from "../../../bundling/function/telemetry";
-import { sleep } from "../../../testing/helpers/sleep";
 import * as path from "path";
 
 // This environment API key has been previously created via a test WCP account created at https://app.webiny.com. Consult internal documentation for more information on the used account.
@@ -27,7 +26,7 @@ interface TelemetryDataResult {
     };
 }
 let postTelemetryData: (data: TelemetryData) => Promise<TelemetryDataResult>;
-let localData: TelemetryData;
+let telemetryData: TelemetryData;
 let handler: () => Promise<any>;
 
 const handlerPath = path.join(getProject().root, ".webiny", "_handler.js");
@@ -40,14 +39,25 @@ beforeAll(async () => {
 beforeEach(() => {
     // The telemetry function is being injected into the folder where the clients handler function is, so it
     // expects there to be a _hander.js file to be in the folder it is placed in, so lets mock a _handler.js
-    // file in this test folder so it wont throw an error it is not important what is in the file so lets put in an
-    // empty string
-    fs.writeFileSync(handlerPath, "");
+    // file in this test folder so it wont throw an error it is not important what is in function just that it
+    // returns something so lets return true
+    fs.writeFileSync(
+        handlerPath,
+        `
+        function handler() {
+            return true;
+        }
+
+        module.exports = {
+            handler
+        }
+    `
+    );
 
     // Now we can export the telemetry functions
     const telemetry = require(getTelemetryFunctionDownloadPath());
     postTelemetryData = telemetry.postTelemetryData;
-    localData = telemetry.localData;
+    telemetryData = telemetry.telemetryData;
     handler = telemetry.handler;
 });
 
@@ -107,28 +117,22 @@ describe("Telemetry functions", () => {
 
             await Promise.all(thousandHandlerFunctions);
 
-            expect(localData.logs.length).toEqual(0);
+            expect(telemetryData.logs.length).toEqual(0);
         });
 
         test("Posts telemetry if it has been more than 5 minutes since data was sent", async () => {
-            const thousandHandlerFunctions = Array.from({ length: 5 }).map(() => handler());
-
-            // Load 5 logs into the telemetry data
-            await Promise.all(thousandHandlerFunctions);
-
-            expect(localData.logs.length).toEqual(5);
+            await handler();
 
             const minutesToFireRequest = 5;
-            const timeInFiveMinutes = Date.now() + minutesToFireRequest * 60000;
+            const timeInFiveMinutesAndOneMillisecond = Date.now() + minutesToFireRequest * 60001;
 
-            // Set time forward 5 minutes
-            Date.now = jest.fn(() => timeInFiveMinutes);
+            // Set time forward 5 minutes and one millisecond
+            Date.now = jest.fn(() => timeInFiveMinutesAndOneMillisecond);
 
-            // Wait a second to let the function fire if 5 minutes have passed
-            await sleep(2000);
+            await handler();
 
             // The timer should have fired and clears the logs
-            expect(localData.logs.length).toEqual(0);
+            expect(telemetryData.logs.length).toEqual(0);
 
             jest.clearAllMocks();
         });
