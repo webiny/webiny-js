d068a320ecf849cbaf70cde680d2f191864b82c9
Adrian Smijulj <adrian1358@gmail.com>
2022-03-31 10:02:47 +0200

    feat: add new utils

--- packages/cli/commands/wcp/utils/index.js
+++ packages/cli/commands/wcp/utils/index.js
@@ -4,11 +4,17 @@ const WCP_API_URL = process.env.WCP_API_URL || "https://api.wcp.webiny.com/graph
 const { getUser } = require("./getUser");
 const { getProjectEnvironmentBySlug } = require("./getProjectEnvironmentBySlug");
 const { updateUserLastActiveOn } = require("./updateUserLastActiveOn");
+const { setProjectId } = require("./setProjectId");
+const { setWcpPat } = require("./setWcpPat");
+const { getWcpPat } = require("./getWcpPat");
 
 module.exports = {
     getUser,
     getProjectEnvironmentBySlug,
     updateUserLastActiveOn,
+    setProjectId,
+    setWcpPat,
+    getWcpPat,
     WCP_APP_URL,
     WCP_API_URL
 };
