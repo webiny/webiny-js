677f31640d791ff98f3aee188ff2a0dd5bbf53f5
Bruno Zorić <bruno.zoric@gmail.com>
2022-03-31 10:24:01 +0200

    fix(api-form-builder): wrong partition key when deleting (#2295)

--- packages/api-page-builder/src/graphql/crud/pages.crud.ts
+++ packages/api-page-builder/src/graphql/crud/pages.crud.ts
@@ -1359,6 +1359,7 @@ export const createPageCrud = (params: CreatePageCrudParams): PagesCrud => {
                         tenant: getTenantId(),
                         locale: getLocaleCode()
                     },
+                    sort: ["version_ASC"],
                     /**
                      * Let's hope there will be no more than 10000 revisions.
                      * Need to implement "after" option if required.
