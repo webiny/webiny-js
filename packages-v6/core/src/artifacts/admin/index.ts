bd4b0d603bb18343602565c812b5852c15f19444
Pavel Denisjuk <pavel@webiny.com>
2022-04-06 17:24:29 +0200

    fix(v6): use type predicates to filter config modifiers

--- packages-v6/core/src/artifacts/admin/index.ts
+++ packages-v6/core/src/artifacts/admin/index.ts
@@ -4,16 +4,51 @@ import { createMorphProject, getSourceFile } from "../../utils";
 import { useWebiny } from "../../webiny";
 import { injectSourceFromPlugins } from "./injectSourceFromPlugins";
 import { BabelConfigModifier, WebpackConfigModifier } from "../bundle/config/webpack.config";
-// import { bundle } from "./bundle";
+import { Plugin, PluginAdminConfig } from "~/definePlugin";
 
 interface Options {
     watch: boolean;
 }
 
+interface AdminPlugin extends Plugin {
+    admin: PluginAdminConfig;
+}
+
+interface AdminConfigWithBabel extends PluginAdminConfig {
+    babel: BabelConfigModifier;
+}
+
+interface AdminConfigWithWebpack extends PluginAdminConfig {
+    webpack: WebpackConfigModifier;
+}
+
+interface AdminBabelPlugin extends AdminPlugin {
+    admin: AdminConfigWithBabel;
+}
+
+interface AdminWebpackPlugin extends AdminPlugin {
+    admin: AdminConfigWithWebpack;
+}
+
+function isAdminPlugin(plugin: Plugin | AdminPlugin): plugin is AdminPlugin {
+    return plugin.admin !== undefined;
+}
+
+function hasBabelModifier(plugin: AdminPlugin | AdminBabelPlugin): plugin is AdminBabelPlugin {
+    return plugin.admin.babel !== undefined;
+}
+
+function hasWebpackModifier(
+    plugin: AdminPlugin | AdminWebpackPlugin
+): plugin is AdminWebpackPlugin {
+    return plugin.admin.webpack !== undefined;
+}
+
 export async function buildAdmin({ watch }: Options) {
     const webiny = useWebiny();
     const outputPath = webiny.getOutputPath();
-    const plugins = webiny.getPlugins().filter(pl => "admin" in pl);
+    const adminPlugins = webiny.getPlugins().filter(isAdminPlugin);
+
     const generatedRoot = path.join(outputPath, "generated", "admin");
     const IndexJs = path.resolve(generatedRoot, "src", "index.tsx");
     const AppJs = path.resolve(generatedRoot, "src", "App.tsx");
@@ -24,35 +59,44 @@ export async function buildAdmin({ watch }: Options) {
 
     const project = createMorphProject([AppJs]);
 
-    const source = getSourceFile(project, AppJs)!;
-    injectSourceFromPlugins(source, plugins);
+    const source = getSourceFile(project, AppJs);
+    if (!source) {
+        throw Error("Missing admin App.tsx file.");
+    }
+
+    injectSourceFromPlugins(source, adminPlugins);
     await project.save();
 
     // Switch to the directory where the generated code is located.
     process.chdir(generatedRoot);
 
     const babelConfigModifier: BabelConfigModifier = config => {
-        return plugins
-            .filter(plugin => "babel" in plugin.admin!)
-            .reduce((config, plugin) => plugin.admin!.babel!(config), config);
+        return adminPlugins
+            .filter(hasBabelModifier)
+            .map(plugin => plugin.admin.babel)
+            .reduce((config, modifier) => modifier(config), config);
     };
 
     const webpackConfigModifier: WebpackConfigModifier = config => {
-        return plugins
-            .filter(plugin => "webpack" in plugin.admin!)
-            .reduce((config, plugin) => plugin.admin!.webpack!(config), config);
+        return adminPlugins
+            .filter(hasWebpackModifier)
+            .map(plugin => plugin.admin.webpack)
+            .reduce((config, modifier) => modifier(config), config);
     };
 
     if (watch) {
         // Watch the app!
         const { watchApp } = await import("../bundle/watchApp");
 
-        return watchApp({
-            cwd: generatedRoot,
-            entry: IndexJs,
-            html: Html,
-            babelConfigModifier,
-            webpackConfigModifier
+        // Return a Promise that will never be resolved, to keep the process running.
+        return new Promise(() => {
+            watchApp({
+                cwd: generatedRoot,
+                entry: IndexJs,
+                html: Html,
+                babelConfigModifier,
+                webpackConfigModifier
+            });
         });
     }
 
