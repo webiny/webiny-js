be68bafcf81e4e5daa39744aba72d72255a8231c
Adrian Smijulj <adrian1358@gmail.com>
2022-04-07 18:54:56 +0200

    fix: export command as a separate `handler` export and make it reusable

--- packages/cli/commands/wcp/project.js
+++ packages/cli/commands/wcp/project.js
@@ -27,159 +27,163 @@ module.exports.command = () => [
                             });
                             command.example("$0 project init");
                         },
-                        async ({ context }) => {
-                            // Check login.
-                            const user = await getUser();
-
-                            // User already initialized a project?
-                            const { id } = context.project.config;
-                            if (id) {
-                                const project = user.projects.find(item => item.id === id);
-                                if (project) {
-                                    console.log(
-                                        `It seems this project was already initialized (project name: ${context.info.hl(
-                                            project.name
-                                        )}).`
-                                    );
-
-                                    const prompt = inquirer.createPromptModule();
-                                    const { proceed } = await prompt({
-                                        name: "proceed",
-                                        message: "Would you like to re-initialize it?",
-                                        type: "confirm",
-                                        default: false
-                                    });
-
-                                    if (!proceed) {
-                                        return;
-                                    }
-                                }
-                            }
-
-                            // Get user's organizations.
-                            if (!user.orgs.length) {
-                                console.log(
-                                    "It seems you're not part of any organization. Please log in to Webiny Control Panel and create one."
-                                );
-
-                                const prompt = inquirer.createPromptModule();
-                                const { proceed } = await prompt({
-                                    name: "proceed",
-                                    message: "Would you like to do that now?",
-                                    type: "confirm",
-                                    default: false
-                                });
-
-                                if (proceed) {
-                                    await open(WCP_APP_URL);
-                                }
-                                return;
-                            }
-
-                            let selectedOrg;
-                            if (user.orgs.length === 1) {
-                                selectedOrg = user.orgs[0];
-                            } else {
-                                console.log("It seems you're part of multiple organizations. ");
-                                const choices = user.orgs.map(item => ({
-                                    name: item.name,
-                                    value: item
-                                }));
-
-                                const prompt = inquirer.createPromptModule();
-                                selectedOrg = await prompt({
-                                    name: "org",
-                                    message: "Select organization:",
-                                    type: "list",
-                                    choices,
-                                    default: choices[0].value
-                                }).then(result => result.org);
-                            }
-
-                            const orgProjects = user.projects.filter(item => item.org.id === selectedOrg.id);
-
-                            // Get user's projects.
-                            if (!orgProjects.length) {
-                                console.log(
-                                    `It seems there are no projects created within the ${context.info.hl(
-                                        selectedOrg.name
-                                    )} organization.`
-                                );
-
-                                const prompt = inquirer.createPromptModule();
-                                const { proceed } = await prompt({
-                                    name: "proceed",
-                                    message: "Would you like to create one now?",
-                                    type: "confirm",
-                                    default: false
-                                });
-
-                                if (proceed) {
-                                    await open(WCP_APP_URL);
-                                }
-                                return;
-                            }
-
-                            let selectedProject;
-                            if (orgProjects.length === 1) {
-                                selectedProject = user.projects[0];
-                            } else {
-                                console.log(
-                                    `It seems there are multiple projects created within the ${context.info.hl(
-                                        selectedOrg.name
-                                    )} organization.`
-                                );
-                                const choices = orgProjects.map(item => ({
-                                    name: item.name,
-                                    value: item
-                                }));
-                                const prompt = inquirer.createPromptModule();
-                                selectedProject = await prompt({
-                                    name: "project",
-                                    message: "Select project:",
-                                    type: "list",
-                                    choices,
-                                    default: choices[0].value
-                                }).then(result => result.project);
-                            }
-
-                            const orgId = selectedOrg.id,
-                                projectId = selectedProject.id;
-
-                            await sleep();
-                            console.log();
-
-                            console.log(`Initializing ${context.success.hl(selectedProject.name)} project...`);
-
-                            await sleep();
-
-                            // Assign the necessary IDs into root `webiny.project.ts` project file.
-                            await setProjectId({
-                                project: context.project,
-                                orgId,
-                                projectId
-                            });
-
-                            console.log(
-                                `${chalk.green("✔")} Project ${context.success.hl(
-                                    selectedProject.name
-                                )} initialized successfully.`
-                            );
-
-                            await sleep();
-
-                            console.log();
-                            console.log(chalk.bold("Next Steps"));
-
-                            console.log(
-                                `‣ if you've just created this project, you might want to deploy it via the ${context.info.hl(
-                                    "yarn webiny deploy"
-                                )} command.`
-                            );
-                        }
+                        () => handler({ context })
                     );
                 }
             );
         }
     }
 ];
+
+const handler = async ({ context }) => {
+    // Check login.
+    const user = await getUser();
+
+    // User already initialized a project?
+    const { id } = context.project.config;
+    if (id) {
+        const project = user.projects.find(item => item.id === id);
+        if (project) {
+            console.log(
+                `It seems this project was already initialized (project name: ${context.info.hl(
+                    project.name
+                )}).`
+            );
+
+            const prompt = inquirer.createPromptModule();
+            const { proceed } = await prompt({
+                name: "proceed",
+                message: "Would you like to re-initialize it?",
+                type: "confirm",
+                default: false
+            });
+
+            if (!proceed) {
+                return;
+            }
+        }
+    }
+
+    // Get user's organizations.
+    if (!user.orgs.length) {
+        console.log(
+            "It seems you're not part of any organization. Please log in to Webiny Control Panel and create one."
+        );
+
+        const prompt = inquirer.createPromptModule();
+        const { proceed } = await prompt({
+            name: "proceed",
+            message: "Would you like to do that now?",
+            type: "confirm",
+            default: false
+        });
+
+        if (proceed) {
+            await open(WCP_APP_URL);
+        }
+        return;
+    }
+
+    let selectedOrg;
+    if (user.orgs.length === 1) {
+        selectedOrg = user.orgs[0];
+    } else {
+        console.log("It seems you're part of multiple organizations. ");
+        const choices = user.orgs.map(item => ({
+            name: item.name,
+            value: item
+        }));
+
+        const prompt = inquirer.createPromptModule();
+        selectedOrg = await prompt({
+            name: "org",
+            message: "Select organization:",
+            type: "list",
+            choices,
+            default: choices[0].value
+        }).then(result => result.org);
+    }
+
+    const orgProjects = user.projects.filter(item => item.org.id === selectedOrg.id);
+
+    // Get user's projects.
+    if (!orgProjects.length) {
+        console.log(
+            `It seems there are no projects created within the ${context.info.hl(
+                selectedOrg.name
+            )} organization.`
+        );
+
+        const prompt = inquirer.createPromptModule();
+        const { proceed } = await prompt({
+            name: "proceed",
+            message: "Would you like to create one now?",
+            type: "confirm",
+            default: false
+        });
+
+        if (proceed) {
+            await open(WCP_APP_URL);
+        }
+        return;
+    }
+
+    let selectedProject;
+    if (orgProjects.length === 1) {
+        selectedProject = user.projects[0];
+    } else {
+        console.log(
+            `It seems there are multiple projects created within the ${context.info.hl(
+                selectedOrg.name
+            )} organization.`
+        );
+        const choices = orgProjects.map(item => ({
+            name: item.name,
+            value: item
+        }));
+        const prompt = inquirer.createPromptModule();
+        selectedProject = await prompt({
+            name: "project",
+            message: "Select project:",
+            type: "list",
+            choices,
+            default: choices[0].value
+        }).then(result => result.project);
+    }
+
+    const orgId = selectedOrg.id,
+        projectId = selectedProject.id;
+
+    await sleep();
+    console.log();
+
+    console.log(`Initializing ${context.success.hl(selectedProject.name)} project...`);
+
+    await sleep();
+
+    // Assign the necessary IDs into root `webiny.project.ts` project file.
+    await setProjectId({
+        project: context.project,
+        orgId,
+        projectId
+    });
+
+    console.log(
+        `${chalk.green("✔")} Project ${context.success.hl(
+            selectedProject.name
+        )} initialized successfully.`
+    );
+
+    await sleep();
+
+    console.log();
+    console.log(chalk.bold("Next Steps"));
+
+    console.log(
+        `‣ if you've just created this project, you might want to deploy it via the ${context.info.hl(
+            "yarn webiny deploy"
+        )} command.`
+    );
+};
+
+module.exports.handler = handler;
