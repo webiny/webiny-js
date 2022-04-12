0e962641e5b0a42425e3617fe7b86c87dc413f91
Bruno Zorić <bruno.zoric@gmail.com>
2022-04-12 10:12:37 +0200

    feat(api-file-manager): add locale to index if env var is set to true

--- packages/api-file-manager-ddb/src/definitions/table.ts
+++ packages/api-file-manager-ddb/src/definitions/table.ts
@@ -1,5 +1,4 @@
 import { Table } from "dynamodb-toolbox";
-import configurations from "~/operations/configurations";
 import { getDocumentClient, getTable } from "~/operations/utils";
 import { FileManagerContext } from "@webiny/api-file-manager/types";
 
@@ -9,7 +8,7 @@ export interface TableParams {
 export default (params: TableParams): Table => {
     const { context } = params;
     return new Table({
-        name: configurations.db().table || getTable(context),
+        name: process.env.DB_TABLE_FILE_MANGER || process.env.DB_TABLE || getTable(context),
         partitionKey: "PK",
         sortKey: "SK",
         DocumentClient: getDocumentClient(context)
