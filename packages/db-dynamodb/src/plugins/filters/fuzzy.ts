0a1bda4a4ef663bea12f98575929576443241d1c
Bruno Zorić <bruno.zoric@gmail.com>
2022-04-25 20:20:15 +0200

    refactor(db-dynamodb): remove fuzzy filter cache

--- packages/db-dynamodb/src/plugins/filters/fuzzy.ts
+++ packages/db-dynamodb/src/plugins/filters/fuzzy.ts
@@ -4,19 +4,6 @@ import {
     ValueFilterPlugin
 } from "../definitions/ValueFilterPlugin";
 
-/**
- * We will cache the Fuse. If value is already in the cache, use that. Otherwise create a new one...
- */
-const cache: Record<string, Fuse<string>> = {};
-const createFuse = (value: string) => {
-    if (cache[value]) {
-        return cache[value];
-    }
-    cache[value] = new Fuse([value], {});
-
-    return cache[value];
-};
-
 export default new ValueFilterPlugin({
     operation: "fuzzy",
     matches: ({
@@ -26,8 +13,7 @@ export default new ValueFilterPlugin({
         if (typeof value !== "string") {
             return false;
         }
-
-        const f = createFuse(value);
+        const f = new Fuse([value], {});
         const result = f.search(compareValue);
 
         return result.length > 0;
