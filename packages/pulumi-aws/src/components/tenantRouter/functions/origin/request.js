fc2ea2ab1d51595dccff8bab2939e1650baad666
Pavel Denisjuk <pavel@webiny.com>
2022-05-04 18:20:22 +0200

    fix(pulumi-aws): improve request.uri handling

--- packages/pulumi-aws/src/components/tenantRouter/functions/origin/request.js
+++ packages/pulumi-aws/src/components/tenantRouter/functions/origin/request.js
@@ -8,14 +8,27 @@ const documentClient = new DocumentClient({
     region: DB_TABLE_REGION
 });
 
-exports.handler = async event => {
-    const request = event.Records[0].cf.request;
-    // console.log(JSON.stringify(event.Records[0].cf, null, 2));
+function sanitizeRequestURI(uri) {
+    // Make sure that / is not appended to index.html, or any path with extension.
+    const parts = uri.split("/").filter(Boolean);
 
+    if (!parts.length) {
+        return "/index.html";
+    }
+
+    const lastPart = parts[parts.length - 1];
+
+    if (lastPart.includes(".")) {
+        return ["", ...parts].join("/");
+    } // Otherwise, construct a valid S3 bucket path to an HTML file.
+
+    return ["", ...parts, "index.html"].join("/");
+}
+
+async function handleOriginRequest(request) {
     const requestedDomain = request.headers.host[0].value;
-    const originDomain = request.origin.custom.domainName;
+    const originDomain = request.origin.custom.domainName; // Find tenant by domain
 
-    // Find tenant by domain
     const params = {
         TableName: DB_TABLE_NAME,
         Key: {
@@ -23,19 +36,34 @@ exports.handler = async event => {
             SK: "A"
         }
     };
-
     const { Item } = await documentClient.get(params).promise();
 
     if (Item) {
-        const from = request.uri.endsWith("/") ? request.uri : request.uri + "/";
-        request.uri = `/${Item.tenant}${from}`;
+        const uri = sanitizeRequestURI(request.uri);
 
-        console.log(`Rewriting request from "${from}" to "${request.uri}"`);
+        if (uri.startsWith(`/${Item.tenant}/`)) {
+            // Don't include tenant ID twice.
+            request.uri = uri;
+        } else {
+            // Append tenant ID to point to the correct S3 bucket folder.
+            request.uri = `/${Item.tenant}${uri}`;
+        }
+
+        console.log(`Rewriting request from "${uri}" to "${request.uri}"`);
     } else {
         console.log(`Failed to find a tenant for domain "${requestedDomain}"`);
-    }
-    // Restore the Host header
+    } // Restore the Host header
+
     request.headers.host[0].value = originDomain;
+    return request;
+}
+
+exports.handler = async event => {
+    const { request, config } = event.Records[0].cf;
+
+    if (config.eventType === "origin-request") {
+        return handleOriginRequest(request);
+    }
 
     return request;
 };
