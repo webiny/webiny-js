fa4c57139ff2bcd41b669a0be5127f5f0d80de14
Ben Read <endymion1818@gmail.com>
2022-05-05 11:41:23 +0100

    feat(page-builder): iframe element (#2319)
    * feat(page-builder): iframe element
    
    * fix(page-builder): code tidy
    
    * chore(page-builder): use package root imports
    
    * chore(pb-iframe-element): uppercase label
    
    * chore(pb-iframe): use dotprop instead of chaining
    
    * fix(pb-iframe): add width settings
    
    * chore(page-builder): restore type, not needed with dotProp
    
    * fix(iframe): fix width to 100% of parent
    
    * fix(iframe): valid urls only
    
    * fix(iframe): use DisplayMode enum, restore optional chaining
    
    * fix(iframe): dont open element settings on create, fix type
    
    * fix(types): stricter types for onCreate params
    
    * fix(types): implement enum value
    
    * fix(app-page-builder): use correct hook outside editor view
    
    * chore(app-page-builder): use webiny validation package

--- packages/app-page-builder/src/render/plugins/elements/iframe/index.tsx
+++ packages/app-page-builder/src/render/plugins/elements/iframe/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import kebabCase from "lodash/kebabCase";
-import Paragraph from "./Paragraph";
+import IFrame from "./IFrame";
+
 import { PbRenderElementPluginArgs, PbRenderElementPlugin } from "~/types";
 
 export default (args: PbRenderElementPluginArgs = {}): PbRenderElementPlugin => {
-    const elementType = kebabCase(args.elementType || "paragraph");
+    const elementType = kebabCase(args.elementType || "iframe");
 
     return {
         name: `pb-render-page-element-${elementType}`,
         type: "pb-render-page-element",
         elementType: elementType,
         render(props) {
-            return <Paragraph {...props} />;
+            return <IFrame {...props} />;
         }
     };
 };
