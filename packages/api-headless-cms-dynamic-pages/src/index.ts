1314db5709b73daa8295aef6a6541df5f7de927f
Pavel Denisjuk <pavel@webiny.com>
2022-05-23 08:27:58 +0200

    chore: delete obsolete packages

--- packages/api-headless-cms-dynamic-pages/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from "@webiny/api-dynamic-pages/plugins/DataSourcePlugin";
-import { GetEntryDataSource } from "./GetEntryDataSource";
-
-export default () => [
-    new DataSourcePlugin({
-        type: "cms-get-entry",
-        factory: () => new GetEntryDataSource()
-    })
-];
