d40703f555b5ec0af619baa229f3d874c5e8c987
Adrian Smijulj <adrian1358@gmail.com>
2022-05-24 18:21:17 +0200

    fix: remove old tests

--- packages/api-wcp/__tests__/types.ts
+++
@@ -1,5 +0,0 @@
-import { Context as BaseContext } from "@webiny/handler/types";
-import { TenancyContext } from "@webiny/api-tenancy/types";
-import { SecurityContext } from "@webiny/api-security/types";
-
-export interface TestContext extends BaseContext, SecurityContext, TenancyContext {}
