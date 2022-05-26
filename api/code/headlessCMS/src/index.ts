52795588c4408e28bf1a3a0b274fe1edf9ec873a
Adrian Smijulj <adrian1358@gmail.com>
2022-05-26 19:59:31 +0200

    feat: add WCP plugins

--- api/code/headlessCMS/src/index.ts
+++ api/code/headlessCMS/src/index.ts
@@ -1,5 +1,6 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { createHandler } from "@webiny/handler-aws";
+import { createWcpContext, createWcpGraphQL } from "@webiny/api-wcp";
 import i18nPlugins from "@webiny/api-i18n/graphql";
 import i18nDynamoDbStorageOperations from "@webiny/api-i18n-ddb";
 import i18nContentPlugins from "@webiny/api-i18n-content/plugins";
@@ -27,6 +28,8 @@ const documentClient = new DocumentClient({
 
 export const handler = createHandler({
     plugins: [
+        createWcpContext(),
+        createWcpGraphQL(),
         dynamoDbPlugins(),
         logsPlugins(),
         dbPlugins({
