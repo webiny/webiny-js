78ea4ae02b1ecf498df6f3a3758606d1bfc0d77c
Pavel Denisjuk <pavel@webiny.com>
2022-06-01 19:55:41 +0200

    feat(react-composition): extract Composition API into a package

---
+++ packages/react-composition/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./Context";
+// export type { HigherOrderComponent } from "./Context";
+export * from "./Compose";
+// export type { ComposeProps, ComposableFC } from "./Compose";
+export * from "./makeComposable";
