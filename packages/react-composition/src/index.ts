dc60c82b69e6417a2daf2e733d9b3094975ff4b0
Pavel Denisjuk <pavel@webiny.com>
2022-06-06 09:46:28 +0200

    fix(react-composition): remove redundant devDependencies

--- packages/react-composition/src/index.ts
+++ packages/react-composition/src/index.ts
@@ -1,5 +1,3 @@
 export * from "./Context";
-// export type { HigherOrderComponent } from "./Context";
 export * from "./Compose";
-// export type { ComposeProps, ComposableFC } from "./Compose";
 export * from "./makeComposable";
