1774ab0ad85a7b8d91342fd1086918ea6d402ba6
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-06-08 15:39:21 +0300

    fix: fix GraphQLSchemaPlugin creation

--- packages/api-page-builder/src/graphql/graphql/blocks.gql.ts
+++ packages/api-page-builder/src/graphql/graphql/blocks.gql.ts
@@ -1,48 +1,49 @@
-import { GraphQLSchemaPlugin } from "@webiny/handler-graphql/types";
+import { GraphQLSchemaPlugin } from "@webiny/handler-graphql/plugins/GraphQLSchemaPlugin";
+
 import resolve from "./utils/resolve";
 import { PbContext } from "../types";
 
-export const createBlockGraphQL: GraphQLSchemaPlugin<PbContext> = {
-    type: "graphql-schema",
-    schema: {
-        typeDefs: /* GraphQL */ `
-            type PbBlock {
-                id: ID
-                createdOn: DateTime
-                createdBy: PbCreatedBy
-                name: String
-                blockCategory: String
-                type: String
-                content: JSON
-                preview: JSON
-            }
+export const createBlockGraphQL = new GraphQLSchemaPlugin<PbContext>({
+    typeDefs: /* GraphQL */ `
+        type PbBlock {
+            id: ID
+            createdOn: DateTime
+            createdBy: PbCreatedBy
+            name: String
+            blockCategory: String
+            type: String
+            content: JSON
+            preview: JSON
+        }
 
-            input PbCreateBlockInput {
-                name: String!
-                type: String!
-                blockCategory: String!
-                content: JSON!
-                preview: JSON!
-            }
+        """
+        This input type is used by users to create Page Blocks.
+        """
+        input PbCreateBlockInput {
+            name: String!
+            type: String!
+            blockCategory: String!
+            content: JSON!
+            preview: JSON!
+        }
 
-            # Response types
-            type PbBlockResponse {
-                data: PbBlock
-                error: PbError
-            }
+        # Response types
+        type PbBlockResponse {
+            data: PbBlock
+            error: PbError
+        }
 
-            extend type PbMutation {
-                createBlock(data: PbCreateBlockInput!): PbBlockResponse
-            }
-        `,
-        resolvers: {
-            PbMutation: {
-                createBlock: async (_, args: any, context) => {
-                    return resolve(() => {
-                        return context.pageBuilder.createBlock(args.data);
-                    });
-                }
+        extend type PbMutation {
+            createBlock(data: PbCreateBlockInput!): PbBlockResponse
+        }
+    `,
+    resolvers: {
+        PbMutation: {
+            createBlock: async (_, args: any, context) => {
+                return resolve(() => {
+                    return context.pageBuilder.createBlock(args.data);
+                });
             }
         }
     }
-};
+});
