cdafbbfbcb1de83a0455e1d12f35c1912688f171
Adrian Smijulj <adrian1358@gmail.com>
2022-06-09 15:39:58 +0200

    feat: add support for passing a loader

--- packages/app-wcp/src/WcpProvider.tsx
+++ packages/app-wcp/src/WcpProvider.tsx
@@ -34,17 +34,29 @@ export const GET_WCP_PROJECT = gql`
     }
 `;
 
-export const WcpProvider: React.FC = ({ children }) => {
+interface WcpProviderProps {
+    loader?: React.ReactElement;
+}
+
+export const WcpProvider: React.FC<WcpProviderProps> = ({ children, loader }) => {
+    // If `REACT_APP_WCP_PROJECT_ID` environment variable is missing, we can immediately exit.
+    if (!process.env.REACT_APP_WCP_PROJECT_ID) {
+        return <WcpProviderComponent project={null}>{children}</WcpProviderComponent>;
+    }
+
     const [project, setProject] = useState<WcpProject | null | undefined>(undefined);
     useQuery<GetWcpProjectGqlResponse>(GET_WCP_PROJECT, {
-        skip: !!project,
+        skip: project !== undefined,
         onCompleted: response => {
             setProject(response.wcp.getProject.data);
         }
     });
 
+    // Initially, the `project` variable is `undefined`. Once the `GET_WCP_PROJECT` GQL query
+    // has been resolved, then it becomes either `null` or `WcpProject`, and that's when we can continue
+    // rendering child React components.
     if (project === undefined) {
-        return null;
+        return loader || null;
     }
 
     return <WcpProviderComponent project={project}>{children}</WcpProviderComponent>;
