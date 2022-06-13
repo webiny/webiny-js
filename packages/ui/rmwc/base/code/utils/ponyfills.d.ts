16b68651d813f69ecca275afa85ff8010fd31024
endymion1818 <endymion1818@gmail.com>
2022-06-13 11:09:05 +0100

    fix(ui): remove forked rmwc packages

--- packages/ui/rmwc/base/code/utils/ponyfills.d.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-export declare const closest: (element: EventTarget | HTMLElement | null, selector: string) => HTMLElement | null;
-export declare const matches: (element: HTMLElement, selector: string) => boolean;
