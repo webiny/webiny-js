38a8954df5f873c89912f2b2b2bbe47a75cbeca8
Pavel Denisjuk <pavel@webiny.com>
2022-06-15 02:15:03 +0200

    fix(api-page-builder): disable migration during test runs

--- packages/api-page-builder/src/updateSettings/index.ts
+++ packages/api-page-builder/src/updateSettings/index.ts
@@ -79,7 +79,9 @@ export default (
                     settings
                 });
 
-                await migrate(storageOperations, settings.prerendering);
+                if (process.env.NODE_ENV !== "test") {
+                    await migrate(storageOperations, settings.prerendering);
+                }
 
                 delete settings.locale;
                 delete settings.tenant;
