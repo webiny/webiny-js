940bd366efd29a2d73b4d75dd119fe30d8eee1ee
Pavel Denisjuk <pavel@webiny.com>
2022-06-22 23:07:47 +0200

    chore: disable APW tests

--- scripts/listPackagesWithTests.js
+++ scripts/listPackagesWithTests.js
@@ -49,13 +49,18 @@ const CUSTOM_HANDLERS = {
     },
     // Split "api-apw" tests.
     "api-apw": () => {
-        return [
-            // TODO 2: APW tests currently only work with DynamoDB-only (it's hard-coded in the tests).
-            // TODO 2: That is because we currently have no way to load multiple storage operations at the same time.
-            "packages/api-apw/*"
-            // "packages/api-apw/* --keyword=cms:ddb --keyword=apw:base",
-            // "packages/api-apw/* --keyword=cms:ddb-es --keyword=apw:base"
-        ];
+        // TODO 1: we had to disable these temporarily because APW relies on WCP, and in our
+        // TODO 1: CI/CD, we're not testing a WCP-enabled Webiny project yet (which causes errors).
+        // TODO 1: Let's bring this ASAP or at least before releasing APW in Q3/2022.
+        return [];
+
+        // return [
+        //     // TODO 2: APW tests currently only work with DynamoDB-only (it's hard-coded in the tests).
+        //     // TODO 2: That is because we currently have no way to load multiple storage operations at the same time.
+        //     "packages/api-apw/*"
+        //     // "packages/api-apw/* --keyword=cms:ddb --keyword=apw:base",
+        //     // "packages/api-apw/* --keyword=cms:ddb-es --keyword=apw:base"
+        // ];
     }
 };
 
