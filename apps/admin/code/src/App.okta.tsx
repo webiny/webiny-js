b7e25a094fa73495bc00ff292edb8f28696129a7
Pavel Denisjuk <pavel@webiny.com>
2022-06-22 18:52:41 +0200

    chore: remove tenant manager from admin app

--- apps/admin/code/src/App.okta.tsx
+++ apps/admin/code/src/App.okta.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Admin } from "@webiny/app-serverless-cms";
 import { Okta } from "@webiny/app-admin-okta";
-import { TenantManager } from "@webiny/app-tenant-manager";
 import "./App.scss";
 
 import { oktaFactory, rootAppClientId } from "./okta";
@@ -10,7 +9,6 @@ export const App: React.FC = () => {
     return (
         <Admin>
             <Okta factory={oktaFactory} rootAppClientId={rootAppClientId} />
-            <TenantManager />
         </Admin>
     );
 };
