756dadae2a09ac0b28cc9ba8e262104c51d46d6c
Pavel Denisjuk <pavel@webiny.com>
2022-06-23 17:28:22 +0200

    test: add initial DDB records to simulate Pulumi table items

--- packages/api-prerendering-service-so-ddb/jest-dynalite-config.js
+++ packages/api-prerendering-service-so-ddb/jest-dynalite-config.js
@@ -1,3 +1,16 @@
 const { createDynaliteTables } = require("../../jest.config.base");
 
-module.exports = createDynaliteTables();
+module.exports = createDynaliteTables({
+    data: [
+        {
+            PK: "PS#SETTINGS",
+            SK: "default",
+            data: {
+                appUrl: "https://www.test.com",
+                bucket: "bucket",
+                cloudfrontId: "cloudfrontId",
+                deliveryUrl: "https://www.test.com"
+            }
+        }
+    ]
+});
