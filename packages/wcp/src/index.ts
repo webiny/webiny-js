64da4c44788e78ec150ceefecfd1308ac84d3c18
Adrian Smijulj <adrian1358@gmail.com>
2022-06-24 13:40:16 +0200

    fix: add `advancedAccessControlLayer` [no ci]

--- packages/wcp/src/index.ts
+++ packages/wcp/src/index.ts
@@ -5,5 +5,6 @@ export * from "./urls";
 export const WCP_FEATURE_LABEL = {
     seats: "User Seats",
     multiTenancy: "Multi-tenancy",
-    advancedPublishingWorkflow: "Advanced Publishing Workflow (APW)"
+    advancedPublishingWorkflow: "Advanced Publishing Workflow (APW)",
+    advancedAccessControlLayer: "Advanced Access Control Layer (ACL)"
 };
