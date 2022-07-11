48c35cdcaba584828296abafae2c4882bd387975
Pavel Denisjuk <pavel@webiny.com>
2022-07-11 16:30:06 +0200

    fix(react-composition): assign dynamic name to EmptyRenderer function

--- packages/react-composition/src/makeComposable.tsx
+++ packages/react-composition/src/makeComposable.tsx
@@ -16,23 +16,25 @@ function useComposableParents() {
 }
 
 const createEmptyRenderer = (name: string): React.FC => {
-    return function EmptyRenderer(): null {
-        useEffect(() => {
-            // We need to debounce the log, as it sometimes only requires a single tick to get the new
-            // composed component to render, and we don't want to scare developers for no reason.
-            const debounced = debounce(() => {
-                console.info(
-                    `<${name}/> is not implemented! To provide an implementation, use the <Compose/> component.`
-                );
-            }, 100);
+    return {
+        [name]: function () {
+            useEffect(() => {
+                // We need to debounce the log, as it sometimes only requires a single tick to get the new
+                // composed component to render, and we don't want to scare developers for no reason.
+                const debounced = debounce(() => {
+                    console.info(
+                        `<${name}/> is not implemented! To provide an implementation, use the <Compose/> component.`
+                    );
+                }, 100);
 
-            return () => {
-                debounced.cancel();
-            };
-        }, []);
+                return () => {
+                    debounced.cancel();
+                };
+            }, []);
 
-        return null;
-    };
+            return null;
+        }
+    }[name];
 };
 
 export function makeComposable<TProps>(name: string, Component?: React.FC<TProps>) {
