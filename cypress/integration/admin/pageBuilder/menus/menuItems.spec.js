1661e8a68b3ad4d827ae5c102c7ce7285f8b203c
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-07-12 16:28:36 +0200

    fix: menuItems Cypress tests

--- cypress/integration/admin/pageBuilder/menus/menuItems.spec.js
+++ cypress/integration/admin/pageBuilder/menus/menuItems.spec.js
@@ -30,70 +30,77 @@ context("Menus Module", () => {
         cy.get("[data-testid='pb.menu.create.items.button'] > button").click();
         cy.findByTestId("pb.menu.create.items.button").within(() => {
             cy.findByText("Page list").click();
-            cy.findByLabelText("Title").type(`Page List ${id}`);
         });
-        cy.findByText(/Save Menu Item/i)
+        cy.findByTestId("pb.menu.new.listitem.title").type(`Page List ${id}`);
+        cy.findByTestId("pb.menu.new.listitem.button.save")
             .click()
             .wait(200);
         cy.findByText("Value is required.").should("exist");
-        cy.findByLabelText("Category").type(`Static`);
+        cy.findByTestId("pb.menu.new.listitem.category").type(`Static`);
         cy.findByText("Static").click();
-        cy.findByText("Sort by...").prev().select("Title");
-        cy.findByText("Sort direction...").prev().select("Descending");
-        cy.findByLabelText("Tags").type(`1: ${id}`);
+        cy.findByTestId("pb.menu.new.listitem.sortby").select("Title");
+        cy.findByTestId("pb.menu.new.listitem.sortdirection").select("Descending");
+        cy.findByTestId("pb.menu.new.listitem.tags").type(`1: ${id}`);
         cy.findByText(`1: ${id}`).click();
-        cy.findByLabelText("Tags").type(`2: ${id}`);
+        cy.findByTestId("pb.menu.new.listitem.tags").type(`2: ${id}`);
         cy.findByText(`2: ${id}`).click();
-        cy.findByText(/Save Menu Item/i).click();
+        cy.findByTestId("pb.menu.new.listitem.button.save").click();
 
         // Test "Page".
-        cy.findByText(/\+ Add Menu Item/i).click();
-        cy.findByText("Page").click();
-        cy.findByLabelText("Title").type(`Single Page Item ${id}`);
-        cy.findByText(/Save Menu Item/i)
+        cy.get("[data-testid='pb.menu.create.items.button'] > button").click();
+        cy.findByTestId("pb.menu.create.items.button").within(() => {
+            cy.findByText("Page").click();
+        });
+        cy.findAllByTestId("pb.menu.new.pageitem.title").type(`Single Page Item ${id}`);
+        cy.findByTestId("pb.menu.new.pageitem.button.save").click()
             .click()
             .wait(200);
         cy.findByText("Value is required.").should("exist");
-        cy.findByLabelText("Page").type(`Menus`);
+        cy.findByTestId("pb.menu.new.pageitem.page").type(`Menus`);
         cy.findByText(`Menus-Module-Welcome-${id}`).click();
-        cy.findByText(/Save Menu Item/i).click();
+        cy.findByTestId("pb.menu.new.pageitem.button.save").click();
 
         // Test "Link".
-        cy.findByText(/\+ Add Menu Item/i).click();
-        cy.findByText("Link").click();
-        cy.findByLabelText("Title").type(`Simple Link Item ${id}`);
-        cy.findByLabelText("URL").type(`https://123`);
-        cy.findByText(/Save Menu Item/i)
+        cy.get("[data-testid='pb.menu.create.items.button'] > button").click();
+        cy.findByTestId("pb.menu.create.items.button").within(() => {
+            cy.findByText("Link").click();
+        });
+        cy.findByTestId("pb.menu.new.link.title").type(`Simple Link Item ${id}`);
+        cy.findByTestId("pb.menu.new.link.url").type(`https://123`);
+        cy.findByTestId("pb.menu.new.link.button.save").click()
             .click()
             .wait(200);
         cy.findByText("Value must be a valid URL.").should("exist");
-        cy.findByLabelText("URL").clear().type(`https://${id}.com`);
-        cy.findByText(/Save Menu Item/i).click();
+        cy.findByTestId("pb.menu.new.link.url").clear().type(`https://${id}.com`);
+        cy.findByTestId("pb.menu.new.link.button.save").click();
 
         // Test "Folder".
-        cy.findByText(/\+ Add Menu Item/i).click();
-        cy.findByText("Folder").click();
-        cy.findByText(/Save Menu Item/i)
+        cy.get("[data-testid='pb.menu.create.items.button'] > button").click();
+        cy.findByTestId("pb.menu.create.items.button").within(() => {
+            cy.findByText("Folder").click();
+        });
+        cy.findByTestId("pb.menu.new.folder.button.save")
             .click()
             .wait(200);
         cy.findByText("Value is required.").should("exist");
-        cy.findByLabelText("Title").type(`Simple Folder Item ${id}`);
-        cy.findByText(/Save Menu Item/i).click();
+        cy.findByTestId("pb.menu.new.folder.title").type(`Simple Folder Item ${id}`);
+        cy.findByTestId("pb.menu.new.folder.button.save").click();
 
-        cy.findByLabelText("Name").type(`Cool Menu ${id}`);
-        cy.findByLabelText("Slug").type(`cool-menu-${id}`);
-        cy.findByLabelText("Description").type("This is a cool test.");
+        cy.findByTestId("pb.menu.create.name").type(`Cool Menu ${id}`);
+        cy.findByTestId("pb.menu.create.slug").type(`cool-menu-${id}`);
+        cy.findByTestId("pb.menu.create.description").type("This is a cool test.");
 
-        cy.findByText("Save menu").click().wait(2000).reload();
+        cy.findByTestId("pb.menu.save.button").click().wait(2000).reload();
 
         // After reload, let's check if we have all items still present.
         cy.findByText(`Page List ${id}`).should("exist");
         cy.findByText(`Single Page Item ${id}`).should("exist");
         cy.findByText(`Simple Link Item ${id}`).should("exist");
         cy.findByText(`Simple Folder Item ${id}`).should("exist");
+        
         // Delete Menu
         cy.findByTestId("default-data-list").within(() => {
-            cy.get("div")
+            cy.get("li")
                 .first()
                 .within(() => {
                     cy.findByText(`Cool Menu ${id}`).should("exist");
@@ -104,7 +111,7 @@ context("Menus Module", () => {
         cy.get('[role="alertdialog"] :visible').within(() => {
             cy.contains("Are you sure you want to continue?")
                 .next()
-                .within(() => cy.findByText("Confirm").click());
+                .within(() => cy.findByText("Confirm").parent("button").click());
         });
 
         cy.findByText(/Menu ".*" deleted\./).should("exist");
