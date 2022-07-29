f128f8c62c403fde7681b300be18c1f49a6e8003
Bruno Zorić <bruno.zoric@gmail.com>
2022-07-29 11:38:34 +0200

    test(api-dynamodb-to-elasticsearch): add event test

---
+++ packages/api-dynamodb-to-elasticsearch/__tests__/event.test.ts
@@ -0,0 +1,56 @@
+import { createEventHandler } from "~/handler";
+import { PluginsContainer } from "@webiny/plugins";
+import { DynamoDBStreamEvent } from "aws-lambda";
+
+const event: DynamoDBStreamEvent = {
+    Records: [
+        {
+            eventID: "123",
+            dynamodb: {
+                NewImage: {}
+            }
+        }
+    ]
+};
+
+const lambdaContext: any = {};
+const request: any = {};
+const context: any = {
+    plugins: new PluginsContainer(),
+    elasticsearch: {
+        bulk: async () => {
+            return {
+                test: true
+            };
+        }
+    }
+};
+
+describe("event", () => {
+    it("should trigger event with no records", async () => {
+        const eventHandler = createEventHandler();
+
+        const result = await eventHandler.cb({
+            context,
+            event: {
+                Records: []
+            },
+            lambdaContext,
+            request
+        });
+
+        expect(result).toEqual(null);
+    });
+    it("should trigger event with a record", async () => {
+        const eventHandler = createEventHandler();
+
+        const result = await eventHandler.cb({
+            context,
+            event,
+            lambdaContext,
+            request
+        });
+
+        expect(result).toEqual(null);
+    });
+});
