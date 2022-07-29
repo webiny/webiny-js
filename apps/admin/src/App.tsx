4ad0c8d3b1f0e9ca5471f26c7eea5aac1a21a2ed
Pavel Denisjuk <pavel@webiny.com>
2022-07-29 16:18:27 +0200

    chore: restore admin app

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Admin } from "@webiny/app-serverless-cms";
+import { Cognito } from "@webiny/app-admin-users-cognito";
+import "./App.scss";
 
 export const App: React.FC = () => {
     return (
-        <h2>yo!</h2>
+        <Admin>
+            <Cognito />
+        </Admin>
     );
 };
