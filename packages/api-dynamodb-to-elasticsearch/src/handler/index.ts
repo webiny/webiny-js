afedeed7b397273e0fe7b4660b2bbcb17e5d4071
Bruno Zorić <bruno.zoric@gmail.com>
2022-07-29 11:11:54 +0200

    fix(api-dynamodb-to-elasticsearch): use new handler

--- packages/api-dynamodb-to-elasticsearch/src/handler/index.ts
+++ packages/api-dynamodb-to-elasticsearch/src/handler/index.ts
@@ -1,9 +1,9 @@
 import WebinyError from "@webiny/error";
 import { Converter } from "aws-sdk/clients/dynamodb";
-import { HandlerPlugin } from "@webiny/handler/types";
-import { ElasticsearchContext } from "@webiny/api-elasticsearch/types";
 import { decompress } from "@webiny/api-elasticsearch/compression";
-import { ApiResponse } from "@webiny/api-elasticsearch/types";
+import { ApiResponse, ElasticsearchContext } from "@webiny/api-elasticsearch/types";
+import { createDynamoDBEventHandler } from "@webiny/handler-fastify-aws";
+import { StreamRecord } from "aws-lambda/trigger/dynamodb-stream";
 
 enum Operations {
     INSERT = "INSERT",
@@ -66,81 +66,86 @@ interface RecordDynamoDbKeys {
     SK: string;
 }
 
-export default (): HandlerPlugin<ElasticsearchContext> => ({
-    type: "handler",
-    async handle(context) {
-        const [event] = context.args;
-        const operations = [];
+export default createDynamoDBEventHandler(async ({ event, context: ctx }) => {
+    const context = ctx as unknown as ElasticsearchContext;
+    if (!context.elasticsearch) {
+        console.log("Missing elasticsearch definition on context.");
+        return;
+    }
+    const operations = [];
 
-        for (const record of event.Records) {
-            const newImage = Converter.unmarshall(record.dynamodb.NewImage) as RecordDynamoDbImage;
+    for (const record of event.Records) {
+        const dynamodb = record.dynamodb as Required<StreamRecord>;
+        if (!dynamodb) {
+            continue;
+        }
+        const newImage = Converter.unmarshall(dynamodb.NewImage) as RecordDynamoDbImage;
 
-            if (newImage.ignore === true) {
+        if (newImage.ignore === true) {
+            continue;
+        }
+
+        const oldImage = Converter.unmarshall(dynamodb.OldImage) as RecordDynamoDbImage;
+        const keys = Converter.unmarshall(dynamodb.Keys) as RecordDynamoDbKeys;
+        const _id = `${keys.PK}:${keys.SK}`;
+        const operation = record.eventName;
+
+        /**
+         * On operations other than REMOVE we decompress the data and store it into the Elasticsearch.
+         * No need to try to decompress if operation is REMOVE since there is no data sent into that operation.
+         */
+        let data: any = undefined;
+        if (operation !== Operations.REMOVE) {
+            /**
+             * We must decompress the data that is going into the Elasticsearch.
+             */
+            data = await decompress(context.plugins, newImage.data);
+            /**
+             * No point in writing null or undefined data into the Elasticsearch.
+             * This might happen on some error while decompressing. We will log it.
+             *
+             * Data should NEVER be null or undefined in the Elasticsearch DynamoDB table, unless it is a delete operations.
+             * If it is - it is a bug.
+             */
+            if (data === undefined || data === null) {
+                console.log(
+                    `Could not get decompressed data, skipping ES operation "${operation}", ID ${_id}`
+                );
                 continue;
             }
-
-            const oldImage = Converter.unmarshall(record.dynamodb.OldImage) as RecordDynamoDbImage;
-            const keys = Converter.unmarshall(record.dynamodb.Keys) as RecordDynamoDbKeys;
-            const _id = `${keys.PK}:${keys.SK}`;
-            const operation = record.eventName;
-
-            /**
-             * On operations other than REMOVE we decompress the data and store it into the Elasticsearch.
-             * No need to try to decompress if operation is REMOVE since there is no data sent into that operation.
-             */
-            let data: any = undefined;
-            if (operation !== Operations.REMOVE) {
-                /**
-                 * We must decompress the data that is going into the Elasticsearch.
-                 */
-                data = await decompress(context.plugins, newImage.data);
-                /**
-                 * No point in writing null or undefined data into the Elasticsearch.
-                 * This might happen on some error while decompressing. We will log it.
-                 *
-                 * Data should NEVER be null or undefined in the Elasticsearch DynamoDB table, unless it is a delete operations.
-                 * If it is - it is a bug.
-                 */
-                if (data === undefined || data === null) {
-                    console.log(
-                        `Could not get decompressed data, skipping ES operation "${operation}", ID ${_id}`
-                    );
-                    continue;
-                }
-            }
-
-            switch (record.eventName) {
-                case Operations.INSERT:
-                case Operations.MODIFY:
-                    operations.push({ index: { _id, _index: newImage.index } }, data);
-                    break;
-                case Operations.REMOVE:
-                    operations.push({ delete: { _id, _index: oldImage.index } });
-                    break;
-                default:
-                    break;
-            }
         }
 
-        if (!operations.length) {
-            return false;
+        switch (record.eventName) {
+            case Operations.INSERT:
+            case Operations.MODIFY:
+                operations.push({ index: { _id, _index: newImage.index } }, data);
+                break;
+            case Operations.REMOVE:
+                operations.push({ delete: { _id, _index: oldImage.index } });
+                break;
+            default:
+                break;
         }
-
-        try {
-            const res = await context.elasticsearch.bulk<BulkOperationsResponseBody>({
-                body: operations
-            });
-            checkErrors(res);
-            if (process.env.DEBUG === "true") {
-                console.log("Bulk response", JSON.stringify(res, null, 2));
-            }
-        } catch (error) {
-            if (process.env.DEBUG === "true") {
-                console.log("Bulk error", JSON.stringify(error, null, 2));
-            }
-            throw error;
-        }
-
-        return true;
     }
+
+    if (!operations.length) {
+        return null;
+    }
+
+    try {
+        const res = await context.elasticsearch.bulk<BulkOperationsResponseBody>({
+            body: operations
+        });
+        checkErrors(res);
+        if (process.env.DEBUG === "true") {
+            console.log("Bulk response", JSON.stringify(res, null, 2));
+        }
+    } catch (error) {
+        if (process.env.DEBUG === "true") {
+            console.log("Bulk error", JSON.stringify(error, null, 2));
+        }
+        throw error;
+    }
+
+    return null;
 });
