66abee3571ae6ec612ca9d8dc934c4140ae33ad4
Pavel Denisjuk <pavel@webiny.com>
2022-08-02 19:13:10 +0200

    fix(serverless-cms-aws): add prepublishOnly script to build handlers

--- packages/serverless-cms-aws/webiny.config.js
+++ packages/serverless-cms-aws/webiny.config.js
@@ -3,11 +3,10 @@ const glob = require("fast-glob");
 
 module.exports = {
     commands: {
-        build: async options => {
-            // First we build the package, as usual.
-            await createBuildPackage({ cwd: __dirname })(options);
-
-            // Then we bundle all of the handlers. These are then used directly in real Webiny projects.
+        build: createBuildPackage({ cwd: __dirname }),
+        watch: createWatchPackage({ cwd: __dirname }),
+        buildHandlers: async options => {
+            // Bundle all handlers. These are then used directly in real Webiny projects.
             const wbyConfigsPaths = glob.sync(`${__dirname}/handlers/**/webiny.config.js`);
 
             // Bundle handlers.
@@ -15,7 +14,6 @@ module.exports = {
                 const wbyConfigPath = wbyConfigsPaths[i];
                 await require(wbyConfigPath).commands.build(options);
             }
-        },
-        watch: createWatchPackage({ cwd: __dirname })
+        }
     }
 };
