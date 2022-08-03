ea96ae93538452e80ed539e073f1b7b44a68251e
Pavel Denisjuk <pavel@webiny.com>
2022-08-03 15:57:11 +0200

    fix: make sure buildHandlers command runs on buildWithCache

--- packages/serverless-cms-aws/webiny.config.js
+++ packages/serverless-cms-aws/webiny.config.js
@@ -1,19 +1,24 @@
 const { createWatchPackage, createBuildPackage } = require("@webiny/project-utils");
 const glob = require("fast-glob");
 
+async function buildHandlers(options) {
+    // Bundle all handlers. These are then used directly in real Webiny projects.
+    const wbyConfigsPaths = glob.sync(`${__dirname}/handlers/**/webiny.config.js`);
+
+    // Bundle handlers.
+    for (let i = 0; i < wbyConfigsPaths.length; i++) {
+        const wbyConfigPath = wbyConfigsPaths[i];
+        await require(wbyConfigPath).commands.build(options);
+    }
+}
+
 module.exports = {
     commands: {
-        build: createBuildPackage({ cwd: __dirname }),
+        build: async options => {
+            await createBuildPackage({ cwd: __dirname })(options);
+            await buildHandlers(options);
+        },
         watch: createWatchPackage({ cwd: __dirname }),
-        buildHandlers: async options => {
-            // Bundle all handlers. These are then used directly in real Webiny projects.
-            const wbyConfigsPaths = glob.sync(`${__dirname}/handlers/**/webiny.config.js`);
-
-            // Bundle handlers.
-            for (let i = 0; i < wbyConfigsPaths.length; i++) {
-                const wbyConfigPath = wbyConfigsPaths[i];
-                await require(wbyConfigPath).commands.build(options);
-            }
-        }
+        buildHandlers
     }
 };
