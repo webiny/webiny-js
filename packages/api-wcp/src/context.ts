453466f04d3a596d93bf294f291b3c80423f314d
Adrian Smijulj <adrian1358@gmail.com>
2022-08-05 18:29:18 +0200

    feat: allow custom permissions only for WCP/AACL-activated projects

--- packages/api-wcp/src/context.ts
+++ packages/api-wcp/src/context.ts
@@ -1,9 +1,38 @@
 import { ContextPlugin } from "@webiny/handler";
+import { SecurityPermission } from "@webiny/api-security/types";
 import { WcpContext } from "~/types";
 import { createWcp } from "~/createWcp";
+import { filterOutCustomWbyAppsPermissions } from "~/utils";
 
 export const createWcpContext = () => {
     return new ContextPlugin<WcpContext>(async context => {
         context.wcp = await createWcp();
+
+        context.waitFor("security", () => {
+            const previousGetPermissions = context.security.getPermissions;
+
+            context.security.getPermissions = async function getPermissions(): Promise<
+                SecurityPermission[]
+            > {
+                const permissions = await previousGetPermissions();
+
+                const securitySystemRecord = await context.security
+                    .getStorageOperations()
+                    .getSystemData({ tenant: "root" });
+
+                if (!securitySystemRecord?.createdOn) {
+                    return permissions;
+                }
+
+                // If Advanced Access Control Layer (AACL) can be used, no need to do anything.
+                if (context.wcp.canUseFeature("advancedAccessControlLayer")) {
+                    return permissions;
+                }
+
+                // If Advanced Access Control Layer (AACL) cannot be used,
+                // we omit all of Webiny apps-related custom permissions.
+                return filterOutCustomWbyAppsPermissions(permissions);
+            };
+        });
     });
 };
