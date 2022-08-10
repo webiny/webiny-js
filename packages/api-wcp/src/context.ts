e5f2c5d02fa21988697209a0d53fac8fd76c6d0b
Adrian Smijulj <adrian1358@gmail.com>
2022-08-10 19:22:15 +0200

    fix: finalize all custom permissions-related checks

--- packages/api-wcp/src/context.ts
+++ packages/api-wcp/src/context.ts
@@ -9,28 +9,35 @@ export const createWcpContext = () => {
         context.wcp = await createWcp();
 
         context.waitFor("security", () => {
-            const previousGetPermissions = context.security.getPermissions;
+            const previousGetPermissions = context.security.getPermissions.bind(context.security);
 
             context.security.getPermissions = async function getPermissions(): Promise<
                 SecurityPermission[]
             > {
+                // For starters, we load permissions as usual.
                 const permissions = await previousGetPermissions();
 
+                // Now we start checking whether we want to return all permissions, or we
+                // need to omit the custom ones because of the one of the following reasons.
+
+                // 1. Are we dealing with an old Webiny project?
                 const securitySystemRecord = await context.security
                     .getStorageOperations()
                     .getSystemData({ tenant: "root" });
 
-                if (!securitySystemRecord?.createdOn) {
+                // Older versions of Webiny do not have the `createdOn` value stored. So,
+                // if missing, we don't want to make any changes to the existing behavior.
+                if (!securitySystemRecord || !securitySystemRecord.createdOn) {
                     return permissions;
                 }
 
-                // If Advanced Access Control Layer (AACL) can be used, no need to do anything.
+                // 2. If Advanced Access Control Layer (AACL) can be used, again, no need to do anything.
                 if (context.wcp.canUseFeature("advancedAccessControlLayer")) {
                     return permissions;
                 }
 
-                // If Advanced Access Control Layer (AACL) cannot be used,
-                // we omit all of Webiny apps-related custom permissions.
+                // 3. If Advanced Access Control Layer (AACL) cannot be used,
+                // we omit all of the Webiny apps-related custom permissions.
                 return filterOutCustomWbyAppsPermissions(permissions);
             };
         });
