68275a7c86d576b0b505eb96489e0f7c3e8c51aa
Adrian Smijulj <adrian1358@gmail.com>
2022-08-12 17:10:46 +0200

    feat: add `advancedAccessControlLayer`

--- packages/app-wcp/src/types.ts
+++ packages/app-wcp/src/types.ts
@@ -14,6 +14,9 @@ export type WcpProjectPackage = {
         advancedPublishingWorkflow: {
             enabled: boolean;
         };
+        advancedAccessControlLayer: {
+            enabled: boolean;
+        };
     };
 };
 
