4e8b3a8c9ee1258a8c6aaba6ac6cf348331ac694
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-08-31 12:13:41 +0300

    feat: update generated pages on block update

--- packages/app-page-builder/src/render/plugins/elements/block/Block.tsx
+++ packages/app-page-builder/src/render/plugins/elements/block/Block.tsx
@@ -8,6 +8,19 @@ import ElementAnimation from "../../../components/ElementAnimation";
 import { Interpolation } from "@emotion/core";
 import { PageBuilderContext } from "../../../../contexts/PageBuilder";
 
+declare global {
+    // eslint-disable-next-line
+    namespace JSX {
+        interface IntrinsicElements {
+            // @ts-ignore
+            "ps-tag": {
+                key?: string;
+                value?: string;
+            };
+        }
+    }
+}
+
 interface BlockProps {
     element: PbElement;
 }
@@ -16,66 +29,69 @@ const Block: React.FC<BlockProps> = ({ element }) => {
         responsiveDisplayMode: { displayMode }
     } = React.useContext(PageBuilderContext);
     return (
-        <ElementAnimation>
-            <ElementRoot element={element}>
-                {({ elementStyle, elementAttributes, customClasses, combineClassNames }) => {
-                    const containerStyle = elementStyle;
-                    // Use per-device style
-                    const width =
-                        elementStyle[
-                            `--${kebabCase(
-                                displayMode
-                            )}-align-items` as unknown as keyof CSSProperties
-                        ];
-                    /**
-                     * We're swapping "justifyContent" & "alignItems" value here because
-                     * ".webiny-pb-layout-block" has "flex-direction: column"
-                     */
-                    const alignItems =
-                        elementStyle[
-                            `--${kebabCase(
-                                displayMode
-                            )}-justify-content` as unknown as keyof CSSProperties
-                        ];
-                    const justifyContent =
-                        elementStyle[
-                            `--${kebabCase(
-                                displayMode
-                            )}-align-items` as unknown as keyof CSSProperties
-                        ];
+        <>
+            <ps-tag data-key={"pb-page-block"} data-value={element.id} />
+            <ElementAnimation>
+                <ElementRoot element={element}>
+                    {({ elementStyle, elementAttributes, customClasses, combineClassNames }) => {
+                        const containerStyle = elementStyle;
+                        // Use per-device style
+                        const width =
+                            elementStyle[
+                                `--${kebabCase(
+                                    displayMode
+                                )}-align-items` as unknown as keyof CSSProperties
+                            ];
+                        /**
+                         * We're swapping "justifyContent" & "alignItems" value here because
+                         * ".webiny-pb-layout-block" has "flex-direction: column"
+                         */
+                        const alignItems =
+                            elementStyle[
+                                `--${kebabCase(
+                                    displayMode
+                                )}-justify-content` as unknown as keyof CSSProperties
+                            ];
+                        const justifyContent =
+                            elementStyle[
+                                `--${kebabCase(
+                                    displayMode
+                                )}-align-items` as unknown as keyof CSSProperties
+                            ];
 
-                    // TODO @ts-refactor style type
-                    return (
-                        <div
-                            style={{ width: "100%", display: "flex" }}
-                            className={
-                                "webiny-pb-layout-block-container " +
-                                css(containerStyle as Interpolation)
-                            }
-                            {...elementAttributes}
-                        >
+                        // TODO @ts-refactor style type
+                        return (
                             <div
-                                style={
-                                    {
-                                        width,
-                                        justifyContent,
-                                        alignItems
-                                    } as any
+                                style={{ width: "100%", display: "flex" }}
+                                className={
+                                    "webiny-pb-layout-block-container " +
+                                    css(containerStyle as Interpolation)
                                 }
-                                className={combineClassNames(
-                                    "webiny-pb-layout-block webiny-pb-base-page-element-style",
-                                    ...customClasses
-                                )}
+                                {...elementAttributes}
                             >
-                                {element.elements.map(element => (
-                                    <Element key={element.id} element={element} />
-                                ))}
+                                <div
+                                    style={
+                                        {
+                                            width,
+                                            justifyContent,
+                                            alignItems
+                                        } as any
+                                    }
+                                    className={combineClassNames(
+                                        "webiny-pb-layout-block webiny-pb-base-page-element-style",
+                                        ...customClasses
+                                    )}
+                                >
+                                    {element.elements.map(element => (
+                                        <Element key={element.id} element={element} />
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    );
-                }}
-            </ElementRoot>
-        </ElementAnimation>
+                        );
+                    }}
+                </ElementRoot>
+            </ElementAnimation>
+        </>
     );
 };
 
