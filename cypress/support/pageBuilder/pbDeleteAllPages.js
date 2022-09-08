19aaa48511757523acd37a53d7beb49debb83d00
adrians5j <adrian@webiny.com>
2022-09-08 16:51:24 +0200

    test: add `pbDeleteAllPages` Cypress command

---
+++ cypress/support/pageBuilder/pbDeleteAllPages.js
@@ -0,0 +1,26 @@
+import { until } from "@webiny/project-utils/testing/helpers/until";
+import { pbListPages } from "./pbListPages";
+import { pbDeletePage } from "./pbDeletePage";
+import { login } from "../login";
+
+/**
+ * Deletes all pages (except for the default "Static" one).
+ */
+Cypress.Commands.add("pbDeleteAllPages", async () => {
+    // Loads admin user with full access permissions.
+    const user = await login();
+
+    const pages = await pbListPages({ user });
+    for (let i = 0; i < pages.length; i++) {
+        if (pages[i].path === "/welcome-to-webiny") {
+            continue;
+        }
+        await pbDeletePage({ user, variables: pages[i] });
+    }
+
+    return until(
+        () => pbListPages({ user }),
+        result => result.length === 1,
+        { wait: 3000 }
+    );
+});
