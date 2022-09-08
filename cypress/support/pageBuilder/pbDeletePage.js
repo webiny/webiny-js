28be3cf77dc3373371e322b1890f6ec5ff05c3c8
adrians5j <adrian@webiny.com>
2022-09-08 16:55:37 +0200

    test: add `pbDeleteAllPages` Cypress command

--- cypress/support/pageBuilder/pbDeletePage.js
+++ cypress/support/pageBuilder/pbDeletePage.js
@@ -1,16 +1,21 @@
 import { GraphQLClient } from "graphql-request";
 import { DELETE_PAGE } from "./graphql";
+import { login } from "../login";
 
-Cypress.Commands.add("pbDeletePage", variables => {
-    cy.login().then(user => {
-        const client = new GraphQLClient(Cypress.env("GRAPHQL_API_URL"), {
-            headers: {
-                authorization: `Bearer ${user.idToken.jwtToken}`
-            }
-        });
+export const pbDeletePage = ({ user, variables = {} }) => {
+    const client = new GraphQLClient(Cypress.env("GRAPHQL_API_URL"), {
+        headers: {
+            authorization: `Bearer ${user.idToken.jwtToken}`
+        }
+    });
 
-        return client
-            .request(DELETE_PAGE, variables)
-            .then(response => response.pageBuilder.deletePage.data);
+    return client
+        .request(DELETE_PAGE, variables)
+        .then(response => response.pageBuilder.deletePage.data);
+};
+
+Cypress.Commands.add("pbDeletePage", (variables = {}) => {
+    return login().then(user => {
+        return pbDeletePage({ user, variables });
     });
 });
--- cypress/support/pageBuilder/pbListPages.js
+++ cypress/support/pageBuilder/pbListPages.js
@@ -1,16 +1,21 @@
 import { GraphQLClient } from "graphql-request";
 import { LIST_PAGES } from "./graphql";
+import { login } from "../login";
 
-Cypress.Commands.add("pbListPages", variables => {
-    cy.login().then(user => {
-        const client = new GraphQLClient(Cypress.env("GRAPHQL_API_URL"), {
-            headers: {
-                authorization: `Bearer ${user.idToken.jwtToken}`
-            }
-        });
+export const pbListPages = ({ user, variables = {} }) => {
+    const client = new GraphQLClient(Cypress.env("GRAPHQL_API_URL"), {
+        headers: {
+            authorization: `Bearer ${user.idToken.jwtToken}`
+        }
+    });
 
-        return client
-            .request(LIST_PAGES, variables)
-            .then(response => response.pageBuilder.listPages.data);
+    return client
+        .request(LIST_PAGES, variables)
+        .then(response => response.pageBuilder.listPages.data);
+};
+
+Cypress.Commands.add("pbListPages", (variables = {}) => {
+    return login().then(user => {
+        return pbListPages({ user, variables });
     });
 });
