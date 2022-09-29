63e98b0f98e2ff1a24a7ee63432869da27513033
adrians5j <adrian@webiny.com>
2022-09-29 16:24:16 +0200

    fix: only use `AWSLambdaVPCAccessExecutionRole` policy if VPC feature is enabled

--- packages/pulumi-aws/src/apps/core/CoreElasticSearch.ts
+++ packages/pulumi-aws/src/apps/core/CoreElasticSearch.ts
@@ -167,15 +167,23 @@ export const ElasticSearch = createAppModule({
         });
 
         // Only use `AWSLambdaVPCAccessExecutionRole` policy if VPC feature is enabled.
-        let policyArn: aws.iam.ManagedPolicy = aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole;
         if (vpc) {
-            policyArn = aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole;
+            app.addResource(aws.iam.RolePolicyAttachment, {
+                name: `${roleName}-AWSLambdaVPCAccessExecutionRole`,
+                config: {
+                    role: role.output,
+                    policyArn: aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole
+                }
+            });
+        } else {
+            app.addResource(aws.iam.RolePolicyAttachment, {
+                name: `${roleName}-AWSLambdaBasicExecutionRole`,
+                config: {
+                    role: role.output,
+                    policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole
+                }
+            });
         }
-
-        app.addResource(aws.iam.RolePolicyAttachment, {
-            name: `${roleName}-AWSLambdaVPCAccessExecutionRole`,
-            config: { role: role.output, policyArn }
-        });
 
         app.addResource(aws.iam.RolePolicyAttachment, {
             name: `${roleName}-AWSLambdaDynamoDBExecutionRole`,
