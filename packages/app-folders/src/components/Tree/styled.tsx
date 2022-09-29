8aec7be0fc11e658ed8f4d3a7e8b8206ceaaf9d1
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-09-29 16:09:17 +0200

    fix: remove fix width

--- packages/app-folders/src/components/Tree/styled.tsx
+++ packages/app-folders/src/components/Tree/styled.tsx
@@ -6,7 +6,7 @@ export const Container = styled("div")`
     & .treeRoot {
         box-sizing: border-box;
         padding: 4px 0 16px;
-        width: 250px;
+        min-width: 200px;
     }
 
     & .dropTarget {
