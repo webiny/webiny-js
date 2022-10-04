24270de34e0534cb81c5f8a132f1514ffd844369
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-10-04 10:05:42 +0200

    chore: restore file

---
+++ packages/app-page-builder/src/editor/plugins/elements/heading/index.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import kebabCase from "lodash/kebabCase";
+import {
+    DisplayMode,
+    PbEditorElement,
+    PbEditorPageElementPlugin,
+    PbEditorTextElementPluginsArgs
+} from "~/types";
+import { createInitialPerDeviceSettingValue } from "../../elementSettings/elementSettingsUtils";
+import { createInitialTextValue } from "../utils/textUtils";
+import Heading from "./Heading";
+
+export default (args: PbEditorTextElementPluginsArgs = {}): PbEditorPageElementPlugin => {
+    const defaultText = "Heading";
+
+    const defaultSettings = [
+        [
+            "pb-editor-page-element-style-settings-text",
+            { useCustomTag: true, tags: ["h1", "h2", "h3", "h4", "h5", "h6"] }
+        ],
+        "pb-editor-page-element-style-settings-background",
+        "pb-editor-page-element-style-settings-border",
+        "pb-editor-page-element-style-settings-shadow",
+        "pb-editor-page-element-style-settings-padding",
+        "pb-editor-page-element-style-settings-margin",
+        "pb-editor-page-element-settings-clone",
+        "pb-editor-page-element-settings-delete"
+    ];
+
+    const defaultToolbar = {
+        title: "Heading",
+        group: "pb-editor-element-group-basic",
+        preview() {
+            return <h2>{defaultText}</h2>;
+        }
+    };
+
+    const elementType = kebabCase(args.elementType || "heading");
+
+    return {
+        name: `pb-editor-page-element-${elementType}`,
+        type: "pb-editor-page-element",
+        elementType: elementType,
+        /**
+         * TODO @ts-refactor @ashutosh
+         * Please check this. args.toolbar() and defaultToolbar are totally different types
+         */
+        // @ts-ignore
+        toolbar: typeof args.toolbar === "function" ? args.toolbar(defaultToolbar) : defaultToolbar,
+        settings:
+            typeof args.settings === "function" ? args.settings(defaultSettings) : defaultSettings,
+        target: ["cell", "block"],
+        create({ content = {}, ...options }) {
+            const previewText = content.text || defaultText;
+
+            const defaultValue: Partial<PbEditorElement> = {
+                type: this.elementType,
+                elements: [],
+                data: {
+                    text: {
+                        ...createInitialPerDeviceSettingValue(
+                            createInitialTextValue({
+                                type: this.elementType,
+                                tag: "h1"
+                            }),
+                            DisplayMode.DESKTOP
+                        ),
+                        data: {
+                            text: previewText
+                        }
+                    },
+                    settings: {
+                        margin: createInitialPerDeviceSettingValue(
+                            { all: "0px" },
+                            DisplayMode.DESKTOP
+                        ),
+                        padding: createInitialPerDeviceSettingValue(
+                            { all: "0px" },
+                            DisplayMode.DESKTOP
+                        ),
+                        /**
+                         * Figure out better way for types.
+                         * TODO @ts-refactor
+                         */
+                        horizontalAlign: createInitialPerDeviceSettingValue(
+                            "center",
+                            DisplayMode.DESKTOP
+                        ) as unknown as "left" | "center" | "right" | "justify"
+                    }
+                },
+                ...options
+            };
+
+            return typeof args.create === "function" ? args.create(defaultValue) : defaultValue;
+        },
+        render(props) {
+            return <Heading {...props} mediumEditorOptions={args.mediumEditorOptions} />;
+        }
+    };
+};
