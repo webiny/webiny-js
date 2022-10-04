9cf89b2732bd61c4f6cd4edafd00d12d05533dfc
adrians5j <adrian@webiny.com>
2022-10-04 16:06:05 +0200

    fix: add missing `CmsContext`

--- packages/cwp-template-aws/template/ddb-es/apps/api/graphql/src/types.ts
+++ packages/cwp-template-aws/template/ddb-es/apps/api/graphql/src/types.ts
@@ -8,6 +8,7 @@ import { PbContext } from "@webiny/api-page-builder/graphql/types";
 import { PrerenderingServiceClientContext } from "@webiny/api-prerendering-service/client/types";
 import { FileManagerContext } from "@webiny/api-file-manager/types";
 import { FormBuilderContext } from "@webiny/api-form-builder/types";
+import { CmsContext } from "@webiny/api-headless-cms/types";
 
 // When working with the `context` object (for example while defining a new GraphQL resolver function),
 // you can import this interface and assign it to it. This will give you full autocomplete functionality
@@ -25,4 +26,5 @@ export interface Context
         PbContext,
         PrerenderingServiceClientContext,
         FileManagerContext,
-        FormBuilderContext {}
+        FormBuilderContext,
+        CmsContext {}
