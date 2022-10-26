f867e6d6f3f11f35bd1f4d09a6767d2f0a087386
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-10-26 10:05:03 +0200

    feat: memoize useFolders and useLinks

--- packages/app-folders/src/hooks/useFolders.ts
+++ packages/app-folders/src/hooks/useFolders.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { FoldersContext } from "~/contexts/folders";
 import { FolderItem } from "~/types";
 
@@ -8,33 +8,39 @@ export const useFolders = (type: string) => {
         throw new Error("useFolders must be used within a FoldersProvider");
     }
 
+    const { folders, loading, listFolders, getFolder, createFolder, updateFolder, deleteFolder } =
+        context;
+
     useEffect(() => {
         /**
          * On first mount, call `listFolders`, which will either issue a network request, or load folders from cache.
          * We don't need to store the result of it to any local state; that is managed by the context provider.
          */
-        context.listFolders(type);
+        listFolders(type);
     }, []);
 
-    return {
-        /**
-         * NOTE: do NOT expose listFolders from this hook, because you already have folders in the `folders` property.
-         * You'll never need to call `listFolders` from any component. As soon as you call `useFolders()`, you'll initiate
-         * fetching of `folders`, which is managed by the FoldersContext.
-         */
-        loading: context.loading,
-        folders: context.folders[type],
-        getFolder(id: string) {
-            return context.getFolder(id);
-        },
-        createFolder(folder: Omit<FolderItem, "id">) {
-            return context.createFolder(folder);
-        },
-        updateFolder(folder: FolderItem) {
-            return context.updateFolder(folder);
-        },
-        deleteFolder(folder: FolderItem) {
-            return context.deleteFolder(folder);
-        }
-    };
+    return useMemo(
+        () => ({
+            /**
+             * NOTE: do NOT expose listFolders from this hook, because you already have folders in the `folders` property.
+             * You'll never need to call `listFolders` from any component. As soon as you call `useFolders()`, you'll initiate
+             * fetching of `folders`, which is managed by the FoldersContext.
+             */
+            loading,
+            folders: folders[type],
+            getFolder(id: string) {
+                return getFolder(id);
+            },
+            createFolder(folder: Omit<FolderItem, "id">) {
+                return createFolder(folder);
+            },
+            updateFolder(folder: FolderItem) {
+                return updateFolder(folder);
+            },
+            deleteFolder(folder: FolderItem) {
+                return deleteFolder(folder);
+            }
+        }),
+        [folders[type], loading]
+    );
 };
--- packages/app-folders/src/hooks/useLinks.ts
+++ packages/app-folders/src/hooks/useLinks.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { LinksContext } from "~/contexts/links";
 import { LinkItem } from "~/types";
 
@@ -8,33 +8,38 @@ export const useLinks = (folderId: string) => {
         throw new Error("useFoldersLinks must be used within a FoldersProvider");
     }
 
+    const { links, loading, listLinks, getLink, createLink, updateLink, deleteLink } = context;
+
     useEffect(() => {
         /**
          * On first mount, call `listLinks`, which will either issue a network request, or load links from cache.
          * We don't need to store the result of it to any local state; that is managed by the context provider.
          */
-        context.listLinks(folderId);
+        listLinks(folderId);
     }, []);
 
-    return {
-        /**
-         * NOTE: do NOT expose listLinks from this hook, because you already have folders in the `links` property.
-         * You'll never need to call `listLinks` from any component. As soon as you call `useLinks()`, you'll initiate
-         * fetching of `links`, which is managed by the LinksContext.
-         */
-        loading: context.loading,
-        links: context.links,
-        getLink(id: string) {
-            return context.getLink(id);
-        },
-        createLink(link: Omit<LinkItem, "linkId">) {
-            return context.createLink(link);
-        },
-        updateLink(link: LinkItem) {
-            return context.updateLink(link, folderId);
-        },
-        deleteLink(link: LinkItem) {
-            return context.deleteLink(link);
-        }
-    };
+    return useMemo(
+        () => ({
+            /**
+             * NOTE: do NOT expose listLinks from this hook, because you already have folders in the `links` property.
+             * You'll never need to call `listLinks` from any component. As soon as you call `useLinks()`, you'll initiate
+             * fetching of `links`, which is managed by the LinksContext.
+             */
+            loading,
+            links,
+            getLink(id: string) {
+                return getLink(id);
+            },
+            createLink(link: Omit<LinkItem, "linkId">) {
+                return createLink(link);
+            },
+            updateLink(link: LinkItem) {
+                return updateLink(link, folderId);
+            },
+            deleteLink(link: LinkItem) {
+                return deleteLink(link);
+            }
+        }),
+        [links, loading]
+    );
 };
