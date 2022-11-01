7526d36369c63321b3f867d576af9b4c502141fe
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-11-01 12:07:43 +0100

    feat: add meta props

--- packages/ui/src/DataTable/DataTable.tsx
+++ packages/ui/src/DataTable/DataTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import {
     DataTableContent,
     DataTableHead,
@@ -6,44 +6,53 @@ import {
     DataTableHeadCell,
     DataTableBody,
     DataTableCell,
-    DataTable
+    DataTable,
+    DataTableCellProps
 } from "@rmwc/data-table";
 
 import { flexRender, getCoreRowModel, useReactTable, ColumnDef } from "@tanstack/react-table";
 
-interface Column {
+export interface Column {
     id: string;
-    header: string;
+    header: string | ReactElement;
+    meta?: DataTableCellProps;
 }
 
-interface TableProps<T> {
+interface TableProps<T extends object> {
     columns: Column[];
     data: T[];
 }
 
 export const Table = <T extends object>({ data, columns }: TableProps<T>) => {
-    const defaultColumns: ColumnDef<T>[] = columns.map(column => {
+    const columnsDefinition: ColumnDef<T>[] = columns.map(column => {
         return {
             accessorKey: column.id,
-            header: column.header,
-            cell: info => info.getValue()
+            header: () => column.header,
+            cell: info => info.getValue(),
+            meta: column.meta
         };
     });
 
     const table = useReactTable({
         data,
-        columns: defaultColumns,
-        getCoreRowModel: getCoreRowModel()
+        columns: columnsDefinition,
+        getCoreRowModel: getCoreRowModel(),
+        debugTable: true,
+        debugHeaders: true,
+        debugColumns: true
     });
 
     return (
-        <DataTable>
+        <DataTable style={{ width: "100%" }}>
             <DataTableContent>
                 <DataTableHead>
                     {table.getHeaderGroups().map(headerGroup => (
                         <DataTableRow key={headerGroup.id}>
                             {headerGroup.headers.map(header => (
-                                <DataTableHeadCell key={header.id}>
+                                <DataTableHeadCell
+                                    key={header.id}
+                                    {...header.column.columnDef.meta}
+                                >
                                     {header.isPlaceholder
                                         ? null
                                         : flexRender(
@@ -59,7 +68,7 @@ export const Table = <T extends object>({ data, columns }: TableProps<T>) => {
                     {table.getRowModel().rows.map(row => (
                         <DataTableRow key={row.id}>
                             {row.getVisibleCells().map(cell => (
-                                <DataTableCell key={cell.id}>
+                                <DataTableCell key={cell.id} {...cell.column.columnDef.meta}>
                                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                 </DataTableCell>
                             ))}
