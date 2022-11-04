9d6c51c6c4b513444b7b9b490dde8eda19332e56
Vitalii Nobis <77202393+neatbyte-vnobis@users.noreply.github.com>
2022-11-04 18:36:03 +0200

    fix: when pasting text, ensure it's pasted in a new row
    * fix: fix pasting into paragraph/text editor component
    
    * fix: fix code review comments

--- packages/app-page-builder/src/editor/components/Text/PbText.tsx
+++ packages/app-page-builder/src/editor/components/Text/PbText.tsx
@@ -38,6 +38,11 @@ const PbText: React.FC<TextElementProps> = ({ elementId, mediumEditorOptions, ro
         [displayMode]
     );
 
+    const initialText = useMemo(
+        () => variableValue || get(element, `${DATA_NAMESPACE}.data.text`),
+        []
+    );
+
     const value = get(element, `${DATA_NAMESPACE}.${displayMode}`, fallbackValue);
 
     const onChange = useCallback(
@@ -59,7 +64,6 @@ const PbText: React.FC<TextElementProps> = ({ elementId, mediumEditorOptions, ro
         return null;
     }
 
-    const textContent = variableValue || get(element, `${DATA_NAMESPACE}.data.text`);
     const tag = get(value, "tag");
     const typography = get(value, "typography");
 
@@ -71,7 +75,7 @@ const PbText: React.FC<TextElementProps> = ({ elementId, mediumEditorOptions, ro
             <ReactMediumEditor
                 elementId={elementId}
                 tag={tag}
-                value={textContent}
+                value={initialText}
                 onChange={onChange}
                 options={mediumEditorOptions}
                 onSelect={onSelect}
