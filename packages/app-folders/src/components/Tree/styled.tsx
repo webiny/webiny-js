a85e26ac74a29a6d7e9b09b08c95942535011394
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-11-14 16:01:37 +0100

    fix: remove folder list max height

--- packages/app-folders/src/components/Tree/styled.tsx
+++ packages/app-folders/src/components/Tree/styled.tsx
@@ -1,8 +1,6 @@
 import styled from "@emotion/styled";
 
 export const Container = styled("div")`
-    height: 100%;
-
     & .treeRoot {
         box-sizing: border-box;
         padding: 4px 0 4px;
