1ebdd49e9bc3536b8b1bd5860202b3aa762b10e4
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-11-17 16:05:04 +0100

    feat: add actions column

--- packages/app-page-builder/src/admin/components/Table/RowName.tsx
+++ packages/app-page-builder/src/admin/components/Table/RowName.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as Folder } from "@material-design-icons/svg/outlined/fo
 import { ReactComponent as File } from "@material-design-icons/svg/outlined/description.svg";
 import styled from "@emotion/styled";
 import { Typography } from "@webiny/ui/Typography";
-import { Link as DefaultLink, useRouter } from "@webiny/react-router";
+import { useRouter } from "@webiny/react-router";
 
 interface Props {
     name: string;
@@ -19,11 +19,6 @@ const Title = styled("div")`
 const Icon = styled("div")`
     margin-right: 8px;
     height: 24px;
-`;
-
-const Link = styled(DefaultLink)`
-    color: var(--mdc-theme-text-primary-on-light);
-    text-decoration: none !important;
 `;
 
 export const FolderName = ({ name, id }: Props) => {
@@ -46,14 +41,18 @@ export const FolderName = ({ name, id }: Props) => {
 };
 
 export const PageName = ({ name, id }: Props) => {
+    const { history } = useRouter();
+
     return (
-        <Link to={`/page-builder/editor/${id}`}>
-            <Title>
-                <Icon>
-                    <File />
-                </Icon>
-                <Typography use={"subtitle2"}>{name}</Typography>
-            </Title>
-        </Link>
+        <Title
+            onClick={() => {
+                history.push(`/page-builder/editor/${id}`);
+            }}
+        >
+            <Icon>
+                <File />
+            </Icon>
+            <Typography use={"subtitle2"}>{name}</Typography>
+        </Title>
     );
 };
