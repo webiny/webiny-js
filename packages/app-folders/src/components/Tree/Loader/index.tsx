e3bd2860ceba05dedcf4ddd43d70f8eaf46a752b
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-12-05 00:57:07 +0100

    chore: loader styling

--- packages/app-folders/src/components/Tree/Loader/index.tsx
+++ packages/app-folders/src/components/Tree/Loader/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import { Skeleton } from "@webiny/ui/Skeleton";
 
-import { SkeletonWrapper } from "./styled";
+import { Container, SkeletonWrapper } from "./styled";
 
 export const Loader = () => {
-    return <Skeleton count={4} inline={true} height={"100%"} wrapper={SkeletonWrapper} />;
+    return (
+        <Container>
+            <Skeleton count={4} inline={true} height={"100%"} wrapper={SkeletonWrapper} />
+        </Container>
+    );
 };
--- packages/app-folders/src/components/Tree/Loader/styled.tsx
+++ packages/app-folders/src/components/Tree/Loader/styled.tsx
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 
+export const Container = styled("div")`
+    padding: 42px 4px 0;
+`;
+
 export const SkeletonWrapper = styled("div")`
-    width: 75%;
-    margin: 0 16px 8px;
+    width: 85%;
+    margin: 0 8px 8px;
     height: 24px;
 
     &:last-of-type {
