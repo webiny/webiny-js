59fb5576ca5d853a1bd7a76af3d4b90512dced54
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-12-05 09:20:16 +0000

    feat: add rich variable input modal

--- packages/app-page-builder/src/editor/plugins/elementSettings/variable/RichVariableInput.tsx
+++ packages/app-page-builder/src/editor/plugins/elementSettings/variable/RichVariableInput.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import styled from "@emotion/styled";
 import { CoreOptions } from "medium-editor";
+import { Dialog, DialogCancel, DialogActions, DialogContent } from "@webiny/ui/Dialog";
+import { ButtonPrimary } from "@webiny/ui/Button";
+import { SimpleButton } from "~/editor/plugins/elementSettings/components/StyledComponents";
 import ReactMediumEditor from "~/editor/components/MediumEditor";
 import { useVariable } from "~/hooks/useVariable";
 
-const InputWrapper = styled("div")({
-    padding: "20px 16px",
-    backgroundColor: "rgba(212, 212, 212, 0.5)",
-    borderBottom: "1px solid",
+const InputWrapper = styled("div")`
+    padding: 20px 16px;
+    background-color: rgba(212, 212, 212, 0.5);
+    border-bottom: 1px solid;
+    width: 700px;
+    height: 400px;
+    overflow-y: auto;
 
-    "&:hover": {
-        backgroundColor: "rgba(212, 212, 212, 0.7)"
-    },
-
-    "&>p": {
-        minHeight: "auto",
-        lineHeight: "normal"
+    &:has(p[data-medium-focused]) {
+        background-color: rgba(212, 212, 212, 0.7);
     }
-});
+
+    & > p {
+        min-height: auto;
+        line-height: normal;
+    }
+`;
+
+const ButtonPrimaryStyled = styled(ButtonPrimary)`
+    margin-left: 8px;
+`;
 
 const DEFAULT_EDITOR_OPTIONS: CoreOptions = {
     toolbar: {
@@ -34,19 +44,48 @@ interface RichVariableInputProps {
 }
 
 const RichVariableInput: React.FC<RichVariableInputProps> = ({ variableId }) => {
-    const { value, onChange, onBlur } = useVariable(variableId);
-    const [initialValue] = useState(value);
+    const { value, onChange } = useVariable(variableId);
+    const [localValue, setLocalValue] = useState(value);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onOpen = useCallback(() => {
+        setIsOpen(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+
+    const onValueChange = useCallback((value: string) => {
+        setLocalValue(value);
+    }, []);
+
+    const onSave = useCallback(() => {
+        onChange(localValue, true);
+        onClose();
+    }, [localValue, onChange, onClose]);
 
     return (
-        <InputWrapper className="webiny-pb-page-element-text">
-            <ReactMediumEditor
-                tag="p"
-                value={initialValue}
-                onChange={onBlur}
-                onSelect={onChange}
-                options={DEFAULT_EDITOR_OPTIONS}
-            />
-        </InputWrapper>
+        <>
+            <SimpleButton onClick={onOpen}>Edit</SimpleButton>
+            <Dialog open={isOpen} onClose={onClose}>
+                <DialogContent>
+                    <InputWrapper className="webiny-pb-page-element-text">
+                        <ReactMediumEditor
+                            tag="p"
+                            value={value}
+                            onChange={onValueChange}
+                            onSelect={onValueChange}
+                            options={DEFAULT_EDITOR_OPTIONS}
+                        />
+                    </InputWrapper>
+                </DialogContent>
+                <DialogActions>
+                    <DialogCancel>Cancel</DialogCancel>
+                    <ButtonPrimaryStyled onClick={onSave}>Save</ButtonPrimaryStyled>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
