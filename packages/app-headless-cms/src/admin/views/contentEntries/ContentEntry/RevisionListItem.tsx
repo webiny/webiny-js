a548304740ab11f316efcbc835424c1b82ebb8cf
Pavel Denisjuk <pavel@webiny.com>
2022-12-16 10:14:48 +0100

    fix(app-headless-cms): use useConfirmationDialog hook instead of Dialog comp

--- packages/app-headless-cms/src/admin/views/contentEntries/ContentEntry/RevisionListItem.tsx
+++ packages/app-headless-cms/src/admin/views/contentEntries/ContentEntry/RevisionListItem.tsx
@@ -16,7 +16,6 @@ import {
 import { IconButton } from "@webiny/ui/Button";
 import { Icon } from "@webiny/ui/Icon";
 import { MenuItem, Menu, MenuDivider } from "@webiny/ui/Menu";
-import { ConfirmationDialog } from "@webiny/ui/ConfirmationDialog";
 import { Tooltip } from "@webiny/ui/Tooltip";
 import { ReactComponent as MoreVerticalIcon } from "~/admin/icons/more_vert.svg";
 import { ReactComponent as LockIcon } from "~/admin/icons/lock.svg";
@@ -32,6 +31,7 @@ import { useRevision } from "./useRevision";
 import usePermission from "~/admin/hooks/usePermission";
 import { useContentEntry } from "~/admin/views/contentEntries/hooks/useContentEntry";
 import { PublishEntryRevisionListItem } from "./PublishEntryRevisionListItem";
+import { useConfirmationDialog } from "@webiny/app-admin";
 
 const t = i18n.ns("app-headless-cms/admin/plugins/content-details/content-revisions");
 
@@ -79,110 +79,105 @@ const RevisionListItem: React.FC<RevisionListItemProps> = ({ revision }) => {
             revision
         });
 
+    const { showConfirmation } = useConfirmationDialog({
+        title: t`Delete content entry revision`,
+        message: (
+            <span>
+                {t`You are about to delete revision {revision}. Are you sure you want to continue?`(
+                    {
+                        revision: <strong>#{revision.meta.version}</strong>
+                    }
+                )}
+            </span>
+        )
+    });
     const { entry, tabsRef } = useContentEntry();
     const { canEdit, canDelete, canPublish, canUnpublish } = usePermission();
     const { icon, text: tooltipText } = getIcon(revision);
 
     return (
-        <ConfirmationDialog
-            title={t`Confirmation required!`}
-            message={
-                <span>
-                    {t`You are about to delete revision {revision}. Are you sure you want to continue?`(
-                        {
-                            revision: <strong>#{revision.meta.version}</strong>
-                        }
-                    )}
-                </span>
-            }
-        >
-            {({ showConfirmation }) => (
-                <ListItem>
-                    <ListItemGraphic>
-                        <Tooltip content={tooltipText} placement={"bottom"}>
-                            {icon}
-                        </Tooltip>
-                    </ListItemGraphic>
-                    <ListItemText>
-                        <ListItemTextPrimary>{revision.meta.title || t`N/A`}</ListItemTextPrimary>
-                        <ListItemTextSecondary>
-                            {t`Last modified {time} (#{version})`({
-                                time: <TimeAgo datetime={revision.savedOn} />,
-                                version: revision.meta.version
-                            })}
-                        </ListItemTextSecondary>
-                    </ListItemText>
-                    <ListItemMeta>
-                        <Menu
-                            handle={<IconButton icon={<MoreVerticalIcon />} />}
-                            className={revisionsMenu}
-                            data-testid={"cms.content-form.revisions.more-options"}
+        <ListItem>
+            <ListItemGraphic>
+                <Tooltip content={tooltipText} placement={"bottom"}>
+                    {icon}
+                </Tooltip>
+            </ListItemGraphic>
+            <ListItemText>
+                <ListItemTextPrimary>{revision.meta.title || t`N/A`}</ListItemTextPrimary>
+                <ListItemTextSecondary>
+                    {t`Last modified {time} (#{version})`({
+                        time: <TimeAgo datetime={revision.savedOn} />,
+                        version: revision.meta.version
+                    })}
+                </ListItemTextSecondary>
+            </ListItemText>
+            <ListItemMeta>
+                <Menu
+                    handle={<IconButton icon={<MoreVerticalIcon />} />}
+                    className={revisionsMenu}
+                    data-testid={"cms.content-form.revisions.more-options"}
+                >
+                    {canEdit(entry, "cms.contentEntry") && (
+                        <MenuItem
+                            onClick={() => createRevision(revision.id)}
+                            data-testid={"cms.revision.create-revision"}
                         >
-                            {canEdit(entry, "cms.contentEntry") && (
-                                <MenuItem
-                                    onClick={() => createRevision(revision.id)}
-                                    data-testid={"cms.revision.create-revision"}
-                                >
-                                    <ListItemGraphic>
-                                        <Icon icon={<AddIcon />} />
-                                    </ListItemGraphic>
-                                    {t`New from current`}
-                                </MenuItem>
-                            )}
+                            <ListItemGraphic>
+                                <Icon icon={<AddIcon />} />
+                            </ListItemGraphic>
+                            {t`New from current`}
+                        </MenuItem>
+                    )}
 
-                            {!revision.meta.locked && canEdit(entry, "cms.contentEntry") && (
-                                <MenuItem
-                                    onClick={() => {
-                                        editRevision();
-                                        if (!tabsRef.current) {
-                                            return;
-                                        }
-                                        tabsRef.current.switchTab(0);
-                                    }}
-                                >
-                                    <ListItemGraphic>
-                                        <Icon icon={<EditIcon />} />
-                                    </ListItemGraphic>
-                                    {t` Edit`}
-                                </MenuItem>
-                            )}
+                    {!revision.meta.locked && canEdit(entry, "cms.contentEntry") && (
+                        <MenuItem
+                            onClick={() => {
+                                editRevision();
+                                if (!tabsRef.current) {
+                                    return;
+                                }
+                                tabsRef.current.switchTab(0);
+                            }}
+                        >
+                            <ListItemGraphic>
+                                <Icon icon={<EditIcon />} />
+                            </ListItemGraphic>
+                            {t` Edit`}
+                        </MenuItem>
+                    )}
 
-                            {revision.meta.status !== "published" &&
-                                canPublish("cms.contentEntry") && (
-                                    <MenuItem onClick={() => publishRevision(revision.id)}>
-                                        <PublishEntryRevisionListItem />
-                                    </MenuItem>
-                                )}
+                    {revision.meta.status !== "published" && canPublish("cms.contentEntry") && (
+                        <MenuItem onClick={() => publishRevision(revision.id)}>
+                            <PublishEntryRevisionListItem />
+                        </MenuItem>
+                    )}
 
-                            {revision.meta.status === "published" &&
-                                canUnpublish("cms.contentEntry") && (
-                                    <MenuItem
-                                        onClick={() => unpublishRevision(revision.id)}
-                                        data-testid={"cms.revision.unpublish"}
-                                    >
-                                        <ListItemGraphic>
-                                            <Icon icon={<UnpublishIcon />} />
-                                        </ListItemGraphic>
-                                        {t`Unpublish`}
-                                    </MenuItem>
-                                )}
+                    {revision.meta.status === "published" && canUnpublish("cms.contentEntry") && (
+                        <MenuItem
+                            onClick={() => unpublishRevision(revision.id)}
+                            data-testid={"cms.revision.unpublish"}
+                        >
+                            <ListItemGraphic>
+                                <Icon icon={<UnpublishIcon />} />
+                            </ListItemGraphic>
+                            {t`Unpublish`}
+                        </MenuItem>
+                    )}
 
-                            {!revision.meta.locked && canDelete(entry, "cms.contentEntry") && (
-                                <div>
-                                    <MenuDivider />
-                                    <MenuItem onClick={() => showConfirmation(deleteRevision)}>
-                                        <ListItemGraphic>
-                                            <Icon icon={<DeleteIcon />} />
-                                        </ListItemGraphic>
-                                        {t` Delete`}
-                                    </MenuItem>
-                                </div>
-                            )}
-                        </Menu>
-                    </ListItemMeta>
-                </ListItem>
-            )}
-        </ConfirmationDialog>
+                    {!revision.meta.locked && canDelete(entry, "cms.contentEntry") && (
+                        <div>
+                            <MenuDivider />
+                            <MenuItem onClick={() => showConfirmation(deleteRevision)}>
+                                <ListItemGraphic>
+                                    <Icon icon={<DeleteIcon />} />
+                                </ListItemGraphic>
+                                {t` Delete`}
+                            </MenuItem>
+                        </div>
+                    )}
+                </Menu>
+            </ListItemMeta>
+        </ListItem>
     );
 };
 
