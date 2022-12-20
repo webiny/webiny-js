b934e6df203451dc3d3a14d22734d9938fd91e30
Pavel Denisjuk <pavel@webiny.com>
2022-12-20 09:23:28 +0100

    fix(api-headless-cms): reference CmsModelField type

--- packages/api-headless-cms/src/utils/getBaseFieldType.ts
+++ packages/api-headless-cms/src/utils/getBaseFieldType.ts
@@ -1,3 +1,5 @@
-export const getBaseFieldType = (field: { type: string }) => {
+import { CmsModelField } from "~/types";
+
+export const getBaseFieldType = (field: { type: CmsModelField["type"] }) => {
     return field.type.split(":")[0];
 };
