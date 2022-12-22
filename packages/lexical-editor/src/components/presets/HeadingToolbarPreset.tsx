23167c4aeec4e89070bf01b7e53912e2ce2a4c38
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2022-12-23 00:10:00 +0100

    wip: add underline action to heading toolbar preset

--- packages/lexical-editor/src/components/presets/HeadingToolbarPreset.tsx
+++ packages/lexical-editor/src/components/presets/HeadingToolbarPreset.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BoldAction } from "~/components/Toolbar/ToolbarActions/BoldAction";
 import { AddToolbarAction } from "~/components/Toolbar/Composable/AddToolbarAction/AddToolbarAction";
 import { ItalicAction } from "~/components/Toolbar/ToolbarActions/ItalicAction";
-import {UnderlineAction} from "~/components/Toolbar/ToolbarActions/UnderlineAction";
+import { UnderlineAction } from "~/components/Toolbar/ToolbarActions/UnderlineAction";
 export const HeadingToolbarPreset = () => {
     return (
         <>
