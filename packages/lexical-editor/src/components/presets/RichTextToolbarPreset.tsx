3f2fbaddc14066e47abafeaaea33c8823fa24ec8
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2022-12-23 13:45:57 +0100

    added active button background on link select

---
+++ packages/lexical-editor/src/components/presets/RichTextToolbarPreset.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BoldAction } from "~/components/Toolbar/ToolbarActions/BoldAction";
+import { AddToolbarAction } from "~/components/Toolbar/Composable/AddToolbarAction/AddToolbarAction";
+import { ItalicAction } from "~/components/Toolbar/ToolbarActions/ItalicAction";
+import { UnderlineAction } from "~/components/Toolbar/ToolbarActions/UnderlineAction";
+import { CodeHighlightAction } from "~/components/Toolbar/ToolbarActions/CodeHighlightAction";
+import { LinkAction } from "~/components/Toolbar/ToolbarActions/LinkAction";
+export const HeadingToolbarPreset = () => {
+    return (
+        <>
+            <AddToolbarAction element={<BoldAction />} type={"heading"} />
+            <AddToolbarAction element={<ItalicAction />} type={"heading"} />
+            <AddToolbarAction element={<UnderlineAction />} type={"heading"} />
+            <AddToolbarAction element={<CodeHighlightAction />} type={"heading"} />
+            <AddToolbarAction element={<LinkAction />} type={"heading"} />
+        </>
+    );
+};
