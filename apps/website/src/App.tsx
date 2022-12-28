a3ee1361679e3f72bf326f7cfc94f22febc32a1d
adrians5j <adrian@webiny.com>
2022-12-28 13:26:33 +0100

    feat: create custom Emotion cache

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -5,14 +5,27 @@ import { PageBuilderProvider } from "@webiny/app-page-builder/contexts/PageBuild
 import { createApolloClient } from "./components/apolloClient";
 import Page from "./components/Page";
 
+import { CacheProvider } from "@emotion/core";
+import createCache from "@emotion/cache";
+
+// As stated in https://emotion.sh/docs/ssr#puppeteer:
+// "If you are using Puppeteer to prerender your application, emotion's
+// speedy option has to be disabled so that the CSS is rendered into the DOM."
+const emotionCache = createCache({
+    key: "wby",
+    speedy: false
+});
+
 export const App: React.FC = () => (
-    <ApolloProvider client={createApolloClient()}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <PageBuilderProvider>
-                <Routes>
-                    <Route path={"*"} component={Page} />
-                </Routes>
-            </PageBuilderProvider>
-        </BrowserRouter>
-    </ApolloProvider>
+    <CacheProvider value={emotionCache}>
+        <ApolloProvider client={createApolloClient()}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <PageBuilderProvider>
+                    <Routes>
+                        <Route path={"*"} component={Page} />
+                    </Routes>
+                </PageBuilderProvider>
+            </BrowserRouter>
+        </ApolloProvider>
+    </CacheProvider>
 );
