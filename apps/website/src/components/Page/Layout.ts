9cc518ccc23f75138b4efba2c947e4d104bc92b2
adrians5j <adrian@webiny.com>
2022-12-28 13:26:12 +0100

    fix: remove `lodash`

--- apps/website/src/components/Page/Layout.ts
+++ apps/website/src/components/Page/Layout.ts
@@ -2,19 +2,19 @@ import React, { useMemo, createElement } from "react";
 import { plugins } from "@webiny/plugins";
 import { PbPageLayoutPlugin, PbPageData } from "@webiny/app-page-builder/types";
 import { SettingsQueryResponseData } from "./graphql";
-import lodashGet from "lodash/get";
 
 interface LayoutProps {
     page: PbPageData;
     settings: SettingsQueryResponseData;
 }
+
 const Layout: React.FC<LayoutProps> = ({ page, settings, children }) => {
     const layouts = useMemo(() => {
         const layoutPlugins = plugins.byType<PbPageLayoutPlugin>("pb-page-layout");
         return layoutPlugins.map(pl => pl.layout);
     }, []);
 
-    const layout = lodashGet(page, "settings.general.layout", null);
+    const layout = page?.settings?.general?.layout || null;
     if (!layout) {
         return children as React.ReactElement;
     }
