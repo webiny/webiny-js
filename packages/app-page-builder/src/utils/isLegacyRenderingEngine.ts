9a746554e96ac0941eac97833ba3aebeb50b512b
adrians5j <adrian@webiny.com>
2022-12-28 21:04:08 +0100

    chore: add deprecation warning

--- packages/app-page-builder/src/utils/isLegacyRenderingEngine.ts
+++ packages/app-page-builder/src/utils/isLegacyRenderingEngine.ts
@@ -1,3 +1,4 @@
 import { featureFlags } from "@webiny/feature-flags";
 
+// @deprecation-warning pb-legacy-rendering-engine
 export const isLegacyRenderingEngine = featureFlags.pbLegacyRenderingEngine === true;
