5e3c1384e99c6a619f46b0155f57db3572e2382c
adrians5j <adrian@webiny.com>
2023-01-02 14:41:49 +0100

    fix: bring back global `scroll-behavior` styles

--- apps/theme/pageBuilder/layouts/Static.tsx
+++ apps/theme/pageBuilder/layouts/Static.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import { Header } from "./Static/Header";
 import { Footer } from "./Static/Footer";
 import styled from "@emotion/styled";
+import { Global, css } from "@emotion/core";
+
+const globalStyles = css`
+    html {
+        scroll-behavior: smooth;
+    }
+
+    @media screen and (prefers-reduced-motion: reduce) {
+        html {
+            scroll-behavior: smooth;
+        }
+    }
+`;
 
 const Layout = styled.div`
     min-height: 100vh;
@@ -16,6 +29,7 @@ const Layout = styled.div`
 const Static: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <Layout>
+            <Global styles={globalStyles} />
             <Header />
             <main>{children}</main>
             <Footer />
