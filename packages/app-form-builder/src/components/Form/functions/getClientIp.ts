6cf3a4e65408d8b39a5056fea2994f0bc39fe2cf
SvenAlHamad <sven@webiny.com>
2023-01-03 16:04:51 +0000

    fix: form builder IP api

--- packages/app-form-builder/src/components/Form/functions/getClientIp.ts
+++ packages/app-form-builder/src/components/Form/functions/getClientIp.ts
@@ -1,16 +1,19 @@
 export default (): Promise<string> => {
-    return new Promise((resolve: (value: string) => void, reject: (error: string) => void) => {
-        const xhr = new window.XMLHttpRequest();
-        xhr.open("GET", "https://api.ipify.org/?format=json", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send();
-        xhr.onload = function () {
-            try {
+    return new Promise((resolve: (value: string) => void) => {
+        resolve('0.0.0.0');
+        /*
+        try {
+            const xhr = new window.XMLHttpRequest();
+            xhr.open("GET", "https://api.ipify.org/?format=json", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send();
+            xhr.onload = function () {
                 const response = JSON.parse(this.responseText);
                 resolve(response.ip);
-            } catch (e) {
-                reject("");
-            }
-        };
+            };
+        } catch (e) {
+            resolve('0.0.0.0');
+        }
+        */
     });
 };
