32719f6546761f8c2c36d5a6b66e30b48b89dc4f
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-01-03 12:04:15 +0100

    feat: add usePageViewNavigation to handle page view navigation

--- packages/app-page-builder/src/admin/views/Pages/Table/Sidebar.tsx
+++ packages/app-page-builder/src/admin/views/Pages/Table/Sidebar.tsx
@@ -1,33 +1,22 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement } from "react";
 import { FolderTree } from "@webiny/app-folders";
-import { useRouter } from "@webiny/react-router";
+
+import { usePageViewNavigation } from "~/hooks/usePageViewNavigation";
 
 interface Props {
     folderId?: string;
 }
 
 export const Sidebar = ({ folderId }: Props): ReactElement => {
-    const [focusedFolderId, setFocusedFolderId] = useState<string>();
-    const { history, location } = useRouter();
-    const query = new URLSearchParams(location.search);
-
-    useEffect(() => {
-        setFocusedFolderId(folderId);
-    }, [folderId]);
-
-    const onFolderClick = (folderId: string): void => {
-        setFocusedFolderId(folderId);
-        query.set("folderId", folderId);
-        history.push({ search: query.toString() });
-    };
+    const { navigateToPageHome, navigateToFolder } = usePageViewNavigation();
 
     return (
         <FolderTree
             type={"page"}
             title={"All pages"}
-            focusedFolderId={focusedFolderId}
-            onTitleClick={() => history.push("/page-builder/pages")}
-            onFolderClick={data => data?.id && onFolderClick(data?.id)}
+            focusedFolderId={folderId}
+            onTitleClick={navigateToPageHome}
+            onFolderClick={data => data?.id && navigateToFolder(data?.id)}
         />
     );
 };
