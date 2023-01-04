da8657ff6ccd618a62e34920cc3186521a20e7ea
adrians5j <adrian@webiny.com>
2023-01-04 12:42:42 +0100

    wip

--- packages/app-page-builder/src/editor/plugins/elements/icon/PeIcon.tsx
+++ packages/app-page-builder/src/editor/plugins/elements/icon/PeIcon.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { createIcon } from "@webiny/app-page-builder-elements/renderers/icon";
-import { createRenderer, useRenderer } from "@webiny/app-page-builder-elements";
 
-const Icon = createIcon();
-
-const PeIcon = createRenderer(() => {
-    const { getElement } = useRenderer();
-    const element = getElement();
-
-    return <Icon element={element} />;
-});
+const PeIcon = createIcon();
 
 export default PeIcon;
