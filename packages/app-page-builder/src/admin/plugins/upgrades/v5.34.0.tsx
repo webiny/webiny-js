10bc374c92de0a048eb5ee8745cd24ef863edaea
Pavel Denisjuk <pavel@webiny.com>
2023-01-09 22:00:41 +0100

    fix(app-page-builder): update admin app migration plugin

--- packages/app-page-builder/src/admin/plugins/upgrades/v5.34.0.tsx
+++ packages/app-page-builder/src/admin/plugins/upgrades/v5.34.0.tsx
@@ -78,7 +78,8 @@ const Upgrade: React.FC<UpgradeProps> = ({ onInstalled }) => {
                             This upgrade will do the following:
                             <ul>
                                 <li>
-                                    link existing pages to <strong>ROOT</strong> folder
+                                    - migrate existing page blocks to be available in the new Blocks
+                                    Manager
                                 </li>
                             </ul>
                         </Typography>
