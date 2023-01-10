bd96a7a959fd24dae6607387c73a98dd714c5e50
adrians5j <adrian@webiny.com>
2023-01-10 22:21:11 +0100

    feat: update CWP template

---
+++ packages/cwp-template-aws/template/common/apps/theme/layouts/forms/DefaultFormLayout/fields/components/FieldLabel.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+import theme from "../../../../../theme";
+
+export const FieldLabel = styled.label`
+    width: 100%;
+    display: inline-block;
+    margin: 0 0 5px 1px;
+    ${theme.breakpoints.mobile} {
+        text-align: left !important;
+    }
+`;
