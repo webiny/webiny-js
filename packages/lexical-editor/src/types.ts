0f7d7f5a64e919e2c55d43a1ea7caf2c5f9beb4f
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2023-01-10 01:12:35 +0100

    wip: remove unused types

--- packages/lexical-editor/src/types.ts
+++ packages/lexical-editor/src/types.ts
@@ -1,11 +1,3 @@
 export type ToolbarType = "heading" | "paragraph" | string;
-export type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-export type ParagraphTag = "p";
-export type RichTextEditorTag = HeadingTag | ParagraphTag;
 export type EditorStateJSONString = string;
-
-// Toolbars
-
-// export actions
-
 
