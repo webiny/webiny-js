66b4fb2d50333c45ab7eaf333c7b14a2adae9d02
adrians5j <adrian@webiny.com>
2023-01-12 14:44:41 +0100

    fix: ensure animations work in the editor

--- packages/app-page-builder/src/editor/contexts/EditorPageElementsProvider.tsx
+++ packages/app-page-builder/src/editor/contexts/EditorPageElementsProvider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PageElementsProvider as PbPageElementsProvider } from "@webiny/app-page-builder-elements/contexts/PageElements";
 
 // Attributes modifiers.
