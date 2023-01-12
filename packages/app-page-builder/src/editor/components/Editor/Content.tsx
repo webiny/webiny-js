29953e033a6ce352fde76cdeb7d045466c765cf6
adrians5j <adrian@webiny.com>
2023-01-12 14:44:12 +0100

    fix: add `overflow: hidden;`

--- packages/app-page-builder/src/editor/components/Editor/Content.tsx
+++ packages/app-page-builder/src/editor/components/Editor/Content.tsx
@@ -29,6 +29,7 @@ const LegacyContentContainer = styled.div(({ theme }: ContentContainerParams) =>
     const backgroundColor = theme?.colors?.background;
     return {
         backgroundColor,
+        overflow: "hidden",
         position: "relative",
         margin: "0 auto",
         ".webiny-pb-page-document, pb-document": {
