80fd7442208b54d13a4c989e14817ac7d3cfc7fc
adrians5j <adrian@webiny.com>
2023-01-13 13:44:35 +0100

    fix: do not render ReCaptcha / TOS if no need

--- apps/theme/layouts/forms/DefaultFormLayout.tsx
+++ apps/theme/layouts/forms/DefaultFormLayout.tsx
@@ -31,7 +31,9 @@ const DefaultFormLayout: FormLayoutComponent = ({
     submit,
     formData,
     ReCaptcha,
-    TermsOfService
+    reCaptchaEnabled,
+    TermsOfService,
+    termsOfServiceEnabled
 }) => {
     // Is the form in loading (submitting) state?
     const [loading, setLoading] = useState(false);
@@ -45,6 +47,7 @@ const DefaultFormLayout: FormLayoutComponent = ({
     /**
      * Once the data is successfully submitted, we show a success message.
      */
+
     const submitForm = async (data: Record<string, any>): Promise<void> => {
         setLoading(true);
         const result = await submit(data);
@@ -74,8 +77,8 @@ const DefaultFormLayout: FormLayoutComponent = ({
                         </Row>
                     ))}
 
-                    <TermsOfServiceSection component={TermsOfService} />
-                    <ReCaptchaSection component={ReCaptcha} />
+                    {termsOfServiceEnabled && <TermsOfServiceSection component={TermsOfService} />}
+                    {reCaptchaEnabled && <ReCaptchaSection component={ReCaptcha} />}
 
                     <SubmitButton onClick={submit} loading={loading}>
                         {formData.settings.submitButtonLabel || "Submit"}
