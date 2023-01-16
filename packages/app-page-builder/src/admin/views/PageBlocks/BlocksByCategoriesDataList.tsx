8bbb971803a40c438aeea9d11622727c4e133309
adrians5j <adrian@webiny.com>
2023-01-16 11:37:51 +0100

    fix: ensure the initially created block has the correct default style-related values

--- packages/app-page-builder/src/admin/views/PageBlocks/BlocksByCategoriesDataList.tsx
+++ packages/app-page-builder/src/admin/views/PageBlocks/BlocksByCategoriesDataList.tsx
@@ -73,6 +73,7 @@ interface Sorter {
     label: string;
     sort: string;
 }
+
 const SORTERS: Sorter[] = [
     {
         label: t`Newest to oldest`,
@@ -171,31 +172,109 @@ const BlocksByCategoriesDataList = ({ canCreate }: PageBuilderBlocksByCategories
                     // Create base block content, and make sure all elements have an `id` property.
                     content: addElementId({
                         type: "block",
-                        // `data` object is required, even if empty.
-                        data: {},
+                        data: {
+                            "settings": {
+                                "width": {
+                                    "desktop": {
+                                        "value": "100%"
+                                    }
+                                },
+                                "margin": {
+                                    "desktop": {
+                                        "top": "0px",
+                                        "right": "0px",
+                                        "bottom": "0px",
+                                        "left": "0px",
+                                        "advanced": true
+                                    }
+                                },
+                                "padding": {
+                                    "desktop": {
+                                        "all": "10px"
+                                    }
+                                },
+                                "horizontalAlignFlex": {
+                                    "desktop": "center"
+                                },
+                                "verticalAlign": {
+                                    "desktop": "flex-start"
+                                }
+                            }
+                        },
                         elements: [
                             {
+                                id: "nbLftfYqrg",
                                 type: "grid",
                                 data: {
                                     settings: {
+                                        width: {
+                                            desktop: {
+                                                value: "1100px"
+                                            }
+                                        },
+                                        margin: {
+                                            desktop: {
+                                                top: "0px",
+                                                right: "0px",
+                                                bottom: "0px",
+                                                left: "0px",
+                                                advanced: true
+                                            }
+                                        },
+                                        padding: {
+                                            desktop: {
+                                                all: "10px"
+                                            }
+                                        },
                                         grid: {
                                             cellsType: "12"
+                                        },
+                                        gridSettings: {
+                                            desktop: {
+                                                flexDirection: "row"
+                                            },
+                                            "mobile-landscape": {
+                                                flexDirection: "column"
+                                            }
+                                        },
+                                        horizontalAlignFlex: {
+                                            desktop: "flex-start"
+                                        },
+                                        verticalAlign: {
+                                            desktop: "flex-start"
                                         }
                                     }
                                 },
                                 elements: [
                                     {
+                                        id: "FoKq0fZfr4",
                                         type: "cell",
                                         data: {
                                             settings: {
+                                                margin: {
+                                                    desktop: {
+                                                        top: "0px",
+                                                        right: "0px",
+                                                        bottom: "0px",
+                                                        left: "0px",
+                                                        advanced: true
+                                                    }
+                                                },
+                                                padding: {
+                                                    desktop: {
+                                                        all: "0px"
+                                                    }
+                                                },
                                                 grid: {
                                                     size: 12
                                                 }
                                             }
                                         },
-                                        elements: []
+                                        elements: [],
+                                        path: ["sUK8viY2oz", "eM2Z1d9gfH", "nbLftfYqrg"]
                                     }
-                                ]
+                                ],
+                                path: ["sUK8viY2oz", "eM2Z1d9gfH"]
                             }
                         ]
                     }),
