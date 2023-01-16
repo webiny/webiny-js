45aaf4456ab2928df75d890de30ca6330c930ada
adrians5j <adrian@webiny.com>
2023-01-16 13:16:47 +0100

    chore: run prettier

--- packages/app-page-builder/src/admin/views/PageBlocks/BlocksByCategoriesDataList.tsx
+++ packages/app-page-builder/src/admin/views/PageBlocks/BlocksByCategoriesDataList.tsx
@@ -171,31 +171,31 @@ const BlocksByCategoriesDataList = ({ canCreate }: PageBuilderBlocksByCategories
                     content: addElementId({
                         type: "block",
                         data: {
-                            "settings": {
-                                "width": {
-                                    "desktop": {
-                                        "value": "100%"
+                            settings: {
+                                width: {
+                                    desktop: {
+                                        value: "100%"
                                     }
                                 },
-                                "margin": {
-                                    "desktop": {
-                                        "top": "0px",
-                                        "right": "0px",
-                                        "bottom": "0px",
-                                        "left": "0px",
-                                        "advanced": true
+                                margin: {
+                                    desktop: {
+                                        top: "0px",
+                                        right: "0px",
+                                        bottom: "0px",
+                                        left: "0px",
+                                        advanced: true
                                     }
                                 },
-                                "padding": {
-                                    "desktop": {
-                                        "all": "10px"
+                                padding: {
+                                    desktop: {
+                                        all: "10px"
                                     }
                                 },
-                                "horizontalAlignFlex": {
-                                    "desktop": "center"
+                                horizontalAlignFlex: {
+                                    desktop: "center"
                                 },
-                                "verticalAlign": {
-                                    "desktop": "flex-start"
+                                verticalAlign: {
+                                    desktop: "flex-start"
                                 }
                             }
                         },
