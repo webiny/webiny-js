e43991dccd58fc5b800bdf15676885c3af73f1e7
adrians5j <adrian@webiny.com>
2023-01-17 13:14:02 +0100

    fix: render `ps-tag` when rendering an existing block

--- packages/app-page-builder/src/render/plugins/elements/block/Block.tsx
+++ packages/app-page-builder/src/render/plugins/elements/block/Block.tsx
@@ -25,6 +25,7 @@ declare global {
 interface BlockProps {
     element: PbElement;
 }
+
 const Block: React.FC<BlockProps> = ({ element }) => {
     const {
         responsiveDisplayMode: { displayMode }
@@ -32,7 +33,9 @@ const Block: React.FC<BlockProps> = ({ element }) => {
 
     return (
         <>
-            <ps-tag data-key={"pb-page-block"} data-value={element.id} />
+            {element.data.blockId && (
+                <ps-tag data-key={"pb-page-block"} data-value={element.data.blockId} />
+            )}
             <ElementAnimation>
                 <ElementRoot element={element}>
                     {({ elementStyle, elementAttributes, customClasses, combineClassNames }) => {
