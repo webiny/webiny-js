d6bbee7042d03b1e2a8674d128a46df381e45009
Pavel Denisjuk <pavel@webiny.com>
2023-01-25 13:45:02 +0100

    chore(app): improve function docs

--- packages/app/src/core/createProviderPlugin.tsx
+++ packages/app/src/core/createProviderPlugin.tsx
@@ -3,7 +3,8 @@ import { HigherOrderComponent } from "@webiny/react-composition";
 import { Provider } from "./Provider";
 
 /**
- * Creates a component, which, when mounted, will register an admin app provider.
+ * Creates a component, which, when mounted, will register an app provider.
+ * This is particularly useful for wrapping the entire app with custom React Context providers.
  * For more information, visit https://www.webiny.com/docs/admin-area/basics/framework.
  */
 export function createProviderPlugin(hoc: HigherOrderComponent): React.FC {
