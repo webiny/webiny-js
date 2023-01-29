62ee41d391363b0fb40abdb437b937dbfc02e051
Bruno Zorić <bruno.zoric@gmail.com>
2023-01-29 19:37:36 +0100

    fix(api-elasticsearch): chars to replace with space

--- packages/api-elasticsearch/src/normalize.ts
+++ packages/api-elasticsearch/src/normalize.ts
@@ -3,6 +3,8 @@
  * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters
  */
 
+const specialCharacterToSpace = ["-"];
+
 const specialCharacters = [
     "\\\\",
     "\\+",
@@ -30,7 +32,11 @@ const specialCharacters = [
 ];
 
 export const normalizeValue = (value: string) => {
-    let result = value.replaceAll("-", " ");
+    let result = value || "";
+    for (const character of specialCharacterToSpace) {
+        result = result.replace(new RegExp(character, "g"), " ");
+    }
+
     for (const character of specialCharacters) {
         result = result.replace(new RegExp(character, "g"), `\\${character}`);
     }
