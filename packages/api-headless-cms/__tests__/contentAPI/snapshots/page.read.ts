1c42b484ba8a7934640f1946e5fe7bf8abb7116c
Bruno Zorić <bruno.zoric@gmail.com>
2023-01-30 10:29:38 +0100

    fix(api-headless-cms): move modelId field to original gql type (#2991)

--- packages/api-headless-cms/__tests__/contentAPI/snapshots/page.read.ts
+++ packages/api-headless-cms/__tests__/contentAPI/snapshots/page.read.ts
@@ -5,6 +5,7 @@ export default `
     type Page {
         id: ID!
         entryId: String!
+        modelId: String!
         createdOn: DateTime!
         savedOn: DateTime!
         createdBy: CmsCreatedBy!
@@ -12,10 +13,6 @@ export default `
         content: [Page_Content!]
         header: Page_Header
         objective: Page_Objective
-    }
-    
-    extend type Page {
-        modelId: String!
     }
 
     union Page_Content = Page_Content_Hero | Page_Content_SimpleText | Page_Content_Objecting
