f2cb77d238e30e4eaa86722ff41349127e776d63
adrians5j <adrian@webiny.com>
2023-02-07 16:03:51 +0100

    fix: remove `window.aosInitialized` flag

--- packages/app-page-builder-elements/src/modifiers/attributes/animation/initializeAos.ts
+++ packages/app-page-builder-elements/src/modifiers/attributes/animation/initializeAos.ts
@@ -1,9 +1,3 @@
-declare global {
-    interface Window {
-        aosInitialized: boolean;
-    }
-}
-
 let pbDocumentCheck: Promise<void>;
 
 export const initializeAos = async () => {
@@ -11,13 +5,6 @@ export const initializeAos = async () => {
     // these dependencies only when the attributes modifier is actually used.
     // Additionally, we only want to do this in the browser, hence the window check.
     if (typeof window === "undefined") {
-        return;
-    }
-
-    // By assigning this flag into the `window` object, we get to ensure
-    // that AOS is not initialized multiple times by multiple `createAnimation`
-    // calls. This is something that's possible in the Admin application.
-    if (window.aosInitialized) {
         return;
     }
 
@@ -38,6 +25,5 @@ export const initializeAos = async () => {
     await import("aos/dist/aos.css");
     const aos = await import("aos");
 
-    window.aosInitialized = true;
     aos.init();
 };
