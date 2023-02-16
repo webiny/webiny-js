4d930b37766e49f84b4c94e8aafa404fd42bcc36
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-02-16 15:37:01 +0100

    chore: pass apply as param instead of new ContextPlugin

--- packages/api-aco/src/createAcoContext.ts
+++ packages/api-aco/src/createAcoContext.ts
@@ -10,49 +10,48 @@ import { isInstallationPending } from "~/utils/isInstallationPending";
 
 import { AcoContext } from "~/types";
 
-const setupAcoContext = () =>
-    new ContextPlugin<AcoContext>(async context => {
-        const { tenancy, security, i18n } = context;
+const setupAcoContext = (context: AcoContext) => {
+    const { tenancy, security, i18n } = context;
 
-        if (isInstallationPending({ tenancy, i18n })) {
-            return;
+    if (isInstallationPending({ tenancy, i18n })) {
+        return;
+    }
+
+    const getLocale = (): I18NLocale => {
+        const locale = i18n.getContentLocale();
+        if (!locale) {
+            throw new WebinyError(
+                "Missing content locale in api-aco/plugins/context.ts",
+                "LOCALE_ERROR"
+            );
         }
 
-        const getLocale = (): I18NLocale => {
-            const locale = i18n.getContentLocale();
-            if (!locale) {
-                throw new WebinyError(
-                    "Missing content locale in api-aco/plugins/context.ts",
-                    "LOCALE_ERROR"
-                );
-            }
+        return locale;
+    };
 
-            return locale;
-        };
+    const getTenant = (): Tenant => {
+        return tenancy.getCurrentTenant();
+    };
 
-        const getTenant = (): Tenant => {
-            return tenancy.getCurrentTenant();
-        };
+    const getIdentity = () => security.getIdentity();
 
-        const getIdentity = () => security.getIdentity();
-
-        context.aco = createAcoCrud({
-            getLocale,
-            getIdentity,
-            getTenant,
-            storageOperations: createAcoStorageOperations({
-                /**
-                 * TODO: We need to figure out a way to pass "cms" from outside (e.g. apps/api/graphql)
-                 */
-                cms: context.cms,
-                /**
-                 * TODO: This is required for "entryFieldFromStorageTransform" which access plugins from context.
-                 */
-                getCmsContext: () => context,
-                security
-            })
-        });
+    context.aco = createAcoCrud({
+        getLocale,
+        getIdentity,
+        getTenant,
+        storageOperations: createAcoStorageOperations({
+            /**
+             * TODO: We need to figure out a way to pass "cms" from outside (e.g. apps/api/graphql)
+             */
+            cms: context.cms,
+            /**
+             * TODO: This is required for "entryFieldFromStorageTransform" which access plugins from context.
+             */
+            getCmsContext: () => context,
+            security
+        })
     });
+};
 
 export const createAcoContext = () => {
     return new ContextPlugin<AcoContext>(async context => {
@@ -60,7 +59,7 @@ export const createAcoContext = () => {
             return;
         }
 
-        await setupAcoContext().apply(context);
-        await createAcoHooks().apply(context);
+        await setupAcoContext(context);
+        await createAcoHooks(context);
     });
 };
--- packages/api-aco/src/createAcoHooks.ts
+++ packages/api-aco/src/createAcoHooks.ts
@@ -1,10 +1,7 @@
-import { ContextPlugin } from "@webiny/api";
-
 import { onFolderBeforeDeleteHook } from "~/folder/onFolderBeforeDelete.hook";
 
 import { AcoContext } from "~/types";
 
-export const createAcoHooks = () =>
-    new ContextPlugin<AcoContext>(async context => {
-        onFolderBeforeDeleteHook(context);
-    });
+export const createAcoHooks = (context: AcoContext) => {
+    onFolderBeforeDeleteHook(context);
+};
