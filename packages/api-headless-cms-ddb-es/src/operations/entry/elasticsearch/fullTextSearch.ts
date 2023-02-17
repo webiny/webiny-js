8e08540696f7cdface1d4afcd593e8ea74e158b3
Bruno Zorić <bruno.zoric@gmail.com>
2023-02-17 13:00:55 +0100

    fix(api-headless-cms-ddb-es): full text search via plugin

--- packages/api-headless-cms-ddb-es/src/operations/entry/elasticsearch/fullTextSearch.ts
+++ packages/api-headless-cms-ddb-es/src/operations/entry/elasticsearch/fullTextSearch.ts
@@ -1,28 +1,78 @@
 import { ElasticsearchBoolQueryConfig } from "@webiny/api-elasticsearch/types";
 import { normalizeValue } from "@webiny/api-elasticsearch";
-import { CmsModelField } from "@webiny/api-headless-cms/types";
+import { CmsModel, CmsModelField } from "@webiny/api-headless-cms/types";
+import { PluginsContainer } from "@webiny/plugins";
+import {
+    CmsEntryElasticsearchFullTextSearchPlugin,
+    createCmsEntryElasticsearchFullTextSearchPlugin
+} from "~/plugins";
+
+const defaultPlugin = createCmsEntryElasticsearchFullTextSearchPlugin({
+    apply: params => {
+        const { query, term, fields, createFieldPath } = params;
+
+        query.must.push({
+            query_string: {
+                allow_leading_wildcard: true,
+                fields: fields.map(createFieldPath),
+                query: normalizeValue(term),
+                default_operator: "and"
+            }
+        });
+    }
+});
+defaultPlugin.name = "headless-cms.elasticsearch.entry.fullTextSearch.default";
+
+interface GetPluginParams {
+    container: PluginsContainer;
+    model: CmsModel;
+}
+const getPlugin = (params: GetPluginParams): CmsEntryElasticsearchFullTextSearchPlugin => {
+    const { container, model } = params;
+    const plugins = container
+        .byType<CmsEntryElasticsearchFullTextSearchPlugin>(
+            CmsEntryElasticsearchFullTextSearchPlugin.type
+        )
+        .reverse();
+    /**
+     * We need to find the most specific plugin for the given model.
+     * Also, we need to use the first possible plugin if the specific one is not found.
+     */
+    let plugin: CmsEntryElasticsearchFullTextSearchPlugin | null = null;
+    for (const pl of plugins) {
+        if (pl.models.includes(model.modelId)) {
+            return pl;
+        } else if (!plugin) {
+            plugin = pl;
+        }
+    }
+
+    return plugin || defaultPlugin;
+};
 
 interface Params {
+    plugins: PluginsContainer;
+    model: CmsModel;
     query: ElasticsearchBoolQueryConfig;
     term?: string;
     fields: CmsModelField[];
 }
 export const applyFullTextSearch = (params: Params): void => {
-    const { query, term, fields } = params;
+    const { plugins, query, term, fields, model } = params;
     if (!term || term.length === 0 || fields.length === 0) {
         return;
     }
 
-    const fieldPaths = fields.map(field => {
-        return `values.${field.storageId}`;
+    const plugin = getPlugin({
+        container: plugins,
+        model
     });
 
-    query.must.push({
-        query_string: {
-            allow_leading_wildcard: true,
-            fields: fieldPaths,
-            query: normalizeValue(term),
-            default_operator: "or"
-        }
+    plugin.apply({
+        model,
+        createFieldPath: field => `values.${field.storageId}`,
+        fields,
+        query,
+        term
     });
 };
