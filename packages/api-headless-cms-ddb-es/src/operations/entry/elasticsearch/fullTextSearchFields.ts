ef00a01638534d5919c26f368a6c358e97d674dd
Bruno Zorić <bruno.zoric@gmail.com>
2023-02-17 14:47:36 +0100

    fix(api-headless-cms): full text search (#3025)

--- packages/api-headless-cms-ddb-es/src/operations/entry/elasticsearch/fullTextSearchFields.ts
+++ packages/api-headless-cms-ddb-es/src/operations/entry/elasticsearch/fullTextSearchFields.ts
@@ -1,16 +1,4 @@
-/* eslint-disable */
 import { CmsModel, CmsModelField } from "@webiny/api-headless-cms/types";
-
-const extractFields = (fields: CmsModelField[]): string[] => {
-    return fields.reduce<string[]>((collection, field) => {
-        if (field.settings?.fields) {
-            collection.push(...extractFields(field.settings.fields));
-        } else if (field) {
-        }
-
-        return collection;
-    }, []);
-};
 
 interface Params {
     model: CmsModel;
@@ -19,21 +7,15 @@ interface Params {
 }
 export const createFullTextSearchFields = (params: Params): CmsModelField[] => {
     const { term, model, fields } = params;
-    if (!fields || fields.length === 0 || !term || term.length === 0) {
+    if (!fields || fields.length === 0 || !term || term.trim().length === 0) {
         return [];
     }
-    const fullTextSearchFields: CmsModelField[] = [];
-    /**
-     * No point in going through fields if there is no search performed - no search term.
-     */
-    if (!!term) {
-        for (const fieldId of fields) {
-            const field = model.fields.find(f => f.fieldId === fieldId);
-            if (!field) {
-                continue;
-            }
-            fullTextSearchFields.push(field);
+    return fields.reduce<CmsModelField[]>((collection, fieldId) => {
+        const field = model.fields.find(f => f.fieldId === fieldId);
+        if (!field) {
+            return collection;
         }
-    }
-    return fullTextSearchFields;
+        collection.push(field);
+        return collection;
+    }, []);
 };
