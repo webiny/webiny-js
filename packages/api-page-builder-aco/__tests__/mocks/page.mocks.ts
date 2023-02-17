9ec0b9526418f146acf3747e289456222875c2da
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-02-17 18:13:22 +0100

    test: add tests to api-page-builder-aco package

---
+++ packages/api-page-builder-aco/__tests__/mocks/page.mocks.ts
@@ -0,0 +1,274 @@
+export const pageContentMock = {
+    id: "fGe7HcBw7Y",
+    type: "document",
+    data: {
+        settings: {}
+    },
+    elements: [
+        {
+            id: "QbeB2uD7Iz",
+            type: "block",
+            data: {
+                settings: {
+                    width: {
+                        desktop: {
+                            value: "100%"
+                        }
+                    },
+                    margin: {
+                        desktop: {
+                            top: "0px",
+                            right: "0px",
+                            bottom: "0px",
+                            left: "0px",
+                            advanced: true
+                        }
+                    },
+                    padding: {
+                        desktop: {
+                            all: "10px"
+                        }
+                    },
+                    horizontalAlignFlex: {
+                        desktop: "center"
+                    },
+                    verticalAlign: {
+                        desktop: "flex-start"
+                    }
+                }
+            },
+            elements: [
+                {
+                    id: "ldZzIsJuqA",
+                    type: "grid",
+                    data: {
+                        settings: {
+                            width: {
+                                desktop: {
+                                    value: "1100px"
+                                }
+                            },
+                            margin: {
+                                desktop: {
+                                    top: "0px",
+                                    right: "0px",
+                                    bottom: "0px",
+                                    left: "0px",
+                                    advanced: true
+                                }
+                            },
+                            padding: {
+                                desktop: {
+                                    all: "10px"
+                                }
+                            },
+                            grid: {
+                                cellsType: "12"
+                            },
+                            gridSettings: {
+                                desktop: {
+                                    flexDirection: "row"
+                                },
+                                "mobile-landscape": {
+                                    flexDirection: "column"
+                                }
+                            },
+                            horizontalAlignFlex: {
+                                desktop: "flex-start"
+                            },
+                            verticalAlign: {
+                                desktop: "flex-start"
+                            }
+                        }
+                    },
+                    elements: [
+                        {
+                            id: "5qKZ6O3RVT",
+                            type: "cell",
+                            data: {
+                                settings: {
+                                    margin: {
+                                        desktop: {
+                                            top: "0px",
+                                            right: "0px",
+                                            bottom: "0px",
+                                            left: "0px",
+                                            advanced: true
+                                        }
+                                    },
+                                    padding: {
+                                        desktop: {
+                                            all: "0px"
+                                        }
+                                    },
+                                    grid: {
+                                        size: 12
+                                    }
+                                }
+                            },
+                            elements: [
+                                {
+                                    id: "ktdB9geeLu",
+                                    type: "heading",
+                                    data: {
+                                        text: {
+                                            desktop: {
+                                                type: "heading",
+                                                typography: "heading1",
+                                                alignment: "left",
+                                                tag: "h1"
+                                            },
+                                            data: {
+                                                text: "Demo Heading"
+                                            }
+                                        },
+                                        settings: {
+                                            margin: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            },
+                                            padding: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            }
+                                        }
+                                    },
+                                    elements: [],
+                                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA", "5qKZ6O3RVT"]
+                                },
+                                {
+                                    id: "mxlD3Lq3dj",
+                                    type: "paragraph",
+                                    data: {
+                                        text: {
+                                            desktop: {
+                                                type: "paragraph",
+                                                typography: "paragraph1",
+                                                alignment: "left",
+                                                tag: "p"
+                                            },
+                                            data: {
+                                                text: "Demo Content"
+                                            }
+                                        },
+                                        settings: {
+                                            margin: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            },
+                                            padding: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            }
+                                        }
+                                    },
+                                    elements: [],
+                                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA", "5qKZ6O3RVT"]
+                                },
+                                {
+                                    id: "Oi8YOYIbHR",
+                                    type: "button",
+                                    data: {
+                                        type: "default",
+                                        buttonText: "Demo button",
+                                        settings: {
+                                            margin: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            },
+                                            horizontalAlignFlex: {
+                                                desktop: "center"
+                                            }
+                                        },
+                                        action: {
+                                            href: "",
+                                            newTab: false,
+                                            actionType: "link"
+                                        }
+                                    },
+                                    elements: [],
+                                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA", "5qKZ6O3RVT"]
+                                },
+                                {
+                                    id: "Z3doBvH0MS",
+                                    type: "image",
+                                    data: {
+                                        settings: {
+                                            horizontalAlignFlex: {
+                                                desktop: "center"
+                                            },
+                                            margin: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            },
+                                            padding: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            }
+                                        },
+                                        image: {
+                                            file: {
+                                                id: "63eb4d956efa13000897445b",
+                                                src: "demo-image-1.jpg"
+                                            },
+                                            title: "Demo Image 1"
+                                        },
+                                        link: {
+                                            href: ""
+                                        }
+                                    },
+                                    elements: [],
+                                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA", "5qKZ6O3RVT"]
+                                },
+                                {
+                                    id: "vuH9hoA8PA",
+                                    type: "images-list",
+                                    data: {
+                                        component: "mosaic",
+                                        settings: {
+                                            margin: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            },
+                                            padding: {
+                                                desktop: {
+                                                    all: "0px"
+                                                }
+                                            }
+                                        },
+                                        images: [
+                                            {
+                                                id: "63eb4d956efa13000897445b",
+                                                src: "demo-image-2.jpg",
+                                                name: "Demo Image 2"
+                                            },
+                                            {
+                                                id: "63eb4d956efa13000897556g",
+                                                src: "demo-image-3.jpg",
+                                                name: "Demo Image 3"
+                                            }
+                                        ]
+                                    },
+                                    elements: [],
+                                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA"]
+                                }
+                            ],
+                            path: ["fGe7HcBw7Y", "QbeB2uD7Iz", "ldZzIsJuqA"]
+                        }
+                    ],
+                    path: ["fGe7HcBw7Y", "QbeB2uD7Iz"]
+                }
+            ],
+            path: ["fGe7HcBw7Y"]
+        }
+    ],
+    path: []
+};
