e8c68114389c8fdb0ecc9b6f2ee1b236036fed0a
adrians5j <adrian@webiny.com>
2023-03-03 11:17:31 +0100

    fix: create `addDomainsUrlsOutputs` utility

---
+++ packages/pulumi-aws/src/utils/addDomainsUrlsOutputs.ts
@@ -0,0 +1,43 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { PulumiApp, PulumiAppResource } from "@webiny/pulumi";
+
+interface AddDomainsUrlsOutputsParams {
+    cloudfrontDistribution: PulumiAppResource<typeof aws.cloudfront.Distribution>;
+    app: PulumiApp;
+    map: {
+        distributionDomain: string;
+        distributionUrl: string;
+        usedDomain: string;
+        usedUrl: string;
+    };
+}
+
+export const addDomainsUrlsOutputs = (params: AddDomainsUrlsOutputsParams) => {
+    const { cloudfrontDistribution, app, map } = params;
+
+    cloudfrontDistribution.output.aliases.apply(aliases => {
+        // These will always contain the default Cloudfront domain,
+        // no matter if the user provided a custom domain or not.
+        const distributionDomain = cloudfrontDistribution.output.domainName;
+        const distributionUrl = distributionDomain.apply(value => `https://${value}`);
+
+        // These will contain a custom domain if provided,
+        // otherwise again the default Cloudfront domain.
+        let usedDomain = distributionDomain;
+        let usedUrl = distributionUrl;
+
+        const [firstAlias] = aliases || [];
+        if (firstAlias) {
+            usedDomain = pulumi.output(firstAlias);
+            usedUrl = pulumi.output(`https://${firstAlias}`);
+        }
+
+        app.addOutputs({
+            [map.distributionDomain]: distributionDomain,
+            [map.distributionUrl]: distributionUrl,
+            [map.usedDomain]: usedDomain,
+            [map.usedUrl]: usedUrl
+        });
+    });
+};
