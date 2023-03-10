3039763b761d1a981487120e6b5990a0d1051453
Pavel Denisjuk <pavel@webiny.com>
2023-03-10 21:07:51 +0100

    fix(serverless-cms-aws): add FB plugins to prebuilt handlers

--- packages/serverless-cms-aws/handlers/ddb-es/api/pageBuilder/import/process/src/index.ts
+++ packages/serverless-cms-aws/handlers/ddb-es/api/pageBuilder/import/process/src/index.ts
@@ -2,6 +2,8 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { createHandler } from "@webiny/handler-aws/raw";
 import i18nPlugins from "@webiny/api-i18n/graphql";
 import i18nDynamoDbStorageOperations from "@webiny/api-i18n-ddb";
+import { createFormBuilder } from "@webiny/api-form-builder";
+import { createFormBuilderStorageOperations } from "@webiny/api-form-builder-so-ddb-es";
 import {
     createPageBuilderGraphQL,
     createPageBuilderContext
@@ -56,6 +58,12 @@ export const handler = createHandler({
             })
         }),
         createPageBuilderGraphQL(),
+        createFormBuilder({
+            storageOperations: createFormBuilderStorageOperations({
+                documentClient,
+                elasticsearch: elasticsearchClient
+            })
+        }),
         pageBuilderImportExportPlugins({
             storageOperations: createPageBuilderImportExportStorageOperations({ documentClient })
         }),
