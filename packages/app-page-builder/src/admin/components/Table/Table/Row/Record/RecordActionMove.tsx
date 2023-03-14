ea737da6a56dc9e1603612661d3f6b5fe5ab7880
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-03-14 11:48:23 +0100

    feat: add icon to move action

--- packages/app-page-builder/src/admin/components/Table/Table/Row/Record/RecordActionMove.tsx
+++ packages/app-page-builder/src/admin/components/Table/Table/Row/Record/RecordActionMove.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from "react";
 
+import { ReactComponent as Move } from "@material-design-icons/svg/filled/drive_file_move.svg";
 import { i18n } from "@webiny/app/i18n";
+import { Icon } from "@webiny/ui/Icon";
 import { MenuItem } from "@webiny/ui/Menu";
+
+import { ListItemGraphic } from "~/admin/components/Table/Table/styled";
 
 const t = i18n.ns("app-headless-cms/app-page-builder/pages-table/actions/page/move");
 
@@ -9,5 +13,13 @@ interface Props {
     onClick: () => void;
 }
 export const RecordActionMove = ({ onClick }: Props): ReactElement => {
-    return <MenuItem onClick={onClick}>{t`Move`}</MenuItem>;
+    return (
+        <MenuItem onClick={onClick}>
+            {" "}
+            <ListItemGraphic>
+                <Icon icon={<Move />} />
+            </ListItemGraphic>
+            {t`Move`}
+        </MenuItem>
+    );
 };
