812060de9572020afbe0ba85fb2507dd2d855ad0
Pavel Denisjuk <pavel@webiny.com>
2023-03-18 20:57:21 +0100

    feat: build packages in parallel threads to use all CPUs

---
+++ scripts/buildPackages/src/buildPackages.ts
@@ -0,0 +1,102 @@
+import { green } from "chalk";
+import yargs from "yargs";
+import writeJson from "write-json-file";
+import { Listr, ListrTask } from "listr2";
+import { getBatches } from "./getBatches";
+import { META_FILE_PATH } from "./constants";
+import { getPackageSourceHash } from "./getPackageSourceHash";
+import { getBuildMeta } from "./getBuildMeta";
+import { buildPackageInNewProcess } from "./buildSinglePackage";
+import { MetaJSON, Package } from "./types";
+
+interface BuildOptions {
+    debug?: boolean;
+    cache?: boolean;
+    buildOverrides?: string;
+}
+
+interface BuildContext {
+    [key: string]: boolean;
+}
+
+export const buildPackages = async () => {
+    const options = yargs.argv as BuildOptions;
+
+    const { batches, packagesNoCache, allPackages } = await getBatches({
+        cache: options.cache ?? true
+    });
+
+    if (!packagesNoCache.length) {
+        console.log("There are no packages that need to be built.");
+        return;
+    }
+
+    if (packagesNoCache.length > 10) {
+        console.log(`\nRunning build for ${green(packagesNoCache.length)} packages.`);
+    } else {
+        console.log("\nRunning build for the following packages:");
+        for (let i = 0; i < packagesNoCache.length; i++) {
+            const item = packagesNoCache[i];
+            console.log(`‣ ${green(item.packageJson.name)}`);
+        }
+    }
+
+    console.log(
+        `\nThe build process will be performed in ${green(batches.length)} ${
+            batches.length > 1 ? "batches" : "batch"
+        }.\n`
+    );
+    const metaJson = getBuildMeta();
+
+    const tasks = new Listr<BuildContext>(
+        batches.map<ListrTask>((packageNames, index) => {
+            const id = `${index + 1}`.padStart(2, "0");
+            const title = `[${id}/${batches.length}] Batch #${id} (${packageNames.length} packages)`;
+
+            return {
+                title,
+                task: (ctx, task): Listr => {
+                    const packages = allPackages.filter(pkg => packageNames.includes(pkg.name));
+
+                    const batchTasks = task.newListr([], {
+                        concurrent: true,
+                        exitOnError: true
+                    });
+
+                    packages.forEach(pkg => {
+                        batchTasks.add(createPackageTask(pkg, options, metaJson));
+                    });
+
+                    return batchTasks;
+                }
+            };
+        }),
+        { concurrent: false, rendererOptions: { showTimer: true, collapse: true } }
+    );
+
+    const start = Date.now();
+    const ctx = {};
+    await tasks.run(ctx);
+    const duration = (Date.now() - start) / 1000;
+
+    console.log(`\nBuild finished in ${green(duration)} seconds.`);
+};
+
+const createPackageTask = (pkg: Package, options: BuildOptions, metaJson: MetaJSON) => {
+    return {
+        title: `${pkg.name}`,
+        task: async () => {
+            try {
+                await buildPackageInNewProcess(pkg, options.buildOverrides);
+
+                // Store package hash
+                const sourceHash = await getPackageSourceHash(pkg);
+                metaJson.packages[pkg.packageJson.name] = { sourceHash };
+
+                await writeJson(META_FILE_PATH, metaJson);
+            } catch (err) {
+                throw new Error(`[${pkg.packageJson.name}] ${err.message}`);
+            }
+        }
+    };
+};
