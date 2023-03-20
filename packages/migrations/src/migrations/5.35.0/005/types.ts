30657eb8cbc90875a741dbc1d7d51014223393da
Bruno Zorić <bruno.zoric@gmail.com>
2023-03-20 14:32:06 +0100

    fix(migrations): package dependencies

--- packages/migrations/src/migrations/5.35.0/005/types.ts
+++ packages/migrations/src/migrations/5.35.0/005/types.ts
@@ -1,19 +1,39 @@
-import { CmsModel as BaseCmsModel } from "@webiny/api-headless-cms/types";
-
 export interface Tenant {
     id: string;
     name: string;
 }
 
-export interface CmsModel
-    extends Omit<BaseCmsModel, "singularApiName" | "pluralApiName">,
-        Pick<Partial<BaseCmsModel>, "singularApiName" | "pluralApiName"> {
+export interface CmsModel {
     PK: string;
     SK: string;
     TYPE: string;
     _ct: string;
     _et: string;
     _md: string;
+    name: string;
+    modelId: string;
+    singularApiName?: string;
+    pluralApiName?: string;
+    tenant: string;
+    locale: string;
+    group: {
+        id: string;
+        name: string;
+    };
+    description: string;
+    createdOn?: string;
+    savedOn?: string;
+    createdBy: {
+        id: string;
+        type: string;
+        displayName: string;
+    };
+    fields: any[];
+    layout: string[][];
+    tags?: string[];
+    lockedFields: any[];
+    titleFieldId: string;
+    webinyVersion: string;
 }
 
 export interface I18NLocale {
