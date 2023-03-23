3a58331035c41f64a04a3e3024ee4d4293cd6f7b
Bruno Zorić <bruno.zoric@gmail.com>
2023-03-23 15:19:39 +0100

    refactor(app-typeform): replace all fc with vfc

--- packages/app-typeform/src/admin/TypeFormEmbed.tsx
+++ packages/app-typeform/src/admin/TypeFormEmbed.tsx
@@ -6,7 +6,7 @@ import useRenderEmptyEmbed from "@webiny/app-page-builder/editor/plugins/element
 type TypeFormEmbedPropsType = {
     elementId: string;
 };
-const TypeFormEmbed: React.FC<TypeFormEmbedPropsType> = ({ elementId }) => {
+const TypeFormEmbed: React.VFC<TypeFormEmbedPropsType> = ({ elementId }) => {
     const element = useRecoilValue(elementByIdSelector(elementId));
     const renderEmpty = useRenderEmptyEmbed(element);
 
