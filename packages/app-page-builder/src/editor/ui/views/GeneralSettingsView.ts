a762b5835b235f11e51cf4f9c374238f2969f5c7
adrians5j <adrian@webiny.com>
2023-03-27 18:17:30 +0200

    fix: improve description

--- packages/app-page-builder/src/editor/ui/views/GeneralSettingsView.ts
+++ packages/app-page-builder/src/editor/ui/views/GeneralSettingsView.ts
@@ -30,7 +30,7 @@ export class GeneralSettingsView extends PageSettingsFormView {
             new InputElement("title", {
                 name: "title",
                 label: "Title",
-                description: "Page title.",
+                description: "Page title. Will be used on the public website if the SEO title wasn't specified (SEO settings tab).",
                 validators: () => validation.create("required")
             })
         );
