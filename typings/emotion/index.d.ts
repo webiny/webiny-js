79bb7b93f2348b668437aa7949c3335aa7ef1461
adrians5j <adrian@webiny.com>
2023-03-30 15:56:00 +0200

    fix: instead of importing, c/p types into the file
    This fixes an issue in our build system.

--- typings/emotion/index.d.ts
+++ typings/emotion/index.d.ts
@@ -1,8 +1,47 @@
-import {Theme as WTheme} from "../../packages/theme/src/types";
+/**
+ * We are copy/pasting the `@webiny/theme/types` file here because of an issue
+ * with our build system. This is not the case in users' projects. There we
+ * simply import the types from `@webiny/theme` packages. See:
+ * packages/cwp-template-aws/template/common/types/emotion/index.d.ts
+ */
 
-declare module "@emotion/react" {
-    // Ignoring "@typescript-eslint/no-empty-interface" rule here.
-    // eslint-disable-next-line
-    export interface Theme extends WTheme {
-    }
+import { type CSSObject } from "@emotion/react";
+
+export type Content = Element;
+
+/**
+ * Should be a `CSSObject` object or an object with breakpoint names as keys and `CSSObject` objects as values.
+ */
+export interface StylesObject {
+    [key: string]: CSSObject | string | number | undefined;
+}
+
+export enum DefaultThemeBreakpoint {
+    DESKTOP = "desktop",
+    TABLET = "tablet",
+    MOBILE_LANDSCAPE = "mobile-landscape",
+    MOBILE_PORTRAIT = "mobile-portrait"
+}
+
+export type ThemeBreakpoints = {
+    [DefaultThemeBreakpoint.DESKTOP]: string;
+    [DefaultThemeBreakpoint.TABLET]: string;
+    [DefaultThemeBreakpoint.MOBILE_LANDSCAPE]: string;
+    [DefaultThemeBreakpoint.MOBILE_PORTRAIT]: string;
+
+    [key: string]: string;
+};
+
+export interface ThemeStyles {
+    colors: Record<string, any>;
+    borderRadius?: number;
+    typography: Record<string, StylesObject>;
+    elements: Record<string, Record<string, any> | StylesObject>;
+
+    [key: string]: any;
+}
+
+export interface Theme {
+    breakpoints: ThemeBreakpoints;
+    styles: ThemeStyles;
 }
