9d6c67f359a3e489658e2f28459bb2379751a592
adrians5j <adrian@webiny.com>
2023-03-30 11:55:31 +0200

    fix: import using relative path

--- typings/emotion/index.d.ts
+++ typings/emotion/index.d.ts
@@ -1,7 +1,8 @@
-import { Theme as WTheme } from "@webiny/theme/types";
+import {Theme as WTheme} from "../../packages/theme/src/types";
 
 declare module "@emotion/react" {
     // Ignoring "@typescript-eslint/no-empty-interface" rule here.
     // eslint-disable-next-line
-    export interface Theme extends WTheme {}
+    export interface Theme extends WTheme {
+    }
 }
