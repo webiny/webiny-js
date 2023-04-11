df2bc2549f9d7660c1a5b45fb844c944666e1520
adrians5j <adrian@webiny.com>
2023-04-11 22:32:16 +0200

    feat: in local dev, if tenant ID is missing, show an informative error message

--- packages/app-website/src/Page/index.tsx
+++ packages/app-website/src/Page/index.tsx
@@ -69,7 +69,7 @@ export const Page: React.FC = () => {
     // Here we get all site data like website name, favicon image, social links etc.
     const getSettingsQuery = useQuery<SettingsQueryResponse>(GET_SETTINGS);
 
-    const { data: page = null, error = null } =
+    const { data: page = null, error = getPublishedPageQuery.error } =
         getPublishedPageQuery.data?.pageBuilder?.getPublishedPage || {};
     const settings =
         getSettingsQuery.data?.pageBuilder?.getSettings?.data || ({} as SettingsQueryResponseData);
