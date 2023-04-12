09ff40916ea868a52f1aca497c8f3f8a32757d49
Bruno Zorić <bruno.zoric@gmail.com>
2023-04-12 11:34:12 +0200

    refactor(api-headless-cms-ddb-es): use withoutAuthorization

--- packages/api-headless-cms-ddb-es/__tests__/graphql/dummyLocales.ts
+++ packages/api-headless-cms-ddb-es/__tests__/graphql/dummyLocales.ts
@@ -5,23 +5,22 @@ export const createDummyLocales = () => {
     return new ContextPlugin<CmsContext>(async context => {
         const { i18n, security } = context;
 
-        await security.authenticate("");
-        security.disableAuthorization();
-        const [items] = await i18n.locales.listLocales({
-            where: {}
+        await security.withoutAuthorization(async () => {
+            await security.authenticate("");
+            const [items] = await i18n.locales.listLocales({
+                where: {}
+            });
+            if (items.length > 0) {
+                return;
+            }
+            await i18n.locales.createLocale({
+                code: "en-US",
+                default: true
+            });
+            await i18n.locales.createLocale({
+                code: "de-DE",
+                default: true
+            });
         });
-        if (items.length > 0) {
-            security.enableAuthorization();
-            return;
-        }
-        await i18n.locales.createLocale({
-            code: "en-US",
-            default: true
-        });
-        await i18n.locales.createLocale({
-            code: "de-DE",
-            default: true
-        });
-        security.enableAuthorization();
     });
 };
