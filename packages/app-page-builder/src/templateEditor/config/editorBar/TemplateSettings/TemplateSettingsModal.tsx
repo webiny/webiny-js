72b5ddea6629a028084c39f0a8ceade9efb88acb
adrians5j <adrian@webiny.com>
2023-04-19 10:59:02 +0200

    chore: run prettier

--- packages/app-page-builder/src/templateEditor/config/editorBar/TemplateSettings/TemplateSettingsModal.tsx
+++ packages/app-page-builder/src/templateEditor/config/editorBar/TemplateSettings/TemplateSettingsModal.tsx
@@ -119,7 +119,12 @@ const TemplateSettingsModal: React.FC = () => {
                                                 name="slug"
                                                 validators={[validation.create("required")]}
                                             >
-                                                <Input label="Slug" description={"Slug should not be changed if there are already existing pages that are using this template."}/>
+                                                <Input
+                                                    label="Slug"
+                                                    description={
+                                                        "Slug should not be changed if there are already existing pages that are using this template."
+                                                    }
+                                                />
                                             </Bind>
                                         </Cell>
                                         <Cell span={12}>
