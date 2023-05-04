8cf01aa970502f830e49c2b499254f9132dea3cc
adrians5j <adrian@webiny.com>
2023-05-04 12:11:25 +0200

    fix: show the name of the website router origin request Lambda fn

--- packages/serverless-cms-aws/src/website/plugins/lambdaEdgeWarning.ts
+++ packages/serverless-cms-aws/src/website/plugins/lambdaEdgeWarning.ts
@@ -1,19 +1,40 @@
 import { CliContext } from "@webiny/cli/types";
+import { getStackOutput } from "@webiny/cli-plugin-deploy-pulumi/utils";
 
 /**
  * This hook is used to display a warning message to the user, that notifies
  * the user that the Lambda@Edge functions are not automatically deleted.
  * Deletion should be done manually.
  */
-export const lambdaEdgeWarning = {
-    type: "hook-after-destroy",
-    name: "hook-after-destroy-lambda-edge-warning",
-    hook(params: Record<string, any>, context: CliContext) {
-        if (process.env.WCP_PROJECT_ENVIRONMENT || process.env.WEBINY_MULTI_TENANCY === "true") {
-            context.warning(
-                "Lambda@Edge function %s was not automatically deleted. Make sure to delete it manually. Learn more: https://webiny.link/lambda-edge-warning",
-                "website-router-origin-request"
-            );
+
+let websiteRouterOriginRequestFunction = "";
+
+export const lambdaEdgeWarning = [
+    // The reason we're retrieving `websiteRouterOriginRequestFunction` before destroy is because
+    // when trying to do it afterwards, the stack is already deleted, and we can't retrieve the
+    // stack output anymore.
+    {
+        type: "hook-before-destroy",
+        name: "hook-before-destroy-lambda-edge-warning",
+        hook(params: Record<string, any>) {
+            const websiteOutput = getStackOutput({ folder: "apps/website", env: params.env });
+
+            if (websiteOutput && websiteOutput.websiteRouterOriginRequestFunction) {
+                websiteRouterOriginRequestFunction =
+                    websiteOutput.websiteRouterOriginRequestFunction;
+            }
+        }
+    },
+    {
+        type: "hook-after-destroy",
+        name: "hook-after-destroy-lambda-edge-warning",
+        hook(params: Record<string, any>, context: CliContext) {
+            if (websiteRouterOriginRequestFunction) {
+                context.warning(
+                    "Lambda@Edge function %s was not automatically deleted. Make sure to delete it manually. Learn more: https://webiny.link/lambda-edge-warning",
+                    websiteRouterOriginRequestFunction
+                );
+            }
         }
     }
-};
+];
