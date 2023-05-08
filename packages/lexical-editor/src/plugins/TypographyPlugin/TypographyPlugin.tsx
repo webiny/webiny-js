5868a83b4d6f20da11ce881f14d78bb52470f6c0
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2023-05-08 10:08:46 +0200

    refactor: remove debugger

--- packages/lexical-editor/src/plugins/TypographyPlugin/TypographyPlugin.tsx
+++ packages/lexical-editor/src/plugins/TypographyPlugin/TypographyPlugin.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { $getSelection, $isRangeSelection, COMMAND_PRIORITY_EDITOR } from "lexical";
-import {
-    ADD_TYPOGRAPHY_ELEMENT_COMMAND,
-    TypographyPayload
-} from "~/nodes/TypographyElementNode";
+import { ADD_TYPOGRAPHY_ELEMENT_COMMAND, TypographyPayload } from "~/nodes/TypographyElementNode";
 import { $wrapNodes } from "@lexical/selection";
 import { $createBaseParagraphNode } from "~/nodes/BaseParagraphNode";
 
@@ -17,7 +14,6 @@ export const TypographyPlugin: React.FC = () => {
             payload => {
                 const selection = $getSelection();
                 if ($isRangeSelection(selection) && payload.value.id && payload.value.tag === "p") {
-                    debugger;
                     $wrapNodes(selection, () => $createBaseParagraphNode(payload.value.id));
                 }
                 return true;
