ac9a37841b5fb3347415b7e50a9661f47516ee48
adrians5j <adrian@webiny.com>
2023-05-15 16:59:18 +0200

    wip: enable setting ES domain name and index prefix via Pulumi app

--- apps/api/webiny.application.ts
+++ apps/api/webiny.application.ts
@@ -1,5 +1,15 @@
 import { createApiApp } from "@webiny/serverless-cms-aws";
 
 export default createApiApp({
-    pulumiResourceNamePrefix: "wby-"
+    pulumiResourceNamePrefix: "wby-",
+    elasticSearch: ({ params }) => {
+        if (params.run.env !== "staging" || params.run.env !== "prod") {
+            return {
+                domainName: "wby-elastic-search",
+                indexPrefix: "wby-"
+            };
+        }
+
+        return true;
+    }
 });
