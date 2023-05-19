8ce152097cb6890c025f1c25e75ab30493c785f4
adrians5j <adrian@webiny.com>
2023-05-19 11:03:29 +0200

    fix: undo changes

--- apps/api/webiny.application.ts
+++ apps/api/webiny.application.ts
@@ -1,15 +1,5 @@
 import { createApiApp } from "@webiny/serverless-cms-aws";
 
 export default createApiApp({
-    pulumiResourceNamePrefix: "wby-",
-    elasticSearch: ({ params }) => {
-        if (params.run.env !== "staging" || params.run.env !== "prod") {
-            return {
-                domainName: "wby-elastic-search",
-                indexPrefix: "wby-"
-            };
-        }
-
-        return true;
-    }
+    pulumiResourceNamePrefix: "wby-"
 });
