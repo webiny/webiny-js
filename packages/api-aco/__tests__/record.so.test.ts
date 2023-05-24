f11238d7197568a1963f5a33c07a2f4ca3968a17
Bruno Zorić <bruno.zoric@gmail.com>
2023-05-24 11:36:38 +0200

    test(api-aco): increase timeout

--- packages/api-aco/__tests__/record.so.test.ts
+++ packages/api-aco/__tests__/record.so.test.ts
@@ -3,7 +3,7 @@ import { userMock } from "~tests/mocks/user.mock";
 import { useGraphQlHandler } from "./utils/useGraphQlHandler";
 import { createMockAcoApp } from "~tests/mocks/app";
 
-jest.setTimeout(200000);
+jest.setTimeout(300000);
 
 describe("`search` CRUD", () => {
     const { search } = useGraphQlHandler({
