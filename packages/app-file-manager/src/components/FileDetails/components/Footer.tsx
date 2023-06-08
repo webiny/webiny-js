0c5b10e0f320a799a659ca034e61c27a476172c1
SvenAlHamad <sven@webiny.com>
2023-06-08 12:11:56 +0100

    feat(app-file-manager): styling of the footer buttons

--- packages/app-file-manager/src/components/FileDetails/components/Footer.tsx
+++ packages/app-file-manager/src/components/FileDetails/components/Footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+import styled from "@emotion/styled";
 import { useFileDetails } from "~/components/FileDetails";
 import { SimpleFormFooter } from "@webiny/app-admin/components/SimpleForm";
 import { ButtonDefault, ButtonPrimary } from "@webiny/ui/Button";
 import { useForm } from "@webiny/form";
+
+const SimpleFormFooterStyled = styled(SimpleFormFooter)`
+    justify-content: space-between;
+`;
 
 export const Footer = () => {
     const { close } = useFileDetails();
     const form = useForm();
 
     return (
-        <SimpleFormFooter>
+        <SimpleFormFooterStyled>
             <ButtonDefault onClick={close}>Cancel</ButtonDefault>
             <ButtonPrimary onClick={form.submit}>Save File</ButtonPrimary>
-        </SimpleFormFooter>
+        </SimpleFormFooterStyled>
     );
 };
