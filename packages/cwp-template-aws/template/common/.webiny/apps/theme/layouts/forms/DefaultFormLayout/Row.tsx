4ee3e0ccc20b13454107d8578bc28ac45635d710
adrians5j <adrian@webiny.com>
2023-06-09 19:13:43 +0200

    wip: update cwp

--- packages/cwp-template-aws/template/common/.webiny/apps/theme/layouts/forms/DefaultFormLayout/Row.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Row = styled.div`
-    display: flex;
-    width: 100%;
-    padding: 0;
-    background-color: ${props => props.theme.styles.colors["color6"]};
-`;
