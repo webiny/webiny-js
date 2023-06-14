2834b91b0fabbbc8e919a981ce252327e990a648
adrians5j <adrian@webiny.com>
2023-06-14 22:30:12 +0200

    wip

--- packages/app-admin-rmwc/src/modules/OmniSearch.tsx
+++ packages/app-admin-rmwc/src/modules/OmniSearch.tsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { useNavigation } from "@webiny/app-admin";
 import styled from "@emotion/styled";
-import { Link, useHistory } from "@webiny/react-router";
+import { useHistory } from "@webiny/react-router";
+import { useSnackbar } from "@webiny/app-admin";
+import { getTenantId } from "@webiny/app/utils";
+import { ReactComponent as SearchIcon } from "@material-design-icons/svg/outlined/search.svg";
 
 // @ts-ignore Library doesn't have types.
 import { useHotkeys } from "react-hotkeyz";
@@ -18,7 +21,7 @@ const Dialog = styled.div`
 
     .dialog {
         position: fixed;
-        top: 300px;
+        top: 350px;
         left: 50%;
         /* bring your own prefixes */
         transform: translate(-50%, -50%);
@@ -32,17 +35,31 @@ const Dialog = styled.div`
         > ul {
             overflow-y: scroll;
             height: 444px;
+            margin-bottom: 10px;
 
             > li {
-                padding: 5px 15px 5px 15px;
-
-                .section-name {
+                .section-title {
                     bottom-bottom: 1px solid var(--mdc-theme-on-background);
+                    padding: 10px 15px 5px 15px;
                 }
 
                 ul {
                     li {
-                        padding: 7px 0;
+                        padding: 10px 15px;
+                        cursor: pointer;
+
+                        :hover {
+                            background-color: var(--mdc-theme-background);
+                        }
+
+                        .section-item-title {
+                            margin-bottom: 3px;
+                            color: var(--mdc-theme-primary);
+                        }
+
+                        .section-item-description {
+                            color: var(--mdc-theme-text-primary-on-background);
+                        }
                     }
                 }
             }
@@ -55,6 +72,7 @@ export const OmniSearch = () => {
     const [filter, setFilter] = useState("");
     const [omniSearchVisible, setShowOmniSearch] = useState(false);
     const history = useHistory();
+    const { showSnackbar } = useSnackbar();
 
     const showOmniSearch = useCallback(() => {
         setShowOmniSearch(true);
@@ -102,6 +120,12 @@ export const OmniSearch = () => {
         return;
     }, [filteredItems]);
 
+    const copyTenantId = useCallback(() => {
+        navigator.clipboard.writeText(getTenantId()!);
+        showSnackbar("Tenant ID copied to clipboard.");
+        hideOmniSearch();
+    }, []);
+
     useHotkeys({
         zIndex: 100,
         keys: {
@@ -122,54 +146,57 @@ export const OmniSearch = () => {
         return (
             <Dialog>
                 <div className={"dialog"}>
-                    <Input
-                        placeholder={"Search..."}
-                        autoFocus
-                        value={filter}
-                        onChange={setFilter}
-                    />
+                    <div className={"search-input"}>
+                        <Input
+                            icon={<SearchIcon />}
+                            placeholder={"Search..."}
+                            autoFocus
+                            value={filter}
+                            onChange={setFilter}
+                        />
+                    </div>
                     <ul>
-                        <li>
-                            <div className={"section-name"}>
-                                <Typography use={"overline"}>Apps</Typography>
-                            </div>
-                            <div className={"section-items"}>
-                                <ul>
-                                    {filteredItems.map(item => {
-                                        return (
+                        {filteredItems.length > 0 && (
+                            <li>
+                                <div className={"section-title"}>
+                                    <Typography use={"overline"}>Apps</Typography>
+                                </div>
+                                <div className={"section-items"}>
+                                    <ul>
+                                        {filteredItems.map(item => (
                                             <li key={item.path}>
-                                                <div>
-                                                    {/* @ts-ignore*/}
-                                                    <Link to={item.path} onClick={hideOmniSearch}>
-                                                        {item.label}
-                                                    </Link>
+                                                <div className={"section-item-title"}>
+                                                    {item.label}
                                                 </div>
-                                                <div>
-                                                    <Typography use={"caption"}>
-                                                        {item.breadcrumb.join(" / ")}
-                                                    </Typography>
-                                                </div>
+                                                <Typography
+                                                    use={"caption"}
+                                                    className={"section-item-description"}
+                                                >
+                                                    {item.breadcrumb.join(" / ")}
+                                                </Typography>
                                             </li>
-                                        );
-                                    })}
-                                </ul>
-                            </div>
-                        </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </li>
+                        )}
+
                         <li>
-                            <div className={"section-name"}>
-                                <Typography use={"overline"}>Other</Typography>
+                            <div className={"section-title"}>
+                                <Typography use={"overline"}>Development</Typography>
                             </div>
                             <div className={"section-items"}>
                                 <ul>
-                                    <li>
-                                        <div>
+                                    <li onClick={copyTenantId}>
+                                        <div className={"section-item-title"}>
                                             Copy current tenant ID
                                         </div>
-                                        <div>
-                                            <Typography use={"caption"}>
-                                                Copies current tenant ID into clipboard.
-                                            </Typography>
-                                        </div>
+                                        <Typography
+                                            use={"caption"}
+                                            className={"section-item-description"}
+                                        >
+                                            Copies current tenant ID into clipboard.
+                                        </Typography>
                                     </li>
                                 </ul>
                             </div>
