d917f2932176e1c4051b937f35335c6cf94d9f1b
Pavel Denisjuk <pavel@webiny.com>
2023-06-18 12:02:03 +0200

    feat(react-properties): add configurable component factory

--- packages/app-file-manager/src/modules/FileManagerRenderer/FileManagerView/FileManagerViewConfig.tsx
+++ packages/app-file-manager/src/modules/FileManagerRenderer/FileManagerView/FileManagerViewConfig.tsx
@@ -1,56 +1,12 @@
-import React, { useContext, useMemo, useState } from "react";
-import { makeComposable, Compose, HigherOrderComponent } from "@webiny/app-admin";
-import { Property, Properties, toObject } from "@webiny/react-properties";
+import { useMemo } from "react";
+import { createConfigurableComponent } from "@webiny/react-properties";
 import { Browser, BrowserConfig } from "./configComponents/Browser";
 import { FileDetails, FileDetailsConfig } from "./configComponents/FileDetails";
 
-const FileManagerViewConfigApply = makeComposable("FileManagerViewConfigApply", ({ children }) => {
-    return <>{children}</>;
-});
+const base = createConfigurableComponent<FileManagerViewConfigData>("FileManagerView");
 
-const createHOC =
-    (newChildren: React.ReactNode): HigherOrderComponent =>
-    BaseComponent => {
-        return function ConfigHOC({ children }) {
-            return (
-                <BaseComponent>
-                    {newChildren}
-                    {children}
-                </BaseComponent>
-            );
-        };
-    };
-
-export const FileManagerViewConfig = ({ children }: { children: React.ReactNode }) => {
-    return <Compose component={FileManagerViewConfigApply} with={createHOC(children)} />;
-};
-
-FileManagerViewConfig.Browser = Browser;
-FileManagerViewConfig.FileDetails = FileDetails;
-
-interface ViewContext {
-    properties: Property[];
-}
-
-const ViewContext = React.createContext<ViewContext>({ properties: [] });
-
-export const FileManagerViewWithConfig = ({ children }: { children: React.ReactNode }) => {
-    const [properties, setProperties] = useState<Property[]>([]);
-    const context = { properties };
-
-    const stateUpdater = (properties: Property[]) => {
-        setProperties(properties);
-    };
-
-    return (
-        <ViewContext.Provider value={context}>
-            <Properties onChange={stateUpdater}>
-                <FileManagerViewConfigApply />
-                {children}
-            </Properties>
-        </ViewContext.Provider>
-    );
-};
+export const FileManagerViewConfig = Object.assign(base.Config, { Browser, FileDetails });
+export const FileManagerViewWithConfig = base.WithConfig;
 
 interface FileManagerViewConfigData {
     browser: BrowserConfig;
@@ -58,11 +14,7 @@ interface FileManagerViewConfigData {
 }
 
 export function useFileManagerViewConfig() {
-    const { properties } = useContext(ViewContext);
-
-    const config = useMemo(() => {
-        return toObject<FileManagerViewConfigData>(properties);
-    }, [properties]);
+    const config = base.useConfig();
 
     const browser = config.browser || {};
 
