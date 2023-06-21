26df65a159027183ee57af5c0be83ebaf36a9c12
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-06-21 10:32:45 +0200

    chore: run linter

--- packages/api-page-builder-aco/src/types.ts
+++ packages/api-page-builder-aco/src/types.ts
@@ -1,4 +1,4 @@
-import { AcoContext,Folder, IAcoApp } from "@webiny/api-aco/types";
+import { AcoContext, Folder, IAcoApp } from "@webiny/api-aco/types";
 import { Page, PbPageElement } from "@webiny/api-page-builder/types";
 import { PbContext } from "@webiny/api-page-builder/graphql/types";
 import { Context as BaseContext } from "@webiny/handler/types";
