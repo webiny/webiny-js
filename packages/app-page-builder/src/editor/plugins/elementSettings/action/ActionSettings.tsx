4301c1cd740db3f3a638ddb88b336c578d83740b
Pavel Denisjuk <pavel@webiny.com>
2023-06-22 13:11:47 +0200

    feat(app-page-builder): introduce PbPageElementActionTypePlugin plugin

--- packages/app-page-builder/src/editor/plugins/elementSettings/action/ActionSettings.tsx
+++ packages/app-page-builder/src/editor/plugins/elementSettings/action/ActionSettings.tsx
@@ -1,29 +1,18 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { css } from "emotion";
 import { merge } from "dot-prop-immutable";
-import { Switch } from "@webiny/ui/Switch";
-import { Typography } from "@webiny/ui/Typography";
 import { Form, FormOnSubmit } from "@webiny/form";
-import { validation } from "@webiny/validation";
+import { plugins } from "@webiny/plugins";
 import { withActiveElement } from "../../../components";
-import { DelayedOnChange } from "@webiny/ui/DelayedOnChange";
 import {
-    PbElement,
-    PbButtonElementClickHandlerPlugin,
     PbEditorElement,
     PbEditorPageElementSettingsRenderComponentProps,
-    PbButtonElementClickHandlerVariable
+    PbPageElementActionTypePlugin
 } from "~/types";
-import { plugins } from "@webiny/plugins";
-import { getElementsPropertiesValues } from "~/render/utils";
-
-// Components
 import Accordion from "../components/Accordion";
 import Wrapper from "../components/Wrapper";
-import InputField from "../components/InputField";
 import SelectField from "../components/SelectField";
 import { useUpdateElement } from "~/editor/hooks/useUpdateElement";
-import { useEventActionHandler } from "~/editor/hooks/useEventActionHandler";
 
 const classes = {
     gridClass: css({
@@ -49,23 +38,8 @@ const ActionSettingsComponent: React.FC<ActionSettingsPropsType> = ({
     defaultAccordionValue
 }) => {
     const updateElement = useUpdateElement();
-    const { getElementTree } = useEventActionHandler();
-    const [elementIds, setElementIds] = useState<Array<string>>([""]);
 
-    // Let's preserve backwards compatibility by extracting href and newTab properties from deprecated
-    //  "link" element object if it exists, otherwise we'll use the newer "action" element object
-    let href: string;
-    let newTab: boolean;
-
-    if (element.data?.link && !element.data?.action) {
-        href = element.data?.link?.href || "";
-        newTab = element.data?.link?.newTab || false;
-    } else {
-        href = element.data?.action?.href || "";
-        newTab = element.data?.action?.newTab || false;
-    }
-
-    const { clickHandler, actionType, variables, scrollToElement } = element.data?.action || {};
+    const { actionType } = element.data?.action || { actionType: "link" };
 
     const updateSettings: FormOnSubmit = data => {
         const attrKey = `data.action`;
@@ -73,190 +47,42 @@ const ActionSettingsComponent: React.FC<ActionSettingsPropsType> = ({
         updateElement(newElement);
     };
 
-    const clickHandlers: Array<{
-        title: string;
-        name?: string;
-        variables?: PbButtonElementClickHandlerVariable[];
-    }> = useMemo(
-        () =>
-            plugins.byType<PbButtonElementClickHandlerPlugin>(
-                "pb-page-element-button-click-handler"
-            ),
-        []
-    );
+    const actionTypes = useMemo(() => {
+        const actionTypePlugins = plugins.byType<PbPageElementActionTypePlugin>(
+            "pb-page-element-action-type"
+        );
 
-    const selectedHandler = useMemo(() => {
-        return clickHandlers.find(handler => clickHandler === handler.name);
-    }, [clickHandler]);
+        return actionTypePlugins.map(plugin => plugin.actionType);
+    }, []);
 
-    useEffect(() => {
-        const getElementIds = async () => {
-            const tree = (await getElementTree()) as PbElement;
-            setElementIds(getElementsPropertiesValues(tree, "data.settings.property.id"));
-        };
+    const activeActionType = actionTypes.find(type => type.name === actionType);
 
-        if (actionType === "scrollToElement") {
-            getElementIds();
-        }
-    }, [actionType]);
-
-    const initialData = { href, newTab, clickHandler, actionType, variables, scrollToElement };
+    // For backwards compatibility, use `data.link` first, then apply the current values.
+    const formData = { ...element.data?.link, ...element.data?.action };
 
     return (
         <Accordion title={"Action"} defaultValue={defaultAccordionValue}>
-            <Form data={initialData} onChange={updateSettings}>
+            <Form data={formData} onChange={updateSettings}>
                 {({ Bind }) => {
-                    const actionTypeOptions = [
-                        { id: "link", name: "Link" },
-                        { id: "scrollToElement", name: "Scroll to element" },
-                        { id: "onClickHandler", name: "Click handler" }
-                    ];
-
                     return (
                         <>
-                            <Bind name="actionType" defaultValue={actionTypeOptions[0].id}>
+                            <Bind name="actionType" defaultValue={actionTypes[0].name}>
                                 {({ value, onChange }) => (
                                     <Wrapper
                                         label={"Action Type"}
                                         containerClassName={classes.gridClass}
                                     >
                                         <SelectField value={value} onChange={onChange}>
-                                            {actionTypeOptions.map(item => (
-                                                <option key={item.id} value={item.id}>
-                                                    {item.name}
+                                            {actionTypes.map(item => (
+                                                <option key={item.name} value={item.name}>
+                                                    {item.label}
                                                 </option>
                                             ))}
                                         </SelectField>
                                     </Wrapper>
                                 )}
                             </Bind>
-                            {actionType === "onClickHandler" && (
-                                <>
-                                    <Bind name={"clickHandler"}>
-                                        {({ value, onChange }) => (
-                                            <Wrapper
-                                                label={"Handler"}
-                                                containerClassName={classes.gridClass}
-                                            >
-                                                <SelectField
-                                                    value={value}
-                                                    onChange={onChange}
-                                                    placeholder={"Select handler..."}
-                                                >
-                                                    {clickHandlers.map(item => (
-                                                        <option key={item.name} value={item.name}>
-                                                            {item.title}
-                                                        </option>
-                                                    ))}
-                                                </SelectField>
-                                            </Wrapper>
-                                        )}
-                                    </Bind>
-
-                                    <Wrapper
-                                        label={"Variables"}
-                                        containerClassName={classes.gridClass}
-                                    >
-                                        <Bind name="variables" defaultValue={{}}>
-                                            <DelayedOnChange>
-                                                {({ value, onChange }: any) => {
-                                                    // TODO @ts-refactor need to have generic on DelayedOnChange
-                                                    if (!selectedHandler?.variables) {
-                                                        return (
-                                                            <Typography use="body2">
-                                                                None required.
-                                                            </Typography>
-                                                        );
-                                                    }
-
-                                                    return (
-                                                        <>
-                                                            {selectedHandler.variables.map(
-                                                                (variable, index) => (
-                                                                    <InputField
-                                                                        key={index}
-                                                                        value={value[variable.name]}
-                                                                        placeholder={variable.label}
-                                                                        className={
-                                                                            classes.bottomMargin
-                                                                        }
-                                                                        onChange={val =>
-                                                                            onChange({
-                                                                                ...value,
-                                                                                [variable.name]: val
-                                                                            })
-                                                                        }
-                                                                    />
-                                                                )
-                                                            )}
-                                                        </>
-                                                    );
-                                                }}
-                                            </DelayedOnChange>
-                                        </Bind>
-                                    </Wrapper>
-                                </>
-                            )}
-                            {actionType === "scrollToElement" && (
-                                <>
-                                    <Bind name={"scrollToElement"}>
-                                        {({ value, onChange }) => (
-                                            <Wrapper
-                                                label={"Element ID"}
-                                                containerClassName={classes.gridClass}
-                                            >
-                                                <SelectField
-                                                    value={value}
-                                                    onChange={onChange}
-                                                    placeholder={"None"}
-                                                >
-                                                    {elementIds.map((item, index) => (
-                                                        <option key={index} value={item}>
-                                                            {item}
-                                                        </option>
-                                                    ))}
-                                                </SelectField>
-                                            </Wrapper>
-                                        )}
-                                    </Bind>
-                                </>
-                            )}
-                            {actionType !== "onClickHandler" &&
-                                actionType !== "scrollToElement" && (
-                                    <>
-                                        <Wrapper
-                                            label={"URL"}
-                                            containerClassName={classes.gridClass}
-                                        >
-                                            <Bind
-                                                name={"href"}
-                                                validators={validation.create(
-                                                    "url:allowRelative:allowHref"
-                                                )}
-                                            >
-                                                <DelayedOnChange>
-                                                    {props => (
-                                                        <InputField
-                                                            {...props}
-                                                            value={props.value || ""}
-                                                            onChange={props.onChange}
-                                                            placeholder={"https://webiny.com/blog"}
-                                                        />
-                                                    )}
-                                                </DelayedOnChange>
-                                            </Bind>
-                                        </Wrapper>
-                                        <Wrapper
-                                            label={"New tab"}
-                                            containerClassName={classes.gridClass}
-                                            rightCellClassName={classes.gridCellClass}
-                                        >
-                                            <Bind name={"newTab"}>
-                                                <Switch />
-                                            </Bind>
-                                        </Wrapper>
-                                    </>
-                                )}
+                            {activeActionType ? activeActionType.element : null}
                         </>
                     );
                 }}
