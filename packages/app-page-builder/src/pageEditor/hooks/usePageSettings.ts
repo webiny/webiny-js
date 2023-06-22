340ef87462ba0502bfcf14681946adb253f73c92
Pavel Denisjuk <pavel@webiny.com>
2023-06-22 13:13:22 +0200

    feat(app-page-builder): update page state with data from update mutation

--- packages/app-page-builder/src/pageEditor/hooks/usePageSettings.ts
+++ packages/app-page-builder/src/pageEditor/hooks/usePageSettings.ts
@@ -6,13 +6,14 @@ import { useKeyHandler } from "~/editor/hooks/useKeyHandler";
 import { UpdateDocumentActionEvent } from "~/editor/recoil/actions";
 import { pageSettingsStateAtom } from "~/pageEditor/config/editorBar/PageSettings/state";
 import { usePage } from "~/pageEditor/hooks/usePage";
+import { UpdatedPage } from "~/pageEditor/config/eventActions/saveRevision/types";
 
 export type UsePageSettings = ReturnType<typeof usePageSettings>;
 
 export function usePageSettings() {
     const [activeSection, setActiveSection] = useState<string | null>(null);
     const eventActionHandler = useEventActionHandler();
-    const [pageData] = usePage();
+    const [pageData, setPageData] = usePage();
     const [, setSettingsState] = useRecoilState(pageSettingsStateAtom);
 
     const { showSnackbar } = useSnackbar();
@@ -28,9 +29,12 @@ export function usePageSettings() {
                 document: pageValue,
                 debounce: false,
                 history: false,
-                onFinish: () => {
+                onFinish: (page?: UpdatedPage) => {
                     showSnackbar("Settings saved!");
                     closeSettings();
+                    if (page) {
+                        setPageData(state => ({ ...state, ...page }));
+                    }
                 }
             })
         );
