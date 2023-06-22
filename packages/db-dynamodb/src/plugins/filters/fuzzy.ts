19b5f64ce478cbc5d639d9b470fd0c827851cb3b
Pavel Denisjuk <pavel@webiny.com>
2023-06-22 18:21:52 +0200

    revert(db-dynamodb): revert fuzzy search threshold to 0.6

--- packages/db-dynamodb/src/plugins/filters/fuzzy.ts
+++ packages/db-dynamodb/src/plugins/filters/fuzzy.ts
@@ -16,7 +16,7 @@ const plugin = new ValueFilterPlugin({
         const f = new Fuse([value], {
             includeScore: true,
             minMatchCharLength: 3,
-            threshold: 0.5
+            threshold: 0.6
         });
         const result = f.search(compareValue);
 
