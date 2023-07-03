0d11dc8beed5b26e039dcc7d6f00637240e066ee
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-07-03 11:35:56 +0200

    chore: extract save and publish into an hook

--- packages/app-headless-cms/src/admin/components/ContentEntryForm/Header/SaveAndPublishContent/SaveAndPublishContent.tsx
+++ packages/app-headless-cms/src/admin/components/ContentEntryForm/Header/SaveAndPublishContent/SaveAndPublishContent.tsx
@@ -1,38 +1,17 @@
-import React, { useCallback } from "react";
-import { useConfirmationDialog } from "@webiny/app-admin/hooks/useConfirmationDialog";
+import React from "react";
+
 import { makeComposable } from "@webiny/react-composition";
 import { ContentEntryEditorConfig } from "~/admin/config/contentEntries";
-import { useRevision } from "~/admin/views/contentEntries/ContentEntry/useRevision";
 import { useContentEntry } from "~/admin/views/contentEntries/hooks/useContentEntry";
 import usePermission from "~/admin/hooks/usePermission";
+import { useSaveAndPublish } from "./useSaveAndPublish";
 
 const SaveAndPublishButtonComponent: React.FC = () => {
-    const { form, loading, entry } = useContentEntry();
-    const { publishRevision } = useRevision({ revision: entry });
+    const { loading, entry } = useContentEntry();
+    const { showConfirmationDialog } = useSaveAndPublish();
     const { ButtonPrimary } = ContentEntryEditorConfig.Actions.useButtons();
 
-    const { showConfirmation } = useConfirmationDialog({
-        title: "Publish content",
-        message: (
-            <p>{"You are about to publish a new revision. Are you sure you want to continue?"}</p>
-        ),
-        dataTestId: "cms-confirm-save-and-publish"
-    });
-
     const { canEdit, canPublish } = usePermission();
-
-    const onPublishClick = useCallback(
-        (ev: React.MouseEvent) => {
-            showConfirmation(async () => {
-                const entry = await form.current.submit(ev);
-                if (!entry || !entry.id) {
-                    return;
-                }
-                await publishRevision(entry.id);
-            });
-        },
-        [showConfirmation]
-    );
 
     if (!canEdit(entry, "cms.contentEntry") || !canPublish("cms.contentEntry")) {
         return null;
@@ -40,7 +19,7 @@ const SaveAndPublishButtonComponent: React.FC = () => {
 
     return (
         <ButtonPrimary
-            onAction={onPublishClick}
+            onAction={showConfirmationDialog}
             disabled={loading}
             data-testid="cms-content-save-publish-content-button"
         >
