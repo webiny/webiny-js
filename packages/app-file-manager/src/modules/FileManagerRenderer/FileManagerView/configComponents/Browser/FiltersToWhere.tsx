9692eef947a32b40d6f425f3b6d2fc3e959694ef
Pavel Denisjuk <pavel@webiny.com>
2023-07-07 11:28:29 +0200

    feat(app-file-manager): let react-properties generate a unique property id

--- packages/app-file-manager/src/modules/FileManagerRenderer/FileManagerView/configComponents/Browser/FiltersToWhere.tsx
+++ packages/app-file-manager/src/modules/FileManagerRenderer/FileManagerView/configComponents/Browser/FiltersToWhere.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { Property, useIdGenerator } from "@webiny/react-properties";
-import { mdbid } from "@webiny/utils";
+import { Property } from "@webiny/react-properties";
 
 export interface FiltersToWhereConverter {
     (filters: Record<string, any>): Record<string, any>;
 }
 
 export const FiltersToWhere = ({ converter }: { converter: FiltersToWhereConverter }) => {
-    const getId = useIdGenerator("filtersToWhere");
-
     return (
         <Property id="browser" name={"browser"}>
-            <Property id={getId(mdbid())} name={"filtersToWhere"} array={true} value={converter} />
+            <Property name={"filtersToWhere"} array={true} value={converter} />
         </Property>
     );
 };
