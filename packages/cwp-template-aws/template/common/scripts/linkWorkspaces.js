9d8a6347a4ff5248fdbc8943b2518c5310ab8077
adrians5j <adrian@webiny.com>
2023-07-07 10:00:21 +0200

    fix: remove `scripts`

--- packages/cwp-template-aws/template/common/scripts/linkWorkspaces.js
+++
@@ -1,9 +0,0 @@
-/**
- * This tool will re-link monorepo workspaces to one of the following directories (by priority):
- * - {workspace}/package.json -> publishConfig.directory
- * - workspace root directory
- */
-(async () => {
-    const { linkWorkspaces } = require("@webiny/project-utils/workspaces");
-    await linkWorkspaces();
-})();
