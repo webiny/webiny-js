e373c68edac5f66aa652c5df3c4df4551fca8aaa
Bruno Zorić <bruno.zoric@gmail.com>
2023-07-07 14:28:29 +0200

    fix(api-mailer): validation of undefined password value

--- scripts/checkPackageNodeModules.js
+++ scripts/checkPackageNodeModules.js
@@ -27,8 +27,8 @@ const hasWebinyPackageVersion = pkg => {
 };
 
 const getPackageName = packageJsonFile => {
-	const packageJson = loadJsonFile.sync(packageJsonFile);
-	return packageJson.name;
+    const packageJson = loadJsonFile.sync(packageJsonFile);
+    return packageJson.name;
 };
 
 const checkPackageNodeModules = () => {
@@ -43,12 +43,12 @@ const checkPackageNodeModules = () => {
         }
         const subpackages = glob.sync(`${pkg}/node_modules/*/package.json`);
         if (subpackages.length !== 0) {
-	        console.log(
-		        `[SUBPACKAGES] "${name}" has ${subpackages.length} subpackages: ${subpackages
-			        .map(p => getPackageName(p))
-			        .join(", ")}`
-	        );
-	        console.log("------------------------------");
+            console.log(
+                `[SUBPACKAGES] "${name}" has ${subpackages.length} subpackages: ${subpackages
+                    .map(p => getPackageName(p))
+                    .join(", ")}`
+            );
+            console.log("------------------------------");
         }
     }
 };
