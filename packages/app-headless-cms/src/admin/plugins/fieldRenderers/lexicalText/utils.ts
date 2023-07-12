1f9a431ccb709361a38666abf1fadf58a1f42000
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2023-07-12 14:45:52 +0200

    refactor: remove unnecessary comments in utils

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/lexicalText/utils.ts
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/lexicalText/utils.ts
@@ -1,8 +1,5 @@
 import { CmsModel } from "@webiny/app-headless-cms-common/types";
 
-/**
- * Check if model have at least one lexical single/multiple field
- */
 export const modelHasLexicalField = (model: CmsModel): boolean => {
     return model.fields.some(field => field.renderer.name.startsWith("lexical-"));
 };
--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/richText/utils.ts
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/richText/utils.ts
@@ -1,22 +1,13 @@
 import { CmsModel, CmsModelField } from "@webiny/app-headless-cms-common/types";
 
-/**
- * Model have at least one single/multiple legacy RTE field
- */
 export const modelHasLegacyRteField = (model: CmsModel): boolean => {
-    return model.fields.some(field => field.renderer.name.startsWith("rich-text-"));
+    return model.fields.some(isLegacyRteField);
 };
 
-/**
- * Check id field is legacy RTE
- */
 export const isLegacyRteField = (field: CmsModelField): boolean => {
     return field.renderer.name.startsWith("rich-text-");
 };
 
-/**
- * Checks if the field is saved as legacy RTE
- */
 export const isLegacyRteFieldSaved = (field: CmsModelField): boolean => {
     return !!field.id && isLegacyRteField(field);
 };
