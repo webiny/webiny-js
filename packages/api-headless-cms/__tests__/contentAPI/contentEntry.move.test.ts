0409cde0c0e3871d664accb037fcaf6d6b6da9ff
Bruno Zorić <bruno.zoric@gmail.com>
2023-07-14 12:01:24 +0200

    test(api-headless-cms): move all entry revisions to another folder

--- packages/api-headless-cms/__tests__/contentAPI/contentEntry.move.test.ts
+++ packages/api-headless-cms/__tests__/contentAPI/contentEntry.move.test.ts
@@ -8,6 +8,7 @@ interface CategoryParams {
 
 interface Category {
     id: string;
+    entryId: string;
     title: string;
     slug: string;
     wbyAco_location: {
@@ -20,6 +21,17 @@ describe("move content entry to another folder", () => {
         path: "manage/en-US"
     });
 
+    const getCategory = async (revision: string): Promise<Category> => {
+        return await manager
+            .getCategory({
+                revision
+            })
+            .then(result => {
+                const [data] = result;
+                return data.data.getCategory.data;
+            });
+    };
+
     const createCategory = async (data: CategoryParams): Promise<Category> => {
         return await manager
             .createCategory({
@@ -28,6 +40,17 @@ describe("move content entry to another folder", () => {
             .then(result => {
                 const [data] = result;
                 return data.data.createCategory.data;
+            });
+    };
+
+    const createCategoryFrom = async (category: Category) => {
+        return await manager
+            .createCategoryFrom({
+                revision: category.id
+            })
+            .then(result => {
+                const [data] = result;
+                return data.data.createCategoryFrom.data;
             });
     };
 
@@ -65,22 +88,13 @@ describe("move content entry to another folder", () => {
             }
         });
 
-        const [getCategoryResponse] = await manager.getCategory({
-            revision: category.id
-        });
+        const getCategoryResponse = await getCategory(category.id);
         expect(getCategoryResponse).toMatchObject({
-            data: {
-                getCategory: {
-                    data: {
-                        id: category.id,
-                        title: category.title,
-                        slug: category.slug,
-                        wbyAco_location: {
-                            folderId: "anotherFolder"
-                        }
-                    },
-                    error: null
-                }
+            id: category.id,
+            title: category.title,
+            slug: category.slug,
+            wbyAco_location: {
+                folderId: "anotherFolder"
             }
         });
 
@@ -95,6 +109,111 @@ describe("move content entry to another folder", () => {
                             slug: category.slug,
                             wbyAco_location: {
                                 folderId: "anotherFolder"
+                            }
+                        }
+                    ],
+                    meta: {
+                        cursor: null,
+                        hasMoreItems: false,
+                        totalCount: 1
+                    },
+                    error: null
+                }
+            }
+        });
+    });
+
+    it("should move content entry and all its revisions to another folder", async () => {
+        const category = await createCategory({
+            title: "Fruits",
+            slug: "fruits"
+        });
+        expect(category).toMatchObject({
+            id: expect.any(String),
+            title: "Fruits",
+            slug: "fruits"
+        });
+        const category2 = await createCategoryFrom(category);
+        expect(category2).toMatchObject({
+            id: `${category.entryId}#0002`,
+            title: "Fruits",
+            slug: "fruits"
+        });
+        const category3 = await createCategoryFrom(category);
+        expect(category3).toMatchObject({
+            id: `${category.entryId}#0003`,
+            title: "Fruits",
+            slug: "fruits"
+        });
+        const category4 = await createCategoryFrom(category);
+        expect(category4).toMatchObject({
+            id: `${category.entryId}#0004`,
+            title: "Fruits",
+            slug: "fruits"
+        });
+
+        const [moveResponse] = await manager.moveCategory({
+            revision: category4.id,
+            folderId: "yetAnotherFolder"
+        });
+        expect(moveResponse).toEqual({
+            data: {
+                moveCategory: {
+                    data: true,
+                    error: null
+                }
+            }
+        });
+        const getCategoryResponse = await getCategory(category.id);
+        expect(getCategoryResponse).toMatchObject({
+            id: category.id,
+            title: category.title,
+            slug: category.slug,
+            wbyAco_location: {
+                folderId: "yetAnotherFolder"
+            }
+        });
+
+        const getCategory2Response = await getCategory(category2.id);
+        expect(getCategory2Response).toMatchObject({
+            id: category2.id,
+            title: category2.title,
+            slug: category2.slug,
+            wbyAco_location: {
+                folderId: "yetAnotherFolder"
+            }
+        });
+
+        const getCategory3Response = await getCategory(category3.id);
+        expect(getCategory3Response).toMatchObject({
+            id: category3.id,
+            title: category3.title,
+            slug: category3.slug,
+            wbyAco_location: {
+                folderId: "yetAnotherFolder"
+            }
+        });
+        const getCategory4Response = await getCategory(category4.id);
+        expect(getCategory4Response).toMatchObject({
+            id: category4.id,
+            title: category4.title,
+            slug: category4.slug,
+            wbyAco_location: {
+                folderId: "yetAnotherFolder"
+            }
+        });
+
+        const [listCategoriesResponse] = await manager.listCategories({});
+        expect(listCategoriesResponse).toMatchObject({
+            data: {
+                listCategories: {
+                    data: [
+                        {
+                            id: category4.id,
+                            title: category4.title,
+                            slug: category4.slug,
+                            wbyAco_location: {
+                                folderId: "yetAnotherFolder"
                             }
                         }
                     ],
