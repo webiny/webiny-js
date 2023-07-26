cbb746e5b68f74670befc2d9d937dfcf4598025a
adrians5j <adrian@webiny.com>
2023-07-26 11:24:29 +0200

    wip

--- packages/cwp-template-aws/template/common/webiny.ts
+++ packages/cwp-template-aws/template/common/webiny.ts
@@ -1,26 +1,22 @@
-import cliWorkspaces from "@webiny/cli-plugin-workspaces";
 import cliPulumiDeploy from "@webiny/cli-plugin-deploy-pulumi";
 import cliAwsTemplate from "@webiny/cwp-template-aws/cli";
 
 // Scaffolds.
 import cliScaffold from "@webiny/cli-plugin-scaffold";
-import cliScaffoldExtendGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-service";
-import cliScaffoldAdminModule from "@webiny/cli-plugin-scaffold-admin-app-module";
 import cliScaffoldCiCd from "@webiny/cli-plugin-scaffold-ci";
+import cliProjectDdb from "@webiny/project-ddb/cli";
 
 export default {
     template: "[TEMPLATE_VERSION]",
     name: "[PROJECT_NAME]",
     cli: {
         plugins: [
-            cliWorkspaces(),
             cliPulumiDeploy(),
             cliAwsTemplate(),
+            cliProjectDdb(),
 
             // Scaffolds.
             cliScaffold(),
-            cliScaffoldExtendGraphQlApi(),
-            cliScaffoldAdminModule(),
             cliScaffoldCiCd()
         ]
     }
