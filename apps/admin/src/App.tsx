b16fc8b3c0f00dfe0831b1c40b860e3a47d2dfd3
Pavel Denisjuk <pavel@webiny.com>
2023-08-02 16:22:00 +0200

    chore: thumbnail config example

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Admin } from "@webiny/app-serverless-cms";
+import { Admin, Decorator } from "@webiny/app-serverless-cms";
 import { Cognito } from "@webiny/app-admin-users-cognito";
+import { FileManagerViewConfig, useFile } from "@webiny/app-file-manager";
 import "./App.scss";
 
-export const App: React.FC = () => {
+const { Browser, FileDetails } = FileManagerViewConfig;
+
+const VideoThumbnail: Decorator = Original => {
+    return function VideoThumbnail() {
+        const { file } = useFile();
+
+        const thumbnail = file.extensions?.carMake;
+        if (file.type.startsWith("video/") && thumbnail) {
+            return <img src={thumbnail} />;
+        }
+
+        return <Original />;
+    };
+};
+
+const BrowserVideoThumbnail = Browser.Thumbnail.createDecorator(VideoThumbnail);
+const FileDetailsVideoThumbnail = FileDetails.Thumbnail.createDecorator(VideoThumbnail);
+
+export const App = () => {
     return (
         <Admin>
             <Cognito />
+            <FileManagerViewConfig>
+                <BrowserVideoThumbnail />
+                <FileDetailsVideoThumbnail />
+            </FileManagerViewConfig>
         </Admin>
     );
 };
