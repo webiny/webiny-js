cc4d2d92dce48cbba17760016c1ffdccdf8621cb
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2023-08-17 08:31:04 +0000

    fix: fix entry title in template editor entry selector

--- packages/app-page-builder/src/templateEditor/config/editorBar/EntrySelector/EntrySelector.tsx
+++ packages/app-page-builder/src/templateEditor/config/editorBar/EntrySelector/EntrySelector.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import { Icon } from "@webiny/ui/Icon";
 import { createComponentPlugin, makeComposable } from "@webiny/app-admin";
 import { ReactComponent as OpenIcon } from "@material-design-icons/svg/round/open_in_new.svg";
+import { useEntryTitleById } from "@webiny/app-dynamic-pages/hooks/useEntryTitleById";
 
 import { EditorBar } from "~/editor";
 import { useSourceModel } from "~/templateEditor/hooks/useSourceModel";
@@ -21,6 +22,22 @@ const DefaultEntrySelector = () => {
     const [sourceModel] = useSourceModel();
     const [toggleModal, setToggleModal] = useState<boolean>(false);
     const [templateAtomValue] = useTemplate();
+    const { data: entryTitle, loading } = useEntryTitleById(
+        sourceModel.data,
+        templateAtomValue.dynamicSource?.entryId
+    );
+
+    const title = useMemo(() => {
+        if (loading) {
+            return "...";
+        }
+
+        if (entryTitle) {
+            return entryTitle;
+        }
+
+        return "Select Entry";
+    }, [entryTitle, loading]);
 
     const onClose = useCallback(() => {
         setToggleModal(false);
@@ -31,9 +48,7 @@ const DefaultEntrySelector = () => {
             <SelectEntry onClick={() => setToggleModal(true)}>
                 <SelectedEntryLeft>
                     <SelectedEntryLable>Preview entry:</SelectedEntryLable>
-                    <SelectedEntryTitle>
-                        {templateAtomValue.dynamicSource?.entryId || "Select Entry"}
-                    </SelectedEntryTitle>
+                    <SelectedEntryTitle>{title}</SelectedEntryTitle>
                 </SelectedEntryLeft>
                 <SelectedEntryRight>
                     <Icon icon={<OpenIcon width={24} height={24} color="#717171" />} />
