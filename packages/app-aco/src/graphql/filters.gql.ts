c9280e195020070225e7b553bd5a10de455dbf25
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-09-27 09:58:30 +0200

    feat: add filter gql query and mutation

---
+++ packages/app-aco/src/graphql/filters.gql.ts
@@ -0,0 +1,84 @@
+import gql from "graphql-tag";
+
+const ERROR_FIELD = /* GraphQL */ `
+    {
+        code
+        data
+        message
+    }
+`;
+
+const DATA_FIELD = /* GraphQL */ `
+    {
+        id
+        name
+        operation
+        groups {
+            operation
+            filters {
+                field
+                condition
+                value
+            }
+        }
+        createdBy {
+            id
+            displayName
+        }
+    }
+`;
+
+export const CREATE_FILTER = gql`
+    mutation CreateFilter($data: FilterCreateInput!) {
+        aco {
+            createFilter(data: $data) {
+                data ${DATA_FIELD}
+                error ${ERROR_FIELD}
+            }
+        }
+    }
+`;
+
+export const LIST_FILTERS = gql`
+    query ListFilters($where: FiltersListWhereInput!, $limit: Int!) {
+        aco {
+            listFilters(where: $where, limit: $limit) {
+                data ${DATA_FIELD}
+                error ${ERROR_FIELD}
+            }
+        }
+    }
+`;
+
+export const GET_FILTER = gql`
+    query GetFilter($id: ID!) {
+        aco {
+            getFilter(id: $id) {
+                data ${DATA_FIELD}
+                error ${ERROR_FIELD}
+            }
+        }
+    }
+`;
+
+export const UPDATE_FILTER = gql`
+    mutation UpdateFilter($id: ID!, $data: FilterUpdateInput!) {
+        aco {
+            updateFilter(id: $id, data: $data) {
+                data ${DATA_FIELD}
+                error ${ERROR_FIELD}
+            }
+        }
+    }
+`;
+
+export const DELETE_FILTER = gql`
+    mutation DeleteFilter($id: ID!) {
+        aco {
+            deleteFilter(id: $id) {
+                data
+                error ${ERROR_FIELD}
+            }
+        }
+    }
+`;
