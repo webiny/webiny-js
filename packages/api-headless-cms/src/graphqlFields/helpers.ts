baa3b4681bb4f6cad2669c16229ea606185eaf56
Bruno Zorić <bruno.zoric@gmail.com>
2023-09-29 10:54:12 +0200

    feat(api-headless-cms): optional validation (#3562)

--- packages/api-headless-cms/src/graphqlFields/helpers.ts
+++ packages/api-headless-cms/src/graphqlFields/helpers.ts
@@ -8,19 +8,6 @@ const getIsRequired = (validations?: CmsModelFieldValidation[]): boolean => {
     });
 };
 
-export const attachRequiredFieldValue = (def: string, field: CmsModelField): string => {
-    if (!field.validation || field.validation.length === 0) {
-        return def;
-    }
-    const isRequired = field.validation.some(validation => {
-        return validation.name === "required";
-    });
-    if (!isRequired) {
-        return def;
-    }
-    return `${def}!`;
-};
-
 const envVars: string[] = [
     "HEADLESS_CMS_GRAPHQL_INPUT_REQUIRE_ARRAY_ITEM",
     "WEBINY_HEADLESS_CMS_GRAPHQL_INPUT_REQUIRE_ARRAY_ITEM"
@@ -31,11 +18,10 @@ const envVars: string[] = [
 export const createGraphQLInputField = (field: CmsModelField, graphQlType: string): string => {
     const singleRequired = getIsRequired(field.validation) ? "!" : "";
     if (!field.multipleValues) {
-        return `${field.fieldId}: ${graphQlType}${singleRequired}`;
+        return `${field.fieldId}: ${graphQlType}`;
     }
-    const multipleRequired = getIsRequired(field.listValidation) ? "!" : "";
 
     const itemRequired = envVars.some(v => process.env[v] === "false") ? "" : singleRequired;
 
-    return `${field.fieldId}: [${graphQlType}${itemRequired}]${multipleRequired}`;
+    return `${field.fieldId}: [${graphQlType}${itemRequired}]`;
 };
