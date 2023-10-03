72590d3db6f2cead049cfa75fa7c844df48d7e97
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2023-10-03 16:13:43 +0200

    wip: simple html paragraph test

--- packages/html-to-lexical-parser/__tests__/html-to-lexical.test.ts
+++ packages/html-to-lexical-parser/__tests__/html-to-lexical.test.ts
@@ -8,8 +8,36 @@ import { simpleHtml } from "./html-articles";
 describe("Test html-to-lexical parser", () => {
     it("should parse html string to lexical object", async () => {
         parseToLexicalObject(simpleHtml, data => {
-            console.log("On success", data);
-            expect(data).toEqual({ test: "string" });
+            expect(data).toMatchObject({
+                root: {
+                    children: [
+                        {
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "Space",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            styles: [],
+                            type: "paragraph-element",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "root",
+                    version: 1
+                }
+            });
         });
     });
 });
