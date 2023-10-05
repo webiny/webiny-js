7f45c6e11c916aa126e185210175564f4220e6b0
Bruno Zorić <bruno.zoric@gmail.com>
2023-10-05 18:54:58 +0200

    feat(api-headless-cms): validate entry mutation (#3568)

---
+++ packages/api-headless-cms/__tests__/contentAPI/cmsEntryValidation/handler.ts
@@ -0,0 +1,51 @@
+import { GraphQLHandlerParams, useGraphQLHandler } from "~tests/testHelpers/useGraphQLHandler";
+import { CmsApiModel } from "~/plugins";
+
+const ERROR = `
+error {
+    message
+    data
+    code
+    stack
+}
+`;
+
+const validateMutation = (model: Pick<CmsApiModel, "singularApiName">) => {
+    return /* GraphQL */ `
+        mutation ValidateProduct($revision: ID, $data: ${model.singularApiName}Input!) {
+        validate: validate${model.singularApiName}(revision: $revision, data: $data) {
+                data {
+                    id
+                    fieldId
+                    error
+                    parents
+                }
+                ${ERROR}
+            }
+        }
+    `;
+};
+
+interface Params extends Partial<GraphQLHandlerParams> {
+    model: Pick<CmsApiModel, "singularApiName">;
+}
+
+export const useValidationManageHandler = (params: Params) => {
+    const contentHandler = useGraphQLHandler({
+        path: "manage/en-US",
+        ...params
+    });
+
+    return {
+        ...contentHandler,
+        async validate(variables: Record<string, any>, headers: Record<string, any> = {}) {
+            return await contentHandler.invoke({
+                body: {
+                    query: validateMutation(params.model),
+                    variables
+                },
+                headers
+            });
+        }
+    };
+};
