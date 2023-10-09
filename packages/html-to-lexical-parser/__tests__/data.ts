2815238a3a4129cb0aec63083afd82d8d685d412
Sasho Mihajlov <sashomihajlov@Sashos-MacBook-Pro.local>
2023-10-09 12:31:47 +0200

    wip: node unit tests part 1

---
+++ packages/html-to-lexical-parser/__tests__/data.ts
@@ -0,0 +1,18 @@
+export const paragraphHtmlTag = `<p>Testing paragraph element</p>`;
+export const htmlWithNotSupportedHtmlTags = `<div class="_1z604">
+                                    <figure data-c="inline-image" class="_3kWNL _2YlTE">
+                                        <button type="button" aria-label="Open Gallery" class="_2bgbL">
+                                            <span class="_1cCV2 _3krT7">See all 37 photos</span>
+                                         </button>
+                                    </figure>
+                                </div>`;
+
+export const boldItalicUnderlineFormatHtml = `<p><u><em><strong>formatted text</strong></em></u></p>`;
+
+export const headingH1Html = `<h1>Testing heading h1 element</h1>`;
+export const headingH4Html = `<h4>Testing heading h4 element</h4>`;
+
+export const bulletListHtml = `<ul><li>list item 1</li><li>list item 2</li></ul>`;
+export const numberedListHtml = `<ol><li>list item 1</li><li>list item 2</li></ol>`;
+export const linkHtml = `<p><a href="https://webiny.com" target="_blank" rel="noopener noreferrer">My webiny link</a></p>`;
+export const quoteHtml = `<blockquote>My quote block</blockquote>`;
--- packages/html-to-lexical-parser/__tests__/html-to-lexical.test.ts
+++ packages/html-to-lexical-parser/__tests__/html-to-lexical.test.ts
@@ -1,13 +1,25 @@
 /**
  * @jest-environment jsdom
  */
-import { createHtmlToLexicalParser } from "~/index";
-import { simpleHtml } from "./html-articles";
 
-describe("Test html-to-lexical parser", () => {
-    it("should parse html paragraph string to lexical object", async () => {
-        const parser = createHtmlToLexicalParser();
-        expect(parser(simpleHtml)).toMatchObject({
+import {
+    boldItalicUnderlineFormatHtml,
+    bulletListHtml,
+    headingH1Html,
+    headingH4Html,
+    htmlWithNotSupportedHtmlTags,
+    linkHtml,
+    numberedListHtml,
+    paragraphHtmlTag,
+    quoteHtml
+} from "./data";
+import { createHtmlToLexicalParser } from "../src/index";
+
+const parser = createHtmlToLexicalParser();
+
+describe("Test how parser convert the html tags into webiny's lexical node objects", () => {
+    it("should parse html to paragraph node", async () => {
+        expect(parser(paragraphHtmlTag)).toMatchObject({
             root: {
                 children: [
                     {
@@ -17,7 +29,7 @@ describe("Test html-to-lexical parser", () => {
                                 format: 0,
                                 mode: "normal",
                                 style: "",
-                                text: "Space",
+                                text: "Testing paragraph element",
                                 type: "text",
                                 version: 1
                             }
@@ -39,3 +51,362 @@ describe("Test html-to-lexical parser", () => {
         });
     });
 });
+it("should normalize the text node - wrap the node in paragraph element", () => {
+    /**
+     * By default, parser will normalize the nodes and convert DOM nodes to supported lexical nodes.
+     * It's expected all unsupported html tags like div, figure, button and other to be removed and not converted.
+     */
+    expect(parser(htmlWithNotSupportedHtmlTags)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            detail: 0,
+                            format: 0,
+                            mode: "normal",
+                            style: "",
+                            text: "See all 37 photos",
+                            type: "text",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    styles: [],
+                    type: "paragraph-element",
+                    version: 1
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should generate paragraph element with text node with bold, italic and underline formats", () => {
+    expect(parser(boldItalicUnderlineFormatHtml)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            detail: 0,
+                            format: 11, // all formats
+                            mode: "normal",
+                            style: "",
+                            text: "formatted text",
+                            type: "text",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "paragraph-element",
+                    version: 1,
+                    styles: []
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should generate heading nodes", () => {
+    /**
+     * Test HTML h1 tag
+     */
+    expect(parser(headingH1Html)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            detail: 0,
+                            format: 0,
+                            mode: "normal",
+                            style: "",
+                            text: "Testing heading h1 element",
+                            type: "text",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    tag: "h1",
+                    type: "heading-element",
+                    version: 1,
+                    styles: []
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+
+    /**
+     * Test HTML h4 tag
+     */
+    expect(parser(headingH4Html)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            detail: 0,
+                            format: 0,
+                            mode: "normal",
+                            style: "",
+                            text: "Testing heading h4 element",
+                            type: "text",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    tag: "h4",
+                    type: "heading-element",
+                    version: 1,
+                    styles: []
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should generate bullet list, and nested list item nodes", () => {
+    expect(parser(bulletListHtml)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            checked: undefined,
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "list item 1",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            type: "webiny-listitem",
+                            version: 1,
+                            value: 1
+                        },
+                        {
+                            checked: undefined,
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "list item 2",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            type: "webiny-listitem",
+                            version: 1,
+                            value: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "webiny-list",
+                    version: 1,
+                    listType: "bullet",
+                    start: 1,
+                    tag: "ul",
+                    themeStyleId: ""
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should generate numbered list, and nested list item nodes", () => {
+    expect(parser(numberedListHtml)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            checked: undefined,
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "list item 1",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            type: "webiny-listitem",
+                            version: 1,
+                            value: 1
+                        },
+                        {
+                            checked: undefined,
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "list item 2",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            type: "webiny-listitem",
+                            version: 1,
+                            value: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "webiny-list",
+                    version: 1,
+                    listType: "number",
+                    start: 1,
+                    tag: "ol",
+                    themeStyleId: ""
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should not generate paragraph and link node", () => {
+    expect(parser(linkHtml)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            children: [
+                                {
+                                    detail: 0,
+                                    format: 0,
+                                    mode: "normal",
+                                    style: "",
+                                    text: "My webiny link",
+                                    type: "text",
+                                    version: 1
+                                }
+                            ],
+                            direction: null,
+                            format: "",
+                            indent: 0,
+                            type: "link-node",
+                            version: 1,
+                            rel: "noopener noreferrer",
+                            target: "_blank",
+                            title: null,
+                            url: "https://webiny.com"
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "paragraph-element",
+                    version: 1,
+                    styles: []
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should not generate image node", () => {});
+
+it("should generate quote node", () => {
+    expect(parser(quoteHtml)).toMatchObject({
+        root: {
+            children: [
+                {
+                    children: [
+                        {
+                            detail: 0,
+                            format: 0,
+                            mode: "normal",
+                            style: "",
+                            text: "My quote block",
+                            type: "text",
+                            version: 1
+                        }
+                    ],
+                    direction: null,
+                    format: "",
+                    indent: 0,
+                    type: "webiny-quote",
+                    version: 1,
+                    styles: [],
+                    styleId: undefined
+                }
+            ],
+            direction: null,
+            format: "",
+            indent: 0,
+            type: "root",
+            version: 1
+        }
+    });
+});
+
+it("should generate code node", () => {});
