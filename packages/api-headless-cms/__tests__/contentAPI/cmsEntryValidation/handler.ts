3b4390c8e255610369a93216fa53a35ed7ee0756
Bruno Zorić <bruno.zoric@gmail.com>
2023-10-11 12:14:44 +0200

    feat(api-headless-cms): enable skipping validator by name (#3582)

--- packages/api-headless-cms/__tests__/contentAPI/cmsEntryValidation/handler.ts
+++ packages/api-headless-cms/__tests__/contentAPI/cmsEntryValidation/handler.ts
@@ -1,30 +1,12 @@
 import { GraphQLHandlerParams, useGraphQLHandler } from "~tests/testHelpers/useGraphQLHandler";
 import { CmsApiModel } from "~/plugins";
-
-const ERROR = `
-error {
-    message
-    data
-    code
-    stack
-}
-`;
-
-const validateMutation = (model: Pick<CmsApiModel, "singularApiName">) => {
-    return /* GraphQL */ `
-        mutation ValidateProduct($revision: ID, $data: ${model.singularApiName}Input!) {
-        validate: validate${model.singularApiName}(revision: $revision, data: $data) {
-                data {
-                    id
-                    fieldId
-                    error
-                    parents
-                }
-                ${ERROR}
-            }
-        }
-    `;
-};
+import {
+    validateMutation,
+    createMutation,
+    updateMutation,
+    createRevisionMutation,
+    publishMutation
+} from "./handler.graphql";
 
 interface Params extends Partial<GraphQLHandlerParams> {
     model: Pick<CmsApiModel, "singularApiName">;
@@ -42,6 +24,42 @@ export const useValidationManageHandler = (params: Params) => {
             return await contentHandler.invoke({
                 body: {
                     query: validateMutation(params.model),
+                    variables
+                },
+                headers
+            });
+        },
+        async create(variables: Record<string, any>, headers: Record<string, any> = {}) {
+            return await contentHandler.invoke({
+                body: {
+                    query: createMutation(params.model),
+                    variables
+                },
+                headers
+            });
+        },
+        async createRevision(variables: Record<string, any>, headers: Record<string, any> = {}) {
+            return await contentHandler.invoke({
+                body: {
+                    query: createRevisionMutation(params.model),
+                    variables
+                },
+                headers
+            });
+        },
+        async update(variables: Record<string, any>, headers: Record<string, any> = {}) {
+            return await contentHandler.invoke({
+                body: {
+                    query: updateMutation(params.model),
+                    variables
+                },
+                headers
+            });
+        },
+        async publish(variables: Record<string, any>, headers: Record<string, any> = {}) {
+            return await contentHandler.invoke({
+                body: {
+                    query: publishMutation(params.model),
                     variables
                 },
                 headers
