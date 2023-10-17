b9df5c7ea27ea4f47cc3f724e39964682d3b6d0c
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-10-17 10:51:14 +0200

    chore: remove docs

--- packages/api-aco/src/createAcoFields.ts
+++ packages/api-aco/src/createAcoFields.ts
@@ -1,6 +1,5 @@
 import { CmsModelFieldToGraphQLPlugin } from "@webiny/api-headless-cms/types";
 
-// Creating an internal JSON field, we are using it inside the `record` type
 const jsonField: CmsModelFieldToGraphQLPlugin = {
     name: "cms-model-field-to-graphql-json",
     type: "cms-model-field-to-graphql",
