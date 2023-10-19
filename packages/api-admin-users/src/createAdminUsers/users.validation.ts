5f6a205291b0a1fa9bd89706643a5c137b18a191
adrians5j <adrian@webiny.com>
2023-10-19 14:07:06 +0200

    fix: update validator

--- packages/api-admin-users/src/createAdminUsers/users.validation.ts
+++ packages/api-admin-users/src/createAdminUsers/users.validation.ts
@@ -14,7 +14,14 @@ import { AdminUsers } from "~/types";
 const CreateUserDataModel = withFields({
     id: string({ validation: validation.create("minLength:2") }),
     displayName: string({ validation: validation.create("minLength:2") }),
-    email: string({ validation: validation.create("minLength:2") }),
+
+    // We did not use an e-mail validator here, just because external
+    // IdPs (Okta, Auth0) do not require e-mail to be present. When creating
+    // admin users, they're actually passing the user's ID as the e-mail.
+    // For example: packages/api-security-okta/src/createAdminUsersHooks.ts:13
+    // In the future, we might want to rename this field to `idpId` or similar.
+    email: string({ validation: validation.create("required,minLength:2") }),
+
     firstName: string({ validation: validation.create("minLength:2") }),
     lastName: string({ validation: validation.create("minLength:2") }),
     avatar: object()
