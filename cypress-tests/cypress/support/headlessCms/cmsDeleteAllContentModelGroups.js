d0395ab2fd927a932383c7f9edba1b50ed005307
adrians5j <adrian@webiny.com>
2023-10-20 16:48:03 +0200

    wip: finalize

--- cypress-tests/cypress/support/headlessCms/cmsDeleteAllContentModelGroups.js
+++ cypress-tests/cypress/support/headlessCms/cmsDeleteAllContentModelGroups.js
@@ -2,7 +2,9 @@ Cypress.Commands.add("cmsDeleteAllContentModelGroups", () => {
     cy.cmsListContentModelGroup().then(groups => {
         for (let i = 0; i < groups.length; i++) {
             const group = groups[i];
-            cy.cmsDeleteContentModelGroup({ id: group.id });
+            if (group.slug !== "ungrouped") {
+                cy.cmsDeleteContentModelGroup({ id: group.id });
+            }
         }
     });
 });
