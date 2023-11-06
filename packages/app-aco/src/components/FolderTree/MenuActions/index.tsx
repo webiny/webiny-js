83a4056fc7f6a3bf2890e84796ea09e06a91136d
adrians5j <adrian@webiny.com>
2023-11-06 13:19:19 +0100

    fix(flp): disable folder-level actions accordingly

--- packages/app-aco/src/components/FolderTree/MenuActions/index.tsx
+++ packages/app-aco/src/components/FolderTree/MenuActions/index.tsx
@@ -32,7 +32,7 @@ export const MenuActions: React.VFC<MenuActionsProps> = ({
         return null;
     }
 
-    // If the user cannot manage structure for the folder, no need to show the menu.
+    // If the user cannot manage folder structure, no need to show the menu.
     if (!folder.canManageStructure) {
         return null;
     }
