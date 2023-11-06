0cace32bfeedf72eb37fc67a2fa68eae366fd505
adrians5j <adrian@webiny.com>
2023-11-06 13:13:24 +0100

    fix(flp): disable folder-level actions accordingly

--- packages/app-aco/src/components/FolderTree/ButtonCreate/styled.tsx
+++ packages/app-aco/src/components/FolderTree/ButtonCreate/styled.tsx
@@ -2,22 +2,22 @@ import styled from "@emotion/styled";
 import { ReactComponent as Plus } from "@material-design-icons/svg/outlined/add_circle_outline.svg";
 
 export const Button = styled("button")`
-  background: none;
-  border: none;
-  cursor: pointer;
-  outline: none;
-  font-family: var(--mdc-typography-font-family);
-  color: var(--webiny-theme-color-text-secondary);
-  fill: currentColor;
-  display: flex;
-  align-items: center;
-  font-size: 1em;
-  padding: 8px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    font-family: var(--mdc-typography-font-family);
+    color: var(--webiny-theme-color-text-secondary);
+    fill: currentColor;
+    display: flex;
+    align-items: center;
+    font-size: 1em;
+    padding: 8px;
 
-  &:disabled {
-    cursor: not-allowed;
-    opacity: 0.5;
-  }
+    &:disabled {
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
 `;
 
 export const IconContainer = styled.div`
