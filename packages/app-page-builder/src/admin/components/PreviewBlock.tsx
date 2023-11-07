6daf38d3ed0c029a8fea005c2b6246e5b325a09c
Pavel Denisjuk <pavel@webiny.com>
2023-11-07 18:14:40 +0100

    fix(app-page-builder): add ResponsiveElementsProvider to fix blocks preview

--- packages/app-page-builder/src/admin/components/PreviewBlock.tsx
+++ packages/app-page-builder/src/admin/components/PreviewBlock.tsx
@@ -3,14 +3,9 @@ import { Content as ContentType } from "@webiny/app-page-builder-elements/types"
 import { Content } from "@webiny/app-page-builder-elements/components/Content";
 import { PbPageBlock, PbEditorElement } from "~/types";
 import { addElementId } from "~/editor/helpers";
-import { PageElementsProvider } from "~/contexts/PageBuilder/PageElementsProvider";
 
 export const PreviewBlock = ({ element }: { element: PbPageBlock | PbEditorElement }) => {
     const elementsWithIds = addElementId(element.content);
 
-    return (
-        <PageElementsProvider>
-            <Content content={elementsWithIds as ContentType} />
-        </PageElementsProvider>
-    );
+    return <Content content={elementsWithIds as ContentType} />;
 };
