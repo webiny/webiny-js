01ef2eabe5cd5f20bf1f4188434b20f964e994d5
Bruno Zorić <bruno.zoric@gmail.com>
2023-11-10 15:47:52 +0100

    fix(api-elasticsearch): use env credentials

--- packages/api-elasticsearch/src/client.ts
+++ packages/api-elasticsearch/src/client.ts
@@ -1,64 +1,79 @@
-import { Client, ClientOptions } from "@elastic/elasticsearch";
-import { fromTemporaryCredentials } from "@webiny/aws-sdk/credential-providers";
-import WebinyError from "@webiny/error";
-/**
- * Package aws-elasticsearch-connector does not have types.
- */
-// @ts-ignore
 import createAwsElasticsearchConnector from "aws-elasticsearch-connector";
+import crypto from "crypto";
+import WebinyError from "@webiny/error";
+import { Client, ClientOptions } from "@elastic/elasticsearch";
 
 export interface ElasticsearchClientOptions extends ClientOptions {
     endpoint?: string;
 }
 
-export const createElasticsearchClient = (options: ElasticsearchClientOptions) => {
-    const { endpoint, node, ...rest } = options;
+const clients = new Map<string, Client>();
 
-    const clientOptions: ClientOptions = {
-        node: endpoint || node,
-        ...rest
+const createClientKey = (options: ElasticsearchClientOptions) => {
+    const key = JSON.stringify(options);
+    const hash = crypto.createHash("sha1");
+    hash.update(key);
+    return hash.digest("hex");
+};
+
+export const createElasticsearchClient = (options: ElasticsearchClientOptions): Client => {
+    const key = createClientKey(options);
+    const existing = clients.get(key);
+    if (existing) {
+        return existing;
+    }
+
+    const cached = (): Client => {
+        const { endpoint, node, ...rest } = options;
+
+        let clientOptions: ClientOptions = {
+            node: endpoint || node,
+            ...rest
+        };
+
+        if (!clientOptions.auth) {
+            const region = String(process.env.AWS_REGION);
+            /**
+             * TODO fix the CI to output the AWS_SESSION_TOKEN
+             */
+            const credentials = {
+                accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
+                secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+                sessionToken: String(process.env.AWS_SESSION_TOKEN)
+            };
+
+            clientOptions = {
+                ...clientOptions,
+                // @ts-expect-error
+                ...createAwsElasticsearchConnector({
+                    region,
+                    credentials
+                })
+            };
+        }
+
+        try {
+            return new Client(clientOptions);
+        } catch (ex) {
+            const data = {
+                error: ex,
+                node: endpoint || node,
+                ...rest,
+                auth: undefined
+            };
+            console.log({
+                ...data
+            });
+            throw new WebinyError(
+                "Could not connect to Elasticsearch.",
+                "ELASTICSEARCH_CLIENT_ERROR",
+                data
+            );
+        }
     };
 
-    if (!clientOptions.auth) {
-        /**
-         * If no `auth` configuration is present, we setup AWS connector.
-         */
-        const credentials = fromTemporaryCredentials({
-            params: {
-                RoleArn: "arn:aws:iam::0123456789012:role/Administrator",
-                RoleSessionName: "temporary-session",
-                DurationSeconds: 3600
-            }
-        })();
+    const client = cached();
 
-        Object.assign(
-            clientOptions,
-            createAwsElasticsearchConnector({
-                region: process.env.AWS_REGION,
-                // aws-elasticsearch-connector still uses aws-sdk v2 types.
-                // TODO fix types
-                // @ts-expect-error
-                credentials
-            })
-        );
-    }
-
-    try {
-        return new Client(clientOptions);
-    } catch (ex) {
-        const data = {
-            error: ex,
-            node: endpoint || node,
-            ...rest,
-            auth: undefined
-        };
-        console.log({
-            ...data
-        });
-        throw new WebinyError(
-            "Could not connect to Elasticsearch.",
-            "ELASTICSEARCH_CLIENT_ERROR",
-            data
-        );
-    }
+    clients.set(key, client);
+    return client;
 };
