a0445c0f63502bf6ead4d5677739f9783efc6200
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-12-14 08:51:32 +0100

    refactor: handle useChangeEntryStatus hooks logic internally

--- packages/app-headless-cms/src/admin/hooks/useChangeEntryStatus.tsx
+++ packages/app-headless-cms/src/admin/hooks/useChangeEntryStatus.tsx
@@ -1,6 +1,7 @@
-import { useCallback } from "react";
-import { useConfirmationDialog } from "@webiny/app-admin";
-import { useRevision } from "~/admin/views/contentEntries/ContentEntry/useRevision";
+import React, { useCallback } from "react";
+import { useConfirmationDialog, useSnackbar } from "@webiny/app-admin";
+import { useRecords } from "@webiny/app-aco";
+import { useCms, useContentEntry } from "~/admin/hooks/index";
 import { CmsContentEntry } from "~/types";
 
 interface UseChangeEntryStatusParams {
@@ -8,14 +9,10 @@ interface UseChangeEntryStatusParams {
 }
 
 export const useChangeEntryStatus = ({ entry }: UseChangeEntryStatusParams) => {
-    const { unpublishRevision, publishRevision } = useRevision({
-        revision: {
-            id: entry.id,
-            meta: {
-                version: entry.meta.version
-            }
-        }
-    });
+    const { publishEntryRevision, unpublishEntryRevision } = useCms();
+    const { contentModel } = useContentEntry();
+    const { showSnackbar } = useSnackbar();
+    const { updateRecordInCache } = useRecords();
 
     const { showConfirmation: showPublishConfirmation } = useConfirmationDialog({
         title: "Publish CMS Entry",
@@ -30,17 +27,55 @@ export const useChangeEntryStatus = ({ entry }: UseChangeEntryStatusParams) => {
     const openDialogPublishEntry = useCallback(
         () =>
             showPublishConfirmation(async () => {
-                await publishRevision(entry.id);
+                const response = await publishEntryRevision({
+                    model: contentModel,
+                    entry,
+                    id: entry.id
+                });
+
+                const { error, entry: entryResult } = response;
+                if (error) {
+                    showSnackbar(error.message);
+                    return;
+                }
+
+                updateRecordInCache(entryResult);
+
+                showSnackbar(
+                    <span>
+                        Successfully published revision{" "}
+                        <strong>#{response.entry!.meta.version}</strong>!
+                    </span>
+                );
             }),
-        [entry.id]
+        [entry.id, contentModel]
     );
 
     const openDialogUnpublishEntry = useCallback(
         () =>
             showUnpublishConfirmation(async () => {
-                await unpublishRevision(entry.id);
+                const response = await unpublishEntryRevision({
+                    model: contentModel,
+                    entry,
+                    id: entry.id
+                });
+
+                const { error, entry: entryResult } = response;
+
+                if (error) {
+                    showSnackbar(error.message);
+                    return;
+                }
+
+                updateRecordInCache(entryResult);
+
+                showSnackbar(
+                    <span>
+                        Successfully unpublished revision <strong>#{entry.meta.version}</strong>!
+                    </span>
+                );
             }),
-        [entry.id]
+        [entry.id, contentModel]
     );
 
     return {
