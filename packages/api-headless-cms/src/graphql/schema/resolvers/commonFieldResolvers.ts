dde48ab3fae82d0eb5056df16ba885f64ed5a6b9
adrians5j <adrian@webiny.com>
2023-12-19 13:16:05 +0100

    wip: implement new meta fields

--- packages/api-headless-cms/src/graphql/schema/resolvers/commonFieldResolvers.ts
+++
@@ -1,9 +0,0 @@
-import { CmsEntry } from "~/types";
-
-// TODO: check this, why do we need this?
-export const commonFieldResolvers = () => ({
-    id: (entry: CmsEntry) => entry.id || null,
-    createdBy: (entry: CmsEntry) => entry.createdBy || null,
-    ownedBy: (entry: CmsEntry) => entry.ownedBy || null,
-    modifiedBy: (entry: CmsEntry) => entry.modifiedBy || null
-});
