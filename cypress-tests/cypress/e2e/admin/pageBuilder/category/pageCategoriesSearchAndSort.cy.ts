5fe9f25735140832c356efdaf9ef41913492ec55
adrians5j <adrian@webiny.com>
2023-12-19 14:36:32 +0100

    wip: improve menu-items test

--- cypress-tests/cypress/e2e/admin/pageBuilder/category/pageCategoriesSearchAndSort.cy.ts
+++ cypress-tests/cypress/e2e/admin/pageBuilder/category/pageCategoriesSearchAndSort.cy.ts
@@ -36,97 +36,96 @@ context("Page Builder - Page Categories Search and Sort", () => {
     it("Should be able to create, edit, and immediately delete a category", () => {
         cy.visit("/page-builder/categories");
 
-        // // Assert all the programmatically created categories are being properly displayed.
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.name).should("exist");
-        //     cy.contains(categoryData1.url).should("exist");
-        //     cy.contains(categoryData2.name).should("exist");
-        //     cy.contains(categoryData2.url).should("exist");
-        //     cy.contains(categoryData3.name).should("exist");
-        //     cy.contains(categoryData3.url).should("exist");
-        //     cy.contains(categoryData4.name).should("exist");
-        //     cy.contains(categoryData4.url).should("exist");
-        // });
-        //
-        // // Assert all the categories are being displayed properly when searched for by name.
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData1.name);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.name).should("exist");
-        //     cy.contains(categoryData2.name).should("not.exist");
-        //     cy.contains(categoryData3.name).should("not.exist");
-        //     cy.contains(categoryData4.name).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData2.name);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.name).should("not.exist");
-        //     cy.contains(categoryData2.name).should("exist");
-        //     cy.contains(categoryData3.name).should("not.exist");
-        //     cy.contains(categoryData4.name).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData3.name);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.name).should("not.exist");
-        //     cy.contains(categoryData2.name).should("not.exist");
-        //     cy.contains(categoryData3.name).should("exist");
-        //     cy.contains(categoryData4.name).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData4.name);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.name).should("not.exist");
-        //     cy.contains(categoryData2.name).should("not.exist");
-        //     cy.contains(categoryData3.name).should("not.exist");
-        //     cy.contains(categoryData4.name).should("exist");
-        // });
-        //
-        // // Assert all the categories are being displayed properly when searched for by URL.
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData1.url);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.url).should("exist");
-        //     cy.contains(categoryData2.url).should("not.exist");
-        //     cy.contains(categoryData3.url).should("not.exist");
-        //     cy.contains(categoryData4.url).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData2.url);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.url).should("not.exist");
-        //     cy.contains(categoryData2.url).should("exist");
-        //     cy.contains(categoryData3.url).should("not.exist");
-        //     cy.contains(categoryData4.url).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData3.url);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.url).should("not.exist");
-        //     cy.contains(categoryData2.url).should("not.exist");
-        //     cy.contains(categoryData3.url).should("exist");
-        //     cy.contains(categoryData4.url).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear().type(categoryData4.url);
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.url).should("not.exist");
-        //     cy.contains(categoryData2.url).should("not.exist");
-        //     cy.contains(categoryData3.url).should("not.exist");
-        //     cy.contains(categoryData4.url).should("exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories")
-        //     .clear()
-        //     .type("This string should not return any results");
-        //
-        // cy.findByTestId("default-data-list").then(() => {
-        //     cy.contains(categoryData1.url).should("not.exist");
-        //     cy.contains(categoryData2.url).should("not.exist");
-        //     cy.contains(categoryData3.url).should("not.exist");
-        //     cy.contains(categoryData4.url).should("not.exist");
-        // });
-        //
-        // cy.findByPlaceholderText("Search categories").clear();
-        //
+        // Assert all the programmatically created categories are being properly displayed.
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.name).should("exist");
+            cy.contains(categoryData1.url).should("exist");
+            cy.contains(categoryData2.name).should("exist");
+            cy.contains(categoryData2.url).should("exist");
+            cy.contains(categoryData3.name).should("exist");
+            cy.contains(categoryData3.url).should("exist");
+            cy.contains(categoryData4.name).should("exist");
+            cy.contains(categoryData4.url).should("exist");
+        });
+
+        // Assert all the categories are being displayed properly when searched for by name.
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData1.name);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.name).should("exist");
+            cy.contains(categoryData2.name).should("not.exist");
+            cy.contains(categoryData3.name).should("not.exist");
+            cy.contains(categoryData4.name).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData2.name);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.name).should("not.exist");
+            cy.contains(categoryData2.name).should("exist");
+            cy.contains(categoryData3.name).should("not.exist");
+            cy.contains(categoryData4.name).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData3.name);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.name).should("not.exist");
+            cy.contains(categoryData2.name).should("not.exist");
+            cy.contains(categoryData3.name).should("exist");
+            cy.contains(categoryData4.name).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData4.name);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.name).should("not.exist");
+            cy.contains(categoryData2.name).should("not.exist");
+            cy.contains(categoryData3.name).should("not.exist");
+            cy.contains(categoryData4.name).should("exist");
+        });
+
+        // Assert all the categories are being displayed properly when searched for by URL.
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData1.url);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.url).should("exist");
+            cy.contains(categoryData2.url).should("not.exist");
+            cy.contains(categoryData3.url).should("not.exist");
+            cy.contains(categoryData4.url).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData2.url);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.url).should("not.exist");
+            cy.contains(categoryData2.url).should("exist");
+            cy.contains(categoryData3.url).should("not.exist");
+            cy.contains(categoryData4.url).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData3.url);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.url).should("not.exist");
+            cy.contains(categoryData2.url).should("not.exist");
+            cy.contains(categoryData3.url).should("exist");
+            cy.contains(categoryData4.url).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear().type(categoryData4.url);
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.url).should("not.exist");
+            cy.contains(categoryData2.url).should("not.exist");
+            cy.contains(categoryData3.url).should("not.exist");
+            cy.contains(categoryData4.url).should("exist");
+        });
+
+        cy.findByPlaceholderText("Search categories")
+            .clear()
+            .type("This string should not return any results");
+
+        cy.findByTestId("default-data-list").then(() => {
+            cy.contains(categoryData1.url).should("not.exist");
+            cy.contains(categoryData2.url).should("not.exist");
+            cy.contains(categoryData3.url).should("not.exist");
+            cy.contains(categoryData4.url).should("not.exist");
+        });
+
+        cy.findByPlaceholderText("Search categories").clear();
 
         // Assert that all the categories are being sorted properly.
         cy.findByTestId("default-data-list.filter").click();
