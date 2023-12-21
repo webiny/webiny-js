1ccc54a755a99f46859c3b99d8d712768cf57dd5
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-12-21 12:00:50 +0100

    fix: handle disabled OptionsMenuLink

--- packages/app-admin/src/components/OptionsMenu/OptionsMenuLink.tsx
+++ packages/app-admin/src/components/OptionsMenu/OptionsMenuLink.tsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { HTMLAttributeAnchorTarget } from "react";
 import { Icon } from "@webiny/ui/Icon";
 import { ListItemGraphic } from "@webiny/ui/List";
 import { MenuItem } from "@webiny/ui/Menu";
 import { Link } from "@webiny/react-router";
 
 export interface OptionsMenuLinkProps {
-    to: string;
     disabled?: boolean;
     icon: React.ReactElement;
     label: string;
+    to: string;
+    target?: HTMLAttributeAnchorTarget;
     ["data-testid"]?: string;
 }
 
-export const OptionsMenuLink = (props: OptionsMenuLinkProps) => {
+const MenuLinkItem = (props: Omit<OptionsMenuLinkProps, "to" | "target">) => {
     return (
-        <Link to={props.to}>
-            <MenuItem disabled={props.disabled ?? false} data-testid={props["data-testid"]}>
-                <ListItemGraphic>
-                    <Icon icon={props.icon} />
-                </ListItemGraphic>
-                {props.label}
-            </MenuItem>
+        <MenuItem disabled={props.disabled ?? false} data-testid={props["data-testid"]}>
+            <ListItemGraphic>
+                <Icon icon={props.icon} />
+            </ListItemGraphic>
+            {props.label}
+        </MenuItem>
+    );
+};
+
+export const OptionsMenuLink = (props: OptionsMenuLinkProps) => {
+    if (props.disabled) {
+        return (
+            <MenuLinkItem
+                icon={props.icon}
+                label={props.label}
+                disabled={props.disabled}
+                data-testid={props["data-testid"]}
+            />
+        );
+    }
+
+    return (
+        <Link to={props.to} target={props.target}>
+            <MenuLinkItem
+                icon={props.icon}
+                label={props.label}
+                data-testid={props["data-testid"]}
+            />
         </Link>
     );
 };
