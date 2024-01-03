33ad86b6636462cb44dd1291608ec9b334b6998b
Pavel Denisjuk <pavel@webiny.com>
2024-01-03 18:07:56 +0100

    chore: remove console logs

--- packages/handler-aws/src/createHandler.ts
+++ packages/handler-aws/src/createHandler.ts
@@ -6,8 +6,6 @@ export const createHandler: HandlerFactory = ({ plugins, ...params }) => {
     const pluginsContainer = new AsyncPluginsContainer(plugins);
 
     return async (event, context) => {
-        console.log("event", event);
-        console.log("context", context);
         const plugins = await pluginsContainer.init();
         const handler = registry.getHandler(event, context);
         return handler.handle({
