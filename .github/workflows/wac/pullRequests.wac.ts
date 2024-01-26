b3db9d9386e1aa3a3498e75a90bdded59aeeb343
adrians5j <adrian@webiny.com>
2024-01-26 16:28:29 +0100

    ci: upgrade `webiny/action-conventional-commits` to v1.3.0

--- .github/workflows/wac/pullRequests.wac.ts
+++ .github/workflows/wac/pullRequests.wac.ts
@@ -10,7 +10,7 @@ export const pullRequests = createWorkflow({
         validateCommits: createJob({
             name: "Validate commit messages",
             if: "github.base_ref != 'dev'",
-            steps: [{ uses: "webiny/action-conventional-commits@v1.2.0" }]
+            steps: [{ uses: "webiny/action-conventional-commits@v1.3.0" }]
         }),
         // Don't allow "feat" commits to be merged into "dev" branch.
         validateCommitsDev: createJob({
@@ -18,7 +18,7 @@ export const pullRequests = createWorkflow({
             if: "github.base_ref == 'dev'",
             steps: [
                 {
-                    uses: "webiny/action-conventional-commits@v1.2.0",
+                    uses: "webiny/action-conventional-commits@v1.3.0",
                     with: {
                         // If dev, use "dev" commit types, otherwise use "next" commit types.
                         "allowed-commit-types":
