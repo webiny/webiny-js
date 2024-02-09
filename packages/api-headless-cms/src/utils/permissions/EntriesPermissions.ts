01bd7c08431b2c9ee5bfc1a32e28334d75535d9b
adrians5j <adrian@webiny.com>
2024-02-09 10:34:46 +0100

    wip: improve models/entries permissions checks

--- packages/api-headless-cms/src/utils/permissions/EntriesPermissions.ts
+++
@@ -1,4 +0,0 @@
-import { CmsEntryPermission } from "~/types";
-import { AppPermissions } from "@webiny/api-security";
-
-export class EntriesPermissions extends AppPermissions<CmsEntryPermission> {}
