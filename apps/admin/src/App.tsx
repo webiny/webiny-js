65d8c4beef7ebcd3115c97eb1e23d93e8e357d55
Bruno Zorić <bruno.zoric@gmail.com>
2024-02-14 19:53:07 +0100

    fix: remove unnecessary code from App.tsx

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,31 +1,12 @@
 import React from "react";
-import { Admin, createComponentPlugin } from "@webiny/app-serverless-cms";
+import { Admin } from "@webiny/app-serverless-cms";
 import { Cognito } from "@webiny/app-admin-users-cognito";
-import { Components } from "@webiny/app-headless-cms";
 import "./App.scss";
-
-const MyCustomComponent = createComponentPlugin(
-    Components.FieldRenderers.DynamicZone.ContentItem,
-    Original => {
-        return function MyCustomComponent(props) {
-            const { field, children } = props;
-
-            console.log(props.bind);
-
-            return (
-                <Original {...props} title={`${field.label} - ${field.type}`}>
-                    {children}
-                </Original>
-            );
-        };
-    }
-);
 
 export const App: React.FC = () => {
     return (
         <Admin>
             <Cognito />
-            <MyCustomComponent />
         </Admin>
     );
 };
