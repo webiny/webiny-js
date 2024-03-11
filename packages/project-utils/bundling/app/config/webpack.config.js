ed8283f237e7738a45000f1ebfe2b489b73d0bdf
Adrian Smijulj <adrian1358@gmail.com>
2024-03-11 15:54:58 +0100

    fix: add newly required Webpack plugins (#3986)

--- packages/project-utils/bundling/app/config/webpack.config.js
+++ packages/project-utils/bundling/app/config/webpack.config.js
@@ -232,7 +232,8 @@ module.exports = function (webpackEnv, { paths, options }) {
                 assert: require.resolve("assert-browserify"),
                 buffer: require.resolve("buffer/"),
                 crypto: require.resolve("crypto-browserify"),
-                path: require.resolve("path-browserify")
+                path: require.resolve("path-browserify"),
+                vm: require.resolve("vm-browserify")
             }
         },
 
@@ -418,6 +419,9 @@ module.exports = function (webpackEnv, { paths, options }) {
         plugins: [
             new webpack.ProvidePlugin({
                 Buffer: ["buffer", "Buffer"]
+            }),
+            new webpack.ProvidePlugin({
+                process: "process/browser"
             }),
             // Generates an `index.html` file with the <script> injected.
             new HtmlWebpackPlugin(
