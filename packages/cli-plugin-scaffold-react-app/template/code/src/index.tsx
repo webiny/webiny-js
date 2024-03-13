2e7f67565553e498f2a1375edd378743f611f865
Bruno Zorić <bruno.zoric@gmail.com>
2024-03-13 12:51:42 +0100

    fix(cli-plugin-scaffold-react-app): missing react import

--- packages/cli-plugin-scaffold-react-app/template/code/src/index.tsx
+++ packages/cli-plugin-scaffold-react-app/template/code/src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "cross-fetch/polyfill";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -8,4 +9,4 @@ import { renderApp } from "@webiny/app";
 // Your React application's top `App` component.
 // Usually, it's best to leave this file as is.
 
-renderApp(App);
+renderApp(<App />);
