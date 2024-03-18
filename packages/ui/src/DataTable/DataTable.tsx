72834894df9e8997d62f2b894dc72c9ab1712a18
Bruno Zorić <bruno.zoric@gmail.com>
2024-03-18 12:39:48 +0100

    fix(ui): memoize data table columns and data

--- packages/ui/src/DataTable/DataTable.tsx
+++ packages/ui/src/DataTable/DataTable.tsx
@@ -348,8 +348,8 @@ const TableRow = <T,>({ selected, cells, getColumnWidth }: TableRowProps<T>) => 
 const MemoTableRow = typedMemo(TableRow);
 
 export const DataTable = <T extends Record<string, any> & DefaultData>({
-    data,
-    columns,
+    data: initialData,
+    columns: initialColumns,
     onSelectRow,
     onToggleRow,
     loadingInitial,
@@ -367,6 +367,8 @@ export const DataTable = <T extends Record<string, any> & DefaultData>({
 }: Props<T>) => {
     const tableRef = useRef<HTMLDivElement>(null);
     const [tableWidth, setTableWidth] = useState(1);
+
+    const data = defineData(initialData, loadingInitial);
 
     useEffect(() => {
         const updateElementWidth = () => {
@@ -426,14 +428,16 @@ export const DataTable = <T extends Record<string, any> & DefaultData>({
         return sorting;
     }, [sorting]);
 
+    const columns = defineColumns(initialColumns, {
+        canSelectAllRows,
+        onSelectRow,
+        onToggleRow,
+        loadingInitial
+    });
+
     const table = useReactTable<T>({
-        data: defineData(data, loadingInitial),
-        columns: defineColumns(columns, {
-            canSelectAllRows,
-            onSelectRow,
-            onToggleRow,
-            loadingInitial
-        }),
+        data,
+        columns,
         enableColumnResizing: true,
         columnResizeMode: "onChange",
         getCoreRowModel: getCoreRowModel(),
@@ -470,7 +474,7 @@ export const DataTable = <T extends Record<string, any> & DefaultData>({
      */
     const tableRows = useMemo(() => {
         return table.getRowModel().rows;
-    }, [table]);
+    }, [table, data, columns]);
 
     return (
         <div ref={tableRef}>
