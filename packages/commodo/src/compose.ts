1c3b1204ceff483334b6223e3e1d844570ed4874
Pavel Denisjuk <pavel@webiny.com>
2024-03-21 22:00:11 +0100

    chore: remove @webiny/commodo and its typings

--- packages/commodo/src/compose.ts
+++
@@ -1,5 +0,0 @@
-/**
- * We need to ignore here because ramda TS produces errors.
- */
-// @ts-expect-error
-export { default as compose } from "ramda/src/compose";
--- packages/commodo/src/fields.ts
+++
@@ -1,5 +0,0 @@
-/**
- * We need to ignore here because @commodo/fields do not have types.
- */
-// @ts-expect-error
-export * from "@commodo/fields";
