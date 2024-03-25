0ce9d11362a6348dfbb2fe443922ebc84167a786
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-03-25 20:40:12 +0100

    chore: fix eslint

--- packages/app-trash-bin/src/components/TrashBin/TrashBin.tsx
+++ packages/app-trash-bin/src/components/TrashBin/TrashBin.tsx
@@ -8,7 +8,6 @@ import {
     ITrashBinListGateway
 } from "@webiny/app-trash-bin-common";
 import {
-    LoadingRepository,
     loadingRepositoryFactory,
     metaRepositoryFactory,
     sortRepositoryFactory
