f7d43ad6c987623e783d6fc884b35488bcd0c6ad
Pavel Denisjuk <pavel@webiny.com>
2024-03-26 20:39:01 +0100

    fix(app-page-builder): add scrollable container to sidebar [skip ci]

--- packages/app-page-builder/src/editor/defaultConfig/Sidebar/ElementSettings/ElementSettingsGroup.tsx
+++ packages/app-page-builder/src/editor/defaultConfig/Sidebar/ElementSettings/ElementSettingsGroup.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Sidebar } from "~/editor/config/Sidebar/Sidebar";
 import { useActiveElement } from "~/editor/hooks/useActiveElement";
+import { ScrollableContainer } from "~/editor/defaultConfig/Sidebar/ScrollableContainer";
 
 export const ElementSettingsGroup = () => {
     const [element] = useActiveElement();
 
     return (
-        <Sidebar.Group.Tab
-            label={"Element"}
-            element={<Sidebar.Elements group={"element"} />}
-            disabled={!element}
-        />
+        <ScrollableContainer>
+            <Sidebar.Group.Tab
+                label={"Element"}
+                element={<Sidebar.Elements group={"element"} />}
+                disabled={!element}
+            />
+        </ScrollableContainer>
     );
 };
--- packages/app-page-builder/src/pageEditor/config/Sidebar/TemplateMode.tsx
+++ packages/app-page-builder/src/pageEditor/config/Sidebar/TemplateMode.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { VariableSettings } from "~/editor/plugins/elementSettings/variable/VariableSettings";
 import { useTemplateMode } from "~/pageEditor/hooks/useTemplateMode";
 import { PageEditorConfig } from "~/pageEditor/editorConfig/PageEditorConfig";
+import { ScrollableContainer } from "~/editor/defaultConfig/Sidebar/ScrollableContainer";
 
 export const TemplateMode = PageEditorConfig.Sidebar.Elements.createDecorator(Original => {
     return function TemplateMode(props) {
         const [isTemplateMode] = useTemplateMode();
 
         if (props.group === "groups" && isTemplateMode) {
-            return <VariableSettings />;
+            return (
+                <ScrollableContainer>
+                    <VariableSettings />
+                </ScrollableContainer>
+            );
         }
 
         return <Original {...props} />;
