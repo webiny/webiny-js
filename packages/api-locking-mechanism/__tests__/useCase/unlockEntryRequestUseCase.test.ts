1631df8986cb08b736d2cd14accd3738177a6f2c
Bruno Zorić <bruno.zoric@gmail.com>
2024-03-27 13:12:03 +0100

    test(api-locking-mechanism): use cases

---
+++ packages/api-locking-mechanism/__tests__/useCase/unlockEntryRequestUseCase.test.ts
@@ -0,0 +1,104 @@
+import { UnlockEntryRequestUseCase } from "~/useCases/UnlockRequestUseCase/UnlockEntryRequestUseCase";
+import {
+    IGetLockRecordUseCase,
+    IGetLockRecordUseCaseExecuteParams
+} from "~/abstractions/IGetLockRecordUseCase";
+import { getSecurityIdentity } from "~tests/helpers/identity";
+import { ILockingMechanismModelManager } from "~/types";
+import { WebinyError } from "@webiny/error";
+
+describe("unlock entry request use case", () => {
+    it("should throw an error on missing lock record", async () => {
+        expect.assertions(1);
+        const useCase = new UnlockEntryRequestUseCase({
+            getLockRecordUseCase: {
+                execute: async () => {
+                    return null;
+                }
+            } as unknown as IGetLockRecordUseCase,
+            getIdentity: getSecurityIdentity,
+            getManager: async () => {
+                return {} as unknown as ILockingMechanismModelManager;
+            }
+        });
+
+        try {
+            await useCase.execute({ id: "id", type: "type" });
+        } catch (ex) {
+            expect(ex).toEqual(
+                new WebinyError("Entry is not locked.", "ENTRY_NOT_LOCKED", {
+                    id: "id",
+                    type: "type"
+                })
+            );
+        }
+    });
+
+    it("should throw an error if current user did not start the unlock request", async () => {
+        expect.assertions(1);
+        const useCase = new UnlockEntryRequestUseCase({
+            getLockRecordUseCase: {
+                execute: async () => {
+                    return {
+                        getUnlockRequested() {
+                            return {
+                                createdBy: {
+                                    id: "other-user-id"
+                                }
+                            };
+                        }
+                    };
+                }
+            } as unknown as IGetLockRecordUseCase,
+            getIdentity: getSecurityIdentity,
+            getManager: async () => {
+                return {} as unknown as ILockingMechanismModelManager;
+            }
+        });
+
+        try {
+            await useCase.execute({ id: "id", type: "type" });
+        } catch (ex) {
+            expect(ex).toEqual(
+                new WebinyError("Unlock request already sent.", "UNLOCK_REQUEST_ALREADY_SENT", {
+                    id: "id",
+                    type: "type",
+                    identity: {
+                        id: "other-user-id"
+                    }
+                })
+            );
+        }
+    });
+
+    it("should return the lock record if unlock request was already approved", async () => {
+        expect.assertions(1);
+        const useCase = new UnlockEntryRequestUseCase({
+            getLockRecordUseCase: {
+                execute: async (params: IGetLockRecordUseCaseExecuteParams) => {
+                    return {
+                        id: typeof params === "object" ? params.id : params,
+                        getUnlockRequested() {
+                            return {
+                                createdBy: getSecurityIdentity()
+                            };
+                        },
+                        getUnlockApproved() {
+                            return {};
+                        },
+                        getUnlockDenied() {
+                            return null;
+                        }
+                    };
+                }
+            } as unknown as IGetLockRecordUseCase,
+            getIdentity: getSecurityIdentity,
+            getManager: async () => {
+                return {} as unknown as ILockingMechanismModelManager;
+            }
+        });
+
+        const result = await useCase.execute({ id: "aTestIdValue#0001", type: "type" });
+        expect(result.id).toEqual("wby-lm-aTestIdValue");
+    });
+});
