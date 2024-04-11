9065e952782b83f0d337dd0805a6add6d31664ab
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-04-11 09:57:09 +0200

    fix: remove unnecessary condition

--- packages/api-headless-cms/src/graphql/schema/resolvers/manage/resolveRestoreFromBin.ts
+++ packages/api-headless-cms/src/graphql/schema/resolvers/manage/resolveRestoreFromBin.ts
@@ -10,13 +10,8 @@ type ResolveRestoreFromBin = ResolverFactory<any, ResolveRestoreFromBinArgs>;
 export const resolveRestoreFromBin: ResolveRestoreFromBin =
     ({ model }) =>
     async (_, args: any, context) => {
-        const { revision } = args;
         try {
-            if (!revision) {
-                throw new Error(`The input value "revision" is required!`);
-            }
-
-            const entry = await context.cms.restoreEntryFromBin(model, revision);
+            const entry = await context.cms.restoreEntryFromBin(model, args.revision);
             return new Response(entry);
         } catch (ex) {
             return new ErrorResponse(ex);
