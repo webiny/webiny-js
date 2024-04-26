9a20c32ce9ef4fbdc170a38ba7aa0e01d7974a17
adrians5j <adrian@webiny.com>
2024-04-26 22:28:54 +0200

    fix: specify all properties explicitly and set default values

--- packages/api-headless-cms/src/utils/createModelField.ts
+++ packages/api-headless-cms/src/utils/createModelField.ts
@@ -9,16 +9,44 @@ export interface CreateModelFieldParams
 }
 
 export const createModelField = (params: CreateModelFieldParams): CmsModelField => {
-    const { id, label, fieldId: initialFieldId, storageId, type } = params;
+    // We are not just spreading `params` because we want to ensure
+    // a default value is set for every property of the model field.
+    const {
+        id,
+        label,
+        fieldId: initialFieldId,
+        storageId,
+        type,
+        tags,
+        settings = {},
+        listValidation = [],
+        validation = [],
+        multipleValues = false,
+        predefinedValues = {
+            values: [],
+            enabled: false
+        },
+        helpText = null,
+        placeholderText = null,
+        renderer = null
+    } = params;
 
     const fieldId = initialFieldId ? camelCase(initialFieldId) : camelCase(label);
 
     return {
-        ...params,
         id: id ?? fieldId,
         storageId: storageId ?? `${type}@${fieldId}`,
         fieldId,
         label,
-        type
+        type,
+        settings,
+        tags,
+        listValidation,
+        validation,
+        multipleValues,
+        predefinedValues,
+        helpText,
+        placeholderText,
+        renderer
     };
 };
