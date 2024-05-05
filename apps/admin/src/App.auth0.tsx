e807266834bed0dc4229e133e9176b4fc43b2e62
adrians5j <adrian@webiny.com>
2024-05-05 22:42:49 +0200

    wip: create essential plugins scaffold

--- apps/admin/src/App.auth0.tsx
+++ apps/admin/src/App.auth0.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Admin } from "@webiny/app-serverless-cms";
 import { Auth0 } from "@webiny/app-admin-auth0";
+import { Scaffolds } from "./plugins/Scaffolds";
 import "./App.scss";
 
 export const App = () => {
@@ -13,6 +14,7 @@ export const App = () => {
                 }}
                 rootAppClientId={String(process.env.REACT_APP_AUTH0_CLIENT_ID)}
             />
+            <Scaffolds/>
         </Admin>
     );
 };
--- apps/admin/src/App.fm.tsx
+++ apps/admin/src/App.fm.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { Admin, createComponentPlugin } from "@webiny/app-serverless-cms";
 import { FileManagerFileItem, FileManagerRenderer, OverlayLayout } from "@webiny/app-admin";
 import { Cognito } from "@webiny/app-admin-users-cognito";
+import { Scaffolds } from "./plugins/Scaffolds";
 import "./App.scss";
 
 const CustomFileManager = createComponentPlugin(FileManagerRenderer, () => {
@@ -36,6 +37,7 @@ export const App = () => {
         <Admin>
             <Cognito />
             <CustomFileManager />
+            <Scaffolds/>
         </Admin>
     );
 };
