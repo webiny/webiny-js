2f4ad51f10d9c55b7956d9e5f2fbfc0b16ff1597
Pavel Denisjuk <pavel@webiny.com>
2024-05-13 19:50:50 +0200

    fix(app-file-manager): improve inline file editing UX [skip ci]

--- packages/app-file-manager/src/components/FileDetails/FileDetailsProvider.tsx
+++ packages/app-file-manager/src/components/FileDetails/FileDetailsProvider.tsx
@@ -1,8 +1,10 @@
 import React, { createContext } from "react";
 import { useFileManagerView } from "~/modules/FileManagerRenderer/FileManagerViewProvider";
+import { FileItem } from "@webiny/app-admin/types";
 
 export interface FileDetailsContext {
     close: () => void;
+    setFile: (file: FileItem) => void;
     own: boolean;
     scope?: string;
 }
@@ -11,12 +13,23 @@ export const FileDetailsContext = createContext<FileDetailsContext | undefined>(
 
 interface FileDetailsProviderProps {
     hideFileDetails: () => void;
+    onSetFile: (file: FileItem) => void;
     children: React.ReactNode;
 }
 
-export const FileDetailsProvider = ({ hideFileDetails, children }: FileDetailsProviderProps) => {
+export const FileDetailsProvider = ({
+    hideFileDetails,
+    onSetFile,
+    children
+}: FileDetailsProviderProps) => {
     const view = useFileManagerView();
-    const value: FileDetailsContext = { close: hideFileDetails, scope: view.scope, own: view.own };
+
+    const value: FileDetailsContext = {
+        close: hideFileDetails,
+        scope: view.scope,
+        own: view.own,
+        setFile: onSetFile
+    };
 
     return <FileDetailsContext.Provider value={value}>{children}</FileDetailsContext.Provider>;
 };
