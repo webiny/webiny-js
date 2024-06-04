9e02e5d107a9fca78d7fcd489b06fa2aafa0d50a
Bruno Zorić <bruno.zoric@gmail.com>
2024-06-04 15:50:54 +0200

    feat(handler-graphql): resolve

--- packages/api-headless-cms-import-export/src/graphql/resolvers.ts
+++ packages/api-headless-cms-import-export/src/graphql/resolvers.ts
@@ -1,6 +1,7 @@
 import { Context } from "~/types";
-import zod from "zod";
 import { createZodError } from "@webiny/utils";
+import { resolve } from "@webiny/handler-graphql";
+import zod from "zod";
 
 const validateAbortExportContentEntries = zod.object({
     id: zod.string()
@@ -14,22 +15,26 @@ export const createResolvers = (models: [string, ...string[]]) => {
     return {
         Mutation: {
             async startExportContentEntries(_: unknown, input: unknown, context: Context) {
-                const result = validateExportContentEntriesInput.safeParse(input);
+                return resolve(async () => {
+                    const result = validateExportContentEntriesInput.safeParse(input);
 
-                if (!result.success) {
-                    throw createZodError(result.error);
-                }
+                    if (!result.success) {
+                        throw createZodError(result.error);
+                    }
 
-                return await context.cmsImportExport.startExportContentEntries(result.data);
+                    return await context.cmsImportExport.startExportContentEntries(result.data);
+                });
             },
             async abortExportContentEntries(_: unknown, input: unknown, context: Context) {
-                const result = validateAbortExportContentEntries.safeParse(input);
+                return resolve(async () => {
+                    const result = validateAbortExportContentEntries.safeParse(input);
 
-                if (!result.success) {
-                    throw createZodError(result.error);
-                }
+                    if (!result.success) {
+                        throw createZodError(result.error);
+                    }
 
-                return await context.cmsImportExport.abortExportContentEntries(result.data);
+                    return await context.cmsImportExport.abortExportContentEntries(result.data);
+                });
             }
         }
     };
