ce7781a9eaf6378192a20fc0f32939823bcbc595
Pavel Denisjuk <pavel@webiny.com>
2024-06-11 15:43:40 +0200

    fix(api-security-cognito): apply admin user plugins to "admin" identity only

--- packages/api-security-cognito/src/createCognito.ts
+++ packages/api-security-cognito/src/createCognito.ts
@@ -49,6 +49,7 @@ export const createCognito = <
     });
 
     const { getIdentity, getPermissions } = config;
+    const isAdminIdentity = config.identityType === "admin";
 
     return [
         new ContextPlugin<TContext>(context => {
@@ -95,7 +96,6 @@ export const createCognito = <
             const teams = context.wcp.canUseTeams();
             context.plugins.register(adminUsersGqlPlugins({ teams }));
         }),
-        installGqlPlugins,
-        createAdminUsersHooks()
+        isAdminIdentity ? [installGqlPlugins, createAdminUsersHooks()] : []
     ];
 };
