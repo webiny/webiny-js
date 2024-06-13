26c54ad2e91a7061d30239eed49b61fa68307706
Pavel Denisjuk <pavel@webiny.com>
2024-06-13 20:51:24 +0200

    feat(api-headless-cms): make usePermission hook decoratable

--- packages/app-headless-cms/src/admin/hooks/index.ts
+++ packages/app-headless-cms/src/admin/hooks/index.ts
@@ -4,7 +4,7 @@ export * from "./useQueryLocale";
 export { default as useLazyQuery } from "./useLazyQuery";
 export { default as useMutation } from "./useMutation";
 export { default as useApolloClient } from "./useApolloClient";
-export { default as usePermission } from "./usePermission";
+export { usePermission } from "./usePermission";
 export { useModel } from "../components/ModelProvider";
 export { useModelEditor } from "../components/ContentModelEditor";
 export { useParentField, ParentFieldProvider } from "../components/ContentEntryForm/ParentValue";
