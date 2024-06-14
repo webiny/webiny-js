164b9cf048d2aa694d51eca83e2578ad023adcf2
Pavel Denisjuk <pavel@webiny.com>
2024-06-14 13:13:09 +0200

    fix(api-headless-cms): revert storageId validation logic

--- packages/api-headless-cms/src/crud/contentModel/validateStorageId.ts
+++ packages/api-headless-cms/src/crud/contentModel/validateStorageId.ts
@@ -1,30 +1,20 @@
 import lodashCamelCase from "lodash/camelCase";
 import WebinyError from "@webiny/error";
 
-// We allow "@" because that's we use it with the `createModelField` function.
+// We allow "@" because that's how we construct storageIds with `createModelField` utility.
 const VALID_STORAGE_ID_REGEX = /^([@a-zA-Z-0-9]+)$/;
 
 export const validateStorageId = (storageId: string) => {
     if (!storageId.match(VALID_STORAGE_ID_REGEX)) {
-        const message = [
-            `Invalid storageId provided ("${storageId}").`,
-            'Only alphanumeric characters and "@" are allowed.'
-        ].join(" ");
+        const camelCasedStorageId = lodashCamelCase(storageId);
 
-        throw new WebinyError(message, "STORAGE_ID_NOT_ALPHANUMERIC_ERROR");
-    }
+        if (storageId !== camelCasedStorageId) {
+            const message = [
+                `Invalid storageId provided ("${storageId}").`,
+                "Must be a camelCased string."
+            ].join(" ");
 
-    // We must do this because in the process of camel casing, Lodash removes the `@`
-    // character from the string. For example, if we received `text@productName`, we
-    // would be doing the `text@productName` vs. `textProductName` (camel cased) comparison.
-    storageId = storageId.replace("@", "");
-
-    if (storageId !== lodashCamelCase(storageId)) {
-        const message = [
-            `Invalid storageId provided ("${storageId}").`,
-            "Must be a camelCased string."
-        ].join(" ");
-
-        throw new WebinyError(message, "STORAGE_ID_NOT_CAMEL_CASED_ERROR");
+            throw new WebinyError(message, "STORAGE_ID_NOT_CAMEL_CASED_ERROR");
+        }
     }
 };
