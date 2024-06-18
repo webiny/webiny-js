2f58fe3e7b0f86aaee61f5931a3fa7c179db3a33
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-06-18 17:53:27 +0200

    chore: fix configs and types

--- packages/app-trash-bin/src/Presentation/TrashBin/controllers/UnselectAllItems/UnselectAllItemsController.ts
+++ packages/app-trash-bin/src/Presentation/TrashBin/controllers/UnselectAllItems/UnselectAllItemsController.ts
@@ -2,7 +2,7 @@ import { IUnselectAllItemsUseCase } from "~/UseCases";
 import { IUnselectAllItemsController } from "./IUnselectAllItemsController";
 
 export class UnselectAllItemsController implements IUnselectAllItemsController {
-    private readonly useCaseFactory: () => IUnelectAllItemsUseCase;
+    private readonly useCaseFactory: () => IUnselectAllItemsUseCase;
 
     constructor(useCaseFactory: () => IUnselectAllItemsUseCase) {
         this.useCaseFactory = useCaseFactory;
