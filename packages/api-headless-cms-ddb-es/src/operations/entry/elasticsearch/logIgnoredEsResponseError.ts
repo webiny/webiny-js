8679e4c6389f44074658b1959d20d8182406c482
adrians5j <adrian@webiny.com>
2024-06-27 14:52:42 +0200

    fix: add modelId and used index name to the log

---
+++ packages/api-headless-cms-ddb-es/src/operations/entry/elasticsearch/logIgnoredEsResponseError.ts
@@ -0,0 +1,25 @@
+import WebinyError from "@webiny/error";
+import { CmsModel } from "@webiny/api-headless-cms/types";
+
+interface LogIgnoredElasticsearchExceptionParams {
+    error: WebinyError;
+    model: CmsModel;
+    indexName: string;
+}
+
+export const logIgnoredElasticsearchException = (
+    params: LogIgnoredElasticsearchExceptionParams
+) => {
+    const { error, indexName, model } = params;
+
+    console.log(`Ignoring Elasticsearch response error: ${error.message}`, {
+        modelId: model.modelId,
+        usedIndexName: indexName,
+        error: {
+            message: error.message,
+            code: error.code,
+            data: error.data,
+            stack: error.stack
+        }
+    });
+};
