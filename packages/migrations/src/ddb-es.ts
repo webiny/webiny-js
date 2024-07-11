ca222801406ab1ebefa2baf0b5b3825828f8ec49
adrians5j <adrian@webiny.com>
2024-07-11 15:54:11 +0200

    wip: create data-migration

--- packages/migrations/src/ddb-es.ts
+++ packages/migrations/src/ddb-es.ts
@@ -1,3 +1,7 @@
+// We only list current version migrations here. No need to have them all
+// listed, as we only need the latest version migrations to be executed.
+// This also helps with keeping the bundle size down / faster boot times.
+
 export const migrations = () => {
     return [];
 };
--- packages/migrations/src/ddb.ts
+++ packages/migrations/src/ddb.ts
@@ -1,3 +1,6 @@
+// We only list current version migrations here. No need to have them all
+// listed, as we only need the latest version migrations to be executed.
+// This also helps with keeping the bundle size down / faster boot times.
 import { AdminUsers_5_41_0_001 } from "~/migrations/5.41.0/001";
 
 export const migrations = () => {
