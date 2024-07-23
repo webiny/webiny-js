9cc65281c96d048ce19b71655198bc4ab5e97fb1
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-07-23 16:49:07 +0200

    feat: add admin package

--- apps/admin/webiny.config.ts
+++ apps/admin/webiny.config.ts
@@ -1,3 +1,30 @@
 import { createAdminAppConfig } from "@webiny/serverless-cms-aws";
+// @ts-ignore `traverseLoaders` has no type declarations.
+import { traverseLoaders } from "@webiny/project-utils/traverseLoaders";
+import tailwindcss from "tailwindcss";
 
-export default createAdminAppConfig();
+export default createAdminAppConfig(({ config }) => {
+    /**
+     * Add a webpack config modifier.
+     */
+    config.webpack(config => {
+        /**
+         * Traverse all loaders, find `postcss-loader`, and overwrite plugins.
+         */
+        traverseLoaders(config.module?.rules, (loader: any) => {
+            /**
+             * `loader` can also be a string, so check for `.loader` property.
+             */
+            if (loader.loader && loader.loader.includes("postcss-loader")) {
+                loader.options.postcssOptions.plugins = [
+                    ...loader.options.postcssOptions.plugins(),
+                    tailwindcss({
+                        config: __dirname + "/tailwind.config.js"
+                    })
+                ];
+            }
+        });
+
+        return config;
+    });
+});
---
+++ packages/admin/README.md
@@ -0,0 +1,15 @@
+# @webiny/admin
+[![](https://img.shields.io/npm/dw/@webiny/admin.svg)](https://www.npmjs.com/package/@webiny/admin) 
+[![](https://img.shields.io/npm/v/@webiny/admin.svg)](https://www.npmjs.com/package/@webiny/admin)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+## Install
+```
+npm install --save @webiny/admin
+```
+
+Or if you prefer yarn: 
+```
+yarn add @webiny/admin
+```
