78ddaa9a99969f1c3c2766c6af3cee629c482d75
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-07-24 11:45:46 +0200

    feat: add storybook

---
+++ packages/admin/.storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import { join, dirname } from "path";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-interactions"),
+        getAbsolutePath("@storybook/addon-styling-webpack")
+    ],
+    framework: {
+        name: getAbsolutePath("@storybook/react-webpack5"),
+        options: {}
+    },
+    core: { disableTelemetry: true },
+    webpackFinal: async (config, { configType }) => {
+        config.resolve.plugins = [new TsconfigPathsPlugin()];
+
+        return config;
+    }
+};
+export default config;
