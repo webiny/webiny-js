87ba470c277b4a201586a803609044f462e3cdc3
Bruno Zorić <bruno.zoric@gmail.com>
2024-08-01 12:08:14 +0200

    wip(app-headless-cms): a todo for entry form

--- packages/app-headless-cms/src/admin/views/contentEntries/ContentEntries.tsx
+++ packages/app-headless-cms/src/admin/views/contentEntries/ContentEntries.tsx
@@ -8,7 +8,9 @@ import {
     ContentEntryListWithConfig
 } from "~/admin/config/contentEntries";
 import { ContentEntriesProvider } from "~/admin/views/contentEntries/ContentEntriesContext";
-import { CMS_MODEL_SINGLETON_TAG } from "~/admin/constants";
+import { CMS_MODEL_SINGLETON_TAG, ROOT_FOLDER } from "~/admin/constants";
+import { ContentEntryProvider } from "./ContentEntry/ContentEntryContext";
+import { ContentEntryForm } from "~/admin/components/ContentEntryForm/ContentEntryForm";
 
 export const ContentEntries = makeDecoratable("ContentEntries", () => {
     const { model } = useModel();
