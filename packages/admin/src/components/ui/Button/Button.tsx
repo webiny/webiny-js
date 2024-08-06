a2fa8366b53ba978a1597638adf7076da4550b8d
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-08-06 16:58:19 +0200

    chore: fix button sm

--- packages/admin/src/components/ui/Button/Button.tsx
+++ packages/admin/src/components/ui/Button/Button.tsx
@@ -11,13 +11,13 @@ const buttonVariants = cva(
             variant: {
                 primary: "bg-primary text-primary-foreground hover:bg-primary/90",
                 secondary:
-                    "bg-gray-200 text-gray-900 fill-gray-900 border-gray-200 hover:bg-gray-300 hover:border-gray-300 hover:text-gray-800 border",
+                    "border border-gray-200 bg-gray-200 fill-gray-900 text-gray-900 hover:border-gray-300 hover:bg-gray-300 hover:text-gray-800",
                 outline:
-                    "bg-white text-gray-900 fill-gray-900 border-gray-400 hover:bg-gray-100 hover:text-gray-900 border",
-                ghost: "bg-transparent border-transparent text-gray-900 fill-gray-900 hover:bg-gray-200 border"
+                    "border border-gray-400 bg-white fill-gray-900 text-gray-900 hover:bg-gray-100 hover:text-gray-900",
+                ghost: "border border-transparent bg-transparent fill-gray-900 text-gray-900 hover:bg-gray-200"
             },
             size: {
-                sm: "rounded px-2 py-1 text-sm font-normal",
+                sm: "rounded px-2 py-1 text-xs font-normal",
                 md: "rounded px-3 py-1.5 text-sm font-normal",
                 lg: "rounded-lg px-4 py-2.5 text-base font-medium",
                 xl: "rounded-lg px-5 py-3.5 text-lg font-medium"
