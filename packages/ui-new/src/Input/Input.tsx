fb09a61e91759951204f9b601bb7bd81a5d174d8
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-08-13 12:38:05 +0200

    wip: multiple chores

--- packages/ui-new/src/Input/Input.tsx
+++ packages/ui-new/src/Input/Input.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "~/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, type, ...props }, ref) => {
