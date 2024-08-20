31f95d2b5e82d74b378480bcf6b7bf3320857949
Pavel Denisjuk <pavel@webiny.com>
2024-08-20 13:52:46 +0200

    fix(app-file-manager): revert to using nested forms for extensions

--- packages/app-file-manager/src/components/FileDetails/components/Extensions.tsx
+++ packages/app-file-manager/src/components/FileDetails/components/Extensions.tsx
@@ -4,7 +4,7 @@ import { CompositionScope } from "@webiny/app-admin";
 import { CmsModel } from "@webiny/app-headless-cms/types";
 import { ModelProvider } from "@webiny/app-headless-cms/admin/components/ModelProvider";
 import { Fields } from "@webiny/app-headless-cms/admin/components/ContentEntryForm/Fields";
-import { Bind, BindComponentProps } from "@webiny/form";
+import { Bind, Form, useBind } from "@webiny/form";
 
 const HideEmptyCells = styled.div`
     .mdc-layout-grid__cell:empty {
@@ -16,13 +16,6 @@ interface ExtensionsProps {
     model: CmsModel;
 }
 
-function BindWithPrefix(props: BindComponentProps) {
-    return (
-        <Bind {...props} name={`extensions.${props.name}`}>
-            {props.children}
-        </Bind>
-    );
-}
 export const Extensions = ({ model }: ExtensionsProps) => {
     const extensionsField = useMemo(() => {
         return model.fields.find(f => f.fieldId === "extensions");
@@ -39,18 +32,26 @@ export const Extensions = ({ model }: ExtensionsProps) => {
         layout.push(...fields.map(field => [field.fieldId]));
     }
 
+    const { value, onChange } = useBind({
+        name: "extensions"
+    });
+
     return (
         <CompositionScope name={"fm.fileDetails.extensionFields"}>
             <ModelProvider model={model}>
-                <HideEmptyCells>
-                    <Fields
-                        contentModel={model}
-                        // @ts-expect-error
-                        Bind={BindWithPrefix}
-                        fields={fields}
-                        layout={layout}
-                    />
-                </HideEmptyCells>
+                <Form data={value} onChange={onChange}>
+                    {() => (
+                        <HideEmptyCells>
+                            <Fields
+                                contentModel={model}
+                                // @ts-expect-error
+                                Bind={Bind}
+                                fields={fields}
+                                layout={layout}
+                            />
+                        </HideEmptyCells>
+                    )}
+                </Form>
             </ModelProvider>
         </CompositionScope>
     );
