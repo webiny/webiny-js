dd0428683debc51619c23134336b7a1e20c78faf
Bruno Zorić <bruno.zoric@gmail.com>
2024-08-22 14:31:11 +0200

    fix(api-headless-cms-tasks): add import and export tasks

--- packages/api-headless-cms-tasks/src/index.ts
+++ packages/api-headless-cms-tasks/src/index.ts
@@ -1,3 +1,4 @@
 import { createHcmsBulkActions } from "@webiny/api-headless-cms-bulk-actions";
+import { createHeadlessCmsImportExport } from "@webiny/api-headless-cms-import-export";
 
-export const createHcmsTasks = () => [createHcmsBulkActions()];
+export const createHcmsTasks = () => [createHcmsBulkActions(), createHeadlessCmsImportExport()];
