01ed10a4269bbb9462e1bb4ab186cb3bc895e505
adrians5j <adrian@webiny.com>
2024-08-28 13:33:20 +0200

    wip: create button component

--- packages/ui/src/Button/ButtonFloating.tsx
+++ packages/ui/src/Button/ButtonFloating.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { IconButton, IconButtonProps } from "@webiny/admin-ui/Button";
+import { IconButton, IconButtonProps } from "./IconButton";
 
+/**
+ * @deprecated Will be removed in the future release.
+ */
 export const ButtonFloating = (props: IconButtonProps) => {
     return <IconButton {...props} variant={"primary"} />;
 };
