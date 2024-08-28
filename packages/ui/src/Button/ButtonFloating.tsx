7ccb7307767862b9408750ea3a43d50bec5fc171
adrians5j <adrian@webiny.com>
2024-08-28 11:04:52 +0200

    wip: create button component

---
+++ packages/ui/src/Button/ButtonFloating.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { IconButton, IconButtonProps } from "@webiny/admin-ui/Button";
+
+export const ButtonFloating = (props: IconButtonProps) => {
+    return <IconButton {...props} variant={"primary"} />;
+};
--- packages/ui/src/Button/index.ts
+++ packages/ui/src/Button/index.ts
@@ -2,6 +2,7 @@ export * from "./Button";
 export * from "./ButtonPrimary";
 export * from "./ButtonSecondary";
 export * from "./ButtonDefault";
+export * from "./ButtonFloating";
 export * from "./ButtonIcon";
 export * from "./CopyButton";
 export * from "./IconButton";
