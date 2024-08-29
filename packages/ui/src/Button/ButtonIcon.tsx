02fb77df80ff8f525a5ccf1b0c94d62ab22be776
adrians5j <adrian@webiny.com>
2024-08-29 17:54:49 +0200

    fix: add deprecation notice

--- packages/ui/src/Button/ButtonIcon.tsx
+++ packages/ui/src/Button/ButtonIcon.tsx
@@ -5,6 +5,10 @@ export interface ButtonIconProps {
     icon: React.ReactElement;
 }
 
+/**
+ * @deprecated This component is deprecated and will be removed in future releases.
+ * Please use the `Icon` component from the `@webiny/admin-ui` package instead.
+ */
 export const ButtonIcon = (props: ButtonIconProps) => {
     return <Icon {...props}/>;
 };
