5b6b6caf2d044f3a2f384da870743b1dce88768a
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-08-29 10:06:57 +0200

    feat: add Label component

--- packages/admin-ui/src/Label/Label.tsx
+++ packages/admin-ui/src/Label/Label.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
+import { makeDecoratable } from "@webiny/react-composition";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "~/utils";
@@ -10,12 +9,21 @@ const labelVariants = cva(
     "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
-const Label = React.forwardRef<
-    React.ElementRef<typeof LabelPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-    <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-));
-Label.displayName = LabelPrimitive.Root.displayName;
+interface LabelProps
+    extends Omit<React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>, "children">,
+        VariantProps<typeof labelVariants> {
+    text: React.ReactNode;
+}
 
-export { Label };
+const LabelBase = React.forwardRef<React.ElementRef<typeof LabelPrimitive.Root>, LabelProps>(
+    ({ className, text, ...props }, ref) => (
+        <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props}>
+            {text}
+        </LabelPrimitive.Root>
+    )
+);
+LabelBase.displayName = LabelPrimitive.Root.displayName;
+
+const Label = makeDecoratable("Label", LabelBase);
+
+export { Label, LabelProps };
