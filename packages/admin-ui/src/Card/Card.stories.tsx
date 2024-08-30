f847bbc31e996babd22b20d2bc99baed12a44cb7
adrians5j <adrian@webiny.com>
2024-08-30 13:11:02 +0200

    wip: create Card component

---
+++ packages/admin-ui/src/Card/Card.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Card, CardContent, CardFooter, CardTitle, CardDescription } from "./Card";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Card> = {
+    title: "Components/Card",
+    component: Card,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ["autodocs"],
+    args: {
+        headerTitle: <CardTitle>This is a card title</CardTitle>,
+        headerDescription: <CardDescription>This is a card description</CardDescription>,
+        content: <CardContent>This is card content. Anything can go in here.</CardContent>,
+        footer: <CardFooter>This is a card footer.</CardFooter>,
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const WithoutHeaderAndFooter: Story = {
+    args: {
+        content: <CardContent>This is card content. Anything can go in here.</CardContent>,
+    }
+};
--- packages/admin-ui/src/Card/Card.tsx
+++ packages/admin-ui/src/Card/Card.tsx
@@ -1,25 +1,31 @@
 import * as React from "react";
 import { cn } from "~/utils";
+import {makeDecoratable} from "@webiny/react-composition";
 
 type CardRootProps = React.HTMLAttributes<HTMLDivElement>;
 
-const CardRoot = React.forwardRef<HTMLDivElement, CardRootProps>(({ className, ...props }, ref) => (
+const CardRootBase = React.forwardRef<HTMLDivElement, CardRootProps>(({ className, ...props }, ref) => (
     <div
         ref={ref}
         className={cn(" rounded-lg border bg-card text-card-foreground shadow-sm", className)}
         {...props}
     />
 ));
-CardRoot.displayName = "CardRoot";
 
-const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+CardRootBase.displayName = "CardRoot";
+
+const CardRoot = makeDecoratable("CardRoot", CardRootBase);
+
+const CardHeaderBase = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
     ({ className, ...props }, ref) => (
         <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
     )
 );
-CardHeader.displayName = "CardHeader";
+CardHeaderBase.displayName = "CardHeader";
 
-const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+const CardHeader = makeDecoratable("CardHeader", CardHeaderBase);
+
+const CardTitleBase = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
     ({ className, ...props }, ref) => (
         <h3
             ref={ref}
@@ -28,47 +34,55 @@ const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HT
         />
     )
 );
-CardTitle.displayName = "CardTitle";
+CardTitleBase.displayName = "CardTitle";
 
-const CardDescription = React.forwardRef<
+const CardTitle = makeDecoratable("CardTitle", CardTitleBase);
+
+const CardDescriptionBase = React.forwardRef<
     HTMLParagraphElement,
     React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
     <p ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
 ));
-CardDescription.displayName = "CardDescription";
+CardDescriptionBase.displayName = "CardDescription";
 
-const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+const CardDescription = makeDecoratable("CardDescription", CardDescriptionBase);
+
+const CardContentBase = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
     ({ className, ...props }, ref) => (
         <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
     )
 );
-CardContent.displayName = "CardContent";
+CardContentBase.displayName = "CardContent";
 
-const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+const CardContent = makeDecoratable("CardContent", CardContentBase);
+
+const CardFooterBase = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
     ({ className, ...props }, ref) => (
         <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
     )
 );
-CardFooter.displayName = "CardFooter";
+CardFooterBase.displayName = "CardFooter";
+
+const CardFooter = makeDecoratable("CardFooter", CardFooterBase);
 
 interface CardProps extends Omit<CardRootProps, "content"> {
-    headerTitle?: React.ReactElement<typeof CardTitle>;
-    headerDescription?: React.ReactElement<typeof CardDescription>;
-    content?: React.ReactElement<typeof CardContent>;
-    footer?: React.ReactElement<typeof CardFooter>;
+    headerTitle?: React.ReactElement<typeof CardTitleBase>;
+    headerDescription?: React.ReactElement<typeof CardDescriptionBase>;
+    content?: React.ReactElement<typeof CardContentBase>;
+    footer?: React.ReactElement<typeof CardFooterBase>;
 }
 
-const Card = (props: CardProps) => {
+const CardBase = (props: CardProps) => {
     const { headerTitle, headerDescription, content, footer, ...rest } = props;
 
     let header: React.ReactNode = null;
     if (headerTitle || headerDescription) {
         header = (
-            <CardHeader>
+            <CardHeaderBase>
                 {headerTitle}
                 {headerDescription}
-            </CardHeader>
+            </CardHeaderBase>
         );
     }
 
@@ -81,4 +95,6 @@ const Card = (props: CardProps) => {
     );
 };
 
-export { Card, CardHeader, CardContent, CardFooter, CardProps };
+const Card = makeDecoratable("Card", CardBase);
+
+export { Card, CardTitle, CardDescription, CardContent, CardFooter, CardProps };
