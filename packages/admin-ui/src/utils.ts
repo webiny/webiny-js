123da4bb6bae2e052c6c48bf40deee4cc7a73dca
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-09-02 11:58:16 +0200

    refactor: use generateId utility

--- packages/admin-ui/src/utils.ts
+++ packages/admin-ui/src/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { generateId as baseGenerateId } from "@webiny/utils/generateId";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,5 +10,5 @@ export const generateId = (initialId?: string): string => {
     if (initialId !== undefined && initialId !== null) {
         return String(initialId);
     }
-    return "wby-" + Math.random().toString(36).slice(2, 9);
+    return "wby-" + baseGenerateId(4);
 };
