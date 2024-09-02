3b4d2a5b2ffb8a0caf359cf8966f037e53f077f2
Adrian Smijulj <adrian1358@gmail.com>
2024-09-02 14:34:09 +0200

    feat: create Button component (#4233)

--- packages/admin-ui/src/Button/Button.tsx
+++ packages/admin-ui/src/Button/Button.tsx
@@ -1,14 +1,16 @@
-import * as React from "react";
+import React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/utils";
+import { makeDecoratable } from "@webiny/react-composition";
 
 const buttonVariants = cva(
     "font-sans inline-flex items-center justify-center whitespace-nowrap leading-tight ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     {
         variants: {
             variant: {
-                primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+                primary:
+                    "bg-primary text-primary-foreground hover:bg-primary/90 [&>svg]:fill-white",
                 secondary:
                     "bg-gray-200 text-gray-900 fill-gray-900 border border-gray-200 hover:bg-gray-300 hover:border-gray-300 hover:text-gray-800",
                 outline:
@@ -29,24 +31,33 @@ const buttonVariants = cva(
     }
 );
 
-export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+interface ButtonProps
+    extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "children">,
         VariantProps<typeof buttonVariants> {
+    text?: React.ReactNode;
+
+    icon?: React.ReactNode;
+
+    iconPosition?: "start" | "end";
+
     asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
-        const Comp = asChild ? Slot : "button";
-        return (
-            <Comp
-                className={cn(buttonVariants({ variant, size, className }))}
-                ref={ref}
-                {...props}
-            />
-        );
-    }
-);
-Button.displayName = "Button";
+const ButtonBase = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+    const { className, variant, size, asChild = false, text, icon, iconPosition, ...rest } = props;
+    const Comp = asChild ? Slot : "button";
 
-export { Button, buttonVariants };
+    return (
+        <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...rest}>
+            {iconPosition !== "end" && icon}
+            {text}
+            {iconPosition === "end" && icon}
+        </Comp>
+    );
+});
+
+ButtonBase.displayName = "Button";
+
+const Button = makeDecoratable("Button", ButtonBase);
+
+export { Button, type ButtonProps };
