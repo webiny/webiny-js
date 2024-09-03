0b18d65bd88329e718f2d4b0eeecba6b75e8a432
Leonardo Giacone <leonardo@webiny.com>
2024-09-03 11:50:50 +0200

    feat(admin-ui): Label and Switch components (#4243)
    Co-authored-by: Adrian Smijulj <adrian1358@gmail.com>

---
+++ packages/admin-ui/src/Label/Label.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { makeDecoratable } from "@webiny/react-composition";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "~/utils";
+
+const labelVariants = cva(
+    "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+);
+
+interface LabelProps
+    extends Omit<React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>, "children">,
+        VariantProps<typeof labelVariants> {
+    text: React.ReactNode;
+}
+
+const LabelBase = React.forwardRef<React.ElementRef<typeof LabelPrimitive.Root>, LabelProps>(
+    ({ className, text, ...props }, ref) => (
+        <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props}>
+            {text}
+        </LabelPrimitive.Root>
+    )
+);
+LabelBase.displayName = LabelPrimitive.Root.displayName;
+
+const Label = makeDecoratable("Label", LabelBase);
+
+export { Label, type LabelProps };
---
+++ packages/admin-ui/src/Switch/Switch.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Switch } from "./Switch";
+
+const meta: Meta<typeof Switch> = {
+    title: "Components/Switch",
+    component: Switch,
+    tags: ["autodocs"],
+    args: { onCheckedChange: fn() }
+};
+
+export default meta;
+type Story = StoryObj<typeof Switch>;
+
+export const Default: Story = {};
+
+export const WithLabel: Story = {
+    args: {
+        label: "Label"
+    }
+};
+
+export const WithLeadingLabel: Story = {
+    args: {
+        label: "Leading label",
+        labelPosition: "start"
+    }
+};
+
+export const WithTrailingLabel: Story = {
+    args: {
+        label: "Trailing label",
+        labelPosition: "end"
+    }
+};
