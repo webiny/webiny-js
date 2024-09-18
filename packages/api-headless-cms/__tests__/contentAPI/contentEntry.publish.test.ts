86d5a500a66616cc61d75cbac827e80fbe933591
adrians5j <adrian@webiny.com>
2024-09-18 13:13:27 +0200

    chore: run prettier

--- packages/api-headless-cms/__tests__/contentAPI/contentEntry.publish.test.ts
+++ packages/api-headless-cms/__tests__/contentAPI/contentEntry.publish.test.ts
@@ -1,8 +1,8 @@
-import { useTestModelHandler } from "~tests/testHelpers/useTestModelHandler";
-import { identityA } from "./security/utils";
+import {useTestModelHandler} from "~tests/testHelpers/useTestModelHandler";
+import {identityA} from "./security/utils";
 
 describe("Content entries - Entry Deletion", () => {
-    const { manage, read } = useTestModelHandler({
+    const {manage, read} = useTestModelHandler({
         identity: identityA
     });
 
@@ -11,15 +11,15 @@ describe("Content entries - Entry Deletion", () => {
     });
 
     test("should be able to publish a previously published revision", async () => {
-        const { data: revision1 } = await manage.createTestEntry({ data: { title: "Revision 1" } });
+        const {data: revision1} = await manage.createTestEntry({data: {title: "Revision 1"}});
 
         await manage.publishTestEntry({
             revision: revision1.id
         });
 
-        const { data: revision2 } = await manage.createTestEntryFrom({
+        const {data: revision2} = await manage.createTestEntryFrom({
             revision: revision1.id,
-            data: { title: "Revision 2" }
+            data: {title: "Revision 2"}
         });
 
         await manage.publishTestEntry({
@@ -31,16 +31,16 @@ describe("Content entries - Entry Deletion", () => {
             revision: revision1.id
         });
 
-        const { data: manageEntriesList } = await manage.listTestEntries();
-        const { data: readEntriesList } = await read.listTestEntries();
+        const {data: manageEntriesList} = await manage.listTestEntries();
+        const {data: readEntriesList} = await read.listTestEntries();
 
         expect(manageEntriesList).toHaveLength(1);
         expect(manageEntriesList).toMatchObject([
-            { id: revision2.id, title: "Revision 2", meta: { status: "unpublished" } }
+            {id: revision2.id, title: "Revision 2", meta: {status: "unpublished"}}
         ]);
 
         expect(readEntriesList).toHaveLength(1);
-        expect(readEntriesList).toMatchObject([{ id: revision1.id, title: "Revision 1" }]);
+        expect(readEntriesList).toMatchObject([{id: revision1.id, title: "Revision 1"}]);
 
     });
 });
