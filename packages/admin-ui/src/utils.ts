e675a7af3cd90fcb292ec925925cf766884e2ace
adrians5j <adrian@webiny.com>
2024-10-02 09:30:52 +0200

    wip: remove avatar-related changes

--- packages/admin-ui/src/utils.ts
+++ packages/admin-ui/src/utils.ts
@@ -1,4 +1,3 @@
-import React from "react";
 import { clsx, type ClassValue } from "clsx";
 import { generateId as baseGenerateId } from "@webiny/utils/generateId";
 import { twMerge } from "tailwind-merge";
@@ -12,11 +11,4 @@ export const generateId = (initialId?: string) => {
         return initialId;
     }
     return "wby-" + baseGenerateId(4);
-};
-
-export const withStaticProps = <TComponent extends React.ComponentType<any>, TProps>(
-    component: TComponent,
-    props: TProps
-) => {
-    return Object.assign(component, props) as TComponent & TProps;
 };
