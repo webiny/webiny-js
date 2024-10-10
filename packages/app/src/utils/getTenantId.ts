ceec6eb487228add56ed70c66452ba852e9b0306
Bruno Zorić <bruno.zoric@gmail.com>
2024-10-10 10:33:15 +0200

    fix(app): remove console logs

--- packages/app/src/utils/getTenantId.ts
+++ packages/app/src/utils/getTenantId.ts
@@ -10,14 +10,12 @@ export const getTenantId = (): string | null => {
     // 1. Get tenant via the `__tenant` query param. Useful when doing page previews.
     let tenant = new URLSearchParams(location.search).get("__tenant");
     if (tenant) {
-        console.log("taking from query param");
         return tenant;
     }
 
     // 2. Get tenant via `window.__PS_RENDER_TENANT__`. Used with prerendered pages.
     tenant = window.__PS_RENDER_TENANT__;
     if (tenant) {
-        console.log("__PS_RENDER_TENANT__");
         return tenant;
     }
 
