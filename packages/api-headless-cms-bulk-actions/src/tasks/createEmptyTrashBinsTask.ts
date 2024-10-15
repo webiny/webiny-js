78742c9ea158a5ce32264e146d285c44d0325153
Bruno Zorić <bruno.zoric@gmail.com>
2024-10-15 10:13:06 +0200

    wip(api-headless-cms-bulk-actions): controller task checks for child task runs

--- packages/api-headless-cms-bulk-actions/src/tasks/createEmptyTrashBinsTask.ts
+++ packages/api-headless-cms-bulk-actions/src/tasks/createEmptyTrashBinsTask.ts
@@ -1,4 +1,4 @@
-import { createTaskDefinition } from "@webiny/tasks";
+import { createTaskDefinition, TaskDataStatus } from "@webiny/tasks";
 import { HcmsBulkActionsContext, IBulkActionOperationByModelInput } from "~/types";
 import { ChildTasksCleanup } from "~/useCases/internals";
 
@@ -18,24 +18,87 @@ const calculateDateTimeString = () => {
     // Return the calculated date-time string in ISO 8601 format.
     return currentDate.toISOString();
 };
+// TODO fix
+const cleanup = async ({ context, task }: any) => {
+    /**
+     * We want to clean all child tasks and logs, which have no errors.
+     */
+    const childTasksCleanup = new ChildTasksCleanup();
+    try {
+        await childTasksCleanup.execute({
+            context,
+            task
+        });
+    } catch (ex) {
+        console.error("Error while cleaning `EmptyTrashBins` child tasks.", ex);
+    }
+};
 
 export const createEmptyTrashBinsTask = () => {
     return createTaskDefinition<HcmsBulkActionsContext>({
-        isPrivate: true,
+        // TODO put to true when done with testing
+        isPrivate: false,
         id: "hcmsEntriesEmptyTrashBins",
         title: "Headless CMS - Empty all trash bins",
         description:
             "Delete all entries found in the trash bin, for each model found in the system.",
-        maxIterations: 1,
+        maxIterations: 24,
         disableDatabaseLogs: true,
         run: async params => {
-            const { response, isAborted, context } = params;
+            const { response, isAborted, isCloseToTimeout, context, trigger, input, store } =
+                params;
+            if (isAborted()) {
+                return response.aborted();
+            } else if (isCloseToTimeout()) {
+                return response.continue(
+                    {
+                        ...input
+                    },
+                    {
+                        seconds: 30
+                    }
+                );
+            }
 
-            try {
-                if (isAborted()) {
-                    return response.aborted();
+            if (input.triggered) {
+                const { items } = await context.tasks.listTasks({
+                    where: {
+                        parentId: store.getTask().id
+                    },
+                    limit: 100000
+                });
+                for (const item of items) {
+                    if (
+                        item.taskStatus === TaskDataStatus.RUNNING ||
+                        item.taskStatus === TaskDataStatus.PENDING
+                    ) {
+                        const status = await context.tasks.fetchServiceInfo(item.id);
+                        if (status?.status === "FAILED" || status?.status === "TIMED_OUT") {
+                            await context.tasks.updateTask(item.id, {
+                                taskStatus: TaskDataStatus.FAILED
+                            });
+                            continue;
+                        } else if (status?.status === "ABORTED") {
+                            await context.tasks.updateTask(item.id, {
+                                taskStatus: TaskDataStatus.ABORTED
+                            });
+                            continue;
+                        }
+                        return response.continue(
+                            {
+                                ...input
+                            },
+                            {
+                                seconds: 3600
+                            }
+                        );
+                    }
                 }
 
+                return response.done();
+            }
+
+            try {
                 const locales = context.i18n.getLocales();
 
                 await context.i18n.withEachLocale(locales, async () => {
@@ -44,7 +107,7 @@ export const createEmptyTrashBinsTask = () => {
                     });
 
                     for (const model of models) {
-                        await context.tasks.trigger<IBulkActionOperationByModelInput>({
+                        await trigger<IBulkActionOperationByModelInput>({
                             name: `Headless CMS - Empty trash bin for "${model.name}" model.`,
                             definition: "hcmsBulkListDeleteEntries",
                             input: {
@@ -55,29 +118,23 @@ export const createEmptyTrashBinsTask = () => {
                             }
                         });
                     }
-                    return;
                 });
 
-                return response.done(
-                    `Task done: emptying the trash bin for all registered models.`
+                return response.continue(
+                    {
+                        triggered: true
+                    },
+                    {
+                        seconds: 60
+                    }
                 );
             } catch (ex) {
                 return response.error(ex.message ?? "Error while executing EmptyTrashBins task");
             }
         },
-        onDone: async ({ context, task }) => {
-            /**
-             * We want to clean all child tasks and logs, which have no errors.
-             */
-            const childTasksCleanup = new ChildTasksCleanup();
-            try {
-                await childTasksCleanup.execute({
-                    context,
-                    task
-                });
-            } catch (ex) {
-                console.error("Error while cleaning `EmptyTrashBins` child tasks.", ex);
-            }
-        }
+        onMaxIterations: cleanup,
+        onDone: cleanup,
+        onError: cleanup,
+        onAbort: cleanup
     });
 };
