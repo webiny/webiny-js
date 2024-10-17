f155108c4e3294c558c2563e0327ec12c3990ff0
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-10-17 13:48:40 +0200

    fix: move folderLevelPermissions into new hook

--- packages/app-aco/src/contexts/folders.tsx
+++ packages/app-aco/src/contexts/folders.tsx
@@ -1,9 +1,5 @@
 import React, { ReactNode, useContext, useMemo } from "react";
-import { useWcp } from "@webiny/app-wcp/hooks/useWcp";
-import { useStateIfMounted } from "@webiny/app-admin";
-import { FolderItem } from "~/types";
 import { AcoAppContext } from "~/contexts/app";
-import { ROOT_FOLDER } from "~/constants";
 
 export interface FoldersContextFolderLevelPermissions {
     canManageStructure(folderId: string): boolean;
@@ -15,7 +11,6 @@ export interface FoldersContextFolderLevelPermissions {
 
 interface FoldersContext {
     type?: string | null;
-    folderLevelPermissions: FoldersContextFolderLevelPermissions;
 }
 
 export const FoldersContext = React.createContext<FoldersContext | undefined>(undefined);
@@ -27,43 +22,20 @@ interface Props {
 
 export const FoldersProvider = ({ children, ...props }: Props) => {
     const appContext = useContext(AcoAppContext);
-    const [folders, setFolders] = useStateIfMounted<FolderItem[] | null>(null);
-    const { canUseFolderLevelPermissions } = useWcp();
 
     const app = appContext ? appContext.app : undefined;
 
     const type = props.type ?? app?.id;
+
     if (!type) {
         throw Error(`FoldersProvider requires a "type" prop or an AcoAppContext to be available!`);
     }
 
-    const folderLevelPermissions: FoldersContextFolderLevelPermissions = useMemo(() => {
-        const createCanManage =
-            (callback: (folder: FolderItem) => boolean) => (folderId: string) => {
-                if (!canUseFolderLevelPermissions() || folderId === ROOT_FOLDER) {
-                    return true;
-                }
-
-                const folder = folders?.find(folder => folder.id === folderId);
-                if (!folder) {
-                    return false;
-                }
-
-                return callback(folder);
-            };
-
-        return {
-            canManageStructure: createCanManage(folder => folder.canManageStructure),
-            canManagePermissions: createCanManage(folder => folder.canManagePermissions),
-            canManageContent: createCanManage(folder => folder.canManageContent)
-        };
-    }, [folders]);
-
     const context = useMemo<FoldersContext>(() => {
         return {
-            folderLevelPermissions
+            type
         };
-    }, [folders, setFolders]);
+    }, [type]);
 
     return <FoldersContext.Provider value={context}>{children}</FoldersContext.Provider>;
 };
--- packages/app-aco/src/hooks/useFolders.ts
+++ packages/app-aco/src/hooks/useFolders.ts
@@ -1,19 +1,24 @@
 import { useContext, useEffect, useMemo, useState } from "react";
 import { autorun } from "mobx";
 import { loadingRepositoryFactory } from "@webiny/app-utils";
-import { FoldersContext } from "~/contexts/folders";
-import { AcoAppContext } from "~/contexts/app";
-import { folderCacheFactory } from "~/features/folder/cache";
-import { useCreateFolder } from "~/features/folder/createFolder/useCreateFolder";
-import { useDeleteFolder } from "~/features/folder/deleteFolder/useDeleteFolder";
-import { useGetDescendantFolders } from "~/features/folder/getDescendantFolders/useGetDescendantFolders";
-import { useGetFolder } from "~/features/folder/getFolder/useGetFolder";
-import { useListFolders } from "~/features/folder/listFolders/useListFolders";
-import { useUpdateFolder } from "~/features/folder/updateFolder/useUpdateFolder";
+import { useWcp } from "@webiny/app-wcp";
+import { FoldersContext, FoldersContextFolderLevelPermissions } from "~/contexts/folders";
+import {
+    folderCacheFactory,
+    FolderDtoMapper,
+    useCreateFolder,
+    useDeleteFolder,
+    useGetDescendantFolders,
+    useGetFolder,
+    useListFolders,
+    useUpdateFolder
+} from "~/features/folder";
 import { FolderItem } from "~/types";
-import { FolderDtoMapper } from "~/features/folder/listFolders/FolderDto";
+import { ROOT_FOLDER } from "~/constants";
 
 export const useFolders = () => {
+    const { canUseFolderLevelPermissions } = useWcp();
+
     const [vm, setVm] = useState<{
         folders: FolderItem[];
         loading: Record<string, boolean>;
@@ -25,21 +30,16 @@ export const useFolders = () => {
     });
 
     const foldersContext = useContext(FoldersContext);
-    const appContext = useContext(AcoAppContext);
 
     if (!foldersContext) {
         throw new Error("useFolders must be used within a FoldersProvider");
     }
 
-    const app = appContext ? appContext.app : undefined;
-
-    const type = foldersContext.type ?? app?.id;
+    const { type } = foldersContext;
 
     if (!type) {
         throw Error(`FoldersProvider requires a "type" prop or an AcoAppContext to be available!`);
     }
-
-    const { folderLevelPermissions } = foldersContext;
 
     // Folders cache
     const foldersCache = useMemo(() => {
@@ -87,6 +87,28 @@ export const useFolders = () => {
             }));
         });
     }, [loadingRepository]);
+
+    const folderLevelPermissions: FoldersContextFolderLevelPermissions = useMemo(() => {
+        const createCanManage =
+            (callback: (folder: FolderItem) => boolean) => (folderId: string) => {
+                if (!canUseFolderLevelPermissions() || folderId === ROOT_FOLDER) {
+                    return true;
+                }
+
+                const folder = vm.folders?.find(folder => folder.id === folderId);
+                if (!folder) {
+                    return false;
+                }
+
+                return callback(folder);
+            };
+
+        return {
+            canManageStructure: createCanManage(folder => folder.canManageStructure),
+            canManagePermissions: createCanManage(folder => folder.canManagePermissions),
+            canManageContent: createCanManage(folder => folder.canManageContent)
+        };
+    }, [vm.folders]);
 
     return {
         ...vm,
