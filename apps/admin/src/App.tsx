ed8725ecb1e4d0cf69c82cbceeca28a95f364a64
adrians5j <adrian@webiny.com>
2024-10-22 16:33:32 +0200

    wip

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,29 +1,13 @@
 import React from "react";
 import { Admin } from "@webiny/app-serverless-cms";
-import { Auth0 } from "@webiny/app-admin-auth0";
+import { Cognito } from "@webiny/app-admin-users-cognito";
 import { Extensions } from "./Extensions";
 import "./App.scss";
 
 export const App = () => {
     return (
         <Admin>
-            <Auth0
-                autoLogin={() => {
-                    return false;
-                }}
-                auth0={{
-                    domain: String(process.env.WEBINY_ADMIN_AUTH0_DOMAIN),
-                    clientId: String(process.env.WEBINY_ADMIN_AUTH0_CLIENT_ID)
-                }}
-                rootAppClientId={String(process.env.WEBINY_ADMIN_AUTH0_CLIENT_ID)}
-                onLogout={logout => {
-                    logout({
-                        logoutParams: {
-                            returnTo: window.location.origin + "?action=logout"
-                        }
-                    });
-                }}
-            />
+            <Cognito />
             <Extensions />
         </Admin>
     );
