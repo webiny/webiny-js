f9e9969f44791cd15ce290d2ea58c85a36e81a39
adrians5j <adrian@webiny.com>
2024-10-23 14:54:04 +0200

    wip

--- packages/api-security-okta/src/index.ts
+++ packages/api-security-okta/src/index.ts
@@ -1,4 +1,8 @@
 export { createIdentityType } from "./createIdentityType";
 export { createAuthenticator } from "./createAuthenticator";
 export type { AuthenticatorConfig } from "./createAuthenticator";
+export {
+    createGroupsTeamsAuthorizer,
+    type GroupsTeamsAuthorizerConfig
+} from "@webiny/api-security";
 export { createOkta } from "./createOkta";
