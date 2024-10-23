88596baef0531be03cd26ce3ad2341aea1f9d38c
adrians5j <adrian@webiny.com>
2024-10-23 14:57:38 +0200

    wip

--- packages/api-security-okta/src/index.ts
+++ packages/api-security-okta/src/index.ts
@@ -1,8 +1,20 @@
-export { createIdentityType } from "./createIdentityType";
-export { createAuthenticator } from "./createAuthenticator";
-export type { AuthenticatorConfig } from "./createAuthenticator";
-export {
+import {
     createGroupsTeamsAuthorizer,
     type GroupsTeamsAuthorizerConfig
 } from "@webiny/api-security";
+
+export { createIdentityType } from "./createIdentityType";
+export { createAuthenticator } from "./createAuthenticator";
+export type { AuthenticatorConfig } from "./createAuthenticator";
 export { createOkta } from "./createOkta";
+
+export { createGroupsTeamsAuthorizer, GroupsTeamsAuthorizerConfig };
+
+// Backwards compatibility.
+// @deprecated Use `createGroupsTeamsAuthorizer` instead.
+const createGroupAuthorizer = createGroupsTeamsAuthorizer;
+
+// @deprecated Use `GroupsTeamsAuthorizerConfig` instead.
+type GroupAuthorizerConfig = GroupsTeamsAuthorizerConfig;
+
+export { createGroupAuthorizer, type GroupAuthorizerConfig };
