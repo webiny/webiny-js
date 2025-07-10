5255f2befab74626ce319d74bdd16d457dc6494f
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-07-10 10:18:54 +0200

    fix: align ColumnSorter items properly

--- packages/admin-ui/src/DataTable/components/ColumnSorter.tsx
+++ packages/admin-ui/src/DataTable/components/ColumnSorter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cn, cva, type VariantProps } from "~/utils";
 
-const columnSorterVariants = cva("wby-flex wby-items-stretch wby-gap-xxs wby-cursor-auto", {
+const columnSorterVariants = cva("wby-flex wby-items-center wby-gap-xxs wby-cursor-auto", {
     variants: {
         sortable: {
             true: "wby-cursor-pointer"
