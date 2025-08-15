718eb294b67c2480ec32818fc445bcb7ab0fdaa0
Bruno Zorić <bruno.zoric@gmail.com>
2025-08-15 14:57:21 +0200

    chore: yarn and sync deps

--- packages/pulumi-aws/src/apps/api/ApiCloudfront.ts
+++ packages/pulumi-aws/src/apps/api/ApiCloudfront.ts
@@ -16,7 +16,14 @@ export const ApiCloudfront = createAppModule({
             whitelistedNames: ["wby-id-token"]
         };
 
-        const forwardHeaders = ["Origin", "Authorization", "Accept", "Accept-Language", "X-Tenant", "X-I18n-Locale"];
+        const forwardHeaders = [
+            "Origin",
+            "Authorization",
+            "Accept",
+            "Accept-Language",
+            "X-Tenant",
+            "X-I18n-Locale"
+        ];
 
         return app.addResource(aws.cloudfront.Distribution, {
             name: "api-cloudfront",
