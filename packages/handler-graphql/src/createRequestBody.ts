8cd9565d2a919e86e4fe90415cdb566386f03f82
Bruno Zorić <bruno.zoric@gmail.com>
2025-08-16 17:31:20 +0200

    fix(handler-graphql): create request body schema passthrough

--- packages/handler-graphql/src/createRequestBody.ts
+++ packages/handler-graphql/src/createRequestBody.ts
@@ -2,11 +2,13 @@ import type { GraphQLRequestBody } from "~/types.js";
 import { createZodError } from "@webiny/utils/createZodError.js";
 import zod from "zod";
 
-const requestBodySchema = zod.object({
-    query: zod.string(),
-    variables: zod.record(zod.any()).nullish().optional(),
-    operationName: zod.string().optional()
-});
+const requestBodySchema = zod
+    .object({
+        query: zod.string(),
+        variables: zod.record(zod.any()).nullish().optional(),
+        operationName: zod.string().optional()
+    })
+    .passthrough();
 
 const schema = zod.union([requestBodySchema, zod.array(requestBodySchema)]);
 
