3422da688bac4ccc84a524932f572c1a562914e2
Bruno Zorić <bruno.zoric@gmail.com>
2025-08-18 15:40:25 +0200

    chore: update jest

--- scripts/updatePackagesLib/presets/jest.ts
+++ scripts/updatePackagesLib/presets/jest.ts
@@ -3,7 +3,7 @@ import { createPreset } from "../createPreset";
 export const jest = createPreset(() => {
     return {
         name: "jest",
-        matching: /^@jest\/|^jest|^@types\/jest/,
+        matching: /^@jest\/|^jest|^@types\/jest|jest/,
         skipResolutions: true
     };
 });
