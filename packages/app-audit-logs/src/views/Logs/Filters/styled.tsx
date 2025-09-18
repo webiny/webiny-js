f97eb58966852e9a7cea1f5ec042f026abdbc107
Bruno Zorić <bruno.zoric@gmail.com>
2025-09-18 11:40:50 +0200

    wip(api-audit-logs): filtering

--- packages/app-audit-logs/src/views/Logs/Filters/styled.tsx
+++ packages/app-audit-logs/src/views/Logs/Filters/styled.tsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 
 export const TimestampFiltersContainer = styled.div`
-  display: flex;
-  column-gap: 16px;
-  flex-grow: 1;
+    display: flex;
+    column-gap: 16px;
+    flex-grow: 1;
 
-  .wby-relative {
-    height: 32px;
-  }
+    .wby-relative {
+        height: 32px;
+    }
 `;
