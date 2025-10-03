dc73e83e5237aca3a35d722237756cf0f66f60c3
adrians5j <adrian@webiny.com>
2025-10-03 18:20:21 +0200

    chore: test PR workflow

--- packages/cli-core/src/createCliContainer.ts
+++ packages/cli-core/src/createCliContainer.ts
@@ -56,6 +56,8 @@ const { bgYellow, bold } = chalk;
 export const createCliContainer = async (params: CliParamsService.Params) => {
     const container = new Container();
 
+    // Dummy change.
+
     // Features (commands).
     container.register(aboutCommand).inSingletonScope();
     container.register(buildCommand).inSingletonScope();
