9da33b64f4523af05a7b2206756fb8acb2ef4104
Sinisa Kusic <contact.sinisa@gmail.com>
2025-10-13 15:42:44 +0200

    feat(cms): add wby-size-md class to menu icon
    Adds the "wby-size-md" CSS class to the FontAwesomeIcon in the menu group
    icon component for consistent sizing and styling.

--- packages/app-headless-cms/src/admin/menus/Icon.tsx
+++ packages/app-headless-cms/src/admin/menus/Icon.tsx
@@ -11,6 +11,7 @@ export const Icon = ({ group }: IconProps) => {
     return (
         <FontAwesomeIcon
             style={{ color: "var(--mdc-theme-text-secondary-on-background)" }}
+            className="wby-size-md"
             icon={(group.icon || "").split("/") as IconProp}
         />
     );
