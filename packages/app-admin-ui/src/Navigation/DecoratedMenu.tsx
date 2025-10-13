3811071bc447226576dc2b11104a3d98bba16a42
Sinisa Kusic <contact.sinisa@gmail.com>
2025-10-13 15:05:54 +0200

    feat(admin-ui): simplify menu decorators for parent icon
    Refactored Menu.Item and Menu.Link decorators to use direct function
    declarations instead of intermediate variables. Removed unnecessary
    displayName assignments for cleaner code. This improves readability and
    maintainability of the DecoratedMenu component.

--- packages/app-admin-ui/src/Navigation/DecoratedMenu.tsx
+++ packages/app-admin-ui/src/Navigation/DecoratedMenu.tsx
@@ -7,27 +7,23 @@ import { useMenuParentIcon } from "./PinnedMenuItems.js";
 const { Menu } = AdminConfig;
 
 const MenuItemWithParentIcon = Menu.Item.createDecorator(Original => {
-    const MenuItemRenderer = (props: SidebarMenuItemButtonProps) => {
+    return function MenuItemRenderer(props: SidebarMenuItemButtonProps) {
         const icon = useMenuParentIcon();
         if (icon) {
             return <Original {...props} icon={icon} />;
         }
         return <Original {...props} />;
     };
-    MenuItemRenderer.displayName = "MenuItemWithParentIcon";
-    return MenuItemRenderer;
 });
 
 const MenuLinkWithParentIcon = Menu.Link.createDecorator(Original => {
-    const MenuLinkRenderer = (props: SidebarMenuItemLinkProps) => {
+    return function MenuLinkRenderer(props: SidebarMenuItemLinkProps) {
         const icon = useMenuParentIcon();
         if (icon) {
             return <Original {...props} icon={icon} />;
         }
         return <Original {...props} />;
     };
-    MenuLinkRenderer.displayName = "MenuLinkWithParentIcon";
-    return MenuLinkRenderer;
 });
 
 export const DecoratedMenu = () => {
