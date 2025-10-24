d6177a0492d5a5f8bb75eaad42cfe7d88f71af46
Pavel Denisjuk <pavel@webiny.com>
2025-10-24 11:08:24 +0200

    fix: correct placement for workflow context

--- packages/app-headless-cms-workflows/src/Components/ContentEntryFormWorkflow.tsx
+++ packages/app-headless-cms-workflows/src/Components/ContentEntryFormWorkflow.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { ContentEntryForm, useContentEntry } from "@webiny/app-headless-cms";
-import { WorkflowStateBar, WorkflowStateSetup } from "@webiny/app-workflows";
+import { WorkflowStateBar } from "@webiny/app-workflows";
 import { Grid } from "@webiny/admin-ui";
 import { useSecurity } from "@webiny/app-security";
-import { useApolloClient } from "@apollo/react-hooks";
-import { CmsEntryFormTooltipButton } from "~/Components/CmsEntryFormTooltipButton.js";
 
 export const ContentEntryFormWorkflow = ContentEntryForm.createDecorator(Original => {
     return function ContentEntryFormWorkflow(props) {
         const { entry, contentModel: model } = useContentEntry();
-        const client = useApolloClient();
 
         const { identity } = useSecurity();
         if (!entry?.id || !model?.modelId || !identity?.id) {
@@ -19,15 +16,7 @@ export const ContentEntryFormWorkflow = ContentEntryForm.createDecorator(Origina
         return (
             <Grid>
                 <Grid.Column span={12}>
-                    <WorkflowStateSetup
-                        app={`cms.${model.modelId}`}
-                        id={entry.id}
-                        identity={identity}
-                        client={client}
-                    >
-                        <WorkflowStateBar />
-                        <CmsEntryFormTooltipButton />
-                    </WorkflowStateSetup>
+                    <WorkflowStateBar />
                 </Grid.Column>
                 <Grid.Column span={12}>
                     <Original {...props} />
--- packages/app-headless-cms-workflows/src/index.tsx
+++ packages/app-headless-cms-workflows/src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Wcp } from "@webiny/app-admin";
 import { ContentEntryFormWorkflow } from "~/Components/ContentEntryFormWorkflow.js";
 import { CmsWorkflowsEditor } from "~/Views/CmsWorkflowsEditor.js";
-import { Wcp } from "@webiny/app-admin";
+import { CmsEntryFormTooltipButton } from "~/Components/CmsEntryFormTooltipButton.js";
+import { ContentEntryWorkflow } from "~/Components/ContentEntryWorkflow.js";
 
 export const CmsWorkflows = () => {
     return (
         <Wcp.CanUseWorkflows>
             <CmsWorkflowsEditor />
             <ContentEntryFormWorkflow />
+            <ContentEntryWorkflow />
+            <CmsEntryFormTooltipButton />
         </Wcp.CanUseWorkflows>
     );
 };
