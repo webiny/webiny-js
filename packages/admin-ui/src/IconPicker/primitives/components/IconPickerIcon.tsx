66da46cc6b6f87ab455fc9018971ae67dac449f4
adrians5j <adrian@webiny.com>
2025-10-24 17:52:54 +0200

    remove default

--- packages/admin-ui/src/IconPicker/primitives/components/IconPickerIcon.tsx
+++ packages/admin-ui/src/IconPicker/primitives/components/IconPickerIcon.tsx
@@ -13,7 +13,7 @@ const IconPickerIcon = ({ name, prefix, ...props }: IconPickerIconProps) => {
             <FontAwesomeIcon
                 icon={[prefix, name]}
                 size={"2x"}
-                className={"block size-full"}
+                className={"block size-full!"}
             />
         </div>
     );
