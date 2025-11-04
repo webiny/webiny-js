9772e241138da10270f709db8357ffe270f3e9ef
adrians5j <adrian@webiny.com>
2025-11-04 11:50:55 +0100

    wip

--- packages/project-aws/src/pulumi/apps/core/CoreOpenSearch.ts
+++ packages/project-aws/src/pulumi/apps/core/CoreOpenSearch.ts
@@ -42,7 +42,7 @@ function getProdClusterConfig(): aws.types.input.opensearch.DomainClusterConfig 
     };
 }
 
-const OS_ENGINE_VERSION = "OpenSearch_3.1";
+const OS_ENGINE_VERSION = "OpenSearch_2.11";
 
 export const OpenSearch = createAppModule({
     name: "OpenSearch",
