2434e8f6c3c5d6765cab865e824990a33804812d
adrians5j <adrian@webiny.com>
2025-11-08 07:14:18 +0100

    wip

--- webiny.config.tsx
+++ webiny.config.tsx
@@ -7,6 +7,8 @@ import { Admin, Cli, Infra, Project } from "./packages/webiny/dist/extensions/in
 export default () => {
     return (
         <>
+            {/* Admin 👇 */}
+            <Admin.Extension src={"./extensions/MyAdminExtension.tsx"} />
 
             {/* Infra 👇 */}
             <Infra.PulumiResourceNamePrefix prefix={"myproj-"} />
@@ -17,74 +19,59 @@ export default () => {
             <Infra.AwsTags tags={{ OWNER: "me", PROJECT: "my-project" }} />
             <Infra.AwsTags tags={{ OWNER2: "me2", PROJECT2: "my-project-2" }} />
 
+            {/*<Infra.Admin.CustomDomains
+                domains={["my.domain.com"]}
+                sslMethod="sni-only"
+                certificateArn="arn:aws:acm:us-east-1:636962863878:certificate/3baf9092-fb27-4efb-9409-XXXXXXXX"
+            />
+
+            <Infra.BlueGreenDeployments
+                enabled={true}
+                domains={{
+                    acmCertificateArn:
+                        "arn:aws:acm:us-east-1:636962863878:certificate/3baf9092-fb27-4efb-9409-XXXXXXXX",
+                    sslSupportMethod: "sni-only",
+                    domains: {
+                        api: ["api.bg.webiny.com"],
+                        admin: ["admin.bg.webiny.com"],
+                        website: ["website.bg.webiny.com"],
+                        preview: ["preview.bg.webiny.com"]
+                    }
+                }}
+                deployments={[
+                    {
+                        name: "green",
+                        env: "dev",
+                        variant: "green"
+                    },
+                    {
+                        name: "blue",
+                        env: "dev",
+                        variant: "blue"
+                    }
+                ]}
+            />*/}
+
             {/* CLI 👇 */}
             <Cli.Command src={"./extensions/MyCustomCommand.ts"} />
 
             {/* Project 👇 */}
             <Project.Telemetry enabled={false} />
 
+            {/* 🚧 WIP 👇 */}
             {/*<Security.ApiKeyBeforeCreate src={"./extensions/ApiKeyBeforeCreate.ts"} />*/}
-
-            {/* Cloud infrastructure related extensions. All within the `Infra.` namespace. */}
-
-            {/*<Infra.Api.BeforeDeploy src={"./extensions/myApiBeforeDeploy.ts"} />*/}
-            {/*<Infra.Api.BeforeBuild src={"./extensions/myApiBeforeBuild.ts"} />*/}
-            {/*<Infra.Api.AfterDeploy src={"./extensions/myApiAfterDeploy.ts"} />*/}
-            {/*<Infra.Api.AfterBuild src={"./extensions/myApiAfterBuild.ts"} />*/}
-
-
-            {/*<Infra.Admin.CustomDomains*/}
-            {/*    domains={["my.domain.com"]}*/}
-            {/*    sslMethod="sni-only"*/}
-            {/*    certificateArn="arn:aws:acm:us-east-1:636962863878:certificate/3baf9092-fb27-4efb-9409-XXXXXXXX"*/}
-            {/*/>*/}
-
-            {/*<Infra.BlueGreenDeployments*/}
-            {/*    enabled={true}*/}
-            {/*    domains={{*/}
-            {/*        acmCertificateArn:*/}
-            {/*            "arn:aws:acm:us-east-1:636962863878:certificate/3baf9092-fb27-4efb-9409-XXXXXXXX",*/}
-            {/*        sslSupportMethod: "sni-only",*/}
-            {/*        domains: {*/}
-            {/*            api: ["api.bg.webiny.com"],*/}
-            {/*            admin: ["admin.bg.webiny.com"],*/}
-            {/*            website: ["website.bg.webiny.com"],*/}
-            {/*            preview: ["preview.bg.webiny.com"]*/}
-            {/*        }*/}
-            {/*    }}*/}
-            {/*    deployments={[*/}
-            {/*        {*/}
-            {/*            name: "green",*/}
-            {/*            env: "dev",*/}
-            {/*            variant: "green"*/}
-            {/*        },*/}
-            {/*        {*/}
-            {/*            name: "blue",*/}
-            {/*            env: "dev",*/}
-            {/*            variant: "blue"*/}
-            {/*        }*/}
-            {/*    ]}*/}
-            {/*/>*/}
-
-
-
-            {/* App (Backend/Admin) related extensions. */}
-            {/* Notice the `Admin.` and `Backend.` namespaces. These are the prefixes
-                users will use to add their own extensions. */}
-            <Admin.Extension src={"./extensions/myAdminExtension.tsx"} />
-
             {/*<AuditLogs.RetentionPeriod days={90} />*/}
-            {/*<AuditLogs.Backend.RetentionPeriod days={90} />*/}
+            {/*<AuditLogs.RetentionPeriod days={90} />*/}
             {/*<AuditLogs.Admin.RetentionPeriod days={90} />*/}
 
-            {/*<Cms.Backend.OnEntryBeforeCreate.../>*/}
+            {/*<Cms.OnEntryBeforeCreate.../>*/}
 
-            {/*<Backend.Security.Authenticator src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
-            {/*<Backend.Cms.OnEntryBeforeCreate src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
-            {/*<Backend.Cms.Model src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
-            {/*<Backend.Cms.ModelGroup src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
-            {/*<Backend.Fm.FileModelModifier src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
-            {/*<Backend.Fm.OnFileBeforeCreate src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Security.Authenticator src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Cms.OnEntryBeforeCreate src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Cms.Model src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Cms.ModelGroup src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Fm.FileModelModifier src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
+            {/*<Fm.OnFileBeforeCreate src={"./extensions/myOnEntryBeforeCreate.ts"} />*/}
 
             {/*Finally, we have these extra extensions that are not even included in the default*/}
             {/*`@webiny/extensions` package. These will most probably always be without any*/}
