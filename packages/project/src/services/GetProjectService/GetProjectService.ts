451701a4aed2dda50dd0cb92d6e5a1def98a56b3
adrians5j <adrian@webiny.com>
2025-11-21 13:36:16 +0100

    wipwip

--- packages/project/src/services/GetProjectService/GetProjectService.ts
+++ packages/project/src/services/GetProjectService/GetProjectService.ts
@@ -38,8 +38,9 @@ export class DefaultGetProjectService implements GetProjectService.Interface {
         const webinyConfigBaseFilePath = workspaceFolderPath.join("webiny.config.base.tsx");
         const localPulumiStateFilesFolderPath = projectRootFolderPath.join(".pulumi");
         const tsConfigFilePath = projectRootFolderPath.join("tsconfig.json");
-        const projectVersion = this.getProjectVersionService.execute(cwd);
+        const projectVersion = this.getProjectVersionService.execute();
 
+        console.log('eyeaaaaa', projectVersion)
         this.cachedProject = new ProjectModel({
             // TODO: read project name from config file.
             name: path.basename(projectRootFolderPath.toString()),
