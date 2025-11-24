84b5f49ffa4e2d97b373cd5869d64479436631c6
adrians5j <adrian@webiny.com>
2025-11-24 14:41:41 +0100

    ci: bring back v5 `/cypress` task [no ci]

--- .github/workflows/wac/v5_PullRequestsCommandCypress.wac.ts
+++ .github/workflows/wac/v5_PullRequestsCommandCypress.wac.ts
@@ -241,7 +241,7 @@ const createCypressJobs = (dbSetup: string) => {
     };
 };
 
-export const pullRequestsCommandCypress = createWorkflow({
+export const v5PullRequestsCommandCypress = createWorkflow({
     name: "(v5) Pull Requests Command - Cypress",
     on: "issue_comment",
     env: {
