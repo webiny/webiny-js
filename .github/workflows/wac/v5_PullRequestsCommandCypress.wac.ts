e3af11c9582457bb620cb36b6383e55ea6c9fe33
adrians5j <adrian@webiny.com>
2025-11-24 14:14:45 +0100

    ci: bring back v5 `/cypress` task [no ci]

--- .github/workflows/wac/v5_PullRequestsCommandCypress.wac.ts
+++ .github/workflows/wac/v5_PullRequestsCommandCypress.wac.ts
@@ -1,7 +1,6 @@
 import { createWorkflow, NormalJob } from "github-actions-wac";
 import {
     createSetupVerdaccioSteps,
-    createDeployWebinySteps,
     createYarnCacheSteps,
     createInstallBuildSteps,
     createGlobalBuildCacheSteps,
@@ -25,6 +24,31 @@ const installBuildSteps = createInstallBuildSteps({ workingDirectory: DIR_WEBINY
 const yarnCacheSteps = createYarnCacheSteps({ workingDirectory: DIR_WEBINY_JS });
 const globalBuildCacheSteps = createGlobalBuildCacheSteps({ workingDirectory: DIR_WEBINY_JS });
 const runBuildCacheSteps = createRunBuildCacheSteps({ workingDirectory: DIR_WEBINY_JS });
+
+const createDeployWebinySteps = ({ workingDirectory = "dev" } = {}) => {
+    return [
+        {
+            name: "Deploy Core",
+            "working-directory": workingDirectory,
+            run: "yarn webiny deploy core --env dev"
+        },
+        {
+            name: "Deploy API",
+            "working-directory": workingDirectory,
+            run: "yarn webiny deploy api --env dev"
+        },
+        {
+            name: "Deploy Admin Area",
+            "working-directory": workingDirectory,
+            run: "yarn webiny deploy admin --env dev"
+        },
+        {
+            name: "Deploy Website",
+            "working-directory": workingDirectory,
+            run: "yarn webiny deploy website --env dev"
+        }
+    ];
+};
 
 const createCheckoutPrSteps = () =>
     [
@@ -156,13 +180,6 @@ const createCypressJobs = (dbSetup: string) => {
             ...createDeployWebinySteps({ workingDirectory: DIR_TEST_PROJECT }),
             ...withCommonParams(
                 [
-                    {
-                        name: "Deployment Summary",
-                        run: `${runNodeScript(
-                            "printDeploymentSummary",
-                            `../${DIR_TEST_PROJECT}`
-                        )} >> $GITHUB_STEP_SUMMARY`
-                    },
                     {
                         name: "Create Cypress config",
                         run: `yarn setup-cypress --projectFolder ../${DIR_TEST_PROJECT}`
