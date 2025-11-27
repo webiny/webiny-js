57ea41676acd8dddfc23361ffdfc3574ab1d52cf
adrians5j <adrian@webiny.com>
2025-11-27 08:57:03 +0100

    wip

--- packages/app-admin-ui/src/Dashboard/Welcome.tsx
+++ packages/app-admin-ui/src/Dashboard/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Grid, Heading } from "@webiny/admin-ui";
 import { useSecurity } from "@webiny/app-security/hooks/useSecurity.js";
 import { useAdminConfig } from "@webiny/app-admin";
@@ -6,24 +6,6 @@ import { useAdminConfig } from "@webiny/app-admin";
 const Welcome = () => {
     const { identity } = useSecurity();
     const { widgets } = useAdminConfig();
-
-    // Group by column and sort by pin
-    const { leftWidgets, rightWidgets } = useMemo(() => {
-        const sortByPin = (widgets: typeof widgets) => {
-            const pinFirst = widgets.filter(w => w.pin === "first");
-            const pinLast = widgets.filter(w => w.pin === "last");
-            const regular = widgets.filter(w => !w.pin);
-            return [...pinFirst, ...regular, ...pinLast];
-        };
-
-        const left = widgets.filter(w => w.column === "left" || !w.column);
-        const right = widgets.filter(w => w.column === "right");
-
-        return {
-            leftWidgets: sortByPin(left),
-            rightWidgets: sortByPin(right)
-        };
-    }, [widgets]);
 
     return (
         <div className={"my-xxl"}>
@@ -35,16 +17,20 @@ const Welcome = () => {
             <Grid gap={"spacious"} className={"max-w-[1200px]"}>
                 <Grid.Column span={6}>
                     <div className={"flex flex-col gap-lg"}>
-                        {leftWidgets.map(widget => (
-                            <React.Fragment key={widget.name}>{widget.element}</React.Fragment>
-                        ))}
+                        {widgets
+                            .filter(w => w.column === "left")
+                            .map(widget => (
+                                <React.Fragment key={widget.name}>{widget.element}</React.Fragment>
+                            ))}
                     </div>
                 </Grid.Column>
                 <Grid.Column span={6}>
                     <div className={"flex flex-col gap-lg"}>
-                        {rightWidgets.map(widget => (
-                            <React.Fragment key={widget.name}>{widget.element}</React.Fragment>
-                        ))}
+                        {widgets
+                            .filter(w => w.column === "right")
+                            .map(widget => (
+                                <React.Fragment key={widget.name}>{widget.element}</React.Fragment>
+                            ))}
                     </div>
                 </Grid.Column>
             </Grid>
