c38985eadb45844dcc92babbe48607694ce45129
Pavel Denisjuk <pavel@webiny.com>
2025-11-28 15:20:28 +0100

    fix(project): replace jsdom with react-test-renderer

--- packages/project/src/services/GetProjectConfigService/renderConfigWorker.tsx
+++ packages/project/src/services/GetProjectConfigService/renderConfigWorker.tsx
@@ -2,11 +2,8 @@ import "tsx/esm";
 import { Properties, toObject } from "@webiny/react-properties";
 import debounce from "debounce";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import Renderer from "react-test-renderer";
 import { serializeError } from "serialize-error";
-
-// @ts-expect-error jsdom types are messing up with the repo, so they're disabled in the root package.json.
-import { JSDOM } from "jsdom";
 import type { RenderConfigWorkerMessageDto, RenderConfigParamsDto } from "./renderConfig.js";
 import { ProjectModel } from "~/models/ProjectModel.js";
 
@@ -51,16 +48,7 @@ const onChange = debounce((value: any) => {
     process.exit(0);
 });
 
-const { window } = new JSDOM(`<div id="root"/>`);
-
-global.window = window;
-global.document = window.document;
-
-const root = window.document.getElementById("root");
-
-const reactRoot = createRoot(root);
-
-reactRoot.render(
+Renderer.create(
     <Properties onChange={onChange}>
         <Extensions />
     </Properties>
