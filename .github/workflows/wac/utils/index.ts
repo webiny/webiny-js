d97996ac981b9995b105da65d633fc60c7252b72
adrians5j <adrian@webiny.com>
2025-12-02 06:40:10 +0100

    ci: bring back v5 workflows [no ci]

--- .github/workflows/wac/utils/index.ts
+++ .github/workflows/wac/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./constants";
 export * from "./runNodeScript";
 export * from "./addToOutputs";
+export * from "./v5ListPackagesWithJestTests";
---
+++ .github/workflows/wac/v5_pullRequestsCommandJest.wac.ts
@@ -0,0 +1,172 @@
+import { createWorkflow, NormalJob } from "github-actions-wac";
+import {
+    createGlobalBuildCacheSteps,
+    createInstallBuildSteps,
+    createRunBuildCacheSteps,
+    createYarnCacheSteps,
+    withCommonParams
+} from "./steps";
+import {
+    AWS_REGION,
+    BUILD_PACKAGES_RUNNER,
+    listPackagesWithJestTests,
+    NODE_OPTIONS,
+    NODE_VERSION
+} from "./utils";
+import { createJob } from "./jobs";
+
+// Will print "next" or "dev". Important for caching (via actions/cache).
+const DIR_WEBINY_JS = "${{ needs.baseBranch.outputs.base-branch }}";
+
+const installBuildSteps = createInstallBuildSteps({ workingDirectory: DIR_WEBINY_JS });
+const yarnCacheSteps = createYarnCacheSteps({ workingDirectory: DIR_WEBINY_JS });
+const globalBuildCacheSteps = createGlobalBuildCacheSteps({ workingDirectory: DIR_WEBINY_JS });
+const runBuildCacheSteps = createRunBuildCacheSteps({ workingDirectory: DIR_WEBINY_JS });
+
+const createCheckoutPrSteps = () =>
+    [
+        {
+            name: "Checkout Pull Request",
+            "working-directory": DIR_WEBINY_JS,
+            run: "gh pr checkout ${{ github.event.issue.number }}",
+            env: { GITHUB_TOKEN: "${{ secrets.GH_TOKEN }}" }
+        }
+    ] as NonNullable<NormalJob["steps"]>;
+
+const createJestTestsJob = (storage: string | null) => {
+    const env: Record<string, string> = { AWS_REGION };
+
+    if (storage) {
+        if (storage === "ddb-es") {
+            env["AWS_ELASTIC_SEARCH_DOMAIN_NAME"] = "${{ secrets.AWS_ELASTIC_SEARCH_DOMAIN_NAME }}";
+            env["ELASTIC_SEARCH_ENDPOINT"] = "${{ secrets.ELASTIC_SEARCH_ENDPOINT }}";
+            env["ELASTIC_SEARCH_INDEX_PREFIX"] = "${{ matrix.package.id }}";
+        } else if (storage === "ddb-os") {
+            // We still use the same environment variables as for "ddb-es" setup, it's
+            // just that the values are read from different secrets.
+            env["AWS_ELASTIC_SEARCH_DOMAIN_NAME"] = "${{ secrets.AWS_OPEN_SEARCH_DOMAIN_NAME }}";
+            env["ELASTIC_SEARCH_ENDPOINT"] = "${{ secrets.OPEN_SEARCH_ENDPOINT }}";
+            env["ELASTIC_SEARCH_INDEX_PREFIX"] = "${{ matrix.package.id }}";
+        }
+    }
+
+    const packages = listPackagesWithJestTests({ storage });
+
+    return createJob({
+        needs: ["constants", "build"],
+        name: "${{ matrix.package.cmd }}",
+        strategy: {
+            "fail-fast": false,
+            matrix: {
+                os: ["ubuntu-latest"],
+                node: [NODE_VERSION],
+                package: "${{ fromJson('" + JSON.stringify(packages) + "') }}"
+            }
+        },
+        "runs-on": "${{ matrix.os }}",
+        env,
+        awsAuth: storage === "ddb-es" || storage === "ddb-os",
+        checkout: { path: DIR_WEBINY_JS },
+        steps: [
+            ...yarnCacheSteps,
+            ...runBuildCacheSteps,
+            ...installBuildSteps,
+            ...withCommonParams(
+                [{ name: "Run tests", run: "yarn test ${{ matrix.package.cmd }}" }],
+                { "working-directory": DIR_WEBINY_JS }
+            )
+        ]
+    });
+};
+
+export const v5PullRequestsCommandJest = createWorkflow({
+    name: "(v5) Pull Requests Command - Jest",
+    on: "issue_comment",
+    env: {
+        NODE_OPTIONS,
+        AWS_REGION
+    },
+    jobs: {
+        checkComment: createJob({
+            name: `Check comment for /jest`,
+            if: "${{ github.event.issue.pull_request }}",
+            checkout: false,
+            steps: [
+                {
+                    name: "Check for Command",
+                    id: "command",
+                    uses: "xt0rted/slash-command-action@v2",
+                    with: {
+                        "repo-token": "${{ secrets.GITHUB_TOKEN }}",
+                        command: "jest",
+                        reaction: "true",
+                        "reaction-type": "eyes",
+                        "allow-edits": "false",
+                        "permission-level": "write"
+                    }
+                },
+                {
+                    name: "Create comment",
+                    uses: "peter-evans/create-or-update-comment@v2",
+                    with: {
+                        "issue-number": "${{ github.event.issue.number }}",
+                        body: "Jest tests have been initiated (for more information, click [here](https://github.com/webiny/webiny-js/actions/runs/${{ github.run_id }})). :sparkles:"
+                    }
+                }
+            ]
+        }),
+        baseBranch: createJob({
+            needs: "checkComment",
+            name: "Get base branch",
+            outputs: {
+                "base-branch": "${{ steps.base-branch.outputs.base-branch }}"
+            },
+            steps: [
+                {
+                    name: "Get base branch",
+                    id: "base-branch",
+                    env: { GITHUB_TOKEN: "${{ secrets.GH_TOKEN }}" },
+                    run: 'echo "base-branch=$(gh pr view ${{ github.event.issue.number }} --json baseRefName -q .baseRefName)" >> $GITHUB_OUTPUT'
+                }
+            ]
+        }),
+        constants: createJob({
+            needs: "baseBranch",
+            name: "Create constants",
+            outputs: {
+                "global-cache-key": "${{ steps.global-cache-key.outputs.global-cache-key }}",
+                "run-cache-key": "${{ steps.run-cache-key.outputs.run-cache-key }}"
+            },
+            checkout: false,
+            steps: [
+                {
+                    name: "Create global cache key",
+                    id: "global-cache-key",
+                    run: `echo "global-cache-key=\${{ needs.baseBranch.outputs.base-branch }}-\${{ runner.os }}-$(/bin/date -u "+%m%d")-\${{ vars.RANDOM_CACHE_KEY_SUFFIX }}" >> $GITHUB_OUTPUT`
+                },
+                {
+                    name: "Create workflow run cache key",
+                    id: "run-cache-key",
+                    run: 'echo "run-cache-key=${{ github.run_id }}-${{ github.run_attempt }}-${{ vars.RANDOM_CACHE_KEY_SUFFIX }}" >> $GITHUB_OUTPUT'
+                }
+            ]
+        }),
+        build: createJob({
+            name: "Build",
+            needs: ["baseBranch", "constants"],
+            checkout: { path: DIR_WEBINY_JS },
+            "runs-on": BUILD_PACKAGES_RUNNER,
+            steps: [
+                ...createCheckoutPrSteps(),
+                ...yarnCacheSteps,
+                ...globalBuildCacheSteps,
+                ...installBuildSteps,
+                ...runBuildCacheSteps
+            ]
+        }),
+        jestTestsNoStorage: createJestTestsJob(null),
+        jestTestsDdb: createJestTestsJob("ddb"),
+        jestTestsDdbEs: createJestTestsJob("ddb-es"),
+        jestTestsDdbOs: createJestTestsJob("ddb-os")
+    }
+});
