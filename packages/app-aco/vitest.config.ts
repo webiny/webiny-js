137eb586555a12f20250b55d3a3f19264333e814
Pavel Denisjuk <pavel@webiny.com>
2025-12-08 14:28:14 +0100

    test: resolve aliases in tests within src

--- packages/app-aco/vitest.config.ts
+++ packages/app-aco/vitest.config.ts
@@ -1,17 +1,8 @@
 import { createTestConfig } from "../../testing";
-import { defineConfig, mergeConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default async () => {
-    const config = await createTestConfig({
+    return createTestConfig({
         path: import.meta.dirname,
         vitestConfig: { fileParallelism: true }
     });
-
-    return mergeConfig(
-        config,
-        defineConfig({
-            plugins: [tsconfigPaths({ root: import.meta.dirname })]
-        })
-    );
 };
--- testing/createTestConfig.ts
+++ testing/createTestConfig.ts
@@ -2,6 +2,7 @@ import { basename, join } from "path";
 import fs from "fs";
 import type { ViteUserConfig } from "vitest/config";
 import { testPattern } from "./vitest.project.js";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 type TestPreset = {
     setupFiles?: string[];
@@ -52,13 +53,15 @@ export const createTestConfig = async ({
 
     process.env.JEST_DYNALITE_CONFIG_DIRECTORY = path;
 
-    const project: ViteUserConfig["test"] = {
+    const project: NonNullable<ViteUserConfig["test"]> = {
         name: name,
         include: [`${path}${testPattern}`],
         dir: path,
         fileParallelism: false,
         ...vitestConfig,
-        css: false
+        css: false,
+        // @ts-expect-error This _does_ actually work!
+        plugins: [tsconfigPaths({ root: path })]
     };
 
     const setupAfterEnv = join(path, "__tests__", "setup", "setupAfterEnv.js");
