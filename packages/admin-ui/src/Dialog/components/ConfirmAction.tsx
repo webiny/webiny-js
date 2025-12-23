12d9a6407c53e025c697aeadac164ac0a3b33508
Adrian Smijulj <adrian1358@gmail.com>
2025-12-23 10:44:09 +0100

    feat: add scrolling to main menu and new content model dialog (#4815)

--- packages/admin-ui/src/Dialog/components/ConfirmAction.tsx
+++ packages/admin-ui/src/Dialog/components/ConfirmAction.tsx
@@ -3,8 +3,6 @@ import { makeDecoratable } from "~/utils.js";
 import type { ButtonProps } from "~/Button/index.js";
 import { Button } from "~/Button/index.js";
 
-const ConfirmActionBase = (props: ButtonProps) => (
-    <Button text={"Confirm"} {...props} variant="primary" />
-);
+const ConfirmActionBase = (props: ButtonProps) => <Button {...props} variant="primary" />;
 
 export const ConfirmAction = makeDecoratable("ConfirmAction", ConfirmActionBase);
