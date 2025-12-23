724efa9c25decba70f71006fd656612b5294d533
adrians5j <adrian@webiny.com>
2025-12-23 07:35:29 +0100

    fix: update import path for utils in ScrollArea component

--- packages/admin-ui/src/ScrollArea/ScrollArea.tsx
+++ packages/admin-ui/src/ScrollArea/ScrollArea.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
-import { cn } from "~/utils";
+import { cn } from "~/utils.js";
 
 function ScrollArea({
     className,
