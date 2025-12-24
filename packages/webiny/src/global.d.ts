e544c5cde4978485bef5a6f3fb06000cfcead173
adrians5j <adrian@webiny.com>
2025-12-24 06:46:53 +0100

    fix: move assets types to global.ts

--- packages/webiny/src/global.d.ts
+++ packages/webiny/src/global.d.ts
@@ -5,3 +5,20 @@
  * This file ensures all type augmentations are loaded without requiring explicit imports.
  */
 import "@webiny/tasks/global.js";
+
+declare module "*.png" {
+    const png: string;
+    export default png;
+}
+declare module "*.jpg" {
+    const jpg: string;
+    export default jpg;
+}
+declare module "*.svg" {
+    import { FunctionComponent, SVGProps } from "react";
+    export const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement> & {
+        alt?: string;
+    }>;
+    const src: string;
+    export default src;
+}
--- packages/webiny/src/global.ts
+++ packages/webiny/src/global.ts
@@ -4,5 +4,27 @@
  *
  * This file ensures all type augmentations are loaded without requiring explicit imports.
  */
-
 import "@webiny/tasks/global.js";
+
+declare module "*.png" {
+    const png: string;
+    export default png;
+}
+
+declare module "*.jpg" {
+    const jpg: string;
+    export default jpg;
+}
+
+declare module "*.svg" {
+    import { FunctionComponent, SVGProps } from "react";
+
+    export const ReactComponent: FunctionComponent<
+        SVGProps<SVGSVGElement> & {
+            alt?: string;
+        }
+    >;
+
+    const src: string;
+    export default src;
+}
