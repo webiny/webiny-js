b637d599bc2f28e9f941daf267f219828b36311c
adrians5j <adrian@webiny.com>
2025-12-29 16:35:43 +0100

    wip:15

--- packages/project-aws/src/services/AdminStackOutputService.ts
+++ packages/project-aws/src/services/AdminStackOutputService.ts
@@ -1,11 +1,14 @@
 import { GetAppStackOutput } from "@webiny/project/abstractions/index.js";
 import { AdminStackOutputService as AdminStackOutputServiceAbstraction } from "../abstractions/index.js";
 
-export class DefaultAdminStackOutputServiceAbstraction implements AdminStackOutputServiceAbstraction.Interface {
+export class DefaultAdminStackOutputServiceAbstraction
+    implements AdminStackOutputServiceAbstraction.Interface
+{
     constructor(private readonly getAppStackOutput: GetAppStackOutput.Interface) {}
 
     async execute<
-        TOutput extends AdminStackOutputServiceAbstraction.Output = AdminStackOutputServiceAbstraction.Output
+        TOutput extends
+            AdminStackOutputServiceAbstraction.Output = AdminStackOutputServiceAbstraction.Output
     >(): Promise<TOutput | null> {
         return this.getAppStackOutput.execute<TOutput>("admin");
     }
