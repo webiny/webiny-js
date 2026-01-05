ef06194939620d141bf5b8445d29ffa9aae537e0
adrians5j <adrian@webiny.com>
2026-01-05 11:20:13 +0100

    wip:13 (vd41d)

--- packages/project/src/extensions/ProjectDecorator.ts
+++ packages/project/src/extensions/ProjectDecorator.ts
@@ -5,6 +5,7 @@ import { zodPathToFile } from "../defineExtension/zodTypes/zodPathToFile.js";
 export const ProjectDecorator = defineExtension({
     type: "Project/Decorator",
     tags: { runtimeContext: "project" },
+    multiple: true,
     description: "Decorate an existing implementation with additional functionality.",
     paramsSchema: ({ project }) => {
         return z.object({
