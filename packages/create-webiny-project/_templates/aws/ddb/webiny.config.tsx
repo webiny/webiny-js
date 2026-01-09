e1eb2fc5f8a310aa09c48b74a9df0c42f9ce1f8c
adrians5j <adrian@webiny.com>
2026-01-09 17:00:38 +0100

    feat: add Cognito integration to webiny.config.tsx and update dependencies

--- packages/create-webiny-project/_templates/aws/ddb/webiny.config.tsx
+++ packages/create-webiny-project/_templates/aws/ddb/webiny.config.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Infra } from "webiny/extensions";
+import { Cognito } from "@webiny/cognito";
 
 export const Extensions = () => {
     return (
         <>
             <Infra.Aws.DefaultRegion name={"{REGION}"} />
+            <Cognito />
         </>
     );
 };
