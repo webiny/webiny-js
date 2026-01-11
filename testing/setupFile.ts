bf93d571994713f33c2c4fcb4262c654f25491b8
Pavel Denisjuk <pavel@webiny.com>
2026-01-11 11:29:58 +0100

    wip: model builders

--- testing/setupFile.ts
+++ testing/setupFile.ts
@@ -3,6 +3,4 @@ import * as matchers from "jest-extended";
 
 expect.extend(matchers as any);
 
-if (!process.env.AWS_REGION) {
-    process.env.AWS_REGION = "us-east-1";
-}
+process.env.AWS_REGION = "local";
