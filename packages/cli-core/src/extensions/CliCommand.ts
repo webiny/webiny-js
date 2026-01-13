775ff529c4270eb655c098360808c19cc4733549
adrians5j <adrian@webiny.com>
2026-01-13 06:01:45 +0100

    wip:8 (n6t8o)

--- packages/cli-core/src/extensions/CliCommand.ts
+++ packages/cli-core/src/extensions/CliCommand.ts
@@ -1,6 +1,5 @@
 import { defineExtension, zodPathToAbstraction } from "@webiny/project/extensions/index.js";
 import { CliCommand as CliCommandAbstraction } from "~/abstractions/index.js";
-import { z } from "zod";
 
 export const CliCommand = defineExtension({
     type: "Cli/Command",
@@ -8,8 +7,8 @@ export const CliCommand = defineExtension({
     description: "An extension for defining CLI commands.",
     multiple: true,
     paramsSchema: ({ project }) => {
-        return z.object({
+        return {
             src: zodPathToAbstraction(CliCommandAbstraction, project)
-        });
+        };
     }
 });
