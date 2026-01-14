1fe0e5eddb90387175d8f4058e89631fe52703e8
adrians5j <adrian@webiny.com>
2026-01-14 10:40:34 +0100

    wip:11 (gh6kl)

--- packages/app-headless-cms/src/admin/menus/GroupMenu.tsx
+++ packages/app-headless-cms/src/admin/menus/GroupMenu.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import type { CmsGroup } from "~/types.js";
 import { AdminConfig } from "@webiny/app-admin";
-import { Icon } from "./Icon.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const { Menu } = AdminConfig;
 
@@ -9,15 +10,24 @@ const { Menu } = AdminConfig;
  * Renders a menu item for a given content group.
  * Displays the group's name and icon in the menu.
  */
-export const GroupMenu = ({ group }: { group: CmsGroup }) => (
-    <Menu
-        name={group.id}
-        parent="headlessCMSContent"
-        element={
-            <Menu.Item
-                text={group.name}
-                icon={<Menu.Link.Icon label="Content" element={<Icon group={group} />} />}
-            />
-        }
-    />
-);
+export const GroupMenu = ({ group }: { group: CmsGroup }) => {
+    const icon = (group.icon || "").split("/") as IconProp;
+
+    return (
+        <Menu
+            name={group.id}
+            parent="headlessCMSContent"
+            element={
+                <Menu.Item
+                    text={group.name}
+                    icon={
+                        <Menu.Item.Icon
+                            label={group.name}
+                            element={<FontAwesomeIcon icon={icon} />}
+                        />
+                    }
+                />
+            }
+        />
+    );
+};
