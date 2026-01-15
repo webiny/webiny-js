7737750bad6ce8868e3eed04847b84fde9a9e5c7
adrians5j <adrian@webiny.com>
2026-01-15 12:16:58 +0100

    wip:30 (paza4)

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/dynamicZone/AddTemplate.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/dynamicZone/AddTemplate.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as AddIcon } from "@webiny/icons/add.svg";
 import type { CmsDynamicZoneTemplate, CmsDynamicZoneTemplateWithTypename } from "~/types.js";
 import { TemplateGallery } from "./TemplateGallery.js";
 import { useTemplateTypename } from "~/admin/plugins/fieldRenderers/dynamicZone/useTemplateTypename.js";
-import { Button, Link, Text, Dialog } from "@webiny/admin-ui";
+import { Button, Dialog } from "@webiny/admin-ui";
 
 interface UseAddTemplateParams {
     onTemplate: (template: CmsDynamicZoneTemplateWithTypename) => void;
