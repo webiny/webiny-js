be2f70c4a2247e040239e564e4495665ff2c4def
adrians5j <adrian@webiny.com>
2026-01-20 16:40:52 +0100

    wip:11 (6i2p8)

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/advanced/components/Entry.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/ref/advanced/components/Entry.tsx
@@ -101,111 +101,104 @@ export const Entry = ({
             }}
             data-role="ref-field-entry"
             className={cn(
-                "w-full rounded-md bg-neutral-light p-sm-extra pr-lg hover:bg-neutral-dimmed border-md border-transparent data-[selected=true]:border-accent-dimmed",
+                "w-full rounded-md bg-neutral-light p-sm-extra pr-lg border-md border-transparent flex items-center gap-md data-[selected=true]:border-accent-dimmed hover:bg-neutral-dimmed",
                 { "hover:cursor-pointer": !!onChange }
             )}
         >
-            <div className="flex items-center gap-md">
-                {onChange && (
-                    <div>
-                        <Checkbox
-                            checked={selected}
-                            onChange={() =>
-                                onChange &&
-                                onChange({
-                                    id: entry.id,
-                                    modelId: entry.model.modelId
-                                })
-                            }
-                        />
-                    </div>
-                )}
-                <div className="flex items-center justify-between gap-lg min-w-0 ">
-                    <Image title={entry.title} src={entry.image} />
-                    <div
-                        className={
-                            "flex flex-col gap-xxs overflow-hidden flex-1 min-w-0 text-sm text-neutral-muted"
+            {onChange && (
+                <div>
+                    <Checkbox
+                        checked={selected}
+                        onChange={() =>
+                            onChange({
+                                id: entry.id,
+                                modelId: entry.model.modelId
+                            })
                         }
-                    >
-                        <div>{entry.model.name}</div>
+                    />
+                </div>
+            )}
 
-                        <div
-                            className={
-                                "text-md text-neutral-primary font-semibold mb-sm truncate min-w-0"
-                            }
-                        >
-                            {entry.title}
-                        </div>
+            <div className="flex items-center justify-between gap-lg w-full">
+                <Image title={entry.title} src={entry.image} />
+                <div
+                    className={
+                        "flex flex-col gap-xxs overflow-hidden flex-1 min-w-0 text-sm text-neutral-muted"
+                    }
+                >
+                    <div>{entry.model.name}</div>
 
-                        <div>
-                            <span className={"w-[60px] inline-block"}>Created:</span>
-                            <span>
-                                {entry.createdBy.displayName},{" "}
-                                <TimeAgo datetime={entry.createdOn} />
-                            </span>
-                        </div>
-                        {/* <div>
+                    <div className={"text-md text-neutral-primary font-semibold mb-sm"}>
+                        {entry.title}
+                    </div>
+
+                    <div>
+                        <span className={"w-[60px] inline-block"}>Created:</span>
+                        <span>
+                            {entry.createdBy.displayName}, <TimeAgo datetime={entry.createdOn} />
+                        </span>
+                    </div>
+                    {/* <div>
                         <span className={"w-[60px] inline-block"}>Location:</span>
                         <span>
                             Home / Content / Preview / ... / Manage / Retail / Local / Products
                         </span>
                     </div>*/}
-                    </div>
-                    <div className={"flex items-center gap-sm"}>
-                        <Tag
-                            content={
-                                <>
-                                    {entryStatusLabel}&nbsp;
-                                    <Text size={"sm"} className={"text-neutral-muted"}>
-                                        ({entryRevision})
-                                    </Text>
-                                </>
-                            }
-                            variant={"neutral-base-outline"}
+                </div>
+                <div className={"flex items-center gap-sm"}>
+                    <Tag
+                        content={
+                            <>
+                                {entryStatusLabel}&nbsp;
+                                <Text size={"sm"} className={"text-neutral-muted"}>
+                                    ({entryRevision})
+                                </Text>
+                            </>
+                        }
+                        variant={"neutral-base-outline"}
+                    />
+
+                    {placement == "multiRef" && (
+                        <>
+                            <div className={"flex gap-xs"}>
+                                <IconButton
+                                    disabled={!onMoveUpClick}
+                                    variant={"ghost"}
+                                    size={"sm"}
+                                    icon={<ArrowUp />}
+                                    onClick={onMoveUp}
+                                />
+                                <IconButton
+                                    disabled={!onMoveDownClick}
+                                    variant={"ghost"}
+                                    size={"sm"}
+                                    icon={<ArrowDown />}
+                                    onClick={onMoveDown}
+                                />
+                            </div>
+                        </>
+                    )}
+
+                    <DropdownMenu
+                        trigger={
+                            <IconButton variant={"ghost"} size={"sm"} icon={<MoreVertIcon />} />
+                        }
+                    >
+                        <DropdownMenu.Link
+                            icon={<OpenInNewIcon />}
+                            text={"Open in new tab"}
+                            to={link}
+                            target={"_blank"}
                         />
 
-                        {placement == "multiRef" && (
-                            <>
-                                <div className={"flex gap-xs"}>
-                                    <IconButton
-                                        disabled={!onMoveUpClick}
-                                        variant={"ghost"}
-                                        size={"sm"}
-                                        icon={<ArrowUp />}
-                                        onClick={onMoveUp}
-                                    />
-                                    <IconButton
-                                        disabled={!onMoveDownClick}
-                                        variant={"ghost"}
-                                        size={"sm"}
-                                        icon={<ArrowDown />}
-                                        onClick={onMoveDown}
-                                    />
-                                </div>
-                            </>
-                        )}
-
-                        <DropdownMenu
-                            trigger={
-                                <IconButton variant={"ghost"} size={"sm"} icon={<MoreVertIcon />} />
-                            }
-                        >
-                            <DropdownMenu.Link
-                                icon={<OpenInNewIcon />}
-                                text={"Open in new tab"}
-                                to={link}
-                                target={"_blank"}
+                        {onRemove && (
+                            <DropdownMenu.Item
+                                icon={<RemoveIcon />}
+                                text={placement === "multiRef" ? "Remove from list" : "Remove"}
+                                onClick={() => onRemove(entry.id)}
                             />
-
-                            {onRemove && (
-                                <DropdownMenu.Item
-                                    icon={<RemoveIcon />}
-                                    text={placement === "multiRef" ? "Remove from list" : "Remove"}
-                                    onClick={() => onRemove(entry.id)}
-                                />
-                            )}
-                        </DropdownMenu>
-                    </div>
+                        )}
+                    </DropdownMenu>
                 </div>
             </div>
         </div>
