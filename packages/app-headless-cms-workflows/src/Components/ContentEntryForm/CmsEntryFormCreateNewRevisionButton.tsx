e0c0a7a69d192cf66c6a628039792586eaea4a94
Pavel Denisjuk <pavel@webiny.com>
2026-01-21 20:15:43 +0100

    fix: always allow creation of new revision

--- packages/app-headless-cms-workflows/src/Components/ContentEntryForm/CmsEntryFormCreateNewRevisionButton.tsx
+++ packages/app-headless-cms-workflows/src/Components/ContentEntryForm/CmsEntryFormCreateNewRevisionButton.tsx
@@ -5,14 +5,12 @@ import { ContentEntryEditorConfig, usePermission } from "@webiny/app-headless-cm
 import { useContentEntryForm } from "@webiny/app-headless-cms/admin/components/ContentEntryForm/useContentEntryForm.js";
 import { usePersistEntry } from "@webiny/app-headless-cms/admin/hooks/usePersistEntry.js";
 import { ReactComponent as NewReleaseIcon } from "@webiny/icons/new_releases.svg";
-import { useWorkflowState } from "@webiny/app-workflows";
 
 const { Actions } = ContentEntryEditorConfig;
 
 const CreateNewRevisionMenuItem = () => {
     const toast = useToast();
     const { setRouteParams } = useRoute();
-    const { presenter } = useWorkflowState();
     const { canEdit } = usePermission();
     const { OptionsMenuItem } = Actions.MenuItemAction.useOptionsMenuItem();
     const { entry } = useContentEntryForm();
@@ -53,7 +51,6 @@ const CreateNewRevisionMenuItem = () => {
             icon={<NewReleaseIcon />}
             label={`Create New Revision`}
             onAction={onClick}
-            disabled={!presenter.vm.state}
             data-testid={"cms.content-form.header.createNewRevision"}
         />
     );
