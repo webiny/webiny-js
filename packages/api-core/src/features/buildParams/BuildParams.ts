a2748daead147711a0b6400085c9732f73ecea23
adrians5j <adrian@webiny.com>
2026-01-29 13:34:49 +0100

    wip: xad234

--- packages/api-core/src/features/buildParams/BuildParams.ts
+++ packages/api-core/src/features/buildParams/BuildParams.ts
@@ -1,12 +1,15 @@
-import type { Container } from "@webiny/di";
 import { BuildParam, BuildParams as BuildParamsAbstraction } from "./abstractions.js";
 
 export class BuildParamsImpl implements BuildParamsAbstraction.Interface {
-    constructor(private container: Container) {}
+    constructor(private params: BuildParam.Interface[]) {}
 
     get<T = any>(key: string): T | null {
-        const params = this.container.resolveAll<BuildParam.Interface>(BuildParam);
-        const param = params.find(p => p.key === key);
+        const param = this.params.find(p => p.key === key);
         return param ? param.value : null;
     }
 }
+
+export const BuildParams = BuildParamsAbstraction.createImplementation({
+    implementation: BuildParamsImpl,
+    dependencies: [[BuildParam, { multiple: true }]]
+});
