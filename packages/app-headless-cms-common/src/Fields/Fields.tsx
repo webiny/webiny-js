4ad5c15484b6b5d21abf1c7495dfa3ea518664ee
Pavel Denisjuk <pavel@webiny.com>
2026-02-05 23:20:11 +0100

    fix: remove redundant code and tests

--- packages/app-headless-cms-common/src/Fields/Fields.tsx
+++ packages/app-headless-cms-common/src/Fields/Fields.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// @ts-expect-error unable to load types, needs investigation
+// @ts-expect-error Remove this one moduleResolution is set to `bundler`
 import { compiler } from "markdown-to-jsx/react";
 import { Grid, type ColumnProps } from "@webiny/admin-ui";
 import { FieldElement } from "./FieldElement.js";
@@ -24,6 +24,10 @@ const getFieldById = (fields: CmsModelField[], id: string): CmsModelField | null
 };
 
 const withMarkdown = (field: CmsModelField) => {
+    if (typeof field.helpText !== "string") {
+        return field;
+    }
+
     return { ...field, helpText: compiler(field.helpText) };
 };
 
--- packages/migrations/src/utils/index.ts
+++ packages/migrations/src/utils/index.ts
@@ -1,7 +1,5 @@
 export * from "./dynamoDb.js";
 export * from "./elasticsearch/index.js";
-export * from "./forEachTenantLocale.js";
 export * from "./createEntity.js";
-export * from "./createLocaleEntity.js";
 export * from "./createTenantEntity.js";
 export * from "./getCompressedData.js";
