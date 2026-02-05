63d4f7ca3867fa5d21cdcdfd8567dd8913a9b225
adrians5j <adrian@webiny.com>
2026-02-05 23:07:32 +0100

    wip:8 (k3huz)

--- packages/webiny/src/cli/index.ts
+++ packages/webiny/src/cli/index.ts
@@ -1,1 +1,2 @@
+export { LoggerService as Logger } from "@webiny/cli-core/abstractions/services/LoggerService.js";
 export { UiService as Ui } from "@webiny/cli-core/abstractions/services/UiService.js";
