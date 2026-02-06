5ced0f6ef4665a99a158f3b938686a33062762e4
Bruno Zorić <bruno.zoric@gmail.com>
2026-02-07 00:24:25 +0100

    test(api-headless-cms): object dz converter

---
+++ packages/api-headless-cms/__tests__/converters/object/dynamicZone/object.dynamicZone.object.test.ts
@@ -0,0 +1,99 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { createModel } from "../../mocks/model.js";
+import { createModelField } from "../../mocks/field.js";
+import { getConverters, type IConvertersResponse } from "../../__helpers/converters.js";
+
+const plainValue = {
+    profile: {
+        content: {
+            _templateId: "objectTemplate",
+            settings: {
+                name: "Configuration Name"
+            }
+        }
+    }
+};
+const convertedValue = {
+    "object@profileId": {
+        "dynamicZone@contentId": {
+            _templateId: "objectTemplate",
+            "object@settingsId": {
+                "text@nameId": "Configuration Name"
+            }
+        }
+    }
+};
+
+const model = createModel({
+    fields: [
+        createModelField({
+            fieldId: "profile",
+            type: "object",
+            multipleValues: false,
+            settings: {
+                fields: [
+                    createModelField({
+                        fieldId: "content",
+                        type: "dynamicZone",
+                        multipleValues: false,
+                        settings: {
+                            templates: [
+                                {
+                                    id: "objectTemplate",
+                                    name: "Object Template",
+                                    gqlTypeName: "ObjectTemplate",
+                                    icon: undefined,
+                                    description: "",
+                                    fields: [
+                                        createModelField({
+                                            fieldId: "settings",
+                                            type: "object",
+                                            multipleValues: false,
+                                            settings: {
+                                                fields: [
+                                                    createModelField({
+                                                        fieldId: "name",
+                                                        type: "text",
+                                                        multipleValues: false
+                                                    })
+                                                ]
+                                            }
+                                        })
+                                    ],
+                                    layout: [],
+                                    validation: []
+                                }
+                            ]
+                        }
+                    })
+                ]
+            }
+        })
+    ]
+});
+
+describe("object storage converter - single object with dynamic zone with object template", () => {
+    let converters: IConvertersResponse;
+
+    beforeEach(async () => {
+        converters = await getConverters(model);
+    });
+
+    it("should convert single object with dynamic zone with object template to and from storage", async () => {
+        const { convertFromStorage, convertToStorage } = converters;
+
+        const storageResult = convertToStorage({
+            fields: model.fields,
+            values: plainValue
+        });
+
+        expect(storageResult).toEqual(convertedValue);
+
+        const fromStorageResult = convertFromStorage({
+            fields: model.fields,
+            values: storageResult
+        });
+
+        expect(fromStorageResult).toEqual(plainValue);
+    });
+});
