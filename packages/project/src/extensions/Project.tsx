22f92719303cdb22af2ec19f67aee41c21162ab3
adrians5j <adrian@webiny.com>
2026-02-07 06:33:30 +0100

    wip:4 (uggij)

--- packages/project/src/extensions/Project.tsx
+++ packages/project/src/extensions/Project.tsx
@@ -7,9 +7,7 @@ import {
     ApiAfterBuild,
     ApiBeforeDeploy,
     ApiBeforeWatch,
-    BeforeBuild,
     BeforeDeploy,
-    BeforeWatch,
     CoreBeforeDeploy
 } from "~/extensions/index.js";
 
