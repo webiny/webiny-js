d98351d4305088918011b7454c3ff8fa499ead71
Pavel Denisjuk <pavel@webiny.com>
2026-02-14 15:39:08 +0100

    fix: reformat code

--- packages/api-headless-cms/__tests__/contentAPI/cmsEntryValidation/mocks/field.base.ts
+++ packages/api-headless-cms/__tests__/contentAPI/cmsEntryValidation/mocks/field.base.ts
@@ -2,10 +2,11 @@ import type { CmsModelField } from "~/types";
 import ucFirst from "lodash/upperFirst";
 import { createMinLengthValidation, createRequiredValidation } from "./validations";
 
-export interface CreateFieldInput extends Pick<
-    CmsModelField,
-    "id" | "fieldId" | "type" | "label" | "listValidation" | "validation" | "multipleValues"
-> {
+export interface CreateFieldInput
+    extends Pick<
+        CmsModelField,
+        "id" | "fieldId" | "type" | "label" | "listValidation" | "validation" | "multipleValues"
+    > {
     parentId?: string;
 }
 
