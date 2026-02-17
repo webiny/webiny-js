f62eec779cb4e55e2c2760b5b9c2c4021dff4641
adrians5j <adrian@webiny.com>
2026-02-17 18:38:30 +0100

    wip:42 (7z8ab)

--- packages/api-headless-cms/src/graphql/schema/cms/typeDefs/mutation.ts
+++ packages/api-headless-cms/src/graphql/schema/cms/typeDefs/mutation.ts
@@ -7,10 +7,27 @@ class CmsMutationTypeDefs implements CoreGraphQLSchemaFactory.Interface {
         builder.addTypeDefs(/* GraphQL */ `
             type CmsMutation {
                 createEntry(modelId: ID!, data: JSON!, fields: [String!]!): CmsEntryResponse!
-                updateEntryRevision(modelId: ID!, revisionId: ID!, data: JSON!, fields: [String!]!): CmsEntryResponse!
-                deleteEntryRevision(modelId: ID!, revisionId: ID!, permanent: Boolean): CmsDeleteResponse!
-                publishEntryRevision(modelId: ID!, revisionId: ID!, fields: [String!]!): CmsEntryResponse!
-                unpublishEntryRevision(modelId: ID!, revisionId: ID!, fields: [String!]!): CmsEntryResponse!
+                updateEntryRevision(
+                    modelId: ID!
+                    revisionId: ID!
+                    data: JSON!
+                    fields: [String!]!
+                ): CmsEntryResponse!
+                deleteEntryRevision(
+                    modelId: ID!
+                    revisionId: ID!
+                    permanent: Boolean
+                ): CmsDeleteResponse!
+                publishEntryRevision(
+                    modelId: ID!
+                    revisionId: ID!
+                    fields: [String!]!
+                ): CmsEntryResponse!
+                unpublishEntryRevision(
+                    modelId: ID!
+                    revisionId: ID!
+                    fields: [String!]!
+                ): CmsEntryResponse!
             }
 
             extend type Mutation {
