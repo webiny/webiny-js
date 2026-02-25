5ccbd034af30be3d5ff6aa543720974b603bea80
adrians5j <adrian@webiny.com>
2026-02-25 06:55:22 +0100

    wip:4 (868vq)

--- packages/project-aws/src/project.ts
+++ packages/project-aws/src/project.ts
@@ -1,9 +1,8 @@
-import { ProjectId, Telemetry, FeatureFlags } from "@webiny/project/extensions/index.js";
+import { ProjectId, Telemetry } from "@webiny/project/extensions/index.js";
 import { AutoInstall } from "./extensions/index.js";
 
 export const Project = {
     Id: ProjectId,
     Telemetry: Telemetry,
-    FeatureFlags,
     AutoInstall
 };
