9636e614f7392ece994a2e53ccd0f469a7b2f2d3
adrians5j <adrian@webiny.com>
2026-03-05 14:49:50 +0100

    wip:1 (psprb)

---
+++ packages/project-aws/src/exports/infra/index.ts
@@ -0,0 +1,7 @@
+export {
+    ApiStackOutputService as ApiStackOutput,
+    CoreStackOutputService as CoreStackOutput,
+    ApiGqlClient,
+    AdminStackOutputService as AdminStackOutput,
+    InvokeLambdaFunction
+} from "~/abstractions/index.js";
