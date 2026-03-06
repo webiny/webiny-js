afdddaa1c968f68315f96ded59b84724affa1505
adrians5j <adrian@webiny.com>
2026-03-06 09:03:46 +0100

    wip:2 (dlrp2)

--- packages/project/src/exports/extensions.ts
+++
@@ -1,1 +0,0 @@
-export { EnvVar } from "~/extensions/EnvVar.js";
--- packages/webiny/src/extensions.ts
+++ packages/webiny/src/extensions.ts
@@ -1,4 +1,3 @@
-export { EnvVar } from "@webiny/project/extensions/EnvVar.js";
 export { Api } from "@webiny/project-aws/api.js";
 export { Admin } from "@webiny/project-aws/admin.js";
 export { Cli } from "@webiny/project-aws/cli.js";
