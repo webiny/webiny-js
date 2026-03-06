6aefed046bf682c0e79b692961372d1e33f4d96e
adrians5j <adrian@webiny.com>
2026-03-06 09:30:41 +0100

    wip:1 (7daf5)

--- packages/project-aws/src/extensions/AwsDefaultRegion.tsx
+++ packages/project-aws/src/extensions/AwsDefaultRegion.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { z } from "zod";
+import { defineExtension } from "@webiny/project/defineExtension/index.js";
 import { EnvVar } from "@webiny/project/extensions/index.js";
 
-export interface AwsDefaultRegionProps {
-    name: string;
-}
-
-export const AwsDefaultRegion = (props: AwsDefaultRegionProps) => {
-    return <EnvVar varName="AWS_REGION" value={props.name} />;
-};
+export const AwsDefaultRegion = defineExtension({
+    type: "Aws/DefaultRegion",
+    tags: { runtimeContext: "project" },
+    description: "Set the default AWS region for the project.",
+    paramsSchema: z.object({
+        name: z.string().describe("The AWS region name.")
+    }),
+    render(props) {
+        return <EnvVar varName="AWS_REGION" value={props.name} />;
+    }
+});
--- webiny.config.tsx
+++ webiny.config.tsx
@@ -29,13 +29,13 @@ export const Extensions = () => {
             {/*    pulumiSrc="/extensions/myLambdaFunction/pulumi.ts"*/}
             {/*/>*/}
 
-            {/*<Api.Extension src={"/extensions/models/ProductCategoryModel.ts"} />*/}
-            {/*<Api.Extension src={"/extensions/models/ProductModel.ts"} />*/}
-            {/*<Api.Extension src={"/extensions/models/contactSubmission/ContactSubmissionModel.ts"} />*/}
-            {/*<Api.Extension src={"/extensions/models/contactSubmission/ContactSubmissionHook.ts"} />*/}
-            {/*<Admin.Extension*/}
-            {/*    src={"/extensions/models/contactSubmission/EmailEntryListColumn.tsx"}*/}
-            {/*/>*/}
+            <Api.Extension src={"/extensions/models/ProductCategoryModel.ts"} />
+            <Api.Extension src={"/extensions/models/ProductModel.ts"} />
+            <Api.Extension src={"/extensions/models/contactSubmission/ContactSubmissionModel.ts"} />
+            <Api.Extension src={"/extensions/models/contactSubmission/ContactSubmissionHook.ts"} />
+            <Admin.Extension
+                src={"/extensions/models/contactSubmission/EmailEntryListColumn.tsx"}
+            />
 
             {/*<Api.BuildParam paramName="MY_CUSTOM_BUILD_PARAM" value="customValue" />*/}
             {/*<Api.BuildParam*/}
