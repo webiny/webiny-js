4b962573cb4ce7557eb668a1ce97ebc21cc87570
Pavel Denisjuk <pavel@webiny.com>
2026-03-06 14:54:26 +0100

    fix: allow relative urls in lexical link

--- packages/app-admin/src/components/LexicalEditor/LexicalLinkForm.tsx
+++ packages/app-admin/src/components/LexicalEditor/LexicalLinkForm.tsx
@@ -48,7 +48,7 @@ const UrlInput = () => {
 
     const urlBind = useBind({
         name: "url",
-        validators: validation.create("required,url")
+        validators: validation.create("required,url:allowRelative")
     });
 
     useEffect(() => {
