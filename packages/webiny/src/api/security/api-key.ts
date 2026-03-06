2dc07bcdbb114c9859cb74ae419a196920caed84
Pavel Denisjuk <pavel@webiny.com>
2026-03-06 12:12:51 +0100

    Merge branch 'next' into pavel/fix/v6-exports

--- packages/webiny/src/api/security/api-key.ts
+++ packages/webiny/src/api/security/api-key.ts
@@ -1,7 +1,19 @@
-export { ApiKeyAfterCreateHandler, ApiKeyBeforeCreateHandler, CreateApiKeyUseCase } from "@webiny/api-core/features/security/apiKeys/CreateApiKey/index.js";
-export { ApiKeyAfterDeleteHandler, ApiKeyBeforeDeleteHandler, DeleteApiKeyUseCase } from "@webiny/api-core/features/security/apiKeys/DeleteApiKey/index.js";
+export {
+    ApiKeyAfterCreateHandler,
+    ApiKeyBeforeCreateHandler,
+    CreateApiKeyUseCase
+} from "@webiny/api-core/features/security/apiKeys/CreateApiKey/index.js";
+export {
+    ApiKeyAfterDeleteHandler,
+    ApiKeyBeforeDeleteHandler,
+    DeleteApiKeyUseCase
+} from "@webiny/api-core/features/security/apiKeys/DeleteApiKey/index.js";
 export { GetApiKeyUseCase } from "@webiny/api-core/features/security/apiKeys/GetApiKey/index.js";
 export { GetApiKeyByTokenUseCase } from "@webiny/api-core/features/security/apiKeys/GetApiKeyByToken/index.js";
 export { ListApiKeysUseCase } from "@webiny/api-core/features/security/apiKeys/ListApiKeys/index.js";
-export { ApiKeyAfterUpdateHandler, ApiKeyBeforeUpdateHandler, UpdateApiKeyUseCase } from "@webiny/api-core/features/security/apiKeys/UpdateApiKey/index.js";
+export {
+    ApiKeyAfterUpdateHandler,
+    ApiKeyBeforeUpdateHandler,
+    UpdateApiKeyUseCase
+} from "@webiny/api-core/features/security/apiKeys/UpdateApiKey/index.js";
 export { ApiKeyFactory } from "@webiny/api-core/features/security/apiKeys/shared/abstractions.js";
--- webiny.config.tsx
+++ webiny.config.tsx
@@ -95,7 +95,6 @@ export const Extensions = () => {
             {/* Project 👇 */}
             <Project.Telemetry enabled={false} />
             <Project.FeatureFlags features={{ fileManager: { threatDetection: false } }} />
-            <Infra.
 
             {process.env.WEBINY_CLI_AUTO_INSTALL && (
                 <Project.AutoInstall
