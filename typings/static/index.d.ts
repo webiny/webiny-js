0ea71f95279ae2ac61860f6f7807f3bbcb837a9c
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-09 12:55:05 +0100

    fix: types

--- typings/static/index.d.ts
+++ typings/static/index.d.ts
@@ -2,6 +2,10 @@ declare module "*.css" {
     const content: Record<string, string>;
     export default content;
 }
+declare module "*.scss" {
+    const content: Record<string, string>;
+    export default content;
+}
 
 declare module "*.svg" {
     import { FunctionComponent, SVGProps } from "react";
