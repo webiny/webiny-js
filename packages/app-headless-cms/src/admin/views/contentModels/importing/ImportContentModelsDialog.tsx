4763988812ce83cdb16da1c01c87d3b73480547f
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-10 12:16:53 +0100

    fix(app-headless-cms): model import modal scroll

--- packages/app-headless-cms/src/admin/views/contentModels/importing/ImportContentModelsDialog.tsx
+++ packages/app-headless-cms/src/admin/views/contentModels/importing/ImportContentModelsDialog.tsx
@@ -1,14 +1,11 @@
 import React, { useCallback } from "react";
-import { CircularProgress } from "@webiny/ui/Progress/index.js";
-import * as UID from "@webiny/ui/Dialog/index.js";
-import { Dialog } from "~/admin/components/Dialog.js";
 import { i18n } from "@webiny/app/i18n/index.js";
-import { ImportButton } from "./components/ImportButton.js";
 import { FileUpload } from "./components/FileUpload.js";
 import { Errors } from "./components/Errors.js";
 import { DataList } from "./components/DataList.js";
 import { DataListInstructions } from "./components/Model/DataListInstructions.js";
 import { ImportContextProvider } from "~/admin/views/contentModels/importing/ImportContext.js";
+import { Dialog, Loader } from "@webiny/admin-ui";
 
 const t = i18n.ns("app-headless-cms/admin/views/content-models/import-content-models-dialog");
 
@@ -36,32 +33,36 @@ export const ImportContentModelsDialog = (props: ImportContentModelsDialogProps)
                 file
             }) => {
                 const onClick = validated ? handleModelsImport : handleModelsValidation;
+
+                const disabledConfirm =
+                    errors.length > 0 || loading || !file || (validated && !hasSelected());
+
                 return (
                     <Dialog
                         open={true}
                         onClose={onCloseClick}
                         data-testid="cms-import-content-models-modal"
-                    >
-                        {loading && <CircularProgress label={"Loading..."} />}
-                        <UID.DialogTitle>{t`Import Content Models`}</UID.DialogTitle>
-                        <UID.DialogContent>
-                            <FileUpload />
-                            {validated && <DataListInstructions />}
-                            <Errors errors={errors} />
-                            <DataList />
-                        </UID.DialogContent>
-                        <UID.DialogActions>
-                            <ImportButton
+                        title={t`Import Content Models`}
+                        actions={
+                            <Dialog.ConfirmAction
                                 onClick={onClick}
-                                validated={validated}
-                                disabled={
-                                    errors.length > 0 ||
-                                    loading ||
-                                    !file ||
-                                    (validated && !hasSelected())
-                                }
+                                text={validated ? t`Import` : t`Validate file`}
+                                disabled={disabledConfirm}
                             />
-                        </UID.DialogActions>
+                        }
+                    >
+                        {loading ? (
+                            <Loader text={"Loading..."} />
+                        ) : (
+                            <>
+                                <FileUpload />
+                                <div className={"max-h-[50dvh] overflow-auto"}>
+                                    {validated && <DataListInstructions />}
+                                    <Errors errors={errors} />
+                                    <DataList />
+                                </div>
+                            </>
+                        )}
                     </Dialog>
                 );
             }}
