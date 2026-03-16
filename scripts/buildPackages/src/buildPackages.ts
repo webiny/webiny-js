cda50c355e408e3678fa3fc22a9d1b806435f3a6
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-16 15:27:13 +0100

    wip: build on windows

--- scripts/buildPackages/src/buildPackages.ts
+++ scripts/buildPackages/src/buildPackages.ts
@@ -150,8 +150,6 @@ export const buildPackages = async () => {
                 const pkgBuildError = listrError.error as PackageBuildError;
                 console.log(red("✖ " + pkgBuildError.getPackage().name));
                 console.log(pkgBuildError.getBuildError().message);
-                // TODO remove
-                console.log(pkgBuildError.getBuildError());
                 console.log();
             });
 
