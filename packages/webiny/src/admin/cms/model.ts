918ee2a710f85aecd6ae45cf7da5a2f3c374c389
Pavel Denisjuk <pavel@webiny.com>
2026-03-17 22:03:51 +0100

    chore: reformat with prettier

--- packages/webiny/src/admin/cms/model.ts
+++ packages/webiny/src/admin/cms/model.ts
@@ -1,3 +1,6 @@
 export { RulesEditor } from "@webiny/app-headless-cms/admin/components/FieldEditor/EditFieldDialog/RulesEditor/index.js";
 export { PermissionsEditor } from "@webiny/app-headless-cms/admin/components/FieldEditor/EditFieldDialog/PermissionsEditor/PermissionsEditor.js";
-export { useFieldAccessControlRules, useFieldEffectiveRules } from "@webiny/app-headless-cms-common/Fields/index.js";
+export {
+    useFieldAccessControlRules,
+    useFieldEffectiveRules
+} from "@webiny/app-headless-cms-common/Fields/index.js";
--- packages/webiny/src/api/tenancy.ts
+++ packages/webiny/src/api/tenancy.ts
@@ -1,6 +1,25 @@
 export { TenantContext } from "@webiny/api-core/features/tenancy/TenantContext/index.js";
-export { CreateTenantUseCase, CreateTenantRepository, TenantBeforeCreateEventHandler, TenantAfterCreateEventHandler } from "@webiny/api-core/features/tenancy/CreateTenant/index.js";
+export {
+    CreateTenantUseCase,
+    CreateTenantRepository,
+    TenantBeforeCreateEventHandler,
+    TenantAfterCreateEventHandler
+} from "@webiny/api-core/features/tenancy/CreateTenant/index.js";
 export { GetTenantByIdUseCase } from "@webiny/api-core/features/tenancy/GetTenantById/index.js";
-export { UpdateTenantUseCase, UpdateTenantRepository, TenantAfterUpdateEventHandler, TenantBeforeUpdateEventHandler } from "@webiny/api-core/features/tenancy/UpdateTenant/index.js";
-export { DeleteTenantUseCase, DeleteTenantRepository, TenantAfterDeleteEventHandler, TenantBeforeDeleteEventHandler } from "@webiny/api-core/features/tenancy/DeleteTenant/index.js";
-export { InstallTenantUseCase, AppInstaller, TenantInstalledEventHandler } from "@webiny/api-core/features/tenancy/InstallTenant/index.js";
+export {
+    UpdateTenantUseCase,
+    UpdateTenantRepository,
+    TenantAfterUpdateEventHandler,
+    TenantBeforeUpdateEventHandler
+} from "@webiny/api-core/features/tenancy/UpdateTenant/index.js";
+export {
+    DeleteTenantUseCase,
+    DeleteTenantRepository,
+    TenantAfterDeleteEventHandler,
+    TenantBeforeDeleteEventHandler
+} from "@webiny/api-core/features/tenancy/DeleteTenant/index.js";
+export {
+    InstallTenantUseCase,
+    AppInstaller,
+    TenantInstalledEventHandler
+} from "@webiny/api-core/features/tenancy/InstallTenant/index.js";
