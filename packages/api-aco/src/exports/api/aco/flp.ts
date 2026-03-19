25528f95a4fb0d946e3e1c4636cd5e6e7379489b
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-19 14:07:03 +0100

    feat: generate package exports

---
+++ packages/api-aco/src/exports/api/aco/flp.ts
@@ -0,0 +1,6 @@
+export { CreateFlpUseCase } from "~/features/flp/CreateFlp/abstractions.js";
+export { DeleteFlpUseCase } from "~/features/flp/DeleteFlp/abstractions.js";
+export { FolderLevelPermissions } from "~/features/flp/FolderLevelPermissions/abstractions.js";
+export { GetFlpUseCase } from "~/features/flp/GetFlp/abstractions.js";
+export { ListFlpsUseCase } from "~/features/flp/ListFlps/abstractions.js";
+export { UpdateFlpUseCase } from "~/features/flp/UpdateFlp/abstractions.js";
---
+++ packages/webiny/src/api/aco/flp.ts
@@ -0,0 +1,6 @@
+export { CreateFlpUseCase } from "@webiny/api-aco/features/flp/CreateFlp/abstractions.js";
+export { DeleteFlpUseCase } from "@webiny/api-aco/features/flp/DeleteFlp/abstractions.js";
+export { FolderLevelPermissions } from "@webiny/api-aco/features/flp/FolderLevelPermissions/abstractions.js";
+export { GetFlpUseCase } from "@webiny/api-aco/features/flp/GetFlp/abstractions.js";
+export { ListFlpsUseCase } from "@webiny/api-aco/features/flp/ListFlps/abstractions.js";
+export { UpdateFlpUseCase } from "@webiny/api-aco/features/flp/UpdateFlp/abstractions.js";
