bfa360b56608515dbaa0e89e28d2681d5830640a
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-21 12:36:01 +0100

    fix: tests

--- packages/handler-aws/src/plugins/index.ts
+++ packages/handler-aws/src/plugins/index.ts
@@ -1,6 +1,7 @@
-import { createHandlerClientPlugin } from "./handlerClient.js";
 import type { Context } from "@webiny/handler/types.js";
 
 export const registerDefaultPlugins = (context: Context): void => {
-    context.plugins.register([createHandlerClientPlugin()]);
+    context.plugins.register([
+        //
+    ]);
 };
