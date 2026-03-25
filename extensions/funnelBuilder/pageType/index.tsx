c9599ab9aa513240ca195390ba45a1a27943305f
adrians5j <adrian@webiny.com>
2026-03-25 22:45:14 +0100

    wip

--- extensions/funnelBuilder/pageType/index.tsx
+++ extensions/funnelBuilder/pageType/index.tsx
@@ -5,6 +5,7 @@ import { FunnelPageForm } from "./FunnelPageForm.js";
 export default () => {
     return (
         <PageListConfig>
+            <PageListConfig.PageType name={"static"} remove />
             <PageListConfig.PageType
                 name={"funnel"}
                 label={"Funnel"}
