821c721aa1c55a5e27dc8ec5918bdda079e1719d
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-26 10:15:06 +0100

    fix: build

--- packages/db-dynamodb/src/utils/count.ts
+++ packages/db-dynamodb/src/utils/count.ts
@@ -2,6 +2,7 @@ import type { QueryAllParams } from "~/utils/query.js";
 
 export const count = async (params: QueryAllParams): Promise<number> => {
     const { entity, partitionKey, options = {} } = params;
+    // @ts-expect-error
     const { Count } = await entity.query(partitionKey, { ...options, select: "count" });
     return Count || 0;
 };
