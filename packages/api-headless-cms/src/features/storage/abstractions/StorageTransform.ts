269a89a8cd9cfc40816a92a4570b5b6bb4489e1f
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-07 14:19:22 +0200

    wip(api-headless-cms): date storage transform

--- packages/api-headless-cms/src/features/storage/abstractions/StorageTransform.ts
+++ packages/api-headless-cms/src/features/storage/abstractions/StorageTransform.ts
@@ -27,5 +27,7 @@ export const StorageTransform = createAbstraction<IStorageTransform<any, any, an
 export namespace StorageTransform {
     export type Interface<T = any, R = any, F extends CmsModelField = CmsModelField> = IStorageTransform<T, R, F>;
     export type ToStorageParams<T = any, R = any, F extends CmsModelField = CmsModelField> = IStorageTransformToStorageParams<T, R, F>;
+    export type ToStorageResponse<T = any> = Promise<T>;
     export type FromStorageParams<T = any, R = any, F extends CmsModelField = CmsModelField> = IStorageTransformFromStorageParams<T, R, F>;
+    export type FromStorageResponse<T = any> = Promise<T>;
 }
--- packages/api-headless-cms/src/features/storage/feature.ts
+++ packages/api-headless-cms/src/features/storage/feature.ts
@@ -1,9 +1,12 @@
 import { createFeature } from "@webiny/feature/api";
 import { StorageTransformRegistry } from "./StorageTransformRegistry.js";
+import { DateStorageTransform } from "./fields/DateStorageTransform.js";
 
 export const StorageFeature = createFeature({
     name: "Cms/StorageFeature",
     register: container => {
+        
+        container.register(DateStorageTransform);
         container.register(StorageTransformRegistry);
     }
 });
