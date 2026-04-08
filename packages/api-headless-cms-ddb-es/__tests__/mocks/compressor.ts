00c12632dbabca8f58789ad7f93d5c881e9f2f63
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-08 12:13:12 +0200

    fix: after merge

--- packages/api-headless-cms-ddb-es/__tests__/mocks/compressor.ts
+++ packages/api-headless-cms-ddb-es/__tests__/mocks/compressor.ts
@@ -1,9 +1,20 @@
 import { PluginsContainer } from "@webiny/plugins";
 import { Context } from "@webiny/api";
+import { CompressionHandler } from "@webiny/utils/exports/api.js";
 
 export const getContextWithCompressor = () => {
     return new Context({
         plugins: new PluginsContainer(),
         WEBINY_VERSION: "0.0.0"
     });
+};
+
+export const getCompressionHandler = () => {
+    const context = getContextWithCompressor();
+
+    const compressionHandler = context.container.resolve(CompressionHandler);
+    if (!compressionHandler) {
+        throw new Error("There is no CompressionHandler on context.");
+    }
+    return compressionHandler;
 };
