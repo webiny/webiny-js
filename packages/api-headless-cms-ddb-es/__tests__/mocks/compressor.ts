fec5fcc1e85c547a2ae6a6f5eccccb379d19b180
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-08 11:59:36 +0200

    test: fix

--- packages/api-headless-cms-ddb-es/__tests__/mocks/compressor.ts
+++ packages/api-headless-cms-ddb-es/__tests__/mocks/compressor.ts
@@ -1,28 +1,5 @@
-import { createDefaultCompressor } from "@webiny/utils/compression";
 import { PluginsContainer } from "@webiny/plugins";
 import { Context } from "@webiny/api";
-
-const compressor = createDefaultCompressor({
-    plugins: new PluginsContainer()
-});
-
-compressor.disable("jsonpack");
-
-export const compress = async (data: unknown): Promise<string> => {
-    const result = await compressor.compress(data);
-    if (!result || typeof result !== "object") {
-        throw new Error("Failed to compress data.");
-    }
-    // @ts-expect-error
-    return result.value as string;
-};
-
-export const decompress = async (data: string): Promise<unknown> => {
-    return await compressor.decompress({
-        compression: "gzip",
-        value: data
-    });
-};
 
 export const getContextWithCompressor = () => {
     return new Context({
