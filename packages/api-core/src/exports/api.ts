b640b6ba17237b2c22b1682383b876e5f0f85446
adrians5j <adrian@webiny.com>
2026-04-08 10:47:09 +0200

    wip:6 (6ttwk)

--- packages/api-core/src/exports/api.ts
+++ packages/api-core/src/exports/api.ts
@@ -2,5 +2,3 @@ export { Logger } from "~/features/logger/index.js";
 export { BuildParam, BuildParams } from "~/features/buildParams/index.js";
 export { DomainEvent, EventPublisher } from "~/features/eventPublisher/index.js";
 export { GlobalKeyValueStore, KeyValueStore } from "~/features/keyValueStore/index.js";
-export { DomainEvent, EventPublisher } from "~/features/eventPublisher/index.js";
-export { GlobalKeyValueStore, KeyValueStore } from "~/features/keyValueStore/index.js";
