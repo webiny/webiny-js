beef959102c3568d50ec7b7e9aa7d51623fe76b0
adrians5j <adrian@webiny.com>
2026-04-09 10:19:07 +0200

    wip:20 (3rtqq)

--- extensions/MyApiRoute.ts
+++ extensions/MyApiRoute.ts
@@ -1,5 +1,4 @@
-import { Route } from "webiny/api/route";
-import { Logger } from "webiny/api/logger";
+import { Logger, Route } from "webiny/api";
 
 class MyApiRouteImpl implements Route.Interface {
     constructor(private logger: Logger.Interface) {}
