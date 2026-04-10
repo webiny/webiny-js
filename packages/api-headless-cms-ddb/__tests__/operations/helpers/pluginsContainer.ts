b5273721f3b4e38c07419cb5371ad1c55457cbc2
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-10 14:30:28 +0200

    fix: cms ddb tests

--- packages/api-headless-cms-ddb/__tests__/operations/helpers/pluginsContainer.ts
+++ packages/api-headless-cms-ddb/__tests__/operations/helpers/pluginsContainer.ts
@@ -1,16 +1,12 @@
 import { PluginsContainer } from "@webiny/plugins";
 import type { PluginCollection } from "@webiny/plugins/types";
 import { createPlainObjectPathPlugin } from "~/dynamoDb/path/plainObject";
-import filters from "@webiny/db-dynamodb/plugins/filters";
 import { createFilterCreatePlugins } from "~/operations/entry/filtering/plugins";
-import dynamoDbValueFilters from "@webiny/db-dynamodb/plugins/filters";
 
 export const createPluginsContainer = (plugins: PluginCollection = []): PluginsContainer => {
     return new PluginsContainer([
-        ...filters(),
         createPlainObjectPathPlugin(),
         ...createFilterCreatePlugins(),
-        ...dynamoDbValueFilters(),
         ...plugins
     ]);
 };
