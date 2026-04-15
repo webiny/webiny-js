09df523f533bbcb9beaeeb5033645bc57f62af08
adrians5j <adrian@webiny.com>
2026-04-15 10:58:50 +0200

    wip:2 (zr7kr)

---
+++ packages/webiny/src/admin/languages.ts
@@ -0,0 +1,2 @@
+export type { LanguageDto } from "@webiny/languages/admin/features/listLanguages/index.js";
+export { useLanguages } from "@webiny/languages/admin/presentation/hooks/useLanguages.js";
