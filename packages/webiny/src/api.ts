5f6b41388ee1fb79afa2e954cb08cca1632f64fd
adrians5j <adrian@webiny.com>
2026-04-19 10:24:20 +0200

    wip:18 (zgimz)

--- packages/webiny/src/api.ts
+++ packages/webiny/src/api.ts
@@ -1,17 +1,9 @@
-export {
-    AiSdk,
-    AiSdkFactory,
-    AiConnectionFactory,
-    Ai
-} from "@webiny/api-core/features/ai/index.js";
+export { AiSdk, AiSdkFactory, AiConnectionFactory, Ai } from "@webiny/api-core/features/ai/index.js";
 export type { IAiConnection, IAiConnectionInline } from "@webiny/api-core/features/ai/index.js";
 export { Logger } from "@webiny/api-core/features/logger/index.js";
 export { BuildParam, BuildParams } from "@webiny/api-core/features/buildParams/index.js";
 export { DomainEvent, EventPublisher } from "@webiny/api-core/features/eventPublisher/index.js";
-export {
-    GlobalKeyValueStore,
-    KeyValueStore
-} from "@webiny/api-core/features/keyValueStore/index.js";
+export { GlobalKeyValueStore, KeyValueStore } from "@webiny/api-core/features/keyValueStore/index.js";
 export { createFeature, createAbstraction, Result, BaseError } from "@webiny/feature/api/index.js";
 export { Route } from "@webiny/handler/abstractions/Route.js";
 export { CompressionHandler } from "@webiny/utils/features/compression/abstractions/CompressionHandler.js";
