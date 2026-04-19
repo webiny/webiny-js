bd61c8a9d98fb7ebc9cbf218c11584da5e558c4a
adrians5j <adrian@webiny.com>
2026-04-19 22:26:26 +0200

    wip:30 (tpc2p)

--- packages/api-file-manager/src/features/ai/AiTagAfterCreateHandler.ts
+++ packages/api-file-manager/src/features/ai/AiTagAfterCreateHandler.ts
@@ -1,10 +1,10 @@
 import { FileAfterCreateEventHandler } from "~/features/file/CreateFile/events.js";
-import { TaskService } from "@webiny/api-core/features/task/TaskService/index.js";
-import type { IAiImageTaggingTaskInput } from "~/tasks/AiImageTaggingTask.js";
-import { AI_IMAGE_TAGGING_TASK_ID } from "~/tasks/AiImageTaggingTask.js";
+// import { TaskService } from "@webiny/api-core/features/task/TaskService/index.js";
+// import type { IAiImageTaggingTaskInput } from "~/tasks/AiImageTaggingTask.js";
+// import { AI_IMAGE_TAGGING_TASK_ID } from "~/tasks/AiImageTaggingTask.js";
 
 class AiTagAfterCreateHandlerImpl implements FileAfterCreateEventHandler.Interface {
-    constructor(private taskService: TaskService.Interface) {}
+    // constructor(private taskService: TaskService.Interface) {}
 
     async handle(event: FileAfterCreateEventHandler.Event): Promise<void> {
         const { file } = event.payload;
@@ -13,16 +13,19 @@ class AiTagAfterCreateHandlerImpl implements FileAfterCreateEventHandler.Interfa
             return;
         }
 
-        await this.taskService.trigger<IAiImageTaggingTaskInput>({
-            definition: AI_IMAGE_TAGGING_TASK_ID,
-            input: {
-                fileId: file.id
-            }
-        });
+        // TODO: enable back once ready.
+        // await this.taskService.trigger<IAiImageTaggingTaskInput>({
+        //     definition: AI_IMAGE_TAGGING_TASK_ID,
+        //     input: {
+        //         fileId: file.id
+        //     }
+        // });
     }
 }
 
 export const AiTagAfterCreateHandler = FileAfterCreateEventHandler.createImplementation({
     implementation: AiTagAfterCreateHandlerImpl,
-    dependencies: [TaskService]
+    dependencies: [
+        /*TaskService*/
+    ]
 });
