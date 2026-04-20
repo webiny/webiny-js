4ca96a465ba8ebbb474dae03b3f89cad637eaf51
adrians5j <adrian@webiny.com>
2026-04-20 22:37:13 +0200

    wip: bring back encr comp

--- webiny.config.tsx
+++ webiny.config.tsx
@@ -22,6 +22,8 @@ export const Extensions = () => {
             <Infra.Vpc enabled={false} />
             <Infra.OpenSearch enabled={false} />
 
+            <Infra.Encryption passphrase={"my-passphrase"} />
+
             <Infra.Aws.Tags tags={{ OWNER: "me", PROJECT: "my-project" }} />
             <Infra.Aws.Tags tags={{ OWNER2: "me2", PROJECT2: "my-project-2" }} />
             <Infra.Aws.DefaultRegion name={"eu-central-1"} />
