c5074a9491089f6d5a77cc5dbc638fddc5554082
adrians5j <adrian@webiny.com>
2026-04-22 22:01:42 +0200

    wip:2 (74usg)

--- packages/app-graphql-playground/src/index.tsx
+++ packages/app-graphql-playground/src/index.tsx
@@ -1,11 +1,12 @@
 import React, { memo } from "react";
 import type { ApolloClient } from "apollo-client";
 import { plugins } from "@webiny/plugins";
-import { useRouter, AdminConfig, AdminLayout } from "@webiny/app-admin";
+import { useRouter, AdminConfig, AdminLayout, HasPermission } from "@webiny/app-admin";
 import { ReactComponent as ApiPlaygroundIcon } from "@webiny/icons/graphql_playground.svg";
 import { ReactComponent as DevToolsIcon } from "@webiny/icons/developer_mode.svg";
 import Playground from "./plugins/Playground.js";
 import playgroundPlugins from "./plugins/index.js";
+import { SecurityPermission } from "./SecurityPermission.js";
 import { Routes } from "~/routes.js";
 
 const { Route, Menu } = AdminConfig;
@@ -23,44 +24,49 @@ const GraphQLPlaygroundExtension = ({ createApolloClient }: GraphQLPlaygroundPro
     plugins.register(playgroundPlugins);
 
     return (
-        <AdminConfig>
-            <Menu
-                name={"dev-tools"}
-                hideIfEmpty={true}
-                pin={"end"}
-                element={
-                    <Menu.Item
-                        text={"Dev Tools"}
-                        icon={<Menu.Item.Icon label="Dev Tools" element={<DevToolsIcon />} />}
-                    />
-                }
-            />
-            <Menu
-                name={"dev-tools.graphql"}
-                parent={"dev-tools"}
-                element={
-                    <Menu.Link
-                        text={"GraphQL Playground"}
-                        to={router.getLink(Routes.ApiPlayground)}
-                        icon={
-                            <Menu.Link.Icon
-                                label="GraphQL Playground"
-                                element={<ApiPlaygroundIcon />}
+        <>
+            <SecurityPermission />
+            <AdminConfig>
+                <Menu
+                    name={"dev-tools"}
+                    hideIfEmpty={true}
+                    pin={"end"}
+                    element={
+                        <Menu.Item
+                            text={"Dev Tools"}
+                            icon={<Menu.Item.Icon label="Dev Tools" element={<DevToolsIcon />} />}
+                        />
+                    }
+                />
+                <HasPermission any={["dev-tools.*", "graphql-playground.*"]}>
+                    <Menu
+                        name={"dev-tools.graphql"}
+                        parent={"dev-tools"}
+                        element={
+                            <Menu.Link
+                                text={"GraphQL Playground"}
+                                to={router.getLink(Routes.ApiPlayground)}
+                                icon={
+                                    <Menu.Link.Icon
+                                        label="GraphQL Playground"
+                                        element={<ApiPlaygroundIcon />}
+                                    />
+                                }
                             />
                         }
                     />
-                }
-            />
+                </HasPermission>
 
-            <Route
-                route={Routes.ApiPlayground}
-                element={
-                    <AdminLayout title={"GraphQL Playground"}>
-                        <Playground createApolloClient={createApolloClient} />
-                    </AdminLayout>
-                }
-            />
-        </AdminConfig>
+                <Route
+                    route={Routes.ApiPlayground}
+                    element={
+                        <AdminLayout title={"GraphQL Playground"}>
+                            <Playground createApolloClient={createApolloClient} />
+                        </AdminLayout>
+                    }
+                />
+            </AdminConfig>
+        </>
     );
 };
 
