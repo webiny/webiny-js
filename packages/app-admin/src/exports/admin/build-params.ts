79dc16009a7a9fb50937413472b89e9db8e4edea
Adrian Smijulj <adrian1358@gmail.com>
2026-04-27 15:12:14 +0200

    fix: export useBuildParams from main webiny/admin entry point (#5136)

--- packages/app-admin/src/exports/admin/build-params.ts
+++ packages/app-admin/src/exports/admin/build-params.ts
@@ -1,2 +1,5 @@
+/** @deprecated Import from "webiny/admin" instead. */
 export { useBuildParams } from "~/presentation/buildParams/useBuildParams.js";
+
+/** @deprecated Import from "webiny/admin" instead. */
 export { BuildParam, BuildParams } from "~/features/buildParams/index.js";
--- packages/webiny/src/admin/build-params.ts
+++ packages/webiny/src/admin/build-params.ts
@@ -1,2 +1,4 @@
+/** @deprecated Import from "webiny/admin" instead. */
 export { useBuildParams } from "@webiny/app-admin/presentation/buildParams/useBuildParams.js";
+/** @deprecated Import from "webiny/admin" instead. */
 export { BuildParam, BuildParams } from "@webiny/app-admin/features/buildParams/index.js";
