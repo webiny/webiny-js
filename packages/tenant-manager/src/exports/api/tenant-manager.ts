1d8cf6af0393768e6a3a5199813b6667d9466b46
adrians5j <adrian@webiny.com>
2026-04-28 11:14:26 +0200

    wip:1 (tgfqs)

--- packages/tenant-manager/src/exports/api/tenant-manager.ts
+++ packages/tenant-manager/src/exports/api/tenant-manager.ts
@@ -1,2 +1,44 @@
 export { TenantModelExtension } from "~/api/domain/TenantModelExtension.js";
 export type { TenantExtensions } from "~/shared/Tenant.js";
+
+export {
+    GetCurrentTenantUseCase,
+    type IGetCurrentTenantUseCase,
+    type IGetCurrentTenantUseCaseErrors
+} from "~/api/features/GetCurrentTenant/abstractions.js";
+export { GetCurrentTenantFeature } from "~/api/features/GetCurrentTenant/feature.js";
+export {
+    GetTenantByIdUseCase,
+    GetTenantByIdRepository,
+    type IGetTenantByIdUseCase,
+    type IGetTenantByIdRepository
+} from "~/api/features/GetTenantById/abstractions.js";
+export { GetTenantByIdFeature } from "~/api/features/GetTenantById/feature.js";
+export {
+    CreateTenantUseCase,
+    type ICreateTenantUseCase
+} from "~/api/features/CreateTenant/abstractions.js";
+export { CreateTenantFeature } from "~/api/features/CreateTenant/feature.js";
+export {
+    CreateAndInstallTenantUseCase,
+    type ICreateAndInstallTenantUseCase
+} from "~/api/features/CreateAndInstallTenant/abstractions.js";
+export { CreateAndInstallTenantFeature } from "~/api/features/CreateAndInstallTenant/feature.js";
+export {
+    UpdateTenantUseCase,
+    type IUpdateTenantUseCase
+} from "~/api/features/UpdateTenant/abstractions.js";
+export { UpdateTenantFeature } from "~/api/features/UpdateTenant/feature.js";
+export {
+    EnableTenantUseCase,
+    type IEnableTenantUseCase
+} from "~/api/features/EnableTenant/abstractions.js";
+export { EnableTenantFeature } from "~/api/features/EnableTenant/feature.js";
+export {
+    DisableTenantUseCase,
+    type IDisableTenantUseCase
+} from "~/api/features/DisableTenant/abstractions.js";
+export { DisableTenantFeature } from "~/api/features/DisableTenant/feature.js";
+export { default as AddCmsPermissions } from "~/api/features/AddCmsPermissions/AddCmsPermissions.js";
+export { AddCmsPermissionsFeature } from "~/api/features/AddCmsPermissions/feature.js";
+export { DeleteTenantOnEntryDeleteFeature } from "~/api/features/DeleteTenantOnEntryDelete/feature.js";
