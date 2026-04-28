584b5f458b3cd51c099df6ab231dd8c73d73df08
adrians5j <adrian@webiny.com>
2026-04-28 12:43:08 +0200

    wip:3 (3jhjt)

--- packages/tenant-manager/src/exports/api/tenant-manager.ts
+++ packages/tenant-manager/src/exports/api/tenant-manager.ts
@@ -1,22 +1,7 @@
-export { TenantModelExtension } from "~/api/domain/TenantModelExtension.js";
-
 export { GetCurrentTenantUseCase } from "~/api/features/GetCurrentTenant/abstractions.js";
-export { GetCurrentTenantFeature } from "~/api/features/GetCurrentTenant/feature.js";
-export {
-    GetTenantByIdUseCase,
-    GetTenantByIdRepository
-} from "~/api/features/GetTenantById/abstractions.js";
-export { GetTenantByIdFeature } from "~/api/features/GetTenantById/feature.js";
+export { GetTenantByIdUseCase } from "~/api/features/GetTenantById/abstractions.js";
 export { CreateTenantUseCase } from "~/api/features/CreateTenant/abstractions.js";
-export { CreateTenantFeature } from "~/api/features/CreateTenant/feature.js";
 export { CreateAndInstallTenantUseCase } from "~/api/features/CreateAndInstallTenant/abstractions.js";
-export { CreateAndInstallTenantFeature } from "~/api/features/CreateAndInstallTenant/feature.js";
 export { UpdateTenantUseCase } from "~/api/features/UpdateTenant/abstractions.js";
-export { UpdateTenantFeature } from "~/api/features/UpdateTenant/feature.js";
 export { EnableTenantUseCase } from "~/api/features/EnableTenant/abstractions.js";
-export { EnableTenantFeature } from "~/api/features/EnableTenant/feature.js";
 export { DisableTenantUseCase } from "~/api/features/DisableTenant/abstractions.js";
-export { DisableTenantFeature } from "~/api/features/DisableTenant/feature.js";
-export { default as AddCmsPermissions } from "~/api/features/AddCmsPermissions/AddCmsPermissions.js";
-export { AddCmsPermissionsFeature } from "~/api/features/AddCmsPermissions/feature.js";
-export { DeleteTenantOnEntryDeleteFeature } from "~/api/features/DeleteTenantOnEntryDelete/feature.js";
--- packages/webiny/src/api/tenant-manager.ts
+++ packages/webiny/src/api/tenant-manager.ts
@@ -1,21 +1,7 @@
-export { TenantModelExtension } from "@webiny/tenant-manager/api/domain/TenantModelExtension.js";
 export { GetCurrentTenantUseCase } from "@webiny/tenant-manager/api/features/GetCurrentTenant/abstractions.js";
-export { GetCurrentTenantFeature } from "@webiny/tenant-manager/api/features/GetCurrentTenant/feature.js";
-export {
-    GetTenantByIdUseCase,
-    GetTenantByIdRepository
-} from "@webiny/tenant-manager/api/features/GetTenantById/abstractions.js";
-export { GetTenantByIdFeature } from "@webiny/tenant-manager/api/features/GetTenantById/feature.js";
+export { GetTenantByIdUseCase } from "@webiny/tenant-manager/api/features/GetTenantById/abstractions.js";
 export { CreateTenantUseCase } from "@webiny/tenant-manager/api/features/CreateTenant/abstractions.js";
-export { CreateTenantFeature } from "@webiny/tenant-manager/api/features/CreateTenant/feature.js";
 export { CreateAndInstallTenantUseCase } from "@webiny/tenant-manager/api/features/CreateAndInstallTenant/abstractions.js";
-export { CreateAndInstallTenantFeature } from "@webiny/tenant-manager/api/features/CreateAndInstallTenant/feature.js";
 export { UpdateTenantUseCase } from "@webiny/tenant-manager/api/features/UpdateTenant/abstractions.js";
-export { UpdateTenantFeature } from "@webiny/tenant-manager/api/features/UpdateTenant/feature.js";
 export { EnableTenantUseCase } from "@webiny/tenant-manager/api/features/EnableTenant/abstractions.js";
-export { EnableTenantFeature } from "@webiny/tenant-manager/api/features/EnableTenant/feature.js";
 export { DisableTenantUseCase } from "@webiny/tenant-manager/api/features/DisableTenant/abstractions.js";
-export { DisableTenantFeature } from "@webiny/tenant-manager/api/features/DisableTenant/feature.js";
-export { default as AddCmsPermissions } from "@webiny/tenant-manager/api/features/AddCmsPermissions/AddCmsPermissions.js";
-export { AddCmsPermissionsFeature } from "@webiny/tenant-manager/api/features/AddCmsPermissions/feature.js";
-export { DeleteTenantOnEntryDeleteFeature } from "@webiny/tenant-manager/api/features/DeleteTenantOnEntryDelete/feature.js";
