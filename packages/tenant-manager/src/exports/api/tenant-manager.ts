1ffb0537fd3d3db9aa30739c5bde285d7609ee2c
adrians5j <adrian@webiny.com>
2026-04-28 11:44:21 +0200

    wip:2 (6lgoj)

--- packages/tenant-manager/src/exports/api/tenant-manager.ts
+++ packages/tenant-manager/src/exports/api/tenant-manager.ts
@@ -1,43 +1,21 @@
 export { TenantModelExtension } from "~/api/domain/TenantModelExtension.js";
-export type { TenantExtensions } from "~/shared/Tenant.js";
 
-export {
-    GetCurrentTenantUseCase,
-    type IGetCurrentTenantUseCase,
-    type IGetCurrentTenantUseCaseErrors
-} from "~/api/features/GetCurrentTenant/abstractions.js";
+export { GetCurrentTenantUseCase } from "~/api/features/GetCurrentTenant/abstractions.js";
 export { GetCurrentTenantFeature } from "~/api/features/GetCurrentTenant/feature.js";
 export {
     GetTenantByIdUseCase,
-    GetTenantByIdRepository,
-    type IGetTenantByIdUseCase,
-    type IGetTenantByIdRepository
+    GetTenantByIdRepository
 } from "~/api/features/GetTenantById/abstractions.js";
 export { GetTenantByIdFeature } from "~/api/features/GetTenantById/feature.js";
-export {
-    CreateTenantUseCase,
-    type ICreateTenantUseCase
-} from "~/api/features/CreateTenant/abstractions.js";
+export { CreateTenantUseCase } from "~/api/features/CreateTenant/abstractions.js";
 export { CreateTenantFeature } from "~/api/features/CreateTenant/feature.js";
-export {
-    CreateAndInstallTenantUseCase,
-    type ICreateAndInstallTenantUseCase
-} from "~/api/features/CreateAndInstallTenant/abstractions.js";
+export { CreateAndInstallTenantUseCase } from "~/api/features/CreateAndInstallTenant/abstractions.js";
 export { CreateAndInstallTenantFeature } from "~/api/features/CreateAndInstallTenant/feature.js";
-export {
-    UpdateTenantUseCase,
-    type IUpdateTenantUseCase
-} from "~/api/features/UpdateTenant/abstractions.js";
+export { UpdateTenantUseCase } from "~/api/features/UpdateTenant/abstractions.js";
 export { UpdateTenantFeature } from "~/api/features/UpdateTenant/feature.js";
-export {
-    EnableTenantUseCase,
-    type IEnableTenantUseCase
-} from "~/api/features/EnableTenant/abstractions.js";
+export { EnableTenantUseCase } from "~/api/features/EnableTenant/abstractions.js";
 export { EnableTenantFeature } from "~/api/features/EnableTenant/feature.js";
-export {
-    DisableTenantUseCase,
-    type IDisableTenantUseCase
-} from "~/api/features/DisableTenant/abstractions.js";
+export { DisableTenantUseCase } from "~/api/features/DisableTenant/abstractions.js";
 export { DisableTenantFeature } from "~/api/features/DisableTenant/feature.js";
 export { default as AddCmsPermissions } from "~/api/features/AddCmsPermissions/AddCmsPermissions.js";
 export { AddCmsPermissionsFeature } from "~/api/features/AddCmsPermissions/feature.js";
--- packages/webiny/src/api/tenant-manager.ts
+++ packages/webiny/src/api/tenant-manager.ts
@@ -1,34 +1,20 @@
 export { TenantModelExtension } from "@webiny/tenant-manager/api/domain/TenantModelExtension.js";
-export type { TenantExtensions } from "@webiny/tenant-manager/shared/Tenant.js";
 export { GetCurrentTenantUseCase } from "@webiny/tenant-manager/api/features/GetCurrentTenant/abstractions.js";
-export {
-    type IGetCurrentTenantUseCase,
-    IGetCurrentTenantUseCaseErrors
-} from "@webiny/tenant-manager/api/features/GetCurrentTenant/abstractions.js";
 export { GetCurrentTenantFeature } from "@webiny/tenant-manager/api/features/GetCurrentTenant/feature.js";
 export {
     GetTenantByIdUseCase,
     GetTenantByIdRepository
 } from "@webiny/tenant-manager/api/features/GetTenantById/abstractions.js";
-export {
-    type IGetTenantByIdUseCase,
-    IGetTenantByIdRepository
-} from "@webiny/tenant-manager/api/features/GetTenantById/abstractions.js";
 export { GetTenantByIdFeature } from "@webiny/tenant-manager/api/features/GetTenantById/feature.js";
 export { CreateTenantUseCase } from "@webiny/tenant-manager/api/features/CreateTenant/abstractions.js";
-export { type ICreateTenantUseCase } from "@webiny/tenant-manager/api/features/CreateTenant/abstractions.js";
 export { CreateTenantFeature } from "@webiny/tenant-manager/api/features/CreateTenant/feature.js";
 export { CreateAndInstallTenantUseCase } from "@webiny/tenant-manager/api/features/CreateAndInstallTenant/abstractions.js";
-export { type ICreateAndInstallTenantUseCase } from "@webiny/tenant-manager/api/features/CreateAndInstallTenant/abstractions.js";
 export { CreateAndInstallTenantFeature } from "@webiny/tenant-manager/api/features/CreateAndInstallTenant/feature.js";
 export { UpdateTenantUseCase } from "@webiny/tenant-manager/api/features/UpdateTenant/abstractions.js";
-export { type IUpdateTenantUseCase } from "@webiny/tenant-manager/api/features/UpdateTenant/abstractions.js";
 export { UpdateTenantFeature } from "@webiny/tenant-manager/api/features/UpdateTenant/feature.js";
 export { EnableTenantUseCase } from "@webiny/tenant-manager/api/features/EnableTenant/abstractions.js";
-export { type IEnableTenantUseCase } from "@webiny/tenant-manager/api/features/EnableTenant/abstractions.js";
 export { EnableTenantFeature } from "@webiny/tenant-manager/api/features/EnableTenant/feature.js";
 export { DisableTenantUseCase } from "@webiny/tenant-manager/api/features/DisableTenant/abstractions.js";
-export { type IDisableTenantUseCase } from "@webiny/tenant-manager/api/features/DisableTenant/abstractions.js";
 export { DisableTenantFeature } from "@webiny/tenant-manager/api/features/DisableTenant/feature.js";
 export { default as AddCmsPermissions } from "@webiny/tenant-manager/api/features/AddCmsPermissions/AddCmsPermissions.js";
 export { AddCmsPermissionsFeature } from "@webiny/tenant-manager/api/features/AddCmsPermissions/feature.js";
