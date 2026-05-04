1fe2ecaa6a130c8cb14267aaa5c330235460bb7d
adrians5j <adrian@webiny.com>
2026-05-04 18:14:43 +0200

    fix: move viewport selector to top app bar

--- packages/app-website-builder/src/BaseEditor/defaultConfig/Content/ContentPreviewConfig.tsx
+++ packages/app-website-builder/src/BaseEditor/defaultConfig/Content/ContentPreviewConfig.tsx
@@ -3,12 +3,14 @@ import { EditorConfig } from "~/BaseEditor/index.js";
 import { Breadcrumbs } from "./Breadcrumbs/index.js";
 import { DocumentPreview } from "./Preview/DocumentPreview.js";
 import { AddressBar } from "./AddressBar/AddressBar.js";
+import { BreakpointSelector } from "./AddressBar/BreakpointSelector.js";
 
 export const ContentPreviewConfig = () => {
     const { Ui } = EditorConfig;
 
     return (
         <>
+            <Ui.TopBar.Element name={"breakpointSelector"} group={"center"} element={<BreakpointSelector />} />
             <Ui.Content.Element name="addressBar" element={<AddressBar />} />
             <Ui.Content.Element name="iframe" element={<DocumentPreview />} />
             <Ui.Content.Element
