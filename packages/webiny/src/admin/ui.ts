65990b33339bac4f32091b3474575494b9cb0ca7
adrians5j <adrian@webiny.com>
2026-05-06 13:48:59 +0200

    wip:9 (qbuz5)

--- packages/webiny/src/admin/ui.ts
+++ packages/webiny/src/admin/ui.ts
@@ -1,4 +1,9 @@
-export { FormComponentDescription, FormComponentLabel, FormComponentNote, FormComponentErrorMessage } from "@webiny/admin-ui/FormComponent/index.js";
+export {
+    FormComponentDescription,
+    FormComponentLabel,
+    FormComponentNote,
+    FormComponentErrorMessage
+} from "@webiny/admin-ui/FormComponent/index.js";
 export { Accordion } from "@webiny/admin-ui/Accordion/index.js";
 export { Alert } from "@webiny/admin-ui/Alert/index.js";
 export { AutoComplete } from "@webiny/admin-ui/AutoComplete/index.js";
@@ -9,7 +14,25 @@ export { Checkbox } from "@webiny/admin-ui/Checkbox/index.js";
 export { CheckboxGroup } from "@webiny/admin-ui/CheckboxGroup/index.js";
 export { CodeEditor } from "@webiny/admin-ui/CodeEditor/index.js";
 export { ColorPicker } from "@webiny/admin-ui/ColorPicker/index.js";
-export { CloneIcon, CreateIcon, DataList, DataListModal, DataListWithSections, DeleteIcon, DownloadIcon, EditIcon, FilterIcon, ListIcon, LoginIcon, NextPageIcon, OptionsIcon, PreviousPageIcon, RefreshIcon, SortIcon, UploadIcon } from "@webiny/admin-ui/DataList/index.js";
+export {
+    CloneIcon,
+    CreateIcon,
+    DataList,
+    DataListModal,
+    DataListWithSections,
+    DeleteIcon,
+    DownloadIcon,
+    EditIcon,
+    FilterIcon,
+    ListIcon,
+    LoginIcon,
+    NextPageIcon,
+    OptionsIcon,
+    PreviousPageIcon,
+    RefreshIcon,
+    SortIcon,
+    UploadIcon
+} from "@webiny/admin-ui/DataList/index.js";
 export { DataTable } from "@webiny/admin-ui/DataTable/index.js";
 export { DelayedOnChange } from "@webiny/admin-ui/DelayedOnChange/index.js";
 export { Dialog } from "@webiny/admin-ui/Dialog/index.js";
