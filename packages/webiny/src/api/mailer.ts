61cd47fbed6d053149c9d1a1ca16f9420b6db406
adrians5j <5121148+adrians5j@users.noreply.github.com>
2026-05-07 12:45:57 +0000

    chore: regenerate webiny package [skip ci]

--- packages/webiny/src/api/mailer.ts
+++ packages/webiny/src/api/mailer.ts
@@ -1,17 +1,4 @@
-export {
-    SendMailUseCase,
-    MailSendErrorEventHandler,
-    MailBeforeSendEventHandler,
-    MailAfterSendEventHandler
-} from "@webiny/api-mailer/features/SendMail/index.js";
-export {
-    GetSettingsUseCase,
-    GetSettingsRepository
-} from "@webiny/api-mailer/features/GetSettings/index.js";
-export {
-    SaveSettingsUseCase,
-    SaveSettingsRepository,
-    MailerSettingsAfterSaveEventHandler,
-    MailerSettingsBeforeSaveEventHandler
-} from "@webiny/api-mailer/features/SaveSettings/index.js";
+export { SendMailUseCase, MailSendErrorEventHandler, MailBeforeSendEventHandler, MailAfterSendEventHandler } from "@webiny/api-mailer/features/SendMail/index.js";
+export { GetSettingsUseCase, GetSettingsRepository } from "@webiny/api-mailer/features/GetSettings/index.js";
+export { SaveSettingsUseCase, SaveSettingsRepository, MailerSettingsAfterSaveEventHandler, MailerSettingsBeforeSaveEventHandler } from "@webiny/api-mailer/features/SaveSettings/index.js";
 export { MailerService } from "@webiny/api-mailer/domain/MailerService/abstractions.js";
