d3a90b3f05df3b4460f2ac7397d75c72ceadde02
Adrian Smijulj <adrian1358@gmail.com>
2026-05-07 13:42:49 +0200

    fix: dialog loading overlay now covers the full dialog surface (#5168)

--- packages/app-admin-ui/src/Dialog/DialogContainer.tsx
+++ packages/app-admin-ui/src/Dialog/DialogContainer.tsx
@@ -55,6 +55,7 @@ export const DialogContainer = () => {
             style={style}
             showCloseButton={showCloseButton}
             title={title}
+            loading={isLoading ? loading : false}
             actions={
                 !actions.cancel && !actions.accept ? null : (
                     <>
@@ -75,7 +76,6 @@ export const DialogContainer = () => {
                 )
             }
         >
-            {isLoading ? loading : null}
             {message}
         </Dialog>
     );
