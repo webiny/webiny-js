8ef294d30600ab90e2d001f6c5d2d98118b34373
SvenAlHamad <3808420+SvenAlHamad@users.noreply.github.com>
2026-05-07 11:50:50 +0000

    chore: regenerate webiny package [skip ci]

--- packages/webiny/src/admin/cms.ts
+++ packages/webiny/src/admin/cms.ts
@@ -13,20 +13,6 @@ export { useModel } from "@webiny/app-headless-cms/admin/components/ModelProvide
 export { usePermission } from "@webiny/app-headless-cms/admin/hooks/usePermission.js";
 export { IsModelPublishable } from "@webiny/app-headless-cms/admin/components/IsModelPublishable.js";
 export { Routes } from "@webiny/app-headless-cms/routes.js";
-export {
-    EntryAfterCreateEventHandler,
-    EntryAfterUpdateEventHandler,
-    EntryAfterDeleteEventHandler
-} from "@webiny/app-headless-cms/features/contentEntry/events/index.js";
-export type {
-    EntryAfterCreatePayload,
-    EntryAfterUpdatePayload,
-    EntryAfterDeletePayload
-} from "@webiny/app-headless-cms/features/contentEntry/events/index.js";
-export type {
-    CmsContentEntry,
-    CmsModel,
-    CmsModelField,
-    CmsModelLayoutField,
-    CmsIdentity
-} from "@webiny/app-headless-cms-common/types/index.ts";
+export { EntryAfterCreateEventHandler, EntryAfterUpdateEventHandler, EntryAfterDeleteEventHandler } from "@webiny/app-headless-cms/features/contentEntry/events/index.js";
+export type { EntryAfterCreatePayload, EntryAfterUpdatePayload, EntryAfterDeletePayload } from "@webiny/app-headless-cms/features/contentEntry/events/index.js";
+export type { CmsContentEntry, CmsModel, CmsModelField, CmsModelLayoutField, CmsIdentity } from "@webiny/app-headless-cms-common/types/index.ts";
