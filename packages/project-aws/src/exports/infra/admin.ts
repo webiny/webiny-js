a67e7166ee61ebf43e40937b711f8c833132f002
adrians5j <adrian@webiny.com>
2026-05-12 13:32:48 +0200

    wip:5 (57tlf)

--- packages/project-aws/src/exports/infra/admin.ts
+++ packages/project-aws/src/exports/infra/admin.ts
@@ -1,2 +1,3 @@
 export { AdminPulumi } from "~/abstractions/features/pulumi/AdminPulumi.js";
 export { AdminStackOutputService as GetAdminStackOutput } from "~/abstractions/services/AdminStackOutputService.js";
+export { SetAdminCustomDomains } from "~/abstractions/features/pulumi/SetAdminCustomDomains.js";
