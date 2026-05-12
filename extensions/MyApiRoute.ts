158a13ed55225b573dd06362a485c3e04b9d8a47
adrians5j <adrian@webiny.com>
2026-05-12 23:03:56 +0200

    wip:2 (y0ch1)

--- extensions/MyApiRoute.ts
+++ extensions/MyApiRoute.ts
@@ -1,30 +1,14 @@
-import { Route, Websockets } from "webiny/api";
+import { Route } from "webiny/api";
 
-interface FileEnrichmentBody {
-    file: { id: string; name: string };
-    tags: string[];
-    description: string;
-}
-
-class NotifyFileEnrichmentRoute implements Route.Interface {
-    constructor(private websockets: Websockets.Interface) {}
+class MyApiRouteImpl implements Route.Interface {
+    constructor() {}
 
     async execute(request: Route.Request, reply: Route.Reply) {
-        const { file, tags, description } = request.body as FileEnrichmentBody;
-
-        const connectionsResult = await this.websockets.listConnections();
-        if (connectionsResult.isOk() && connectionsResult.value.length > 0) {
-            await this.websockets.sendToConnections(connectionsResult.value, {
-                action: "fm.file.enrichment",
-                data: { id: file.id, name: file.name, tags, description }
-            });
-        }
-
-        reply.send({ notified: connectionsResult.value?.length ?? 0 });
+        return reply.send({ message: "Hello world!" });
     }
 }
 
 export default Route.createImplementation({
-    implementation: NotifyFileEnrichmentRoute,
-    dependencies: [Websockets]
+    implementation: MyApiRouteImpl,
+    dependencies: []
 });
