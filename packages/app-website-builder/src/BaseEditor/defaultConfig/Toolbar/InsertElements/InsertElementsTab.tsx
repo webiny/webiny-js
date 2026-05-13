da7b4422734c18790b8a6eaa1a427eed16738c23
adrians5j <adrian@webiny.com>
2026-05-13 18:21:32 +0200

    wip

--- packages/app-website-builder/src/BaseEditor/defaultConfig/Toolbar/InsertElements/InsertElementsTab.tsx
+++ packages/app-website-builder/src/BaseEditor/defaultConfig/Toolbar/InsertElements/InsertElementsTab.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@webiny/admin-ui";
 import { ReactComponent as InsertIcon } from "@webiny/icons/add_circle_outline.svg";
 import { Toolbar } from "~/BaseEditor/config/Toolbar/Toolbar.js";
 import { InsertElements } from "./InsertElements.js";
-1;
+
 export const InsertElementsTab = () => (
     <Toolbar.Tab
         name={"insert"}
