69618f1143e15c41d00de0d0f91d0bf56e0effc7
adrians5j <adrian@webiny.com>
2026-05-18 13:57:19 +0200

    wip

--- webiny.config.tsx
+++ webiny.config.tsx
@@ -25,6 +25,7 @@ export const Extensions = () => {
             <Infra.OpenSearch enabled={false} />
 
             <Infra.Encryption passphrase={"my-passphrase"} />
+            {/*<Infra.Api.MaxBundleSize sizeInMb={2}  />*/}
 
             <Infra.Aws.Tags tags={{ OWNER: "me", PROJECT: "my-project" }} />
             <Infra.Aws.Tags tags={{ OWNER2: "me2", PROJECT2: "my-project-2" }} />
