47f242d2b75efdc867361f674b5df109ef12cd50
Pavel Denisjuk <pavel@webiny.com>
2026-05-22 13:44:42 +0200

    wip: add exports for file manager

---
+++ packages/webiny/src/admin/ui/file-manager.ts
@@ -0,0 +1,5 @@
+export { FileUrlFormatter } from "@webiny/app-admin/features/fileUrlFormatter/abstractions.js";
+export { FileManager } from "@webiny/app-admin/base/ui/FileManager.js";
+export type { FileManagerFileItem } from "@webiny/app-admin/base/ui/FileManager.js";
+export { FileManagerViewConfig as FileManagerConfig } from "@webiny/app-file-manager/presentation/config/FileManagerViewConfig.js";
+export { useFileManagerConfig } from "@webiny/app-file-manager/presentation/config/FileManagerViewConfig.js";
