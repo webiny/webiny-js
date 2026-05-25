ec020f04c94dab6faa34872a6b872303ce4990e1
adrians5j <adrian@webiny.com>
2026-05-25 11:32:25 +0200

    wip:2 (zfjwu)

--- packages/webiny/src/api/security/user.ts
+++ packages/webiny/src/api/security/user.ts
@@ -1,18 +1,6 @@
-export {
-    CreateUserUseCase,
-    UserAfterCreateEventHandler,
-    UserBeforeCreateEventHandler
-} from "@webiny/api-core/features/users/CreateUser/index.js";
-export {
-    DeleteUserUseCase,
-    UserAfterDeleteEventHandler,
-    UserBeforeDeleteEventHandler
-} from "@webiny/api-core/features/users/DeleteUser/index.js";
-export {
-    UpdateUserUseCase,
-    UserAfterUpdateEventHandler,
-    UserBeforeUpdateEventHandler
-} from "@webiny/api-core/features/users/UpdateUser/index.js";
+export { CreateUserUseCase, UserAfterCreateEventHandler, UserBeforeCreateEventHandler } from "@webiny/api-core/features/users/CreateUser/index.js";
+export { DeleteUserUseCase, UserAfterDeleteEventHandler, UserBeforeDeleteEventHandler } from "@webiny/api-core/features/users/DeleteUser/index.js";
+export { UpdateUserUseCase, UserAfterUpdateEventHandler, UserBeforeUpdateEventHandler } from "@webiny/api-core/features/users/UpdateUser/index.js";
 export { GetUserUseCase } from "@webiny/api-core/features/users/GetUser/index.js";
 export { ListUsersUseCase } from "@webiny/api-core/features/users/ListUsers/index.js";
 export { ListUserTeamsUseCase } from "@webiny/api-core/features/users/ListUserTeams/index.js";
