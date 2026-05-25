8ef89115623ed2be674224d85e9b9595864387db
adrians5j <adrian@webiny.com>
2026-05-25 11:32:48 +0200

    wip:3 (h91ir)

--- packages/webiny/src/api/webhooks.ts
+++ packages/webiny/src/api/webhooks.ts
@@ -1,6 +1,21 @@
-export { WebhookSignPayload, WebhookVerifyPayload, WebhookDispatcher, WebhookFactory, WebhookProvider } from "@webiny/api-core/features/webhooks/index.js";
-export type { Webhook, WebhookCmsEntry, WebhookCmsEntryValues } from "@webiny/webhooks/api/domain/Webhook.js";
-export type { WebhookDelivery, WebhookDeliveryCmsEntry, WebhookDeliveryCmsEntryValues, WebhookDeliveryStatus } from "@webiny/webhooks/api/domain/WebhookDelivery.js";
+export {
+    WebhookSignPayload,
+    WebhookVerifyPayload,
+    WebhookDispatcher,
+    WebhookFactory,
+    WebhookProvider
+} from "@webiny/api-core/features/webhooks/index.js";
+export type {
+    Webhook,
+    WebhookCmsEntry,
+    WebhookCmsEntryValues
+} from "@webiny/webhooks/api/domain/Webhook.js";
+export type {
+    WebhookDelivery,
+    WebhookDeliveryCmsEntry,
+    WebhookDeliveryCmsEntryValues,
+    WebhookDeliveryStatus
+} from "@webiny/webhooks/api/domain/WebhookDelivery.js";
 export type { IWebhookPayload } from "@webiny/webhooks/api/features/SendWebhookTask/types.js";
 export type { IListMeta } from "@webiny/webhooks/api/features/ListWebhooks/abstractions.js";
 export { createWebhooks } from "@webiny/webhooks/api/index.js";
