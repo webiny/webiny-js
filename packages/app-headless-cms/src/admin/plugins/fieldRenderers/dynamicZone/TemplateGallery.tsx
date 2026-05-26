b526ece7de4c467542f2bf949267144c740ec6e3
adrians5j <adrian@webiny.com>
2026-05-26 10:12:08 +0200

    wip:6 (23sit)

--- packages/app-headless-cms/src/admin/plugins/fieldRenderers/dynamicZone/TemplateGallery.tsx
+++ packages/app-headless-cms/src/admin/plugins/fieldRenderers/dynamicZone/TemplateGallery.tsx
@@ -3,11 +3,9 @@ import type { DecoratableComponent } from "@webiny/app-admin";
 import { makeDecoratable, withDecoratorFactory } from "@webiny/app-admin";
 import type { CmsDynamicZoneTemplate } from "~/types.js";
 import { useModel, useModelField } from "~/admin/hooks/index.js";
-import { TemplateItem } from "./TemplateItem.js";
-import { TemplateIcon } from "./TemplateIcon.js";
-import { Text, Button, Input, Icon, DelayedOnChange, ToggleGroup } from "@webiny/admin-ui";
-import { Dialog } from "@webiny/admin-ui";
-import { ReactComponent as PlusIcon } from "@webiny/icons/add.svg";
+import { TemplateGridItem } from "./TemplateGridItem.js";
+import { TemplateListItem } from "./TemplateListItem.js";
+import { Input, Icon, DelayedOnChange, ToggleGroup } from "@webiny/admin-ui";
 import { ReactComponent as SearchIcon } from "@webiny/icons/search.svg";
 import { ReactComponent as GridIcon } from "@webiny/icons/grid_view.svg";
 import { ReactComponent as ListIcon } from "@webiny/icons/list.svg";
@@ -36,44 +34,6 @@ export interface TemplateGalleryProps {
     onClose: () => void;
     templates?: CmsDynamicZoneTemplate[];
 }
-
-interface TemplateListItemProps {
-    template: CmsDynamicZoneTemplate;
-    onTemplate: (template: CmsDynamicZoneTemplate) => void;
-}
-
-// Used #f1f2f4 b/c in Figma, the color was result of multiple colors combined.
-const TemplateListItem = ({ template, onTemplate }: TemplateListItemProps) => {
-    return (
-        <Dialog.Close asChild>
-            <div
-                onClick={() => onTemplate(template)}
-                className={
-                    "group flex items-center gap-y-md py-sm-extra px-md rounded-lg bg-neutral-light hover:bg-[#f1f2f4] cursor-pointer"
-                }
-            >
-                <div className={"flex items-center justify-center shrink-0 pr-md"}>
-                    <TemplateIcon icon={template.icon} style={{ width: 24, height: 24 }} />
-                </div>
-                <div className={"flex-1 min-w-0"}>
-                    <Text size={"md"} className={"text-neutral-primary font-semibold truncate"}>
-                        {template.name}
-                    </Text>
-                    {template.description && (
-                        <Text size={"sm"} as={"div"} className={"text-neutral-muted truncate"}>
-                            {template.description}
-                        </Text>
-                    )}
-                </div>
-                <div className={"hidden group-hover:block"}>
-                    <Button size={"md"} variant={"primary"} icon={<PlusIcon />}>
-                        Insert
-                    </Button>
-                </div>
-            </div>
-        </Dialog.Close>
-    );
-};
 
 const Gallery = makeDecoratable("TemplateGallery", (props: TemplateGalleryProps) => {
     const { field } = useModelField();
@@ -120,7 +80,7 @@ const Gallery = makeDecoratable("TemplateGallery", (props: TemplateGalleryProps)
             {viewMode === "grid" ? (
                 <GalleryList>
                     {filteredTemplates.map(template => (
-                        <TemplateItem
+                        <TemplateGridItem
                             key={template.id}
                             template={template}
                             onTemplate={props.onTemplate}
@@ -163,5 +123,5 @@ function withShouldRender<T extends DecoratableComponent>(Component: T) {
 export const TemplateGallery = Object.assign(withShouldRender(Gallery), {
     Container: withShouldRender(GalleryContainer),
     List: withShouldRender(GalleryList),
-    Item: withShouldRender(TemplateItem)
+    Item: withShouldRender(TemplateGridItem)
 });
