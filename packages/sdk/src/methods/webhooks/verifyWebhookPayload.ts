c9cf01c1f9287c5b33a36137fe774944e07502e8
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-02 10:55:27 +0200

    fix(sdk): export verify webhook payload

---
+++ packages/sdk/src/methods/webhooks/verifyWebhookPayload.ts
@@ -0,0 +1,30 @@
+/**
+ * This is a node only file, as it uses the "standardwebhooks" package which is not compatible with browsers.
+ * NEVER import into UI or export alongside ui-facing code.
+ */
+import { Webhook } from "standardwebhooks";
+import { Result } from "~/Result.js";
+
+export interface WebhookSignPayloadHeaders {
+    "webhook-id": string;
+    "webhook-timestamp": string;
+    "webhook-signature": string;
+}
+
+export const createWebhookVerifyPayload = (secret: string | undefined) => {
+    if (!secret) {
+        return async () => {
+            throw new Error("Signing secret is not defined.");
+        };
+    }
+
+    return async <T>(rawBody: string | Buffer, headers: WebhookSignPayloadHeaders) => {
+        try {
+            const wh = new Webhook(secret);
+            const payload = wh.verify(rawBody, headers) as T;
+            return Result.ok<T>(payload);
+        } catch (error) {
+            return Result.fail(error);
+        }
+    };
+};
