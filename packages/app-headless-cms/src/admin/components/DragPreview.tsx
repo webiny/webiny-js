e5cc3b10f3868d0f599c54ca1eb025c3e5c6a30c
adrians5j <adrian@webiny.com>
2026-06-05 15:46:10 +0200

    wip:22 (ojfgj)

--- packages/app-headless-cms/src/admin/components/DragPreview.tsx
+++ packages/app-headless-cms/src/admin/components/DragPreview.tsx
@@ -1,66 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import type { DragLayerMonitor } from "react-dnd";
 import { useDragLayer } from "react-dnd";
-import { plugins } from "@webiny/plugins";
 import { DragCursor } from "@webiny/admin-ui";
 import { subscribeToDropZoneOver } from "./dropZoneOverState.js";
-import type {
-    CmsModelFieldTypePlugin,
-    CmsModelLayoutFieldTypePlugin,
-    DragSource
-} from "~/types.js";
-
-const getDragInfo = (item: DragSource | null): { label: string; icon?: React.ReactElement } => {
-    if (!item) {
-        return { label: "" };
-    }
-
-    if (item.type === "newField" && item.fieldType) {
-        const plugin = plugins
-            .byType<CmsModelFieldTypePlugin>("cms-editor-field-type")
-            .find(p => p.field.type === item.fieldType);
-        return {
-            label: plugin?.field.label ?? item.fieldType,
-            icon: plugin?.field.icon as React.ReactElement | undefined
-        };
-    }
-
-    if (item.type === "field" && item.field) {
-        const plugin = plugins
-            .byType<CmsModelFieldTypePlugin>("cms-editor-field-type")
-            .find(p => p.field.type === item.field!.type);
-        return {
-            label: item.field.label,
-            icon: plugin?.field.icon as React.ReactElement | undefined
-        };
-    }
-
-    if (item.type === "newLayoutField" && item.layoutFieldType) {
-        const plugin = plugins
-            .byType<CmsModelLayoutFieldTypePlugin>("cms-editor-layout-field-type")
-            .find(p => p.field.type === item.layoutFieldType);
-        return {
-            label: plugin?.field.label ?? item.layoutFieldType,
-            icon: plugin?.field.icon as React.ReactElement | undefined
-        };
-    }
-
-    if (item.type === "layoutField" && item.layoutField) {
-        const plugin = plugins
-            .byType<CmsModelLayoutFieldTypePlugin>("cms-editor-layout-field-type")
-            .find(p => p.field.type === item.layoutField!.type);
-        return {
-            label: plugin?.field.label ?? "Layout",
-            icon: plugin?.field.icon as React.ReactElement | undefined
-        };
-    }
-
-    if (item.type === "row") {
-        return { label: "Row" };
-    }
-
-    return { label: "Field" };
-};
+import { getDragInfo } from "./getDragInfo.js";
+import type { DragSource } from "~/types.js";
 
 let dragPreviewRef: HTMLDivElement | null = null;
 
