168eb7baa1346c5f86d5a083161f3af1cc388a63
adrians5j <adrian@webiny.com>
2026-06-05 14:23:37 +0200

    wip:21 (tw2en)

--- packages/app-headless-cms/src/admin/components/ContentModelEditor/FieldsSidebar.tsx
+++ packages/app-headless-cms/src/admin/components/ContentModelEditor/FieldsSidebar.tsx
@@ -3,40 +3,11 @@ import React from "react";
 import { plugins } from "@webiny/plugins";
 import Draggable from "../Draggable.js";
 import type { CmsModelFieldTypePlugin, CmsModelLayoutFieldTypePlugin } from "~/types.js";
-import { Icon, IconButton } from "@webiny/admin-ui";
+import { IconButton } from "@webiny/admin-ui";
+import { GridItem } from "./GridItem.js";
 import { SectionHeader } from "./SectionHeader.js";
 import { FieldsGrid } from "./FieldsGrid.js";
 import { ReactComponent as CollapseSidebarIcon } from "@webiny/icons/right_panel_open.svg";
-
-interface GridItemProps {
-    testId: string;
-    label: string;
-    icon: React.ReactElement;
-    onDragStart: DragEventHandler;
-    dragRef: (element: HTMLElement | null) => void;
-}
-
-const GridItem = ({ testId, label, icon, onDragStart, dragRef }: GridItemProps) => {
-    return (
-        <div
-            ref={dragRef}
-            data-testid={testId}
-            onDragStart={onDragStart}
-            className={
-                "flex flex-col items-center justify-center gap-[8px] size-20 bg-neutral-subtle rounded-md cursor-grab shrink-0"
-            }
-        >
-            <Icon icon={icon} label={label} size={"lg"} color={"neutral-strong"} />
-            <span
-                className={
-                    "text-xs font-normal text-neutral-strong text-center leading-4 overflow-hidden text-ellipsis whitespace-nowrap w-full px-[8px]"
-                }
-            >
-                {label}
-            </span>
-        </div>
-    );
-};
 
 interface FieldProps {
     onFieldDragStart: DragEventHandler;
