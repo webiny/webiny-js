1d689402d68bb6840bbf368bc527a6407661b97e
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-11 14:36:33 +0200

    feat(api-core-sql): add SqlServiceManifestLoader, wire into createApiCoreSql

--- packages/api-core-sql/src/index.ts
+++ packages/api-core-sql/src/index.ts
@@ -1,2 +1,3 @@
 export { createApiCoreSql } from "./createApiCoreSql.js";
 export { getSqlTablePrefix } from "./getSqlTablePrefix.js";
+export { SqlServiceManifestLoader } from "./serviceDiscovery/index.js";
