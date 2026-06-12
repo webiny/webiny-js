99e0ec3ab031f395d789802b25057f793a428e55
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-12 09:55:27 +0200

    fix: format

--- packages/api-website-builder-scheduler/__tests__/redirectActionHandlers.test.ts
+++ packages/api-website-builder-scheduler/__tests__/redirectActionHandlers.test.ts
@@ -53,6 +53,7 @@ describe("Redirect Action Handlers", () => {
 
         const publishActionResult = await schedulePublishRedirect.execute({
             id: redirect.id,
+            tenant: "root",
             scheduleFor: new Date(Date.now() + 100000)
         });
 
@@ -65,6 +66,7 @@ describe("Redirect Action Handlers", () => {
 
         await executeScheduledAction.execute({
             id: publishActionResult.value!.scheduledAction.id,
+            tenant: "root",
             namespace: publishActionResult.value!.scheduledAction.namespace
         });
 
@@ -93,10 +95,12 @@ describe("Redirect Action Handlers", () => {
 
         const publishResult = await schedulePublishRedirect.execute({
             id: redirect.id,
+            tenant: "root",
             scheduleFor: new Date(Date.now() + 100000)
         });
         await executeScheduledAction.execute({
             id: publishResult.value!.scheduledAction.id,
+            tenant: "root",
             namespace: publishResult.value!.scheduledAction.namespace
         });
 
@@ -105,10 +109,12 @@ describe("Redirect Action Handlers", () => {
 
         const unpublishResult = await scheduleUnpublishRedirect.execute({
             id: redirect.id,
+            tenant: "root",
             scheduleFor: new Date(Date.now() + 1000000)
         });
         await executeScheduledAction.execute({
             id: unpublishResult.value!.scheduledAction.id,
+            tenant: "root",
             namespace: unpublishResult.value!.scheduledAction.namespace
         });
 
