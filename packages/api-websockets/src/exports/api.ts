9c5078e62712af14a8610abd2c90753872618717
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-17 12:38:41 +0200

    fix: websockets export

--- packages/api-websockets/src/exports/api.ts
+++ packages/api-websockets/src/exports/api.ts
@@ -3,4 +3,5 @@ export { WebsocketsListConnectionsUseCase } from "~/features/ListConnections/abs
 export { WebsocketsSendToIdentityUseCase } from "~/features/SendToIdentity/abstractions.js";
 export { WebsocketsSendToConnectionsUseCase } from "~/features/SendToConnections/abstractions.js";
 export { WebsocketsDisconnectUseCase } from "~/features/Disconnect/abstractions.js";
+export { ConnectionRegistry } from "~/features/ConnectionRegistry/abstractions.js";
 export type { WebsocketsError } from "~/features/shared/errors.js";
--- packages/api-websockets/src/index.ts
+++ packages/api-websockets/src/index.ts
@@ -1,24 +1,13 @@
 import type { Plugin } from "@webiny/plugins/types.js";
 import { createRegisterExtensionPlugin } from "@webiny/handler";
-import { WebsocketsFeature as Feature } from "./features/feature.js";
+import { WebsocketsFeature } from "./features/feature.js";
 import { WebsocketsGraphQLFactoryFeature } from "./graphql/feature.js";
 
 export const createWebsockets = (): Plugin[] => {
     const featurePlugin = createRegisterExtensionPlugin(context => {
-        Feature.register(context.container);
+        WebsocketsFeature.register(context.container);
         WebsocketsGraphQLFactoryFeature.register(context.container);
     });
     featurePlugin.name = "websockets.feature";
     return [featurePlugin];
 };
-
-export { WebsocketsFeature } from "./features/feature.js";
-export { ConnectionRegistry } from "./features/ConnectionRegistry/abstractions.js";
-
-export type * from "./validator/index.js";
-export { WebsocketsTransport } from "./transport/index.js";
-export * from "./runner/index.js";
-export * from "./registry/index.js";
-
-export * from "./plugins/index.js";
-export type * from "./types.js";
