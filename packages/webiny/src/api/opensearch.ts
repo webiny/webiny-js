bc280ec670af7148079869bf218ceeb31beb03bc
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-19 11:52:13 +0200

    chore: regenerate exports

--- packages/webiny/src/api/opensearch.ts
+++ packages/webiny/src/api/opensearch.ts
@@ -1,3 +1,5 @@
 export { createOpenSearchClient } from "@webiny/api-opensearch/client.js";
 export { OpenSearchClient } from "@webiny/api-opensearch/features/OpenSearchClient/abstraction.js";
 export { OpenSearchClientFactory } from "@webiny/api-opensearch/features/OpenSearchClientFactory/abstraction.js";
+export { OpenSearchQueryBuilderOperator } from "@webiny/api-opensearch/features/OpenSearchQueryBuilderOperator/abstractions/OpenSearchQueryBuilderOperator.js";
+export { OpenSearchQueryBuilderOperatorRegistry } from "@webiny/api-opensearch/features/OpenSearchQueryBuilderOperator/abstractions/OpenSearchQueryBuilderOperatorRegistry.js";
