a4a3f471393c3818149a87884d6b1b9addce3a32
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-25 13:39:58 +0200

    chore: regenerate webiny

--- packages/webiny/src/api/db.ts
+++ packages/webiny/src/api/db.ts
@@ -1,7 +1,25 @@
 export { DbRegistry, DbRegistryFeature } from "@webiny/db/features/DbRegistry/index.js";
 export { DynamoDBClient } from "@webiny/db-dynamodb/features/DynamoDBClient/index.js";
+export { DynamoDbDocumentClient } from "@webiny/db-dynamodb/features/DynamoDbDocumentClient/abstractions.js";
 export {
-    ValueFilter,
-    ValueFilterRegistry
-} from "@webiny/db-dynamodb/features/ValueFilter/index.js";
+    type IScanParams,
+    IScanResponse,
+    IQueryParams,
+    IQueryPageResponse
+} from "@webiny/db-dynamodb/features/DynamoDbDocumentClient/abstractions.js";
+export { DynamoDbTableFactory } from "@webiny/db-dynamodb/features/DynamoDbTableFactory/abstractions.js";
+export {
+    DynamoDbEntityFactory,
+    standardEntityAttributes,
+    globalEntityAttributes
+} from "@webiny/db-dynamodb/features/DynamoDbEntityFactory/abstractions.js";
+export {
+    type IStandardEntityAttributes,
+    IGlobalEntityAttributes
+} from "@webiny/db-dynamodb/features/DynamoDbEntityFactory/abstractions.js";
+export { DynamoDbBatchFactory } from "@webiny/db-dynamodb/features/DynamoDbBatchFactory/abstractions.js";
+export type { IEntity } from "@webiny/db-dynamodb/utils/entity/types.js";
+export type { EntityQueryOptions } from "@webiny/db-dynamodb/utils/entity/types.js";
+export { ValueFilter } from "@webiny/db-dynamodb/features/ValueFilter/index.js";
+export { ValueFilterRegistry } from "@webiny/db-dynamodb/features/ValueFilter/index.js";
 export { FilterUtil } from "@webiny/db-dynamodb/features/FilterUtil/index.js";
