7a18cf509d3b9a8c6f9867171ea9a46526469f5c
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-29 09:40:06 +0200

    refactor(app-workflows): remove apollo client

--- packages/app-workflows/src/presentation/workflowStatesWidget/components/WorkflowStatesOwnWidget.tsx
+++ packages/app-workflows/src/presentation/workflowStatesWidget/components/WorkflowStatesOwnWidget.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from "react";
-import { useFeature } from "@webiny/app";
 import { useCanUseWorkflows } from "~/hooks/canUseWorkflows.js";
 import { Alert } from "@webiny/admin-ui";
 import { WorkflowStateWidgetCard } from "./Card/WorkflowStatesWidgetCard.js";
 import { WorkflowStateValue } from "~/types.js";
-import { WorkflowStatesWidgetPresenterProvider } from "~/presentation/workflowStatesWidget/useWorkflowStatesWidgetPresenter.js";
-import { WorkflowStatesWidgetPresenterFeature } from "~/presentation/workflowStatesWidget/feature.js";
+import { useWorkflowStatesWidgetPresenter } from "~/presentation/workflowStatesWidget/useWorkflowStatesWidgetPresenter.js";
 
 export const WorkflowStatesOwnWidget = () => {
     const canUseWorkflows = useCanUseWorkflows();
-    const { presenter } = useFeature(WorkflowStatesWidgetPresenterFeature);
+    const presenter = useWorkflowStatesWidgetPresenter();
 
     useEffect(() => {
         presenter.init({
@@ -32,15 +30,12 @@ export const WorkflowStatesOwnWidget = () => {
     }
 
     return (
-        <WorkflowStatesWidgetPresenterProvider presenter={presenter}>
-            <WorkflowStateWidgetCard
-                title={
-                    <span>
-                        <span className={"text-accent-primary"}>Content Reviews</span> assigned by
-                        me
-                    </span>
-                }
-            />
-        </WorkflowStatesWidgetPresenterProvider>
+        <WorkflowStateWidgetCard
+            title={
+                <span>
+                    <span className={"text-accent-primary"}>Content Reviews</span> assigned by me
+                </span>
+            }
+        />
     );
 };
