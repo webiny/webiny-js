c9571503752e53e77924d101614a7a6812337ec5
Bruno Zorić <bruno.zoric@gmail.com>
2026-06-30 09:04:14 +0200

    refactor(app-audit-logs): remove apollo client (#5342)

--- packages/app-audit-logs/src/hooks/graphql.ts
+++ packages/app-audit-logs/src/hooks/graphql.ts
@@ -1,6 +1,3 @@
-import gql from "graphql-tag";
-import type { IAuditLogRaw, IAuditLogsError, IAuditLogsMeta } from "~/types.js";
-
 export interface IListAuditLogsVariablesWhere {
     app?: string;
     action?: string;
@@ -10,63 +7,3 @@ export interface IListAuditLogsVariablesWhere {
     createdOn_gte?: Date;
     createdOn_lte?: Date;
 }
-
-export interface IListAuditLogsVariables {
-    where: Partial<IListAuditLogsVariablesWhere>;
-    after: string | undefined;
-    sort: "ASC" | "DESC" | undefined;
-    limit: number | undefined;
-}
-
-export type IListAuditLogsVariablesPartial = Partial<IListAuditLogsVariables>;
-
-export interface IListAuditLogsResponse {
-    auditLogs: {
-        listAuditLogs: {
-            data: IAuditLogRaw[] | null;
-            meta: IAuditLogsMeta | null;
-            error: IAuditLogsError | null;
-        };
-    };
-}
-
-export const LIST_AUDIT_LOGS = gql`
-    query ListAuditLogs(
-        $where: ListAuditLogsWhere!
-        $sort: AuditLogsSort
-        $after: String
-        $limit: Number
-    ) {
-        auditLogs {
-            listAuditLogs(where: $where, after: $after, sort: $sort, limit: $limit) {
-                data {
-                    id
-                    createdBy {
-                        id
-                        displayName
-                        type
-                    }
-                    createdOn
-                    app
-                    action
-                    message
-                    entity
-                    entityId
-                    tags
-                    expiresAt
-                    content
-                }
-                meta {
-                    hasMoreItems
-                    cursor
-                }
-                error {
-                    message
-                    code
-                    data
-                    stack
-                }
-            }
-        }
-    }
-`;
