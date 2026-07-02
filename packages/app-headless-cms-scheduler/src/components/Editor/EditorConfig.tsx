90d5cbd5dd7f1afa7165a8adf62386e5f33f710f
Pavel Denisjuk <pavel@webiny.com>
2026-07-02 19:00:57 +0200

    fix: import from internal package

--- packages/app-headless-cms-scheduler/src/components/Editor/EditorConfig.tsx
+++ packages/app-headless-cms-scheduler/src/components/Editor/EditorConfig.tsx
@@ -1,6 +1,6 @@
-import { IsModelPublishable } from "@webiny/app-headless-cms/exports/admin/cms.js";
-import { InternalContentEntryEditorConfig } from "@webiny/app-headless-cms/exports/admin/cms/entry/editor.js";
 import React from "react";
+import { InternalContentEntryEditorConfig } from "@webiny/app-headless-cms";
+import { IsModelPublishable } from "@webiny/app-headless-cms/exports/admin/cms.js";
 import { MenuItem } from "./MenuItem.js";
 
 const { Actions } = InternalContentEntryEditorConfig;
