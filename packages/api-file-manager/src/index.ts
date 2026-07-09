31f77c66b3b10ab0bfc62869c06224718b4c17c4
adrians5j <adrian@webiny.com>
2026-07-09 12:05:18 +0200

    wip:11 (19y8s)

--- packages/api-file-manager/src/index.ts
+++ packages/api-file-manager/src/index.ts
@@ -1,45 +1,8 @@
-import type { ApiCoreContext } from "@webiny/api-core/types/core.js";
-import { ContextPlugin } from "@webiny/api";
 import { setupAssetDelivery } from "./delivery/setupAssetDelivery.js";
-import { FileManagerFeature } from "~/features/FileManagerFeature.js";
-import { FmPermissionsFeature } from "~/features/permissions/feature.js";
-import { GetModelUseCase } from "@webiny/api-headless-cms/features/contentModel/GetModel/index.js";
-import { FileModel as FileModelAbstraction } from "~/domain/file/abstractions.js";
-import { TenantContext } from "@webiny/api-core/features/tenancy/TenantContext/index.js";
-import { IdentityContext } from "@webiny/api-core/features/security/IdentityContext/abstractions.js";
-import { FILE_MODEL_ID, FileModel } from "~/domain/file/file.model.js";
 import { createRegisterExtensionPlugin } from "@webiny/handler";
 import { AssetDeliveryFeature } from "~/features/assetDelivery/feature.js";
 
 export * from "./delivery/index.js";
-
-export const createFileManagerContext = () => {
-    const extensionPlugin = createRegisterExtensionPlugin(context => {
-        const container = context.container;
-        container.register(FileModel);
-        FmPermissionsFeature.register(container);
-        FileManagerFeature.register(container);
-    });
-    extensionPlugin.name = "file-manager.extension";
-
-    const contextPlugin = new ContextPlugin<ApiCoreContext>(async context => {
-        const container = context.container;
-        const tenantContext = container.resolve(TenantContext);
-        const getModel = container.resolve(GetModelUseCase);
-
-        if (!tenantContext.getTenant()) {
-            return;
-        }
-
-        await container.resolve(IdentityContext).withoutAuthorization(async () => {
-            const fileModel = await getModel.execute(FILE_MODEL_ID);
-            container.registerInstance(FileModelAbstraction, fileModel.value);
-        });
-    });
-    contextPlugin.name = "file-manager.createContext";
-
-    return [extensionPlugin, contextPlugin];
-};
 
 export const createAssetDelivery = () => {
     return [
