b19242f63b5460374cc23b3ff74369952cd59bd2
brunozoric <bruno.zoric@gmail.com>
2026-07-21 09:55:04 +0200

    refactor(api-headless-cms-ddb, api-headless-cms-ddb-es): resolve filter registries from container, remove plugins threading

--- packages/api-headless-cms-ddb/__tests__/operations/helpers/createTestContainer.ts
+++ packages/api-headless-cms-ddb/__tests__/operations/helpers/createTestContainer.ts
@@ -1,8 +1,10 @@
 import { Container } from "@webiny/feature/api";
 import { ValueFilterFeature } from "@webiny/db-utils";
+import { FilterRegistriesFeature } from "@webiny/api-headless-cms-storage";
 
 export const createTestContainer = () => {
     const container = new Container();
     ValueFilterFeature.register(container);
+    FilterRegistriesFeature.register(container);
     return container;
 };
