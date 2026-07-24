e391de4e778cda1f61cee3ddc2bbd4c48c9d36d2
brunozoric <bruno.zoric@gmail.com>
2026-07-24 09:55:35 +0200

    feat(api-search-index-tasks): add IndexManagerFactory abstraction

--- packages/api-search-index-tasks/src/index.ts
+++ packages/api-search-index-tasks/src/index.ts
@@ -1,3 +1,4 @@
 export { StorageScanner } from "./abstractions/StorageScanner.js";
 export { IndexManager } from "./abstractions/IndexManager.js";
 export { StorageWriter } from "./abstractions/StorageWriter.js";
+export { IndexManagerFactory } from "./abstractions/IndexManagerFactory.js";
